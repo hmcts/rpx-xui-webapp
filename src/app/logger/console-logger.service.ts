f33aa53cadbb3b456a520394f9ff100be500871e
Dmitry Panov <dpanov76@yahoo.com>
2019-04-26 11:42:25 +0100

    RIUI-1742 - done

--- src/app/logger/console-logger.service.ts
+++ src/app/logger/console-logger.service.ts
@@ -12,6 +12,9 @@ enum IsDebugMode {
 export let isDebugMode = IsDebugMode[environment.loggingLevel];
 const noop = (): any => undefined;
 
+const now = Date();
+const user = 'dummy@user.com';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,7 +22,7 @@ export class ConsoleLoggerService implements Logger {
 
   get info() {
     if (isDebugMode !== 0 && isDebugMode !== 1 && isDebugMode !== 2) {
-      return console.info.bind(console);
+      return console.info.bind(console, [user, now]);
     } else {
       return noop;
     }
@@ -27,7 +30,7 @@ export class ConsoleLoggerService implements Logger {
 
   get warn() {
     if (isDebugMode !== 0 && isDebugMode !== 1) {
-      return console.warn.bind(console);
+      return console.warn.bind(console, [user, now]);
     } else {
       return noop;
     }
@@ -35,15 +38,10 @@ export class ConsoleLoggerService implements Logger {
 
   get error() {
     if (isDebugMode !== 0) {
-      return console.error.bind(console);
+      return console.error.bind(console, [user, now]);
     } else {
       return noop;
     }
   }
 
-  invokeConsoleMethod(type: string, args?: any): void {
-    type func = (name: string) => void;
-    const logFn: func = (console)[type] || console.log || noop;
-    logFn.apply(console, [args]);
-  }
 }

b51cfe54bbf99aaa2ed3bc6388230bbf39376ce9
Matthew Speake <matthew.speake@solirius.com>
2019-04-26 12:51:44 +0100

    Set application title so we know the app is running and it's rendered in the app component template.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { LoggerService } from './logger/logger.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = "XUI"
+
   constructor(private logger: LoggerService) {
     logger.info('AppComponent: logger.info()');
     logger.warn('AppComponent: logger.warn()');

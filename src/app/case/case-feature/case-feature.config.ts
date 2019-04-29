fa4943cb18ac5ee27e5c9501600e5d13f1b32fb5
mas <masahmad7@gmail.com>
2019-04-29 11:14:36 +0100

    move config to environments

--- src/app/case/case-feature/case-feature.config.ts
+++ src/app/case/case-feature/case-feature.config.ts
@@ -1,27 +1,11 @@
 import { Injectable } from '@angular/core'
 import { Http } from '@angular/http'
 import { AbstractAppConfig, CaseEditorConfig, CaseEventData } from '@hmcts/ccd-case-ui-toolkit'
-// import { environment as mainConfig } from '../environments/environment'
+import { environment as mainConfig } from '../../../environments/environment'
 //    'case_data_url': 'http://localhost:3001/api/data',
 @Injectable()
 export class AppConfig extends AbstractAppConfig {
-    protected config: CaseEditorConfig = {
-        'api_url': '/aggregated',
-        'case_data_url': '/data',
-        'document_management_url': '/documents',
-        'login_url': '/login',
-        'oauth2_client_id': 'ccd_gateway',
-        'postcode_lookup_url': '/addresses?postcode=${postcode}',
-        'remote_document_management_url': '/documents',
-        'payments_url': '/payments',
-        'activity_batch_collection_delay_ms': 1,
-        'activity_next_poll_request_ms': 5000,
-        'activity_retry': 5,
-        'activity_url': '',
-        'activity_max_request_per_batch': 25,
-        'print_service_url': '/print',
-        'remote_print_service_url': '/remote_print'
-    };
+    protected config: CaseEditorConfig = mainConfig.CaseEditorConfig
     constructor(private http: Http) {
         super();
     }

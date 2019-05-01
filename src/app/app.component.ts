4f1ae90408d9fb20f14b8ebf7be7a07504313c78
Matthew Speake <matthew.speake@solirius.com>
2019-05-01 11:05:41 +0100

    TSLint fixes for sloppy code which has been introduced into the project.  Doing this so I can deploy to AKS.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { LoggerService } from './logger/logger.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "XUI"
+  title = 'XUI';
 
   constructor(private logger: LoggerService) {
     logger.info('AppComponent: logger.info()');

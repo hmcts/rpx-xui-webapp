d2894d1738ec2a5a7d8a3b6188588d9a0775cbc7
Matthew Speake <matthew.speake@solirius.com>
2019-05-10 15:49:06 +0100

    Change resource group name to align with rpx-shared-infrastructure.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,7 +17,7 @@ module "app" {
     additional_host_name = "${var.additional_host_name}"
     https_only="false"
     common_tags  = "${var.common_tags}"
-    asp_rg = "${local.shared_product_name}-${var.env}"
+    asp_rg = "${local.app_full_name}-${var.env}"
     #asp_name = "${local.shared_product_name}-${var.env}"
     asp_name = "${var.env == "prod" ? "TBD" : "${local.shared_product_name}-${var.env}"}"
 

19661a9118ac4b1e07c36cfed451b861c18cdba3
Alan McCabe - Jones <alan@localhost.localdomain>
2019-05-13 13:04:08 +0100

    utility

--- api/services/serviceAuth.ts
+++ api/services/serviceAuth.ts
@@ -2,14 +2,23 @@ import * as express from 'express'
 import * as otp from 'otp'
 import { config } from '../config'
 import { http } from '../lib/http'
+import * as log4jui from '../lib/log4jui'
 import { getHealth, getInfo } from '../lib/util'
 
 const url = config.services.s2s
 const microservice = config.microservice
 const s2sSecret = process.env.S2S_SECRET || 'AAAAAAAAAAAAAAAA'
 
+const logger = log4jui.getLogger('service auth')
+
 export async function postS2SLease() {
+
     const oneTimePassword = otp({ secret: s2sSecret }).totp()
+
+    logger.info('generating from secret  :', s2sSecret,
+        microservice,
+        oneTimePassword
+    )
 
     const request = await http.post(`${url}/lease`, {
         microservice,

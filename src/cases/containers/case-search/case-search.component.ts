f9d3424647d4a2e6805b0dac99176658edd86975
fazanki <fazanki@gmail.com>
2019-05-16 14:37:21 +0100

    moving search into its own component

--- src/cases/containers/case-search/case-search.component.ts
+++ src/cases/containers/case-search/case-search.component.ts
@@ -1,5 +1,10 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-
+import {ActionBindingModel} from '../../models/create-case-actions.model';
+import {Jurisdiction} from '@hmcts/ccd-case-ui-toolkit';
+import * as fromCasesFeature from '../../store';
+import {mocked} from '../../models/search-filter-dummy-data-to-delete';
+import {Store} from '@ngrx/store';
+import * as fromCaseCreate from '../../store/reducers';
 /**
  * Entry component wrapper for CCD-CASE-CREATE
  * Smart Component
@@ -7,13 +12,34 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
  */
 @Component({
   selector: 'exui-search-case',
-  template: `<exui-page-wrapper [title]="'Search'"></exui-page-wrapper>`,
+  template: `<exui-page-wrapper [title]="'Search'">
+    <exui-ccd-connector
+      [eventsBindings]="caseSearchEventsBindings"
+      [store]="store"
+      [fromFeatureStore]="fromCasesFeature">
+      <ccd-search-filters #ccdComponent
+        [autoApply]="true"
+        [jurisdictions]="jurisdictions">
+      </ccd-search-filters>
+    </exui-ccd-connector>
+  </exui-page-wrapper>`,
   encapsulation: ViewEncapsulation.None
 })
 export class CaseSearchComponent implements OnInit {
-  constructor() {}
+  caseSearchEventsBindings: ActionBindingModel[];
+  fromCasesFeature: any;
+  jurisdictions: Jurisdiction[];
+  constructor(private store: Store<fromCaseCreate.State>) {}
 
   ngOnInit(): void {
+    this.fromCasesFeature = fromCasesFeature;
+    this.jurisdictions = [ mocked.juristdiction1, mocked.juristdiction2 ];
+    this.caseSearchEventsBindings = [
+      {type: 'onJurisdiction', action: 'JurisdictionSelected'},
+      {type: 'onApply', action: 'Applied'},
+      {type: 'onReset', action: 'Reset'}
+    ];
   }
+
 
 }

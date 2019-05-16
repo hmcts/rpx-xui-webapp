5d826958c4db4cbd4bb07a5d95d5db172af1acbc
Dmitry Panov <dpanov76@yahoo.com>
2019-05-16 16:08:53 +0100

    Reducesrs hocked to Actions for search

--- src/cases/store/reducers/search-filter.reducer.ts
+++ src/cases/store/reducers/search-filter.reducer.ts
@@ -2,13 +2,13 @@ import * as fromCases from '../actions/case-search.action';
 
 // todo this is just a place holder
 export interface SearchState {
-  someState: any;
+  searchFilterCriteria: any;
   loading: boolean;
   loaded: boolean;
 }
 
 export const initialStateSearch: SearchState = {
-  someState: {},
+  searchFilterCriteria: {},
   loaded: false,
   loading: false,
 };
@@ -18,29 +18,13 @@ export function reducer(
   action: fromCases.CaseSearchAction
 ): SearchState {
   switch (action.type) {
-
-    case fromCases.JURISDICTION_SELECTED: {
-      return {
-        ...state,
-        loading: true,
-      };
-    }
-
     case fromCases.APPLIED: {
+      const searchFilterCriteria = action.payload;
       return {
-        ...state,
-        loading: true,
+          ...state,
+          searchFilterCriteria
       };
     }
-
-    case fromCases.RESET: {
-      return {
-        ...state,
-        loading: true,
-      };
-    }
-
   }
-
   return state;
 }

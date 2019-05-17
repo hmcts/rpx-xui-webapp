060a01538d2273490c69f9466f78fbe73bd6c3cd
Dmitry Panov <dpanov76@yahoo.com>
2019-05-17 14:26:54 +0100

    Unit test added to search filter

---
+++ src/cases/store/reducers/search-filter.reducer.spec.ts
@@ -0,0 +1,26 @@
+import * as fromCases from './search-filter.reducer';
+import * as fromActions from '../actions/case-search.action';
+
+describe('SearchFilterReducer', () => {
+  describe('undefined action', () => {
+    it('should return the default state', () => {
+      const { initialStateSearch } = fromCases;
+      const action = {} as any;
+      const state = fromCases.reducer(undefined, action);
+
+      expect(state).toBe(initialStateSearch);
+    });
+  });
+
+  describe('APPLIED action', () => {
+    it('should apply patload to store', () => {
+      const { initialStateSearch } = fromCases;
+      const payload = {};
+      const action = new fromActions.Applied(payload);
+      const state = fromCases.reducer(initialStateSearch, action);
+      expect(state.loading).toEqual(false);
+      expect(state.loaded).toEqual(false);
+    });
+  });
+
+});

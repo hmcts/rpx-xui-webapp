aa10e2461af3d6a4a1e8c7f1d5730c07d82bbb59
fazanki <fazanki@gmail.com>
2019-05-17 16:17:27 +0100

    adding app selectors tests

---
+++ src/app/store/selectors/app.selectors.spec.ts
@@ -0,0 +1,83 @@
+import { TestBed } from '@angular/core/testing';
+import { StoreModule, Store, combineReducers } from '@ngrx/store';
+
+import * as fromRoot from '../../../app/store/reducers';
+import * as fromReducers from '../reducers';
+import * as fromActions from '../actions';
+import * as fromSelectors from './app.selectors';
+
+
+describe('App Selectors', () => {
+  let store: Store<fromReducers.State>;
+
+  const appConfig = {
+    config: { features: {}},
+    loaded: false,
+    loading: false
+  };
+
+  const appPayload = {
+    features: {
+      ccdCaseCreate: {
+        isEnabled: true,
+        label: 'CCDCaseCreate'
+      }
+    }
+  };
+
+  const appConfigLoaded = {
+      config: {
+        features: {
+          ccdCaseCreate: {
+            isEnabled: true,
+            label: 'CCDCaseCreate'
+          }
+        }
+      },
+      loaded: true,
+      loading: false
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot({
+          ...fromRoot.reducers,
+          products: combineReducers(fromReducers.reducers),
+        }),
+      ],
+    });
+
+    store = TestBed.get(Store);
+
+    spyOn(store, 'dispatch').and.callThrough();
+  });
+
+  describe('Configuration State', () => {
+    it('should return config initial state', () => {
+      let result;
+
+      store
+        .select(fromSelectors.getConfigState)
+        .subscribe(value => (result = value));
+
+      expect(result).toEqual(appConfig);
+
+      store.dispatch(new fromActions.LoadConfigSuccess(appPayload));
+      expect(result).toEqual(appConfigLoaded);
+    });
+
+    it('should return app feature state', () => {
+      let result;
+
+      store
+        .select(fromSelectors.getAppFeatures)
+        .subscribe(value => (result = value));
+
+      store.dispatch(new fromActions.LoadConfigSuccess(appPayload));
+      expect(result).toEqual(appConfigLoaded.config);
+    });
+  });
+
+
+});

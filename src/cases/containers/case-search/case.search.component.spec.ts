6af4a2abae71e92ea5028202f146c3d3f1f7732f
fazanki <fazanki@gmail.com>
2019-05-20 15:53:32 +0100

    adding tests

---
+++ src/cases/containers/case-search/case.search.component.spec.ts
@@ -0,0 +1,100 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  CaseUIToolkitModule,
+  DraftService,
+  AlertService,
+  HttpService,
+  AuthService as CCDAuthService,
+  CasesService,
+  HttpErrorService,
+  AbstractAppConfig,
+  CaseEditWizardGuard,
+  RouterHelperService,
+  DocumentManagementService,
+  PageValidationService,
+  PlaceholderService,
+  SearchService,
+  RequestOptionsBuilder,
+  SearchFiltersModule,
+} from '@hmcts/ccd-case-ui-toolkit';
+import {AppConfig} from '../../case.config';
+import {ScrollToService} from '@nicky-lenaers/ngx-scroll-to';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientModule} from '@angular/common/http';
+import {StoreModule} from '@ngrx/store';
+import {HttpModule} from '@angular/http';
+import {SharedModule} from '../../../app/shared/shared.module';
+import {AppConfigService} from '../../../app/services/config/configuration.services';
+import {CaseSearchComponent} from './case-search.component';
+
+class MockSortService {
+  features = {};
+  getFeatureToggle() {}
+}
+describe('CaseCaseComponent', () => {
+  let component: CaseSearchComponent;
+  let fixture: ComponentFixture<CaseSearchComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        CaseUIToolkitModule,
+        HttpClientModule,
+        StoreModule.forRoot({}),
+        HttpModule,
+        SharedModule,
+        SearchFiltersModule,
+      ],
+      declarations: [ CaseSearchComponent ],
+      providers: [
+        PlaceholderService,
+        CasesService,
+        CCDAuthService,
+        HttpService,
+        HttpErrorService,
+        AlertService,
+        DraftService,
+        PageValidationService,
+        CaseEditWizardGuard,
+        RouterHelperService,
+        DocumentManagementService,
+        AppConfig,
+        AppConfigService,
+        RequestOptionsBuilder,
+        {
+          provide: SearchService,
+          useValue: {
+            requestOptionsBuilder: RequestOptionsBuilder
+          }
+        },
+        {
+          provide: AbstractAppConfig,
+          useExisting: AppConfig
+        },
+        {
+          provide: AppConfigService,
+          useClass: MockSortService
+        },
+        ScrollToService
+      ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CaseSearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+  });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have ngOnInit', () => {
+    expect(component.ngOnInit).toBeTruthy();
+  });
+
+
+});

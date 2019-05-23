6125c84a9d65296b00630b76f7b6000870eb716f
mas <masahmad7@gmail.com>
2019-05-23 12:00:28 +0100

    fix lint issue

--- src/cases/containers/case-create/case-create.component.spec.ts
+++ src/cases/containers/case-create/case-create.component.spec.ts
@@ -17,21 +17,21 @@ import {
   RequestOptionsBuilder,
   SearchFiltersModule,
 } from '@hmcts/ccd-case-ui-toolkit';
-import {AppConfig} from '../../../app/services/ccd-config/ccd-case.config';
-import {ScrollToService} from '@nicky-lenaers/ngx-scroll-to';
-import {RouterTestingModule} from '@angular/router/testing';
-import {HttpClientModule} from '@angular/common/http';
-import {StoreModule} from '@ngrx/store';
-import {HttpModule} from '@angular/http';
-import {SharedModule} from '../../../app/shared/shared.module';
-import {AppConfigService} from '../../../app/services/config/configuration.services';
-import {CasesCreateComponent} from './case-create.component';
-import {ProvidersModule} from '../../../app/providers/providers.module';
+import { AppConfig } from '../../../app/services/ccd-config/ccd-case.config';
+import { ScrollToService } from '@nicky-lenaers/ngx-scroll-to';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { HttpModule } from '@angular/http';
+import { SharedModule } from '../../../app/shared/shared.module';
+import { AppConfigService } from '../../../app/services/config/configuration.services';
+import { CasesCreateComponent } from './case-create.component';
+import { ProvidersModule } from '../../../app/providers/providers.module';
 
 class MockSortService {
   features = {};
-  getFeatureToggle() {}
-  getEditorConfiguration() {}
+  getFeatureToggle() { }
+  getEditorConfiguration() { }
 }
 describe('CaseCaseComponent', () => {
   let component: CasesCreateComponent;
@@ -48,7 +48,7 @@ describe('CaseCaseComponent', () => {
         SharedModule,
         SearchFiltersModule,
       ],
-      declarations: [ CasesCreateComponent ],
+      declarations: [CasesCreateComponent],
       providers: [
         PlaceholderService,
         CasesService,

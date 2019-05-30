0de8ac4686dfe5be6125323cf5808a2d12843952
Uday Denduluri <uday.denduluri@gmail.com>
2019-05-30 15:47:25 +0100

    pre-push fixes for the linter errors

--- src/app/services/logger/console-logger.service.ts
+++ src/app/services/logger/console-logger.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Logger } from './logger.service';
 import { environment } from '../../../environments/environment';
 import { NGXLogger } from 'ngx-logger';
-import { AuthService } from '../auth/auth.service';
 import { CookieService } from 'ngx-cookie';
 
 const now = Date();
@@ -12,47 +11,45 @@ const now = Date();
 })
 
 export class ConsoleLoggerService implements Logger {
-  user:string;
+  user: string;
   COOKIE_KEYS;
 
  constructor(
-   private ngxLogger: NGXLogger, 
-  private authService: AuthService,
-  private cookieService: CookieService){
+   private ngxLogger: NGXLogger,
+   private cookieService: CookieService) {
     this.COOKIE_KEYS = {
       TOKEN: environment.cookies.token,
       USER: environment.cookies.userId
     };
 
-    let userId = this.cookieService.get(this.COOKIE_KEYS.USER);
+    const userId = this.cookieService.get(this.COOKIE_KEYS.USER);
     if (!userId) {
       this.user = 'dummy@user.com';
-    } 
-    else{
+    } else {
       this.user = userId;
     }
   }
 
-  debug(message: string): void{
+  debug(message: string): void {
     this.ngxLogger.debug(message, [this.user, now]);
   }
 
-  trace(message: string): void{
+  trace(message: string): void {
       this.ngxLogger.trace(message, [this.user, now]);
-  }  
+  }
 
-  info(message: string) : void {
+  info(message: string): void {
       this.ngxLogger.info(message, [this.user, now]);
   }
 
-  warn(message: string) : void {
+  warn(message: string): void {
       this.ngxLogger.warn(message, [this.user, now]);
   }
 
-  error(message: string) : void {
+  error(message: string): void {
       this.ngxLogger.error(message, [this.user, now]);
   }
-  fatal(message: string) : void {
+  fatal(message: string): void {
     this.ngxLogger.fatal(message, [this.user, now]);
   }
 }

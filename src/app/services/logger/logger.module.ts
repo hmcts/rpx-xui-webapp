06eb6269dcca176c0991c17505443bdd4801cea7
Uday Denduluri <uday.denduluri@gmail.com>
2019-06-04 16:44:08 +0100

    Moving the component to Logger

--- src/app/services/logger/logger.module.ts
+++ src/app/services/logger/logger.module.ts
@@ -2,7 +2,7 @@ import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { ExuiLoggerComponent } from 'src/app/components/exui-logger/exui-logger.component';
+import { ExuiLoggerComponent } from './exui-logger/exui-logger.component';
 
 @NgModule({
     declarations: [

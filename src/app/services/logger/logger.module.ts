892fcbdeaa03ab79b724f407e020b27c11d0edbe
Uday Denduluri <uday.denduluri@gmail.com>
2019-06-04 16:00:40 +0100

    Lint errors

--- src/app/services/logger/logger.module.ts
+++ src/app/services/logger/logger.module.ts
@@ -1,16 +1,19 @@
-import {NgModule} from '@angular/core';
+import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { ExuiLoggerComponent } from 'src/app/components/exui-logger/exui-logger.component';
 
 @NgModule({
     declarations: [
+        ExuiLoggerComponent
     ],
     imports: [
         BrowserModule,
         !environment.production ? StoreDevtoolsModule.instrument() : []
     ],
-    providers: []
+    providers: [],
+    exports: [ExuiLoggerComponent]
 })
 export class LoggerModule {
 }

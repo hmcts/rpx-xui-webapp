900d088661eaa8835f79fa316a3efc5069f63eca
Uday Denduluri <uday.denduluri@gmail.com>
2019-06-04 13:40:02 +0100

    Selector logic

--- src/app/services/logger/logger.module.ts
+++ src/app/services/logger/logger.module.ts
@@ -3,15 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
-import { LoggerEffects } from '../logger/effects/logger.effects';
 
 @NgModule({
     declarations: [
     ],
     imports: [
         BrowserModule,
-        !environment.production ? StoreDevtoolsModule.instrument() : [],
-        EffectsModule.forFeature([LoggerEffects])
+        !environment.production ? StoreDevtoolsModule.instrument() : []
     ],
     providers: []
 })

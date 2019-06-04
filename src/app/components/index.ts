e796eac0a7db5307baa847538eda8f9c2b579867
Uday Denduluri <uday.denduluri@gmail.com>
2019-06-04 16:44:08 +0100

    Moving the component to Logger

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,6 +1,5 @@
 import {ExuiPageWrapperComponent} from './exui-mian-wrapper/exui-page-wrapper.component';
-import {ExuiLoggerComponent} from './exui-logger/exui-logger.component';
 
-export const components: any[] = [ExuiPageWrapperComponent, ExuiLoggerComponent];
+export const components: any[] = [ExuiPageWrapperComponent];
 
 export * from './exui-mian-wrapper/exui-page-wrapper.component';

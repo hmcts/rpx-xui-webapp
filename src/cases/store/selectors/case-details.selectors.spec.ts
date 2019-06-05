fd7a895aa9a54a9ee1108abd371ec35638614b81
fazanki <fazanki@gmail.com>
2019-06-05 15:49:46 +0100

    cleaning up

--- src/cases/store/selectors/case-details.selectors.spec.ts
+++ src/cases/store/selectors/case-details.selectors.spec.ts
@@ -1,7 +1,7 @@
 // import { TestBed } from '@angular/core/testing';
 // import { StoreModule, Store, combineReducers } from '@ngrx/store';
 //
-// import * as fromRoot from '../../../app/store/reducers';
+// import * as fromRoot from '../../../app/store/';
 // import * as fromReducers from '../reducers';
 // import * as fromSelectors from './case-details.selectors';
 //
@@ -15,27 +15,6 @@
 //     loading: false
 //   };
 //
-//   const appPayload = {
-//     features: {
-//       ccdCaseCreate: {
-//         isEnabled: true,
-//         label: 'CCDCaseCreate'
-//       }
-//     }
-//   };
-//
-//   const appConfigLoaded = {
-//     config: {
-//       features: {
-//         ccdCaseCreate: {
-//           isEnabled: true,
-//           label: 'CCDCaseCreate'
-//         }
-//       }
-//     },
-//     loaded: true,
-//     loading: false
-//   };
 //
 //   beforeEach(() => {
 //     TestBed.configureTestingModule({
@@ -54,17 +33,13 @@
 //
 //   describe('Configuration State', () => {
 //     it('should return config initial state', () => {
+//
 //       let result;
 //
 //       store
 //         .select(fromSelectors.getCaseId)
 //         .subscribe(value => (result = value));
 //
-//       // expect(result).toEqual(appConfig);
-//       //
-//       // // store.dispatch(new fromActions.LoadConfigSuccess(appPayload));
-//       // expect(result).toEqual(appConfigLoaded);
-//     });
-//
+//       expect(result).toEqual({});
 //   });
 // });

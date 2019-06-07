110b9b99588d4de8687128feaf613ae9a49585c1
Alan McCabe-Jones <alan@localhost.localdomain>
2019-06-07 14:24:06 +0100

    fix for github wierdness

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -36,5 +36,6 @@ module "app" {
         S2S_SECRET = "${data.azurerm_key_vault_secret.s2s_secret.value}"
         IDAM_SECRET = "${data.azurerm_key_vault_secret.oauth2_secret.value}"
     
+    
     }
 }

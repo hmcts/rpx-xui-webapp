5836afd7ef6171221d59457070826769be4eaaed
Alan McCabe-Jones <alan@localhost.localdomain>
2019-06-07 20:16:30 +0100

    Allow reading of XUI ENV

--- api/config/index.ts
+++ api/config/index.ts
@@ -10,7 +10,7 @@ export const configs = {
     local,
 }
 
-export const configEnv = process ? process.env.JUI_ENV || 'local' : 'local'
+export const configEnv = process ? process.env.XUI_ENV || 'local' : 'local'
 export const baseConfig = { ...application }
 export const config = { ...baseConfig, ...configs[configEnv].default }
 

7f597a6856e06cb0b177bda610828eccb8699f13
Alan <alan.work@teamsmog.com>
2019-06-13 12:29:50 +0100

    Added preview to XUI

--- api/config/index.ts
+++ api/config/index.ts
@@ -1,13 +1,14 @@
-
 import * as process from 'process'
 import { application } from './application.config'
 
 import * as aat from './environments/aat.config'
 import * as local from './environments/local.config'
+import * as preview from './environments/preview.config'
 
 export const configs = {
     aat,
     local,
+    preview,
 }
 
 export const configEnv = process ? process.env.JUI_ENV || 'local' : 'local'
@@ -15,6 +16,5 @@ export const baseConfig = { ...application }
 export const config = { ...baseConfig, ...configs[configEnv].default }
 
 if (process) {
-    config.appInsightsInstrumentationKey =
-        process.env.APPINSIGHTS_INSTRUMENTATIONKEY || 'AAAAAAAAAAAAAAAA'
+    config.appInsightsInstrumentationKey = process.env.APPINSIGHTS_INSTRUMENTATIONKEY || 'AAAAAAAAAAAAAAAA'
 }

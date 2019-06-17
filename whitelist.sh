ec8c9612a1fcca89ff162ca03ffad643be4f43dc
Alan McCabe-Jones <alan@localhost.localdomain>
2019-06-17 11:41:06 +0100

    changes to whitelist

--- whitelist.sh
+++ whitelist.sh
@@ -1,20 +1,22 @@
-export
-if [[ $HOSTNAME = *xui-webapp-pr* ]]
+
+if [[ $JOB_NAME = *PR-* ]]
 then
 
-NUMBERSTR=$(echo "$HOSTNAME" |  grep -o -E '\-[0-9]+\-')
-NUMBER=$(echo "$NUMBERSTR" |  grep -o -E '[0-9]+')
- 
+NUMBER=$(echo "$JOB_NAME" |  grep -o -E '[0-9]+')
+
 echo "PR Number $NUMBER" 
+
+PARAM="{
+   "operation": "add",
+   "field": "redirect_uri",
+  "value": "https://xui-webapp-pr-${NUMBER}.service.core-compute-preview.internal/oauth2/callback"
+}"
+
 https_proxy=proxyout.reform.hmcts.net:8080 curl -v -X PATCH \
  https://idam-api.aat.platform.hmcts.net/testing-support/services/judicial-ui \
  -H 'Accept: application/json' \
  -H 'Content-Type: application/json' \
- -d '[{
-   "operation": "add",
-   "field": "redirect_uri",
-  "value": "https://xui-webapp-pr-$NUMBER.service.core-compute-preview.internal/oauth2/callback"
-}]'
+ -d "${PARAM}"
 
 fi
 

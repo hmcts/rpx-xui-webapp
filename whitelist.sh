8d7ebdb74d15ce01954b4b1342b810fa5cf7b88d
Alan McCabe-Jones <alan@localhost.localdomain>
2019-06-17 12:09:46 +0100

    fix auto approve

--- whitelist.sh
+++ whitelist.sh
@@ -5,18 +5,21 @@ then
 NUMBER=$(echo "$JOB_NAME" |  grep -o -E '[0-9]+')
 
 echo "PR Number $NUMBER" 
+echo "Will try and whitelist https://xui-webapp-pr-${NUMBER}.service.core-compute-preview.internal/oauth2/callback"
 
 PARAM="{
-   "operation": "add",
-   "field": "redirect_uri",
-  "value": "https://xui-webapp-pr-${NUMBER}.service.core-compute-preview.internal/oauth2/callback"
+   \"operation\": \"add\",
+   \"field\": \"redirect_uri\",
+  \"value\": \"https://xui-webapp-pr-${NUMBER}.service.core-compute-preview.internal/oauth2/callback\"
 }"
 
 https_proxy=proxyout.reform.hmcts.net:8080 curl -v -X PATCH \
  https://idam-api.aat.platform.hmcts.net/testing-support/services/judicial-ui \
  -H 'Accept: application/json' \
  -H 'Content-Type: application/json' \
- -d "${PARAM}"
+ -d "[${PARAM}]"
 
 fi
+
+
 

20a4ae3407790506c23040fbcda95a903a8b0c2e
Alan McCabe-Jones <alan@localhost.localdomain>
2019-06-17 10:46:52 +0100

    added auto pr tests

---
+++ whitelist.sh
@@ -0,0 +1,19 @@
+if [[ $HOSTNAME = *xui-webapp-pr* ]]
+then
+
+NUMBERSTR=$(echo "$HOSTNAME" |  grep -o -E '\-[0-9]+\-')
+NUMBER=$(echo "$NUMBERSTR" |  grep -o -E '[0-9]+')
+ 
+echo "PR Number $NUMBER" 
+https_proxy=proxyout.reform.hmcts.net:8080 curl -v -X PATCH \
+ https://idam-api.aat.platform.hmcts.net/testing-support/services/judicial-ui \
+ -H 'Accept: application/json' \
+ -H 'Content-Type: application/json' \
+ -d '[{
+   "operation": "add",
+   "field": "redirect_uri",
+  "value": "https://xui-webapp-pr-$NUMBER.service.core-compute-preview.internal/oauth2/callback"
+}]'
+
+fi
+

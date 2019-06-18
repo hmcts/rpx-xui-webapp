a9cabce289a61adb194a9c36186f0a69cc05ad88
Uday Denduluri <uday.denduluri@gmail.com>
2019-06-18 13:44:42 +0100

    Case Filter component

--- src/cases/containers/case-create/case-create.component.spec.ts
+++ src/cases/containers/case-create/case-create.component.spec.ts
@@ -21,11 +21,16 @@ import { AppConfig } from '../../../app/services/ccd-config/ccd-case.config';
 import { ScrollToService } from '@nicky-lenaers/ngx-scroll-to';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, combineReducers, Store } from '@ngrx/store';
 import { HttpModule } from '@angular/http';
 import { SharedModule } from '../../../app/shared/shared.module';
 import { AppConfigService } from '../../../app/services/config/configuration.services';
 import { CasesCreateComponent } from './case-create.component';
+import * as fromCasesFeature from '../../store';
+import * as fromCaseCreate from '../../store/reducers';
+import { MockStore } from '@ngrx/store/testing';
+import { Observable } from 'rxjs';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
 class MockSortService {
   features = {};
@@ -35,27 +40,39 @@ class MockSortService {
 describe('CaseCreateComponent', () => {
   let component: CasesCreateComponent;
   let fixture: ComponentFixture<CasesCreateComponent>;
+  let store: MockStore<fromCaseCreate.State>;
+  const mockPlaceholderService = jasmine.createSpyObj(['resolvePlaceholders']);
+  const mockCasesService = jasmine.createSpyObj(['jurisdictionId', 'getCaseView', 'getCaseViewV2', 'getEventTrigger', 'validateCase',
+  'createCase', 'getPrintDocuments']);
+  const mockCCDAuthService = jasmine.createSpyObj(['signIn', 'redirectUri']);
+  const mockHttpService = jasmine.createSpyObj(['get', 'post', 'put', 'delete']);
+  const mockHttpErrorService = jasmine.createSpyObj(['setError', 'removeError', 'handle']);
+  const mockAlertService = jasmine.createSpyObj(['push', 'clear', 'error', 'warning', 'success', 'setPreserveAlerts', 'isPreserveAlerts',
+  'message']);
 
-  beforeEach(async(() => {
+  beforeEach(() => {
+    TestBed.resetTestEnvironment();
+    TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
         CaseUIToolkitModule,
         HttpClientModule,
-        StoreModule.forRoot({}),
-        HttpModule,
+        StoreModule.forRoot({
+          ...fromCasesFeature.reducers,
+          feature: combineReducers(fromCasesFeature.reducers)
+        }),
         SharedModule,
         SearchFiltersModule,
       ],
       declarations: [CasesCreateComponent],
       providers: [
-        PlaceholderService,
-        CasesService,
-        CCDAuthService,
-        HttpService,
-        HttpErrorService,
-        AlertService,
-        DraftService,
+        { provide: PlaceholderService, useValue: mockPlaceholderService },
+        { provide: CasesService, useValue: mockCasesService },
+        { provide: mockCCDAuthService, useValue: CCDAuthService },
+        { provide: mockHttpService, useValue: HttpService },
+        { provide: mockHttpErrorService, useValue: HttpErrorService },
+        { provide: mockAlertService, useValue: AlertService },
         PageValidationService,
         CaseEditWizardGuard,
         RouterHelperService,
@@ -82,27 +99,20 @@ describe('CaseCreateComponent', () => {
       ]
     })
       .compileComponents();
-      // .then(()=>{
-      //   fixture = TestBed.createComponent(CasesCreateComponent);
-      //   component = fixture.componentInstance;
-      //   component.caseCreateInputs = {jurisdictionId: '', caseTypeId: '', eventId: ''};
-      // });
-  }));
+  });
 
-  beforeEach(() => {
+  it('should create', () => {
     fixture = TestBed.createComponent(CasesCreateComponent);
     component = fixture.componentInstance;
     component.caseCreateInputs = {jurisdictionId: '', caseTypeId: '', eventId: ''};
 
     fixture.detectChanges();
 
-  });
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    store = TestBed.get(Store);
+    spyOn(store, 'pipe').and.callFake(() =>  new Observable<any>() );
+    spyOn(store, 'dispatch').and.callThrough();
 
-  it('should have ngOnInit', () => {
-    expect(component.ngOnInit).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
 

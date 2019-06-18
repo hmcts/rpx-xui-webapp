405e1992036c8fc569210b60ec394f2e99471949
Uday Denduluri <uday.denduluri@gmail.com>
2019-06-18 12:25:37 +0100

    Making the store as mock store

--- src/cases/containers/case-filter/case-filter.component.spec.ts
+++ src/cases/containers/case-filter/case-filter.component.spec.ts
@@ -21,22 +21,39 @@ import { AppConfig } from '../../../app/services/ccd-config/ccd-case.config';
 import { ScrollToService } from '@nicky-lenaers/ngx-scroll-to';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, Store } from '@ngrx/store';
 import { HttpModule } from '@angular/http';
 import { SharedModule } from '../../../app/shared/shared.module';
 import { AppConfigService } from '../../../app/services/config/configuration.services';
 import { CaseFilterComponent } from './case-filter.component';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { MockStore } from '@ngrx/store/testing';
+import * as fromCaseCreate from '../../store/reducers';
+import { Observable } from 'rxjs';
 
 class MockSortService {
   features = {};
   getFeatureToggle() { }
   getEditorConfiguration() { }
 }
+
+let store: MockStore<fromCaseCreate.State>;
+const mockPlaceholderService = jasmine.createSpyObj(['resolvePlaceholders']);
+const mockCasesService = jasmine.createSpyObj(['jurisdictionId', 'getCaseView', 'getCaseViewV2', 'getEventTrigger', 'validateCase',
+'createCase', 'getPrintDocuments']);
+const mockCCDAuthService = jasmine.createSpyObj(['signIn', 'redirectUri']);
+const mockHttpService = jasmine.createSpyObj(['get', 'post', 'put', 'delete']);
+const mockHttpErrorService = jasmine.createSpyObj(['setError', 'removeError', 'handle']);
+const mockAlertService = jasmine.createSpyObj(['push', 'clear', 'error', 'warning', 'success', 'setPreserveAlerts', 'isPreserveAlerts',
+'message']);
+
 describe('Case Filter Component', () => {
   let component: CaseFilterComponent;
   let fixture: ComponentFixture<CaseFilterComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
+      TestBed.resetTestEnvironment();
+      TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
       TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
@@ -50,20 +67,12 @@ describe('Case Filter Component', () => {
       ],
       declarations: [CaseFilterComponent],
       providers: [
-        PlaceholderService,
-        CasesService,
-        CCDAuthService,
-        HttpService,
-        HttpErrorService,
-        AlertService,
-        DraftService,
-        PageValidationService,
-        CaseEditWizardGuard,
-        RouterHelperService,
-        DocumentManagementService,
-        AppConfigService,
-        RequestOptionsBuilder,
-        AppConfigService,
+        { provide: PlaceholderService, useValue: mockPlaceholderService },
+        { provide: CasesService, useValue: mockCasesService },
+        { provide: mockCCDAuthService, useValue: CCDAuthService },
+        { provide: mockHttpService, useValue: HttpService },
+        { provide: mockHttpErrorService, useValue: HttpErrorService },
+        { provide: mockAlertService, useValue: AlertService },
         AppConfig,
         {
           provide: SearchService,
@@ -84,27 +93,21 @@ describe('Case Filter Component', () => {
     })
       .compileComponents()
       .then(() => {
-        fixture = TestBed.createComponent(CaseFilterComponent);
-        component = fixture.componentInstance;
-        component.startButtonText = 'start';
-        component.caseCreatFilterBindings = [];
+
       });
-  }));
-
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(CaseFilterComponent);
-  //   component = fixture.componentInstance;
-  //   component.startButtonText = 'start';
-  //   component.caseCreatFilterBindings = [];
-  //   fixture.detectChanges();
-
-  // });
-  it('should create', () => {
-    expect(component).toBeTruthy();
   });
 
-  it('should have ngOnInit', () => {
-    expect(component.ngOnInit).toBeTruthy();
+  it('should create', () => {
+    store = TestBed.get(Store);
+    spyOn(store, 'pipe').and.callFake(() =>  new Observable<any>() );
+    spyOn(store, 'dispatch').and.callThrough();
+
+    fixture = TestBed.createComponent(CaseFilterComponent);
+    console.log('fixture is ' + fixture);
+    component = fixture.componentInstance;
+    component.startButtonText = 'start';
+    component.caseCreatFilterBindings = [];
+    expect(component).toBeTruthy();
   });
 
 

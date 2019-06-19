4f62137064b855d4bfbbcbaad7415d12e6216951
Uday Denduluri <uday.denduluri@gmail.com>
2019-06-19 15:54:48 +0100

    Adding the Testing config

--- src/app/services/logger/console-logger.service.spec.ts
+++ src/app/services/logger/console-logger.service.spec.ts
@@ -4,24 +4,15 @@ import { NGXLogger } from 'ngx-logger';
 import { CookieService } from 'ngx-cookie';
 import {StoreModule} from '@ngrx/store';
 
+let mockNGXLogger;
 describe('ConsoleLoggerService', () => {
+  mockNGXLogger = jasmine.createSpyObj('mockNGXLogger', ['trace', 'debug', 'info', 'log', 'warn', 'error', 'fatal', 'setCustomHttpHeaders',
+'setCustomParams', 'registerMonitor', 'updateConfig', 'getConfigSnapshot']);
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [StoreModule.forRoot({})],
       providers : [
-        { provide: NGXLogger,
-          useValue: {
-            info(message) {
-              return 'test info';
-            },
-            warn(message) {
-              return 'test warning';
-            },
-            error(message) {
-              return 'test error';
-            }
-          }
-        },
+        { provide: NGXLogger, useValue: mockNGXLogger },
       { provide: CookieService,
         useValue: {
           get(key) {
@@ -35,5 +26,36 @@ describe('ConsoleLoggerService', () => {
   it('should be created', () => {
     const service: ConsoleLoggerService = TestBed.get(ConsoleLoggerService);
     expect(service).toBeTruthy();
+  });
+
+  it('should have called NGX Logger Debug', () => {
+    const service: ConsoleLoggerService = TestBed.get(ConsoleLoggerService);
+    service.debug('some message');
+    expect(mockNGXLogger.debug).toHaveBeenCalled();
+  });
+  it('should have called NGX Logger Info', () => {
+    const service: ConsoleLoggerService = TestBed.get(ConsoleLoggerService);
+    service.info('some message');
+    expect(mockNGXLogger.info).toHaveBeenCalled();
+  });
+  it('should have called NGX Logger fatal', () => {
+    const service: ConsoleLoggerService = TestBed.get(ConsoleLoggerService);
+    service.fatal('some message');
+    expect(mockNGXLogger.fatal).toHaveBeenCalled();
+  });
+  it('should have called NGX Logger trace', () => {
+    const service: ConsoleLoggerService = TestBed.get(ConsoleLoggerService);
+    service.trace('some message');
+    expect(mockNGXLogger.trace).toHaveBeenCalled();
+  });
+  it('should have called NGX Logger warn', () => {
+    const service: ConsoleLoggerService = TestBed.get(ConsoleLoggerService);
+    service.warn('some message');
+    expect(mockNGXLogger.warn).toHaveBeenCalled();
+  });
+  it('should have called NGX Logger error', () => {
+    const service: ConsoleLoggerService = TestBed.get(ConsoleLoggerService);
+    service.error('some message');
+    expect(mockNGXLogger.error).toHaveBeenCalled();
   });
 });

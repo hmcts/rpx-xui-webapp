386ea4ad5de65155ed8dc70d880494ab76aaa971
Boris <36511748+borislavpetrovhmcts@users.noreply.github.com>
2019-06-19 15:31:18 +0100

    Add footer to application (#73)
    * Add footer to application
    
    * lint fixes
    
    * unit test and tslint fixes
    
    * export footer content in a separate file
    
    * remove test data from unit tests and import it instead
    
    * typo fix in unit test

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,5 +1,10 @@
 import {ExuiPageWrapperComponent} from './exui-mian-wrapper/exui-page-wrapper.component';
+import {HmctsGlobalFooterComponent} from './hmcts-global-footer/hmcts-global-footer.component';
 
-export const components: any[] = [ExuiPageWrapperComponent];
+export const components: any[] = [
+  ExuiPageWrapperComponent,
+  HmctsGlobalFooterComponent
+];
 
 export * from './exui-mian-wrapper/exui-page-wrapper.component';
+export * from './hmcts-global-footer/hmcts-global-footer.component';

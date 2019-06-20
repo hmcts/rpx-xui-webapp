40202d73e05eb5ca66f7366d67d78d1542fc820b
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-06-20 15:22:52 +0100

    EUI-235 another temp

--- src/cases/containers/case-search/case-search.component.ts
+++ src/cases/containers/case-search/case-search.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import * as fromCasesFeature from '../../store';
+import { Jurisdiction, CaseType, CaseState, SearchResultView, PaginationMetadata } from '@hmcts/ccd-case-ui-toolkit';
+import { Subscription } from 'rxjs';
+import { AppConfig } from '../../../app/services/ccd-config/ccd-case.config';
+import { ActionBindingModel } from 'src/cases/models/create-case-actions.model';
 
 /**
  * Entry component wrapper for CCD-CASE-CREATE
@@ -14,14 +18,69 @@ import * as fromCasesFeature from '../../store';
   encapsulation: ViewEncapsulation.None
 })
 export class CaseSearchComponent implements OnInit {
+  caseSearchEventsBindings: ActionBindingModel[];
+  fromCasesFeature; any;
+
+  jurisdiction: Jurisdiction;
+  caseType: CaseType;
+  caseState: CaseState;
+  resultView: SearchResultView;
+  paginationMetadata: PaginationMetadata;
+  metadataFields: string[];
+
+  resultsArr: any[] = [];
+
+  paginationSize: number;
+
+  state: any;
 
   constructor(
     public store: Store<fromCasesFeature.State>,
+    private appConfig: AppConfig
   ) {
 
   }
 
   ngOnInit(): void {
+    this.fromCasesFeature = fromCasesFeature;
+    this.caseSearchEventsBindings = [
+      { type: 'onJurisdiction', action: 'JurisdictionSelected' },
+      { type: 'onApply', action: 'ApplySearchFilter' },
+      { type: 'onReset', action: 'Reset' }
+    ];
+    this.state = this.store.pipe(select(fromCasesFeature.getSearchState));
+    this.paginationSize = this.appConfig.getPaginationPageSize();
+
+    this.state.subscribe(st => {
+      if (st.resultView) {
+        this.jurisdiction = st.jurisdiction.value as Jurisdiction;
+        this.caseType = st.caseType.value as CaseType;
+        this.metadataFields = st.metadataFields.value as Array<string>;
+        this.caseState = this.caseType.states[0];
+        this.paginationMetadata = new PaginationMetadata();
+        const data = st.resultView;
+        this.paginationMetadata.total_results_count = data.results.length;
+        this.paginationMetadata.total_pages_count = Math.ceil(data.results.length / this.paginationSize);
+        this.resultsArr = data.results;
+        this.resultView = {
+          ...data,
+          results: data.results.slice(0, this.paginationSize),
+          hasDrafts: data.hasDrafts ? data.hasDrafts : () => false
+        };
+      }
+    });
+  }
+
+  applyChangePage(selected) {
+    const startingPoint = (selected.page - 1) * this.paginationSize;
+    const endingPoint = startingPoint + this.paginationSize;
+    const newArr = this.resultsArr.slice(startingPoint, endingPoint);
+
+    this.resultView = {
+      ...this.resultView,
+      results: newArr,
+      hasDrafts: this.resultView.hasDrafts
+    };
 
   }
 

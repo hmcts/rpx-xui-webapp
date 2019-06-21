2ff599f75328deaa64cc01ecc5bfb7f7f2a52cad
fazanki <fazanki@gmail.com>
2019-06-21 07:39:39 +0100

    adding correct semantics and search case

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,45 +1,22 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import { Store, select } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 import * as fromRoot from '../../store';
-// import {AppTitlesModel} from '../../models/app-titles.model';
-// import {UserNavModel} from '../../models/user-nav.model';
+import {AppTitleModel} from '../../models/app-title.model';
+import {UserNavModel} from '../../models/user-nav.model';
 
 @Component({
   selector: 'exui-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
   @Input() navItems: { active: boolean; href: string; }[];
-  @Input() title;
-  @Input() userNav;
+  @Input() title: AppTitleModel;
+  @Input() userNav: UserNavModel;
   @Output() navigate = new EventEmitter<string>();
 
-  isUserLoggedIn$: Observable<boolean>;
-
   constructor(public store: Store<fromRoot.State>) {}
-
-
-  ngOnInit(): void {
-
-    // this.isUserLoggedIn$ = this.store.pipe(select(fromAuth.getIsAuthenticated));
-    // this.store.pipe(select(fromRoot.getRouterState)).subscribe(rootState => {
-    //   if (rootState) {
-    //     this.updateNavItems(rootState.state.url);
-    //   }
-    // });
-  }
-
-  // updateNavItems(url): void {
-  //   this.navItems = this.navItems.map((item: {href}) => {
-  //     return {
-  //       ...item,
-  //       active: item.href === url
-  //     };
-  //   });
-  // }
 
   onNavigate(event) {
     this.navigate.emit(event);

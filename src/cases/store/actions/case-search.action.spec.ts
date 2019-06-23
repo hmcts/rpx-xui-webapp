88130b7a61a92b37e51e727c8366bf3ba3d92f9c
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-06-23 22:40:00 +0100

    EUi-235 tests

--- src/cases/store/actions/case-search.action.spec.ts
+++ src/cases/store/actions/case-search.action.spec.ts
@@ -1,7 +1,8 @@
 
 import { TestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-import { JURISDICTION_SELECTED, JurisdictionSelected, RESET, Reset, APPLIED, Applied } from './case-search.action';
+import { JURISDICTION_SELECTED, JurisdictionSelected, RESET, Reset, APPLIED, Applied, ApplySearchFilter, APPLY_SEARCH_FILTER,
+  ApplySearchFilterSuccess, APPLY_SEARCH_FILTER_SUCCESS, ApplySearchFilterFail, APPLY_SEARCH_FILTER_FAIL } from './case-search.action';
 
 describe('Case Search Actions', () => {
   describe('Search Cases filter', () => {
@@ -29,6 +30,39 @@ describe('Case Search Actions', () => {
 
         expect({ ...action }).toEqual({
           type: RESET
+        });
+      });
+    });
+
+    describe('ApplySearchFilter', () => {
+      it('should create an action', () => {
+        const payload = {};
+        const action = new ApplySearchFilter({});
+        expect({ ...action }).toEqual({
+          type: APPLY_SEARCH_FILTER,
+          payload
+        });
+      });
+    });
+
+    describe('ApplySearchFilterSuccess', () => {
+      it('should create an action', () => {
+        const payload = {};
+        const action = new ApplySearchFilterSuccess({});
+        expect({ ...action }).toEqual({
+          type: APPLY_SEARCH_FILTER_SUCCESS,
+          payload
+        });
+      });
+    });
+
+    describe('ApplySearchFilterFail', () => {
+      it('should create an action', () => {
+        const payload = {};
+        const action = new ApplySearchFilterFail({});
+        expect({ ...action }).toEqual({
+          type: APPLY_SEARCH_FILTER_FAIL,
+          payload
         });
       });
     });

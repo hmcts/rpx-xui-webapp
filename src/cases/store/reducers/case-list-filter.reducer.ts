25485cc2de79155f4cc21ccf9811b256443d287b
Uday Denduluri <uday.denduluri@gmail.com>
2019-06-25 16:51:29 +0100

    Adding changes to the CaseFilterState

--- src/cases/store/reducers/case-list-filter.reducer.ts
+++ src/cases/store/reducers/case-list-filter.reducer.ts
@@ -3,19 +3,58 @@ import * as fromCases from '../actions/case-list.filter.action';
 export interface CaseFilterState {
   loading: boolean;
   loaded: boolean;
+  selected: {
+    caseState: {id: string;  name: string; description: string},
+    caseType: {
+      id: string,
+      name: string,
+      description: string
+    },
+    jurisdiction: {
+      id: string,
+      name: string,
+      description: string
+    }
+  };
 }
 
-export function reducerCaseListFilter( state , action: fromCases.CaseListFilterAction): CaseFilterState {
+const initialState: CaseFilterState = {
+  loading: false,
+  loaded: false,
+  selected: {
+    caseState: {
+      id: '',
+      name: '',
+      description: ''
+    },
+    jurisdiction: {
+      id: '',
+      name: '',
+      description: ''
+    },
+    caseType: {
+      id: '',
+      name: '',
+      description: ''
+    }
+  }
+};
+
+export function reducerCaseListFilter( state = initialState, action: fromCases.CaseListFilterAction): CaseFilterState {
   switch (action.type) {
     case fromCases.APPLYFILTER: {
+     const caseType = {...action.payload.selected.caseType.id};
+      debugger; 
       return {
-          loading: false,
-          loaded: true
-      };
+          ...state,
+          caseType,
+          loading: true,
+          loaded: false
+      } as CaseFilterState;
     }
 
     case fromCases.RESETFILTER:
-      return null;
+      return initialState;
   }
   return state;
 }

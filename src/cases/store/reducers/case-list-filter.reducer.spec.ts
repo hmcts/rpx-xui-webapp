995a5b7515f625a9a12773818d76d531c54e61c5
Uday Denduluri <uday.denduluri@gmail.com>
2019-06-26 15:00:18 +0100

    Adding the unit tests for the ApplyFilter reducer and create list component

---
+++ src/cases/store/reducers/case-list-filter.reducer.spec.ts
@@ -0,0 +1,59 @@
+import * as filterCaseList from './case-list-filter.reducer';
+import * as fromCases from '../actions/case-list.filter.action';
+
+describe('CaseList Filter Reducer', () => {
+    describe('undefined action', () => {
+      it('should return the default state', () => {
+        const { initialCaseFilterState } = filterCaseList;
+        const action = {} as any;
+        const state = filterCaseList.reducerCaseListFilter(undefined, action);
+        expect(state).toBe(initialCaseFilterState);
+      });
+    });
+});
+
+
+describe('[CaseListFilter] Applied', () => {
+  it('should set correct object', () => {
+    const { initialCaseFilterState } = filterCaseList;
+    const action = new fromCases.ApplyFilter({
+      selected: {
+      caseState: {
+        id: '1',
+        name: 'One',
+        description: 'One desc'
+      } ,
+      jurisdiction: {
+        id: '2',
+        name: 'Two',
+        description: 'Two desc'
+      } ,
+      caseType: {
+        id: '3',
+        name: 'case type',
+        description: 'Case Type Desc'
+      }
+    }
+    });
+
+    const state = filterCaseList.reducerCaseListFilter(initialCaseFilterState, action);
+
+    expect(state.loading).toEqual(true);
+    expect(state.loaded).toEqual(false);
+    expect(state.caseState).toEqual({
+        id: '1',
+        name: 'One',
+        description: 'One desc'
+      });
+    expect(state.jurisdiction).toEqual({
+        id: '2',
+        name: 'Two',
+        description: 'Two desc'
+      });
+    expect(state.caseType).toEqual({
+        id: '3',
+        name: 'case type',
+        description: 'Case Type Desc'
+      });
+  });
+});
--- src/cases/store/reducers/case-list-filter.reducer.ts
+++ src/cases/store/reducers/case-list-filter.reducer.ts
@@ -1,20 +1,28 @@
 import * as fromCases from '../actions/case-list.filter.action';
 
-interface CaseIdNameDescription {
-  id: string;
-  name: string;
-  description: string;
-}
+
 
 export interface CaseFilterState {
   loading: boolean;
   loaded: boolean;
-    caseState: CaseIdNameDescription;
-    caseType: CaseIdNameDescription;
-    jurisdiction: CaseIdNameDescription;
+    caseState: {
+      id: string;
+      name: string;
+      description: string;
+    };
+    caseType: {
+      id: string;
+      name: string;
+      description: string;
+    };
+    jurisdiction: {
+      id: string;
+      name: string;
+      description: string;
+    };
 }
 
-const initialState: CaseFilterState = {
+export const initialCaseFilterState: CaseFilterState = {
   loading: false,
   loaded: false,
   caseState: {
@@ -34,21 +42,21 @@ const initialState: CaseFilterState = {
   }
 };
 
-export function reducerCaseListFilter( state = initialState, action: fromCases.CaseListFilterAction): CaseFilterState {
+export function reducerCaseListFilter( state = initialCaseFilterState, action: fromCases.CaseListFilterAction): CaseFilterState {
   switch (action.type) {
-    case fromCases.APPLYFILTER: {
+    case fromCases.APPLYCASELISTFILTER: {
       return {
           ...state,
           loading: true,
           loaded: false,
           caseState: {...action.payload.selected.caseState},
-          jurisdiction: {...action.payload.selected.caseState},
+          jurisdiction: {...action.payload.selected.jurisdiction},
           caseType: {...action.payload.selected.caseType}
       } as CaseFilterState;
     }
 
     case fromCases.RESETFILTER:
-      return initialState;
+      return initialCaseFilterState;
   }
   return state;
 }

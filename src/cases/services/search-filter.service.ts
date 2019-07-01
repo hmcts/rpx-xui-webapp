31ca29a0fb45bb383ec505ba771cc8da2b69dd2f
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-07-01 12:55:49 +0100

    EUI-337 sanitised formgroup and fixed bug, tests

--- src/cases/services/search-filter.service.ts
+++ src/cases/services/search-filter.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { SearchService } from '@hmcts/ccd-case-ui-toolkit';
 import { Observable } from 'rxjs';
+import { FormGroup } from '@angular/forms';
 
 @Injectable()
 export class SearchFilterService {
@@ -9,66 +10,81 @@ export class SearchFilterService {
         private ccdSearchService: SearchService
     ) { }
 
+    metadataFields: string[];
+
     search(payload): Observable<any> {
-        const jurisdictionId = payload.selected.jurisdiction.id;
-        const caseTypeId = payload.selected.caseType.id;
-        const metaCriteria = payload.selected.metadataFields;
-        const caseCriteria = this.stringifyKeysAndValues(payload.selected.formGroup.value);
-        debugger;
-        return this.ccdSearchService.search(jurisdictionId, caseTypeId, metaCriteria, caseCriteria) as any;
+
+        const filter = payload.selected;
+
+        let searchParams = {};
+
+        this.metadataFields = filter.metadataFields;
+
+        if (filter.caseState) {
+            searchParams = {
+                ...searchParams,
+                state: filter.caseState.id
+            };
+        }
+
+        if (filter.page) {
+            searchParams = {
+                ...searchParams,
+                page: filter.page
+            };
+        }
+
+        const jurisdictionId = filter.jurisdiction.id;
+        const caseTypeId = filter.caseType.id;
+        const filters = this.getCaseFilterFromFormGroup(filter.formGroup);
+        const caseFilters = filters.caseFilter;
+        const metadataFilters = Object.assign(searchParams, filters.metadataFilter);
+
+        return this.ccdSearchService.search(jurisdictionId, caseTypeId, metadataFilters, caseFilters) as any;
     }
 
+    private getCaseFilterFromFormGroup(formGroup?: FormGroup): { caseFilter, metadataFilter } {
+        const result = {
+            caseFilter: {},
+            metadataFilter: {}
+        };
 
-    stringifyKeysAndValues(formGroupValue) {
-        for (const attributeName of Object.keys(formGroupValue)) {
-            if (attributeName.indexOf('[') > -1) {
-                const newAttributeName = attributeName.slice(1, attributeName.length - 1).toLowerCase();
-                const newValue = formGroupValue[attributeName].toString();
-                formGroupValue = {
-                    ...formGroupValue,
-                    [newAttributeName]: newValue
-                };
-                delete formGroupValue[attributeName];
+        if (formGroup) {
+            this.buildFormDetails('', result, formGroup.value);
+        }
+        return result;
+    }
+
+    private buildFormDetails(parentPrefix: string, target: object, formGroupValue: object): void {
+        let prefix = parentPrefix;
+        if (parentPrefix && parentPrefix.length > 0) {
+            prefix = parentPrefix + '.';
+        }
+        for (let attributeName of Object.keys(formGroupValue)) {
+            const value = formGroupValue[attributeName];
+            if (this.isStringOrNumber(value)) {
+                const filterType = this.getFilterType(attributeName);
+                attributeName = this.sanitiseMetadataFieldName(filterType, attributeName);
+                target[filterType][prefix + attributeName] = value;
+            } else if (value) {
+                this.buildFormDetails(attributeName, target, value);
             }
         }
-        return formGroupValue;
     }
 
-    // search(payload): Observable<any> {
-    //     const jurisdictionId = payload.selected.jurisdiction.id;
-    //     const caseTypeId = payload.selected.caseType.id;
-    //     const metaCriteria = payload.selected.metadataFields;
-    //     const caseCriteria = this.getCaseFilterFromFormGroup(payload.selected.formGroup);
-    //     debugger;
-    //     return this.ccdSearchService.search(jurisdictionId, caseTypeId, metaCriteria, caseCriteria) as any;
-    // }
+    private isStringOrNumber(value: any): boolean {
+        return (typeof value === 'string' && value.length !== 0) || (typeof value === 'number');
+    }
 
-    // private getCaseFilterFromFormGroup(formGroup?: FormGroup): object {
-    //     const result = {};
+    private getFilterType(fieldName: string): string {
+        return (this.metadataFields && (this.metadataFields.indexOf(fieldName) > -1)) ?
+            'metadataFilter' : 'caseFilter';
+    }
 
-    //     if (formGroup) {
-    //         this.buildFormDetails('case', result, formGroup.value);
-    //     }
-    //     return result;
-    // }
-
-    // private buildFormDetails(parentPrefix: string, target: object, formGroupValue: object): void {
-    //     let prefix = parentPrefix;
-    //     if (parentPrefix && parentPrefix.length > 0) {
-    //         prefix = parentPrefix + '.';
-    //     }
-    //     for (const attributeName of Object.keys(formGroupValue)) {
-    //         const value = formGroupValue[attributeName];
-    //         if (this.isStringOrNumber(value)) {
-    //             debugger;
-    //             target[prefix + attributeName] = value;
-    //         } else if (value) {
-    //             this.buildFormDetails(attributeName, target, value);
-    //         }
-    //     }
-    // }
-
-    // private isStringOrNumber(value: any): boolean {
-    //     return (typeof value === 'string' && value.length !== 0) || (typeof value === 'number');
-    // }
+    private sanitiseMetadataFieldName(filterType: string, fieldName: string): string {
+        if (filterType === 'metadataFilter') {
+            fieldName = fieldName.replace(/\[(.*?)]/g, '$1').toLocaleLowerCase();
+        }
+        return fieldName;
+    }
 }

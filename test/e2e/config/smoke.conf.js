392031e2ba8f66876cc3ce851181c320b7e75205
Toireasa Moley <toireasa.moley@hmcts.net>
2019-07-25 11:28:07 +0100

    Adding prod config

--- test/e2e/config/smoke.conf.js
+++ test/e2e/config/smoke.conf.js
@@ -86,7 +86,7 @@ const config = {
     strict: true,
     // format: ['node_modules/cucumber-pretty'],
     format: ['node_modules/cucumber-pretty', 'json:reports_json/results.json'],
-    tags: ['@smoke'],
+    tags: ['@ignore'],
     require: [
       '../support/timeout.js',
       '../support/world.js',

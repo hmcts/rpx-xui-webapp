d5b8576aa875ab350ad6c26630fde479676dd549
praveenrajendran101 <Praveen.Rajendran@hmcts.net>
2019-07-30 13:12:48 +0100

    EXUI-408 Manage case phase-banner

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -3,13 +3,14 @@ import {HmctsGlobalFooterComponent} from './hmcts-global-footer/hmcts-global-foo
 import {HeaderComponent} from './header/header.component';
 import {HmctsGlobalHeaderComponent} from './hmcts-global-header/hmcts-global-header.component';
 import {HmctsPrimaryNavigationComponent} from './hmcts-primary-navigation/hmcts-primary-navigation.component';
-
+import {PhaseBannerComponent} from './phase-banner/phase-banner.component'
 export const components: any[] = [
   ExuiPageWrapperComponent,
   HmctsGlobalFooterComponent,
   HeaderComponent,
   HmctsGlobalHeaderComponent,
-  HmctsPrimaryNavigationComponent
+  HmctsPrimaryNavigationComponent,
+  PhaseBannerComponent
 ];
 
 export * from './exui-mian-wrapper/exui-page-wrapper.component';
@@ -17,4 +18,4 @@ export * from './hmcts-global-footer/hmcts-global-footer.component';
 export * from './header/header.component';
 export * from './hmcts-global-header/hmcts-global-header.component';
 export * from './hmcts-primary-navigation/hmcts-primary-navigation.component';
-
+export * from './phase-banner/phase-banner.component';

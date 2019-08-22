26b83b2a1b8d503fd6a0540d5ccc60e4fb112013
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-08-22 14:32:02 +0100

    Another trial

--- src/cases/containers/case-list/case-list.component.spec.ts
+++ src/cases/containers/case-list/case-list.component.spec.ts
@@ -1,86 +1,71 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import {CaseListComponent} from './case-list.component';
-import {
-  CaseUIToolkitModule, DraftService, AlertService, HttpService, AuthService as CCDAuthService, CasesService,
-  HttpErrorService, AbstractAppConfig, CaseEditWizardGuard, RouterHelperService,
-  DocumentManagementService, PageValidationService, PlaceholderService, SearchService, RequestOptionsBuilder, SearchFiltersModule
-} from '@hmcts/ccd-case-ui-toolkit';
-import {AppConfig} from '../../../app/services/ccd-config/ccd-case.config';
-import {ScrollToService} from '@nicky-lenaers/ngx-scroll-to';
-import {RouterTestingModule} from '@angular/router/testing';
-import {combineReducers, StoreModule} from '@ngrx/store';
-import {SharedModule} from '../../../app/shared/shared.module';
-import {AppConfigService} from '../../../app/services/config/configuration.services';
-import {reducers} from '../../store/reducers';
-import * as fromCases from '../../store/reducers/';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CaseListComponent } from './case-list.component';
+import { Store, StoreModule, combineReducers } from '@ngrx/store';
+import * as fromCaseSearchStore from '../../store';
+import { RouterTestingModule } from '@angular/router/testing';
+import * as fromRoot from '../../../app/store/reducers';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AppConfig } from '../../../app/services/ccd-config/ccd-case.config';
+import { of } from 'rxjs';
+import { Jurisdiction, CaseType, CaseState, SearchResultView, PaginationMetadata } from '@hmcts/ccd-case-ui-toolkit';
+import { mockedSearchResultPayload, mockedSearchResultResult, JURISDICTION_1 } from '../../mock/search-filter.mock';
+import { ApplySearchFilterSuccess } from '../../store';
 
-class MockSortService {
-  features = {};
-  getFeatureToggle() {}
-  getEditorConfiguration() { }
-}
 describe('CaseListComponent', () => {
-  let component: CaseListComponent;
   let fixture: ComponentFixture<CaseListComponent>;
+  let component: CaseListComponent;
+  let store: Store<fromCaseSearchStore.SearchState>;
+  let storePipeMock: any;
+  let storeDispatchMock: any;
 
-  beforeEach(async(() => {
+  const appConfigMock = {
+    getPaginationPageSize: () => 10
+  };
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        CaseUIToolkitModule,
-        HttpClientTestingModule,
-        StoreModule.forRoot({...reducers, cases: combineReducers(fromCases.reducers)}),
-        SharedModule,
-        SearchFiltersModule,
+        StoreModule.forRoot({
+          ...fromRoot.reducers,
+          feature: combineReducers(fromCaseSearchStore.reducers),
+        }),
       ],
-      declarations: [ CaseListComponent ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ],
+      declarations: [
+        CaseListComponent
+      ],
       providers: [
-        PlaceholderService,
-        CasesService,
-        CCDAuthService,
-        HttpService,
-        HttpErrorService,
-        AlertService,
-        DraftService,
-        PageValidationService,
-        CaseEditWizardGuard,
-        RouterHelperService,
-        DocumentManagementService,
-        AppConfig,
-        AppConfigService,
-        RequestOptionsBuilder,
-        {
-          provide: SearchService,
-          useValue: {
-            requestOptionsBuilder: RequestOptionsBuilder
-          }
-        },
-        {
-          provide: AbstractAppConfig,
-          useExisting: AppConfig
-        },
-        {
-          provide: AppConfigService,
-          useClass: MockSortService
-        },
-        ScrollToService
+        { provide: AppConfig, useValue: appConfigMock }
       ]
-    })
-    .compileComponents();
-  }));
+    }).compileComponents();
 
-  beforeEach(() => {
+    localStorage.setItem('savedQueryParams', '{"jurisdiction": ' + JSON.stringify(JURISDICTION_1) + '}');
+
+    store = TestBed.get(Store);
+
+    storePipeMock = spyOn(store, 'pipe');
+    storeDispatchMock = spyOn(store, 'dispatch');
+
     fixture = TestBed.createComponent(CaseListComponent);
     component = fixture.componentInstance;
-
+    component.jurisdiction$ = storePipeMock.and.returnValue(of(new Jurisdiction()));
+    component.caseType$ = storePipeMock.and.returnValue(of(new CaseType()));
+    component.caseState$ = storePipeMock.and.returnValue(of(new CaseState()));
+    component.resultView$ = storePipeMock.and.returnValue(of(new SearchResultView()));
+    component.paginationMetadata$ = storePipeMock.and.returnValue(of(new PaginationMetadata()));
+    component.metadataFields$ = storePipeMock.and.returnValue(of([]));
+    fixture.detectChanges();
   });
+
+  afterEach(() => {
+    localStorage.removeItem('savedQueryParams');
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-
 
 });

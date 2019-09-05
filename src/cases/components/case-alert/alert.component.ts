1c4d289b81adcc0b16699e966b8cf56db823122c
praveenrajendran101 <Praveen.Rajendran@hmcts.net>
2019-09-05 17:21:23 +0100

    Updated Alert component

--- src/cases/components/case-alert/alert.component.ts
+++ src/cases/components/case-alert/alert.component.ts
@@ -2,34 +2,35 @@ import { Component, Input, OnDestroy } from '@angular/core';
 import { AlertService, Alert, AlertLevel } from '@hmcts/ccd-case-ui-toolkit';
 import { select } from '@ngrx/store';
 import { Subscription, Observable } from 'rxjs';
-
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'exui-alert',
-  templateUrl: './alert.component.html'
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.scss']
 })
 
 export class AlertComponent implements OnDestroy {
   message = '';
   level = '';
+  currentUrl = '';
   alertMessageObservable: Observable<Alert>;
   alertMessageSubscription: Subscription;
+  routeSubscription: Subscription;
 
-  constructor(private alertService: AlertService) {
-
+  constructor(private alertService: AlertService, private router: Router) {
     this.alertMessageObservable = this.alertService.alerts.pipe(select( alert => alert));
-
+    this.routeSubscription = this.router.events.subscribe((val) => this.message = '');
     this.alertMessageSubscription = this.alertMessageObservable.subscribe(o => {
       if (o) {
         this.message = o.message;
         this.level = o.level;
       }
-
     });
   }
 
   ngOnDestroy() {
     this.alertMessageSubscription.unsubscribe();
+    this.routeSubscription.unsubscribe();
   }
 }

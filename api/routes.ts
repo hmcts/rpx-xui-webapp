9a693b1a8f42f24b6035f6c464995c2b7ca26d68
Pardeep Singh Basi <paddybasi@gmail.com>
2019-09-10 17:21:33 +0100

    fixed document upload and viewing

--- api/routes.ts
+++ api/routes.ts
@@ -1,4 +1,3 @@
-
 import * as express from 'express'
 import * as auth from './auth'
 import authInterceptor from './lib/middleware/auth'
@@ -10,7 +9,6 @@ const router = express.Router({ mergeParams: true })
 router.use(serviceTokenMiddleware)
 router.use(authInterceptor)
 
-router.post('/documents', proxy.postDocuments)
 router.get('/*', proxy.get)
 router.post('/*', proxy.post)
 router.put('/*', proxy.put)

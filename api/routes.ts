0d1f58d08d7bbd8f4e377651129580a8a2879d23
Uday Denduluri <uday.denduluri@gmail.com>
2019-09-11 11:42:05 +0100

    Config cleanup

--- api/routes.ts
+++ api/routes.ts
@@ -1,12 +1,10 @@
 import * as express from 'express'
 import * as auth from './auth'
 import authInterceptor from './lib/middleware/auth'
-import serviceTokenMiddleware from './lib/middleware/serviceToken'
 import * as proxy from './lib/proxy'
 
 const router = express.Router({ mergeParams: true })
 
-router.use(serviceTokenMiddleware)
 router.use(authInterceptor)
 
 router.get('/*', proxy.get)

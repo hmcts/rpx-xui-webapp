3407a9076e05eff9351018052c09b85811bce43f
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-09-12 09:33:41 +0100

    EUI-758 removed cohCorApi and s2s health

--- api/config/environments/prod.config.ts
+++ api/config/environments/prod.config.ts
@@ -24,9 +24,7 @@ export default {
     health: {
         ccdComponentApi: 'https://ccd-api-gateway-web-prod.service.core-compute-prod.internal/health',
         ccdDataApi: 'https://ccd-data-store-api-prod.service.core-compute-prod.internal/health',
-        cohCorApi: 'https://coh-cor-prod.service.core-compute-prod.internal/health',
         documentsApi: 'https://dm-store-prod.service.core-compute-prod.internal/health',
-        s2s: 'https://rpe-service-auth-provider-prod.service.core-compute-prod.internal/health',
     },
     sessionSecret: 'secretSauce',
     useProxy: false,

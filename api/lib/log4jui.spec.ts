e093bae99c3182b55d4493cffe3921ea71d5a725
Pardeep Singh Basi <paddybasi@gmail.com>
2019-09-18 11:21:49 +0100

    cleaned up some tests

--- api/lib/log4jui.spec.ts
+++ api/lib/log4jui.spec.ts
@@ -3,18 +3,39 @@ import { expect } from 'chai'
 import 'mocha'
 import * as sinon from 'sinon'
 import * as sinonChai from 'sinon-chai'
-import { mockReq, mockRes } from 'sinon-express-mock'
+import { mockReq } from 'sinon-express-mock'
 
 chai.use(sinonChai)
 
 import * as log4js from 'log4js'
-import * as log4jui from '../lib/log4jui'
-import { leftPad } from '../lib/log4jui'
-import { isJUILogger, JUILogger } from '../lib/models'
+import * as log4jui from './log4jui'
+import { leftPad } from './log4jui'
 import * as responseRequest from './middleware/responseRequest'
+import { isJUILogger } from './models'
 
 describe('log4jui', () => {
+    let sandbox
+    let spyObj
+
+    beforeEach( () => {
+      sandbox = sinon.createSandbox()
+      spyObj = {
+        addContext: sandbox.spy(),
+        error: sandbox.spy(),
+        info: sandbox.spy(),
+        warn: sandbox.spy(),
+      }
+      sandbox.stub(log4js, 'getLogger').returns(spyObj)
+    })
+
+    afterEach( () => {
+      sandbox.restore()
+    })
+
     describe('getLogger', () => {
+        beforeEach( () => {
+            sandbox.restore()
+        })
         it('Should  return an instance of JUILogger', () => {
             expect(isJUILogger(log4jui.getLogger(''))).to.equal(true)
         })
@@ -24,83 +45,60 @@ describe('log4jui', () => {
     })
 
     describe('warn', () => {
-        it('should log a warn with log4js', async () => {
-            const spy = sinon.spy()
-            const stub = sinon.stub(log4js, 'getLogger')
-            stub.returns({ warn: spy, addContext: sinon.spy() })
-
+        it('should log a warn with log4js', () => {
             const logger = log4jui.getLogger('test')
             logger.warn('warning')
-
-            expect(spy).to.be.calledWith('warning')
-            stub.restore()
+            expect(spyObj.warn).to.be.calledWith('warning')
         })
     })
 
     describe('info', () => {
-        it('should log an info with log4js', async () => {
-            const spy = sinon.spy()
-            const stub = sinon.stub(log4js, 'getLogger')
-            stub.returns({ info: spy, addContext: sinon.spy() })
-
+        it('should log an info with log4js', () => {
             const logger = log4jui.getLogger('test')
             logger.info('warning')
-
-            expect(spy).to.be.calledWith('warning')
-            stub.restore()
+            expect(spyObj.info).to.be.calledWith('warning')
         })
     })
-})
 
-describe('error', () => {
-    it('should log an error with log4js', async () => {
-        const spy = sinon.spy()
-        const stub = sinon.stub(log4js, 'getLogger')
-        stub.returns({ error: spy, addContext: sinon.spy() })
-
-        const logger = log4jui.getLogger('test')
-        logger.error('warning')
-
-        expect(spy).to.be.calledWith('warning')
-        stub.restore()
+    describe('error', () => {
+        it('should log an error with log4js',  () => {
+            const logger = log4jui.getLogger('test')
+            logger.error('warning')
+            expect(spyObj.error).to.be.calledWith('warning')
+        })
     })
-})
 
-describe('prepareMessage', () => {
-    it('prepare a message to be sent with userId and sessionId', async () => {
-        const req = mockReq({
-            cookies: [],
-            session: {
-                user: {
-                    id: 'testId',
+    describe('prepareMessage', () => {
+        it('prepare a message to be sent with userId and sessionId', () => {
+            const req = mockReq({
+                cookies: [],
+                session: {
+                    user: {
+                        id: 'testId',
+                    },
                 },
-            },
+            })
+
+            req.cookies.__sessionId__ = 'testCookie'
+            sandbox.stub(responseRequest, 'isReqResSet').returns(true)
+            sandbox.stub(responseRequest, 'request').returns(req)
+
+            expect(log4jui.prepareMessage('hello')).to.equal('[testId - testCookie] - hello')
         })
-        const res = mockRes()
-
-        req.cookies.__sessionId__ = 'testCookie'
-        const stub = sinon.stub(responseRequest, 'isReqResSet')
-        const stub2 = sinon.stub(responseRequest, 'request')
-        stub.returns(true)
-        stub2.returns(req)
-
-        expect(log4jui.prepareMessage('hello')).to.equal('[testId - testCookie] - hello')
-
-        stub.restore()
-        stub2.restore()
-    })
-})
-
-describe('leftPad', () => {
-    it('should not left pad a string if string length is same as param length', () => {
-        const str = 'test'
-        const result = `    ${str}`
-        expect(leftPad(str, 4)).not.to.equal(result)
     })
 
-    it('should left pad a string if param length is greater than string length', () => {
-        const str = 'test'
-        const result = `    ${str}`
-        expect(leftPad(str, 8)).to.equal(result)
+    describe('leftPad', () => {
+        it('should not left pad a string if string length is same as param length', () => {
+            const str = 'test'
+            const result = `    ${str}`
+            expect(leftPad(str, 4)).not.to.equal(result)
+        })
+
+        it('should left pad a string if param length is greater than string length', () => {
+            const str = 'test'
+            const result = `    ${str}`
+            expect(leftPad(str, 8)).to.equal(result)
+        })
     })
+
 })

6179181fdad665d372f8cc133b0db8f149484098
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-09-26 15:00:10 +0100

    EUI-706 simplified and integrated code

--- src/cases/containers/case-create-submit/case-create-submit.component.ts
+++ src/cases/containers/case-create-submit/case-create-submit.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { CaseEventTrigger, CaseEventData, Draft, CaseReferencePipe, CaseEditPageComponent, AlertService,
-  CasesService, DraftService } from '@hmcts/ccd-case-ui-toolkit';
-import { Observable } from 'rxjs';
-import { EventStatusService } from '@hmcts/ccd-case-ui-toolkit/dist/shared/services';
+import { CaseEventTrigger, CaseEventData, CasesService, DraftService, Draft } from '@hmcts/ccd-case-ui-toolkit';
 import * as fromCaseCreate from '../../store';
 import {Store} from '@ngrx/store';
 import { ActionBindingModel } from 'src/cases/models/create-case-actions.model';
 import * as fromCases from '../../../cases/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'exui-case-create-submit',
@@ -26,10 +24,7 @@ export class CaseCreateSubmitComponent implements OnInit {
   constructor(
     private casesService: CasesService,
     private draftService: DraftService,
-    private router: Router,
-    private alertService: AlertService,
     private route: ActivatedRoute,
-    private caseReferencePipe: CaseReferencePipe,
     private store: Store<fromCaseCreate.State>
   ) {
     this.eventTrigger = route.snapshot.data.eventTrigger;
@@ -41,104 +36,34 @@ export class CaseCreateSubmitComponent implements OnInit {
       this.jurisdictionId = params.jid;
       this.caseTypeId = params.ctid;
     });
+
+    /**
+     * Mapping CCD components eventsBindings to ExUI Actions
+     */
+    this.caseCreateSubmitEventsBindings = [
+      {type: 'cancelled', action: 'CreateCaseReset'},
+      {type: 'submitted', action: 'ApplyChange'}
+    ];
   }
 
-  submit(): (sanitizedEditForm: CaseEventData) => any {
+  submit(): (sanitizedEditForm: CaseEventData) => Observable<object> {
     return (sanitizedEditForm: CaseEventData) => {
       sanitizedEditForm.draft_id = this.eventTrigger.case_id;
-      return this.casesService.createCase(this.jurisdictionId, this.caseTypeId, sanitizedEditForm);
+      return this.casesService.createCase(this.jurisdictionId, this.caseTypeId, sanitizedEditForm) as any;
     };
   }
 
-  validate(): (sanitizedEditForm: CaseEventData, pageId: string) => any {
+  validate(): (sanitizedEditForm: CaseEventData, pageId: string) => Observable<object> {
     return (sanitizedEditForm: CaseEventData, pageId: string) => this.casesService.validateCase(this.caseTypeId,
-      sanitizedEditForm, pageId);
+      sanitizedEditForm, pageId) as any;
   }
 
-  saveDraft(): (caseEventData: CaseEventData) => any {
+  saveDraft(): (caseEventData: CaseEventData) => Observable<Draft> {
     if (this.eventTrigger.can_save_draft) {
       return (caseEventData: CaseEventData) => this.draftService.createOrUpdateDraft(this.caseTypeId,
             this.eventTrigger.case_id,
-            caseEventData);
+            caseEventData) as any;
     }
   }
 
-  submitted(event: any): void {
-    const caseId: string = event.caseId;
-    const eventStatus: string = event.status;
-    this.router
-      .navigate(['case', this.jurisdictionId, this.caseTypeId, caseId])
-      .then(() => {
-        const caseReference = this.caseReferencePipe.transform(String(caseId));
-        if (EventStatusService.isIncomplete(eventStatus)) {
-          this.alertFailure(eventStatus, caseReference);
-        } else {
-          this.alertSuccess(eventStatus, caseReference);
-        }
-    });
-  }
-
-  cancel(event: any): Promise<boolean> {
-    if (this.eventTrigger.can_save_draft) {
-      switch (event.status) {
-        case CaseEditPageComponent.NEW_FORM_DISCARD:
-          return this.router.navigate(['list/case']);
-        case CaseEditPageComponent.RESUMED_FORM_DISCARD:
-          return this.router.navigate([`case/${this.jurisdictionId}/${this.caseTypeId}/${this.eventTrigger.case_id}`]);
-        case CaseEditPageComponent.NEW_FORM_SAVE:
-          this.saveDraft().call(null, event.data).subscribe(_ => {
-            return this.router.navigate(['list/case'])
-              .then(() => {
-                this.alertService.setPreserveAlerts(true);
-                this.alertService.success(`The draft has been successfully saved`);
-              });
-          }, error => {
-            console.log('error=', error);
-            this.alertService.setPreserveAlerts(true);
-            this.alertService.warning(error.message);
-            this.router.navigate(['list/case']);
-          });
-          break;
-        case CaseEditPageComponent.RESUMED_FORM_SAVE:
-          this.saveDraft().call(null, event.data).subscribe(_ => {
-            return this.router.navigate([`case/${this.jurisdictionId}/${this.caseTypeId}/${this.eventTrigger.case_id}`])
-              .then(() => {
-                this.alertService.setPreserveAlerts(true);
-                this.alertService.success(`The draft has been successfully saved`);
-              });
-            }, error => {
-              console.log('error=', error);
-              this.alertService.setPreserveAlerts(true);
-              this.alertService.warning(error.message);
-          });
-          break;
-      }
-    } else {
-      return this.router.navigate(['list/case']);
-    }
-  }
-
-  private alertSuccess(eventStatus, caseReference) {
-    eventStatus = eventStatus || EventStatusService.CALLBACK_STATUS_COMPLETE;
-    switch (eventStatus) {
-      case EventStatusService.CALLBACK_STATUS_COMPLETE:
-        this.alertService.success(`Case #${caseReference} has been created.`);
-        break;
-      case EventStatusService.DELETE_DRAFT_STATUS_COMPLETE:
-        this.alertService.success(`Case #${caseReference} has been created. The draft has been successfully deleted`);
-        break;
-    }
-  }
-
-  private alertFailure(eventStatus, caseReference) {
-    switch (eventStatus) {
-      case EventStatusService.CALLBACK_STATUS_INCOMPLETE:
-        this.alertService.warning(`Case #${caseReference} has been created but the callback service cannot be completed`);
-        break;
-      case EventStatusService.DELETE_DRAFT_STATUS_INCOMPLETE:
-        this.alertService.warning(`Case #${caseReference} has been created. The draft store is currently down so the draft
-         was not deleted.`);
-        break;
-    }
-  }
 }

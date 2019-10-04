d7d7bd66bbef1e77e87260df94e4c1cdf4c7551f
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-10-04 17:42:51 +0100

    EUI-882 unfinished

--- src/app/shared/guards/health-check.guard.spec.ts
+++ src/app/shared/guards/health-check.guard.spec.ts
@@ -1,8 +1,9 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { HealthCheckGuard } from './health-check.guard';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, Store } from '@ngrx/store';
 import { HealthCheckService } from '../services/health-check.service';
 import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 class HttpClientMock {
 
@@ -11,22 +12,59 @@ class HttpClientMock {
     }
 }
 
+const healthCheckServiceMock = {
+    http: null,
+    store: null,
+    doHealthCheck: () => {
+        console.log('@@@@@@@@@@@@@@@@@@@@@@@');
+        return of('response');
+    },
+    ngOnDestroy: () => {}
+};
+
 describe('HealthCheckGuard', () => {
-    beforeEach(() => {
+    let healthCheckGuardInstance: HealthCheckGuard;
+    let healthCheckServiceInstance: HealthCheckService;
+
+    beforeEach(async () => {
         TestBed.configureTestingModule({
             imports: [
                 StoreModule.forRoot({})
             ],
             providers: [
                 HealthCheckGuard,
-                HealthCheckService,
-                { provide: HttpClient, useClass: HttpClientMock }
+                { provide: HttpClient, useClass: HttpClientMock },
+                { provide: HealthCheckService, useValue: healthCheckServiceMock }
             ]
         });
     });
 
-    it('should exist', inject([HealthCheckGuard], (guard: HealthCheckGuard) => {
-        expect(guard).toBeTruthy();
+    beforeEach(() => {
+        healthCheckGuardInstance = TestBed.get(HealthCheckGuard);
+        healthCheckServiceInstance = TestBed.get(HealthCheckService);
+    });
+
+    it('should have canActivate', inject([HealthCheckGuard], (guard: HealthCheckGuard) => {
+        expect(guard.canActivate).toBeDefined();
     }));
+
+    describe('canActivate', () => {
+        // it('should trigger service call', () => {
+        //     healthCheckGuardInstance = new HealthCheckGuard(healthCheckServiceInstance, null);
+        //     const doHealthCheckSpy = spyOn(healthCheckServiceInstance, 'doHealthCheck');
+        //     healthCheckGuardInstance.canActivate();
+        //     expect(doHealthCheckSpy()).toHaveBeenCalled();
+        // });
+
+
+        // it('should trigger service call', inject([HealthCheckGuard, HealthCheckService],
+        //     (guard: HealthCheckGuard, service: HealthCheckService) => {
+
+        //         spyOn(service, 'doHealthCheck');
+        //         guard.canActivate();
+        //         expect(service.doHealthCheck()).toHaveBeenCalled();
+        // }));
+    });
+
 
 });

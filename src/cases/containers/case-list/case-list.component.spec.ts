b76236ceabae7518e2f6cd4c31351f0d81b3e01c
Philip <philip.aberneithy@acp.aws.accenture.com>
2019-10-07 15:10:33 +0100

    Tests around Toggle Button.

--- src/cases/containers/case-list/case-list.component.spec.ts
+++ src/cases/containers/case-list/case-list.component.spec.ts
@@ -1,11 +1,12 @@
-import { State } from './../../../app/store/reducers/index';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {State} from './../../../app/store/reducers/index';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {CaseListComponent} from './case-list.component';
 import {AppConfig} from '../../../app/services/ccd-config/ccd-case.config';
-import { Store} from '@ngrx/store';
+import {DefinitionsService} from '@hmcts/ccd-case-ui-toolkit/dist/shared/services/definitions/definitions.service';
+import {Store} from '@ngrx/store';
 import {AppConfigService} from '../../../app/services/config/configuration.services';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {provideMockStore, MockStore} from '@ngrx/store/testing';
 
 describe('CaseListComponent', () => {
   let component: CaseListComponent;
@@ -14,7 +15,7 @@ describe('CaseListComponent', () => {
   const mockService = jasmine.createSpy();
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CaseListComponent ],
+      declarations: [CaseListComponent],
       schemas: [NO_ERRORS_SCHEMA],
       providers: [
         {
@@ -25,6 +26,10 @@ describe('CaseListComponent', () => {
           provide: AppConfig,
           useClass: mockService
         },
+        {
+          provide: DefinitionsService,
+          useClass: mockService
+        },
         provideMockStore(),
       ]
     });
@@ -33,8 +38,14 @@ describe('CaseListComponent', () => {
     component = fixture.componentInstance;
   }));
 
-  xit('should create', () => {
-    // expect(component).not.toBeUndefined();
+  it('should return the toggle button name as \'Hide Filter\' if we have shown ' +
+    'the filter', () => {
+    expect(component.getToggleButtonName(true)).toEqual('Hide Filter');
+  });
+
+  it('should return the toggle button name as \'Show Filter\' if we do not show ' +
+    'the filter', () => {
+    expect(component.getToggleButtonName(false)).toEqual('Show Filter');
   });
 });
 

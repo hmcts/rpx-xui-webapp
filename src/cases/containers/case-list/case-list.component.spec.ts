cfb1dac9837a95a526ded0dad2f218fd9ea951aa
Philip <philip.aberneithy@gmail.com>
2019-10-07 16:26:33 +0100

    Spying on pagination metadata action.

--- src/cases/containers/case-list/case-list.component.spec.ts
+++ src/cases/containers/case-list/case-list.component.spec.ts
@@ -6,13 +6,20 @@ import {DefinitionsService} from '@hmcts/ccd-case-ui-toolkit/dist/shared/service
 import {Store} from '@ngrx/store';
 import {AppConfigService} from '../../../app/services/config/configuration.services';
 import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {CaseFilterToggle, FindCaselistPaginationMetadata} from '../../store/actions/case-list.action';
 import {provideMockStore, MockStore} from '@ngrx/store/testing';
 
 describe('CaseListComponent', () => {
   let component: CaseListComponent;
   let fixture: ComponentFixture<CaseListComponent>;
   let store: MockStore<State>;
+
+  /**
+   * Spies
+   */
   const mockService = jasmine.createSpy();
+  let spyOnDispatchToStore = jasmine.createSpy();
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [CaseListComponent],
@@ -34,6 +41,8 @@ describe('CaseListComponent', () => {
       ]
     });
     store = TestBed.get(Store);
+    spyOnDispatchToStore = spyOn(store, 'dispatch').and.callThrough();
+
     fixture = TestBed.createComponent(CaseListComponent);
     component = fixture.componentInstance;
   }));
@@ -46,6 +55,21 @@ describe('CaseListComponent', () => {
   it('should return the toggle button name as \'Show Filter\' if we do not show ' +
     'the filter', () => {
     expect(component.getToggleButtonName(false)).toEqual('Show Filter');
+  });
+
+  // TODO: event should show the shape of event object.
+  it('should dispatch an action to find the case list pagination metadata.', () => {
+    const event = {
+      test: 'test',
+    };
+    component.findCaseListPaginationMetadata(event);
+    expect(spyOnDispatchToStore).toHaveBeenCalledWith(new FindCaselistPaginationMetadata(event));
+  });
+
+  // TODO: We should always give the payload a proper name, not just payload.
+  it('should dispatch an action on toggle of the filter to show and hide the filter.', () => {
+    component.toggleFilter();
+    expect(spyOnDispatchToStore).toHaveBeenCalledWith(new CaseFilterToggle(true));
   });
 });
 

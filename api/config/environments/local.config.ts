e4deb08872eb6501d1282bd9a02b3a1235db3d0e
VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>
2019-10-09 14:20:59 +0100

    perf_test_Changes

--- api/config/environments/local.config.ts
+++ api/config/environments/local.config.ts
@@ -1,8 +1,8 @@
 export default {
     health: {
-        ccdComponentApi: 'https://ccd-api-gateway-web-aat.service.core-compute-aat.internal/health',
-        ccdDataApi: 'https://ccd-data-store-api-aat.service.core-compute-aat.internal/health',
-        documentsApi: 'https://dm-store-aat.service.core-compute-aat.internal/health',
+        ccdComponentApi: 'https://ccd-api-gateway-web-perftest.service.core-compute-perftest.internal/health',
+        ccdDataApi: 'https://ccd-data-store-api-perftest.service.core-compute-perftest.internal/health',
+        documentsApi: 'https://dm-store-perftest.service.core-compute-perftest.internal/health',
     },
     logging: 'debug',
     protocol: 'http',
@@ -13,20 +13,20 @@ export default {
     secureCookie: false,
     services: {
         ccd: {
-            componentApi: 'https://ccd-api-gateway-web-aat.service.core-compute-aat.internal',
-            dataApi: 'https://ccd-data-store-api-aat.service.core-compute-aat.internal',
+            componentApi: 'https://ccd-api-gateway-web-perftest.service.core-compute-perftest.internal',
+            dataApi: 'https://ccd-data-store-api-perftest.service.core-compute-perftest.internal',
         },
         documents: {
-            api: 'https://dm-store-aat.service.core-compute-aat.internal',
+            api: 'https://dm-store-perftest.service.core-compute-perftest.internal',
         },
         idam: {
-            idamApiUrl: 'https://idam-api.aat.platform.hmcts.net',
+            idamApiUrl: 'https://idam-api.perftest.platform.hmcts.net',
             idamClientID: 'xuiwebapp',
-            idamLoginUrl: 'https://idam-web-public.aat.platform.hmcts.net',
+            idamLoginUrl: 'https://idam-web-public.perftest.platform.hmcts.net',
             indexUrl: '/',
             oauthCallbackUrl: 'oauth2/callback',
         },
-        s2s: 'http://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
+        s2s: 'http://rpe-service-auth-provider-perftest.service.core-compute-perftest.internal',
     },
     sessionSecret: 'secretSauce',
 }

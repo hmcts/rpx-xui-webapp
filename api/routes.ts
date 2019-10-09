735954dcd5a61d13697ced849ee5e2f1a44f2247
Pardeep Singh Basi <paddybasi@gmail.com>
2019-10-09 14:33:18 +0100

    fixed document/print authentication

--- api/routes.ts
+++ api/routes.ts
@@ -1,13 +1,8 @@
 import * as express from 'express'
 import * as auth from './auth'
-import authInterceptor from './lib/middleware/auth'
-import serviceTokenMiddleware from './lib/middleware/serviceToken'
 import * as proxy from './lib/proxy'
 
 const router = express.Router({ mergeParams: true })
-
-router.use(serviceTokenMiddleware)
-router.use(authInterceptor)
 
 router.get('/*', proxy.get)
 router.post('/*', proxy.post)

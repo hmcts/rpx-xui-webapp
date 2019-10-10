dd26ddf25af81f63ae593b12bac94c1c4ef96679
Elviani <elviani@users.noreply.github.com>
2019-10-10 14:15:14 +0100

    Feature/eui 889 case home unit test & alert component unit test fix (#193)
    * work in progress
    
    * fixing the alert component unit test
    
    * update description

--- src/cases/components/case-alert/alert.component.spec.ts
+++ src/cases/components/case-alert/alert.component.spec.ts
@@ -1,5 +1,5 @@
 import { AlertComponent } from './alert.component';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 import { AlertComponent as CCDAlertComponent } from '@hmcts/ccd-case-ui-toolkit/dist/components/banners/alert/alert.component';
 import {AlertIconClassPipe} from '@hmcts/ccd-case-ui-toolkit/dist/components/banners/alert/alert-icon-class.pipe';
 import { AlertService, Alert } from '@hmcts/ccd-case-ui-toolkit';
@@ -8,12 +8,6 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs/internal/observable/of';
 import {ConnectableObservable} from 'rxjs';
 import { publish } from 'rxjs/internal/operators/publish';
-
-export class AlertServiceMock {
-  alerts = of({message: 'message', level: 'success'}).pipe(
-    publish()
-  ) as ConnectableObservable<Alert>;
-}
 
 describe('AlertComponent', () => {
   let component: AlertComponent;
@@ -26,7 +20,9 @@ describe('AlertComponent', () => {
         ])
       ],
       declarations: [ AlertComponent, CCDAlertComponent, AlertIconClassPipe ],
-      providers: [{ provide: AlertService, useClass: AlertServiceMock }]
+      providers: [
+        AlertService
+      ]
     })
     .compileComponents();
   }));
@@ -39,6 +35,16 @@ describe('AlertComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have updated the value of message and level in ngOnInit', async () => {
+    const alertService = fixture.debugElement.injector.get(AlertService);
+    alertService.push({message: 'message', level: 'success'});
+    component.ngOnInit();
+    await fixture.whenStable();
+    fixture.detectChanges();
+    expect(component.message).toEqual('message');
+    expect(component.level).toEqual('success');
   });
 
   it('should unsubscribe onDestroy', () => {

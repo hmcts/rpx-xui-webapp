388808849c4a82bde6596919a9b58887abe37a0f
Elviani <elviani@users.noreply.github.com>
2019-10-10 11:55:53 +0100

    adding unit test, also moving constructors into ngOnInit to enable unit test on those codes (#201)

--- src/app/shared/services/exui-title.service.spec.ts
+++ src/app/shared/services/exui-title.service.spec.ts
@@ -1,61 +1,61 @@
-import { Router, ActivatedRoute, Routes } from '@angular/router';
-import { TestBed, inject, fakeAsync, getTestBed } from '@angular/core/testing';
-import { ExUITitleService } from './exui-title.service';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import { ExUITitleService} from './exui-title.service';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { Component, NgModule } from '@angular/core';
-
-@Component({ template: '<router-outlet></router-outlet>' })
-class TestBootstrapComponent {
-  constructor(private titleService: ExUITitleService) {}
-}
-
-@Component({ template: '' })
-class TestComponent {}
-
-@NgModule({
-  declarations: [TestComponent, TestBootstrapComponent],
-  imports: [RouterTestingModule],
-  providers: [
-    ExUITitleService
-  ]
-})
-class TestSharedModule { }
-
-const testRoutes: Routes = [
-  {
-    path: 'home',
-    component: TestComponent,
-    data: {
-      title: 'test title'
-    }
-  }
-];
+import { of, BehaviorSubject } from 'rxjs';
 
 describe('ExUITitleService', () => {
   beforeEach(() => {
-    TestBed.resetTestEnvironment();
-    TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting())
-        .configureTestingModule({
-          imports: [
-            TestSharedModule,
-            RouterTestingModule.withRoutes(testRoutes),
-          ]
-        });
+    TestBed.configureTestingModule({
+      imports: [ RouterTestingModule ],
+      providers: [
+        ExUITitleService,
+        {
+          provide: Router,
+          useValue: {
+            events: new BehaviorSubject<Event>(null)
+          }
+        },
+        {
+          provide: ActivatedRoute,
+          useValue:
+          {
+            firstChild: {
+              snapshot: {
+                data: { title: 'First Child 1'}
+              },
+              firstChild: {
+                snapshot: {
+                  data: { title: 'First Child 2'},
+                }
+              }
+            }
+          }
+        }
+      ]
+    });
   });
 
-  it('should be able to return title from observable',
-      () => {
-          const injector = getTestBed();
-          const router = injector.get(Router);
-          const titleService = injector.get(ExUITitleService);
-          const fixture = TestBed.createComponent(TestBootstrapComponent);
-          fixture.detectChanges();
-          const component = fixture.componentRef.instance;
-          titleService.title$.subscribe( title  => {
-            expect(title).toEqual('test title');
-          });
-          // initial navigation
-          router.navigate(['home']);
-        });
+  it('should be created', inject([ExUITitleService], (service: ExUITitleService) => {
+    expect(service).toBeTruthy();
+  }));
+
+  it('ngOnInit should update the child title', inject([ExUITitleService], (service: ExUITitleService) => {
+    const event = new NavigationEnd(42, '/', '/');
+    TestBed.get(Router).events.next(event);
+    service.ngOnInit();
+    service.title$.subscribe(title => {
+      expect(title).toBe('First Child 2');
+    });
+  }));
+
+  it('should unsubscribe onDestroy', inject([ExUITitleService], (service: ExUITitleService) => {
+    const event = new NavigationEnd(42, '/', '/');
+    TestBed.get(Router).events.next(event);
+    service.ngOnInit();
+    spyOn(service.titleSubscription, 'unsubscribe').and.callThrough();
+    service.ngOnDestroy();
+    expect(service.titleSubscription.unsubscribe).toHaveBeenCalled();
+  }));
+
 });

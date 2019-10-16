acf2579ba4455d0f4cd8b344b95efc8731a63d17
Pardeep Singh Basi <paddybasi@gmail.com>
2019-10-16 13:54:55 +0100

    refactor

--- api/amendedJurisdictions/index.ts
+++ api/amendedJurisdictions/index.ts
@@ -2,21 +2,7 @@ import * as express from 'express'
 import * as striptags from 'striptags'
 import { config } from '../config'
 import { http } from '../lib/http'
-import { EnhancedRequest } from '../lib/models'
-
-export function setHeaders(req: EnhancedRequest) {
-    const headers: any = {}
-
-    headers['content-type'] = req.headers['content-type']
-    if (req.headers.accept) {
-        headers.accept = req.headers.accept
-    }
-    if (req.headers.experimental) {
-        headers.experimental = req.headers.experimental
-    }
-
-    return headers
-}
+import { setHeaders } from '../lib/proxy'
 
 export async function getJurisdictions(req: express.Request, res: express.Response) {
     let url = striptags(req.url)

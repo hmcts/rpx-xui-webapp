a5cca67c84aa80c1825583bd35a84e64a38ac049
VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>
2019-10-25 13:26:59 +0100

    demo config changed to demo

--- api/config/environments/demo.config.ts
+++ api/config/environments/demo.config.ts
@@ -3,24 +3,24 @@ export default {
     secureCookie: false,
     services: {
         ccd: {
-            componentApi: 'https://ccd-api-gateway-web-aat.service.core-compute-aat.internal',
-            dataApi: 'https://ccd-data-store-api-aat.service.core-compute-aat.internal',
+            componentApi: 'http://ccd-api-gateway-web-demo.service.core-compute-demo.internal',
+            dataApi: 'http://ccd-data-store-api-demo.service.core-compute-demo.internal',
         },
         coh: {
-            corApi: 'http://coh-cor-aat.service.core-compute-aat.internal',
+            corApi: 'http://coh-cor-demo.service.core-compute-demo.internal',
         },
         documents: {
-            api: 'https://dm-store-aat.service.core-compute-aat.internal',
+            api: 'http://dm-store-demo.service.core-compute-demo.internal',
         },
         idam: {
-            idamApiUrl: 'https://idam-api.aat.platform.hmcts.net',
+            idamApiUrl: 'https://idam-api.demo.platform.hmcts.net',
             idamClientID: 'xuiwebapp',
-            idamLoginUrl: 'https://idam-web-public.aat.platform.hmcts.net',
+            idamLoginUrl: 'https://idam-web-public.demo.platform.hmcts.net',
             indexUrl: '/',
             oauthCallbackUrl: 'oauth2/callback',
         },
         s2s:
-            'https://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
+            'http://rpe-service-auth-provider-demo.service.core-compute-demo.internal',
     },
     sessionSecret: 'secretSauce',
     useProxy: false,

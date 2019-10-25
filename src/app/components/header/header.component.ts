ce7b6b5c595900f76ea731f00176de18979f557f
Elviani Huseyin <elviani_s@hotmail.com>
2019-10-25 13:53:30 +0100

    adding my HMCTS header for pui-case-manager role user

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,19 +4,32 @@ import { Store } from '@ngrx/store';
 import * as fromRoot from '../../store';
 import {AppTitleModel} from '../../models/app-title.model';
 import {UserNavModel} from '../../models/user-nav.model';
+import { CookieService } from 'ngx-cookie';
 
 @Component({
   selector: 'exui-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   @Input() navItems: { active: boolean; href: string; }[];
   @Input() title: AppTitleModel;
   @Input() userNav: UserNavModel;
   @Output() navigate = new EventEmitter<string>();
 
-  constructor(public store: Store<fromRoot.State>) {}
+  public isCaseManager = false;
+
+  constructor(
+    public store: Store<fromRoot.State>,
+    private cookieService: CookieService
+  ) {}
+
+  ngOnInit() {
+     const userRoles = this.cookieService.get('roles');
+     if (userRoles.indexOf('pui-case-manager') !== -1) {
+        this.isCaseManager = true;
+     }
+  }
 
   onNavigate(event) {
     this.navigate.emit(event);

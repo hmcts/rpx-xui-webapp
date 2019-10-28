cad9bac3fb8fa9e287367fe740ab4265e56bbbe0
Philip <philip.aberneithy@gmail.com>
2019-10-28 12:21:14 +0000

    Abstracting Header from App component.

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -1,11 +1,4 @@
-import { Component, ViewEncapsulation, OnInit } from '@angular/core';
-import { LoggerService } from '../../services/logger/logger.service';
-import * as fromActions from '../../store';
-import { Store } from '@ngrx/store';
-import {NavItemsModel} from '../../models/nav-item.model';
-import {AppTitleModel} from '../../models/app-title.model';
-import {UserNavModel} from '../../models/user-nav.model';
-import {AppConstants} from '../../app.constants';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'exui-root',
@@ -13,27 +6,5 @@ import {AppConstants} from '../../app.constants';
   styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent implements OnInit {
-  navItems: NavItemsModel[];
-  appHeaderTitle: AppTitleModel;
-  userNav: UserNavModel;
-  componentName = 'App Component';
-
-  constructor(
-    private logger: LoggerService,
-    private store: Store<fromActions.State> ) {
-  }
-
-  ngOnInit(): void {
-    this.appHeaderTitle = AppConstants.APP_HEADER_TITLE;
-    this.navItems = AppConstants.NAV_ITEMS;
-    this.userNav = AppConstants.USER_NAV;
-  }
-
-  onNavigate(event): void {
-    if (event === 'sign-out') {
-      return this.store.dispatch(new fromActions.Logout());
-    }
-  }
-
+export class AppComponent {
 }
--- src/app/containers/index.ts
+++ src/app/containers/index.ts
@@ -1,12 +1,15 @@
 import {ExuiCcdConnectorComponent} from './exiu-ccd-connector-wrapper/exui-ccd-connector.component';
 import {FooterComponent} from './footer/footer.component';
+import {HeaderComponent} from './header/header.component';
 
 export const containers: any[] = [
   ExuiCcdConnectorComponent,
-  FooterComponent
+  FooterComponent,
+  HeaderComponent
 ];
 
 export * from './exiu-ccd-connector-wrapper/exui-ccd-connector.component';
 export * from './footer/footer.component';
+export * from './header/header.component';
 
 

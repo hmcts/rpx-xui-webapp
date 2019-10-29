2e2f078c980b954a85cce607da617ed9c6426dfe
Elviani Huseyin <elviani_s@hotmail.com>
2019-10-29 11:45:02 +0000

    adding google anayltics service

---
+++ src/app/shared/services/google-analytics.service.ts
@@ -0,0 +1,12 @@
+import { Injectable, OnInit } from '@angular/core';
+
+@Injectable()
+export class GoogleAnalyticsService implements OnInit {
+
+    constructor(
+    ) { }
+
+    ngOnInit() {
+    }
+
+}

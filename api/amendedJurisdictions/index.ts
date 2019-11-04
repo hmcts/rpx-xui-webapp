5b2a2210fc1dfbb5764d4b51b5683a99cd006fbf
fazanki <fazanki@gmail.com>
2019-11-04 14:01:46 +0000

    adding filetes for returned jurisdiction

--- api/amendedJurisdictions/index.ts
+++ api/amendedJurisdictions/index.ts
@@ -4,25 +4,26 @@ import { config } from '../config'
 import { http } from '../lib/http'
 import { setHeaders } from '../lib/proxy'
 
+/**
+ * Manually filtering returned jurisdictions
+ * to make available jurisdiction in filters array only
+ */
 export async function getJurisdictions(req: express.Request, res: express.Response) {
+    console.log(req.url)
     let url = striptags(req.url)
+    console.log(url)
     url = req.baseUrl  + url
     const headers: any = setHeaders(req)
 
     try {
         const response = await http.get(`${config.services.ccd.componentApi}${url}`, { headers })
-
+        const filters = {jurisdiction: ['DIVORCE', 'PROBATE']}
         let amendedJurisdictions = []
-        if (config.environment === 'prod') {
-            response.data.forEach(element => {
-                if (element && element.id === 'PROBATE') {
-                    amendedJurisdictions.push(element)
-                }
-            })
+        if (config.environment === 'prod' || config.environment === 'aat') {
+            amendedJurisdictions = [...response.data].filter(o => filters.jurisdiction.includes(o.id))
         } else {
             amendedJurisdictions = response.data
         }
-
         res.status(200)
         res.send(amendedJurisdictions)
     } catch (e) {

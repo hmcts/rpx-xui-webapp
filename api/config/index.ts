2eb30977baa79ce3a0870dafea1619ae5e487c03
Franco Zanki <fazanki@users.noreply.github.com>
2019-11-05 15:00:30 +0000

    adding adding filters for returned jurisdiction (#265)
    * adding filetes for returned jurisdiction
    
    * removing condition for prod and aat
    
    * fixing test
    
    * adding back yarn.lock
    
    * adding back yarn.lock
    
    * updating yarn.lock
    
    * adding extra condition for local demo and cmc
    
    * fxing unit test
    
    * increasing delay for e2e tests
    
    * adding demo config and chaning condition logic
    
    * removing console logs

--- api/config/index.ts
+++ api/config/index.ts
@@ -1,6 +1,7 @@
 import * as process from 'process'
 import { application } from './application.config'
 import * as aat from './environments/aat.config'
+import * as demo from './environments/demo.config'
 import * as ithc from './environments/ithc.config'
 import * as local from './environments/local.config'
 import * as perftest from './environments/perftest.config'
@@ -9,6 +10,7 @@ import * as prod from './environments/prod.config'
 
 export const configs = {
     aat,
+    demo,
     ithc,
     local,
     perftest,

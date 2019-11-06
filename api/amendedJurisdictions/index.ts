30157d78ab997dcad6b3ff6482374dbf73ae1473
fazanki <fazanki@gmail.com>
2019-11-06 13:38:25 +0000

    adding extra ia jurisdiction

--- api/amendedJurisdictions/index.ts
+++ api/amendedJurisdictions/index.ts
@@ -23,7 +23,7 @@ export async function getJurisdictions(req: express.Request, res: express.Respon
         // export JURISDICTIONS=DIVORCE,PROBATE,CMC && npm run start:node
         // const filtersString = process.env.JURISDICTIONS.split(',')
         // const filters = {jurisdiction: filtersString}
-        const filters = {jurisdiction: config.environment === 'demo' ? ['DIVORCE', 'PROBATE', 'CMC'] : ['DIVORCE', 'PROBATE']}
+        const filters = {jurisdiction: config.environment === 'demo' ? ['DIVORCE', 'PROBATE', 'CMC', 'IA'] : ['DIVORCE', 'PROBATE']}
         let amendedJurisdictions = []
         if (config.environment === 'local') {
           amendedJurisdictions = response.data

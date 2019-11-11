facba7e2dd0c665a7bbc9ddd830f08af13659f96
Uday Denduluri <uday.denduluri@gmail.com>
2019-11-11 17:06:06 +0000

    After user accepts we raise the take the user back to cases

--- src/app/accept-tc.wrapper/accept-tc-wrapper.component.ts
+++ src/app/accept-tc.wrapper/accept-tc-wrapper.component.ts
@@ -4,6 +4,7 @@ import * as fromStore from '../store';
 import * as fromApp from '../../../src/app/store/index';
 import { CookieService } from 'ngx-cookie';
 import { Observable, of } from 'rxjs';
+import { ofType, Actions } from '@ngrx/effects';
 /**
  * Terms And Condition smart component wrapper
  * absorbs Terms and Condition dumb component
@@ -14,11 +15,16 @@ import { Observable, of } from 'rxjs';
 })
 export class AcceptTcWrapperComponent implements OnInit {
   uId: Observable<string>;
-  constructor(private store: Store<fromApp.State>, private cookieService: CookieService) {
+  constructor(private store: Store<fromApp.State>,
+              private cookieService: CookieService,
+              private actions$: Actions) {
   }
 
   ngOnInit(): void {
     this.uId = of(this.cookieService.get('__userid__'));
+    this.actions$.pipe(ofType(fromApp.ACCEPT_T_AND_C_SUCCESS)).subscribe(() => {
+      this.store.dispatch(new fromStore.Go({ path: ['cases'] }));
+    });
   }
 
   onAcceptTandC() {

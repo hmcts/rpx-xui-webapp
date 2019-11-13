50d4e1d6a6ad636cfafeb4b1ef9dbf026a0eff21
fazanki <fazanki@gmail.com>
2019-11-13 10:33:12 +0000

    adding public law

--- api/amendedJurisdictions/index.ts
+++ api/amendedJurisdictions/index.ts
@@ -24,7 +24,7 @@ export async function getJurisdictions(req: express.Request, res: express.Respon
         // const filtersString = process.env.JURISDICTIONS.split(',')
         // const filters = {jurisdiction: filtersString}
         const filters = {jurisdiction: config.environment === 'demo' ?
-            ['DIVORCE', 'PROBATE', 'CMC', 'IA', 'FR'] : ['DIVORCE', 'PROBATE', 'FR']}
+            ['DIVORCE', 'PROBATE', 'CMC', 'IA', 'FR', 'PUBLICLAW'] : ['DIVORCE', 'PROBATE', 'FR']}
         let amendedJurisdictions = []
         if (config.environment === 'local') {
           amendedJurisdictions = response.data

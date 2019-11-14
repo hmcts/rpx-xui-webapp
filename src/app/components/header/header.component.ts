e881d83dc7cab44534292193237326360c51cf14
Uday Denduluri <uday.denduluri@gmail.com>
2019-11-14 11:47:08 +0000

    Adding tests for the header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,18 +1,18 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, OnDestroy} from '@angular/core';
 import { Store, select } from '@ngrx/store';
 
 import * as fromRoot from '../../store';
 import {AppTitleModel} from '../../models/app-title.model';
 import {UserNavModel} from '../../models/user-nav.model';
 import { CookieService } from 'ngx-cookie';
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subscription } from 'rxjs';
 import { AppUtils } from 'src/app/app-utils';
 
 @Component({
   selector: 'exui-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
   @Input() navItems: { active: boolean; href: string; }[];
   @Input() title: AppTitleModel;
@@ -21,6 +21,7 @@ export class HeaderComponent implements OnInit {
 
   public isCaseManager = false;
   showNavItems: Observable<boolean>;
+  subscription: Subscription;
 
   constructor(
     public store: Store<fromRoot.State>,
@@ -29,15 +30,40 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
      const userRoles = this.cookieService.get('roles');
-     if (userRoles && userRoles.indexOf('pui-case-manager') !== -1) {
-        this.isCaseManager = true;
-     }
-     this.store.pipe(select(fromRoot.getRouterUrl)).subscribe(url => {
+     this.isCaseManager = this.getIsCaseManager(userRoles);
+     const observable = this.getObservable(this.store);
+     this.subscription = this.subscribe(observable);
+  }
+
+  subscribe(observable: Observable<string>): Subscription {
+    return  observable.subscribe(url => {
       this.showNavItems = of(AppUtils.showNavItems(url));
-     });
+    });
+  }
+
+  getObservable(store: Store<fromRoot.State>): Observable<string> {
+    return store.pipe(select(fromRoot.getRouterUrl));
+  }
+
+  getIsCaseManager(userRoles: string): boolean {
+    return userRoles && userRoles.indexOf('pui-case-manager') !== -1;
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe(this.subscription);
+  }
+
+  unsubscribe(subscription: Subscription) {
+    if (subscription) {
+      subscription.unsubscribe();
+    }
   }
 
   onNavigate(event) {
-    this.navigate.emit(event);
+    this.emitNavigate(event, this.navigate);
+  }
+
+  emitNavigate(event: any, emitter: EventEmitter<string>) {
+    emitter.emit(event);
   }
 }

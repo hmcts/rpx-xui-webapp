bae0aad24a7862a78f791f67798d1c5a97dd89d6
Ronald Mansveld <ronald.mansveld@atos.net>
2019-11-15 11:26:41 +0000

    Fetch TC from API

--- src/app/services/terms-and-conditions/terms-and-conditions.service.ts
+++ src/app/services/terms-and-conditions/terms-and-conditions.service.ts
@@ -1,19 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TCDocument } from '@hmcts/rpx-xui-common-lib';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class TermsConditionsService {
+    constructor(private http: HttpClient) {}
+
     public getTermsConditions(): Observable<TCDocument> {
-        return of({
-            version: 1,
-            mimeType: 'text/html',
-            content: `
-                <h1 class="govuk-heading-xl">Test Terms</h1>
-                <p class="govuk-body">These are mock terms returned by the service until we can integrate the backend.</p>
-            `
-        });
+        return this.http.get<TCDocument>(`api/termsAndConditions`);
     }
 }

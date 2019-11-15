3fc58e7fa053081df67654e51176792e4700c155
Uday Denduluri <uday.denduluri@gmail.com>
2019-11-15 13:59:20 +0000

    Moving canActivate to common guardUtil

--- src/app/guards/acceptTerms.guard.spec.ts
+++ src/app/guards/acceptTerms.guard.spec.ts
@@ -3,14 +3,12 @@ import { AcceptTermsGuard } from './acceptTerms.guard';
 describe('Accept terms guard', () => {
     let guard: AcceptTermsGuard;
     let mockStore: any;
-    let mockService: any;
     let mockGuardUtil: any;
 
     beforeEach(() => {
         mockStore = jasmine.createSpyObj('mockStore', ['unsubscribe', 'dispatch', 'pipe']);
-        mockService = jasmine.createSpyObj('mockService', ['get']);
         mockGuardUtil = jasmine.createSpyObj('guardUtil', ['checkStore', 'handleTC']);
-        guard = new AcceptTermsGuard(mockStore, mockService, mockGuardUtil);
+        guard = new AcceptTermsGuard(mockStore, mockGuardUtil);
     });
 
     it('is Truthy', () => {

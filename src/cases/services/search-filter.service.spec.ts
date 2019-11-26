55b6bf8d6d08e5eeb4b9d60e71c7d74e90d1d020
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-11-26 13:51:28 +0000

    EUI-1174 fix for duplication, refactoring of tests (#300)

--- src/cases/services/search-filter.service.spec.ts
+++ src/cases/services/search-filter.service.spec.ts
@@ -1,10 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import createSpyObj = jasmine.createSpyObj;
 import { SearchFilterService } from './';
-import { SearchService, Jurisdiction, CaseType, CaseState, AbstractAppConfig,
-         HttpService, RequestOptionsBuilder } from '@hmcts/ccd-case-ui-toolkit';
+import {
+    SearchService, Jurisdiction, CaseType, CaseState, AbstractAppConfig,
+    HttpService, RequestOptionsBuilder
+} from '@hmcts/ccd-case-ui-toolkit';
 import { FormControl, FormGroup } from '@angular/forms';
 import { JURISDICTION_1, CASE_TYPE_1, CASE_STATE_1 } from '../mock/search-filter.mock';
+import { StoreModule, combineReducers, Store } from '@ngrx/store';
+import * as fromRoot from '../../app/store/reducers';
+import * as fromCaseSearchStore from '../store';
 
 const JURISDICTION: Jurisdiction = JURISDICTION_1;
 
@@ -20,20 +25,30 @@ describe('SearchFilterService', () => {
     const abstractAppConfigMock = createSpyObj<AbstractAppConfig>('AbstractAppConfig', ['getCaseDataUrl']);
     const httpService = createSpyObj<HttpService>('HttpService', ['get']);
     const requestOptionsBuilder = createSpyObj<RequestOptionsBuilder>('RequestOptionsBuilder', ['buildOptions']);
+    let storeDispatchMock: any;
+    let store: Store<fromCaseSearchStore.SearchState>;
 
     beforeEach(() => {
 
         TestBed.configureTestingModule({
+            imports: [
+                StoreModule.forRoot({
+                    ...fromRoot.reducers,
+                    feature: combineReducers(fromCaseSearchStore.reducers),
+                }),
+            ],
             providers: [
                 SearchFilterService,
                 { provide: SearchService, useValue: ccdSearchServiceMock },
                 { provide: AbstractAppConfig, useValue: abstractAppConfigMock },
                 { provide: HttpService, useValue: httpService },
-                { provide: RequestOptionsBuilder, useValue: requestOptionsBuilder}
+                { provide: RequestOptionsBuilder, useValue: requestOptionsBuilder }
             ]
         });
 
+        store = TestBed.get(Store);
         searchFilterService = TestBed.get(SearchFilterService);
+        storeDispatchMock = spyOn(store, 'dispatch');
     });
 
     it('should make inputs fields turn into query parameters', () => {
@@ -127,5 +142,50 @@ describe('SearchFilterService', () => {
         }, 'SEARCH');
 
     });
+
+    describe('redirectToCase', () => {
+        it('should dispatch an action', () => {
+
+            searchFilterService.redirectToCase({});
+
+
+            expect(storeDispatchMock).toHaveBeenCalled();
+
+        });
+    });
+
+
+    describe('findPaginationMetadata', () => {
+        it('should httpService get call', () => {
+
+            const nameControl = new FormControl();
+            const NAME_VALUE = 'something';
+
+            nameControl.setValue(NAME_VALUE);
+
+            const filterContents = {
+                name: nameControl
+            };
+            const formGroupDummy = new FormGroup(filterContents);
+            const filter = {
+                selected: {
+                    formGroup: formGroupDummy,
+                    jurisdiction: JURISDICTION,
+                    caseType: CASE_TYPES[0],
+                    caseState: CASE_STATE,
+                    page: 1,
+                    view: 'SEARCH'
+                }
+            };
+
+            searchFilterService.findPaginationMetadata(filter);
+
+
+
+            expect(httpService.get).toHaveBeenCalled();
+
+        });
+    });
+
 
 });

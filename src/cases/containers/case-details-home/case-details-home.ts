acf0463900c899af48b9347044aef0ededcd8530
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-11-26 15:45:15 +0000

    EUI-876 reduction

--- src/cases/containers/case-details-home/case-details-home.ts
+++ src/cases/containers/case-details-home/case-details-home.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'exui-case-details-home',
-  templateUrl: './case-details-home.html',
-
+  templateUrl: './case-details-home.html'
 })
 export class CaseDetailsHomeComponent {
   constructor() {}

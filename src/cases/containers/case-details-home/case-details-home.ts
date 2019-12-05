f6e33e380cadd55b2a88452baae80b8c2005e343
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-12-05 14:51:51 +0000

    EUI-876 CSS hacks

--- src/cases/containers/case-details-home/case-details-home.ts
+++ src/cases/containers/case-details-home/case-details-home.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'exui-case-details-home',
-  templateUrl: './case-details-home.html'
+  templateUrl: './case-details-home.html',
+  styleUrls: ['case-details-home.scss']
 })
 export class CaseDetailsHomeComponent {
   constructor() {}

adea060053efd101ca8b710a1e3a2e2d0fe6ae18
Elviani Huseyin <elviani_s@hotmail.com>
2019-12-10 14:56:45 +0000

    remove the current probate contact from footer add get help page for footer

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,3 +1,5 @@
+import { BADGE_COLOUR, GetHelpDetailsDataModel } from './components/get-help-details/get-help-details.model';
+
 const FooterData =  {
   heading: 'Help',
   email: {
@@ -23,7 +25,8 @@ const FooterDataNavigation = {
     { text: 'Accessibility', href: '/accessibility', target: '_blank' },
     { text: 'Terms and conditions', href: '/terms-and-conditions', target: '_blank' },
     { text: 'Cookies', href: '/cookies', target: '_blank' },
-    { text: 'Privacy policy', href: '/privacy-policy', target: '_blank' }
+    { text: 'Privacy policy', href: '/privacy-policy', target: '_blank' },
+    { text: 'Get Help', href: '/get-help', target: '_blank' }
   ]
 };
 
@@ -44,6 +47,33 @@ const userNav = {
     emit: 'sign-out'
   }]
 };
+
+const getHelpDetailsData: GetHelpDetailsDataModel[] = [
+  {
+    title: 'Probate',
+    badgeColour: BADGE_COLOUR.BADGE_RED,
+    badgeText: 'PRIVATE BETA',
+    email: 'contactprobate@justice.gov.uk',
+    phone: '0300 303 0648',
+    openingTimes: 'Monday to Friday, 9.30pm to 5pm (excluding public holidays)'
+  },
+  {
+    title: 'Divorce',
+    badgeColour: BADGE_COLOUR.BADGE_BLUE,
+    badgeText: 'PUBLIC BETA',
+    email: 'divorcecase@justice.gov.uk',
+    phone: '0300 303 0642',
+    openingTimes: 'Monday to Friday, 9.30pm to 5pm (excluding public holidays)'
+  },
+  {
+    title: 'Financial Remedy',
+    badgeColour: BADGE_COLOUR.BADGE_RED,
+    badgeText: 'PRIVATE BETA',
+    email: 'contactfinancialremedy@justice.gov.uk',
+    phone: '0300 303 0642',
+    openingTimes: 'Monday to Friday, 9.30pm to 5pm (excluding public holidays)'
+  }
+];
 
 const environmentNames = {
   aat: 'aat',
@@ -70,11 +100,12 @@ const appHeaderTitle = {name: 'Manage Cases', url: '/'};
 
 
 export class AppConstants {
-  static FOOTER_DATA = FooterData;
+  static FOOTER_DATA = null;
   static FOOTER_DATA_NAVIGATION = FooterDataNavigation;
   static NAV_ITEMS = navItems;
   static USER_NAV = userNav;
   static APP_HEADER_TITLE = appHeaderTitle;
   static ENVIRONMENT_NAMES = environmentNames;
   static REDIRECT_URL = redirectUrl;
+  static GET_HELP_DETAILS_DATA = getHelpDetailsData;
 }

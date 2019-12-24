03ae82a7c32854bc48b37ade5f105d0c99f8fe17
Sreekanth <sreekanth.puligadda@hmcts.net>
2019-12-24 08:57:25 +0000

    Fixed search cases scenario.

--- test/e2e/config/conf.js
+++ test/e2e/config/conf.js
@@ -24,7 +24,7 @@ const localConfig = [
 
     browserName: 'chrome',
     acceptInsecureCerts: true,
-    chromeOptions: { args: ['--headless','--no-sandbox', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-zygote '] },
+    chromeOptions: { args: ['--no-sandbox', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-zygote '] },
     proxy: {
       proxyType: 'manual',
       httpProxy: 'proxyout.reform.hmcts.net:8080',

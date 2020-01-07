2d2cc0ea3eefd6998589461e33d8ad29be7c72db
Pardeep Singh Basi <paddybasi@gmail.com>
2020-01-07 11:48:44 +0000

    Initial commit to get openid flow working and to unblock other devs [WIP]

--- api/emAnno/routes.ts
+++ api/emAnno/routes.ts
@@ -1,10 +1,10 @@
 import * as express from 'express'
-import authInterceptor from '../lib/middleware/auth'
+// import authInterceptor from '../lib/middleware/auth'
 import {deleteAnnotations, getAnnotations, postAnnotations, putAnnotations} from './index'
 
 export const router = express.Router({ mergeParams: true })
 
-router.use(authInterceptor)
+// router.use(authInterceptor)
 router.get('/*', getAnnotations)
 router.post('/*', postAnnotations)
 router.put('/*', putAnnotations)

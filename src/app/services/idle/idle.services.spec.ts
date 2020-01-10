93cb6cb593e13b68bd98bac4d6e4d52dd1518ae2
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2020-01-10 15:36:17 +0000

    EUI-1120 increased coverage

--- src/app/services/idle/idle.services.spec.ts
+++ src/app/services/idle/idle.services.spec.ts
@@ -1,22 +1,36 @@
 import { inject, TestBed } from '@angular/core/testing';
-import {StoreModule} from '@ngrx/store';
+import {StoreModule, Store} from '@ngrx/store';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { IdleService } from './idle.services';
 import {Idle, IdleExpiry} from '@ng-idle/core';
 import {Keepalive} from '@ng-idle/keepalive';
-
-class MockConfigService {
-  config;
-  caseEditorConfig = {};
-  getEditorConfiguration() {}
-  constructor() {
-    this.config = {
-      login_url: 'test'
-    };
-  }
-}
+import { of } from 'rxjs';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import * as fromRoot from '../../store';
 
 describe('Idle Services', () => {
+
+  const mockIdle = jasmine.createSpyObj('Idle', [
+    'setIdleName',
+    'setTimeout',
+    'setInterrupts'
+  ]);
+
+  mockIdle.onIdleEnd = { pipe: (param) => of('something') };
+  mockIdle.onTimeout = of('something');
+  mockIdle.onIdleStart = of('something');
+  mockIdle.onTimeoutWarning = { pipe: (param) => of('something') };
+
+  let store: MockStore<fromRoot.State>;
+  let spyOnDispatchToStore = jasmine.createSpy();
+  let spyOnPipeToStore = jasmine.createSpy();
+
+  const mockKeepAlive = jasmine.createSpyObj('KeepInterval', [
+    'interval'
+  ]);
+
+  mockKeepAlive.onPing = { pipe: (param) => of('something') };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -24,32 +38,26 @@ describe('Idle Services', () => {
         HttpClientTestingModule
       ],
       providers: [
-        Idle,
-        IdleExpiry,
-        Keepalive,
+        { provide: Idle, useValue: mockIdle },
+        { provide: Keepalive, useValue: mockKeepAlive },
         IdleService,
+        provideMockStore(),
       ]
     });
+    
+    store = TestBed.get(Store);
+    spyOnDispatchToStore = spyOn(store, 'dispatch').and.callThrough();
+    spyOnPipeToStore = spyOn(store, 'pipe').and.returnValue(of('something'));
+
   });
 
-  it('should be created', inject([IdleService], (service: IdleService) => {
-    expect(service).toBeTruthy();
-  }));
 
-  it('should have init method', inject([IdleService], (service: IdleService) => {
-    expect(service.init).toBeTruthy();
-  }));
-
-  it('should have init dispatchModal', inject([IdleService], (service: IdleService) => {
-    expect(service.dispatchModal).toBeTruthy();
-  }));
-
-  it('should have init dispatchSignedOut', inject([IdleService], (service: IdleService) => {
-    expect(service.dispatchSignedOut).toBeTruthy();
-  }));
-
-  it('should have init initWatch', inject([IdleService], (service: IdleService) => {
-    expect(service.initWatch).toBeTruthy();
-  }));
+  describe('init', () => {
+    it('should setup values', inject([IdleService], (service: IdleService) => {
+      service.init();
+      expect(mockIdle.setIdleName).toHaveBeenCalledWith('idleSession');
+      expect(mockIdle.setTimeout).toHaveBeenCalled();
+    }));
+  });
 
 });

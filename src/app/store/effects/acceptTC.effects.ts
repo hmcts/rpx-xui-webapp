aec0900daaae18695633923e04d14b2d682509a7
Uday Denduluri <uday.denduluri@gmail.com>
2020-02-20 13:07:03 +0000

    T&C error handling

--- src/app/store/effects/acceptTC.effects.ts
+++ src/app/store/effects/acceptTC.effects.ts
@@ -1,40 +1,40 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import * as acceptTandCActions from '../actions';
-import { catchError, map, switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
 import { AcceptTermsService } from 'src/app/services/acceptTerms/acceptTerms.service';
-import { CookieService } from 'ngx-cookie';
+import * as fromRoot from '../../store';
+import * as acceptTandCActions from '../actions';
 
 @Injectable()
 export class AcceptTcEffects {
   constructor(
-    private actions$: Actions,
-    private acceptTcService: AcceptTermsService
+    private readonly actions$: Actions,
+    private readonly acceptTcService: AcceptTermsService
   ) { }
 
   @Effect()
-  loadHasAccepted$ = this.actions$.pipe(
+  public loadHasAccepted$ = this.actions$.pipe(
     ofType(acceptTandCActions.LOAD_HAS_ACCEPTED_TC),
     switchMap((action: any) => {
       return this.acceptTcService.getIsUserAccepted(action.payload).pipe(
         map(userId => {
             return new acceptTandCActions.LoadHasAcceptedTCSuccess(userId);
         }),
-        catchError(error => of(new acceptTandCActions.LoadHasAcceptedTCFail(error)))
+        catchError(() => of(new fromRoot.Go({ path: ['/service-down']})))
       );
     })
   );
 
   @Effect()
-  userHasAccepted$ = this.actions$.pipe(
+  public userHasAccepted$ = this.actions$.pipe(
     ofType(acceptTandCActions.ACCEPT_T_AND_C),
     switchMap((action: any) => {
       return this.acceptTcService.postUserAccepted(action.payload).pipe(
         map(accepted => {
             return new acceptTandCActions.AcceptTandCSuccess(accepted);
         }),
-        catchError(error => of(new acceptTandCActions.AcceptTandCFail(error)))
+        catchError(error => of(new fromRoot.Go({ path: ['/service-down'] })))
       );
     })
   );

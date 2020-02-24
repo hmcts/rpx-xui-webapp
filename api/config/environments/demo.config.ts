1cdde20f28016450d0d4f6306e98b0721b773b37
sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
2020-02-24 12:19:33 +0000

    Fix annotation url to point to demo.

--- api/config/environments/demo.config.ts
+++ api/config/environments/demo.config.ts
@@ -3,7 +3,7 @@ export default {
     ccdComponentApi: 'http://ccd-api-gateway-web-demo.service.core-compute-demo.internal/health',
     ccdDataApi: 'http://ccd-data-store-api-demo.service.core-compute-demo.internal/health',
     documentsApi: 'http://dm-store-demo.service.core-compute-demo.internal/health',
-    em_anno_api: 'http://em-anno-aat.service.core-compute-aat.internal/health',
+    em_anno_api: 'http://em-anno-demo.service.core-compute-demo.internal/health',
   },
   logging: 'debug',
   loginRoleMatcher: 'caseworker',
@@ -21,7 +21,7 @@ export default {
     documents: {
       api: 'http://dm-store-demo.service.core-compute-demo.internal',
     },
-    em_anno_api: 'http://em-anno-aat.service.core-compute-aat.internal',
+    em_anno_api: 'http://em-anno-demo.service.core-compute-demo.internal',
     idam: {
       idamApiUrl: 'https://idam-api.demo.platform.hmcts.net',
       idamClientID: 'xuiwebapp',

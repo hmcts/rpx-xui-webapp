6aed52e1337cc75857662e623665b443061780de
Uday Denduluri <uday.denduluri@gmail.com>
2020-03-08 14:09:42 +0000

    fix the failing tests

--- src/app/guards/acceptTerms.guard.spec.ts
+++ src/app/guards/acceptTerms.guard.spec.ts
@@ -1,15 +1,19 @@
-import { AcceptTermsGuard } from './acceptTerms.guard';
 import { CookieService } from 'ngx-cookie';
+import { of } from 'rxjs';
+import { TermsConditionsService } from '../services/terms-and-conditions/terms-and-conditions.service';
+import { AcceptTermsGuard } from './acceptTerms.guard';
 
 describe('Accept terms guard', () => {
     let guard: AcceptTermsGuard;
     let mockStore: any;
     let mockCookie: jasmine.SpyObj<CookieService>;
+    let mockTermsService: jasmine.SpyObj<TermsConditionsService>;
 
     beforeEach(() => {
         mockStore = jasmine.createSpyObj('mockStore', ['unsubscribe', 'dispatch', 'pipe']);
         mockCookie = jasmine.createSpyObj<CookieService>('mockCookie', ['get']);
-        guard = new AcceptTermsGuard(mockStore, mockCookie);
+        mockTermsService = jasmine.createSpyObj<TermsConditionsService>('mockTermsService', ['getTermsConditions', 'isTermsConditionsFeatureEnabled']);
+        guard = new AcceptTermsGuard(mockStore, mockCookie, mockTermsService);
     });
 
     it('is Truthy', () => {
@@ -17,6 +21,7 @@ describe('Accept terms guard', () => {
     });
 
     it('returns true for non pui users', (done: any) => {
+        mockTermsService.isTermsConditionsFeatureEnabled.and.returnValue(of(true));
         mockCookie.get.and.returnValue('zui-case-manager');
         guard.canActivate().subscribe(access => {
             expect(access).toBeTruthy();

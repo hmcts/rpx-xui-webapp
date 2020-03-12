a7f8bdc448e8a897f28213c81367323521013992
ernestman28 <ernest_man28@hotmail.com>
2020-03-12 19:25:26 +0000

    add redis chart & terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -88,6 +88,10 @@ module "app" {
 
         WEBSITE_NODE_DEFAULT_VERSION  = "12.13.0"
 
+        // Redis Cloud
+        REDISCLOUD_URL = "redis://ignore:${urlencode(module.redis-cache.access_key)}@${module.redis-cache.host_name}:${module.redis-cache.redis_port}?tls=true"
+        REDIS_ENCRYPTION_SECRET = "${data.azurerm_key_vault_secret.redis_secret.value}"
+
     }
 }
 
@@ -109,4 +113,30 @@ data "azurerm_key_vault_secret" "oauth2_secret" {
 
 provider "azurerm" {
     version = "1.22.1"
+}
+
+data "azurerm_subnet" "core_infra_redis_subnet" {
+  name                 = "core-infra-subnet-1-${var.env}"
+  virtual_network_name = "core-infra-vnet-${var.env}"
+  resource_group_name  = "core-infra-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "redis_secret" {
+  name      = "redis-secret"
+  key_vault_id = "${data.azurerm_key_vault.div_key_vault.id}"
+}
+
+resource "azurerm_key_vault_secret" "redis_connection_string" {
+  name = "${var.component}-redis-connection-string"
+  value = "redis://ignore:${urlencode(module.redis-cache.access_key)}@${module.redis-cache.host_name}:${module.redis-cache.redis_port}?tls=true"
+  key_vault_id = "${data.azurerm_key_vault.div_key_vault.id}"
+}
+
+module "redis-cache" {
+  source      = "git@github.com:hmcts/cnp-module-redis?ref=master"
+  product     = "${var.shared_product_name}-redis"
+  location    = "${var.location}"
+  env         = "${var.env}"
+  subnetid    = "${data.azurerm_subnet.core_infra_redis_subnet.id}"
+  common_tags = "${var.common_tags}"
 }

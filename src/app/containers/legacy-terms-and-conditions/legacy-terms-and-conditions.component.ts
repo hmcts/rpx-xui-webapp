c5e112d1a7da976df780a4e600508f0e313e39a1
Uday Denduluri <uday.denduluri@gmail.com>
2020-03-17 16:23:13 +0000

    fix the Lint error

--- src/app/containers/legacy-terms-and-conditions/legacy-terms-and-conditions.component.ts
+++ src/app/containers/legacy-terms-and-conditions/legacy-terms-and-conditions.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-legacy-terms-and-conditions',
+  selector: 'exui-legacy-terms-and-conditions',
   templateUrl: './legacy-terms-and-conditions.component.html'
 })
 export class LegacyTermsAndConditionsComponent {

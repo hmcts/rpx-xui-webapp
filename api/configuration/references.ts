f57a65d4bba0b4cccbbbb91ef907d89ef28b4b2a
Philip <philip.aberneithy@gmail.com>
2020-03-24 08:02:06 +0000

    Remove Redis access key completely.

--- api/configuration/references.ts
+++ api/configuration/references.ts
@@ -71,7 +71,7 @@ export const REDIS_SSL_ENABLED = 'redis.tls'
 export const REDIS_KEY_PREFIX = 'redis.prefix'
 export const REDIS_TTL = 'redis.ttl'
 
-export const REDIS_ACCESS_KEY = 'redis.accessKey'
+// export const REDIS_ACCESS_KEY = 'redis.accessKey'
 // export const REDIS_CLOUD_URL = 'redis.cloudUrl'
 
 export const USER_TIMEOUT_IN_SECONDS = 'userTimeoutInSeconds'

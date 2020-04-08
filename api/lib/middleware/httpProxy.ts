e6d83219c84d30c6abcf7f849c44d26a0e268dc4
Pardeep Singh Basi <paddybasi@gmail.com>
2020-04-08 16:35:52 +0100

    tried the same but different way

--- api/lib/middleware/httpProxy.ts
+++ api/lib/middleware/httpProxy.ts
@@ -1,20 +1,74 @@
 import {Application} from 'express'
 import {ProxyAgentConfigurationType} from 'global-agent'
-import {createProxyMiddleware} from 'http-proxy-middleware'
+import {ClientRequest, IncomingMessage} from 'http'
+import {createProxyMiddleware, Options} from 'http-proxy-middleware'
 import {getConfigValue} from '../../configuration'
 import {SERVICES_DOCUMENTS_API_PATH, SERVICES_EM_ANNO_API_URL} from '../../configuration/references'
+import * as log4jui from '../log4jui'
 import authInterceptor from '../middleware/auth'
 import {JUILogger} from '../models'
-import * as log4jui from '../log4jui'
+import {Request, Response} from "http-proxy-middleware/dist/types";
 
 const logger: JUILogger = log4jui.getLogger('HttpProxy')
 const docUrl: string = getConfigValue(SERVICES_DOCUMENTS_API_PATH)
 const annoUrl: string = getConfigValue(SERVICES_EM_ANNO_API_URL)
 const SERVICE_AUTHORIZATION_HEADER: string = 'serviceauthorization'
 
+
+const applyProxy = (app, config) => {
+  const options = {
+    changeOrigin: true,
+    logLevel: 'debug',
+    logProvider: ()  => {
+      return {
+        debug: msg => logger.debug(msg),
+        error: msg => logger.error(msg),
+        info: msg => logger.info(msg),
+        log: msg => logger.info(msg),
+        warn: msg => logger.warn(msg),
+      }
+    },
+    onError: function onError(err, req, res) {
+      console.error(err)
+      res.status(500)
+      res.json({
+        error: 'Error when connecting to remote server',
+        status: 504
+      })
+    },
+    onProxyReq: (req: ClientRequest) => {
+      console.debug(req.getHeaders())
+      /*req.setHeader("user-roles", "caseworker")
+      req.setHeader("ServiceAuthorization", this.serviceAuthRepository.getToken())*/
+    },
+    onProxyRes: (proxyRes: IncomingMessage, req: Request, res: Response) => {
+      proxyRes.headers['x-added'] = 'foobar'
+      console.debug('response ->:', proxyRes.pipe(res))
+      // res.set(proxyRes.headers)
+      /*req.setHeader("user-roles", "caseworker")
+      req.setHeader("ServiceAuthorization", this.serviceAuthRepository.getToken())*/
+    },
+    pathRewrite: path => {
+      return path.replace('/binary', '')
+    },
+    target: config.target,
+  }
+
+  app.use(config.source, async (req, res, next) => {
+    await authInterceptor(req, res, next)
+    next()
+  }, createProxyMiddleware(options as Options))
+}
+
 // Set up the proxy for the doc store
 export function createDocStoreProxy(app: Application, tunnelAgent: ProxyAgentConfigurationType) {
-  logger.debug('setting up reverse proxy with agent-proxy ', tunnelAgent.HTTP_PROXY)
+
+  applyProxy(app, {
+    source: '/documents',
+    target: docUrl,
+  })
+
+  /*logger.debug('setting up reverse proxy with agent-proxy ', tunnelAgent.HTTP_PROXY)
   const onProxyReq = (proxyReq, req, res) => {
     logger.debug('Proxying request ', req.method, req.url, '->', proxyReq.path)
     const s2s = req.headers.serviceauthorization
@@ -37,13 +91,13 @@ export function createDocStoreProxy(app: Application, tunnelAgent: ProxyAgentCon
   }
   const baseOptions = {
     //agent: tunnelAgent
-    changeOrigin: false,
+    changeOrigin: true,
     onError: onProxyError,
     onProxyReq,
     onProxyRes,
     secure: false,
   }
-  /*const annoOptions = {
+  /!*const annoOptions = {
     //endpoints: ["/em-anno"],
     pathRewrite: {
       '^/em-anno': '/api',
@@ -51,22 +105,23 @@ export function createDocStoreProxy(app: Application, tunnelAgent: ProxyAgentCon
     rewrite: true,
     target: annoUrl,
     ...baseOptions,
-  }*/
+  }*!/
   const docOptions = {
     //endpoints: ["/documents"],
-    agent: tunnelAgent.HTTP_PROXY,
+    // agent: tunnelAgent.HTTP_PROXY,
     rewrite: false,
+    changeOrigin: true,
     target: docUrl,
     ws: false,
     ...baseOptions,
-  }
-  const mwDoc = createProxyMiddleware('/documents', docOptions)
+  }*/
+  /*const mwDoc = createProxyMiddleware('/documents', docOptions)
   app.use('/documents',
     async (req, res, next) => {
       await authInterceptor(req, res, next)
       next()
     },
-    mwDoc)
+    mwDoc)*/
   /*
   const mwAnno = createProxyMiddleware( annoOptions )
    app.use ('/em-anno',

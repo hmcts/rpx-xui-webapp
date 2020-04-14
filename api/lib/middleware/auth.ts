e514d868b9fa11a79ce01fdb3aac85f1b81f3068
Pardeep Singh Basi <paddybasi@gmail.com>
2020-04-14 15:11:02 +0100

    fixed lint issue

--- api/lib/middleware/auth.ts
+++ api/lib/middleware/auth.ts
@@ -75,7 +75,7 @@ export default async (req, res, next) => {
         // axios.defaults.headers.common['user-roles'] = req.auth.data.roles.join()
 
         logger.info('Auth token: ' + `Bearer ${req.auth.token}`)
-        req.headers['user-roles'] = axios.defaults.headers.common['user-roles']
+        req.headers['user-roles'] = req.auth.data.roles.join()
 
         // moved s2s here so we authenticate first
         await serviceTokenMiddleware.default(req, res, () => {

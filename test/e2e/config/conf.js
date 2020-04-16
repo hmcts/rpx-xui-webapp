d8a3def67441187068e7f2f749d0762594738d08
Vamshi-Muniganti <vamshi.muniganti@gmail.com>
2020-04-16 13:11:51 +0100

    Update conf.js

--- test/e2e/config/conf.js
+++ test/e2e/config/conf.js
@@ -76,7 +76,7 @@ const config = {
     strict: true,
     // format: ['node_modules/cucumber-pretty'],
     format: ['node_modules/cucumber-pretty', 'json:reports/tests/json/results.json'],
-    tags: ['@smoke','not @ignore'],
+    tags: ['@ignore'],
     require: [
       '../support/timeout.js',
       '../support/hooks.js',

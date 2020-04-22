c50b3891d63e07fdfd7d2c56ef7da2bf85dd1014
Philip <philip.aberneithy@gmail.com>
2020-04-22 16:59:47 +0100

    Show the contact details for MyHMCTS services if the User has a pui-case-manager role.

--- src/app/components/get-help/get-help.component.ts
+++ src/app/components/get-help/get-help.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { AppConstants } from 'src/app/app.constants';
 import { ContactDetailsDataModel } from '@hmcts/rpx-xui-common-lib';
+import {CookieService} from 'ngx-cookie';
 
 @Component({
     selector: 'exui-get-help',
     templateUrl: './get-help.component.html'
 })
-export class GetHelpComponent {
+export class GetHelpComponent implements OnInit {
 
     public helpContactDetails: ContactDetailsDataModel[] = AppConstants.HELP_CONTACT_DETAILS;
+    public isCaseManager = false;
 
-    constructor() {
-    }
+    constructor(
+      private cookieService: CookieService
+    ) {}
+
+  public ngOnInit() {
+    const userRoles = this.cookieService.get('roles');
+    this.isCaseManager = this.getIsCaseManager(userRoles);
+  }
+
+  public getIsCaseManager(userRoles: string): boolean {
+    return userRoles && userRoles.indexOf('pui-case-manager') !== -1;
+  }
 }

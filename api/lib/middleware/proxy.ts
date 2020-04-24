c264167617f1838c2eb78f951c9b9e921d9deb64
Pardeep Singh Basi <paddybasi@gmail.com>
2020-04-24 16:25:58 +0100

    implemented proxy middleware for documents and print route

---
+++ api/lib/middleware/proxy.ts
@@ -0,0 +1,45 @@
+import {createProxyMiddleware as proxy, Options} from 'http-proxy-middleware'
+import {getConfigValue} from '../../configuration'
+import {LOGGING} from '../../configuration/references'
+import * as log4jui from '../log4jui'
+import authInterceptor from './auth'
+
+const logger = log4jui.getLogger('proxy')
+
+export const onProxyError = (err, req, res) => {
+    logger.error(err)
+    res.status(500).send({
+        error: 'Error when connecting to remote server',
+        status: 504,
+    })
+}
+
+export const applyProxy = (app, config) => {
+    const options: Options = {
+        changeOrigin: true,
+        logLevel: getConfigValue(LOGGING),
+        logProvider: ()  => {
+            return {
+                debug: msg => logger.debug(msg),
+                error: msg => logger.error(msg),
+                info: msg => logger.info(msg),
+                log: msg => logger.info(msg),
+                warn: msg => logger.warn(msg),
+            }
+        },
+        onError: onProxyError,
+        target: config.target,
+    }
+
+    if (false !== config.rewrite) {
+        options.pathRewrite = {
+            [`^${config.source}`]: config.rewriteUrl || ''
+        }
+    }
+
+    if (config.filter) {
+        app.use(config.source, authInterceptor, proxy(config.filter, options))
+    } else {
+        app.use(config.source, authInterceptor, proxy(options))
+    }
+}

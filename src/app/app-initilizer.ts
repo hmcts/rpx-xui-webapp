494477231479838c0153a4dd9b2b94722698d277
Uday Denduluri <uday.denduluri@gmail.com>
2020-05-03 23:24:52 +0100

    fix the app initializer

--- src/app/app-initilizer.ts
+++ src/app/app-initilizer.ts
@@ -1,5 +1,5 @@
 import {select, Store} from '@ngrx/store';
-import {take} from 'rxjs/operators';
+import {takeWhile} from 'rxjs/operators';
 import * as fromApp from './store';
 
 /**
@@ -12,11 +12,18 @@ export function initApplication(store: Store<fromApp.State>): VoidFunction {
     store.dispatch(new fromApp.StartAppInitilizer());
     store.dispatch(new fromApp.LoadConfig());
     store.dispatch(new fromApp.LoadFeatureToggleConfig());
+    let take = true;
+    let i = 1;
     store.pipe(
-      select((state: any) => state.appConfig), take(2)).subscribe(appConfig => {
+      select((state: any) => state.appConfig), takeWhile(x => take)).subscribe(appConfig => {
+      console.log('number ', i);
+      i++;
       if (appConfig.config.features && Object.keys(appConfig.config.features).length) {
         store.dispatch(new fromApp.FinishAppInitilizer());
+        take = false;
         resolve(true);
+      } else {
+        console.log('resolve(false)');
       }
     });
   });

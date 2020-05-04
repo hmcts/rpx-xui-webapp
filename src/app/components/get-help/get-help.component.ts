03d16932719194a2731e4a54e3cf30904f80a49f
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2020-05-04 12:10:57 +0100

    Merge pull request #474 from hmcts/feature/EUI-1774-get-help-gen-page
    Feature/eui 1774 get help gen page

--- src/app/components/get-help/get-help.component.ts
+++ src/app/components/get-help/get-help.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { AppConstants } from 'src/app/app.constants';
 import { ContactDetailsDataModel } from '@hmcts/rpx-xui-common-lib';
+import {CookieService} from 'ngx-cookie';
 
 @Component({
     selector: 'exui-get-help',
     templateUrl: './get-help.component.html'
 })
-export class GetHelpComponent {
+export class GetHelpComponent implements OnInit {
 
-    public getHelpData: ContactDetailsDataModel[] = AppConstants.GET_HELP_DETAILS_DATA;
+    public helpContactDetails: ContactDetailsDataModel[] = AppConstants.HELP_CONTACT_DETAILS;
+    public caseManager = false;
 
-    constructor() {
-    }
+    constructor(
+      private cookieService: CookieService
+    ) {}
+
+  public ngOnInit() {
+    const userRoles = this.cookieService.get('roles');
+    this.caseManager = this.isCaseManager(userRoles);
+  }
+
+  /**
+   * Is Case Manager
+   *
+   * Note that I'm not sure why userRoles is prefixed with j. This should
+   * be removed by the Node service when supplying userRoles to the UI.
+   *
+   * But it would require a rework of the header as well.
+   *
+   * @param userRoles - 'j:["caseworker", "pui-case-manager"]'
+   */
+  public isCaseManager(userRoles: string): boolean {
+    return userRoles && userRoles.indexOf('pui-case-manager') !== -1;
+  }
 }

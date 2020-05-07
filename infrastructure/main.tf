9d3ba4321f9456b2da0ee82d78ca33509cc6b4f1
ernestman28 <ernest_man28@hotmail.com>
2020-05-07 15:37:21 +0100

    cleanup ase

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -20,6 +20,7 @@ module "app" {
     asp_rg = "${local.app_full_name}-${var.env}"
     asp_name = "${var.shared_product_name}-${var.env}"
     #asp_name = "${var.env == "prod" ? "TBD" : "${var.shared_product_name}-${var.env}"}"
+    enable_ase = "${var.enable_ase}"
 
     app_settings = {
         # logging vars & healthcheck
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -153,3 +153,7 @@ variable "node_config_dir" {
   // for Windows
   default = "D:\\home\\site\\wwwroot\\config"
 }
+
+variable "enable_ase" {
+    default = false
+}

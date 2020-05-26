1f2c54b6593848a42e2dfa03291bde23aaaedc95
Philip <philip.aberneithy@gmail.com>
2020-05-26 16:00:35 +0100

    Idle Signout page, with the correct signed out header.

--- src/app/containers/app-header-signed-out/app-header-signed-out.component.ts
+++ src/app/containers/app-header-signed-out/app-header-signed-out.component.ts
@@ -8,10 +8,10 @@ import {UserNavModel} from '../../models/user-nav.model';
 import * as fromActions from '../../store';
 
 @Component({
-  selector: 'exui-app-header',
-  templateUrl: './app-header.component.html',
+  selector: 'exui-app-header-signed-out',
+  templateUrl: './app-header-signed-out.component.html',
 })
-export class AppHeaderComponent implements OnInit {
+export class AppHeaderSignedOutComponent implements OnInit {
   navItems: NavItemsModel[];
   appHeaderTitle: AppTitleModel;
   userNav: UserNavModel;
@@ -23,15 +23,8 @@ export class AppHeaderComponent implements OnInit {
 
   ngOnInit(): void {
     this.appHeaderTitle = AppConstants.APP_HEADER_TITLE;
-    this.navItems = AppConstants.NAV_ITEMS;
-    this.userNav = AppConstants.USER_NAV;
-    this.showFindCase = true;
-  }
-
-  onNavigate(event): void {
-    if (event === 'sign-out') {
-      this.store.dispatch(new fromActions.StopIdleSessionTimeout());
-      return this.store.dispatch(new fromActions.Logout());
-    }
+    this.navItems = AppConstants.SIGNED_OUT_NAV_ITEMS;
+    this.userNav = AppConstants.SIGNED_OUT_USER_NAV;
+    this.showFindCase = false;
   }
 }

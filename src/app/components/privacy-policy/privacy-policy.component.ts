b3673707a58a8bf159f837a6bb6ca53f672d06a1
Boris <36511748+borislavpetrovhmcts@users.noreply.github.com>
2020-06-01 09:46:09 +0100

    Update privacy policy with latest version (#510)
    * update privacy policy with latest version
    
    * fix linking bugs
    
    * refactor privacy policy component
    
    * refactor privacy policy component bug fix
    
    * fix typo in component name

--- src/app/components/privacy-policy/privacy-policy.component.ts
+++ src/app/components/privacy-policy/privacy-policy.component.ts
@@ -1,19 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
-    selector: 'exui-privacy-policy',
-    templateUrl: './privacy-policy.component.html'
+  selector: 'exui-privacy-policy',
+  templateUrl: './privacy-policy.component.html'
 })
 export class PrivacyPolicyComponent implements OnInit {
 
-    constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) { }
 
-    ngOnInit() {
-        this.route.fragment.subscribe(fragment => {
-            try {
-                document.querySelector('#' + fragment).scrollIntoView();
-            } catch (e) { }
-        });
+  private subscription: Subscription;
+
+  ngOnInit() {
+    this.subscription = this.route.fragment.subscribe(fragment => {
+      try {
+        document.querySelector('#' + fragment).scrollIntoView();
+      } catch (e) { }
+    });
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickout(event) {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
     }
+    this.subscription = this.route.fragment.subscribe(fragment => {
+      try {
+        document.querySelector('#' + fragment).scrollIntoView();
+      } catch (e) { }
+    });
+  }
 }

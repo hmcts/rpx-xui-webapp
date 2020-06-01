7dce7a5db2f2eff5f48a2120720a9f3d305fad7c
Philip <philip.aberneithy@gmail.com>
2020-06-01 14:20:16 +0100

    Fixed typings around express request.

--- api/user/index.ts
+++ api/user/index.ts
@@ -3,16 +3,7 @@ import { getUserSessionTimeout } from './userTimeout';
 import { getConfigValue } from '../configuration'
 import { SESSION_TIMEOUTS } from '../configuration/references'
 
-interface SessionRequest extends express.Request {
-  session: {
-    user: {
-      roles: ['']
-    }
-  },
-}
-
-// TODO: Need to test error condition here.
-export async function getUserDetails(req: SessionRequest, res: express.Response, next: express.NextFunction) {
+export async function getUserDetails(req: express.Request, res: express.Response, next: express.NextFunction) {
 
   const { roles } = req.session.user
 

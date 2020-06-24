d56b4db627f4f12fe11a1ab28b902a887c7d45a3
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2020-06-24 16:59:02 +0100

    EUI-1948 linting

--- api/configuration/uiConfigRouter.ts
+++ api/configuration/uiConfigRouter.ts
@@ -1,11 +1,11 @@
 import * as express from 'express'
 import {getConfigValue} from './index'
 import {
+  LAUNCH_DARKLY_CLIENT_ID,
   PROTOCOL,
   SERVICES_IDAM_CLIENT_ID,
   SERVICES_IDAM_LOGIN_URL,
   SERVICES_IDAM_OAUTH_CALLBACK_URL,
-  LAUNCH_DARKLY_CLIENT_ID,
 } from './references'
 
 export const router = express.Router({mergeParams: true})
@@ -24,9 +24,9 @@ async function uiConfigurationRouter(req, res) {
   res.status(200).send({
     clientId: getConfigValue(SERVICES_IDAM_CLIENT_ID),
     idamWeb: getConfigValue(SERVICES_IDAM_LOGIN_URL),
+    launchDarklyClientId: getConfigValue(LAUNCH_DARKLY_CLIENT_ID),
     oAuthCallback: getConfigValue(SERVICES_IDAM_OAUTH_CALLBACK_URL),
     protocol: getConfigValue(PROTOCOL),
-    launchDarklyClientId: getConfigValue(LAUNCH_DARKLY_CLIENT_ID),
   })
 }
 

3895f9c8bd4a955173313442905724fa4b3bb100
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2020-06-30 16:17:30 +0100

    EUI-2191 dynamic error handling

--- api/emAnno/emAnnoService.ts
+++ api/emAnno/emAnnoService.ts
@@ -21,7 +21,7 @@ export async function handleGet(annotationsPath: string, req: EnhancedRequest): 
         const response = await http.get(annotationsPath, { headers })
         return response
     } catch (e) {
-        logger.error(e.message)
+        e.message ? logger.error(e.message) : logger.error('Error in em-anno-service get response')
         throw e
     }
 
@@ -42,7 +42,7 @@ export async function handlePost(annotationsPath: string, body: Annotation, req:
         const response = await http.post(annotationsPath, body, { headers })
         return response
     } catch (e) {
-        logger.error(e.message)
+        e.message ? logger.error(e.message) : logger.error('Error in em-anno-service post response')
         throw e
     }
 
@@ -63,7 +63,7 @@ export async function handlePut(annotationsPath: string, body: Annotation, req: 
         const response = await http.put(annotationsPath, body, { headers })
         return response
     } catch (e) {
-        logger.error(e.message)
+        e.message ? logger.error(e.message) : logger.error('Error in em-anno-service put response')
         throw e
     }
 
@@ -83,7 +83,7 @@ export async function handleDelete(annotationsPath: string, req: EnhancedRequest
         const response = await http.delete(annotationsPath, { headers })
         return response
     } catch (e) {
-        logger.error(e.message)
+        e.message ? logger.error(e.message) : logger.error('Error in em-anno-service delete response')
         throw e
     }
 

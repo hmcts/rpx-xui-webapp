76001d5442f706bd6ad19601b9999cc6c4b219bc
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2020-07-17 18:05:32 +0100

    EUI-1753 Moved all action into the node layer

--- api/routes.ts
+++ api/routes.ts
@@ -2,12 +2,15 @@ import * as express from 'express'
 import * as amendedJurisdictions from './amendedJurisdictions'
 import authInterceptor from './lib/middleware/auth'
 import * as proxy from './lib/proxy'
+import * as searchCases from './searchCases'
 
 const router = express.Router({ mergeParams: true })
 
 router.use(authInterceptor)
 
 router.get('/caseworkers/:uid/jurisdictions', amendedJurisdictions.getJurisdictions)
+
+router.post('/internal/searchCases', searchCases.getCases)
 
 router.get('/*', proxy.get)
 router.post('/*', proxy.post)

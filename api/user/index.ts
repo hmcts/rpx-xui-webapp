2c3dc6d0bac677a3cf6ec2b84ada207517686392
Philip <philip.aberneithy@gmail.com>
2020-07-20 11:13:43 +0100

    Lib Session Timeout finder

--- api/user/index.ts
+++ api/user/index.ts
@@ -1,13 +1,14 @@
 import { NextFunction, Response } from 'express'
 import { getConfigValue } from '../configuration'
 import { SESSION_TIMEOUTS } from '../configuration/references'
-import { getUserSessionTimeout } from './userTimeout'
+import { getUserSessionTimeout } from '@hmcts/rpx-xui-node-lib'
+import { RoleGroupSessionTimeout } from '@hmcts/rpx-xui-node-lib/dist/common/util/userTimeout'
 
 export async function getUserDetails(req, res: Response, next: NextFunction) {
 
   const { roles } = req.session.passport.user.userinfo
 
-  const sessionTimeouts = getConfigValue(SESSION_TIMEOUTS)
+  const sessionTimeouts = getConfigValue(SESSION_TIMEOUTS) as RoleGroupSessionTimeout[]
   const sessionTimeout = getUserSessionTimeout(roles, sessionTimeouts)
 
   try {

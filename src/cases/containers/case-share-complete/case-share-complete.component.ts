3ff9f4391bec4444e84b55511bbe45c55e484008
kpokala <kpokala@gmail.com>
2020-07-21 15:09:40 +0100

    update

--- src/cases/containers/case-share-complete/case-share-complete.component.ts
+++ src/cases/containers/case-share-complete/case-share-complete.component.ts
@@ -8,7 +8,7 @@ import * as fromCaseList from '../../store/reducers';
 @Component({
   selector: 'exui-case-share-complete',
   templateUrl: './case-share-complete.component.html',
-  styleUrls: ['./case-share-complete.component.scss']
+  styleUrls: ['case-share-complete.component.scss']
 })
 export class CaseShareCompleteComponent implements OnInit {
 

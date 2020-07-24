ab3cf0426a19fb3b8e6b5a3377abf0a9b77c6ccc
udaydenduluri33 <50869558+udaydenduluri33@users.noreply.github.com>
2020-07-24 12:00:11 +0100

    adding unit tests for authguard and appp-config.reducer (#589)
    Co-authored-by: Uday Denduluri <uday.denduluri@gmail.com>

--- src/app/store/reducers/app-config.reducer.spec.ts
+++ src/app/store/reducers/app-config.reducer.spec.ts
@@ -1,3 +1,4 @@
+import { TCDocument } from '@hmcts/rpx-xui-common-lib';
 import * as fromActions from '../actions/';
 import * as appConfigReducer from './app-config.reducer';
 
@@ -50,6 +51,37 @@ describe('App Config Reducer', () => {
                     totalIdleTime: 100,
                 }
             }
+        };
+        expect(state).toEqual(expectedState);
+    });
+
+    it('StartIdleSessionTimeout', () => {
+        const action = new fromActions.StartIdleSessionTimeout();
+        const state = appConfigReducer.reducer(appConfigReducer.initialState, action);
+        const expectedState = {
+            ...appConfigReducer.initialState,
+            useIdleSessionTimeout: true
+        };
+        expect(state).toEqual(expectedState);
+    });
+
+    it('StopIdleSessionTimeout', () => {
+        const action = new fromActions.StopIdleSessionTimeout();
+        const state = appConfigReducer.reducer(appConfigReducer.initialState, action);
+        const expectedState = {
+            ...appConfigReducer.initialState,
+            useIdleSessionTimeout: false
+        };
+        expect(state).toEqual(expectedState);
+    });
+
+    it('LoadTermsConditionsSuccess', () => {
+        const document: TCDocument = {version: 1234, content: 'some', mimeType: 'text/html'};
+        const action = new fromActions.LoadTermsConditionsSuccess(document);
+        const state = appConfigReducer.reducer(appConfigReducer.initialState, action);
+        const expectedState = {
+            ...appConfigReducer.initialState,
+            termsAndConditions: document
         };
         expect(state).toEqual(expectedState);
     });

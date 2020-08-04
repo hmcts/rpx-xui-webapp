14ba2ac25acb3c2d4f459ce566878bef8aa2e7cd
sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
2020-08-04 17:33:01 +0100

    cheryypick pa11y test commit

---
+++ test/accessibility/config/pa11yProtractor.conf.js
@@ -0,0 +1,31 @@
+// Protractor configuration file, see link for more information
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+const { SpecReporter } = require('jasmine-spec-reporter');
+
+
+exports.config = {
+    allScriptsTimeout: 11000,
+    specs: [
+        '../tests/index.js'
+    ],
+    capabilities: {
+        'browserName': 'chrome',
+        'chromeOptions': { args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-zygote ', '--disableChecks', '--disable-notifications'] }
+
+    },
+    directConnect: true,
+    baseUrl: 'http://localhost:4200/',
+    framework: 'mocha',
+    mochaOpts: {
+        reporter: 'test/accessibility/reporter/customReporter.js',
+        // reporter: 'spec',
+
+        timeout: 120000
+    },
+    onPrepare() {
+
+    },
+    onComplete() {
+    }
+};

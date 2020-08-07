c6c864a32955de96c7b627224a2da84d29ab793a
ernestman28 <ernest_man28@hotmail.com>
2020-08-07 09:42:56 +0100

    terraform clean up

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -23,24 +23,6 @@ variable "env" {
   type = "string"
 }
 
-// health service urls
-variable "health_ccd_component_api" {}
-variable "health_ccd_data_api" {}
-variable "health_documents_api" {}
-variable "health_en_anno_api" {}
-variable "health_terms_and_conditions_api" {}
-
-// service urls
-variable "services_ccd_component_api" {}
-variable "services_ccd_data_store_api" {}
-variable "services_documents_api" {}
-variable "services_em_anno_api" {}
-variable "services_idam_api_url" {}
-variable "services_idam_login_url" {}
-variable "services_payments_url" {}
-variable "services_s2s" {}
-variable "services_terms_and_conditions" {}
-
 variable "mc_http_proxy" {
   default = "http://172.16.0.7:8080"
 }

16c1d9989086e16db05c25707ba6eef6f101038e
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2020-08-17 10:54:54 +0100

    Fixed bug

--- api/user/index.ts
+++ api/user/index.ts
@@ -5,14 +5,14 @@ import { CASE_SHARE_PERMISSIONS, SESSION_TIMEOUTS } from '../configuration/refer
 
 export async function getUserDetails(req, res: Response, next: NextFunction) {
 
-  const { roles } = req.session.user
-
-  const permissions = CASE_SHARE_PERMISSIONS.split(',')
-  const canShareCases = roles.some(role => permissions.includes(role))
-  const sessionTimeouts = getConfigValue(SESSION_TIMEOUTS) as RoleGroupSessionTimeout[]
-  const sessionTimeout = getUserSessionTimeout(roles, sessionTimeouts)
-
   try {
+    const { roles } = req.session.passport.user.userinfo
+
+    const permissions = CASE_SHARE_PERMISSIONS.split(',')
+    const canShareCases = roles.some(role => permissions.includes(role))
+    const sessionTimeouts = getConfigValue(SESSION_TIMEOUTS) as RoleGroupSessionTimeout[]
+    const sessionTimeout = getUserSessionTimeout(roles, sessionTimeouts)
+
     res.send({
       canShareCases,
       sessionTimeout,

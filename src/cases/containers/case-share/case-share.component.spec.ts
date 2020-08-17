e8203acc743415935cce5ab7bfed90b919479dcf
Raymond Liang <ray.liang@atos.net>
2020-08-17 17:48:25 +0100

    fix test

--- src/cases/containers/case-share/case-share.component.spec.ts
+++ src/cases/containers/case-share/case-share.component.spec.ts
@@ -1,7 +1,9 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FeatureToggleService } from '@hmcts/rpx-xui-common-lib';
 import { Store } from '@ngrx/store';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { of } from 'rxjs';
 import { State } from '../../../app/store/reducers';
 import { CaseShareComponent } from './case-share.component';
 
@@ -11,6 +13,7 @@ describe('CaseShareComponent', () => {
 
   let mockStore: MockStore<State>;
   let dispatchSpy: jasmine.Spy;
+  const mockFeatureToggleService = jasmine.createSpyObj('FeatureToggleService', ['getValue']);
 
   const sharedCases = [{
     caseId: '9417373995765133',
@@ -30,9 +33,14 @@ describe('CaseShareComponent', () => {
       declarations: [ CaseShareComponent ],
       providers: [
         provideMockStore(),
+        {
+          provide: FeatureToggleService,
+          useValue: mockFeatureToggleService
+        }
       ]
     }).compileComponents();
     mockStore = TestBed.get(Store);
+    mockFeatureToggleService.getValue.and.returnValue(of(true));
     dispatchSpy = spyOn(mockStore, 'dispatch');
     fixture = TestBed.createComponent(CaseShareComponent);
     component = fixture.componentInstance;

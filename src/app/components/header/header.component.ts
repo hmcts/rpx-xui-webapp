09e6b96907f558419e18e25836570ac10a2caccf
Philip <philip.aberneithy@gmail.com>
2020-09-09 10:06:35 +0100

    Thoughts before starting.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,6 +12,20 @@ import { AppUtils } from 'src/app/app-utils';
   selector: 'exui-header',
   templateUrl: './header.component.html'
 })
+
+/**
+ * Header Component
+ *
+ * This header component is common to both MO and MC, and is doing the work of fetching data from the store,
+ * and displaying the items in the header dependent on the User.
+ *
+ * TODO:
+ * If we want to keep our /component folder pure and gradually move these components across to a common lib,
+ * we should not have application logic in here ie. this.cookieService.get('roles'), and none of the logic that
+ * fetches items from the store, instead they should be passed down into this component.
+ *
+ * Therefore application logic needs to be abstracted up a level, to app-header.
+ */
 export class HeaderComponent implements OnInit, OnDestroy {
 
   @Input() navItems: { active: boolean; href: string; }[];
@@ -29,13 +43,18 @@ export class HeaderComponent implements OnInit, OnDestroy {
     private cookieService: CookieService
   ) {}
 
+
   ngOnInit() {
+     // This is in the wrong location? This should be in app-header.component.ts,
+     // and we should use it to pull out the correct Judicial role, and send
+     // the configuration of the header into this header component.
      const userRoles = this.cookieService.get('roles');
      this.isCaseManager = this.getIsCaseManager(userRoles);
      const observable = this.getObservable(this.store);
      this.subscription = this.subscribe(observable);
   }
 
+  // So over here we're subscribing to the nav items
   subscribe(observable: Observable<string>): Subscription {
     return  observable.subscribe(url => {
       this.showNavItems = of(AppUtils.showNavItems(url));
@@ -46,6 +65,8 @@ export class HeaderComponent implements OnInit, OnDestroy {
     return store.pipe(select(fromRoot.getRouterUrl));
   }
 
+  // we're doing a simple way of checking what role the User has
+  // the header component is checking.
   getIsCaseManager(userRoles: string): boolean {
     return userRoles && userRoles.indexOf('pui-case-manager') !== -1;
   }
--- src/app/containers/app-header/app-header.component.ts
+++ src/app/containers/app-header/app-header.component.ts
@@ -11,6 +11,18 @@ import * as fromActions from '../../store';
   selector: 'exui-app-header',
   templateUrl: './app-header.component.html',
 })
+
+/**
+ * AppHeaderComponent
+ *
+ * TODO: There is no App Header Component in MO.
+ * I initially believed that this component could be an unnecessary abstraction.
+ *
+ * But everywhere in this project we use exui-app-header.
+ *
+ * But this takes in application constants and passes it down to other components, which makes sense, so that
+ * components in the component folder are pure.
+ */
 export class AppHeaderComponent implements OnInit {
   navItems: NavItemsModel[];
   appHeaderTitle: AppTitleModel;
@@ -21,8 +33,22 @@ export class AppHeaderComponent implements OnInit {
     private store: Store<fromActions.State>) {
   }
 
+  // Note that all components use this app-header.component.html, and therefore all components use
+  // this: exui-app-header
+
   ngOnInit(): void {
+
+
+    // So we should be able to pass a User in, and be returned the appropiate appHeaderTitle, navItems, and
+    // userNav for that User.
+    // maybe we want a style object that we pass through as well.
+    // So an app header configuration object is passed into app header
+
+    // The app header changes dependent on the User.
     this.appHeaderTitle = AppConstants.APP_HEADER_TITLE;
+
+    // I guess in the future the navItems,
+    // and user nav may change dependent on the User.
     this.navItems = AppConstants.NAV_ITEMS;
     this.userNav = AppConstants.USER_NAV;
     this.showFindCase = true;

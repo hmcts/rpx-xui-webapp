fea1a7c267d697465f45e9173629f4fad384af19
Pardeep Singh Basi <paddybasi@gmail.com>
2020-10-13 14:34:07 +0100

    Feature/noc/eui 2450 microservice mock (#664)
    * EUI-2625-accessibility-policy (#646)
    
    * upgraded to latest node lib - csrf feature (#642)
    
    * upgraded to latest node lib
    
    * fixed pact issue
    
    * added new security script and audit file
    
    * ZAP report added
    
    Co-authored-by: VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>
    
    * added models and routes
    
    * added mock data
    
    * correctly returning generated mock data, 1st request is always a network timeout
    
    * fixed lint issues
    
    * fixed MOCK issue intercepting all requests
    
    * updated mock routes for POST
    
    Co-authored-by: rayliangatos <57181888+rayliangatos@users.noreply.github.com>
    Co-authored-by: VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>

--- api/noc/index.ts
+++ api/noc/index.ts
@@ -3,7 +3,7 @@ import {getConfigValue} from '../configuration'
 import {SERVICES_CCD_DATA_STORE_API_PATH} from '../configuration/references'
 import {EnhancedRequest} from '../lib/models'
 import {NoCQuestions} from './models/noCQuestions.interface'
-import {handleGet} from './noCService'
+import {handleGet, handlePost} from './noCService'
 import * as mock from './noCService.mock'
 
 mock.init()
@@ -27,9 +27,10 @@ export async function getNoCQuestions(req: EnhancedRequest, res: Response, next:
 
 export async function validateNoCQuestions(req: EnhancedRequest, res: Response, next: NextFunction) {
     const markupPath: string = url + req.originalUrl
+    const body: any = req.body
 
     try {
-        const {status, data}: { status: number, data: NoCQuestions } = await handleGet(markupPath, req)
+        const {status, data}: { status: number, data: NoCQuestions } = await handlePost(markupPath, body, req)
         res.status(status).send(data)
     } catch (error) {
         next(error)
@@ -38,9 +39,10 @@ export async function validateNoCQuestions(req: EnhancedRequest, res: Response, 
 
 export async function noCEvents(req: EnhancedRequest, res: Response, next: NextFunction) {
     const markupPath: string = url + req.originalUrl
+    const body: any = req.body
 
     try {
-        const {status, data}: { status: number, data: NoCQuestions } = await handleGet(markupPath, req)
+        const {status, data}: { status: number, data: NoCQuestions } = await handlePost(markupPath, body, req)
         res.status(status).send(data)
     } catch (error) {
         next(error)

48edd0052880dbcc6eb0fa9a9ac2e245c7ef421f
udaydenduluri33 <50869558+udaydenduluri33@users.noreply.github.com>
2020-10-15 17:05:14 +0100

    handling server errors which not api specific (#672)
    * handling server errors which not api specific
    
    * rename describe/it for unit test is404Or5xxError
    
    Co-authored-by: Uday Denduluri <uday.denduluri@gmail.com>

--- src/noc/store/effects/noc.effects.spec.ts
+++ src/noc/store/effects/noc.effects.spec.ts
@@ -184,4 +184,31 @@ describe('Noc Effects', () => {
     });
   });
 
+  describe('is404Or5xxError', () => {
+    it('should return isError', () => {
+      let isRelaventError = NocEffects.is404Or5xxError(400);
+      expect(isRelaventError).toBeFalsy();
+
+      isRelaventError = NocEffects.is404Or5xxError(undefined);
+      expect(isRelaventError).toBeFalsy();
+
+
+      isRelaventError = NocEffects.is404Or5xxError(null);
+      expect(isRelaventError).toBeFalsy();
+
+
+      isRelaventError = NocEffects.is404Or5xxError(401);
+      expect(isRelaventError).toBeFalsy();
+
+      isRelaventError = NocEffects.is404Or5xxError(404);
+      expect(isRelaventError).toBeTruthy();
+
+      isRelaventError = NocEffects.is404Or5xxError(501);
+      expect(isRelaventError).toBeTruthy();
+
+      isRelaventError = NocEffects.is404Or5xxError(502);
+      expect(isRelaventError).toBeTruthy();
+    });
+  });
+
 });

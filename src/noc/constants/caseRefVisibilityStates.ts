fe1644d2b33e7f29c11722830d192fbb598ff300
Uday Denduluri <uday.denduluri@gmail.com>
2020-10-20 12:58:09 +0100

    noc changes for showing api specific error messages

--- src/noc/constants/caseRefVisibilityStates.ts
+++ src/noc/constants/caseRefVisibilityStates.ts
@@ -1,3 +1,3 @@
 import { NocState } from '../models';
 
-export const caseRefVisibilityStates = [NocState.START, NocState.CASE_REF_VALIDATION_FAILURE, NocState.CASE_REF_SUBMISSION_FAILURE];
+export const caseRefVisibilityStates = [NocState.START, NocState.CASE_REF_VALIDATION_FAILURE];
--- src/noc/containers/noc-errors/noc-error.component.ts
+++ src/noc/containers/noc-errors/noc-error.component.ts
@@ -1,12 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { NocNavigation, NocNavigationEvent } from 'src/noc/models';
+import * as fromFeature from '../../store';
 
 @Component({
     selector: 'exui-noc-error',
     templateUrl: 'noc-error.component.html'
   })
 
-export class NocErrorComponent implements OnInit {
-  public ngOnInit(): void {
-    console.log('NocErrorComponent');
+export class NocErrorComponent implements OnChanges {
+  @Input() public navEvent: NocNavigation;
+  constructor(private readonly store: Store<fromFeature.State>) {
+    this.navEvent = {
+      event: null,
+      timestamp: null
+    };
+  }
+
+  public ngOnChanges(changes: SimpleChanges) {
+    if (changes.navEvent && this.navEvent) {
+      this.navigationHandler(this.navEvent.event);
+    }
+  }
+
+  public navigationHandler(navEvent: NocNavigationEvent) {
+    switch (navEvent) {
+      case NocNavigationEvent.BACK: {
+        this.store.dispatch(new fromFeature.Reset());
+        break;
+      }
+      case NocNavigationEvent.CONTINUE:
+      break;
+      default:
+        throw new Error('Invalid option');
+    }
   }
 }

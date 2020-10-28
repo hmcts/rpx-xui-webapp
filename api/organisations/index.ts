c2db103822864ebcd3be519e9c0b5592c40b55d3
rayliangatos <57181888+rayliangatos@users.noreply.github.com>
2020-10-28 17:18:21 +0000

    refactor-case-share-api-2 (#682)
    * refactor-case-share-api-2
    
    * fix lint issue
    
    * fix failing unit tests
    
    Co-authored-by: Uday Denduluri <uday.denduluri@gmail.com>

--- api/organisations/index.ts
+++ api/organisations/index.ts
@@ -2,10 +2,7 @@ import { NextFunction, Response } from 'express'
 import { handleGet } from '../common/crudService'
 import { getConfigValue } from '../configuration'
 import {SERVICES_PRD_API_URL} from '../configuration/references'
-import * as log4jui from '../lib/log4jui'
-import { EnhancedRequest, JUILogger } from '../lib/models'
-
-const logger: JUILogger = log4jui.getLogger('organisations')
+import { EnhancedRequest } from '../lib/models'
 
 export async function handleGetOrganisationsRoute(req: EnhancedRequest, res: Response, next: NextFunction) {
     try {

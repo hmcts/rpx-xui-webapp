15f380d4adbb0fb65582891f14f022c108d0c029
Philip A <philip.aberneithy@gmail.com>
2020-10-29 10:59:03 +0000

    Feature/eui 2292 judicial header 5 (#681)
    * Thoughts before starting.
    
    * Abstracted out business logic that was in component to parent, after discussions.
    
    * Working through.
    
    * Unit tests working
    
    * deserialising the cookie roles.
    
    * Need to mock out cookies.
    
    * Writing func to get the theme for the User.
    
    * finding the theme, using the users roles.
    
    * Working through dynamically setting header based on user.
    
    * Testing logic works.
    
    * Testing users.
    
    * Default theme.
    
    * Fixed up unit tests.
    
    * Prototyping change of background color.
    
    * Header Color changes.
    
    * Pass through logo type.
    
    * Before larger changes to header
    
    * Refactor of global header component.
    
    * Comments.
    
    * Remove isCaseManager.
    
    * Cleaning App header component.
    
    * Cleaning after testing with test users.
    
    * Moving theming out into constants file.
    
    * Idle Sign out screen has a header, note that we do not have the Users roles on this page, as the User has been signed out.
    
    * Comment out unused unit test.
    
    * Cleaning comments, and tests.
    
    * Fix errors.
    
    * Fix Jenkins build, as it picked up linting issues.
    
    * backgrond class, and unit tests.
    
    * Clean.
    
    * Trigger build.
    
    * Override background color.
    
    * Absolute positioning logo.
    
    * Testing some css positioning.
    
    * All in one component, logo absolutly positioned.
    
    * Upper case Judicial Case Manager.
    
    * Cleaning.
    
    * Improving accessibility and fixing a bug.
    
    * Unit tests, and clean.
    
    * update media viewer to 2.5.1 requested by Mo
    
    * ngrx-chips to help resolve the build.
    
    * Updated lock file.
    
    * Add link on logo.
    
    * Working through LD around menu items.
    
    * Trying to get LD flag working.
    
    * Works dynamically with Launch Darkly.
    
    * Working through unit test.
    
    * Fix unit tests.
    
    * Remove NOC item for deployment.
    
    * Code review fixes.
    
    * Cleaning code.
    
    Co-authored-by: Raymond Liang <ray.liang@atos.net>

--- src/app/containers/app-header/app-header.component.spec.ts
+++ src/app/containers/app-header/app-header.component.spec.ts
@@ -1,79 +1,250 @@
-import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppHeaderComponent } from './app-header.component';
-import { StoreModule, Store } from '@ngrx/store';
+import { Action, Store, StoreModule } from '@ngrx/store';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppConstants } from 'src/app/app.constants';
 import * as fromActions from '../../store';
-import { CookieService, CookieModule } from 'ngx-cookie';
+import { CookieService } from 'ngx-cookie';
+import {FeatureToggleService} from '@hmcts/rpx-xui-common-lib';
+import {of} from 'rxjs';
 
-
-const cookieService = {
-  get: key => {
-    return cookieService[key];
+const storeMock = {
+  pipe: () => {
   },
-  set: (key, value) => {
-    cookieService[key] = value;
-  },
-  removeAll: () => { }
+  dispatch: (action: Action) => {
+  }
 };
 
+/**
+ * Serialised roles as returned from the Node layer.
+ */
+const SERIALISED_ROLES = 'j:["pui-organisation-manager", "caseworker-publiclaw", "caseworker-divorce-financialremedy-solicitor", "caseworker"]';
+
+const cookieServiceMock = {
+  get: () => {
+    return SERIALISED_ROLES;
+  },
+};
+
+const featureToggleServiceMock = {
+  getValue: () => {
+    return {
+      subscribe: () => AppConstants.APPLICATION_USER_THEMES,
+    };
+  }
+};
+
+let pipeSpy: jasmine.Spy;
+let dispatchSpy: jasmine.Spy;
 
 describe('AppHeaderComponent', () => {
+
   let component: AppHeaderComponent;
   let fixture: ComponentFixture<AppHeaderComponent>;
   let store: Store<fromActions.State>;
 
+  const mockDetails = '/cases';
+
   beforeEach(async(() => {
+
+    pipeSpy = spyOn(storeMock, 'pipe').and.returnValue(of(mockDetails));
+    dispatchSpy = spyOn(storeMock, 'dispatch');
+
     TestBed.configureTestingModule({
       imports: [
         StoreModule.forRoot({})
       ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
       declarations: [
         AppHeaderComponent
       ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {
+          provide: Store,
+          useValue: storeMock,
+        },
+        {
+          provide: CookieService,
+          useValue: cookieServiceMock,
+        },
+        {
+          provide: FeatureToggleService,
+          useValue: featureToggleServiceMock,
+        },
+        AppHeaderComponent
+      ],
     }).compileComponents();
-  }));
 
-  beforeEach(() => {
+    store = TestBed.get(Store);
+
     fixture = TestBed.createComponent(AppHeaderComponent);
     component = fixture.componentInstance;
-    store = TestBed.get(Store);
     fixture.detectChanges();
+  }));
+
+  describe('getDefaultTheme()', () => {
+
+    it('should return the applications default theme.', () => {
+      expect(component.getDefaultTheme()).toEqual(AppConstants.DEFAULT_USER_THEME);
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('getApplicationThemes()', () => {
+
+    it('should return the applications themes.', () => {
+      expect(component.getDefaultApplicationThemes()).toEqual(AppConstants.APPLICATION_USER_THEMES);
+    });
   });
 
-  it('should update parameter on ngOnInit', () => {
-    const dummyAppHeaderTitle = {name: 'Dummy', url: '/'};
-    const dummyNavItems = [{
-      text: 'dummy',
-      href: '/dummy',
-      active: false
-    }];
-    const dummyUserNav = {
-      label: 'dummy',
-      items: [{
-        text: 'Sign out',
-        emit: 'sign-out'
-      }]
-    };
-    AppConstants.APP_HEADER_TITLE = dummyAppHeaderTitle;
-    AppConstants.NAV_ITEMS = dummyNavItems;
-    AppConstants.USER_NAV = dummyUserNav;
-    component.ngOnInit();
-    expect(component.appHeaderTitle).toBe(dummyAppHeaderTitle);
-    expect(component.navItems).toBe(dummyNavItems);
-    expect(component.userNav).toBe(dummyUserNav);
+  describe('deserialiseUserRoles()', () => {
+
+    it('should take in serialised roles, and deserialise them into an array.', () => {
+
+      const serialisedRoles = 'j:["pui-organisation-manager","caseworker-publiclaw","caseworker-divorce-financialremedy-solicitor","caseworker"]';
+      expect(component.deserialiseUserRoles(serialisedRoles)).toEqual([
+        'pui-organisation-manager',
+        'caseworker-publiclaw',
+        'caseworker-divorce-financialremedy-solicitor',
+        'caseworker'
+      ]);
+    });
   });
 
-  it('should logout when onNavigate sign-out is called ', () => {
-    const spyOnDispatchToStore = spyOn(store, 'dispatch').and.callThrough();
-    component.onNavigate('anything');
-    expect(spyOnDispatchToStore).toHaveBeenCalledTimes(0);
-    component.onNavigate('sign-out');
-    expect(spyOnDispatchToStore).toHaveBeenCalledWith(new fromActions.Logout());
+  describe('getUsersTheme()', () => {
+
+    it('should return the theme in priority order ie. a theme higher up the themes array' +
+      'will be compared with the user\'s roles first.', () => {
+
+      // Remember we want exact matches not partial matches
+      const userRoles = ['pui-case-manager', 'caseworker-sscs-judge'];
+
+      // We compare the first set of theme roles first.
+      const themes = [
+        {
+          roles: [
+            'caseworker-sscs-judge',
+            'caseworker-sscs-panelmember',
+            'caseworker-cmc-judge',
+            'caseworker-divorce-judge',
+          ],
+          appTitle: {name: 'Judicial case manager', url: '/'},
+          navigationItems: [
+            {
+              text: 'Case list',
+              href: '/cases',
+              active: false
+            },
+            {
+              text: 'Create case',
+              href: '/cases/case-filter',
+              active: false
+            },
+          ],
+          accountNavigationItems: {
+            label: 'Account navigation',
+            items: [{
+              text: 'Sign out d',
+              emit: 'sign-out'
+            }]
+          },
+          showFindCase: false,
+          backgroundColor: '#8d0f0e',
+          logoIsUsed: true,
+          logoType: 'judicial',
+        },
+        {
+          roles: ['pui-case-manager'],
+          appTitle: {name: 'Manage Cases', url: '/'},
+          navigationItems: [
+            {
+              text: 'Case list',
+              href: '/cases',
+              active: false
+            },
+            {
+              text: 'Create case',
+              href: '/cases/case-filter',
+              active: false
+            }
+          ],
+          accountNavigationItems: {
+            label: 'Account navigation',
+            items: [{
+              text: 'Sign out',
+              emit: 'sign-out'
+            }]
+          },
+          showFindCase: true,
+          backgroundColor: '#202020',
+          logoIsUsed: true,
+          logoType: 'myhmcts',
+        },
+      ];
+
+      const defaultTheme = AppConstants.DEFAULT_USER_THEME;
+
+      expect(component.getUsersTheme(userRoles, themes, defaultTheme)).toEqual(themes[0]);
+    });
+
+    it('should return a default theme if there are no user roles.', () => {
+
+      const userRoles = [];
+
+      const themes = [
+        {
+          roles: [
+            'caseworker-sscs-judge',
+            'caseworker-sscs-panelmember',
+            'caseworker-cmc-judge',
+            'caseworker-divorce-judge',
+          ],
+          appTitle: 'Judicial Case Manager',
+        },
+        {
+          roles: ['pui-case-manager'],
+          appTitle: 'Case Manager',
+        },
+      ];
+      const defaultTheme = AppConstants.DEFAULT_USER_THEME;
+
+      expect(component.getUsersTheme(userRoles, themes, defaultTheme)).toEqual(AppConstants.DEFAULT_USER_THEME);
+    });
+
+    it('should return a default theme if there are no themes.', () => {
+
+      const userRoles = ['pui-case-manager'];
+      const themes = [];
+      const defaultTheme = AppConstants.DEFAULT_USER_THEME;
+
+      expect(component.getUsersTheme(userRoles, themes, defaultTheme)).toEqual(AppConstants.DEFAULT_USER_THEME);
+    });
+
+  });
+
+  describe('setAppHeaderProperties()', () => {
+
+    it('should take a theme and update the app header properties.', () => {
+
+      const defaultTheme = AppConstants.DEFAULT_USER_THEME;
+
+      component.setAppHeaderProperties(defaultTheme);
+
+      expect(component.appHeaderTitle).toBe(AppConstants.DEFAULT_USER_THEME.appTitle);
+      expect(component.navItems).toBe(AppConstants.DEFAULT_USER_THEME.navigationItems);
+      expect(component.userNav).toBe(AppConstants.DEFAULT_USER_THEME.accountNavigationItems);
+      expect(component.backgroundColor).toBe(AppConstants.DEFAULT_USER_THEME.backgroundColor);
+      expect(component.logoType).toBe(AppConstants.DEFAULT_USER_THEME.logoType);
+      expect(component.logoIsUsed).toBe(AppConstants.DEFAULT_USER_THEME.logoIsUsed);
+    });
+  });
+
+  describe('onNavigate()', () => {
+
+    it('should logout when onNavigate sign-out is called ', () => {
+      component.onNavigate('anything');
+      expect(dispatchSpy).toHaveBeenCalledTimes(0);
+      component.onNavigate('sign-out');
+      expect(dispatchSpy).toHaveBeenCalledWith(new fromActions.Logout());
+    });
   });
 });

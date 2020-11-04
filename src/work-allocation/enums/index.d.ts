0218189646c0aedef2a1218c8383942f0e680ce6
paul-graham <73112256+paul-graham@users.noreply.github.com>
2020-11-04 17:35:33 +0000

    Task models (#691)
    * Task models
    
    EUI-2842. Created the interfaces for the task models within the work-allocation area, along with appropriate enumerations. The only thing that needed some unit tests is the TaskConfig class.
    
    * Linting
    
    EUI-2842. Uday pointed out that I clearly hadn't run yarn lint. Apologies. Sorted out the various things it flagged up now.
    
    Co-authored-by: Paul Graham <paul@smartcasual.io>

---
+++ src/work-allocation/enums/index.d.ts
@@ -0,0 +1,11 @@
+import { TaskFieldType } from './task-field-type';
+import { TaskService } from './task-service';
+import { TaskSort } from './task-sort';
+import { TaskView } from './task-view';
+
+export {
+  TaskFieldType,
+  TaskService,
+  TaskSort,
+  TaskView
+};
---
+++ src/work-allocation/models/tasks/index.ts
@@ -0,0 +1,15 @@
+import InvokedTaskAction from './invoked-task-action.model';
+import TaskAction from './task-action.model';
+import TaskConfig from './task-config.model';
+import TaskFieldConfig from './task-field-config.model';
+import TaskServiceConfig from './task-service-config.model';
+import Task from './task.model';
+
+export {
+  InvokedTaskAction,
+  Task,
+  TaskAction,
+  TaskConfig,
+  TaskFieldConfig,
+  TaskServiceConfig
+};

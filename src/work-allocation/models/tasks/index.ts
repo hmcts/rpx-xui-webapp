4da0c186dfb090727363f4676788830c54836b03
udaydenduluri33 <50869558+udaydenduluri33@users.noreply.github.com>
2020-11-06 14:41:31 +0000

    task DTOs (#693)
    Co-authored-by: Uday Denduluri <uday.denduluri@gmail.com>

--- src/work-allocation/models/tasks/index.ts
+++ src/work-allocation/models/tasks/index.ts
@@ -1,6 +1,6 @@
+import JsonConfig from '../json-config';
 import InvokedTaskAction from './invoked-task-action.model';
 import TaskAction from './task-action.model';
-import TaskConfig from './task-config.model';
 import TaskFieldConfig from './task-field-config.model';
 import TaskServiceConfig from './task-service-config.model';
 import Task from './task.model';
@@ -9,7 +9,7 @@ export {
   InvokedTaskAction,
   Task,
   TaskAction,
-  TaskConfig,
+  JsonConfig,
   TaskFieldConfig,
   TaskServiceConfig
 };

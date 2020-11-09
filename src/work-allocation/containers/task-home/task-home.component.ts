411d9c8d8d2e9f7e7c96380c9982cb48a021cac3
Philip <philip.aberneithy@gmail.com>
2020-11-09 15:38:06 +0000

    Lift Tasks and Config mocks outside of the TaskListComponent.

--- src/work-allocation/containers/task-home/task-home.component.ts
+++ src/work-allocation/containers/task-home/task-home.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Task, TaskFieldConfig} from '../../models/tasks';
+import {TaskFieldType} from '../../enums';
 
 @Component({
   selector: 'exui-task-home',
@@ -7,11 +9,123 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 })
 export class TaskHomeComponent implements OnInit, OnDestroy {
 
+  /**
+   * Temp Task List
+   */
+  public tasks: Task[] = [
+    {
+      id: '1549476532065586',
+      caseReference: '1549 4765 3206 5586',
+      caseName: 'Kili Muso',
+      caseCategory: 'Protection',
+      location: 'Taylor House',
+      taskName: 'Review respondent evidence',
+      dueDate: new Date(628021800000),
+      actions: [
+        {
+          id: 'actionId',
+          title: 'actionTitle',
+        }
+      ]
+    },
+    {
+      id: '1549476532065586',
+      caseReference: '1549 4765 3206 5586',
+      caseName: 'Mankai Lit',
+      caseCategory: 'Revocation',
+      location: 'Taylor House',
+      taskName: 'Review appellant case',
+      dueDate: new Date(628021800000),
+      actions: [
+        {
+          id: 'actionId',
+          title: 'actionTitle',
+        }
+      ]
+    }
+  ];
+
+  /**
+   * Mock TaskFieldConfig[]
+   *
+   * Fields is the property of the TaskFieldConfig[], containing the configuration
+   * for the fields as returned by the API.
+   *
+   * The sorting will handled by this component, via the
+   * WP api as this component.
+   *
+   * TODO: Having a strange issue here where the compiler can't resolve,
+   * TaskView.TASK_LIST
+   */
+  public fields: TaskFieldConfig[] = [
+    {
+      name: 'id',
+      type: 'id' as TaskFieldType,
+      columnLabel: 'Id',
+      views: 0x01,
+    },
+    {
+      name: 'caseReference',
+      type: 'caseReference' as TaskFieldType,
+      columnLabel: 'Case reference',
+      views: 0x01,
+    },
+    {
+      name: 'caseName',
+      type: 'caseName' as TaskFieldType,
+      columnLabel: 'Case name',
+      views: 0x01,
+    },
+    {
+      name: 'caseCategory',
+      type: 'caseCategory' as TaskFieldType,
+      columnLabel: 'Case category',
+      views: 0x01,
+    },
+    {
+      name: 'location',
+      type: 'location' as TaskFieldType,
+      columnLabel: 'Location',
+      views: 0x01,
+    },
+    {
+      name: 'taskName',
+      type: 'taskName' as TaskFieldType,
+      columnLabel: 'Task',
+      views: 0x01,
+    },
+    {
+      name: 'dueDate',
+      type: 'dueDate' as TaskFieldType,
+      columnLabel: 'Due Dated',
+      views: 0x01,
+    },
+    {
+      name: 'actions',
+      type: 'actions' as TaskFieldType,
+      columnLabel: 'Actions',
+      views: 0x01,
+    }
+  ]
+
   constructor() {
   }
 
   public ngOnInit(): void {
     console.log('onInitTaskHome');
+  }
+
+  /**
+   * We need to sort the Task List based on the fieldName.
+   *
+   * Following on from this function we will need to retrieve the sorted tasks from
+   * the WA Api, once we have these then we need to set the tasks and fields, and pass
+   * these into the TaskListComponent.
+   *
+   * @param fieldname - ie. 'caseName'
+   */
+  public onSortHandler(fieldName: string): void {
+    console.log(fieldName);
   }
 
   public ngOnDestroy(): void {
--- src/work-allocation/containers/task-list/task-list.component.ts
+++ src/work-allocation/containers/task-list/task-list.component.ts
@@ -1,112 +1,6 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {BehaviorSubject, Observable, of} from 'rxjs';
-import {TaskFieldType, TaskView} from '../../enums';
 import {Task, TaskFieldConfig} from '../../models/tasks';
-
-/**
- * Fields is the property of the TaskFieldConfig[], containing the configuration
- * for the fields as returned by the API.
- *
- * TODO: Fields should be an Input into this component.
- * TODO: I believe that the parent component will take in TaskServiceConfig,
- * and this component will receive TaskFieldConfig[], to set the fields.
- *
- * I believe that sorting will handled by the parent component, via the
- * WP api as this component should only be responsible for the displaying
- * of the task list.
- *
- * TODO: Having a strange issue here where the compiler can't resolve,
- * TaskView.TASK_LIST
- */
-const fields: TaskFieldConfig[] = [
-  {
-    name: 'id',
-    type: 'id' as TaskFieldType,
-    columnLabel: 'Id',
-    views: 0x01,
-  },
-  {
-    name: 'caseReference',
-    type: 'caseReference' as TaskFieldType,
-    columnLabel: 'Case reference',
-    views: 0x01,
-  },
-  {
-    name: 'caseName',
-    type: 'caseName' as TaskFieldType,
-    columnLabel: 'Case name',
-    views: 0x01,
-  },
-  {
-    name: 'caseCategory',
-    type: 'caseCategory' as TaskFieldType,
-    columnLabel: 'Case category',
-    views: 0x01,
-  },
-  {
-    name: 'location',
-    type: 'location' as TaskFieldType,
-    columnLabel: 'Location',
-    views: 0x01,
-  },
-  {
-    name: 'taskName',
-    type: 'taskName' as TaskFieldType,
-    columnLabel: 'Task',
-    views: 0x01,
-  },
-  {
-    name: 'dueDate',
-    type: 'dueDate' as TaskFieldType,
-    columnLabel: 'Due Dated',
-    views: 0x01,
-  },
-  {
-    name: 'actions',
-    type: 'actions' as TaskFieldType,
-    columnLabel: 'Actions',
-    views: 0x01,
-  }
-]
-
-/**
- * TODO: Should take in tasks from outside datasource.
- * TODO: Should be passed into this Task List Component
- *
- * Not part of EUI-2844
- */
-const tasks: Task[] = [
-  {
-    id: '1549476532065586',
-    caseReference: '1549 4765 3206 5586',
-    caseName: 'Kili Muso',
-    caseCategory: 'Protection',
-    location: 'Taylor House',
-    taskName: 'Review respondent evidence',
-    dueDate: new Date(628021800000),
-    actions: [
-      {
-        id: 'actionId',
-        title: 'actionTitle',
-      }
-    ]
-  },
-  {
-    id: '1549476532065586',
-    caseReference: '1549 4765 3206 5586',
-    caseName: 'Mankai Lit',
-    caseCategory: 'Revocation',
-    location: 'Taylor House',
-    taskName: 'Review appellant case',
-    dueDate: new Date(628021800000),
-    actions: [
-      {
-        id: 'actionId',
-        title: 'actionTitle',
-      }
-    ]
-  }
-];
 
 @Component({
   selector: 'exui-task-list',
@@ -115,22 +9,18 @@ const tasks: Task[] = [
 })
 export class TaskListComponent implements OnInit {
 
-  @Output() public sortColumn = new EventEmitter<string>();
-  // You can use this as a single source of truth that gets mapped to other obserables.
-  // Use a single data source using BehavoirSubject
-  private tasks$ = new BehaviorSubject(tasks);
+  @Input() public tasks: Task[];
+  @Input() public fields: TaskFieldConfig[];
+  @Output() public sortByFieldName = new EventEmitter<string>();
 
-  // TODO: SHould we use a BehaviorSubject for fields?
+  private tasks$;
 
   /**
    * The datasource is an Observable of data to be displayed, as per LLD.
    */
   public dataSource$: Observable<Task[]>;
 
-  // array of column names
   public displayedColumns;
-
-  public fields = fields;
 
   constructor() {
   }
@@ -139,9 +29,10 @@ export class TaskListComponent implements OnInit {
   // TODO: Consider renaming dataSource$
   public ngOnInit() {
 
+    this.tasks$ = new BehaviorSubject(this.tasks);
     this.dataSource$ = this.tasks$;
 
-    this.displayedColumns = this.getDisplayedColumn(fields);
+    this.displayedColumns = this.getDisplayedColumn(this.fields);
   }
 
   /**
@@ -167,6 +58,6 @@ export class TaskListComponent implements OnInit {
    */
   public sort(fieldName: string) {
 
-    this.sortColumn.emit(fieldName);
+    this.sortByFieldName.emit(fieldName);
   }
 }

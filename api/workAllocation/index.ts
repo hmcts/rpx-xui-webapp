e4cea4180c40428abeaf7415db7bf133fcf81cf1
udaydenduluri33 <50869558+udaydenduluri33@users.noreply.github.com>
2020-11-10 15:30:59 +0000

    Feature/eui 2886 pact stub server ud (#701)
    * Enabling ECM services to live (#689)
    
    EUI-2685
    
    * initial implementation for gtm
    
    * initial POC for PACT stub server
    
    * pact mock server implementation for workAllocation
    
    * node runtime changes
    
    * new line
    
    * fix the lint error
    
    Co-authored-by: Mo-Lala84 <49687052+Mo-Lala84@users.noreply.github.com>
    Co-authored-by: Pardeep Singh Basi <paddybasi@gmail.com>
    Co-authored-by: Uday Denduluri <uday.denduluri@gmail.com>

---
+++ api/workAllocation/index.ts
@@ -0,0 +1,26 @@
+import * as express from 'express'
+import { EnhancedRequest } from '../lib/models'
+import { handleTaskGet } from './taskService'
+
+const baseUrl: string = 'http://localhost:8080'
+
+/**
+ * getPayments
+ */
+export async function getTask(req: EnhancedRequest, res: express.Response, next: express.NextFunction) {
+
+  try {
+        const getTaskPath: string = prepareGetTaskUrl(baseUrl, req.params.taskId)
+
+        const jsonResponse = await handleTaskGet(getTaskPath, req)
+        res.status(200)
+        res.send(jsonResponse)
+  } catch (error) {
+    next(error)
+  }
+
+}
+
+export function prepareGetTaskUrl(url: string, taskId: string): string {
+    return `${url}/task/${taskId}`
+}
---
+++ api/workAllocation/taskService.ts
@@ -0,0 +1,14 @@
+import {AxiosResponse} from 'axios'
+import {http} from '../lib/http'
+import * as log4jui from '../lib/log4jui'
+import { EnhancedRequest, JUILogger } from "../lib/models"
+import { setHeaders } from '../lib/proxy'
+
+const logger: JUILogger = log4jui.getLogger('task-service')
+
+export async function handleTaskGet(path: string, req: EnhancedRequest): Promise<any> {
+    logger.info('getting tasks for', path)
+    const headers = setHeaders(req)
+    const response: AxiosResponse = await http.get(path, { headers })
+    return response.data
+}

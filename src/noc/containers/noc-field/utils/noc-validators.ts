ce4271df35825fd234bf8ca062ddb3f1db7767e2
Raymond Liang <ray.liang@atos.net>
2020-11-11 16:33:00 +0000

    allow lowercase in postcode

--- src/noc/containers/noc-field/utils/noc-validators.ts
+++ src/noc/containers/noc-field/utils/noc-validators.ts
@@ -15,7 +15,7 @@ export class NocValidators {
   public static postcodeValidator(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
       if (control.value === null || control.value === '') { return; }
-      if (!control.value.toString().match(/^(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$/)) {
+      if (!control.value.toString().match(/^(([A-Za-z]{1,2}[0-9][A-Za-z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?[0-9][A-Za-z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Za-z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$/)) {
         return { postcode: true };
       }
       return;

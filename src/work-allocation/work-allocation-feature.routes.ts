41fa988d83a44b1b46f34c96d2b807e1d9bffc55
Uday Denduluri <uday.denduluri@gmail.com>
2020-11-16 13:29:25 +0000

    workallocation feature toggle

--- src/work-allocation/work-allocation-feature.routes.ts
+++ src/work-allocation/work-allocation-feature.routes.ts
@@ -4,26 +4,29 @@ import { HealthCheckGuard } from 'src/app/shared/guards/health-check.guard';
 import { TaskHomeComponent } from './containers/task-home/task-home.component';
 import { TaskListComponent } from './containers/task-list/task-list.component';
 import { TaskManagerComponent } from './containers/task-manager/task-manager.component';
+import { WorkAllocationFeatureToggleGuard } from './guards/work-allocation-feature-toggle.guard';
 
 export const ROUTES: Routes = [
     {
       path: '',
-      component: TaskHomeComponent
+      component: TaskHomeComponent,
+      canActivate: [ HealthCheckGuard, WorkAllocationFeatureToggleGuard ],
     },
     // TODO: We probably don't need the following routes,
     // as these components parents should be TaskHomeComponent.
     // to discuss.
     {
       path: 'task-list',
-      component: TaskListComponent
+      component: TaskListComponent,
+      canActivate: [WorkAllocationFeatureToggleGuard]
     },
     {
       path: 'task-manager',
       component: TaskManagerComponent,
-      canActivate: [ HealthCheckGuard ],
+      canActivate: [ HealthCheckGuard, WorkAllocationFeatureToggleGuard ],
       data: {
         title: 'HMCTS Manage WorkAllocation | Task Manager'
-      }
+      },
     }
    ];
 

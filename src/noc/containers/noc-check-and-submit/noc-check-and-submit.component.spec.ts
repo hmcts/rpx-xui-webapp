8aa197482bb910711ab56cee0622c7ce16ad0da3
Lam <daniel.2.lam@atos.net>
2020-11-17 13:30:18 +0000

    More unit test fixes

--- src/noc/containers/noc-check-and-submit/noc-check-and-submit.component.spec.ts
+++ src/noc/containers/noc-check-and-submit/noc-check-and-submit.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
 import { NocAnswer } from '../../models';
@@ -9,6 +10,7 @@ describe('NocCheckAndSubmitComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
       declarations: [ NocCheckAndSubmitComponent ]
     })
     .compileComponents();

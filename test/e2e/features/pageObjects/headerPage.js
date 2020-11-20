6b89eb431729607101e118b82fc7a022e71d827d
sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
2020-11-20 23:29:03 +0000

    ng integration tests added.
    (cherry picked from commit c81f02a618587129ebbf7e7f7ab08ea94756c7e4)

--- test/e2e/features/pageObjects/headerPage.js
+++ test/e2e/features/pageObjects/headerPage.js
@@ -6,6 +6,7 @@ var BrowserWaits = require('../../support/customWaits');
 
 function HeaderPage() {
 
+    this.primaryNavBar = element(by.css("nav.hmcts-primary-navigation ul"));
     this.manageCases = element(by.css(".hmcts-header .hmcts-header__link"));
     this.caseList = element(by.xpath("//a[contains(text(),'Case list')]"));
     this.createCase = element(by.xpath("//li/a[contains(text(),'Create case')]"));
@@ -42,6 +43,20 @@ function HeaderPage() {
   this.clickSignOut = function () {
     this.signOut.click();
     browser.sleep(SHORT_DELAY);
+  };
+
+  this.isTabPresent = async function (tabDisplatText) {
+
+    return await this.getTabElementWithText(tabDisplatText).isPresent();
+  };
+
+  this.waitForPrimaryNavDisplay = async function () {
+    await BrowserWaits.waitForElement(this.primaryNavBar);
+  };
+
+
+  this.getTabElementWithText = function (tabText) {
+    return this.primaryNavBar.element(by.xpath('//a[contains(text(),"' + tabText + '")]'));
   };
 }
 
--- test/nodeMock/app.js
+++ test/nodeMock/app.js
@@ -1,15 +1,18 @@
 
 
 const express = require('express');
+var bodyParser = require('body-parser');
+
 let { requestMapping,configurations} = require('./reqResMapping');
 const { browser } = require('protractor');
+const CCDCaseConfig = require('./ccd/ccdCaseConfig/caseCreateConfigGenerator')
 const port = 3001;
 
 
 class MockApp{
     init(){
-        this.conf = {
-            get: { ...requestMapping.get },
+        this.conf = { 
+            get: { ...requestMapping.get},
             post: { ...requestMapping.post },
             put: { ...requestMapping.put },
             delete: { ...requestMapping.delete }
@@ -21,7 +24,8 @@ class MockApp{
 
     async startServer(){
         const app = express();
-        app.use(express.json()) 
+        app.use(bodyParser.urlencoded({ extended: false }));
+        app.use(bodyParser.json());
         for (const [key, value] of Object.entries(this.conf.get)) {
             app.get(key, value);
         }
@@ -45,12 +49,16 @@ class MockApp{
     }
 
     async stopServer(){
-        await this.server.close();
+        if (this.server){
+            await this.server.close();
+            this.server = null;
+        }else{
+            console.log("Mock server is null or undefined");
+        }
         this.conf = {  };
         this.configurations = { };
     }
 
-   
     onGet(path, callback){
         this.conf.get[path] = callback; 
     }
@@ -76,7 +84,63 @@ class MockApp{
 
 const mockInstance = new MockApp();
 
-// mockInstance.init();;
-// mockInstance.startServer();
 
 module.exports = mockInstance;
+
+
+// mockInstance.init();
+// createCustomCCDCaseConfig();
+// mockInstance.startServer();
+
+
+
+function createCustomCCDCaseConfig(){
+    // const scenario = { fieldType: "Collection", showField: false, retainHiddenField: false };
+
+    const scenario = { fieldType: "Document", showField: false, retainHiddenField: true };
+
+    const caseConfig = new CCDCaseConfig('TEST_CaseType', 'Test case type hidden field retain value', 'test description');
+    const page1 = caseConfig.addWizardPage('HiddenFieldPage_1', 'Hidden field retain value test page');
+
+    let testFieldShowYesNo = caseConfig.addCCDFieldToPage(page1, "YesOrNo", "showTestField", "Show Test Field?");
+    testFieldShowYesNo.value = true;
+
+    let complexField = caseConfig.addCCDFieldToPage(page1, scenario.fieldType, "complexField", "Complex field");
+    complexField.value = {
+            "document_url": "http://dm-store-aat.service.core-compute-aat.internal/documents/a612199d-9972-4b99-b653-5ec7c310e21a",
+            "document_binary_url": "http://dm-store-aat.service.core-compute-aat.internal/documents/a612199d-9972-4b99-b653-5ec7c310e21a/binary",
+            "document_filename": "Redacted-dm-store135044941749889827327305460282199740737.pdf"
+    }; 
+    complexField.retain_hidden_value = scenario.retainHiddenField;
+    complexField.show_condition = `${testFieldShowYesNo.id}=\"Yes\"`;
+
+    setUpcaseConfig(caseConfig.caseConfigTemplate);
+}
+
+
+function setUpcaseConfig(caseConfig) {
+    mockInstance.onGet('/data/internal/cases/:caseid/event-triggers/:eventId', (req, res) => {
+        res.send(caseConfig);
+    });
+
+    mockInstance.onPost('/data/case-types/:caseType/validate', (req, res) => {
+        caseValidationRequestBody = req.body;
+        const responseBody = {
+            data: req.body.data,
+            "_links": { "self": { "href": "http://ccd-data-store-api-aat.service.core-compute-demo.internal" + req.path + "?pageId=" + req.query.pageId } }
+        }
+        res.send(responseBody);
+    });
+
+    mockInstance.onPost('/data/cases/:caseid/events', (req, res) => {
+        caseEventSubmitRequestBody = req.body;
+        const responseBody = {
+            id: Date.now(),
+            data: req.body.data,
+            "_links": { "self": { "href": "http://ccd-data-store-api-demo.service.core-compute-demo.internal" + req.path + "?ignore-warning=false" } }
+        }
+        res.send(responseBody)
+    });
+
+}
+

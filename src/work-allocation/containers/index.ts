666b3d5648b89031f163b0a22a6f68db93ea57be
Paul Graham <paul@smartcasual.io>
2020-12-02 11:52:04 +0000

    Update index.ts
    Needed to add TaskListWrapperComponent to the module. This is only showing up as an issue in a prod build.

--- src/work-allocation/containers/index.ts
+++ src/work-allocation/containers/index.ts
@@ -2,19 +2,22 @@ import { AvailableTasksComponent } from './available-tasks/available-tasks.compo
 import { MyTasksComponent } from './my-tasks/my-tasks.component';
 import { TaskAssignmentContainerComponent } from './task-assignment/task-assignment-container.component';
 import { TaskHomeComponent } from './task-home/task-home.component';
+import { TaskListWrapperComponent } from './task-list-wrapper/task-list-wrapper.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskManagerComponent } from './task-manager/task-manager.component';
 import { WorkAllocationHomeComponent } from './work-allocation-home/work-allocation-home.component';
 
 export const containers: any[] = [
-  AvailableTasksComponent, MyTasksComponent, TaskHomeComponent, TaskListComponent,
-  TaskManagerComponent, TaskAssignmentContainerComponent, WorkAllocationHomeComponent
+  AvailableTasksComponent, MyTasksComponent, TaskAssignmentContainerComponent,
+  TaskHomeComponent, TaskListComponent, TaskListWrapperComponent, TaskManagerComponent,
+  WorkAllocationHomeComponent
 ];
 
 export * from './available-tasks/available-tasks.component';
 export * from './my-tasks/my-tasks.component';
 export * from './task-home/task-home.component';
 export * from './task-list/task-list.component';
+export * from './task-list-wrapper/task-list-wrapper.component';
 export * from './task-manager/task-manager.component';
 export * from './task-assignment/task-assignment-container.component';
 export * from './work-allocation-home/work-allocation-home.component';

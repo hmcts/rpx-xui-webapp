7ba7492a5c1e9732fae3c387d2f6dad0037f2c29
Pardeep Singh Basi <paddybasi@gmail.com>
2020-12-04 17:34:19 +0000

    caching jurisdictions into session

--- api/amendedJurisdictions/index.spec.ts
+++ api/amendedJurisdictions/index.spec.ts
@@ -3,7 +3,7 @@ import { expect } from 'chai'
 import 'mocha'
 import * as sinon from 'sinon'
 import * as sinonChai from 'sinon-chai'
-import { mockReq } from 'sinon-express-mock'
+import { mockReq, mockRes } from 'sinon-express-mock'
 import * as amendedJurisdictions from './index'
 
 chai.use(sinonChai)
@@ -13,12 +13,30 @@ describe('Amended Jurisdiction', () => {
   let sandbox
   let result0
   let result1
+  let res
   let req
+  let proxyRes
 
   beforeEach(() => {
     sandbox = sinon.createSandbox()
 
-    req = mockReq()
+    proxyRes = {}
+    res = mockRes()
+    req = mockReq({
+      baseUrl: '/api/documents/',
+      cookies: [],
+      headers: {
+          'accept': '*/*',
+          'content-type': 'text/test',
+          'experimental': 'experiment/test',
+      },
+      session: {
+          save: fun => {
+              fun()
+          },
+      },
+      url: 'fdafu4543543/binary',
+  })
 
     result0 = {
         data: [
@@ -50,15 +68,15 @@ describe('Amended Jurisdiction', () => {
         },
     ]
 
-    const response = amendedJurisdictions.getJurisdictions(req, expected)
+    const response = amendedJurisdictions.getJurisdictions(proxyRes, req, res, expected)
     expect(response).to.eql(expected)
   })
 
   it('should send empty array', () => {
     const expected = []
 
-      const response = amendedJurisdictions.getJurisdictions(req, expected)
-      expect(response).to.eql(expected)
+    const response = amendedJurisdictions.getJurisdictions(proxyRes, req, res, expected)
+    expect(response).to.eql(expected)
   })
 
 })

e0899656ebb1649e13b90ceb510988966d177aa4
Pardeep Singh Basi <paddybasi@gmail.com>
2020-12-04 19:12:44 +0000

    Hotfix/e UI 2995 performance and ie11 fix (#774)
    * refactored the api to take more advantage of tcp proxy tunnel
    
    * merged from master. Updated to update workAllocation as proxy
    
    * merged from master. Updated to update workAllocation as proxy
    
    * fixed open/closed routes
    
    * fixed open/closed routes
    
    * proxy websocket requests
    
    * fixing functional test issues
    
    * fixed postcode lookup
    
    * fixed work allocation route
    
    * fixed work allocation route
    
    * fixed work allocation tests
    
    * Revert "fixed work allocation route"
    
    This reverts commit b3b76e51
    
    * fixed work allocation tests
    
    * fixed typo
    
    * fixed pact test failure
    
    * fixing proxy issues and IE11 fix
    
    * fixed lint issues and introduced middlewares for proxy
    
    * fixed lint errors
    
    * now proxying searchCases too
    
    * now proxying searchCases too
    
    * caching jurisdictions into session
    
    * classlist prototype
    
    * fix issue with prototype
    
    * caching jurisdictions into session
    
    * reverting polyfills.ts
    
    Co-authored-by: leepavlouhmcts <lefkos.hadjiPavlou@HMCTS.NET>

--- api/application.ts
+++ api/application.ts
@@ -2,114 +2,46 @@ import * as bodyParser from 'body-parser'
 import * as cookieParser from 'cookie-parser'
 import * as express from 'express'
 import * as helmet from 'helmet'
-// import {router as termsAndCRoutes} from './termsAndConditions/routes'
-// import {router as userTandCRoutes} from './userTermsAndConditions/routes'
 import {getXuiNodeMiddleware} from './auth'
-import { router as caseShareRoutes } from './caseshare/routes'
 import { getConfigValue, showFeature } from './configuration'
 import {
-    APP_INSIGHTS_KEY,
     FEATURE_HELMET_ENABLED,
-    HELMET,
-    PROTOCOL,
-    SERVICES_CCD_COMPONENT_API_PATH,
-    SERVICES_DOCUMENTS_API_PATH, SERVICES_ICP_API_URL, SESSION_SECRET,
+    HELMET, PROTOCOL,
+    SESSION_SECRET,
 } from './configuration/references'
-import {router as emAnnoRouter} from './emAnno/routes'
 import * as health from './health'
-import healthCheck from './healthCheck'
 import * as log4jui from './lib/log4jui'
-import authInterceptor from './lib/middleware/auth'
-import {applyProxy} from './lib/middleware/proxy'
 import {JUILogger} from './lib/models'
 import * as tunnel from './lib/tunnel'
 import openRoutes from './openRoutes'
-import {router as paymentsRouter} from './payments/routes'
-import * as postCodeLookup from './postCodeLookup'
-import {router as markupRouter} from './redaction/markupRoutes'
-import {router as redactionRouter} from './redaction/redactionRoutes'
+import {initProxy} from './proxy.config'
 import routes from './routes'
-import userRouter from './user/routes'
-import taskRouter from './workAllocation/routes'
 
 export const app = express()
-if (showFeature(FEATURE_HELMET_ENABLED)) {
-    app.use(helmet(getConfigValue(HELMET)))
-}
-
-// app.use(errorStack)
-app.use(bodyParser.json({limit: '5mb'}))
-app.use(bodyParser.urlencoded({limit: '5mb', extended: true}))
-app.use(cookieParser(getConfigValue(SESSION_SECRET)))
-
-app.use(getXuiNodeMiddleware())
-tunnel.init()
 
 /**
  * Add Reform Standard health checks.
  */
 health.addReformHealthCheck(app)
 
+if (showFeature(FEATURE_HELMET_ENABLED)) {
+    app.use(helmet(getConfigValue(HELMET)))
+}
+
+app.use(cookieParser(getConfigValue(SESSION_SECRET)))
+
+// TODO: remove tunnel and configurations
+tunnel.init()
+app.use(getXuiNodeMiddleware())
+
+// applyProxy needs to be used before bodyParser
+initProxy(app)
+
+app.use(bodyParser.json({limit: '5mb'}))
+app.use(bodyParser.urlencoded({limit: '5mb', extended: true}))
+
 app.use('/external', openRoutes)
-
-app.get('/api/addresses', authInterceptor, postCodeLookup.doLookup)
-
-app.get('/api/monitoring-tools', (req, res) => {
-    res.send({key: getConfigValue(APP_INSIGHTS_KEY)})
-})
-app.use('/api/user', userRouter)
-app.use('/api/healthCheck', healthCheck)
-/*if (showFeature(FEATURE_TERMS_AND_CONDITIONS_ENABLED)) {
-    app.use('/api/userTermsAndConditions', userTandCRoutes)
-    app.use('/api/termsAndConditions', termsAndCRoutes)
-}*/
-
-app.get('/api/configuration', (req, res) => {
-    res.send(showFeature(req.query.configurationKey as string))
-})
-
-// TODO: move to proxy route as below
-app.use('/api/markups', markupRouter)
-app.use('/api/redaction', redactionRouter)
-
-app.use('/api/caseshare', caseShareRoutes)
-
-// TODO: move these to proxy routes as well
-app.use('/aggregated', routes)
-app.use('/data', routes)
-
-applyProxy(app, {
-    rewrite: false,
-    source: '/documents',
-    target: getConfigValue(SERVICES_DOCUMENTS_API_PATH),
-})
-
-applyProxy(app, {
-    rewrite: false,
-    source: '/print',
-    target: getConfigValue(SERVICES_CCD_COMPONENT_API_PATH),
-})
-
-applyProxy(app, {
-    rewrite: false,
-    source: '/icp',
-    target: getConfigValue(SERVICES_ICP_API_URL),
-})
-
-// TODO: works when getting annotation-set but not when creating one?
-/*applyProxy(app, {
-    rewrite: true,
-    rewriteUrl: '/api',
-    source: '/em-anno',
-    target: getConfigValue(SERVICES_EM_ANNO_API_URL),
-})*/
-
-// TODO: move to proxy route as above
-app.use('/em-anno', emAnnoRouter)
-
-app.use('/payments', paymentsRouter)
-
-app.use('/workallocation', taskRouter)
+app.use('/api', routes)
 
 // @ts-ignore
 const logger: JUILogger = log4jui.getLogger('Application')
--- api/caseshare/routes.ts
+++ api/caseshare/routes.ts
@@ -1,11 +1,9 @@
 import * as express from 'express'
-import authInterceptor from '../lib/middleware/auth'
 import { handleGetOrganisationsRoute as organisationsRoute } from '../organisations/index'
 import * as restAPI from './index'
 
 export const router = express.Router({ mergeParams: true })
 
-router.use(authInterceptor)
 router.get('/orgs', organisationsRoute)
 router.get('/users', restAPI.getUsers)
 router.get('/cases', restAPI.getCases)

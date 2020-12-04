7a4c49b4fbb0b6b53f4a81dbeb6906cd9d44b68c
Pardeep Singh Basi <paddybasi@gmail.com>
2020-12-04 18:06:00 +0000

    reverting polyfills.ts

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,128 +18,50 @@
  * BROWSER POLYFILLS
  */
 
- /** IE9, IE10 and IE11 requires all of the following polyfills. */
- import 'core-js/es6/symbol';
- import 'core-js/es6/object';
- import 'core-js/es6/function';
- import 'core-js/es6/parse-int';
- import 'core-js/es6/parse-float';
- import 'core-js/es6/number';
- import 'core-js/es6/math';
- import 'core-js/es6/string';
- import 'core-js/es6/date';
- import 'core-js/es6/array';
- import 'core-js/es6/regexp';
- import 'core-js/es6/map';
- import 'core-js/es6/weak-map';
- import 'core-js/es6/set';
- import 'core-js/es6/promise';
- import 'core-js/fn/promise/finally';
- import 'core-js/es7/object';
- import 'core-js/es7/array';
+/** IE10 and IE11 requires the following for NgClass support on SVG elements */
+import 'classlist.js';  // Run `npm install --save classlist.js`.
 
- /** IE10 and IE11 requires the following for NgClass support on SVG elements */
- import 'classlist.js';  // Run `npm install --save classlist.js`.
+/* IE9, IE10 and IE11 require all of the following polyfills. **/
+import 'isomorphic-fetch';  // Run `yarn add isomorphic-fetch`.
 
- /* Evergreen browsers require these. **/
- import 'core-js/es6/reflect';
- import 'core-js/es7/reflect';
+/**
+ * Web Animations `@angular/platform-browser/animations`
+ * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
+ * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
+ */
+import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
- /**
-  * Web Animations `@angular/platform-browser/animations`
-  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
-  * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
-  * IE10 and IE11 requires the following to support `@angular/animation`.
-  */
- import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+/**
+ * By default, zone.js will patch all possible macroTask and DomEvents
+ * user can disable parts of macroTask/DomEvents patch by setting following flags
+ * because those flags need to be set before `zone.js` being loaded, and webpack
+ * will put import in the top of bundle, so user need to create a separate file
+ * in this directory (for example: zone-flags.ts), and put the following flags
+ * into that file, and then add the following code before importing zone.js.
+ * import './zone-flags.ts';
+ *
+ * The flags allowed in zone-flags.ts are listed here.
+ *
+ * The following flags will work for all browsers.
+ *
+ * (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
+ * (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
+ * (window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
+ *
+ *  in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
+ *  with the following flag, it will bypass `zone.js` patch for IE/Edge
+ *
+ *  (window as any).__Zone_enable_cross_context_check = true;
+ *
+ */
 
- /* IE9, IE10 and IE11 require all of the following polyfills. **/
- import 'isomorphic-fetch';  // Run `yarn add isomorphic-fetch`.
+/***************************************************************************************************
+ * Zone JS is required by default for Angular itself.
+ */
+import 'zone.js/dist/zone';  // Included with Angular CLI.
 
- /**
-  * By default, zone.js will patch all possible macroTask and DomEvents
-  * user can disable parts of macroTask/DomEvents patch by setting following flags
-  * because those flags need to be set before `zone.js` being loaded, and webpack
-  * will put import in the top of bundle, so user need to create a separate file
-  * in this directory (for example: zone-flags.ts), and put the following flags
-  * into that file, and then add the following code before importing zone.js.
-  * import './zone-flags.ts';
-  *
-  * The flags allowed in zone-flags.ts are listed here.
-  *
-  * The following flags will work for all browsers.
-  *
-  * (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
-  * (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
-  * (window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
-  *
-  *  in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
-  *  with the following flag, it will bypass `zone.js` patch for IE/Edge
-  *
-  *  (window as any).__Zone_enable_cross_context_check = true;
-  *
-  */
 
- /***************************************************************************************************
-  * Zone JS is required by default for Angular itself.
-  */
- if (document['documentMode'] || /Edge/.test(navigator.userAgent)) {
-  (window as any).__Zone_enable_cross_context_check = true;
- }
- import 'zone.js/dist/zone';  // Included with Angular CLI.
- import 'zone.js/dist/zone-error';
-
- if (!Element.prototype.matches) {
-  Element.prototype.matches = (Element.prototype as any).msMatchesSelector || Element.prototype.webkitMatchesSelector;
- }
-
- if (!Object.getOwnPropertyDescriptor(Element.prototype,'classList')){
-  if (HTMLElement&&Object.getOwnPropertyDescriptor(HTMLElement.prototype,'classList')){
-      Object.defineProperty(Element.prototype,'classList',Object.getOwnPropertyDescriptor(HTMLElement.prototype,'classList'));
-  }
-}
-
-Object.defineProperty(Element.prototype, 'classList', {
-    get: function() {
-        let self = this, bValue = self.className.split(" ")
-
-        bValue.add = function (){
-            let b;
-            for(let i in arguments){
-                b = true;
-                for (let j = 0; j<bValue.length;j++)
-                    if (bValue[j] == arguments[i]){
-                        b = false
-                        break
-                    }
-                if(b)
-                    self.className += (self.className?" ":"")+arguments[i]
-            }
-        }
-        bValue.remove = function(){
-            self.className = ""
-            for(let i in arguments)
-                for (let j = 0; j<bValue.length;j++)
-                    if(bValue[j] != arguments[i])
-                        self.className += (self.className?" " :"")+bValue[j]
-        }
-        bValue.toggle = function(x){
-            let b;
-            if(x){
-                self.className = ""
-                b = false;
-                for (let j = 0; j<bValue.length;j++)
-                    if(bValue[j] != x){
-                        self.className += (self.className?" " :"")+bValue[j]
-                        b = false
-                    } else b = true
-                if(!b)
-                    self.className += (self.className?" ":"")+x
-            } else throw new TypeError("Failed to execute 'toggle': 1 argument required")
-            return !b;
-        }
-
-        return bValue; 
-    },
-    enumerable: false
-})
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
+import 'core-js/es7/reflect';

f43b30da22f9644c3d892acfa47bf448b7cab1e3
Alex McAusland <alex.mcausland@gmail.com>
2021-01-12 10:29:59 +0000

    add additional spec for non jurisdiction url

--- api/amendedJurisdictions/index.spec.ts
+++ api/amendedJurisdictions/index.spec.ts
@@ -35,7 +35,7 @@ describe('Amended Jurisdiction', () => {
               fun()
           },
       },
-      url: 'aggregated/caseworkers/:uid/jurisdictions?access=read',
+      url: 'fdafu4543543/binary',
   })
 
     result0 = {
@@ -61,12 +61,36 @@ describe('Amended Jurisdiction', () => {
     sandbox.restore()
   })
 
-  it('should send PROBATE array',  () => {
-    const expected = [
-        {
-            id: 'PROBATE',
-        },
+  it('should filter jurisdicitons for the jurisdictions endpoint',  () => {
+    const data = [
+      {
+        id: 'PROBATE',
+      },
+      {
+        id: 'RANDOM',
+      },
     ]
+    req.url = 'aggregated/caseworkers/:uid/jurisdictions?access=read'
+    const response = amendedJurisdictions.getJurisdictions(proxyRes, req, res, data)
+    // Unknown jurisdiction should be filtered
+    const expected = [
+      {
+        id: 'PROBATE',
+      },
+    ]
+    expect(response).to.eql(expected)
+  })
+
+  it('should not filter jurisdictions for non-jurisdictions endpoint',  () => {
+    const expected = [
+      {
+        id: 'PROBATE',
+      },
+      {
+        id: 'RANDOM',
+      },
+    ]
+    req.url = '/aggregated/some/other/url'
 
     const response = amendedJurisdictions.getJurisdictions(proxyRes, req, res, expected)
     expect(response).to.eql(expected)

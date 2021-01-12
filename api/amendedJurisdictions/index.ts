854a6297b326a28e3847d740c0a69c2408ac1e6c
Mo-Lala84 <49687052+Mo-Lala84@users.noreply.github.com>
2021-01-12 11:03:00 +0000

    checking whether the endpoint called is actually for jurisdictions inside
    EUI-3102

--- api/amendedJurisdictions/index.ts
+++ api/amendedJurisdictions/index.ts
@@ -1,22 +1,28 @@
 import {getConfigValue} from '../configuration'
 import { JURISDICTIONS } from '../configuration/references'
 
+const jurisdictions = /aggregated\/.+jurisdictions\?/
+
 /**
  * Manually filtering returned jurisdictions
  * to make available jurisdiction in filters array only
  */
 export const getJurisdictions = (proxyRes, req, res, data: any[]) => {
-    if (!Array.isArray(data)) {
+   if (!Array.isArray(data)
+        || !jurisdictions.test(req.url)) {
         return data
     }
     const filters = getConfigValue(JURISDICTIONS)
+  
     req.session.jurisdictions = [...data].filter(o => filters.includes(o.id))
     return req.session.jurisdictions
 }
 
 export const checkCachedJurisdictions = (proxyReq, req, res) => {
-    if (req.session.jurisdictions) {
+    if (jurisdictions.test(req.url)) {
+      if (req.session.jurisdictions) {
         res.send(req.session.jurisdictions)
         proxyReq.end()
+        }
     }
 }

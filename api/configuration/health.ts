e0969d81fbd0b0f2b197c4028e3f5547b70feddc
Lee Pavlou <leepavlou@MacBook-Pro.localdomain>
2021-01-20 22:53:30 +0000

    lint error fixed

--- api/configuration/health.ts
+++ api/configuration/health.ts
@@ -3,7 +3,6 @@ import {getConfigValue} from './index'
 import { SERVICES_CCD_COMPONENT_API_PATH } from './references'
 import { SERVICES_IDAM_LOGIN_URL } from './references'
 import { SERVICE_S2S_PATH } from './references'
-import { SERVICES_FEE_AND_PAY_API_PATH } from './references'
 import { SERVICES_TERMS_AND_CONDITIONS_URL } from './references'
 
 export const healthEndpoints = (): EnvironmentConfigServices => {
@@ -12,7 +11,6 @@ export const healthEndpoints = (): EnvironmentConfigServices => {
 
   return {
     ccdDataApi: getConfigValue(SERVICES_CCD_COMPONENT_API_PATH) + HEALTH,
-    // feeAndPayApi: getConfigValue(SERVICES_FEE_AND_PAY_API_PATH) + HEALTH,
     idamApi: getConfigValue(SERVICES_IDAM_LOGIN_URL) + HEALTH,
     s2s: getConfigValue(SERVICE_S2S_PATH) + HEALTH,
     termsAndConditions: getConfigValue(SERVICES_TERMS_AND_CONDITIONS_URL) + HEALTH,

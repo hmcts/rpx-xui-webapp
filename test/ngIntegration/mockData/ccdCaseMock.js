0b62028bb98d91bf9c301282e0ce3f3945e51801
sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
2021-01-26 13:49:30 +0000

    mock data for ccd cases added

---
+++ test/ngIntegration/mockData/ccdCaseMock.js
@@ -0,0 +1,56 @@
+const CCDCaseConfig = require('../../nodeMock/ccd/ccdCaseConfig/caseCreateConfigGenerator');
+
+function getTestJurisdiction(scenario) {
+    const job = {
+        id: "job", type: "Complex", label: "Job details",
+        complex_fields: [
+            { id: "Title", type: "Text", label: "Title" },
+            { id: "Description", type: "Text", label: "Description" },
+        ]
+    };
+
+    const personFields = [
+        { id: "Title", type: "Text", label: "Title" },
+        { id: "FirstName", type: "Text", label: "First Name" },
+        { id: "LastName", type: "Text", label: "Last name" },
+        { id: "MaidenName", type: "Text", label: "Maiden Name" },
+        {
+            id: "Gender", type: "FixedRadioList", label: "Select your gender",
+            list: [{ code: "male", label: "Male" }, { code: "female", label: "Female" }, { code: "notGiven", label: "Not given" }]
+        },
+        job
+    ];;
+
+    const customCase = new CCDCaseConfig("testCaseType", "Test jurisdiction", "test description");
+
+    customCase
+        .setEventProps(scenario ? scenario : {})
+        .addWizardPage("testPage1", "Test Page 1")
+        .addCaseField({ id: "TextField0", type: "Text", label: "Text Field 0" })
+        .addCaseField({ id: "TextField1", type: "Text", label: "Text Field 1" })
+        .setFieldProps({ show_condition: 'TextField0!="Hide TextField1" AND TextField0!="Hide all"' })
+        .addCaseField({ id: "TextField2", type: "Text", label: "Text Field 2" })
+        .setFieldProps({ show_condition: 'TextField0!="Hide TextField2" AND TextField0!="Hide all"' })
+        .addCaseField({ id: "TextField3", type: "Text", label: "Text Field 3" })
+        .addWizardPage("testPage2", "Test Page 2")
+        .addCaseField({
+            id: "Gender", type: "FixedRadioList", label: "Select your gender",
+            list: [{ code: "male", label: "Male" }, { code: "female", label: "Female" }, { code: "notGiven", label: "Not given" }]
+        })
+        .setFieldProps({ show_condition: 'TextField0!="Hide all"' })
+        .addCaseField({
+            id: "optionsMultiVal", type: "FixedList", label: "Select all that match",
+            list: [{ code: "a", label: "Option A" }, { code: "b", label: "Option B" }, { code: "c", label: "Option c" }]
+        })
+        .addCaseField({ id: "person1", type: "Complex", label: "Person 1", complex_fields: personFields })
+        .setFieldProps({ show_condition: 'TextField0!="Hide all"' })
+        .addCaseField({ id: "person2", type: "Complex", label: "Person 2", complex_fields: personFields })
+        .setFieldProps({ show_condition: 'TextField0!="Hide all"' })
+        .addCaseField({ id: "people", type: "Collection", label: "People", collection_field_type: { id: "person2", type: "Complex", label: "Person 2", complex_fields: personFields } })
+        .setFieldProps({ show_condition: 'TextField0!="Hide all"' })
+        .getCase();
+    return customCase;
+}
+
+
+module.exports = { getTestJurisdiction }

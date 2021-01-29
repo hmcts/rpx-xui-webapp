3177761ce19b4be0e014470c7dc557a5c12200f1
Lee Pavlou <leepavlou@MacBook-Pro.localdomain>
2021-01-29 10:29:21 +0000

    changes to code after review

--- api/configuration/health.ts
+++ api/configuration/health.ts
@@ -1,5 +1,5 @@
-import {EnvironmentConfigServices} from '../interfaces/environment.config'
-import {getConfigValue} from './index'
+import { EnvironmentConfigServices } from '../interfaces/environment.config'
+import { getConfigValue } from './index'
 import { SERVICES_CCD_COMPONENT_API_PATH } from './references'
 import { SERVICES_CCD_DATA_STORE_API_PATH } from './references'
 import { SERVICES_IDAM_LOGIN_URL } from './references'
@@ -11,10 +11,10 @@ export const healthEndpoints = (): EnvironmentConfigServices => {
   const HEALTH = '/health'
 
   return {
-    ccdComponentApi: getConfigValue(SERVICES_CCD_COMPONENT_API_PATH) + HEALTH,
-    ccdDataApi: getConfigValue(SERVICES_CCD_DATA_STORE_API_PATH) + HEALTH,
-    idamApi: getConfigValue(SERVICES_IDAM_LOGIN_URL) + HEALTH,
-    s2s: getConfigValue(SERVICE_S2S_PATH) + HEALTH,
-    termsAndConditions: getConfigValue(SERVICES_TERMS_AND_CONDITIONS_URL) + HEALTH,
+    ccdComponentApi: `${getConfigValue(SERVICES_CCD_COMPONENT_API_PATH)}${HEALTH}`,
+    ccdDataApi: `${getConfigValue(SERVICES_CCD_DATA_STORE_API_PATH)}${HEALTH}`,
+    idamApi: `${getConfigValue(SERVICES_IDAM_LOGIN_URL)}${HEALTH}`,
+    s2s: `${getConfigValue(SERVICE_S2S_PATH)}${HEALTH}`,
+    termsAndConditions: `${getConfigValue(SERVICES_TERMS_AND_CONDITIONS_URL)}${HEALTH}`,
   }
 }

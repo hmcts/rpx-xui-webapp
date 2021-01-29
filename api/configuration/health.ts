ed8be059d7a49b6617f495a4e5b646349b5eea0d
Lee Pavlou <73109695+leepavlouhmcts@users.noreply.github.com>
2021-01-29 11:12:56 +0000

    EUI 3061 (#836)
    * updated configuration
    
    * fixed lint errors
    
    * removed all but 1 endpoint to see which is causing an error on the PR environment
    
    * removed all but 1 endpoint to see which is causing an error on the PR environment
    
    * updated healthchecker to actually hit getPromises function
    
    * added additional endpoints for health check
    
    * fixed lint error
    updated yaml file with endpoint variables
    
    * fixed unit test
    
    * fixed test
    
    * removed property
    
    * lint error fixed
    
    * updated urls
    
    * updated endpoint urls
    
    * Bumping chart version
    
    * updated urls in environment variables
    
    * updated yaml files with correct urls
    
    * diagnosing issue with udnefined endpoint
    
    * added ccdComponentApi to list of health endpoints
    
    * lint error
    
    * upgrade @hmcts/nodejs-healthcheck
    
    * bump up nodejs-healthcheck version
    
    * fix health check not call issue
    
    * added rays changes from his branch into mine for testing
    
    * fixed lint error
    
    * make sure redis is running for health
    
    * removed variable
    
    * fix lint errors
    
    * lint error fix
    
    * updated code after code review
    
    * changed after code review
    
    * changes to code after review
    
    Co-authored-by: Lee Pavlou <leepavlou@Lees-MacBook-Pro.local>
    Co-authored-by: Lee Pavlou <leepavlou@MacBook-Pro.localdomain>
    Co-authored-by: Jenkins <nobody@nowhere>
    Co-authored-by: hmcts-jenkins-xui <61146108+hmcts-jenkins-xui[bot]@users.noreply.github.com>
    Co-authored-by: Uday Denduluri <uday.denduluri@gmail.com>
    Co-authored-by: Raymond Liang <ray.liang@atos.net>

---
+++ api/configuration/health.ts
@@ -0,0 +1,20 @@
+import { EnvironmentConfigServices } from '../interfaces/environment.config'
+import { getConfigValue } from './index'
+import { SERVICES_CCD_COMPONENT_API_PATH } from './references'
+import { SERVICES_CCD_DATA_STORE_API_PATH } from './references'
+import { SERVICES_IDAM_LOGIN_URL } from './references'
+import { SERVICE_S2S_PATH } from './references'
+import { SERVICES_TERMS_AND_CONDITIONS_URL } from './references'
+
+export const healthEndpoints = (): EnvironmentConfigServices => {
+
+  const HEALTH = '/health'
+
+  return {
+    ccdComponentApi: `${getConfigValue(SERVICES_CCD_COMPONENT_API_PATH)}${HEALTH}`,
+    ccdDataApi: `${getConfigValue(SERVICES_CCD_DATA_STORE_API_PATH)}${HEALTH}`,
+    idamApi: `${getConfigValue(SERVICES_IDAM_LOGIN_URL)}${HEALTH}`,
+    s2s: `${getConfigValue(SERVICE_S2S_PATH)}${HEALTH}`,
+    termsAndConditions: `${getConfigValue(SERVICES_TERMS_AND_CONDITIONS_URL)}${HEALTH}`,
+  }
+}

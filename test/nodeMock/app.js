a2ab1002b2a1a7ea70594e560f2c5eb1a2169c60
sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
2021-02-01 22:38:16 +0000

    ngIntegration nodemock updates

--- test/nodeMock/app.js
+++ test/nodeMock/app.js
@@ -2,15 +2,22 @@
 
 const express = require('express');
 var bodyParser = require('body-parser');
+const minimist = require('minimist');
+
 
 let { requestMapping,configurations} = require('./reqResMapping');
 const { browser } = require('protractor');
-const CCDCaseConfig = require('./ccd/ccdCaseConfig/caseCreateConfigGenerator')
+const CCDCaseConfig = require('./ccd/ccdCaseConfig/caseCreateConfigGenerator');
+const CCDCaseDetails = require('./ccd/ccdCaseConfig/caseDetailsConfigGenerator');
+
+const { getDLCaseConfig} = require('../ngIntegration/mockData/ccdCaseMock');
+
 const port = 3001;
 
 
 class MockApp{
     init(){
+        this.intercepts =[];
         this.conf = {
             get: { ...requestMapping.get },
             post: { ...requestMapping.post },
@@ -22,11 +29,20 @@ class MockApp{
         return "done";
     }
 
+    addIntercept(url,callback){
+        this.intercepts.push({url: url, callback: callback})
+    }
+
     async startServer(){
         const app = express();
         app.use(bodyParser.urlencoded({ extended: false }));
         app.use(bodyParser.json());
-        app.use(express.json()) 
+        app.use(express.json()); 
+
+        this.intercepts.forEach(intercept =>{
+            app.use(intercept.url, intercept.callback);
+        }); 
+
         for (const [key, value] of Object.entries(this.conf.get)) {
             app.get(key, value);
         }
@@ -84,43 +100,26 @@ class MockApp{
 
 }
 
+
 const mockInstance = new MockApp();
 module.exports = mockInstance;
 
-
-// mockInstance.init();
-// createCustomCCDCaseConfig();
-// mockInstance.startServer();
-
-
-
-function createCustomCCDCaseConfig(){
-    // const scenario = { fieldType: "Collection", showField: false, retainHiddenField: false };
-
-    const scenario = { fieldType: "Document", showField: false, retainHiddenField: true };
-
-    const caseConfig = new CCDCaseConfig('TEST_CaseType', 'Test case type hidden field retain value', 'test description');
-    const page1 = caseConfig.addWizardPage('HiddenFieldPage_1', 'Hidden field retain value test page');
-
-    let testFieldShowYesNo = caseConfig.addCCDFieldToPage(page1, "YesOrNo", "showTestField", "Show Test Field?");
-    testFieldShowYesNo.value = true;
-
-    let complexField = caseConfig.addCCDFieldToPage(page1, scenario.fieldType, "complexField", "Complex field");
-    complexField.value = {
-            "document_url": "http://dm-store-aat.service.core-compute-aat.internal/documents/a612199d-9972-4b99-b653-5ec7c310e21a",
-            "document_binary_url": "http://dm-store-aat.service.core-compute-aat.internal/documents/a612199d-9972-4b99-b653-5ec7c310e21a/binary",
-            "document_filename": "Redacted-dm-store135044941749889827327305460282199740737.pdf"
-    }; 
-    complexField.retain_hidden_value = scenario.retainHiddenField;
-    complexField.show_condition = `${testFieldShowYesNo.id}=\"Yes\"`;
-
-    setUpcaseConfig(caseConfig.caseConfigTemplate);
+const args = minimist(process.argv)
+if (args.standalone){
+    mockInstance.init();
+    setUpcaseConfig();
+    // getDLCaseConfig();
+    // collectionDynamicListeventConfig()
+    // createCustomCaseDetails();
+    mockInstance.startServer()
 }
+
+
 
 
 function setUpcaseConfig(caseConfig) {
     mockInstance.onGet('/data/internal/cases/:caseid/event-triggers/:eventId', (req, res) => {
-        res.send(caseConfig);
+        res.send(getDLCaseConfig().getCase());
     });
 
     mockInstance.onPost('/data/case-types/:caseType/validate', (req, res) => {
@@ -143,4 +142,5 @@ function setUpcaseConfig(caseConfig) {
     });
 
 }
+
 

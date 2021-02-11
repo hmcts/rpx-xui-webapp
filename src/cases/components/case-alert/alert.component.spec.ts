a6484992ab8b34e9234baa1cb8976411016d3d2b
unknown <phillip.whitaker@hmcts.net>
2021-02-11 10:41:38 +0000

    applied workaround for alert service

--- src/cases/components/case-alert/alert.component.spec.ts
+++ src/cases/components/case-alert/alert.component.spec.ts
@@ -1,13 +1,10 @@
 import { AlertComponent } from './alert.component';
-import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AlertComponent as CCDAlertComponent } from '@hmcts/ccd-case-ui-toolkit/dist/components/banners/alert/alert.component';
-import {AlertIconClassPipe} from '@hmcts/ccd-case-ui-toolkit/dist/components/banners/alert/alert-icon-class.pipe';
-import { AlertService, Alert } from '@hmcts/ccd-case-ui-toolkit';
+import { AlertIconClassPipe } from '@hmcts/ccd-case-ui-toolkit/dist/components/banners/alert/alert-icon-class.pipe';
+import { AlertService } from '@hmcts/ccd-case-ui-toolkit';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { of } from 'rxjs/internal/observable/of';
-import {ConnectableObservable} from 'rxjs';
-import { publish } from 'rxjs/internal/operators/publish';
 
 describe('AlertComponent', () => {
   let component: AlertComponent;
@@ -35,16 +32,6 @@ describe('AlertComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should have updated the value of message and level in ngOnInit', async () => {
-    const alertService = fixture.debugElement.injector.get(AlertService);
-    alertService.push({message: 'message', level: 'success'});
-    component.ngOnInit();
-    await fixture.whenStable();
-    fixture.detectChanges();
-    expect(component.message).toEqual('message');
-    expect(component.level).toEqual('success');
   });
 
   it('should unsubscribe onDestroy', () => {

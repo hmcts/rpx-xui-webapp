a13e19ed6c7396eabe5ba24807c597467727f416
issshiO <64783858+issshiO@users.noreply.github.com>
2021-02-17 10:45:29 +0000

    EUI-3369-content-change-unshare-case-failed

--- src/cases/containers/case-share-complete/case-share-complete.component.spec.ts
+++ src/cases/containers/case-share-complete/case-share-complete.component.spec.ts
@@ -2,16 +2,18 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { State } from '../../../app/store/reducers';
 import { CaseShareCompleteComponent } from './case-share-complete.component';
+import { FeatureToggleService } from '@hmcts/rpx-xui-common-lib';
 
 describe('CaseShareCompleteComponent', () => {
   let component: CaseShareCompleteComponent;
   let fixture: ComponentFixture<CaseShareCompleteComponent>;
 
   let store: MockStore<State>;
-
+  const mockFeatureToggleService = jasmine.createSpyObj('FeatureToggleService', ['getValue']);
   beforeEach(() => {
     TestBed.configureTestingModule({
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -19,12 +21,17 @@ describe('CaseShareCompleteComponent', () => {
       imports: [ RouterTestingModule ],
       providers: [
         provideMockStore(),
+        {
+          provide: FeatureToggleService,
+          useValue: mockFeatureToggleService
+        }
       ]
     }).compileComponents();
     store = TestBed.get(Store);
     fixture = TestBed.createComponent(CaseShareCompleteComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    mockFeatureToggleService.getValue.and.returnValue(of(true));
   });
 
   it('should create', () => {
@@ -115,6 +122,20 @@ describe('CaseShareCompleteComponent', () => {
     component.completeScreenMode = 'COMPLETE';
     component.ngOnDestroy();
     expect(component.shareCases.length).toEqual(0);
+  });
+
+  it('should not see remove user from case if feature is toggled off', () => {
+    component.removeUserFromCaseToggleOn$ = of(false);
+    fixture.detectChanges();
+    const removeUserFromCaseToggleOn = fixture.debugElement.nativeElement.querySelector('#remove-user-error');
+    expect(removeUserFromCaseToggleOn).toBeFalsy();
+  });
+
+  it('should see remove user from case if feature is toggled on', () => {
+    component.removeUserFromCaseToggleOn$ = of(true);
+    fixture.detectChanges();
+    const removeUserFromCaseToggleOn = fixture.debugElement.nativeElement.querySelector('#add-and-remove-user-error');
+    expect(removeUserFromCaseToggleOn).toBeTruthy();
   });
 
   afterEach(() => {

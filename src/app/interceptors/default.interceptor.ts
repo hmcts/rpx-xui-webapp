ce2ff1072283f0ea91c1e9eab5d64d2b81d7ea75
Lefkos Hadjipavlou <lefkos.hadjiPavlou@hmcts.net>
2021-02-19 10:01:05 +0000

    lint errors

--- src/app/interceptors/default.interceptor.ts
+++ src/app/interceptors/default.interceptor.ts
@@ -1,11 +1,11 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class HttpDefaultInterceptor implements HttpInterceptor {
 
-    constructor() { }
+    constructor(private http: HttpClient) { }
 
     public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         if (req.headers.has('X-Powered-By') || req.headers.has('x-powered-by')) {

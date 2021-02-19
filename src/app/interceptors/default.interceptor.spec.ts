79cc6a46cf34538b0582104752490ce70edba4b0
Lefkos Hadjipavlou <lefkos.hadjiPavlou@hmcts.net>
2021-02-19 12:59:42 +0000

    lint errors

---
+++ src/app/interceptors/default.interceptor.spec.ts
@@ -0,0 +1,28 @@
+import {TestBed, inject} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {HTTP_INTERCEPTORS, HttpClient} from '@angular/common/http';
+import {HttpDefaultInterceptor} from './default.interceptor';
+
+describe('HttpDefaultInterceptor', () => {
+    beforeEach(() => TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [{
+                        provide: HTTP_INTERCEPTORS,
+                        useClass: HttpDefaultInterceptor,
+                        multi: true
+            }]
+    }));
+
+    describe('intercept HTTP requests', () => {
+        it('should add X-XSS-Protection to Headers', inject([HttpClient, HttpTestingController],
+            (http: HttpClient, mock: HttpTestingController) => {
+                http.get('/api').subscribe(response => expect(response).toBeTruthy());
+                mock.expectOne(req => (req.headers.has('X-XSS-Protection') && req.headers.get('X-XSS-Protection') === '1; mode=block'));
+                mock.verify();
+            }));
+    });
+
+    afterEach(inject([HttpTestingController], (mock: HttpTestingController) => {
+            mock.verify();
+    }));
+});

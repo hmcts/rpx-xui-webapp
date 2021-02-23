303ee0dafe82f8cb91f5cde9a45319f1a02aa9f7
issshiO <64783858+issshiO@users.noreply.github.com>
2021-02-23 15:26:01 +0000

    EUI-3384-hyphens-in-case-number

--- src/cases/components/case-alert/alert.component.ts
+++ src/cases/components/case-alert/alert.component.ts
@@ -30,10 +30,10 @@ export class AlertComponent implements OnInit, OnDestroy {
       if (alert) {
           const msg = alert.message;
           const caseId = msg.match(/[0-9]{16}/g);
-          if(caseId){
+          if(caseId) {
             const caseIdHyphen = msg.match(/([0-9][0-9][0-9][0-9])/g).join("-");
             this.message = msg.replace(caseId.toString(),caseIdHyphen);
-          }else{
+          }else {
             this.message = msg;
           }
           this.level = alert.level;

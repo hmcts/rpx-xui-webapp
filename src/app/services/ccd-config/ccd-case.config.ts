04b1421c6133c817be8cd328c845fbe078799d29
unknown <phillip.whitaker@hmcts.net>
2021-02-24 15:37:50 +0000

    removed fixed set for secure_enabled

--- src/app/services/ccd-config/ccd-case.config.ts
+++ src/app/services/ccd-config/ccd-case.config.ts
@@ -33,8 +33,7 @@ export class AppConfig extends AbstractAppConfig {
       next: (val) => this.config.document_management_secure_enabled = val
     });
     this.config = {...this.appConfigService.getEditorConfiguration()} || {};
-    this.config.document_management_secure_enabled = true;
-
+    
     this.featureToggleService.getValue('mc-document-secure-mode-enabled', false).subscribe({
       next: (val) => this.config.document_management_secure_enabled = val
     });

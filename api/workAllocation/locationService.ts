258c5abfc2b43bef09caff0a196cd6f79b4399ae
connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>
2021-02-25 09:59:46 +0000

    Update locationService.ts
    Add in TODO comment

--- api/workAllocation/locationService.ts
+++ api/workAllocation/locationService.ts
@@ -2,6 +2,7 @@ import {EnhancedRequest} from '../lib/models';
 import {ALL_LOCATIONS} from './constants/locations';
 
 export async function handleLocationGet(path: string, req: EnhancedRequest): Promise<any> {
+  /*TODO: Implement get location - currently using given data without endpoint connection*/
   const response = {
     data: ALL_LOCATIONS,
   };

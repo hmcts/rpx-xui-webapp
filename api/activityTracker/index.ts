7a12a49cf8b7200267c5380fe345a76db9f69bcb
kuda <ku-da@hotmail.co.uk>
2021-03-12 12:26:35 +0000

    add safety check whilst accessing user info object

--- api/activityTracker/index.ts
+++ api/activityTracker/index.ts
@@ -3,15 +3,20 @@ import * as log4jui from '../lib/log4jui';
 const logger = log4jui.getLogger('proxy');
 
 export async function activityTrackerProxyRequest(proxyReq, req): Promise<void> {
-  logger.info('ActivityTrackerRequest => ',
-    `id:${req.user.userinfo.id} forename:${req.user.userinfo.forename} surname:${req.user.userinfo.surname}`
-  );
+  if (req.user && req.user.userinfo) {
+    logger.info('ActivityTrackerRequest => ',
+      `id:${req.user.userinfo.id} forename:${req.user.userinfo.forename} surname:${req.user.userinfo.surname}`
+    );
+  }
+
   proxyReq.end();
 }
 
 export async function activityTrackerProxyResponse(proxyReq, req, res, json): Promise<any> {
-  logger.info('ActivityTrackerResponse => ',
-    `id: ${req.user.userinfo.id} forename:${req.user.userinfo.forename} surname:${req.user.userinfo.surname}`
-  );
+  if (req.user && req.user.userinfo) {
+    logger.info('ActivityTrackerResponse => ',
+      `id: ${req.user.userinfo.id} forename:${req.user.userinfo.forename} surname:${req.user.userinfo.surname}`
+    );
+  }
   return json;
 }

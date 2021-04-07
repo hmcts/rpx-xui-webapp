4c853d1d2ebe1f6fbe8029ae031c38c4f339477e
rayliangatos <57181888+rayliangatos@users.noreply.github.com>
2021-04-07 10:18:07 +0100

    EUI-2992 get noc integration working with CCD apis (#827)
    * EUI-2992-noc-integration:get NoC integration working with CCD API
    
    * EUI-2992-noc-integration:address comments
    
    * E UI 2992 noc integration ishita (#844)
    
    * EUI-2992-noc-integration-ishita
    
    * EUI-2992-noc-integration-ishita
    
    * EUI-3181, EUI-3084
    
    * fix tests
    
    Co-authored-by: issshiO <64783858+issshiO@users.noreply.github.com>
    Co-authored-by: Raymond Liang <ray.liang@atos.net>
    
    * EUI-3181:add error style in div
    
    * EUI-3181:add error style in div
    
    * EUI-3181:add error style in div
    
    * EUI-3181: reduce the gap between the two checkboxes
    
    * EUI-2992-noc-integration:fix back button issue
    
    * EUI-2992-noc-integration:fix tests
    
    * EUI 2992 noc integration ishita (#857)
    
    * EUI-2992-noc-integration-ishita
    
    * EUI-2992-noc-integration-ishita
    
    * EUI-3181, EUI-3084
    
    * fix tests
    
    * EUI-2992-noc-integration-ishita
    
    * EUI-2992-noc-integration-ishita
    
    * EUI-2992-noc-integration-ishita
    
    * EUI-2992-noc-integration-ishita
    
    * EUI-2992-noc-integration-ishita
    
    * EUI-2992-noc-integration-ishita
    
    * EUI-2992-noc-integration-ishita
    
    Co-authored-by: issshiO <64783858+issshiO@users.noreply.github.com>
    Co-authored-by: Raymond Liang <ray.liang@atos.net>
    
    * EUI 3344 noc column changes (#877)
    
    * EUI-3344-noc-column-changes
    
    * EUI-3344-noc-column-changes
    
    * EUI-3344-noc-column-changes
    
    * EUI-3344-noc-column-changes
    
    * EUI-3344-noc-column-changes
    
    Co-authored-by: issshiO <64783858+issshiO@users.noreply.github.com>
    
    * EUI-2992-noc-integration: address review comments
    
    * EUI-2992-noc-integration: address review comments
    
    * test validation fix/enhancement to check includes
    
    Co-authored-by: Ishita Oswal <74009839+ishOsw@users.noreply.github.com>
    Co-authored-by: issshiO <64783858+issshiO@users.noreply.github.com>
    Co-authored-by: sreekanthpuligadda <sreekanth.puligadda@hmcts.net>

--- src/noc/containers/noc-check-and-submit/noc-check-and-submit.component.spec.ts
+++ src/noc/containers/noc-check-and-submit/noc-check-and-submit.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Store } from '@ngrx/store';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { of } from 'rxjs';
+import { AFFIRMATION_DEFAULT_DISAGREE_ERROR, AFFIRMATION_NOTIFY_EVERY_PARTY_ERROR } from '../../constants/nocErrorMap.enum';
 import { NocAnswer, NocEvent, NocNavigationEvent, NocQuestion } from '../../models';
 import * as fromFeature from '../../store';
 import { NocCheckAndSubmitComponent } from './noc-check-and-submit.component';
@@ -118,9 +119,10 @@ describe('NocCheckAndSubmitComponent', () => {
 
   it('should verify and submit NoC', () => {
     component.affirmationAgreed = true;
+    component.notifyEveryParty = true;
     const nocEvent: NocEvent = {
-      caseReference: '1111222233334444',
-      nocAnswers: [{
+      case_id: '1111222233334444',
+      answers: [{
         question_id: 'q1',
         value: 'a1'
       }]
@@ -136,9 +138,35 @@ describe('NocCheckAndSubmitComponent', () => {
 
   it('should set affirmation disagree error', () => {
     component.affirmationAgreed = false;
+    component.notifyEveryParty = false;
+    const affirmationError = {
+      AFFIRMATION_DEFAULT_DISAGREE_ERROR,
+      AFFIRMATION_NOTIFY_EVERY_PARTY_ERROR
+    };
     component.verifyAndSubmitNoC();
-    expect(spyOnDispatchToStore).toHaveBeenCalledWith(new fromFeature.SetAffirmationDisagreeError());
+    expect(spyOnDispatchToStore).toHaveBeenCalledWith(new fromFeature.SetAffirmationError(affirmationError));
   });
+
+  it('should set notify every party error when user doesn not notify every related party', () => {
+    component.affirmationAgreed = true;
+    component.notifyEveryParty = false;
+    const affirmationError = {
+      AFFIRMATION_NOTIFY_EVERY_PARTY_ERROR
+    };
+    component.verifyAndSubmitNoC();
+    expect(spyOnDispatchToStore).toHaveBeenCalledWith(new fromFeature.SetAffirmationError(affirmationError));
+  });
+
+  it('should set affirmation agreed error when user does not confirm their answers are accurate', () => {
+    component.affirmationAgreed = false;
+    component.notifyEveryParty = true;
+    const affirmationError = {
+      AFFIRMATION_DEFAULT_DISAGREE_ERROR
+    };
+    component.verifyAndSubmitNoC();
+    expect(spyOnDispatchToStore).toHaveBeenCalledWith(new fromFeature.SetAffirmationError(affirmationError));
+  });
+
 
   afterEach(() => {
     component = null;

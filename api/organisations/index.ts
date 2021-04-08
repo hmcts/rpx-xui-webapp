27710ee4ab0fcaba5682aa788a50c055f4cb39ed
Raymond Liang <ray.liang@atos.net>
2021-04-08 10:46:24 +0100

    feature/EUI-3348: yarn lint issue

--- api/organisations/index.ts
+++ api/organisations/index.ts
@@ -1,45 +1,45 @@
-import { NextFunction, Request, Response, Router } from 'express'
-import { handleGet } from '../common/crudService'
-import { getConfigValue } from '../configuration'
-import { SERVICES_PRD_API_URL } from '../configuration/references'
-import { EnhancedRequest } from '../lib/models'
+import { NextFunction, Response, Router } from 'express';
+import { handleGet } from '../common/crudService';
+import { getConfigValue } from '../configuration';
+import { SERVICES_PRD_API_URL } from '../configuration/references';
+import { EnhancedRequest } from '../lib/models';
 
 export async function handleGetOrganisationsRoute(req: EnhancedRequest, res: Response, next: NextFunction) {
     try {
-        const path = getOrganisationUri()
-        const response = await handleGet(path, req, next)
+        const path = getOrganisationUri();
+        const response = await handleGet(path, req, next);
 
         if (response.data.organisations) {
-            res.send(response.data.organisations)
+            res.send(response.data.organisations);
         } else {
-            res.send(response.data)
+            res.send(response.data);
         }
     } catch (error) {
-        next(error)
+        next(error);
     }
 }
 
 export async function handleOrganisationRoute(req: EnhancedRequest, res: Response, next: NextFunction) {
     try {
-        const path = `${getConfigValue(SERVICES_PRD_API_URL)}/refdata/external/v1/organisations`
-        const response = await handleGet(path, req, next)
-        res.send(response.data)
+        const path = `${getConfigValue(SERVICES_PRD_API_URL)}/refdata/external/v1/organisations`;
+        const response = await handleGet(path, req, next);
+        res.send(response.data);
     } catch (error) {
         const errReport = {
             apiError: error.data.message,
             apiStatusCode: error.status,
             message: 'Organisation route error',
-        }
-        res.status(errReport.apiStatusCode).send(errReport)
+          };
+        res.status(errReport.apiStatusCode).send(errReport);
     }
 }
 
 function getOrganisationUri(): string {
-    return `${getConfigValue(SERVICES_PRD_API_URL)}/refdata/external/v1/organisations/status/ACTIVE?address=true`
+    return `${getConfigValue(SERVICES_PRD_API_URL)}/refdata/external/v1/organisations/status/ACTIVE?address=true`;
 }
 
-export const router = Router({ mergeParams: true })
+export const router = Router({ mergeParams: true });
 
-router.get('', handleOrganisationRoute)
+router.get('', handleOrganisationRoute);
 
-export default router
+export default router;

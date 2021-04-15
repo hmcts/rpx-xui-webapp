244d841b838015bbe90d41acf4ea19d9d66ea790
sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
2021-04-15 23:42:19 +0100

    added WA api tests for actions.

--- test/integration/tests/utils/authUtil.ts
+++ test/integration/tests/utils/authUtil.ts
@@ -19,6 +19,26 @@ export async function getSessionCookieString( username, password) {
     return cookieString;
 }
 
+export function updateSessionCookieString(username, name, value) {
+    let isNewCookie = true;
+    for (const cookie of authCookiesForUsers[username]) {
+        // console.log(`${cookie.name} : ${cookie.value}`);
+        if (cookie.name === name){
+            cookie.value=value;
+            isNewCookie = false;
+        }
+    }
+    if (isNewCookie){
+        authCookiesForUsers[username].push({ name: value});
+    }
+    let cookieString = '';
+    for (const cookie of authCookiesForUsers[username]) {
+        cookieString = `${cookieString}${cookie.name}=${cookie.value};`;
+    }
+    return cookieString;
+
+}
+
 export async function getXSRFToken(username, password) {
     if (!authCookiesForUsers.hasOwnProperty(username)) {
         authCookiesForUsers[username] = await authenticateAndGetcookies(username, password);
@@ -110,7 +130,11 @@ async function  authenticateAndGetcookies(username, password)  {
 
             isLoginSuccess = true;
         } catch (error) {
-            let usernameInput = await page.$eval('#username', element => element.value);
+            let usernameInput = "";
+            try{
+                usernameInput = await page.$eval('#username', element => element.value);
+            }catch(err){}
+           
             if (usernameInput === "") {
                 loginAttemptsCounter++;
                 console.log("Login error : " + error.message);

071687fcbbce3353e4284cbe1292fe9cc0726d93
taleb <taleb.benouaer@hmcts.net>
2021-04-16 14:30:11 +0100

    fix linting errors

--- api/proxy.config.ts
+++ api/proxy.config.ts
@@ -4,7 +4,7 @@ import * as amendedJurisdictions from './amendedJurisdictions'
 import {getConfigValue} from './configuration'
 import {
     SERVICES_CCD_COMPONENT_API_PATH,
-    SERVICES_DOCUMENTS_API_PATH, SERVICES_EM_HRS_API_PATH, SERVICES_EM_ANNO_API_URL,
+    SERVICES_DOCUMENTS_API_PATH, SERVICES_EM_ANNO_API_URL, SERVICES_EM_HRS_API_PATH,
     SERVICES_ICP_API_URL, SERVICES_MARKUP_API_URL, SERVICES_PAYMENTS_URL
 } from './configuration/references'
 import {applyProxy} from './lib/middleware/proxy'

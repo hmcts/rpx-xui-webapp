686bbded718039b5309a099a723a4f3cfa6ede0a
ernestman28 <ernest_man28@hotmail.com>
2021-05-06 16:05:09 +0100

    Revert "Update package.json with additional resolutions (#1031)"
    This reverts commit 82f27e614061de66a2245b1fc8d0a96c34c0f1f4.

--- test/e2e/support/reports.js
+++ test/e2e/support/reports.js
@@ -1,15 +1,15 @@
-// const reporter = require('cucumberjs-allure-reporter');
-// const fs = require('fs');
-// const mkdirp = require('mkdirp');
+const reporter = require('cucumberjs-allure-reporter');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
 
-// const xmlReports = `${process.cwd()}/reports/xml`;
+const xmlReports = `${process.cwd()}/reports/xml`;
 
-// // var createXmlDir = (function() {
-// //     if (!fs.existsSync(xmlReports)) {
-// //         mkdirp.sync(xmlReports);
-// //     }
-// // })();
+// var createXmlDir = (function() {
+//     if (!fs.existsSync(xmlReports)) {
+//         mkdirp.sync(xmlReports);
+//     }
+// })();
 
-// reporter.config({ targetDir: xmlReports });
+reporter.config({ targetDir: xmlReports });
 
-// module.exports = reporter;
+module.exports = reporter;

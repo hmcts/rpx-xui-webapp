1590d9b0cfcd67d7fe03c7984e787771c756803d
connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>
2021-05-11 15:49:18 +0100

    Manage Link Logic
    Note: Tests are unfinished

--- api/workAllocation2/util.ts
+++ api/workAllocation2/util.ts
@@ -1,5 +1,5 @@
 import { ActionViews, TaskPermission, TASK_ACTIONS } from './constants/actions';
-import { Caseworker, CaseworkerApi, Location, LocationApi } from './interfaces/task';
+import { Action, Caseworker, CaseworkerApi, Location, LocationApi } from './interfaces/task';
 
 export function prepareGetTaskUrl(baseUrl: string, taskId: string): string {
   return `${baseUrl}/task/${taskId}`
@@ -141,50 +141,56 @@ export function prepareRoleApiRequest(locationId?: number): any {
  * @param view This dictates which set of actions we should use.
  * @param isTaskAssigned Dummy parameter to test change based on whether task is assigned
  */
-export function getWATaskActions(permissions: TaskPermission[], view: ActionViews, isTaskAssigned: boolean) {
-  let actionList = [];
+export function getWATaskActions(permissions: TaskPermission[], view: ActionViews, isTaskAssigned: boolean): Action[] {
+  let actionList:Action[] = [];
   permissions.forEach(permission => {
     switch(permission) {
       case TaskPermission.MANAGE:
         actionList = actionList.concat(getWATaskActionsForManage(view, isTaskAssigned));
+        break;
       case TaskPermission.EXECUTE:
-        // actionList = actionList.concat(getWATaskActionsForExecute(view, isTaskAssigned));
+        actionList = actionList.concat(getWATaskActionsForExecute(view));
+        break;
       case TaskPermission.CANCEL:
-        // actionList = actionList.concat(getWATaskActionsForCancel(view, isTaskAssigned));
+        actionList = actionList.concat(getWATaskActionsForCancel(view));
+        break;
     }
   })
+  return actionList;
 }
 
-export function getWATaskActionsForManage(view: ActionViews, isTaskAssigned: boolean) {
-    switch (view) {
-        case ActionViews.MY:
-          return TASK_ACTIONS.MY;
-        case ActionViews.AVAILABLE:
-          return TASK_ACTIONS.AVAILABLE;
-        case ActionViews.ACTIVE:
-          return TASK_ACTIONS.ACTIVE;
-        case ActionViews.ALL_WORK:
-          return isTaskAssigned ? TASK_ACTIONS.MANAGER.ASSIGNED : TASK_ACTIONS.MANAGER.UNASSIGNED;
-        default:
-          // If we don't recognise the view, just make sure we at least have an array.
-          return []
-      }
-}
-
-/* export function getWATaskActionsForExecute(view: ActionViews, isTaskAssigned: boolean) {
+export function getWATaskActionsForManage(view: ActionViews, isTaskAssigned: boolean): Action[] {
   switch (view) {
+    case ActionViews.MY:
+      return TASK_ACTIONS.MY;
+    case ActionViews.AVAILABLE:
+      return TASK_ACTIONS.AVAILABLE;
+    case ActionViews.ACTIVE:
+      // TODO - More logic required for Active tasks for tasks tab in case details page
+      // Works in reverse i.e. if view, then check has permission (and also check if task assigned)
+      return TASK_ACTIONS.ACTIVE;
     case ActionViews.ALL_WORK:
       return isTaskAssigned ? TASK_ACTIONS.ALL_WORK.ASSIGNED : TASK_ACTIONS.ALL_WORK.UNASSIGNED;
     default:
+      // If we don't recognise the view, just make sure we at least have an array.
       return [];
   }
-} */
+}
 
-/* export function getWATaskActionsForCancel(view: ActionViews, isTaskAssigned: boolean) {
+export function getWATaskActionsForExecute(view: ActionViews): Action[] {
   switch (view) {
-    case WA_ActionViews.ALL_WORK:
-      return isTaskAssigned ? TASK_ACTIONS.WA_CANCEL.ASSIGNED : TASK_ACTIONS.WA_CANCEL.UNASSIGNED;
+    case ActionViews.ALL_WORK:
+      return TASK_ACTIONS.EXECUTE;
     default:
       return [];
   }
-} */
+}
+
+export function getWATaskActionsForCancel(view: ActionViews): Action[] {
+  switch (view) {
+    case ActionViews.ALL_WORK:
+      return TASK_ACTIONS.CANCEL;
+    default:
+      return [];
+  }
+}

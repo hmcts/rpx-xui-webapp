8dca3865dcfb77acbc6d84c37d4204df04f6ca95
unknown <phillip.whitaker@hmcts.net>
2021-05-15 13:09:38 +0100

    implemented the design for task assignment confirmation

---
+++ src/work-allocation-2/containers/task-assignment-confirm/task-assignment-confirm.component.spec.ts
@@ -0,0 +1,49 @@
+import { CdkTableModule } from "@angular/cdk/table";
+import { Component, ViewChild } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { ExuiCommonLibModule } from "@hmcts/rpx-xui-common-lib";
+import { Router } from "express";
+import { WorkAllocationComponentsModule } from "src/work-allocation-2/components/work-allocation.components.module";
+import { WorkAllocationTaskService } from "src/work-allocation-2/services";
+import { TaskAssignmentConfirmComponent } from "./task-assignment-confirm.component";
+
+@Component({
+  template: `<exui-task-assignment-confirm></exui-task-assignment-confirm>`
+})
+class WrapperComponent {
+  @ViewChild(TaskAssignmentConfirmComponent) public appComponentRef: TaskAssignmentConfirmComponent;
+}
+
+fdescribe('TaskAssignmentConfirmComponent', () => {
+  let component: TaskAssignmentConfirmComponent;
+  let wrapper: WrapperComponent;
+  let fixture: ComponentFixture<WrapperComponent>;
+  let router: Router;
+  const mockTaskService = jasmine.createSpyObj('mockTaskService', ['searchTask']);
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        CdkTableModule,
+        ExuiCommonLibModule,
+        RouterTestingModule,
+        WorkAllocationComponentsModule,
+        ExuiCommonLibModule
+      ],
+      declarations: [TaskAssignmentConfirmComponent, WrapperComponent],
+      providers: [
+        { provide: WorkAllocationTaskService, useValue: mockTaskService }
+      ]
+    }).compileComponents();
+    fixture = TestBed.createComponent(WrapperComponent);
+    wrapper = fixture.componentInstance;
+    component = wrapper.appComponentRef;
+    router = TestBed.get(Router);
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeDefined();
+  });
+});

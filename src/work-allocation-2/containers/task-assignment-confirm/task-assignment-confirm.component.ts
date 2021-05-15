ae55e6700b94f02591b2401dcbc8f25531aca602
unknown <phillip.whitaker@hmcts.net>
2021-05-15 13:09:38 +0100

    implemented the design for task assignment confirmation

---
+++ src/work-allocation-2/containers/task-assignment-confirm/task-assignment-confirm.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { TaskActionType } from "src/work-allocation-2/enums";
+
+@Component({
+  selector: 'exui-task-assignment-confirm',
+  templateUrl: './task-assignment-confirm.component.html'
+})
+export class TaskAssignmentConfirmComponent implements OnInit {
+
+  public verb: TaskActionType;
+  
+  constructor(private readonly route: ActivatedRoute) { }
+  
+  public ngOnInit(): void {
+    this.verb = this.route.snapshot.data.verb as TaskActionType;
+  }
+  
+}

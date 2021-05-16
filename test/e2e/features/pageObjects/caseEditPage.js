a4d8161d8cf842a25343081bec5f4c337f7a6bb8
udaydenduluri33 <50869558+udaydenduluri33@users.noreply.github.com>
2021-05-16 10:42:17 +0100

    Feature/activity tracker ud (#1061)
    * activity tracker changes
    
    * lint error fix
    
    * audit issues
    
    * fix the failing unit tests
    
    * tests updated inline with api response change.
    
    * functional test fix to land in correct tab for validation
    
    (cherry picked from commit 277483d55e60b0124173d0d0969ab987e7fd09cc)
    
    * test fix to handle empty column header labels in case list
    
    Co-authored-by: Uday Denduluri <uday.denduluri@gmail.com>
    Co-authored-by: sreekanthpuligadda <sreekanth.puligadda@hmcts.net>

--- test/e2e/features/pageObjects/caseEditPage.js
+++ test/e2e/features/pageObjects/caseEditPage.js
@@ -82,7 +82,10 @@ class caseEditPage {
             for (let i = index; i < count; i++) {
                 let thText = thLable.get(i).$$(".search-result-column-label");
                 let text = await thText.getText();
-                caseResultsThTitle.push(`${text}`);
+                if (text.length !== 0){
+                    caseResultsThTitle.push(`${text}`);
+                }
+                
             }
             return await caseResultsThTitle;
         }
@@ -113,6 +116,8 @@ class caseEditPage {
     async caseDetailsCheck() {
         let caseDetailsRes = await CcdApi.getCaseResultsResponse();
         this.caseDetailsTabs = $$("mat-tab-body table tbody>div>tbody");
+        await element(by.xpath("//mat-tab-header//div[contains(text(),'Tab 2')]")).click();
+        await BrowserWaits.waitForSeconds(1);
         let fieldCount = await this.caseDetailsTabs.count();
         let tabName = "Tab 2";
         let tab = await caseDetailsRes.tabs.find(tab => tab.label == tabName);

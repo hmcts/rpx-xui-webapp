fd9e4e57d3fcbd95fdbbb0556505204d76dd4f22
unknown <phillip.whitaker@hmcts.net>
2021-05-17 09:34:21 +0100

    removed cancel link

--- src/work-allocation-2/containers/task-assignment-confirm/task-assignment-confirm.component.spec.ts
+++ src/work-allocation-2/containers/task-assignment-confirm/task-assignment-confirm.component.spec.ts
@@ -6,6 +6,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { ExuiCommonLibModule } from '@hmcts/rpx-xui-common-lib';
 import { WorkAllocationComponentsModule } from 'src/work-allocation-2/components/work-allocation.components.module';
 import { WorkAllocationTaskService } from 'src/work-allocation-2/services';
+import { TaskActionType } from 'src/work-allocation/enums';
 import { TaskAssignmentConfirmComponent } from './task-assignment-confirm.component';
 
 @Component({
@@ -39,6 +40,7 @@ fdescribe('TaskAssignmentConfirmComponent', () => {
     fixture = TestBed.createComponent(WrapperComponent);
     wrapper = fixture.componentInstance;
     component = wrapper.appComponentRef;
+    component.verb = TaskActionType.Reassign;
     router = TestBed.get(Router);
     fixture.detectChanges();
   });

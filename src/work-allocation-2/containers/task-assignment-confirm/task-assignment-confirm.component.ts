3c9d8bee38910fa955911189dd315d133c9037ce
unknown <phillip.whitaker@hmcts.net>
2021-05-17 09:06:21 +0100

    fix lint and build issues

--- src/work-allocation-2/containers/task-assignment-confirm/task-assignment-confirm.component.ts
+++ src/work-allocation-2/containers/task-assignment-confirm/task-assignment-confirm.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { TaskActionType } from "src/work-allocation-2/enums";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TaskActionType } from 'src/work-allocation-2/enums';
 
 @Component({
   selector: 'exui-task-assignment-confirm',
@@ -9,11 +9,10 @@ import { TaskActionType } from "src/work-allocation-2/enums";
 export class TaskAssignmentConfirmComponent implements OnInit {
 
   public verb: TaskActionType;
-  
+
   constructor(private readonly route: ActivatedRoute) { }
-  
+
   public ngOnInit(): void {
     this.verb = this.route.snapshot.data.verb as TaskActionType;
   }
-  
 }

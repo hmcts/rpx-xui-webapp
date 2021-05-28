70c3551cc55b0d9cb86351e47a0f7d0e169f1d46
Raymond Liang <ray.liang@atos.net>
2021-05-28 13:02:31 +0100

    feature/EUI-4119-set-default-theme-logo

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -123,8 +123,8 @@ const defaultUserTheme: Theme = {
   },
   showFindCase: true,
   backgroundColor: '#202020',
-  logoIsUsed: false,
-  logoType: 'default',
+  logoIsUsed: true,
+  logoType: 'myhmcts',
 };
 
 const signedOutTheme: Theme = {

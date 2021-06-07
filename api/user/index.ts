9d8ca57cc2d9aa7e7d61a42da22ee200868e3f97
Uday Denduluri <uday.denduluri@gmail.com>
2021-06-07 17:38:33 +0100

    adding role management Info for the currently logged in User

--- api/user/index.ts
+++ api/user/index.ts
@@ -1,7 +1,11 @@
 import { getUserSessionTimeout, RoleGroupSessionTimeout } from '@hmcts/rpx-xui-node-lib'
+import { UserInfo } from 'auth/interfaces/UserInfo'
+import { AxiosResponse } from 'axios'
 import { NextFunction, Response } from 'express'
 import { getConfigValue } from '../configuration'
-import { CASE_SHARE_PERMISSIONS, SESSION_TIMEOUTS } from '../configuration/references'
+import { CASE_SHARE_PERMISSIONS, SERVICES_ROLE_ASSIGNMENT_API_PATH, SESSION_TIMEOUTS } from '../configuration/references'
+import { setHeaders } from '../lib/proxy'
+import { http } from '../lib/http'
 
 export async function getUserDetails(req, res: Response, next: NextFunction) {
 
@@ -12,13 +16,47 @@ export async function getUserDetails(req, res: Response, next: NextFunction) {
     const canShareCases = roles.some(role => permissions.includes(role))
     const sessionTimeouts = getConfigValue(SESSION_TIMEOUTS) as RoleGroupSessionTimeout[]
     const sessionTimeout = getUserSessionTimeout(roles, sessionTimeouts)
+    const locationInfo = await getUserRoleAssignments(req.session.passport.user.userinfo, req)
     const userInfo = {...req.session.passport.user.userinfo, token: `Bearer ${req.session.passport.user.tokenset.accessToken}`}
     res.send({
       canShareCases,
       sessionTimeout,
       userInfo,
+      locationInfo
     })
   } catch (error) {
     next(error)
   }
+}
+
+export async function getUserRoleAssignments(userInfo: UserInfo, req): Promise<any []> {
+  const locationInfo = req.session.roleAssignmentResponse ?
+                      getLocationInfo(req.session.roleAssignmentResponse) : 
+                      await getRoleAssignmentForUser(userInfo, req);
+  return locationInfo
+}
+
+export async function getRoleAssignmentForUser(userInfo: UserInfo, req: any): Promise<any []> {
+  let locationInfo = []
+  const baseUrl = getConfigValue(SERVICES_ROLE_ASSIGNMENT_API_PATH)
+  const path = `${baseUrl}/am/role-assignments/actors/${userInfo.uid}`
+  const headers = setHeaders(req)
+  try {
+    const response: AxiosResponse = await http.get(path, { headers })
+    locationInfo = getLocationInfo(response.data.roleAssignmentResponse)
+    req.session.roleAssignmentResponse = response.data.roleAssignmentResponse
+  } catch (error) {
+    console.log(error)
+  }
+  return locationInfo
+}
+
+export function getLocationInfo(roleAssignmentResponse: any): any [] {
+  let locationInfo = []
+  roleAssignmentResponse.forEach(roleAssignment => {
+    if(roleAssignment.attributes.primaryLocation) {
+      locationInfo.push(roleAssignment.attributes)
+    }
+  });
+  return locationInfo
 }

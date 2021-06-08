fb784604d987d4855493c7edb976c33fdb0b29a6
phillip-whitaker <75982584+phillip-whitaker@users.noreply.github.com>
2021-06-08 10:04:24 +0100

    added logic for deep linking (#926)
    * added logic for deep linking
    
    * fixed tests
    
    * mmade amends as per Paul Graham's review
    
    * added additional tests for auth guard
    
    * remove extraneous space
    
    * fixed linting errors
    
    * made changes as per recommendations from Adnan
    
    * fixed linting

---
+++ src/app/services/window-location/window-location.service.spec.ts
@@ -0,0 +1,9 @@
+import { WindowLocationService } from './window-location.service';
+
+describe('Window Location Service', () => {
+  it('should return pathname', () => {
+    const service = new WindowLocationService();
+
+    expect(service.getPathName()).toBeTruthy();
+  });
+});

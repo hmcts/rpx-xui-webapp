e079a0827a918550cb292d5764631498208c1863
Uday Denduluri <uday.denduluri@gmail.com>
2021-06-24 11:53:52 +0100

    spaces between h3

--- src/work-allocation-2/containers/all-work-home/all-work-home.component.ts
+++ src/work-allocation-2/containers/all-work-home/all-work-home.component.ts
@@ -3,7 +3,8 @@ import { SubNavigation } from '@hmcts/rpx-xui-common-lib';
 
 @Component({
 selector: 'exui-all-tasks',
-templateUrl: 'all-work-home.component.html'
+templateUrl: 'all-work-home.component.html',
+styleUrls: ['all-work-home.component.scss']
 })
 export class AllWorkHomeComponent {
     public subNavigationItems: SubNavigation[] = [

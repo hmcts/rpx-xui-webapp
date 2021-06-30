a04c82f3dc03621e8200c5167aa0ec44d9921ce8
kuda <ku-da@hotmail.co.uk>
2021-06-30 17:13:36 +0100

    add a full stop

--- src/work-allocation-2/enums/info-message.ts
+++ src/work-allocation-2/enums/info-message.ts
@@ -3,7 +3,7 @@ export enum InfoMessage {
   ASSIGNED_TASK_AVAILABLE_IN_MY_TASKS = 'You\'ve assigned yourself a task. It\'s available in My tasks.',
   CANCELLED_TASK = 'You\'ve cancelled a task.',
   LIST_OF_TASKS_REFRESHED = 'The list has been refreshed.',
-  MARKED_TASK_AS_DONE = 'You\'ve marked a task as done. It has been removed from the task list',
+  MARKED_TASK_AS_DONE = 'You\'ve marked a task as done. It has been removed from the task list.',
   REASSIGNED_TASK = 'You\'ve reassigned a task to somebody else.',
   TASK_NO_LONGER_AVAILABLE = 'The task is no longer available.',
   UNASSIGNED_TASK = 'You\'ve unassigned a task.',

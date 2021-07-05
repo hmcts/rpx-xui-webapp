d98b274f6e1672d686e09bb9921d1fbb041ffea1
sreekanthpuligadda <52752138+sreekanthpuligadda@users.noreply.github.com>
2021-07-05 10:00:33 +0100

    WA pact tests updated to resolve data mismatch on provider side tests… (#1152)
    * WA pact tests updated to resolve data mismatch on provider side tests and state value.
    
    * Tests updated
    
    * review comments addressed
    
    * fixed lint issues and PACT date time matcher to match with provider date time format.
    
    * WA pact test update removed content type on no body response from assign task api

--- api/test/pact/pact-tests/wa/searchTasks.spec.ts
+++ api/test/pact/pact-tests/wa/searchTasks.spec.ts
@@ -1,47 +1,81 @@
-import { expect } from 'chai';
+import * as chai from 'chai';
+import { expect } from 'chai'
+
 import { PactTestSetup } from '../settings/provider.mock';
-import { searchTasks } from "../../pactUtil";
+// import { searchTasks } from "../../pactUtil";
+import * as sinon from 'sinon'
+
+import * as config from 'config'
+import { mockReq, mockRes } from 'sinon-express-mock';
+
+import { getSearchTaskOverrides} from '../utils/configOverride';
+import { requireReloaded } from '../utils/moduleUtil';
 
 const { Matchers } = require('@pact-foundation/pact');
-const { somethingLike } = Matchers;
+import { DateTimeMatcher } from '../utils/matchers';
+const { somethingLike, iso8601DateTime, term } = Matchers;
 const pactSetUp = new PactTestSetup({ provider: 'wa_task_management_api_search', port: 8000 });
 
-const taskId = "f782bde3-8d51-11eb-a9a4-06d032acc76d"
+
+const taskId1 = "4d4b6fgh-c91f-433f-92ac-e456ae34f72a"
+const taskId2 = "fda422de-b381-43ff-94ea-eea5790188a3"
 
 describe("Task management api, Search task", () => {
 
     const RESPONSE_BODY = {
         "tasks":[
                 {
-                "assignee": somethingLike("004b7164-0943-41b5-95fc-39794af4a9fe"),
-                "auto_assigned": somethingLike(true),
-                "case_category": somethingLike("protection"),
-                "case_id": somethingLike("1616666869740714"),
-                "case_name": somethingLike("Bob Smith"),
-                "case_type_id": somethingLike("Asylum"),
-                "created_date": somethingLike("2020-09-05T14:47:01.250542+01:00"),
-                "due_date": somethingLike("2020-09-25T14:47:01.250542+01:00"),
-                "execution_type": somethingLike("Case Management Task"),
-                "id": somethingLike(taskId),
-                "jurisdiction": somethingLike("IA"),
-                "location": somethingLike("765324"),
-                "location_name": somethingLike("Taylor House"),
-                "name": somethingLike("task name"),
-                "region": somethingLike("1"),
-                "security_classification": somethingLike("PUBLIC"),
+                "id": somethingLike("4d4b6fgh-c91f-433f-92ac-e456ae34f72a"),
+                "name": somethingLike("Review the appeal"),
+                "type": somethingLike("reviewTheAppeal"),
                 "task_state": somethingLike("assigned"),
                 "task_system": somethingLike("SELF"),
-                "task_title": somethingLike("task name"),
-                "type": somethingLike("wa-task-configuration-api-task"),
-                "warnings": somethingLike(false)
+                "security_classification": somethingLike("PUBLIC"),
+                "task_title": somethingLike("Review the appeal"),
+                "created_date": term(DateTimeMatcher("2021-06-30T16:53:10+0100")),
+                "due_date": term(DateTimeMatcher("2021-06-30T16:53:10+0100")),
+                "assignee": somethingLike("10bac6bf-80a7-4c81-b2db-516aba826be6"),
+                "auto_assigned": somethingLike(false),
+                "execution_type": somethingLike("Case Management Task"),
+                "jurisdiction": somethingLike("IA"),
+                "region": somethingLike("1"),
+                "location": somethingLike("765324"),
+                "location_name": somethingLike("Taylor House"),
+                "case_type_id": somethingLike("Asylum"),
+                "case_id": somethingLike("1617708245335311"),
+                "case_category": somethingLike("refusalOfHumanRights"), 
+                "case_name": somethingLike("Bob Smith"),  
+                "warnings": somethingLike(false) 
+            },
+            {
+                "id": somethingLike("fda422de-b381-43ff-94ea-eea5790188a3"),
+                "name": somethingLike("Review the appeal"),
+                "type": somethingLike("reviewTheAppeal"),
+                "task_state": somethingLike("unassigned"),
+                "task_system": somethingLike("SELF"),
+                "security_classification": somethingLike("PUBLIC"),
+                "task_title": somethingLike("Review the appeal"),
+                "created_date": term(DateTimeMatcher("2021-06-30T16:53:10+0100")),
+                "due_date": term(DateTimeMatcher("2021-06-30T16:53:10+0100")),
+                "auto_assigned": somethingLike(true),
+                "execution_type": somethingLike("Case Management Task"),
+                "jurisdiction": somethingLike("IA"),
+                "region": somethingLike("1"),
+                "location": somethingLike("765324"),
+                "location_name": somethingLike("Taylor House"),
+                "case_type_id": somethingLike("Asylum"),
+                "case_id": somethingLike("1617708245308495"),
+                "case_category": somethingLike("refusalOfHumanRights"),
+                "case_name": somethingLike("John Doe"),
+                "warnings": somethingLike(true)
             }
         ]
     }
 
-    const mockRequest = {
+    const mockSearchRequestBody = {
         "search_parameters": [
             {
-                "key": "CASE_ID",
+                "key": "caseId",
                 "operator": "AFTER",
                 "values": [
                     "f782bde3-8d51-11eb-a9a4-06d032acc76d"
@@ -50,16 +84,21 @@ describe("Task management api, Search task", () => {
         ],
         "sorting_parameters": [
             {
-                "sort_by": "CASE_CATEGORY",
-                "sort_order": "ASCENDANT"
+                "sort_by": "case_category",
+                "sort_order": "asc"
             }
         ]
     }
 
     describe("post /task", () => {
+        let sandbox: sinon.SinonSandbox = sinon.createSandbox();
+        let next;
+        beforeEach(() => {
+            next = sandbox.spy();
+        });
 
-        const jwt = 'some-access-token';
         before(async () => {
+           
             await pactSetUp.provider.setup()
             const interaction = {
                 state: "appropriate tasks are returned by criteria",
@@ -68,16 +107,16 @@ describe("Task management api, Search task", () => {
                     method: "POST",
                     path: "/task",
                     headers: {
-                        'Authorization': 'Bearer some-access-token',
-                        'ServiceAuthorization': 'some service authorisation',
-                        'Content-Type': 'application/json'
+                        'Authorization': 'Bearer someAuthorizationToken',
+                        'ServiceAuthorization': 'Bearer someServiceAuthorizationToken',
+                        // 'content-Type': 'application/json',
+
                     },
-                    body : mockRequest
+                    body: mockSearchRequestBody
                 },
                 willRespondWith: {
                     status: 200,
                     headers: {
-                        "Content-Type": "application/json",
                     },
                     body: RESPONSE_BODY,
                 },
@@ -85,48 +124,65 @@ describe("Task management api, Search task", () => {
             // @ts-ignore
             pactSetUp.provider.addInteraction(interaction)
         })
+
+        afterEach(() => {
+            sandbox.restore();
+            sinon.reset();
+        });
+
+
         it("returns the correct response", async () => {
-            const taskUrl = `${pactSetUp.provider.mockService.baseUrl}/task`;
+            const configValues = getSearchTaskOverrides(pactSetUp.provider.mockService.baseUrl)
+            sandbox.stub(config,'get').callsFake((prop) =>{
+                return configValues[prop];
+            });
+            
+            const { searchTask } = requireReloaded('../../../../workAllocation/index');
 
-            const response: Promise<any> = searchTasks(taskUrl,mockRequest);
+            const req = mockReq({
+                headers:{
+                    'Authorization': 'Bearer someAuthorizationToken',
+                    'ServiceAuthorization': 'Bearer someServiceAuthorizationToken',
+                    // 'content-Type': 'application/json',
+                },
+                body: { searchRequest: mockSearchRequestBody, view: 'task view' },
+            });
+            let returnedResponse = null;
+            const response = mockRes();
+            response.send = (ret) => {
+                returnedResponse = ret
+            };
 
-            response.then((axiosResponse) => {
-                const dto: any = axiosResponse;
-                assertResponses(dto.data);
-                console.log("search");
-
-            }).then(() => {
+            try{
+                await searchTask(req, response, next);
+              
+                assertResponses(returnedResponse);
                 pactSetUp.provider.verify()
                 pactSetUp.provider.finalize()
-            })
+            }catch(err){
+                console.log(err.stack);
+                pactSetUp.provider.verify()
+                pactSetUp.provider.finalize()
+                throw new Error(err);
+            }
+          
         })
     })
 })
 
 function assertResponses(dto: any) {
-    expect(dto.tasks[0].assignee).to.be.equal("004b7164-0943-41b5-95fc-39794af4a9fe");
-
-    expect(dto.tasks[0].auto_assigned).to.be.equal(true);
-    expect(dto.tasks[0].case_category).to.be.equal("protection");
-    expect(dto.tasks[0].case_id).to.be.equal("1616666869740714");
+    expect(dto.tasks[0].assignee).to.be.equal("10bac6bf-80a7-4c81-b2db-516aba826be6");
     expect(dto.tasks[0].case_name).to.be.equal("Bob Smith");
-    expect(dto.tasks[0].case_type_id).to.be.equal("Asylum");
-    expect(dto.tasks[0].created_date).to.be.equal("2020-09-05T14:47:01.250542+01:00");
-    expect(dto.tasks[0].due_date).to.be.equal("2020-09-25T14:47:01.250542+01:00");
-    expect(dto.tasks[0].execution_type).to.be.equal("Case Management Task");
+    expect(dto.tasks[0].case_category).to.be.equal("refusalOfHumanRights");
 
-    expect(dto.tasks[0].id).to.be.equal(taskId);
-    expect(dto.tasks[0].jurisdiction).to.be.equal("IA");
-    expect(dto.tasks[0].location).to.be.equal("765324");
+
+    expect(dto.tasks[0].case_id).to.be.equal("1617708245335311");
     expect(dto.tasks[0].location_name).to.be.equal("Taylor House");
-    expect(dto.tasks[0].name).to.be.equal("task name");
-    expect(dto.tasks[0].region).to.be.equal("1");
-    expect(dto.tasks[0].security_classification).to.be.equal("PUBLIC");
-    expect(dto.tasks[0].task_state).to.be.equal("assigned");
-    expect(dto.tasks[0].task_system).to.be.equal("SELF");
-    expect(dto.tasks[0].task_title).to.be.equal("task name");
-    expect(dto.tasks[0].type).to.be.equal("wa-task-configuration-api-task");
-    expect(dto.tasks[0].warnings).to.be.equal(false);
+    expect(dto.tasks[0].task_title).to.be.equal("Review the appeal");
+    expect(dto.tasks[0].dueDate).to.be.equal("2021-06-30T16:53:10+0100");
+
+    expect(dto.tasks[0].assignee).to.be.equal("10bac6bf-80a7-4c81-b2db-516aba826be6");
+
 
 }
 

c9797e4b6cd54169ab5756d8f8e7fbede1232400
Paul Howes <paul.howes@HMCTS.NET>
2021-07-10 13:32:43 +0100

    Sort imports to resolve build failure on deployment.

--- api/workAllocation2/util.ts
+++ api/workAllocation2/util.ts
@@ -1,10 +1,10 @@
+import { Action, Caseworker, CaseworkerApi, Location, LocationApi } from './interfaces/common';
 import { AxiosResponse } from 'axios';
 import { EnhancedRequest } from 'lib/models';
 import { http } from '../lib/http';
+import { Person, PersonDomain } from './interfaces/person';
 import { setHeaders } from '../lib/proxy';
 import { TaskPermission, VIEW_PERMISSIONS_ACTIONS_MATRIX } from './constants/actions';
-import { Person, PersonDomain } from './interfaces/person';
-import { Action, Caseworker, CaseworkerApi, Location, LocationApi } from './interfaces/common';
 
 export function prepareGetTaskUrl(baseUrl: string, taskId: string): string {
   return `${baseUrl}/task/${taskId}`;

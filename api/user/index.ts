8f5fc49acb0df42acc1614b6f088a9ac5db9fd80
RiteshHMCTS <ritesh.dsouza@HMCTS.net>
2021-07-14 15:52:55 +0100

    return the object to avoid error

--- api/user/index.ts
+++ api/user/index.ts
@@ -5,7 +5,7 @@ import { CASE_SHARE_PERMISSIONS, SESSION_TIMEOUTS } from '../configuration/refer
 
 export async function getUserDetails(req, res: Response, next: NextFunction) {
   if (!req.session || !req.session.passport || !req.session.passport.user) {
-    res.send({});
+    return res.send({}).status(200);
   }
 
   try {

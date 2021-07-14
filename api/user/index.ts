790b9a64097b7aa9d01c4bb4aee9ecd19e9f3ca7
RiteshHMCTS <ritesh.dsouza@HMCTS.net>
2021-07-14 11:24:05 +0100

    checking for null value

--- api/user/index.ts
+++ api/user/index.ts
@@ -4,6 +4,9 @@ import { getConfigValue } from '../configuration'
 import { CASE_SHARE_PERMISSIONS, SESSION_TIMEOUTS } from '../configuration/references'
 
 export async function getUserDetails(req, res: Response, next: NextFunction) {
+  if(!req.session || !req.session.passport || !req.session.passport.user) {
+    res.send({});
+  }
 
   try {
     const { roles } = req.session.passport.user.userinfo

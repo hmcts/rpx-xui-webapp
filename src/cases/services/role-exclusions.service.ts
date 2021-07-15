251ecacf2a3b9fa1f3fed1efacf56edac9773e55
Uday Denduluri <uday.denduluri@gmail.com>
2021-07-15 09:47:07 +0100

    SearchFilterService unit test

--- src/cases/services/role-exclusions.service.ts
+++ src/cases/services/role-exclusions.service.ts
@@ -5,9 +5,10 @@ import { RoleExclusion } from '../models/role-exclusions/role-exclusion.model';
 
 @Injectable()
 export class RoleExclusionsService {
+    public static exclusionsUrl = '/api/user/exclusions';
     constructor(private http: HttpClient) { }
 
     public getCurrentUserRoleExclusions(): Observable<RoleExclusion[]> {
-        return this.http.get<any>('/api/user/exclusions');
+        return this.http.get<any>(RoleExclusionsService.exclusionsUrl);
     }
 }

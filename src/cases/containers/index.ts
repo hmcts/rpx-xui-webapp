9be18572f7a80cd7dd2d54660565e7c85b63d8f8
connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>
2021-07-16 11:34:21 +0100

    Adding in generic allocate-role component with container (#1163)
    * Adding in generic allocate-role component with container
    
    - Added tests
    - Added temporary routes for testing
    
    * Remove stub methods
    
    * Adding in selector for case allocator
    
    Also, adding tests for container and global constants for case allocator role
    
    * Update allocate-role-container.component.ts
    
    * Replace with correct styling
    
    * Change after PR comment
    
    Rearrange util function
    Add Role to start of enums
    Make locationInfo list specific
    
    * Adding in extra interfaces
    
    * LocationApi instead of Location
    
    * Replace import
    
    * Update roleAssignment.ts
    
    * Editing for functionality and tests

--- src/cases/containers/index.ts
+++ src/cases/containers/index.ts
@@ -1,18 +1,19 @@
+import { AllocateRoleContainerComponent } from './allocate-role-container/allocate-role-container.component';
+import { CaseCreateSubmitComponent } from './case-create-submit/case-create-submit.component';
+import { CasesCreateComponent } from './case-create/case-create.component';
+import { CaseDetailsHomeComponent } from './case-details-home/case-details-home.component';
+import { CaseDetailsComponent } from './case-details/case-details.component';
+import { CaseFilterComponent } from './case-filter/case-filter.component';
 import { CaseHomeComponent } from './case-home/case-home.component';
 import { CaseListComponent } from './case-list/case-list.component';
-import { CaseFilterComponent } from './case-filter/case-filter.component';
 import { CaseSearchComponent } from './case-search/case-search.component';
-import { CasesCreateComponent } from './case-create/case-create.component';
-import { CaseDetailsComponent } from './case-details/case-details.component';
-import { CaseCreateSubmitComponent } from './case-create-submit/case-create-submit.component';
-import { CaseDetailsHomeComponent } from './case-details-home/case-details-home.component';
-import { CaseShareComponent } from './case-share/case-share.component';
-import { CaseShareConfirmComponent } from './case-share-confirm/case-share-confirm.component';
 import { CaseShareCompleteComponent } from './case-share-complete/case-share-complete.component';
-import { AllocateRoleContainerComponent } from './role-container/allocate-role-container.component';
+import { CaseShareConfirmComponent } from './case-share-confirm/case-share-confirm.component';
+import { CaseShareComponent } from './case-share/case-share.component';
 
 
 export const containers: any[] = [
+  AllocateRoleContainerComponent,
   CaseListComponent,
   CasesCreateComponent,
   CaseFilterComponent,
@@ -27,6 +28,7 @@ export const containers: any[] = [
   AllocateRoleContainerComponent,
 ];
 
+export * from './allocate-role-container/allocate-role-container.component';
 export * from './case-list/case-list.component';
 export * from './case-create/case-create.component';
 export * from './case-filter/case-filter.component';
@@ -37,4 +39,3 @@ export * from './case-details-home/case-details-home.component';
 export * from './case-share/case-share.component';
 export * from './case-share-confirm/case-share-confirm.component';
 export * from './case-share-complete/case-share-complete.component';
-export * from './role-container/allocate-role-container.component';

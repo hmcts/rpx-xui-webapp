8ebf9dd75063b30f72710cfbdc84b7550d7a806c
Raymond Liang <ray.liang@atos.net>
2021-07-16 11:07:36 +0100

    feature/EUI-4296-role-access-infra

---
+++ src/role-access/containers/exclusion/choose-exclusion/choose-exclusion.component.spec.ts
@@ -0,0 +1,34 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { provideMockStore } from '@ngrx/store/testing';
+import { ChooseExclusionComponent } from './choose-exclusion.component';
+
+describe('ChooseExclusionComponent', () => {
+  let component: ChooseExclusionComponent;
+  let fixture: ComponentFixture<ChooseExclusionComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ChooseExclusionComponent],
+      providers: [
+        provideMockStore()
+      ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChooseExclusionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  afterEach(() => {
+    component = null;
+    fixture.destroy();
+  });
+});

3beb37d5e4e08e255fc0cddcefc7281bf8838033
issshiO <64783858+issshiO@users.noreply.github.com>
2021-07-19 13:06:46 +0100

    EUI-4112-cve-fix

--- src/app/services/ccd-config/ccd-case.config.ts
+++ src/app/services/ccd-config/ccd-case.config.ts
@@ -80,6 +80,9 @@ export class AppConfig extends AbstractAppConfig {
     return this.config.hrs_url;
   }
 
+  public getRemoteHrsUrl() {
+    return this.config.remote_hrs_url;
+  }
 
   public getCaseHistoryUrl(caseId: string, eventId: string) {
     return (

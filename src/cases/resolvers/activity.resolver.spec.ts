6f200500a5bf2b94930fa382d6fac3f32f3a2dc3
Paul Graham <paul.graham@hmcts.net>
2021-07-19 11:27:18 +0100

    Activity Resolver
    Adjusted the way the ActivityResolver works so it now verifies whether the user is authorised ONLY if activity tracking is not toggled off.

--- src/cases/resolvers/activity.resolver.spec.ts
+++ src/cases/resolvers/activity.resolver.spec.ts
@@ -1,30 +1,57 @@
 import { ActivityService } from '@hmcts/ccd-case-ui-toolkit';
+import { FeatureToggleService } from '@hmcts/rpx-xui-common-lib';
+import { of } from 'rxjs';
+
 import { ActivityResolver } from './activity.resolver';
 
 describe('ActivityResolver', () => {
-
+  let featureToggleService: any;
   let activityService: any;
   let activityResolver: ActivityResolver;
 
   beforeEach(() => {
     activityService = jasmine.createSpyObj<ActivityService>('activityService', ['verifyUserIsAuthorized']);
-    activityService.verifyUserIsAuthorized.and.returnValue(true);
-
-    activityResolver = new ActivityResolver(activityService);
+    featureToggleService = jasmine.createSpyObj<FeatureToggleService>('featureToggleService', ['getValue']);
   });
 
-  it('should verify if user is authorised for activity on resolve and return true when is authorised', () => {
-    let result: boolean;
-    activityResolver.resolve().subscribe(r => result = r);
-    expect(activityService.verifyUserIsAuthorized).toHaveBeenCalled();
-    expect(result).toBe(true);
+  const instantiate = (mode: string): void => {
+    featureToggleService.getValue.and.returnValue(of(mode));
+    activityResolver = new ActivityResolver(activityService, featureToggleService);
+  };
+
+  describe('when activity tracking is toggled off', () => {
+
+    beforeEach(() => {
+      instantiate('off');
+    });
+
+    it('should not bother to verify the user and return false', () => {
+      let result: boolean;
+      activityResolver.resolve().subscribe(r => result = r);
+      expect(activityService.verifyUserIsAuthorized).not.toHaveBeenCalled();
+      expect(result).toBe(false);
+    });
   });
 
-  it('should verify if user is authorised for activity on resolve and return true when is not authorised', () => {
-    let result: boolean;
-    activityResolver.resolve().subscribe(r => result = r);
-    activityService.verifyUserIsAuthorized.and.returnValue(false);
+  describe('when activity tracking is set to "polling"', () => {
 
-    expect(result).toBe(true);
+    beforeEach(() => {
+      instantiate('polling');
+    });
+
+    it('should verify if the user is authorised and return true', () => {
+      let result: boolean;
+      activityService.verifyUserIsAuthorized.and.returnValue(true);
+      activityResolver.resolve().subscribe(r => result = r);
+      expect(activityService.verifyUserIsAuthorized).toHaveBeenCalled();
+      expect(result).toBe(true);
+    });
+    it('should verify if the user is authorised and return true even when they are NOT authorised', () => {
+      let result: boolean;
+      activityService.verifyUserIsAuthorized.and.returnValue(false);
+      activityResolver.resolve().subscribe(r => result = r);
+      expect(activityService.verifyUserIsAuthorized).toHaveBeenCalled();
+      expect(result).toBe(true);
+    });
   });
 });

caabb62f8743ce76f44913cc547ac2ea9541a5b2
connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>
2021-07-20 16:33:44 +0100

    Fixing issue with role-exclusions

--- src/role-access/components/exclusion/index.ts
+++
@@ -1,7 +0,0 @@
-import { DescribeExclusionComponent } from './describe-exclusion/describe-exclusion.component';
-import { RoleExclusionsComponent } from './role-exclusions/role-exclusions.component';
-
-export const components: any[] = [ DescribeExclusionComponent, RoleExclusionsComponent ];
-
-export * from './describe-exclusion/describe-exclusion.component';
-export * from './role-exclusions/role-exclusions.component';
--- src/role-access/components/index.ts
+++ src/role-access/components/index.ts
@@ -1,8 +1,11 @@
 import { ChooseRoleComponent } from './choose-role/choose-role.component';
 import { AllocateRoleComponent } from './allocate-role/allocate-role.component';
+import { DescribeExclusionComponent } from './describe-exclusion/describe-exclusion.component';
+import { RoleExclusionsComponent } from './role-exclusions/role-exclusions.component';
 
-export const components: any[] = [ AllocateRoleComponent, ChooseRoleComponent ];
+export const components: any[] = [ AllocateRoleComponent, ChooseRoleComponent, DescribeExclusionComponent, RoleExclusionsComponent ];
 
 export * from './allocate-role/allocate-role.component';
 export * from './choose-role/choose-role.component';
-export * from './exclusion';
+export * from './describe-exclusion/describe-exclusion.component';
+export * from './role-exclusions/role-exclusions.component';

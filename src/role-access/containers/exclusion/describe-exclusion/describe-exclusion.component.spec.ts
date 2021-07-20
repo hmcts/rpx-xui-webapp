5f63ef3ec7559d0bbcba41719df03602a42948bc
Uday Denduluri <uday.denduluri@gmail.com>
2021-07-20 15:39:49 +0100

    DescribeExclusionComponent ngrx unit test

--- src/role-access/containers/exclusion/describe-exclusion/describe-exclusion.component.spec.ts
+++ src/role-access/containers/exclusion/describe-exclusion/describe-exclusion.component.spec.ts
@@ -1,33 +1,46 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideMockStore } from '@ngrx/store/testing';
+import { async } from '@angular/core/testing';
 import { DescribeExclusionComponent } from './describe-exclusion.component';
+import { Validators } from '@angular/forms';
+import { ExclusionNavigationEvent, ExclusionState } from '../../../models';
+import { UpdateDescribeExclusionText } from 'src/role-access/store';
 
 describe('DescribeExclusionComponent', () => {
   let component: DescribeExclusionComponent;
-  let fixture: ComponentFixture<DescribeExclusionComponent>;
-
+  let mockStore: any;
+  let mockFormBuilder: any;
+  let formGroup: any;
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [DescribeExclusionComponent],
-      providers: [
-        provideMockStore()
-      ]
-    })
-      .compileComponents();
+    mockStore = jasmine.createSpyObj('mockFormBuilder', ['pipe', 'dispatch']);
+    mockFormBuilder = jasmine.createSpyObj('mockFormBuilder', ['group']);
+    formGroup = jasmine.createSpyObj('formGroup', ['get']);
+    mockFormBuilder.group.and.returnValue(formGroup);
+    component = new DescribeExclusionComponent(mockStore, mockFormBuilder);
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DescribeExclusionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
+  it('should create and initialise FormGroup', () => {
     expect(component).toBeTruthy();
+    expect(mockFormBuilder.group).toHaveBeenCalledWith({['text']: ['', [Validators.required]]});
+    expect(component.formGroup).not.toBeNull();
   });
 
-  afterEach(() => {
-    component = null;
-    fixture.destroy();
+  it('navigationHandler should handleError', () => {
+    formGroup.get.and.returnValue(jasmine.createSpyObj('control', ['markAsTouched']));
+    formGroup.valid = false;
+    const navEvent = ExclusionNavigationEvent.CONTINUE;
+    component.navigationHandler(navEvent);
+    expect(component.error).toEqual({
+      title: 'There is a problem',
+      description: 'Enter exclusion',
+      fieldId: 'exclusion-description'
+    });
+  });
+
+  it('navigationHandler should dispatch action UpdateDescribeExclusionText', () => {
+    formGroup.get.and.returnValue(jasmine.createSpyObj('control', ['markAsTouched']));
+    formGroup.valid = true;
+    formGroup.value = {text: 'some text'};
+    const navEvent = ExclusionNavigationEvent.CONTINUE;
+    component.navigationHandler(navEvent);
+    expect(mockStore.dispatch).toHaveBeenCalledWith(new UpdateDescribeExclusionText(ExclusionState.CHECK_ANSWERS, 'some text'));
   });
 });

9c8518d4b0a5a499389a723d80e83cfcbc0f21e9
connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>
2021-07-21 09:47:50 +0100

    Remove reference to no longer existing component

--- src/role-access/components/index.ts
+++ src/role-access/components/index.ts
@@ -1,11 +1,9 @@
 import { ChooseRoleComponent } from './choose-role/choose-role.component';
 import { AllocateRoleComponent } from './allocate-role/allocate-role.component';
-import { DescribeExclusionComponent } from './describe-exclusion/describe-exclusion.component';
 import { RoleExclusionsComponent } from './role-exclusions/role-exclusions.component';
 
-export const components: any[] = [ AllocateRoleComponent, ChooseRoleComponent, DescribeExclusionComponent, RoleExclusionsComponent ];
+export const components: any[] = [ AllocateRoleComponent, ChooseRoleComponent, RoleExclusionsComponent ];
 
 export * from './allocate-role/allocate-role.component';
 export * from './choose-role/choose-role.component';
-export * from './describe-exclusion/describe-exclusion.component';
 export * from './role-exclusions/role-exclusions.component';

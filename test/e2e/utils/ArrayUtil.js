10daf71bb02a01b3e88d0699fd1198cf5b2b5b9c
sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
2021-07-22 14:40:07 +0100

    ngIntegration WA test enhancements and improvements

--- test/e2e/utils/ArrayUtil.js
+++ test/e2e/utils/ArrayUtil.js
@@ -1,14 +1,17 @@
 
+class ArrayUtil {
 
-class ArrayUtil{
-
-    async map(arr, callback){
+    async map(arr, callback) {
         return await Promise.all(arr.map(callback));
+    }
+
+    async forEach(arr, callback) {
+        await Promise.all(arr.map(callback));
     }
 
     async filter(arr, callback) {
         const results = await Promise.all(arr.map(callback));
-        return arr.filter((element,index) => results[index]);
+        return arr.filter((element, index) => results[index]);
     }
 
 }

b7c1420146aba04120501e9039bd9d19a79ff7f8
Paul Howes <paul.howes@HMCTS.NET>
2021-07-26 12:35:51 +0100

    Add info message component to module

--- src/work-allocation/containers/index.ts
+++ src/work-allocation/containers/index.ts
@@ -1,3 +1,4 @@
+import { InfoMessageComponent } from '../components';
 import { AvailableTasksComponent } from './available-tasks/available-tasks.component';
 import { InfoMessageContainerComponent } from './info-message-container/info-message-container.component';
 import { MyTasksComponent } from './my-tasks/my-tasks.component';
@@ -11,7 +12,7 @@ import { TaskManagerComponent } from './task-manager/task-manager.component';
 import { WorkAllocationHomeComponent } from './work-allocation-home/work-allocation-home.component';
 
 export const containers: any[] = [
-  AvailableTasksComponent, InfoMessageContainerComponent, MyTasksComponent,
+  AvailableTasksComponent, InfoMessageComponent, InfoMessageContainerComponent, MyTasksComponent,
   TaskActionContainerComponent, TaskAssignmentContainerComponent, TaskHomeComponent, TaskListComponent,
   TaskListWrapperComponent, TaskManagerComponent, TaskManagerListComponent, WorkAllocationHomeComponent
 ];

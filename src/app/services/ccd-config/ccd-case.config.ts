a036f3293c85a1013c13a49784002a3e890b66ea
RiteshHMCTS <ritesh.dsouza@HMCTS.net>
2021-07-29 10:26:14 +0100

    missing functions

--- src/app/services/ccd-config/ccd-case.config.ts
+++ src/app/services/ccd-config/ccd-case.config.ts
@@ -148,4 +148,12 @@ export class AppConfig extends AbstractAppConfig {
   public getWorkAllocationApiUrl(): string {
     return this.workallocationUrl;
   }
+
+  public getHrsUrl(): string {
+    return '';
+  }
+
+  public getRemoteHrsUrl(): string {
+    return '';
+  }
 }

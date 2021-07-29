9b5317ec23e5c02b932aabeef3b4927a977c9b48
Daniel Lam (A533913) <Daniel-Lam@users.noreply.github.com>
2021-07-29 12:00:06 +0100

    EUI-3955: All work > Tasks tab > Assign task (#1177)
    * all work component with changes
    
    * adding sort functionality
    
    * pageSessionKey
    
    * adding Unit test for AllWorkTaskComponent
    
    * AllWorkHomeComponent unit tests
    
    * fix src
    
    * added task-list-container class and wrapped it in div to control the styling
    
    * Adding in all work manage link logic
    
    * sorting persistency and fix it
    
    * EUI-3955: All work > Tasks tab > Assign task
    
    Ensure correct verb ("assign") is used on task assignment screens and messages. Also ensure navigation back to "All work" on completing task reassignment or cancelling the process.
    
    * EUI-4194: Accessibility change for "Assign Task" journey
    
    Change confirmation screen and success message wording to match screenshot in ticket.
    
    * Unit tests for EUI-3955 and EUI-4194
    
    Add unit tests for correct page navigation, message display, and success/failure cases for task (re)assignment.
    
    * Fix lint error
    
    Correct "shadowed name" error.
    
    * Fix lint warnings
    
    Correct various warnings in changed files.
    
    * Remove duplicate constant
    
    * Remove extra space before </table> tag
    
    * Addressing PR review comments from @rayliangatos
    
    * Remove error message component added... in error :)
    
    * Change default "return URL" to empty string
    
    Set fallback to empty string to return the user to the application home page, if `returnUrl` is not available because the user has used browser navigation buttons, which clear the `window.history.state` object (containing `returnUrl`).
    
    * Functional test fix
    
    * Restore missing import
    
    Restore import removed in error, during merge conflict resolution.
    
    * Restore pageObject fix
    
    Restore fix lost in merge.
    
    * Address PR review comment from @paulhowes-HMCTS
    
    Move conditional verb logic to component class and choose relevant phrase instead of just the verb.
    
    Co-authored-by: Uday Denduluri <uday.denduluri@gmail.com>
    Co-authored-by: connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>

--- src/work-allocation-2/containers/task-assignment-confirm/task-assignment-confirm.component.spec.ts
+++ src/work-allocation-2/containers/task-assignment-confirm/task-assignment-confirm.component.spec.ts
@@ -4,13 +4,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ExuiCommonLibModule } from '@hmcts/rpx-xui-common-lib';
-import { Observable } from 'rxjs';
-import { WorkAllocationComponentsModule } from 'src/work-allocation-2/components/work-allocation.components.module';
-import { WorkAllocationTaskService } from 'src/work-allocation-2/services';
-import { TaskActionType } from 'src/work-allocation/enums';
+import { Observable, of, throwError } from 'rxjs';
 import { PersonRole } from '../../../../api/workAllocation2/interfaces/person';
 import { TaskActionConstants } from '../../components/constants';
+import { WorkAllocationComponentsModule } from '../../components/work-allocation.components.module';
+import { InfoMessage, InfoMessageType, TaskActionType } from '../../enums';
+import { InformationMessage } from '../../models/comms';
+import { InfoMessageCommService, WorkAllocationTaskService } from '../../services';
 import { getMockTasks } from '../../tests/utils.spec';
+import { REDIRECTS } from '../../utils';
 import { TaskAssignmentConfirmComponent } from './task-assignment-confirm.component';
 
 @Component({
@@ -32,9 +34,29 @@ describe('TaskAssignmentConfirmComponent', () => {
     domain: PersonRole.CASEWORKER
   };
   const mockTasks = getMockTasks();
-  const mockTaskService = jasmine.createSpyObj('mockTaskService', ['searchTask']);
+  // Provide a fake implementation of assignTask(), which returns different responses based on the task ID
+  const mockTaskService = jasmine.createSpyObj('mockTaskService', ['assignTask']);
+  mockTaskService.assignTask.and.callFake((taskId: any) => {
+    switch (taskId) {
+      // For testing recognised error status 401
+      case '401':
+        // Doesn't work with just `throwError({ status: 401 })`; need to "return the return"
+        return throwError({ status: 401 });
+      // For testing recognised error status 500
+      case '500':
+        return throwError({ status: 500 });
+      // For testing unrecognised error status
+      case '999':
+        return throwError({ status: 999 });
+      // For testing success case
+      default:
+        return of(null);
+    }
+  });
+  let mockInfoMessageCommService: jasmine.SpyObj<InfoMessageCommService>;
 
   beforeEach(() => {
+    mockInfoMessageCommService = jasmine.createSpyObj('mockInfoMessageCommService', ['nextMessage']);
     TestBed.configureTestingModule({
       imports: [
         CdkTableModule,
@@ -59,10 +81,11 @@ describe('TaskAssignmentConfirmComponent', () => {
               }
             },
             params: Observable.of({ task: mockTasks[0] }),
-            paramMap: Observable.of({ selectedPerson: SELECTED_PERSON})
+            paramMap: Observable.of({ selectedPerson: SELECTED_PERSON })
           }
         },
-        { provide: Router, useValue: { url: 'localhost/test'} }
+        { provide: Router, useValue: { url: 'localhost/test', navigate: (_1: any, _2: any) => {} } },
+        { provide: InfoMessageCommService, useValue: mockInfoMessageCommService }
       ]
     }).compileComponents();
     fixture = TestBed.createComponent(WrapperComponent);
@@ -82,6 +105,8 @@ describe('TaskAssignmentConfirmComponent', () => {
       domain: '2'
     };
     expect(component).toBeDefined();
+    const titleElement = fixture.debugElement.nativeElement.querySelector('.govuk-heading-l');
+    expect(titleElement.textContent).toContain('Check your answers');
   });
 
   it('should display the correct verb', () => {
@@ -94,7 +119,122 @@ describe('TaskAssignmentConfirmComponent', () => {
     };
     fixture.detectChanges();
 
-    const titleElement = document.querySelector('.govuk-caption-l');
+    const titleElement = fixture.debugElement.nativeElement.querySelector('.govuk-caption-l');
     expect(titleElement.textContent).toContain(TaskActionType.Reassign);
+    const hintElement = fixture.debugElement.nativeElement.querySelector('.govuk-hint');
+    expect(hintElement.textContent).toContain(TaskActionType.Reassign.toLowerCase());
+    const buttonElement = fixture.debugElement.nativeElement.querySelector('.govuk-button');
+    expect(buttonElement.textContent).toContain(TaskActionType.Reassign);
+  });
+
+  it('should go back to the "Find the person" page on clicking "Change", displaying the correct verb', () => {
+    component.verb = TaskActionType.Assign;
+    fixture.detectChanges();
+    const navigateSpy = spyOn(router, 'navigate');
+    component.onChange();
+    expect(navigateSpy).toHaveBeenCalled();
+    // Check the third part of the URL passed to router.navigate() is "assign"
+    expect(navigateSpy.calls.argsFor(0)[0][2]).toEqual(TaskActionType.Assign.toLowerCase());
+  });
+
+  it('should redirect to the "All work" page on cancelling task assignment', () => {
+    window.history.pushState({ returnUrl: 'all-work/tasks', showAssigneeColumn: false }, '', 'all-work/tasks');
+    const navigateSpy = spyOn(router, 'navigate');
+    component.onCancel();
+    expect(navigateSpy).toHaveBeenCalledWith(['all-work/tasks']);
+  });
+
+  it('should redirect to the fallback URL (\'\') on cancelling task assignment, if the return URL is not in the history', () => {
+    window.history.pushState({}, '');
+    const navigateSpy = spyOn(router, 'navigate');
+    component.onCancel();
+    expect(navigateSpy).toHaveBeenCalledWith(['']);
+  });
+
+  it('should return to the "All work" page on successful task assignment', () => {
+    window.history.pushState({ returnUrl: 'all-work/tasks', showAssigneeColumn: false }, '', 'all-work/tasks');
+    const navigateSpy = spyOn(router, 'navigate');
+    component.onSubmit();
+    expect(mockTaskService.assignTask).toHaveBeenCalledWith('task1111111', { userId: undefined });
+    expect(navigateSpy).toHaveBeenCalledWith(['all-work/tasks'], {
+      state: {
+        badRequest: false,
+        retainMessages: true
+      }
+    });
+  });
+
+  it('should set the correct confirmation message on successful task assignment', () => {
+    component.verb = TaskActionType.Assign;
+    fixture.detectChanges();
+    const message = {
+      type: InfoMessageType.SUCCESS,
+      message: InfoMessage.ASSIGNED_TASK
+    } as InformationMessage;
+    component.onSubmit();
+    expect(mockInfoMessageCommService.nextMessage).toHaveBeenCalledWith(message);
+  });
+
+  it('should return to the "My work" page on successful task reassignment', () => {
+    window.history.pushState({ returnUrl: 'my-work/list', showAssigneeColumn: false }, '', 'my-work/list');
+    const navigateSpy = spyOn(router, 'navigate');
+    component.onSubmit();
+    expect(mockTaskService.assignTask).toHaveBeenCalledWith('task1111111', { userId: undefined });
+    expect(navigateSpy).toHaveBeenCalledWith(['my-work/list'], {
+      state: {
+        badRequest: false,
+        retainMessages: true
+      }
+    });
+  });
+
+  it('should set the correct confirmation message on successful task reassignment', () => {
+    component.verb = TaskActionType.Reassign;
+    fixture.detectChanges();
+    const message = {
+      type: InfoMessageType.SUCCESS,
+      message: InfoMessage.REASSIGNED_TASK
+    } as InformationMessage;
+    component.onSubmit();
+    expect(mockInfoMessageCommService.nextMessage).toHaveBeenCalledWith(message);
+  });
+
+  it('should handle an HTTP 401 error on submission', () => {
+    // Set dummy task ID to trigger an error response from the fake assignTask() method
+    component.taskId = '401';
+    fixture.detectChanges();
+    const navigateSpy = spyOn(router, 'navigate');
+    component.onSubmit();
+    expect(mockInfoMessageCommService.nextMessage).not.toHaveBeenCalled();
+    expect(navigateSpy).toHaveBeenCalledWith([REDIRECTS.NotAuthorised]);
+  });
+
+  it('should handle an HTTP 500 error on submission', () => {
+    // Set dummy task ID to trigger an error response from the fake assignTask() method
+    component.taskId = '500';
+    fixture.detectChanges();
+    const navigateSpy = spyOn(router, 'navigate');
+    component.onSubmit();
+    expect(mockInfoMessageCommService.nextMessage).not.toHaveBeenCalled();
+    expect(navigateSpy).toHaveBeenCalledWith([REDIRECTS.ServiceDown]);
+  });
+
+  it('should handle an unknown error on submission', () => {
+    // Set dummy task ID to trigger an error response from the fake assignTask() method
+    component.taskId = '999';
+    fixture.detectChanges();
+    const message = {
+      type: InfoMessageType.WARNING,
+      message: InfoMessage.TASK_NO_LONGER_AVAILABLE
+    } as InformationMessage;
+    const navigateSpy = spyOn(router, 'navigate');
+    component.onSubmit();
+    expect(mockInfoMessageCommService.nextMessage).toHaveBeenCalledWith(message);
+    expect(navigateSpy).toHaveBeenCalledWith([''], {
+      state: {
+        badRequest: true,
+        retainMessages: true
+      }
+    });
   });
 });

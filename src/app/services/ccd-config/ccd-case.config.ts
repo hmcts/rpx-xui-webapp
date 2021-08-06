1d1ae81d4f98b5696cbee733816ddad97878fb52
Lee Pavlou <leepavlou@Lees-MacBook-Pro.local>
2021-08-06 16:48:22 +0100

    added abstract methods

--- src/app/services/ccd-config/ccd-case.config.ts
+++ src/app/services/ccd-config/ccd-case.config.ts
@@ -148,4 +148,12 @@ export class AppConfig extends AbstractAppConfig {
   public getWorkAllocationApiUrl(): string {
     return this.workallocationUrl;
   }
+
+  public getHrsUrl(): string {
+    return this.config.hrs_url;
+  }
+
+  public getRemoteHrsUrl(): string {
+    return this.config.remote_hrs_url;
+  }
 }

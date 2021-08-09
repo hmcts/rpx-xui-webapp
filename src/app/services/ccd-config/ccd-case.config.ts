370f0fa9abf86a437285bb92f7e3bb4a03c6b3ee
RiteshHMCTS <ritesh.dsouza@HMCTS.net>
2021-08-09 15:32:51 +0100

    removed space to avoid lint error

--- src/app/services/ccd-config/ccd-case.config.ts
+++ src/app/services/ccd-config/ccd-case.config.ts
@@ -148,7 +148,7 @@ export class AppConfig extends AbstractAppConfig {
   public getWorkAllocationApiUrl(): string {
     return this.workallocationUrl;
   }
-  
+
   public getHrsUrl(): string {
     return this.config.hrs_url;
   }

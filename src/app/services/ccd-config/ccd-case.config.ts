ba525256ce02205f0bf91a18148cbf1ba5a9d3b9
Lee Pavlou <leepavlou@Lees-MacBook-Pro.local>
2021-08-11 17:03:42 +0100

    removed hotfix

--- src/app/services/ccd-config/ccd-case.config.ts
+++ src/app/services/ccd-config/ccd-case.config.ts
@@ -26,13 +26,6 @@ export class AppConfig extends AbstractAppConfig {
     super();
     this.config = this.appConfigService.getEditorConfiguration() || {};
     this.featureToggleWorkAllocation();
-
-    this.featureToggleService.getValue('mc-document-secure-mode-enabled', false).subscribe({
-      next: (val) => this.config = {
-        ...this.config,
-        document_management_secure_enabled: val
-      }
-    });
   }
 
   private featureToggleWorkAllocation(): void {

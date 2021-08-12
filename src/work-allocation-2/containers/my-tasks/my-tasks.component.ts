cd1d28425ac14d5c5bc245035321a6b5f05d2a88
connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>
2021-08-12 15:09:25 +0100

    Add task list filter apply work
    - Give locations to the api via filterservice
    - Mock the filtered data
    - Test the lists
    - Replace Glagow location id
    - Fix lint errors and warnings

--- src/work-allocation-2/containers/my-tasks/my-tasks.component.ts
+++ src/work-allocation-2/containers/my-tasks/my-tasks.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { UserInfo } from '../../../app/models/user-details.model';
 import { ConfigConstants, ListConstants, PageConstants, SortConstants } from '../../components/constants';
 import { FieldConfig } from '../../models/common';
-import { SearchTaskRequest } from '../../models/dtos';
+import { SearchTaskParameter, SearchTaskRequest } from '../../models/dtos';
 import { TaskListWrapperComponent } from '../task-list-wrapper/task-list-wrapper.component';
 
 @Component({
   selector: 'exui-my-tasks',
   templateUrl: 'my-tasks.component.html'
 })
-export class MyTasksComponent extends TaskListWrapperComponent {
+export class MyTasksComponent extends TaskListWrapperComponent implements OnInit {
 
   public get emptyMessage(): string {
     return ListConstants.EmptyMessage.MyTasks;
@@ -31,6 +32,8 @@ export class MyTasksComponent extends TaskListWrapperComponent {
     return ConfigConstants.MyWorkTasks;
   }
 
+  public filteredLocations: string[] = [];
+
   public getSearchTaskRequestPagination(): SearchTaskRequest {
     const userInfoStr = this.sessionStorageService.getItem('userDetails');
     if (userInfoStr) {
@@ -40,12 +43,17 @@ export class MyTasksComponent extends TaskListWrapperComponent {
       return {
         search_parameters: [
           { key: 'user', operator: 'IN', values: [ id ] },
+          this.getLocationParameter()
         ],
         sorting_parameters: [this.getSortParameter()],
         search_by: isJudge ? 'judge' : 'caseworker',
         pagination_parameters: this.getPaginationParameter()
       };
     }
+  }
+
+  private getLocationParameter(): SearchTaskParameter {
+    return { key: 'location', operator: 'IN', values: this.selectedLocations };
   }
 
   /**

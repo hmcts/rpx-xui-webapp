f4985c089aca501c10187fe484542772ad2c9ef8
unknown <ishita.oswal@HMCTS.net>
2021-08-18 14:32:02 +0100

    EUI-4109-mc-cve-fix-v2

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -64,4 +64,3 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-import 'core-js/es7/reflect';

403bbb53cca44b73e85c5b9b16653e0987988bb0
sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
2021-08-23 12:10:30 +0100

    test updated

--- test/integration/tests/nodeApp_endpoints.spec.ts
+++ test/integration/tests/nodeApp_endpoints.spec.ts
@@ -4,6 +4,8 @@ import {http} from './utils';
 import Request from './utils/request';
 import { setTestContext } from './utils/helper';
 
+const nodeAppDataModels = require('../../dataModels/nodeApp')
+
 describe('nodeApp endpoint', () => {
   const userName = 'lukesuperuserxui@mailnesia.com';
   const password = 'Monday01';
@@ -47,13 +49,24 @@ describe('nodeApp endpoint', () => {
 
     const response = await Request.get('api/user/details', null, 200);
     expect(response.status).to.equal(200);
-    expect(response.data).to.have.all.keys('canShareCases', 'sessionTimeout', 'userInfo','locationInfo');
-    expect(response.data.userInfo).to.have.all.keys('id', 'forename', 'surname', 'email', 'active', 'roles','token');
-    expect(response.data.userInfo.roles).to.be.an('array');
+
+
+    const actualLocationObjKeys = response.data;
+
+    const expectedUserDetailsObj_oidc = nodeAppDataModels.getUserDetails_oidc();
+
+    expect(actualLocationObjKeys).to.include.members(Object.keys(expectedUserDetailsObj_oidc));
+
+    if (actualLocationObjKeys.locationInfo.length > 0){
+      expect(actualLocationObjKeys.locationInfo[0]).to.include.members(Object.keys(expectedUserDetailsObj_oidc.locationInfo[0]));
+    }
+
+    expect(actualLocationObjKeys.roles).to.be.an('array');
+
     if (configRes.data.oidcEnabled) {
-      expect(response.data.userInfo).to.have.all.keys('uid', 'family_name', 'given_name', 'name', 'sub', 'roles', 'token');
+      expect(actualLocationObjKeys.userInfo).to.have.all.keys(Object.keys(expectedUserDetailsObj_oidc.userInfo));
     } else {
-      expect(response.data.userInfo).to.have.all.keys('id', 'forename', 'surname', 'email', 'active', 'roles', 'token');
+      expect(actualLocationObjKeys.userInfo).to.have.all.keys(Object.keys(nodeAppDataModels.getUserDetails_oauth().userInfo));
     }
   });
 

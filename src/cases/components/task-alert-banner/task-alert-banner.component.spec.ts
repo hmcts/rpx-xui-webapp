05bf5859079b7c7cb93aa3eaca051545731c61ff
connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>
2021-08-27 17:11:09 +0100

    Remove fdescribe

--- src/cases/components/task-alert-banner/task-alert-banner.component.spec.ts
+++ src/cases/components/task-alert-banner/task-alert-banner.component.spec.ts
@@ -1,8 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
 import { TaskAlertBannerComponent } from '..';
 
-fdescribe('TaskAlertBannerComponent', () => {
+describe('TaskAlertBannerComponent', () => {
   let component: TaskAlertBannerComponent;
   let fixture: ComponentFixture<TaskAlertBannerComponent>;
 

2cda42d91a7a79398b10c4f4b7a1b6ad4493396a
Uday Denduluri <uday.denduluri@gmail.com>
2021-09-02 13:26:32 +0100

    case-task dynamic changes

--- src/cases/components/case-task/case-task.component.ts
+++ src/cases/components/case-task/case-task.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Task } from '../../../work-allocation-2/models/tasks';
 
 @Component({
   selector: 'exui-case-task',
   templateUrl: './case-task.component.html',
   styleUrls: ['./case-task.component.scss']
 })
-export class CaseTaskComponent {
-
+export class CaseTaskComponent implements OnInit {
+  ngOnInit(): void {
+  }
+  @Input() public task: Task;
 }

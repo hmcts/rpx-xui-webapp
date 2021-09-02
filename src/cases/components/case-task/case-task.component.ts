59eeb07a554da9c7c881825651179e0bd25d36c9
Uday Denduluri <uday.denduluri@gmail.com>
2021-09-02 16:23:12 +0100

    fix moack data

--- src/cases/components/case-task/case-task.component.ts
+++ src/cases/components/case-task/case-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { UserInfo } from '../../../app/models';
+import { SessionStorageService } from '../../../app/services';
 import { Task } from '../../../work-allocation-2/models/tasks';
 
 @Component({
@@ -8,10 +10,20 @@ import { Task } from '../../../work-allocation-2/models/tasks';
 })
 export class CaseTaskComponent implements OnInit {
   @Input() public task: Task;
+  constructor(private readonly sessionStorageService: SessionStorageService) {}
 
   public ngOnInit(): void {
   }
   public getAssigneeName(task: Task): string {
     return task.assigneeName ? task.assigneeName : 'Unassigned';
+  }
+  public isTaskAssignedToCurrentUser(task: Task): boolean {
+    const userInfoStr = this.sessionStorageService.getItem('userDetails');
+    if (userInfoStr) {
+      const userInfo: UserInfo = JSON.parse(userInfoStr);
+      const userId = userInfo.id ? userInfo.id : userInfo.uid;
+      return task.assignee && task.assignee === userId;
+    }
+    return false;
   }
 }

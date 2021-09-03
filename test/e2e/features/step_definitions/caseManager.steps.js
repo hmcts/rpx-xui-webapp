370a5e324cddb9e9e93e313484f237cf8e87b14f
marianadpereira <mariana.pereira@hmcts.net>
2021-09-03 15:31:14 +0100

    fixing conflicts

--- test/e2e/features/step_definitions/caseManager.steps.js
+++ test/e2e/features/step_definitions/caseManager.steps.js
@@ -1,14 +1,10 @@
-
 var CaseListPage = require("../pageObjects/CaseListPage");
 var FRCase = require('../pageObjects/FRCase');
 var ProbateCase = require('../pageObjects/ProbateCase');
 var DivorceCase = require('../pageObjects/Divorcecase');
 var IACCase = require('../pageObjects/iacCase');
-<<<<<<< HEAD
-=======
-
 var HearingRecordingsCase = require('../pageObjects/hearingRecordingsCase');
->>>>>>> 87f9abed409b506ae576e6c1ecd77b83649f7351
+
 
 var CaseManager = require('../pageObjects/common/CaseManager');
 
@@ -17,65 +13,69 @@ var { defineSupportCode } = require('cucumber');
 const creatCaseStepTimeout = 600*1000;
 
 defineSupportCode(function ({ And, But, Given, Then, When }) {
-    var caseListPage = new CaseListPage();
-    let frCase = new FRCase();
-    let probateCase = new ProbateCase();
-    let divorceCase = new DivorceCase();
-    let iacCase = new IACCase();
+  var caseListPage = new CaseListPage();
+  let frCase = new FRCase();
+  let probateCase = new ProbateCase();
+  let divorceCase = new DivorceCase();
+  let iacCase = new IACCase();
+  let hearingRecordingsCase = new HearingRecordingsCase();
 
-    let caseManager = new CaseManager();
-
-
-    When('I start case with jurisdiction {string} case type {string} and event {string}', async function (jurisidiction, casetype, event) {
-        await caseManager.startCaseCreation(jurisidiction, casetype, event);
-    });
-
-    When('I create FR case', { timeout: creatCaseStepTimeout }, async function () {
-        await frCase.createCase(false);
-    });
-
-    When('I create Divorce case', { timeout: creatCaseStepTimeout }, async function () {
-        await divorceCase.createCase(false);
-    });
-
-    When('I create Probate case', { timeout: creatCaseStepTimeout }, async function () {
-        await probateCase.createCase(false);
-    })
-
-    When('I create IAC case', { timeout: creatCaseStepTimeout }, async function () {
-        await iacCase.createCase(false);
-    })
-
-    When('I start case next step', async function () {
-        await caseManager.startNextStep(false);
-    });
+  let caseManager = new CaseManager();
 
 
-    When('I start case next step {string}', async function (stepName) {
-        await caseManager.startNextStep(stepName);
-    });
+  When('I start case with jurisdiction {string} case type {string} and event {string}', async function (jurisidiction, casetype, event) {
+    await caseManager.startCaseCreation(jurisidiction, casetype, event);
+  });
 
-    When('I click cancel link', async function () {
-        await caseManager.cancelCaseCreation();
-    });
+  When('I create FR case', { timeout: creatCaseStepTimeout }, async function () {
+    await frCase.createCase(false);
+  });
 
-    When('I click previous button', async () => {
-        await caseManager.clickPreviousButton();
-    });
+  When('I create Divorce case', { timeout: creatCaseStepTimeout }, async function () {
+    await divorceCase.createCase(false);
+  });
 
-    When('I submit case', async function () {
-        await caseManager.submitCase(false);
-    });
+  When('I create Probate case', { timeout: creatCaseStepTimeout }, async function () {
+    await probateCase.createCase(false);
+  })
 
-    Then('I see case details page', async function () {
-        await caseManager.AmOnCaseDetailsPage();
-    });
+  When('I create IAC case', { timeout: creatCaseStepTimeout }, async function () {
+    await iacCase.createCase(false);
+  })
 
-    Then('I am on check your answers page', async function () {
-        await caseManager.AmOnChekYourAnswersPage();
-    });
+  When('I create Hearing Recordings case', { timeout: creatCaseStepTimeout }, async function () {
+    await hearingRecordingsCase.createCase();
+  })
 
-    Then('I am on case form page', async function () {
-        await caseManager.AmOnCCDCaseEditPage();
-    });
+  When('I start case next step', async function () {
+    await caseManager.startNextStep(false);
+  });
+
+  When('I start case next step {string}', async function (stepName) {
+    await caseManager.startNextStep(stepName);
+  });
+
+  When('I click cancel link', async function () {
+    await caseManager.cancelCaseCreation();
+  });
+
+  When('I click previous button', async () => {
+    await caseManager.clickPreviousButton();
+  });
+
+  When('I submit case', async function () {
+    await caseManager.submitCase(false);
+  });
+
+  Then('I see case details page', async function () {
+    await caseManager.AmOnCaseDetailsPage();
+  });
+
+  Then('I am on check your answers page', async function () {
+    await caseManager.AmOnChekYourAnswersPage();
+  });
+
+  Then('I am on case form page', async function () {
+    await caseManager.AmOnCCDCaseEditPage();
+  });
 });

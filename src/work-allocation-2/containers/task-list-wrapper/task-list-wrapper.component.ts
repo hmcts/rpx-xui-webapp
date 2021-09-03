67d02c92890a5097b0ca82e2cb540622b080a58e
connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>
2021-09-03 13:06:01 +0100

    Update task-list-wrapper.component.ts
    Removing console.log()

--- src/work-allocation-2/containers/task-list-wrapper/task-list-wrapper.component.ts
+++ src/work-allocation-2/containers/task-list-wrapper/task-list-wrapper.component.ts
@@ -281,7 +281,6 @@ export class TaskListWrapperComponent implements OnDestroy, OnInit {
     this.performSearchPagination().subscribe(result => {
         this.loadingService.unregister(loadingToken);
         this.tasks = result.tasks;
-        console.log(this.tasks);
         this.tasksTotal = result.total_records;
         this.tasks.forEach(task => task.assigneeName = getAssigneeName(this.caseworkers, task.assignee));
         this.tasksLoaded = true;

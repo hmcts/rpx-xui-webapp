709982a29d0a947f4452f3a55163648a771468d4
Raymond Liang <ray.liang@atos.net>
2021-09-09 14:02:09 +0100

    remove useless module

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -20,7 +20,6 @@ module.exports = function (config) {
       require('karma-spec-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-sonarqube-unit-reporter'),
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser

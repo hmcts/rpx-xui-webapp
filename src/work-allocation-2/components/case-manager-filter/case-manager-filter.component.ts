e4b14b56a1f1bdf694bc00e6edd149e0591af096
kuda <ku-da@hotmail.co.uk>
2021-09-09 11:34:28 +0100

    case filters

--- src/work-allocation-2/components/case-manager-filter/case-manager-filter.component.ts
+++ src/work-allocation-2/components/case-manager-filter/case-manager-filter.component.ts
@@ -1,97 +1,118 @@
-import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
-
-import { SessionStorageService } from '../../../app/services';
-import { Location } from '../../models/dtos';
-import { FilterConstants } from '../constants';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { FilterConfig, FilterFieldConfig, FilterSetting } from '@hmcts/rpx-xui-common-lib/lib/models/filter.model';
 
 @Component({
   selector: 'exui-case-manager-filter',
   templateUrl: './case-manager-filter.component.html',
-  styleUrls: ['./case-manager-filter.component.scss']
+  styleUrls: ['./case-manager-filter.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class CaseManagerFilterComponent implements OnChanges {
-  // Protected instances of the exported constants.
-  public readonly ALL_LOCATIONS: Location = FilterConstants.Options.Locations.ALL;
+export class CaseManagerFilterComponent implements OnInit {
 
-  /**
-   * The locations that are available to be selected from.
-   */
-  @Input() public locations: Location[];
+  private static FILTER_NAME: string = 'cases';
 
-  @Input()
-  public get location(): Location {
-    return this.pLocation;
+  @Input() public locations: Location[] = [];
+
+  public fieldsConfig: FilterConfig = {
+    persistence: 'local',
+    id: CaseManagerFilterComponent.FILTER_NAME,
+    fields: [],
+    cancelButtonText: 'Reset to default',
+    applyButtonText: 'Apply',
+    cancelSetting: null
+  };
+
+  public fieldsSettings: FilterSetting = {
+    id: CaseManagerFilterComponent.FILTER_NAME,
+    fields: [],
+  };
+
+  private static initServiceFilter(): FilterFieldConfig {
+    return {
+      name: CaseManagerFilterComponent.FILTER_NAME,
+      options: [
+        {
+          key: 'IA',
+          label: 'Immigration and Asylum'
+        }
+      ],
+      minSelected: 1,
+      maxSelected: null,
+      minSelectedError: 'You must select a service',
+      maxSelectedError: null,
+      title: 'Service',
+      type: 'select'
+    };
   }
 
-  public set location(value: Location) {
-    value = value || this.ALL_LOCATIONS; // undefined or null means "All"
-    if (this.pLocation !== value) {
-      this.pLocation = value;
-      this.saveFilterToSession();
-      this.emitChangedEvent();
-    }
+  private static initCaseLocationFilter(): FilterFieldConfig {
+    return {
+      name: CaseManagerFilterComponent.FILTER_NAME,
+      options: [
+        {
+          key: 'Taylor House',
+          label: 'Taylor House'
+        }
+      ],
+      minSelected: 1,
+      maxSelected: null,
+      minSelectedError: 'You must select a location',
+      maxSelectedError: null,
+      title: 'Case Location',
+      type: 'select'
+    };
   }
 
-  @Input()
-  public get jurisdiction(): any {
-    return this.pJurisdiction;
+  private static initRoleTypeFilter(): FilterFieldConfig {
+    return {
+      name: CaseManagerFilterComponent.FILTER_NAME,
+      options: [
+        {
+          key: 'Judicial',
+          label: 'Judicial'
+        },
+        {
+          key: 'Legal Ops',
+          label: 'Legal Ops'
+        }
+      ],
+      minSelected: 1,
+      maxSelected: null,
+      minSelectedError: 'You must select a role type',
+      maxSelectedError: null,
+      title: 'Role type',
+      type: 'select'
+    };
   }
 
-  public set jurisdiction(value: any) {
-    if (this.pJurisdiction !== value) {
-      this.pJurisdiction = value;
-      this.saveFilterToSession();
-      this.emitChangedEvent();
-    }
+  private static initPersonFilter(): FilterFieldConfig {
+    return {
+      name: CaseManagerFilterComponent.FILTER_NAME,
+      options: [
+        {
+          key: 'All',
+          label: 'All'
+        },
+        {
+          key: 'Specific person',
+          label: 'Specific person'
+        }
+      ],
+      minSelected: 1,
+      maxSelected: null,
+      minSelectedError: 'You must select a person',
+      maxSelectedError: null,
+      title: 'Role type',
+      type: 'radio'
+    };
   }
 
-  private pLocation: Location = this.ALL_LOCATIONS;
-  private pJurisdiction: any =  {id: 'IA', name: 'Immigration and Asylum'};
-
-  /**
-   * Emit an event to notify the parent component that the selected Caseworker
-   * and/or Location has changed. The new selection is emitted with the event
-   * but can also be retrieved from component.location.
-   */
-  @Output() public selectionChanged: EventEmitter<{ location: Location, jurisdiction: any}>
-    = new EventEmitter<{ location: Location, jurisdiction: any }>();
-
-  /**
-   * Accept the SessionStorageService for adding to and retrieving from sessionStorage.
-   */
-  constructor(private readonly sessionStorageService: SessionStorageService) {
-  }
-
-  public ngOnChanges(): void {
-    if (this.locations) {
-      // See if we have anything stored in the session for the filter.
-      const stored: string = this.sessionStorageService.getItem(FilterConstants.Session.TaskManager);
-      if (stored) {
-        const { caseworkerId, locationId } = JSON.parse(stored);
-        this.pLocation = this.getLocationById(locationId);
-      }
-      this.emitChangedEvent();
-    }
-  }
-
-  private saveFilterToSession(): void {
-    const toStore = JSON.stringify({
-      locationId: this.location.id
-    });
-    this.sessionStorageService.setItem(FilterConstants.Session.TaskManager, toStore);
-  }
-
-  public getLocationById(id: string): Location {
-    if (id === this.ALL_LOCATIONS.id) {
-      return this.ALL_LOCATIONS;
-    }
-    return this.locations.find(loc => loc.id === id) || this.ALL_LOCATIONS;
-  }
-
-  private emitChangedEvent(): void {
-    this.selectionChanged.emit({
-      location: this.location,
-      jurisdiction: this.jurisdiction,
-    });
+  public ngOnInit(): void {
+    this.fieldsConfig.fields = [
+      CaseManagerFilterComponent.initServiceFilter(),
+      CaseManagerFilterComponent.initCaseLocationFilter(),
+      CaseManagerFilterComponent.initRoleTypeFilter(),
+      CaseManagerFilterComponent.initPersonFilter()
+    ];
   }
 }

ec304877f2f923e236bc2a3bf8a904445a82b68a
waynepHMCTS <wayne.porter@hmcts.net>
2021-09-21 14:21:10 +0100

    linting-correction

--- src/app/containers/main-error-page/main-error-page.component.ts
+++ src/app/containers/main-error-page/main-error-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'xui-main-error-page',
+  selector: 'exui-main-error-page',
   templateUrl: './main-error-page.component.html'
 })
 export class MainErrorPageComponent {

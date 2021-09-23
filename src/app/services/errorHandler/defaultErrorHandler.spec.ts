4578ddfd9486159a6667e7443ddd6dddfc319dc7
waynepHMCTS <wayne.porter@hmcts.net>
2021-09-23 12:52:16 +0100

    default error spec resolve constructor

--- src/app/services/errorHandler/defaultErrorHandler.spec.ts
+++ src/app/services/errorHandler/defaultErrorHandler.spec.ts
@@ -1,15 +1,23 @@
+import { Injector, NgZone } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { DefaultErrorHandler } from './defaultErrorHandler';
 
 describe('Default Error Handler', () => {
     const mockedLoggerService = jasmine.createSpyObj('mockedLoggerService', ['error']);
 
-    it('Should call Default Error Handler and be returned a DEH instance', () => {
-       const errorHandler = new DefaultErrorHandler(mockedLoggerService);
+    fit('Should call Default Error Handler and be returned a DEH instance', () => {
+        const injector = Injector.create([]);
+        const ngZone = TestBed.get(NgZone);
+        spyOn(ngZone, 'runOutsideAngular').and.callFake((fn: Function) => fn());
+       const errorHandler = new DefaultErrorHandler(mockedLoggerService, injector, ngZone);
        expect(errorHandler).toBeTruthy();
     });
 
     it('should be able to call error', () => {
-        const errorHandler = new DefaultErrorHandler(mockedLoggerService);
+        const injector = Injector.create([]);
+        const ngZone = TestBed.get(NgZone);
+        spyOn(ngZone, 'runOutsideAngular').and.callFake((fn: Function) => fn());
+        const errorHandler = new DefaultErrorHandler(mockedLoggerService, injector, ngZone);
         errorHandler.handleError(new Error('Some Error Message'));
         expect(mockedLoggerService.error).toHaveBeenCalledWith(new Error('Some Error Message'));
     });

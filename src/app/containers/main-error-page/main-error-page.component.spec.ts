248d983ceffad9baa8275d8a8906806ab7e81083
waynepHMCTS <wayne.porter@hmcts.net>
2021-09-24 14:13:09 +0100

    spec host comp

--- src/app/containers/main-error-page/main-error-page.component.spec.ts
+++ src/app/containers/main-error-page/main-error-page.component.spec.ts
@@ -1,29 +1,48 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA, DebugElement, ViewChild } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ExuiCommonLibModule } from '@hmcts/rpx-xui-common-lib';
-import { AppHeaderComponent } from '../app-header/app-header.component';
-import { FooterComponent } from '../footer/footer.component';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { MainErrorPageComponent } from './main-error-page.component';
 
-describe('MainErrorPageComponent', () => {
+describe('MainErrorPageComponent', () => {  
+  @Component({
+    selector: `exui-app-host-dummy-component`,
+    template: `<exui-main-error-page></exui-main-error-page>`
+  })
+  class TestDummyHostComponent {
+      @ViewChild(MainErrorPageComponent)
+      public mainErrorPageComponent: MainErrorPageComponent;
+  }
+
+  let testHostComponent: TestDummyHostComponent;
+  let testHostFixture: ComponentFixture<TestDummyHostComponent>;
   let component: MainErrorPageComponent;
   let fixture: ComponentFixture<MainErrorPageComponent>;
+  let element: DebugElement;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [ ExuiCommonLibModule ],
-      declarations: [ MainErrorPageComponent, FooterComponent, AppHeaderComponent ]
-    })
-    .compileComponents();
+      TestBed.configureTestingModule({
+          imports: [
+              RouterTestingModule
+          ],
+          declarations: [ MainErrorPageComponent, TestDummyHostComponent ],
+          schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      })
+          .compileComponents();
   }));
-
   beforeEach(() => {
-    fixture = TestBed.createComponent(MainErrorPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      testHostFixture = TestBed.createComponent(TestDummyHostComponent);
+      testHostComponent = testHostFixture.componentInstance;
+  });
+  beforeEach(() => {
+      fixture = TestBed.createComponent(MainErrorPageComponent);
+      component = fixture.componentInstance;
+      element = fixture.debugElement;
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+      expect(component).toBeTruthy();
+  });
+  it('should be created by angular', () => {
+      expect(fixture).not.toBeNull();
   });
 });

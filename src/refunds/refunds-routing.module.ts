00a6387c391a61ec5b64556f7a572e8439e38608
Ronald Mansveld <ronald.mansveld@atos.net>
2021-10-05 16:34:37 +0100

    Add guard to refund page

--- src/refunds/refunds-routing.module.ts
+++ src/refunds/refunds-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RoleGuard, RoleMatching } from '@hmcts/rpx-xui-common-lib';
 import { RefundsComponent } from './refunds/refunds.component';
 
 const routes: Routes = [{
   path: '',
-  component: RefundsComponent
+  component: RefundsComponent,
+  canActivate: [ RoleGuard ],
+  data: {
+    needsRole: ['payments-refund-approver', 'payments-refund'],
+    roleMatching: RoleMatching.ANY,
+    noRoleMatchRedirect: '/'
+  }
 }];
 
 @NgModule({

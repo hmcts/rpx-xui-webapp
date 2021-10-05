be21f2a0404f5b080ec6735ada044dfe0730f797
Ronald Mansveld <ronald.mansveld@atos.net>
2021-10-05 12:31:50 +0100

    Fix tests and linting errors

--- src/app/components/hmcts-global-header/hmcts-global-header.component.ts
+++ src/app/components/hmcts-global-header/hmcts-global-header.component.ts
@@ -7,6 +7,7 @@ import * as fromNocStore from '../../../noc/store';
 import { NavItemsModel } from '../../models/nav-item.model';
 import { UserNavModel } from '../../models/user-nav.model';
 import { UserService } from '../../services/user/user.service';
+import c = require('config');
 
 
 @Component({
@@ -76,13 +77,13 @@ export class HmctsGlobalHeaderComponent implements OnChanges {
   private async filterNavItemsOnRole(items: NavItemsModel[]): Promise<NavItemsModel[]> {
     return this.userService.getUserDetails().pipe(
       map(details => details.userInfo.roles),
-      map(roles => items.filter(item => (item.roles || []).some(role => roles.includes(role))))
+      map(roles => items.filter(item => (item.roles && item.roles.length > 0 ? item.roles.some(role => roles.includes(role)) : true)))
     ).toPromise();
   }
-  
+
   private async filterNavItemsOnFlag(items: NavItemsModel[]): Promise<NavItemsModel[]> {
-    var flags: {[flag: string]: boolean} = {};
-    var obs: Observable<boolean>[] = [];
+    const flags: {[flag: string]: boolean} = {};
+    const obs: Observable<boolean>[] = [];
     items.forEach(
       item => (item.flags || []).forEach(
         flag => obs.push(
@@ -92,13 +93,13 @@ export class HmctsGlobalHeaderComponent implements OnChanges {
         )
       )
     );
-    if (obs.length > 1) { 
+    if (obs.length > 1) {
       await obs[0].combineLatest(obs.slice(1)).toPromise();
     } else if (obs.length > 0) {
       await obs[0].toPromise();
     } else {
       return items; // no flags found, so nothing to filter
     }
-    return items.filter(item => (item.flags || []).every(flag => flags[flag]))
+    return items.filter(item => item.flags && item.flags.length > 0 ? item.flags.every(flag => flags[flag]) : true)
   }
 }

2f277ebc99eb085796c51165593abb0640fa6de9
John Benjamin <john.benjamin@hmcts.net>
2021-10-08 14:59:18 +0100

    EUI-4444 Display of services dropdown node layer

--- api/jurisdictions/index.ts
+++ api/jurisdictions/index.ts
@@ -4,6 +4,7 @@ import {
     SERVICES_PRD_API_URL
 } from '../configuration/references';
 import { EnhancedRequest } from 'lib/models';
+import { getConfigValue } from '../configuration';
 
 /**
  * Get jurisdictions
@@ -16,8 +17,7 @@ export async function getJurisdictions(req: EnhancedRequest, res: Response, next
         if (req.session.jurisdictions) {
             res.send(req.session.jurisdictions);
         } else {
-            // const path = `${getConfigValue(SERVICES_PRD_API_URL)}/aggregated/caseworkers/:uid/jurisdictions?access=read`;
-            const path = `https://manage-case.aat.platform.hmcts.net/aggregated/caseworkers/:uid/jurisdictions?access=read`;
+            const path = `${getConfigValue(SERVICES_PRD_API_URL)}/aggregated/caseworkers/:uid/jurisdictions?access=read`;
             const response = await handleGet(path, req, next);
 
             // Store jurisdictions to session

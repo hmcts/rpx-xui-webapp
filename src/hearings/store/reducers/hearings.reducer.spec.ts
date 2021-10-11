a49485d8115598a58113b105bbec886bf23a1a5e
Raymond Liang <ray.liang@atos.net>
2021-10-11 12:23:33 +0100

    EUI-4446-create-hearings-tab-case-details-page

--- src/hearings/store/reducers/hearings.reducer.spec.ts
+++ src/hearings/store/reducers/hearings.reducer.spec.ts
@@ -1,3 +1,6 @@
+import { CaseHearingModel } from '../../models/caseHearing.model';
+import { CaseHearingsMainModel } from '../../models/caseHearingsMain.model';
+import { HearingDayScheduleModel } from '../../models/hearingDaySchedule.model';
 import * as fromActions from '../actions/hearings.action';
 import * as fromReducer from './hearings.reducer';
 
@@ -11,6 +14,38 @@ describe('Hearings Reducer', () => {
         const action = new fromActions.Reset();
         const hearingsState = fromReducer.hearingsReducer(initialState, action);
         expect(hearingsState).toEqual(initialState);
+      });
+    });
+
+    describe('Load all hearings success action', () => {
+      it('should set correct object', () => {
+        const initialState = fromReducer.initialState;
+        const HEARING_DAY_SCHEDULE_1: HearingDayScheduleModel = {
+          hearingStartDateTime: '2021-05-01T16:00:00.000+0000',
+          hearingEndDateTime: '2021-05-04T16:00:00.000+0000',
+          ListAssistSessionID: '0d22d836-b25a-11eb-a18c-f2d58a9b7ba5',
+          hearingVenueId: 'venue 1',
+          hearingRoomId: 'room 1',
+          hearingPanel: ['child'],
+        };
+        const CASE_HEARING_1: CaseHearingModel = {
+          hearingID: 'h111111',
+          hearingType: 'hearing type 1',
+          hmcStatus: 'pending',
+          lastResponseReceivedDateTime: '2021-05-05T16:00:00.000+0000',
+          responseVersion: 'rv1',
+          hearingListingStatus: 'pending',
+          listAssistCaseStatus: 'pending',
+          hearingDaySchedule: [HEARING_DAY_SCHEDULE_1],
+        };
+        const HEARINGS_LIST: CaseHearingsMainModel = {
+          hmctsServiceID: 'SSCS',
+          caseRef: '1568642646198441',
+          caseHearings: [CASE_HEARING_1],
+        };
+        const action = new fromActions.LoadAllHearingsSuccess(HEARINGS_LIST);
+        const hearingsState = fromReducer.hearingsReducer(initialState, action);
+        expect(hearingsState.caseHearingsMainModel).toEqual(HEARINGS_LIST);
       });
     });
   });

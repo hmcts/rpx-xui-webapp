3b17c752045e1ce68a343764a56e410769b4bd2d
waynepHMCTS <wayne.porter@hmcts.net>
2021-10-11 08:48:15 +0100

    dumb component

--- src/cases/containers/case-hearings/case-hearings.component.spec.ts
+++ src/cases/containers/case-hearings/case-hearings.component.spec.ts
@@ -1,11 +1,14 @@
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { CaseHearingsComponent } from './case-hearings.component';
 
 describe('CaseHearingsComponent', () => {
   let component: CaseHearingsComponent;
   let fixture: ComponentFixture<CaseHearingsComponent>;
-
+  let de: DebugElement;
+  
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CaseHearingsComponent ]
@@ -16,10 +19,61 @@ describe('CaseHearingsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CaseHearingsComponent);
     component = fixture.componentInstance;
+    const mockHearing = [];
+    mockHearing.push({ 
+      hmctsServiceID: '1',
+      caseRef : 'Upcoming',
+      caseHearings: [{
+        hearingID: '1',
+        hearingType: 'Final hearing',
+        hmcStatus: 'LISTED',
+        lastResponseReceivedDateTime: '12TH October 2021',
+        responseVersion: 'string',
+        hearingListingStatus: 'COMPLETED',
+        listAssistCaseStatus: 'COMPLETED',
+        hearingDaySchedule: []
+      }]
+    });
+    component.caseHearing = mockHearing;
+    de = fixture.debugElement;
     fixture.detectChanges();
+    fixture.debugElement.queryAll(By.css('.govuk-table__cell'))[1];
   });
 
-  it('should create', () => {
+  it('should create hearing component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should have the correct value for status in the markup', () => {
+    const secondColumn = fixture.debugElement.queryAll(By.css('.govuk-table__cell strong'))[0];
+    expect(secondColumn.nativeElement.innerHTML).toEqual('COMPLETED');
+    expect(secondColumn.nativeElement.className).toEqual('TORQUISE');
+  });
+
+  it('Should have the requested hearing when request button is clicked', () => {
+    spyOn(component.requestHearing, 'emit');
+
+    const requestHearingButton = fixture.debugElement.queryAll(By.css('.govuk-button--start'))[0];
+    requestHearingButton.triggerEventHandler('click', null);
+    expect(requestHearingButton).toBeTruthy();
+    expect(component.requestHearing.emit).toHaveBeenCalled();;
+  });
+
+  it('Should have the requested to view hearing when view link is clicked', () => {
+    spyOn(component.viewHearing, 'emit');
+
+    const viewHearingButton = fixture.debugElement.queryAll(By.css('.govuk-table__cell a'))[0];
+    viewHearingButton.triggerEventHandler('click', null);
+    expect(viewHearingButton).toBeTruthy();
+    expect(component.viewHearing.emit).toHaveBeenCalled();
+  });
+
+  it('Should have the requested to cancel hearing when cancel link is clicked', () => {
+    spyOn(component.cancelHearing, 'emit');
+
+    const cancelHearingButton = fixture.debugElement.queryAll(By.css('.govuk-table__cell a'))[1];
+    cancelHearingButton.triggerEventHandler('click', null);
+    expect(cancelHearingButton).toBeTruthy();
+    expect(component.cancelHearing.emit).toHaveBeenCalled();
   });
 });
--- src/cases/containers/case-hearings/case-hearings.component.ts
+++ src/cases/containers/case-hearings/case-hearings.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CaseHearingModel } from 'api/hearings/models/caseHearing.model';
+import { CaseHearingsMainModel } from 'api/hearings/models/caseHearingsMain.model';
 
 @Component({
   selector: 'exui-case-hearings',
-  templateUrl: './case-hearings.component.html'
+  templateUrl: './case-hearings.component.html',
+  styles:[`
+    .header-container { margin-top: 30px; }
+    .TORQUISE { background-color: purple; padding:5px }
+    .GREEN { background-color: green; padding:5px }
+    .GREY { background-color: grey; padding:5px }
+    .RED { background-color: red; padding:5px}
+  `]
 })
-export class CaseHearingsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class CaseHearingsComponent {
+  @Input() caseHearing: CaseHearingsMainModel[];
+  @Output() viewHearing = new EventEmitter<CaseHearingModel>();
+  @Output() cancelHearing = new EventEmitter<CaseHearingModel>();
+  @Output() requestHearing = new EventEmitter<any>();
 }

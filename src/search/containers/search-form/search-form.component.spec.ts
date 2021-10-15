50e75e8ec0db97314dcc1ef378f3fc80cf5d8a29
Lam <daniel.2.lam@atos.net>
2021-10-15 15:00:00 +0100

    Fix test failure

--- src/search/containers/search-form/search-form.component.spec.ts
+++ src/search/containers/search-form/search-form.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchFormComponent } from './search-form.component';
@@ -8,7 +9,8 @@ describe('SearchFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchFormComponent ]
+      declarations: [ SearchFormComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));

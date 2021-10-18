79b696e2760ee1613846a31233a3f821aad594e9
John Benjamin <john.benjamin@hmcts.net>
2021-10-18 16:11:44 +0100

    EUI-4595 Global Search No Results Page Fixes after review

--- src/search/search-routing.module.ts
+++ src/search/search-routing.module.ts
@@ -1,19 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RoleGuard, RoleMatching } from '@hmcts/rpx-xui-common-lib';
-import { SearchFormComponent } from './containers';
+import { NoResultsComponent, SearchFormComponent } from './containers';
 
-const routes: Routes = [{
-  path: '',
-  component: SearchFormComponent,
-  canActivate: [RoleGuard],
-  data: {
-    // TODO: Roles will need replacing with actual ones to be granted access, or loaded from configuration
-    needsRole: ['pui\-case\-manager'],
-    roleMatching: RoleMatching.ANY,
-    noRoleMatchRedirect: '/'
+const routes: Routes = [
+  {
+    path: '',
+    component: SearchFormComponent,
+    canActivate: [RoleGuard],
+    data: {
+      // TODO: Roles will need replacing with actual ones to be granted access, or loaded from configuration
+      needsRole: ['pui\-case\-manager'],
+      roleMatching: RoleMatching.ANY,
+      noRoleMatchRedirect: '/'
+    }
+  },
+  {
+    path: 'noresults',
+    component: NoResultsComponent,
+    data: {
+      title: 'Search cases | No results',
+      // TODO: Roles will need replacing with actual ones to be granted access, or loaded from configuration
+      needsRole: ['pui\-case\-manager'],
+      roleMatching: RoleMatching.ANY,
+      noRoleMatchRedirect: '/'
+    }
   }
-}];
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

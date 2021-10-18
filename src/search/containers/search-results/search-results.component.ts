03a06fe70ccf82888ea00ae120efa955d256d417
John Benjamin <john.benjamin@hmcts.net>
2021-10-18 11:21:27 +0100

    EUI-4396 Global Search Display search results page

---
+++ src/search/containers/search-results/search-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'exui-search-results',
+  templateUrl: './search-results.component.html',
+  styleUrls: ['./search-results.component.scss']
+})
+export class SearchResultsComponent implements OnInit {
+  
+	
+	
+	ngOnInit(): void {
+
+	}
+}
--- src/search/search-routing.module.ts
+++ src/search/search-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { RoleGuard, RoleMatching } from '@hmcts/rpx-xui-common-lib';
 import { SearchFormComponent } from './containers';
 import { NoResultsComponent } from './containers/no-results/no-results.component';
+import { SearchResultsComponent } from './containers/search-results/search-results.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,13 @@ const routes: Routes = [
     component: NoResultsComponent,
     data: {
       title: 'No results'
+    }
+  },
+  {
+    path: 'results',
+    component: SearchResultsComponent,
+    data: {
+      title: 'Search Results'
     }
   }
 ];

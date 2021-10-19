5c75a4129c8a2f1b1c2be27967f76b33233a57e9
John Benjamin <john.benjamin@hmcts.net>
2021-10-19 17:11:15 +0100

    EUI-4396 Global Search Display search results page

--- src/search/containers/search-results/search-results.component.ts
+++ src/search/containers/search-results/search-results.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SearchRequestCriteria } from 'src/search/models/search-request-criteria.model';
+import { SearchRequestSortCriteria } from 'src/search/models/search-request-sort-criteria.model';
 import { SearchRequest } from 'src/search/models/search-request.model';
 import { SearchResult } from 'src/search/models/search-result.model';
 import { SearchService } from '../../services/search.service';
@@ -10,17 +12,37 @@ import { SearchService } from '../../services/search.service';
 })
 export class SearchResultsComponent implements OnInit {
 
-  searchRequest: SearchRequest;
+  // searchRequest: SearchRequest;
   searchResult: SearchResult;
   showSpinner: boolean = false;
 
   constructor(private searchService: SearchService) { }
 
   ngOnInit(): void {
-    this.searchService.getResults(this.searchRequest).subscribe(searchResult => {
-    this.searchResult = searchResult;
-    console.log(searchResult);
-    this.showSpinner = false;
-  });
-}
+    const searchRequestCriteria: SearchRequestCriteria = {
+      ccdCaseTypeIds: null,
+      ccdJurisdictionIds: ['DIVORCE', 'PROBATE', 'PUBLICLAW'],
+      caseManagementBaseLocationIds: null,
+      caseManagementRegionIds: null,
+      caseReferences: null,
+      otherReferences: null,
+      parties: null,
+      stateIds: null
+    };
+
+    const searchRequestSortCriteria: SearchRequestSortCriteria = null;
+
+    const searchRequest: SearchRequest = {
+      searchCriteria: searchRequestCriteria,
+      sortCriteria: searchRequestSortCriteria,
+      maxReturnRecordCount: 25,
+      startRecordNumber: 1
+    }
+
+    this.searchService.getResults(searchRequest).subscribe(searchResult => {
+      this.searchResult = searchResult;
+      console.log(searchResult);
+      this.showSpinner = false;
+    });
+  }
 }

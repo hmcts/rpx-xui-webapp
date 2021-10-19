b88cc210dbfd32a0e0ecb1860133d5b616712504
Raymond Liang <ray.liang@atos.net>
2021-10-19 10:59:26 +0100

    EUI-4603-b

--- src/cases/containers/case-hearings/case-hearings.component.spec.ts
+++ src/cases/containers/case-hearings/case-hearings.component.spec.ts
@@ -19,22 +19,32 @@ describe('CaseHearingsComponent', () => {
   let de: DebugElement;
   let mockStore: any;
   let mockRoleCategoryMappingService: RoleCategoryMappingService;
+  const HEARING_DAY_SCHEDULE_1: HearingDayScheduleModel = {
+    hearingStartDateTime: '2021-05-01T16:00:00.000+0000',
+    hearingEndDateTime: '2021-05-04T16:00:00.000+0000',
+    listAssistSessionID: '0d22d836-b25a-11eb-a18c-f2d58a9b7ba5',
+    hearingVenueId: 'venue 1',
+    hearingRoomId: 'room 1',
+    hearingPanel: ['child'],
+  };
+  const CASE_HEARING_1: CaseHearingModel = {
+    hearingID: 'h111111',
+    hearingType: 'Case management hearing',
+    hmcStatus: 'Upcoming',
+    lastResponseReceivedDateTime: '',
+    responseVersion: 'rv1',
+    hearingListingStatus: HearingListingStatusEnum.LISTED,
+    listAssistCaseStatus: '',
+    hearingDaySchedule: [HEARING_DAY_SCHEDULE_1],
+  };
+  const HEARINGS_LIST: CaseHearingsMainModel = {
+    hmctsServiceID: 'SSCS',
+    caseRef: '1568642646198441',
+    caseHearings: [CASE_HEARING_1],
+  };
   const initialState = {
     hearings: {
-      hearingsList: {
-        hmctsServiceID: 'SSCS',
-        caseRef: '1568642646198441',
-        caseHearings: [{
-          hearingID: 'h111111',
-          hearingType: 'Case management hearing',
-          hmcStatus: 'Upcoming',
-          lastResponseReceivedDateTime: '',
-          responseVersion: 'rv1',
-          hearingListingStatus: HearingListingStatusEnum.LISTED,
-          listAssistCaseStatus: '',
-          hearingDaySchedule: null,
-        }],
-      },
+      hearingsList: HEARINGS_LIST,
     }
   };
 
@@ -102,29 +112,6 @@ describe('CaseHearingsComponent', () => {
   });
 
   it('should getHearsListByStatus', () => {
-    const HEARING_DAY_SCHEDULE_1: HearingDayScheduleModel = {
-      hearingStartDateTime: '2021-05-01T16:00:00.000+0000',
-      hearingEndDateTime: '2021-05-04T16:00:00.000+0000',
-      listAssistSessionID: '0d22d836-b25a-11eb-a18c-f2d58a9b7ba5',
-      hearingVenueId: 'venue 1',
-      hearingRoomId: 'room 1',
-      hearingPanel: ['child'],
-    };
-    const CASE_HEARING_1: CaseHearingModel = {
-      hearingID: 'h111111',
-      hearingType: 'Case management hearing',
-      hmcStatus: 'Upcoming',
-      lastResponseReceivedDateTime: '',
-      responseVersion: 'rv1',
-      hearingListingStatus: HearingListingStatusEnum.LISTED,
-      listAssistCaseStatus: '',
-      hearingDaySchedule: [HEARING_DAY_SCHEDULE_1],
-    };
-    const HEARINGS_LIST: CaseHearingsMainModel = {
-      hmctsServiceID: 'SSCS',
-      caseRef: '1568642646198441',
-      caseHearings: [CASE_HEARING_1],
-    };
     mockStore.pipe.and.returnValue(of(HEARINGS_LIST));
     const hearingList = component.getHearsListByStatus(HearingsSectionStatusEnum.UPCOMING);
     fixture.detectChanges();

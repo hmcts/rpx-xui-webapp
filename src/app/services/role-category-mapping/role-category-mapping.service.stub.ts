528d9897d467f668fcdc65487cf83de626f9c730
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2021-10-20 07:09:20 +0100

    linting 2

--- src/app/services/role-category-mapping/role-category-mapping.service.stub.ts
+++ src/app/services/role-category-mapping/role-category-mapping.service.stub.ts
@@ -1,5 +1,6 @@
-import { UserRole } from "src/app/models/user-details.model";
 import { Observable, of } from "rxjs";
+import { UserRole } from "src/app/models/user-details.model";
+
 export class RoleCategoryMappingServiceStub {
     public isJudicialCategory = (userRoles$: Observable<string[]>): Observable<boolean>  => of(true);
     public isLegalOpsCategory = (userRoles$: Observable<string[]>): Observable<boolean>  => of(true);

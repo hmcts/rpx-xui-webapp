c650094c1648cd93595cb95a5ba69b460f70bf83
Uday Denduluri <uday.denduluri@gmail.com>
2021-10-21 14:04:39 +0100

    retrieving role types

--- api/roleAccess/roleAssignmentService.ts
+++ api/roleAccess/roleAssignmentService.ts
@@ -1,11 +1,31 @@
+import { getConfigValue } from '../configuration';
 import { NextFunction, Response } from 'express';
 import { EnhancedRequest } from '../lib/models';
-import { RoleCategory } from './models/allocate-role.enum';
+import { SERVICES_ROLE_ASSIGNMENT_API_PATH } from '../configuration/references';
+import { setHeaders } from '../lib/proxy';
+import { http } from '../lib/http';
+import { AxiosResponse } from 'axios';
+import { Role } from './models/roleType';
 
 export async function getPossibleRoles(req: EnhancedRequest, res: Response, next: NextFunction): Promise<any> {
-  const personRoles = [
-    {roleId: 'lead-judge', roleName: 'Lead judge', roleCategory: RoleCategory.JUDICIAL},
-    {roleId: 'hearing-judge', roleName: 'Hearing judge', roleCategory: RoleCategory.JUDICIAL},
-    {roleId: 'case-manager', roleName: 'Case manager', roleCategory: RoleCategory.LEGAL_OPERATIONS}];
-  return res.send(personRoles).status(200);
+  const basePath = getConfigValue(SERVICES_ROLE_ASSIGNMENT_API_PATH);
+  const fullPath = `${basePath}/am/role-assignments/roles`;
+  const headers = setHeaders(req);
+  try {
+    const response: AxiosResponse = await http.get(fullPath, {headers});
+    const results = (response.data as Role[]);
+    const filteredResults = results.filter(filterRoleAssignments());
+    console.log(JSON.stringify(filteredResults));
+    const roles = filteredResults.map(roleApi => ({
+      roleCategory: roleApi.category,
+      roleId: roleApi.name,
+      roleName: roleApi.label,
+    }));
+    return res.send(roles).status(200);
+  } catch (error) {
+      next(error);
+  }
+}
+function filterRoleAssignments(): (value: Role, index: number, array: Role[]) => unknown {
+  return role => role.substantive && role.patterns.filter(pattern => pattern.roleType.values.includes('CASE')).length > 0;
 }

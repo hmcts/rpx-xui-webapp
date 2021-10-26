e699b7d648e4a2625df077fdc0b18c35833a987d
John Benjamin <john.benjamin@hmcts.net>
2021-10-26 17:18:21 +0100

    EUI-4372 Global search validations unhappy path

--- src/search/models/index.ts
+++ src/search/models/index.ts
@@ -2,3 +2,4 @@ export { SearchRequestCriteria } from './search-request-criteria.model';
 export { SearchRequestSortCriteria } from './search-request-sort-criteria.model';
 export { SearchRequest } from './search-request.model';
 export { SearchResult } from './search-result.model';
+export { SearchValidationError } from './search-validatinon-error.model';

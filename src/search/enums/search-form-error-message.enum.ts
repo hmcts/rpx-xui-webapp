5a0291774d9d2f2c82cce1cac615c5c87177a77e
John Benjamin <john.benjamin@hmcts.net>
2021-10-28 09:29:10 +0100

    EUI-4372 Global search validations unhappy path

---
+++ src/search/enums/search-form-error-message.enum.ts
@@ -0,0 +1,6 @@
+export enum SearchFormErrorMessage {
+  EMAIL = 'Enter a valid email',
+  POSTCODE = 'Enter a valid postcode',
+  DATE_OF_BIRTH = 'Enter a valid date of birth',
+  DATE_OF_DEATH = 'Enter a valid date of death'
+}

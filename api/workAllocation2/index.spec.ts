88ed1e2dcb1a20959113e15f5a439e745d497d02
connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>
2021-10-29 15:28:54 +0100

    Fix tests and remove console.log

--- api/workAllocation2/index.spec.ts
+++ api/workAllocation2/index.spec.ts
@@ -72,7 +72,7 @@ describe('workAllocation', () => {
   describe('searchTask', () => {
 
     it('should make a post request and respond appropriately', async () => {
-      spy = sandbox.stub(httpMock, 'post').resolves(res);
+      spy = sandbox.stub(http, 'post').resolves(res);
       const req = mockReq({
         body: {
           searchRequest: { search_parameters: [] },
@@ -88,7 +88,7 @@ describe('workAllocation', () => {
       await searchTask(req, response, next);
       // Should have the correct URL and the appropriate payload.
       const args = spy.getCall(0).args;
-      expect(args[0]).to.equal(`${baseWorkAllocationTaskUrl}/myTasks?view=caseworker`);
+      expect(args[0]).to.equal(`${baseWorkAllocationTaskUrl}/task`);
       expect(args[1]).to.deep.equal({search_parameters: []});
 
       // Should have received the HTTP response. The search simply returns the data.
@@ -97,7 +97,7 @@ describe('workAllocation', () => {
     });
 
     it('should make a post request with pagination and respond appropriately', async () => {
-      spy = sandbox.stub(httpMock, 'post').resolves(res);
+      spy = sandbox.stub(http, 'post').resolves(res);
       const req = mockReq({
         body: {
           searchRequest: { search_parameters: [], pagination_parameters: {page_size: 11, page_number: 3}},
@@ -113,12 +113,8 @@ describe('workAllocation', () => {
       await searchTask(req, response, next);
       // Should have the correct URL and the appropriate payload.
       const args = spy.getCall(0).args;
-      expect(args[0]).to.equal(`${baseWorkAllocationTaskUrl}/myTasks?view=caseworker?first_result=22&max_results=11`);
-      expect(args[1]).to.deep.equal({"pagination_parameters": {
-        "page_number": 3,
-        "page_size": 11,
-      },
-      search_parameters: []});
+      expect(args[0]).to.equal(`${baseWorkAllocationTaskUrl}/task?first_result=22&max_results=11`);
+      expect(args[1]).to.deep.equal({search_parameters: []});
 
       // Should have received the HTTP response. The search simply returns the data.
       expect(response.data.length).to.equal(3);

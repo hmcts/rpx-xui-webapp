efc75e923973d2db06d5585bd000d64cd0605f02
John Benjamin <john.benjamin@hmcts.net>
2021-10-29 16:39:44 +0100

    EUI-4679 Global search date of death validation unhappy path

--- src/search/enums/search-form-error-message.enum.ts
+++ src/search/enums/search-form-error-message.enum.ts
@@ -1,5 +1,5 @@
 export enum SearchFormErrorMessage {
-  EMAIL = 'Enter a valid email',
+  EMAIL = 'Enter an email address in the correct format, like name@example.com',
   POSTCODE = 'Enter a valid postcode',
   DATE_OF_BIRTH = 'Enter a valid date of birth',
   DATE_OF_DEATH = 'Enter a valid date of death',

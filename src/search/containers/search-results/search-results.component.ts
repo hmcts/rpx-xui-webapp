bdce3e1ab82df65a743a02dc734b4f76e18cdd44
John Benjamin <john.benjamin@hmcts.net>
2021-11-02 09:12:40 +0000

    EUI-4396 Global Search Display search results page

--- src/search/containers/search-results/search-results.component.ts
+++ src/search/containers/search-results/search-results.component.ts
@@ -17,73 +17,10 @@ export class SearchResultsComponent implements OnInit {
   public searchResult: SearchResult;
   public showSpinner: boolean = false;
 
-  constructor(private searchService: SearchService) { }
+  constructor(private readonly searchService: SearchService) { }
 
   ngOnInit(): void {
-    const searchRequestCriteria: SearchRequestCriteria = {
-      "ccdJurisdictionIds": ["BEFTA_MASTER"]
-    };
-
-		// const searchRequestCriteria: SearchRequestCriteria = {
-		// 	stateIds: [
-		// 		"CaseCreated",
-		// 		"CaseUpdated"
-		// 	]
-		// };
-
-    const searchRequestSortCriteria: SearchRequestSortCriteria[] = [
-			{
-					"sortBy": "caseName",
-					"sortDirection": "descending"
-			},
-			{
-					"sortBy": "caseManagementCategoryName",
-					"sortDirection": "ascending"
-			},
-			{
-					"sortBy": "createdDate",
-					"sortDirection": "ascending"
-			}
-	];
-
-    const searchRequest: SearchRequest = {
-      searchCriteria: searchRequestCriteria,
-      sortCriteria: searchRequestSortCriteria,
-      maxReturnRecordCount: 25,
-      startRecordNumber: 1
-    };
-
-		console.log(searchRequest);
-
-		const request = {
-			"searchCriteria": {
-					"stateIds": [
-							"CaseCreated",
-							"CaseUpdated"
-					]
-			},
-			"sortCriteria": [
-					{
-							"sortBy": "caseName",
-							"sortDirection": "descending"
-					},
-					{
-							"sortBy": "caseManagementCategoryName",
-							"sortDirection": "ascending"
-					},
-					{
-							"sortBy": "createdDate",
-							"sortDirection": "ascending"
-					}
-			],
-			"maxReturnRecordCount": 500,
-			"startRecordNumber": 1
-	};
-
-	console.log(request);
-
-
-    this.searchService.getResults(searchRequest).subscribe(searchResult => {
+		this.searchService.getResults('searchParameters').subscribe(searchResult => {
       this.searchResult = searchResult;
       console.log(searchResult);
       this.showSpinner = false;

0ff9c2a3367664f4f77f6a8679d82a5a67f0460c
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2021-11-09 09:20:47 +0000

    responedate fixes and change creation date

--- src/hearings/viewModel/case-hearing-view.model.ts
+++ src/hearings/viewModel/case-hearing-view.model.ts
@@ -1,4 +1,4 @@
 import { CaseHearingModel } from 'api/hearings/models/caseHearing.model';
 export interface CaseHearingViewModel extends CaseHearingModel {
-    creationDateTime: string;
+    mostRecentHearingStartDateTime: string;
 }

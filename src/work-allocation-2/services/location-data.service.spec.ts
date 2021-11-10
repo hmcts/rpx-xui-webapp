1e48292254eb52f618338a252fca641fabf37b39
Kuda Nyamainashe <35928491+kuda1992@users.noreply.github.com>
2021-11-10 15:48:03 +0000

    cache the types of work

--- src/work-allocation-2/services/location-data.service.spec.ts
+++ src/work-allocation-2/services/location-data.service.spec.ts
@@ -1,22 +1,25 @@
+import { of } from 'rxjs/internal/observable/of';
 import { LocationDataService } from './location-data.service';
 
 describe('WorkAllocation', () => {
-  const mockHttpService = jasmine.createSpyObj('mockHttpService', ['put', 'get', 'post']);
+  const mockHttpService = jasmine.createSpyObj('mockHttpService', ['put', 'get', 'post'])
+  mockHttpService.get.and.returnValue(of(null));
+  const mockSessionStorageService = jasmine.createSpyObj('mockSessionStorageService', ['getItem', 'setItem']);
 
   describe('LocationDataService', () => {
     it('should be Truthy', () => {
-      const service = new LocationDataService(mockHttpService);
+      const service = new LocationDataService(mockHttpService, mockSessionStorageService);
       expect(service).toBeTruthy();
     });
 
     it('getLocations should make correct api call', () => {
-      const service = new LocationDataService(mockHttpService);
+      const service = new LocationDataService(mockHttpService, mockSessionStorageService);
       service.getLocations();
       expect(mockHttpService.get).toHaveBeenCalledWith(LocationDataService.locationUrl);
     });
 
     it('getLocation should make correct api call', () => {
-      const service = new LocationDataService(mockHttpService);
+      const service = new LocationDataService(mockHttpService, mockSessionStorageService);
       service.getLocation('location123');
       expect(mockHttpService.get).toHaveBeenCalledWith(`${LocationDataService.locationUrl}/location123`);
     });

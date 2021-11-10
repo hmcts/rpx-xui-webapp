54c34b62e1a81cb34e03e2b1624c8affdbf59b52
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2021-11-10 11:44:34 +0000

    component

---
+++ src/hearings/components/index.ts
@@ -0,0 +1,7 @@
+import { SearchLocationComponent } from "./search-location/search-location.component";
+
+export const components: any[] = [
+    SearchLocationComponent,
+];
+
+export * from './search-location/search-location.component';

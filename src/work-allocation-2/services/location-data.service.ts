c50aaeb5c32448a36b26cf00462592fcda581310
Kuda Nyamainashe <35928491+kuda1992@users.noreply.github.com>
2021-11-10 15:48:03 +0000

    cache the types of work

--- src/work-allocation-2/services/location-data.service.ts
+++ src/work-allocation-2/services/location-data.service.ts
@@ -1,18 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { of } from 'rxjs/internal/observable/of';
+import { tap } from 'rxjs/operators';
+import { SessionStorageService } from '../../app/services';
 
 import { Location } from '../models/dtos';
 
 @Injectable()
 export class LocationDataService {
   public static locationUrl: string = '/workallocation2/location';
-  public constructor(private readonly http: HttpClient) {}
+  public static locationsStorageKey: string = 'locations_cache';
+
+  public constructor(private readonly http: HttpClient, private readonly sessionStorage: SessionStorageService) {
+  }
 
   public getLocation(locationId: string): Observable<Location> {
     return this.http.get<Location>(`${LocationDataService.locationUrl}/${locationId}`);
   }
+
   public getLocations(): Observable<Location[]> {
-    return this.http.get<Location[]>(LocationDataService.locationUrl);
+    if (this.sessionStorage.getItem(LocationDataService.locationsStorageKey)) {
+      const locations = JSON.parse(this.sessionStorage.getItem(LocationDataService.locationsStorageKey));
+      return of(locations);
+    }
+    return this.http.get<Location[]>(LocationDataService.locationUrl)
+      .pipe(
+        tap((locations: Location[]) => this.sessionStorage.setItem(LocationDataService.locationsStorageKey, JSON.stringify(locations)))
+      );
   }
 }

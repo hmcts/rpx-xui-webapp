b251ee1b5094b8bb7dae32c8113174ff28c9836d
rayliangatos <57181888+rayliangatos@users.noreply.github.com>
2021-11-11 14:43:15 +0000

    EUI-4692-mock-hearing-stage (#1446)
    * EUI-4692-mock-hearing-stage
    
    * EUI-4692-mock-hearing-stage
    
    * EUI-4692-mock-hearing-stage
    
    * EUI-4692-mock-hearing-stage
    
    * EUI-4692-mock-hearing-stage
    
    * reformat code

--- api/hearings/index.ts
+++ api/hearings/index.ts
@@ -1,15 +1,17 @@
-import { NextFunction, Response } from 'express';
-import { handleGet } from '../common/mockService';
-import { getConfigValue } from '../configuration';
-import { SERVICES_HEARINGS_COMPONENT_API } from '../configuration/references';
+import {NextFunction, Response} from 'express';
+import {handleGet} from '../common/mockService';
+import {getConfigValue} from '../configuration';
+import {SERVICES_HEARINGS_COMPONENT_API, SERVICES_PRD_API_URL} from '../configuration/references';
 import * as mock from '../hearings/hearing.mock';
-import { EnhancedRequest } from '../lib/models';
-import { CaseHearingsMainModel } from './models/caseHearingsMain.model';
-import { hearingStatusMappings } from './models/hearingStatusMappings';
+import {EnhancedRequest} from '../lib/models';
+import {CaseHearingsMainModel} from './models/caseHearingsMain.model';
+import {hearingStatusMappings} from './models/hearingStatusMappings';
+import {RefDataByCategoryModel, RefDataByServiceModel} from "./models/refData.model";
 
 mock.init();
 
-const url: string = getConfigValue(SERVICES_HEARINGS_COMPONENT_API);
+const hearingsUrl: string = getConfigValue(SERVICES_HEARINGS_COMPONENT_API);
+const prdUrl: string = getConfigValue(SERVICES_PRD_API_URL);
 
 /**
  * getHearings from case ID
@@ -17,7 +19,7 @@ const url: string = getConfigValue(SERVICES_HEARINGS_COMPONENT_API);
 export async function getHearings(req: EnhancedRequest, res: Response, next: NextFunction) {
   // @ts-ignore
   const caseId = req.query.caseId;
-  const markupPath: string = `${url}/hearings/${caseId}`;
+  const markupPath: string = `${hearingsUrl}/hearings/${caseId}`;
 
   try {
     const {status, data}: { status: number, data: CaseHearingsMainModel } = await handleGet(markupPath, req);
@@ -28,6 +30,34 @@ export async function getHearings(req: EnhancedRequest, res: Response, next: Nex
       }));
 
     res.status(status).send(data);
+  } catch (error) {
+    next(error);
+  }
+}
+
+/**
+ * getRefData from category and service ID
+ */
+export async function getRefData(req: EnhancedRequest, res: Response, next: NextFunction) {
+  // @ts-ignore
+  const category = req.query.category;
+  const service = req.query.service;
+  const markupPath: string = `${prdUrl}/refdata/lov/${category}/${service}`;
+  try {
+    const {status, data}: { status: number, data: RefDataByCategoryModel[] } = await handleGet(markupPath, req);
+    const refDataByCategory: RefDataByCategoryModel = data.find(refDataByCategoryModel =>
+      refDataByCategoryModel.categoryKey === category);
+    if (refDataByCategory && refDataByCategory.services) {
+      const refDataByService: RefDataByServiceModel = refDataByCategory.services.find(aService =>
+        aService.serviceID === service);
+      if (refDataByService && refDataByService.values) {
+        res.status(status).send(refDataByService.values);
+      } else {
+        res.status(status).send([]);
+      }
+    } else {
+      res.status(status).send([]);
+    }
   } catch (error) {
     next(error);
   }

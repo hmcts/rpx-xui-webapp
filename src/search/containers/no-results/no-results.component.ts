d8d3420ed69225bd2863dd5acd89c862913a4940
John Benjamin <john.benjamin@hmcts.net>
2021-11-12 15:47:32 +0000

    EUI-4449 Global search other unhappy paths

--- src/search/containers/no-results/no-results.component.ts
+++ src/search/containers/no-results/no-results.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'exui-no-results',
   templateUrl: './no-results.component.html',
   styleUrls: ['./no-results.component.scss']
 })
-export class NoResultsComponent { }
+export class NoResultsComponent implements OnInit, OnDestroy {
+
+  public paramMapSubscription$: Subscription;
+  public isError: boolean;
+
+  constructor(private readonly route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.paramMapSubscription$ = this.route.paramMap.subscribe(params => {
+      this.isError = params.get('error') === 'false' ? false : true;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.paramMapSubscription$) {
+      this.paramMapSubscription$.unsubscribe();
+    }
+  }
+}

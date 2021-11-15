33257b4bda757c24ade11cfadf14e0597abdf254
John Benjamin <john.benjamin@hmcts.net>
2021-11-15 09:53:23 +0000

    EUI-4449 Global search other unhappy paths

--- src/search/containers/no-results/no-results.component.spec.ts
+++ src/search/containers/no-results/no-results.component.spec.ts
@@ -1,9 +1,10 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
 
 import { NoResultsComponent } from './no-results.component';
 
-describe('NoResultsComponent', () => {
+fdescribe('NoResultsComponent', () => {
   let component: NoResultsComponent;
   let fixture: ComponentFixture<NoResultsComponent>;
 
@@ -23,5 +24,23 @@ describe('NoResultsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should unsubscribe subscriptions onDestroy', () => {
+    component.paramMapSubscription$ = new Observable().subscribe();
+    spyOn(component.paramMapSubscription$, 'unsubscribe').and.callThrough();
+
+    component.ngOnDestroy();
+    expect(component.paramMapSubscription$.unsubscribe).toHaveBeenCalled();
+  });
+
+  it('should display no results content if no error', () => {
+    component.isError = false;
+    expect(fixture.debugElement.nativeElement.querySelector('.govuk-heading-xl').innerText).toContain('No results');
+  });
+
+  it('should display something went wrong content if error', () => {
+    component.isError = true;
+    expect(fixture.debugElement.nativeElement.querySelector('.govuk-heading-xl').innerText).toContain('Something went wrong');
   });
 });

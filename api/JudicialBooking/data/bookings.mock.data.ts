8721f5cbe87e5bebccb429e9f96f35aa43e20992
tarik.dogan <tarik.dogan@hmcts.net>
2021-11-17 16:31:55 +0000

    EUI-4742 - mock service integrated

---
+++ api/JudicialBooking/data/bookings.mock.data.ts
@@ -0,0 +1,16 @@
+import { Bookings } from "JudicialBooking/models/booking.model";
+
+export const bookings: Bookings  = {
+  "bookings":
+  [{
+    "appointmentId": "101",
+    "base_location_id": "103",
+    "beginTime": new Date("2021-01-01T00:00:00Z"),
+    "contract_type_id": "102",
+    "created" : new Date("2021-02-23T06:37:58Z"),
+    "endTime": new Date("2021-09-01T00:00:00Z"),
+    "region_id": "104",
+    "roleId": "caseworker",
+    "userId": "21334a2b-79ce-44eb-9168-2d49a744be9c",
+  }],
+};

33d927d50ea124f1a9234e844e067c521a5d78c7
John Benjamin <john.benjamin@hmcts.net>
2021-11-17 14:49:14 +0000

    EUI-4449 Global search other unhappy paths

--- src/search/containers/search-results/search-results.component.ts
+++ src/search/containers/search-results/search-results.component.ts
@@ -91,7 +91,8 @@ export class SearchResultsComponent implements OnInit, OnDestroy {
       this.searchService.getResults(),
       this.jurisdictionService.getJurisdictions()
     ]).subscribe(
-      results => this.onSearchSubscriptionHandler(results)
+      results => this.onSearchSubscriptionHandler(results),
+      error => this.router.navigate(['/search/noresults', NoResultsMessageId.ERROR], { relativeTo: this.route })
     );
   }
 

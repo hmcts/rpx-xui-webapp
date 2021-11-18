433817092e545e6f1b7c12715efc9333448cc5b5
Paul Howes <paul.howes@HMCTS.NET>
2021-11-18 17:40:33 +0000

    Resolve linting errors and warnings

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -1,11 +1,10 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import * as fromComponents from './components';
-import * as fromContainers from './containers';
 import { AlertService } from '@hmcts/ccd-case-ui-toolkit';
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { SharedModule } from '../app/shared/shared.module';
 import { bookingRouting } from './booking.routes';
+import * as fromComponents from './components';
 import { BookingNavigationComponent } from './components/booking-navigation/booking-navigation.component';
 import { BookingLocationComponent } from './containers/booking-location/booking-location.component';
 import { BookingDateComponent } from './containers/booking-date/booking-date.component';
@@ -23,10 +22,7 @@ import { ReactiveFormsModule } from '@angular/forms';
   declarations: [
     ... fromComponents.components,
     ...fromContainers.containers,
-    BookingNavigationComponent,
-    BookingLocationComponent,
-    BookingDateComponent,
-    BookingCheckComponent
+    BookingNavigationComponent
   ],
   providers: [
     AlertService

8b540764588b1bde017647b76e32c3e02965fd47
tarik.dogan <tarik.dogan@hmcts.net>
2021-11-18 11:45:04 +0000

    EUI-4742 review changes

--- api/JudicialBooking/index.ts
+++ api/JudicialBooking/index.ts
@@ -1,7 +1,7 @@
 import { bookings } from './data/bookings.mock.data';
 
 export const getBookings = (proxyReq, req, res) => {
-    // when am/booking url is developed , there will be switch flag implimantation to get responds from url or from mock data
+    // when am/booking url is developed , there will be switch flag implementation to get responds from url or from mock data
     if (!bookings) {
         return res.status(404).write('{"errorMessage": "Resource Not found}"');
       } else {

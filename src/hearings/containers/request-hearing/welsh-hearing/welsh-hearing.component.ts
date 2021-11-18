a932facbad4c9cbc726afd6bfdb47a3db89e6ba2
Kartheek-S <92437222+Kartheek-S@users.noreply.github.com>
2021-11-18 17:51:50 +0000

    Feature/enext 4688 (#1481)
    * feature/ENEXT-4688 Welsh hearing
    
    * feature/ENEXT-4688 fixed tests
    
    * feature/ENEXT-4688 Fixed lint issues
    
    * feature/EUI-4688 Fixed CR comments
    
    * feature/EUI-4688 updated with relative path
    
    * EUI-4688 updated return type

---
+++ src/hearings/containers/request-hearing/welsh-hearing/welsh-hearing.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ErrorMessage } from '../../../../app/models';
+
+export const NAME_ERROR: ErrorMessage = {
+  title: 'There is a problem',
+  description: 'You must select Yes or No',
+  fieldId: 'welsh-hint',
+};
+
+@Component({
+  selector: 'exui-welsh-hearing',
+  templateUrl: './welsh-hearing.component.html',
+  styleUrls: ['./welsh-hearing.component.scss'],
+})
+export class WelshHearingComponent implements OnInit {
+  public error: ErrorMessage = null;
+  public welshForm: FormGroup;
+
+  constructor(private readonly formBuilder: FormBuilder) { }
+
+  public ngOnInit(): void {
+    this.initForm();
+  }
+
+  /**
+   * Inits form
+   */
+  public initForm(): void {
+    this.welshForm = this.formBuilder.group({
+      hearingInWelshFlag: [false, Validators.required],
+    });
+  }
+}

c2b3fbbc1ef1e5600f1a402d00f42dd33ac829e6
Paul Howes <paul.howes@HMCTS.NET>
2021-11-18 15:13:12 +0000

    Booking Wrapper WIP

---
+++ src/booking/containers/booking-home/booking-home.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { BookingNavigationEvent } from '../../models';
+import { BookingNavigation } from '../../models/booking-navigation.interface';
+
+@Component({
+  selector: 'exui-booking-home',
+  templateUrl: './booking-home.component.html',
+  styleUrls: ['./booking-home.component.scss']
+})
+export class BookingHomeComponent implements OnInit {
+
+  @Input() public navEvent: BookingNavigation;
+  @Input() public bookingOptionIndex: boolean;
+
+  @Output() public bookingOptionIndexChange = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  public ngOnInit() {
+  }
+
+  public selectOption(index) {
+    this.bookingOptionIndexChange.emit(index);
+  }
+
+}
+

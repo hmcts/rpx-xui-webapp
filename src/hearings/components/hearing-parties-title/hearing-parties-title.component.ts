b301b91fffaf9e8be4ad2737c60e96fe7f61bf09
Kartheek-S <92437222+Kartheek-S@users.noreply.github.com>
2021-11-18 17:51:50 +0000

    Feature/enext 4688 (#1481)
    * feature/ENEXT-4688 Welsh hearing
    
    * feature/ENEXT-4688 fixed tests
    
    * feature/ENEXT-4688 Fixed lint issues
    
    * feature/EUI-4688 Fixed CR comments
    
    * feature/EUI-4688 updated with relative path
    
    * EUI-4688 updated return type

---
+++ src/hearings/components/hearing-parties-title/hearing-parties-title.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'exui-hearing-parties-title',
+  templateUrl: './hearing-parties-title.component.html',
+  styleUrls: ['./hearing-parties-title.component.scss'],
+})
+export class HearingPartiesTitleComponent {
+}

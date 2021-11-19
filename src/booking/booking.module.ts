c9a1b7a903ac75bd494fd3cecaf73116a93e641c
Paul Howes <paul.howes@HMCTS.NET>
2021-11-19 10:38:52 +0000

    Reorder imports for linting

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -1,12 +1,14 @@
-import { AlertService } from '@hmcts/ccd-case-ui-toolkit';
+import * as fromComponents from './components';
+import * as fromContainers from './containers';
+
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { AlertService } from '@hmcts/ccd-case-ui-toolkit';
+
 import { SharedModule } from '../app/shared/shared.module';
 import { bookingRouting } from './booking.routes';
-import * as fromComponents from './components';
 import { BookingNavigationComponent } from './components/booking-navigation/booking-navigation.component';
-import * as fromContainers from './containers';
 
 @NgModule({
   imports: [

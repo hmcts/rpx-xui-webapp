a327ea527777d83f76c444d7002f3ebf52ad871f
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2021-11-19 14:21:52 +0000

    conflict again

--- src/hearings/hearings.routes.ts
+++ src/hearings/hearings.routes.ts
@@ -1,12 +1,8 @@
 import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HealthCheckGuard } from '../app/shared/guards/health-check.guard';
-<<<<<<< HEAD
-=======
 import { LocationSearchContainerComponent } from './containers/request-hearing/location-search-container/location-search-container.component';
->>>>>>> 7d4f2f8065770c1f4115e24f760a882004dc445c
 import { CancelHearingComponent, ChangeHearingComponent, RequestHearingComponent, ViewHearingComponent } from './containers';
-import { LocationSearchContainerComponent } from './containers/request-hearing/location-search-container/location-search-container.component';
 import { WelshHearingComponent } from './containers/request-hearing/welsh-hearing/welsh-hearing.component';
 
 export const ROUTES: Routes = [

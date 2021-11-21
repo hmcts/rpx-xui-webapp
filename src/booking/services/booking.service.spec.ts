16b4efbd88fc72c8163ddec812bf9ed924c79aaa
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2021-11-21 12:18:04 +0000

    EUI-4350 testing

--- src/booking/services/booking.service.spec.ts
+++ src/booking/services/booking.service.spec.ts
@@ -2,7 +2,7 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 import { inject, TestBed } from '@angular/core/testing';
 import { BookingService } from './booking.service';
 
-describe('NocService', () => {
+describe('BookingService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,8 +15,30 @@ describe('NocService', () => {
     });
   });
 
-  it('should be created', inject([BookingService], (service: BookingService) => {
-    expect(service).toBeTruthy();
-  }));
+  describe('getBookings()', () => {
+
+    it('should make a get call', inject([HttpTestingController, BookingService], (httpMock: HttpTestingController, service: BookingService) => {
+      service.getBookings().subscribe(response => {
+        expect(response).toBeNull();
+      });
+
+      const req = httpMock.expectOne('/am/bookings');
+      expect(req.request.method).toEqual('GET');
+      req.flush(null);
+    }));
+  });
+
+  describe('getBookingLocation()', () => {
+
+    it('should make a get call', inject([HttpTestingController, BookingService], (httpMock: HttpTestingController, service: BookingService) => {
+      service.getBookingLocation('123').subscribe(response => {
+        expect(response).toBeNull();
+      });
+
+      const req = httpMock.expectOne('/refdata/location/building-locations?epimms_id=123');
+      expect(req.request.method).toEqual('GET');
+      req.flush(null);
+    }));
+  });
 
 });

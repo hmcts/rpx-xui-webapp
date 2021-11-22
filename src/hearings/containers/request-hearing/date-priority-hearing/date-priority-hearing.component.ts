a8736775bf670e756324f7e4ab702b61aa17190f
Kartheek-S <92437222+Kartheek-S@users.noreply.github.com>
2021-11-22 22:54:10 +0000

    feature/EUI-4698 display logic

--- src/hearings/containers/request-hearing/date-priority-hearing/date-priority-hearing.component.ts
+++ src/hearings/containers/request-hearing/date-priority-hearing/date-priority-hearing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'exui-date-priority-hearing',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./date-priority-hearing.component.scss']
 })
 export class DatePriorityHearingComponent implements OnInit {
+  public priorityForm: FormGroup;
+  public checkedHearingAvailability: string;
+  public partiesNotAvailableDates: string[];
 
-  constructor() { }
+  constructor(private readonly formBuilder: FormBuilder) { }
 
-  ngOnInit() {
+  public ngOnInit(): void {
+    this.initForm();
+    // TODO: Get dates from a service
+    this.partiesNotAvailableDates = ['2 November 2021', '3 November 2021', '4 November 2021'];
+  }
+
+  /**
+   * Inits form
+   */
+  public initForm(): void {
+    this.priorityForm = this.formBuilder.group({
+      durationLength: this.formBuilder.group({
+        hours: [],
+        minutes: []
+      }),
+      hearingAvailability: [],
+    });
+  }
+
+  public showDateAvailability(): void {
+    this.checkedHearingAvailability = this.priorityForm.get('hearingAvailability').value;
   }
 
 }

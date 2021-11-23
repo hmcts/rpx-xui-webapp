754c2e6abdd7214c7ecdf9bb55ab33cf601b1a6f
Raymond Liang <ray.liang@atos.net>
2021-11-23 17:49:54 +0000

    feature/EUI-4753-service-hearing-value

--- api/hearings/data/serviceHearingValues.mock.data.ts
+++ api/hearings/data/serviceHearingValues.mock.data.ts
@@ -1,18 +1,18 @@
 /* tslint:disable:object-literal-sort-keys */
-const searviceHearingValues = {
-  autoListFlag: true,
-  hearingType: '',
-  caseType: '',
-  caseSubTypes: [''],
+const serviceHearingValues = {
+  autoListFlag: false,
+  hearingType: 'Final',
+  caseType: 'Personal Independence Payment',
+  caseSubTypes: ['Conditions of Entitlement', 'Good cause', 'Rate of Assessment / Payability Issues - complex'],
   hearingWindow: {
     range: {
-      start: '2021-10-20',
-      end: '2022-01-31',
+      start: '2021-11-23T09:00:00.000+0000',
+      end: '2021-11-30T09:00:00.000+0000',
     },
     firstDateTimeMustBe: '',
   },
   duration: 45,
-  hearingPriorityType: '',
+  hearingPriorityType: 'Standard',
   numberOfPhysicalAttendees: 2,
   hearingInWelshFlag: false,
   hearingLocations: [],
@@ -39,40 +39,149 @@ const searviceHearingValues = {
     judiciarySpecialisms: [''],
   },
   hearingIsLinkedFlag: false,
-  parties: [{
-    partyName: 'Jane and Smith',
-    partyChannel: 'string',
-    unavailability: [
-      {
-        start: '20211122',
-        end: '20211123',
-      },
-    ],
-  }],
+  parties: [
+    {
+      partyName: 'Jane and Smith',
+      partyChannel: '',
+      unavailability: [
+        {
+          start: '2021-12-10T09:00:00.000+0000',
+          end: '2021-12-31T09:00:00.000+0000',
+        },
+      ],
+    },
+    {
+      partyName: 'DWP',
+      partyChannel: '',
+      unavailability: [
+        {
+          start: '2021-12-20T09:00:00.000+0000',
+          end: '2021-12-31T09:00:00.000+0000',
+        },
+      ],
+    }],
   caseFlags: {
     flags: [
       {
         partyName: 'Jane and Smith',
-        flagId: 'string',
-        flagDescription: 'string',
-        flagStatus: 'string',
+        flagId: 'Language Interpreter',
+        flagDescription: 'Spanish interpreter required',
+        flagStatus: 'ACTIVE',
+      },
+      {
+        partyName: 'Jane and Smith',
+        flagId: 'Sign language interpreter',
+        flagDescription: 'Sign language interpreter required',
+        flagStatus: 'ACTIVE',
+      },
+      {
+        partyName: 'Jane and Smith',
+        flagId: 'Reasonable adjustment',
+        flagDescription: 'Hearing loop required',
+        flagStatus: 'ACTIVE',
+      },
+      {
+        partyName: 'DWP',
+        flagId: 'case flag 1',
+        flagDescription: 'case flag 1 description',
+        flagStatus: 'ACTIVE',
       },
     ],
-    flagAmendURL: 'string',
+    flagAmendURL: '/',
   },
   screenFlow: [
     {
-      screenName: 'string',
+      screenName: 'hearing-requirement',
       navigation: [
         {
-          resultValue1: 'string',
+          resultValue1: 'additional-facilities',
+        },
+      ],
+    },
+    {
+      screenName: 'additional-facilities',
+      navigation: [
+        {
+          resultValue1: 'hearing-stage',
+        },
+      ],
+    },
+    {
+      screenName: 'hearing-stage',
+      navigation: [
+        {
+          resultValue1: 'how-party-attend',
+        },
+      ],
+    },
+    {
+      screenName: 'how-party-attend',
+      navigation: [
+        {
+          resultValue1: 'location-search',
+        },
+      ],
+    },
+    {
+      screenName: 'location-search',
+      conditionKey: 'region',
+      navigation: [
+        {
+          conditionOperator: 'EQUAL',
+          conditionValue: 'Wales',
+          resultValue1: 'welsh-hearing',
+        },
+        {
+          conditionOperator: 'NOT EQUAL',
+          conditionValue: 'Wales',
+          resultValue1: 'specify-judge',
+        },
+      ],
+    },
+    {
+      screenName: 'welsh-hearing',
+      navigation: [
+        {
+          resultValue1: 'specify-judge',
+        },
+      ],
+    },
+    {
+      screenName: 'specify-judge',
+      navigation: [
+        {
+          resultValue1: 'require-panel-or-not',
+        },
+      ],
+    },
+    {
+      screenName: 'require-panel-or-not',
+      navigation: [
+        {
+          resultValue1: 'hearing-timing',
+        },
+      ],
+    },
+    {
+      screenName: 'hearing-timing',
+      navigation: [
+        {
+          resultValue1: 'additional-instruction',
+        },
+      ],
+    },
+    {
+      screenName: 'additional-instruction',
+      navigation: [
+        {
+          resultValue1: 'check-answers',
         },
       ],
     },
   ],
   vocabulary: [
     {
-      word1: 'string',
+      word1: '',
     },
   ],
 };

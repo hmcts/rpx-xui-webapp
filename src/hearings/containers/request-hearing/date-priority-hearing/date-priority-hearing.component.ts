16d71e5778c4bc89cbe97809922c32550bbae6ee
Kartheek-S <92437222+Kartheek-S@users.noreply.github.com>
2021-11-23 21:05:48 +0000

    feature/EUI-4698 updated date config

--- src/hearings/containers/request-hearing/date-priority-hearing/date-priority-hearing.component.ts
+++ src/hearings/containers/request-hearing/date-priority-hearing/date-priority-hearing.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { GovUiConfigModel } from '@hmcts/rpx-xui-common-lib/lib/gov-ui/models';
 import { RefDataModel } from 'api/hearings/models/refData.model';
+
 
 @Component({
   selector: 'exui-date-priority-hearing',
@@ -13,17 +15,49 @@ export class DatePriorityHearingComponent implements OnInit {
   public priorities: RefDataModel[];
   public checkedHearingAvailability: string;
   public partiesNotAvailableDates: string[];
+  public firstHearingDate: GovUiConfigModel;
+  public earliestHearingDate: GovUiConfigModel;
+  public latestHearingDate: GovUiConfigModel;
 
   constructor(private readonly formBuilder: FormBuilder,
               private readonly route: ActivatedRoute) { }
 
   public ngOnInit(): void {
+    this.initDateConfig();
     this.initForm();
 
     this.priorities = this.route.snapshot.data.hearingPriorities.priorities.sort((a, b) => (a.order < b.order ? -1 : 1));
 
     // TODO: Get dates from a service
     this.partiesNotAvailableDates = ['2 November 2021', '3 November 2021', '4 November 2021'];
+  }
+
+  /**
+   * Inits date config
+   */
+  public initDateConfig(): void {
+    this.firstHearingDate = {
+      id: 'firstHearingDate',
+      name: 'firstHearingDate',
+      hint: '',
+      classes: 'govuk-fieldset__legend govuk-fieldset__legend--s',
+      label: 'The first date of the hearing must be'
+    };
+
+    this.earliestHearingDate = {
+      id: 'earliestHearingDate',
+      name: 'earliestHearingDate',
+      hint: '',
+      classes: 'govuk-fieldset__legend govuk-fieldset__legend--s',
+      label: 'Earliest hearing date'
+    };
+    this.latestHearingDate = {
+      id: 'latestHearingDate',
+      name: 'latestHearingDate',
+      hint: '',
+      classes: 'govuk-fieldset__legend govuk-fieldset__legend--s',
+      label: 'Latest hearing date'
+    };
   }
 
   /**

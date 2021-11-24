d63653a2f2d5f18665bc382dfabff2b95822bb94
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2021-11-24 11:05:42 +0000

    stage 1

--- src/hearings/containers/request-hearing/location-search-container/location-search-container.component.spec.ts
+++ src/hearings/containers/request-hearing/location-search-container/location-search-container.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { provideMockStore } from '@ngrx/store/testing';
 import { LocationSearchContainerComponent } from './location-search-container.component';
+import { LocationModel } from 'api/locations/models/location.model';
 
 describe('LocationSearchContainerComponent', () => {
   let component: LocationSearchContainerComponent;
@@ -37,5 +38,62 @@ describe('LocationSearchContainerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display selection in selection list', async (done) => {
+    const location = {
+      court_venue_id: '100',
+      epims_id: '219164',
+      is_hearing_location: 'Y',
+      is_case_management_location: 'Y',
+      site_name: 'Aberdeen Tribunal Hearing Centre',
+      court_name: 'ABERDEEN TRIBUNAL HEARING CENTRE',
+      court_status: 'Open',
+      region_id: '9',
+      region: 'Scotland',
+      court_type_id: '17',
+      court_type: 'Employment Tribunal',
+      open_for_public: 'Yes',
+      court_address: 'AB1, 48 HUNTLY STREET, ABERDEEN',
+      postcode: 'AB11 6LT'
+    } as LocationModel;
+    component.selectedLocation = location;
+    component.addSelection();
+    fixture.detectChanges();
+    done();
+    const selectedLoctions = fixture.debugElement.queryAll(By.css('.location-selection'));
+    expect(selectedLoctions.length).toBeGreaterThan(0);
+  });
+
+  it('should remove selection in selection list', async () => {
+    const location = {
+      court_venue_id: '100',
+      epims_id: '219164',
+      is_hearing_location: 'Y',
+      is_case_management_location: 'Y',
+      site_name: 'Aberdeen Tribunal Hearing Centre',
+      court_name: 'ABERDEEN TRIBUNAL HEARING CENTRE',
+      court_status: 'Open',
+      region_id: '9',
+      region: 'Scotland',
+      court_type_id: '17',
+      court_type: 'Employment Tribunal',
+      open_for_public: 'Yes',
+      court_address: 'AB1, 48 HUNTLY STREET, ABERDEEN',
+      postcode: 'AB11 6LT'
+    } as LocationModel;
+
+    component.selectedLocation = location;
+    component.addSelection();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const selectedLoctions = fixture.debugElement.queryAll(By.css('.location-selection'));
+      const button =  fixture.debugElement.query(By.css('.remove-loation-button'));
+      button.nativeElement.dispatchEvent(new Event('click'));
+      fixture.detectChanges();
+      const selectedLoctionsAfterClick = fixture.debugElement.queryAll(By.css('.location-selection'));
+      expect(selectedLoctions.length).toBeGreaterThan(0);
+      expect(selectedLoctionsAfterClick.length).toEqual(0);
+    });
   });
 });

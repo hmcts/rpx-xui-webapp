3ac0ab48ffdc744783628b8d8ae48b244e9f7b38
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2021-11-24 17:21:34 +0000

    EUI-4350 Booking UI- Work access screen- display story (#1479)
    * EUI-4350 empty folders and files
    
    * EUI-4350 stubs and dummies
    
    * EUI-4742 - mock service integrated
    
    * EUI-4742 fine tune
    
    * EUI-4742 fine tune
    
    * EUI-4742 review changes
    
    * EUI-4742 review changes
    
    * EUI-4350 working dummy
    
    * EUI-4350 bound to mock data
    
    * EUI-4350 booking name call
    
    * Bumping chart version
    
    * EUI-4350 linting
    
    * EUI-4350 testing
    
    * EUI-4350 audit
    
    * EUI-4350 removing redundant scss file
    
    * EUI-4350 empty folders and files
    
    * EUI-4350 stubs and dummies
    
    * EUI-4742 - mock service integrated
    
    * EUI-4742 fine tune
    
    * EUI-4742 fine tune
    
    * EUI-4742 review changes
    
    * EUI-4742 review changes
    
    * Booking Wrapper WIP
    
    * EUI-4350 working dummy
    
    * Resolve linting errors and warnings
    
    * Reorder imports for linting
    
    * Minor tweaks to module and booking wrapper tests
    
    * EUI-4350 bound to mock data
    
    * Booking Wrapper Tests
    
    * EUI-4350 booking name call
    
    * Bumping chart version
    
    * EUI-4350 linting
    
    * EUI-4350 testing
    
    * Remove unnecessary ngOnInit
    
    * EUI-4350 removing redundant scss file
    
    * EUI-4350 correct naming in api
    
    * EUI-4350 removed file
    
    * EUI-4350 fix after navigation changes
    
    * EUI-4350 change request
    
    * EUI-4350 fixed tests
    
    Co-authored-by: tarik.dogan <tarik.dogan@hmcts.net>
    Co-authored-by: Jenkins <nobody@nowhere>
    Co-authored-by: hmcts-jenkins-xui <61146108+hmcts-jenkins-xui[bot]@users.noreply.github.com>
    Co-authored-by: Paul Howes <paul.howes@HMCTS.NET>

--- src/booking/containers/booking-home/booking-home.component.spec.ts
+++ src/booking/containers/booking-home/booking-home.component.spec.ts
@@ -1,27 +1,102 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NewBooking } from '../../models';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { Booking } from '../../models/booking.interface';
+import { BookingService } from '../../services';
 import { BookingHomeComponent } from './booking-home.component';
+
+const DUMMY_BOOKING_1: Booking = {
+  userId: 'Dummy User Id',
+  appointmentId: 'Dummy Appointment Id',
+  roleId: 'Dummy Role Id',
+  contract_type_id: 'Dummy Contract Type Id',
+  base_location_id: 'Dummy Location Id',
+  region_id: 'Dummy Region Id',
+  beginTime: new Date(),
+  endTime: new Date(),
+  created: new Date()
+};
+
+const DUMMY_LOCATION_NAME: string = 'Some place name';
 
 describe('BookingHomeComponent', () => {
   let component: BookingHomeComponent;
   let fixture: ComponentFixture<BookingHomeComponent>;
+  const bookingService = jasmine.createSpyObj<BookingService>('BookingService', ['getBookings', 'getBookingLocation']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BookingHomeComponent ]
+      imports: [
+        ReactiveFormsModule
+      ],
+      declarations: [ BookingHomeComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      providers: [
+        { provide: BookingService, useValue: bookingService }
+      ]
     })
     .compileComponents();
-  }));
 
-  beforeEach(() => {
+    bookingService.getBookings.and.returnValue(of({bookings: [
+      DUMMY_BOOKING_1
+    ]}));
+    bookingService.getBookingLocation.and.returnValue(of([{
+      building_location_name: DUMMY_LOCATION_NAME
+    }]));
+
     fixture = TestBed.createComponent(BookingHomeComponent);
     component = fixture.componentInstance;
     component.newBooking = {} as NewBooking;
     fixture.detectChanges();
+  }));
+
+  it('should display bookings list when there are existing bookings and first radio button is clicked', () => {
+
+    // Click on the first radio button.
+    const element = fixture.debugElement.nativeElement;
+    const firstRadioButton = element.querySelector('#type-0');
+    firstRadioButton.click();
+    fixture.detectChanges();
+    const bookingsList = element.querySelector('#conditional-booking-type-0');
+    expect(bookingsList).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('onSelectOption()', () => {
+
+    it('should change bookingOptionIndex', () => {
+
+      // Click on the second radio button.
+      const element = fixture.debugElement.nativeElement;
+      const secondRadioButton = element.querySelector('#type-1');
+      secondRadioButton.click();
+      expect(component.newBooking.bookingOptionIndex).toEqual(1);
+    });
+  });
+
+  describe('onEventTrigger()', () => {
+
+    it('should emit a navigation enum', () => {
+
+      const eventTriggerSpy = spyOn(component.eventTrigger, 'emit');
+      // Click on the second radio button.
+      const element = fixture.debugElement.nativeElement;
+      const secondRadioButton = element.querySelector('#type-1');
+      secondRadioButton.click();
+      fixture.detectChanges();
+      const button = element.querySelector('button.govuk-button');
+      button.click();
+      expect(eventTriggerSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe('assignBookingLocation()', () => {
+
+    it('should assign locationName', () => {
+
+      expect(component.existingBookings[0].locationName).toEqual('Some place name');
+    });
   });
 });

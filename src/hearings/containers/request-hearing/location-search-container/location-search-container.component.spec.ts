1810f96d60947b9183b2741c15cc5bccedfdf6bd
Kartheek-S <92437222+Kartheek-S@users.noreply.github.com>
2021-11-25 10:41:18 +0000

    feature/EUI-4698 test fix

--- src/hearings/containers/request-hearing/location-search-container/location-search-container.component.spec.ts
+++ src/hearings/containers/request-hearing/location-search-container/location-search-container.component.spec.ts
@@ -1,36 +1,26 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Store } from '@ngrx/store';
 import { provideMockStore } from '@ngrx/store/testing';
 import { LocationSearchContainerComponent } from './location-search-container.component';
 
 describe('LocationSearchContainerComponent', () => {
   let component: LocationSearchContainerComponent;
   let fixture: ComponentFixture<LocationSearchContainerComponent>;
-
-  const initialState = {
-    hearings: {
-      hearingsList: {
-        caseHearingsMainModel: [
-          {
-            hmctsServiceID: 'SSCS'
-          }
-        ],
-        hearingListMainModel: null
-      },
-    }
-  };
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LocationSearchContainerComponent ],
-      providers: [
-        provideMockStore({initialState})],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    })
-    .compileComponents();
-  }));
-
+  let mockStore: any;
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [LocationSearchContainerComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        provideMockStore(),
+      ]
+    })
+      .compileComponents();
+    mockStore = TestBed.get(Store);
+    mockStore = jasmine.createSpyObj('Store', ['pipe', 'dispatch']);
     fixture = TestBed.createComponent(LocationSearchContainerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -38,5 +28,9 @@ describe('LocationSearchContainerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  afterEach(() => {
+    fixture.destroy();
   });
 });

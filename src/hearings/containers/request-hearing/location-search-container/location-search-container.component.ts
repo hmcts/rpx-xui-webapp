f49549d5deea7cc288f2c90a851a0734c5f857cb
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2021-11-26 11:41:02 +0000

    model name changs

--- src/hearings/containers/request-hearing/location-search-container/location-search-container.component.ts
+++ src/hearings/containers/request-hearing/location-search-container/location-search-container.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Store, select } from '@ngrx/store';
-import { LocationModel } from 'api/locations/models/location.model';
+import { LocationByEPIMSModel, LocationModel } from 'api/locations/models/location.model';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import * as fromHearingStore from '../../../../hearings/store';
 @Component({
   selector: 'exui-location-search-container',
   templateUrl: './location-search-container.component.html',
-  styleUrls: ['./location-search.component.scss']
+  styleUrls: ['./location-search-container.component.scss']
 })
 export class LocationSearchContainerComponent implements OnInit {
   public locationType: string;
   public findLocationFormGroup: FormGroup;
-  public selectedLocations$: Observable<LocationModel[]>;
-  public selectedLocation: LocationModel;
+  public selectedLocations$: Observable<LocationByEPIMSModel[]>;
+  public selectedLocation: LocationByEPIMSModel ;
   public serviceIds: string = 'SSCS';
 
   constructor(private readonly hearingStore: Store<fromHearingStore.State>, fb: FormBuilder) {
@@ -35,14 +35,14 @@ export class LocationSearchContainerComponent implements OnInit {
 
   public addSelection(): void {
     this.selectedLocations$.subscribe(selectedLocations => {
-        selectedLocations.push(this.findLocationFormGroup.controls.locationSelectedFormControl.value as LocationModel);
+        selectedLocations.push(this.findLocationFormGroup.controls.locationSelectedFormControl.value as LocationByEPIMSModel);
         this.findLocationFormGroup.controls.locationSelectedFormControl.setValue(undefined);
     });
   }
 
   public removeSelection(location: LocationModel): void {
     this.selectedLocations$.subscribe(selectedLocations => {
-      const index = selectedLocations.findIndex(selectedLocation => selectedLocation.court_venue_id === location.court_venue_id);
+      const index = selectedLocations.findIndex(selectedLocation => selectedLocation.epims_id === location.epims_id);
       selectedLocations.splice(index, 1);
     });
   }

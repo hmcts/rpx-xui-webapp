5d82f8d8372ac0ab0524b5a38de3c9cf0d6aaab3
tarik.dogan <tarik.dogan@hmcts.net>
2021-12-02 13:08:12 +0000

    EUI-4633 merged, logics merged , tests added

--- src/booking/containers/booking-check/booking-check.component.spec.ts
+++ src/booking/containers/booking-check/booking-check.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BookingCheckComponent } from './booking-check.component';
+
+import { BookingProcess } from 'src/booking/models';
 
 describe('BookingCheckComponent', () => {
   let component: BookingCheckComponent;
@@ -11,15 +12,50 @@ describe('BookingCheckComponent', () => {
       declarations: [ BookingCheckComponent ]
     })
     .compileComponents();
-  }));
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(BookingCheckComponent);
     component = fixture.componentInstance;
+    component.bookingProcess = {locationName: 'London'} as BookingProcess;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should create component and show the correct info message and values on row for today only case', () => {
+    component.bookingProcess = {locationName: 'London', startDate: new Date('Thu Dec 02 2021 09:00:00 GMT+0000') , endDate: new Date('Sun Dec 02 2021 23:59:59 GMT+0000')} as BookingProcess;
+    fixture.detectChanges();
+    const caption = fixture.debugElement.nativeElement.querySelector('.govuk-caption-l');
+    expect(caption.textContent).toContain('Work access');
+    const infoMassage = fixture.debugElement.nativeElement.querySelector('h1');
+    expect(infoMassage.textContent).toContain('Check your new booking');
+    const location = fixture.debugElement.nativeElement.querySelector('.govuk-summary-list__row').childNodes[1];
+    expect(location.textContent).toContain('London');
+    debugger;
+    const dateInterval = fixture.debugElement.nativeElement.querySelector('.govuk-summary-list__row').nextElementSibling.childNodes[1];
+    expect(dateInterval.textContent).toContain('02 December 2021 to 02 December 2021');
+  });
+  it('should create component and show the correct info message and values on row for this week case', () => {
+    component.bookingProcess = {locationName: 'London', startDate: new Date('Thu Dec 02 2021 09:00:00 GMT+0000') , endDate: new Date('Sun Dec 05 2021 23:59:59 GMT+0000')} as BookingProcess;
+    fixture.detectChanges();
+    const caption = fixture.debugElement.nativeElement.querySelector('.govuk-caption-l');
+    expect(caption.textContent).toContain('Work access');
+    const infoMassage = fixture.debugElement.nativeElement.querySelector('h1');
+    expect(infoMassage.textContent).toContain('Check your new booking');
+    const location = fixture.debugElement.nativeElement.querySelector('.govuk-summary-list__row').childNodes[1];
+    expect(location.textContent).toContain('London');
+    const dateInterval = fixture.debugElement.nativeElement.querySelector('.govuk-summary-list__row').nextElementSibling.childNodes[1];
+    expect(dateInterval.textContent).toContain('02 December 2021 to 05 December 2021');
+  });
+  it('should create component and show the correct info message and values on row for date range case', () => {
+    component.bookingProcess = {locationName: 'London', startDate: new Date('Thu Dec 02 2021 09:00:00 GMT+0000') , endDate: new Date('Sun Dec 12 2021 23:59:59 GMT+0000')} as BookingProcess;
+    fixture.detectChanges();
+    const caption = fixture.debugElement.nativeElement.querySelector('.govuk-caption-l');
+    expect(caption.textContent).toContain('Work access');
+    const infoMassage = fixture.debugElement.nativeElement.querySelector('h1');
+    expect(infoMassage.textContent).toContain('Check your new booking');
+    const location = fixture.debugElement.nativeElement.querySelector('.govuk-summary-list__row').childNodes[1];
+    expect(location.textContent).toContain('London');
+    const dateInterval = fixture.debugElement.nativeElement.querySelector('.govuk-summary-list__row').nextElementSibling.childNodes[1];
+    expect(dateInterval.textContent).toContain('02 December 2021 to 12 December 2021');
   });
 });
--- src/booking/containers/booking-date/booking-date.component.ts
+++ src/booking/containers/booking-date/booking-date.component.ts
@@ -1,7 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import {  BookingNavigationEvent, BookingProcess} from '../../models';
-import { BookingDateValidationError, BookingNavigationEvent } from '../../models';
-import { BookingDateValidationError, BookingNavigationEvent, NewBooking } from '../../models';
+import { ErrorMessagesModel, GovUiConfigModel } from '@hmcts/rpx-xui-common-lib/lib/gov-ui/models';
+import { BookingDateValidationError, BookingNavigationEvent, BookingProcess } from '../../models';
+import { DateValidators } from '../utils';
+
 import {
   FormBuilder,
   FormControl,
@@ -9,22 +10,11 @@ import {
   Validators,
 } from '@angular/forms';
 import {
+  BookingDateFormErrorMessage,
   BookingDateOption,
   BookingDatePageText,
-  TimeOption,
-  BookingTimePageText,
-  DateFormControl,
-  BookingDateFormErrorMessage,
+  DateFormControl
 } from '../../models/booking-date.enum';
-import { ErrorMessage } from '../../../app/models';
-import { ErrorMessagesModel, GovUiConfigModel } from '@hmcts/rpx-xui-common-lib/lib/gov-ui/models';
-import { DateValidators } from '../utils';
-
-export const ERROR_TEMPLATE: ErrorMessage = {
-  title: 'There is a problem',
-  description: '',
-  fieldId: ''
-};
 
 @Component({
   selector: 'exui-booking-date',
@@ -36,10 +26,9 @@ export class BookingDateComponent implements OnInit {
   @Output() public eventTrigger = new EventEmitter();
   public title: string;
   public caption: string;
-  public readonly dateInterval: DisplayedDateInterval[];
+  public dateInterval: DisplayedDateInterval[];
   public formGroup: FormGroup;
   public submitted = false;
-  public errorMessage: ErrorMessage;
   public configStart: GovUiConfigModel;
   public configEnd: GovUiConfigModel;
   public dateValidationErrors: BookingDateValidationError[];
@@ -80,31 +69,16 @@ export class BookingDateComponent implements OnInit {
       startDate_day: new FormControl(null, null),
       endDate_year: new FormControl(null, null),
       endDate_month: new FormControl(null, null),
-      endDate_day: new FormControl(null, null),
-      bookingDateType: new FormControl(null, Validators.required),
+      endDate_day: new FormControl(null, null)
     });
-
     this.formGroup.get('dateOption').patchValue(this.bookingProcess.selectedDateOption);
     if (typeof this.bookingProcess.selectedDateOption === 'number') {
       this.dateInterval[this.bookingProcess.selectedDateOption].checked = true;
     }
-    this.errorMessage = ERROR_TEMPLATE;
     this.setValidators();
   }
 
-  public onEventTrigger() {
-    const {startDate, endDate} = this.getStartEndDate(this.formGroup.get('dateOption').value);
-
-    if (startDate && endDate) {
-      this.bookingProcess.startDate = startDate;
-      this.bookingProcess.endDate = endDate;
-      this.eventTrigger.emit(BookingNavigationEvent.CONFIRMBOOKINGDATESUBMIT);
-    } else {
-      this.errorMessage.description = 'Select an option to book your time at the location';
-      this.errorMessage.fieldId = 'dateOption';
-      this.submitted = true;
-    }
-  private setValidators(): void {
+  public setValidators(): void {
     const dayValidator = DateValidators.dayValidator();
     const monthValidator = DateValidators.monthValidator();
     const yearValidator = DateValidators.yearValidator();
@@ -190,55 +164,45 @@ export class BookingDateComponent implements OnInit {
   }
 
   public onSelectOption(selectedOption): void {
-    if (selectedOption !== TimeOption.DATERANGE ) {
+    if (selectedOption !== BookingDateOption.DATERANGE) {
       this.resetValidationErrorMessages();
       this.formGroup.setErrors(null);
    }
-  }
-  public onSubmit(): void {
-    if (!this.validateForm()) {
-      // Scroll to error summary
-      window.scrollTo({ top: 0, left: 0 });
-    } else {
-      this.eventTrigger.emit(BookingNavigationEvent.CONFIRMBOOKINGDATESUBMIT);
-    }
   }
 
   public dateOptionOnChange(optionIndex: number): void {
     this.bookingProcess.selectedDateOption = optionIndex;
   }
 
-  public getStartEndDate(bookingDateOption: number): {startDate: Date, endDate: Date} {
+  public getStartEndDate(bookingDateOption: BookingDateOption): {startDate: Date, endDate: Date} {
     let startDate;
     let endDate;
     switch (bookingDateOption) {
-      case 0:
+      case BookingDateOption.TODAY:
         startDate = new Date();
         const now = new Date();
         endDate = new Date(now.setUTCHours(23, 59, 59, 999));
         break;
-
-      case 1:
-          startDate = new Date();
-          const sunday = new Date();
-          const sundayUTC = sunday.setTime(sunday.getTime() - ((sunday.getDay() ? sunday.getDay() : 7) - 7) * 24 * 60 * 60 * 1000);
-          const sundayMignightUTC = new Date(sundayUTC).setUTCHours(23, 59, 59, 999);
-          endDate = new Date(sundayMignightUTC);
-          break;
-
-      case 2:
-          startDate = new Date(
-                        this.formGroup.get('startDate_year').value,
-                        this.formGroup.get('startDate_month').value - 1,
-                        this.formGroup.get('startDate_day').value
-                      );
-          const endDateMidnight = new Date(
-                                    this.formGroup.get('endDate_year').value,
-                                    this.formGroup.get('endDate_month').value - 1,
-                                    this.formGroup.get('endDate_day').value
-                                  ).setUTCHours(23, 59, 59, 999);
-          endDate = new Date(endDateMidnight);
-          break;
+      case BookingDateOption.WEEK:
+        startDate = new Date();
+        const sunday = new Date();
+        const sundayUTC = sunday.setTime(sunday.getTime() - ((sunday.getDay() ? sunday.getDay() : 7) - 7) * 24 * 60 * 60 * 1000);
+        const sundayMignightUTC = new Date(sundayUTC).setUTCHours(23, 59, 59, 999);
+        endDate = new Date(sundayMignightUTC);
+        break;
+      case BookingDateOption.DATERANGE:
+        startDate = new Date(
+                      this.formGroup.get(DateFormControl.BOOKING_START_YEAR).value,
+                      this.formGroup.get(DateFormControl.BOOKING_START_MONTH).value - 1,
+                      this.formGroup.get(DateFormControl.BOOKING_START_DAY).value
+                    );
+        const endDateMidnight = new Date(
+                                  this.formGroup.get(DateFormControl.BOOKING_END_YEAR).value,
+                                  this.formGroup.get(DateFormControl.BOOKING_END_MONTH).value - 1,
+                                  this.formGroup.get(DateFormControl.BOOKING_END_DAY).value
+                                ).setUTCHours(23, 59, 59, 999);
+        endDate = new Date(endDateMidnight);
+        break;
       default:
         break;
     }
@@ -248,8 +212,27 @@ export class BookingDateComponent implements OnInit {
       endDate
     };
   }
-}
 
+  public onSubmit(): void {
+    if (!this.validateForm()) {
+      // Scroll to error summary
+      window.scrollTo({ top: 0, left: 0 });
+    } else {
+      this.onEventTrigger();
+    }
+  }
+
+  public onEventTrigger() {
+    const {startDate, endDate} = this.getStartEndDate(this.formGroup.get('dateOption').value);
+    if (startDate && endDate) {
+      this.bookingProcess.startDate = startDate;
+      this.bookingProcess.endDate = endDate;
+      this.eventTrigger.emit(BookingNavigationEvent.CONFIRMBOOKINGDATESUBMIT);
+    } else {
+      this.submitted = true;
+    }
+  }
+}
 
 export interface DisplayedDateInterval {
   date: BookingDateOption;

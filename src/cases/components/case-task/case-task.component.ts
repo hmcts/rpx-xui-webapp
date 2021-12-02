7a9ed45364f78f15cad56c3a69964604f7d2a858
Kuda Nyamainashe <35928491+kuda1992@users.noreply.github.com>
2021-12-02 15:39:05 +0000

    revert formattting

--- src/cases/components/case-task/case-task.component.ts
+++ src/cases/components/case-task/case-task.component.ts
@@ -1,16 +1,16 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertService } from '@hmcts/ccd-case-ui-toolkit';
+import { InfoMessage } from '../../../work-allocation-2/enums';
 
 import { AppUtils } from '../../../app/app-utils';
 import { UserInfo, UserRole } from '../../../app/models';
 import { SessionStorageService } from '../../../app/services';
-import { InfoMessage } from '../../../work-allocation-2/enums';
+import { replaceAll } from '../../../cases/utils/utils';
 import { Caseworker } from '../../../work-allocation-2/models/dtos';
 import { Task, TaskPermission } from '../../../work-allocation-2/models/tasks';
 import { WorkAllocationTaskService } from '../../../work-allocation-2/services';
 import { getAssigneeName, handleTasksFatalErrors, REDIRECTS } from '../../../work-allocation-2/utils';
-import { replaceAll } from '../../utils/utils';
 
 @Component({
   selector: 'exui-case-task',
@@ -26,13 +26,8 @@ export class CaseTaskComponent implements OnInit {
     CaseTaskComponent.CASE_ID_VARIABLE,
     CaseTaskComponent.TASK_ID_VARIABLE
   ];
-  public manageOptions: { id: string, text: string }[];
+  public manageOptions: {id: string, text: string }[];
   public isUserJudicial: boolean;
-  @Input() public caseworkers: Caseworker[] = [];
-  /**
-   * Emit an event to refresh tasks
-   */
-  @Output() public taskRefreshRequired: EventEmitter<void> = new EventEmitter();
   private pTask: Task;
 
   constructor(private readonly alertService: AlertService,
@@ -45,15 +40,23 @@ export class CaseTaskComponent implements OnInit {
     return this.pTask;
   }
 
+  public get returnUrl(): string {
+    return this.router ? this.router.url : `case-details/${this.task.case_id}/tasks`;
+  }
+
   @Input()
   public set task(value: Task) {
     value.description = CaseTaskComponent.replaceVariablesWithRealValues(value);
     this.pTask = value;
   }
 
-  public get returnUrl(): string {
-    return this.router ? this.router.url : `case-details/${this.task.case_id}/tasks`;
-  }
+  @Input() public caseworkers: Caseworker[] = [];
+
+  /**
+   * Emit an event to refresh tasks
+   */
+   @Output() public taskRefreshRequired: EventEmitter<void>
+   = new EventEmitter();
 
   public static replaceVariablesWithRealValues(task: Task): string {
     if (!task.description) {
@@ -93,7 +96,7 @@ export class CaseTaskComponent implements OnInit {
   public onActionHandler(task: Task, option: any): void {
     if (option.id === 'claim') {
       this.taskService.claimTask(task.id).subscribe(() => {
-        this.alertService.success(InfoMessage.ASSIGNED_TASK_AVAILABLE_IN_MY_TASKS);
+        this.alertService.success(InfoMessage.ASSIGNED_TASK_AVAILABLE_IN_MY_TASKS)
         this.taskRefreshRequired.emit();
       }, error => {
         this.claimTaskErrors(error.status);
@@ -106,15 +109,15 @@ export class CaseTaskComponent implements OnInit {
       showAssigneeColumn: true
     };
     const actionUrl = `/work/${task.id}/${option.id}`;
-    this.router.navigate([actionUrl], { state });
+    this.router.navigate([actionUrl], {state});
   }
 
   /**
    * Navigate the User to the correct error page, or throw an on page warning
    * that the Task is no longer available.
    */
-  public claimTaskErrors(status: number): void {
-    const REDIRECT_404 = [{ status: 404, redirectTo: REDIRECTS.ServiceDown }];
+   public claimTaskErrors(status: number): void {
+    const REDIRECT_404 = [{status: 404, redirectTo: REDIRECTS.ServiceDown}];
     const handledStatus = handleTasksFatalErrors(status, this.router, REDIRECT_404);
     if (handledStatus > 0) {
       this.alertService.warning(InfoMessage.TASK_NO_LONGER_AVAILABLE);
@@ -124,32 +127,32 @@ export class CaseTaskComponent implements OnInit {
     }
   }
 
-  public getManageOptions(task: Task): { id: string, text: string } [] {
+  public getManageOptions(task: Task): {id: string, text: string} [] {
     const permissions = task && task.permissions && task.permissions.values ? task.permissions.values : [];
     if (!task.assignee) {
       if (permissions.length === 0 || (permissions.length === 1 && permissions.includes(TaskPermission.MANAGE))) {
         return [];
       } else {
-        return [{ id: 'claim', text: 'Assign to me' }];
+        return [{id: 'claim', text: 'Assign to me'}];
       }
     }
 
     if (this.isTaskAssignedToCurrentUser(task)) {
       return [
-        { id: 'reassign', text: 'Reassign task' },
-        { id: 'unclaim', text: 'Unassign task' }
+        {id: 'reassign', text: 'Reassign task'},
+        {id: 'unclaim', text: 'Unassign task'}
       ];
     } else {
       if (permissions.includes(TaskPermission.EXECUTE) && permissions.includes(TaskPermission.MANAGE)) {
         return [
-          { id: 'claim', text: 'Assign to me' },
-          { id: 'reassign', text: 'Reassign task' },
-          { id: 'unclaim', text: 'Unassign task' }
+          {id: 'claim', text: 'Assign to me'},
+          {id: 'reassign', text: 'Reassign task'},
+          {id: 'unclaim', text: 'Unassign task'}
         ];
       } else if (permissions.includes(TaskPermission.MANAGE)) {
         return [
-          { id: 'reassign', text: 'Reassign task' },
-          { id: 'unclaim', text: 'Unassign task' }
+          {id: 'reassign', text: 'Reassign task'},
+          {id: 'unclaim', text: 'Unassign task'}
         ];
       } else {
         return [];
@@ -157,7 +160,7 @@ export class CaseTaskComponent implements OnInit {
     }
   }
 
-  public toDate(value: string | number | Date): Date {
+    public toDate(value: string | number | Date): Date {
     if (value) {
       const d = new Date(value);
       return isNaN(d.getTime()) ? null : d;

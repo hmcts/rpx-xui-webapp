35674bd86dbd1c6338737e2a4ceb75328e2fce22
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2021-12-03 15:01:42 +0000

    revert route name

--- src/hearings/hearings.routes.ts
+++ src/hearings/hearings.routes.ts
@@ -65,7 +65,7 @@ export const ROUTES: Routes = [
         }
       },
       {
-        path: 'hearing-venue',
+        path: 'location-search',
         resolve: { hearingPriorities: PriorityResolver },
         component: LocationSearchContainerComponent,
         canActivate: [HealthCheckGuard],

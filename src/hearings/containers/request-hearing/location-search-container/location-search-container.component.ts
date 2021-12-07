d8b27b463e971227a108b740fc1b0f12c4c13130
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2021-12-07 10:12:33 +0000

    formcontrolloopproblem

--- src/hearings/containers/request-hearing/location-search-container/location-search-container.component.ts
+++ src/hearings/containers/request-hearing/location-search-container/location-search-container.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Store, select } from '@ngrx/store';
 import { LocationByEPIMSModel } from '@hmcts/rpx-xui-common-lib/lib/models/location.model';
+import { select, Store } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import * as fromHearingStore from '../../../../hearings/store';

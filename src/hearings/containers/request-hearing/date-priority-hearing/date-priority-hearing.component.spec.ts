8172e30ce9c433b323f0581bf456573476619ab7
Kartheek-S <92437222+Kartheek-S@users.noreply.github.com>
2021-12-08 14:59:10 +0000

    fixed tests

--- src/hearings/containers/request-hearing/date-priority-hearing/date-priority-hearing.component.spec.ts
+++ src/hearings/containers/request-hearing/date-priority-hearing/date-priority-hearing.component.spec.ts
@@ -12,7 +12,7 @@ import { HearingDatePriorityEnum } from '../../../../hearings/models/hearings.en
 import { PartyUnavailabilityRange } from '../../../../hearings/models/partyUnavilabilityRange.model';
 import { RefDataModel } from '../../../../hearings/models/refData.model';
 import { HearingsService } from '../../../../hearings/services/hearings.service';
-import { ValidatorsService } from '../../../utils/validators.utils';
+import { ValidatorsUtils } from '../../../utils/validators.utils';
 import { DatePriorityHearingComponent } from './date-priority-hearing.component';
 
 @Component({
@@ -47,7 +47,7 @@ describe('DatePriorityHearingComponent', () => {
       providers: [
         provideMockStore(),
         HearingsService,
-        ValidatorsService,
+        ValidatorsUtils,
         {
           provide: ActivatedRoute,
           useValue: {

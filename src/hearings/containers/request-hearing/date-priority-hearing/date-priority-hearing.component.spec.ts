ca1fa72cb785e0235dc3e320176c20325811568b
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2021-12-09 15:15:52 +0000

    parties title

--- src/hearings/containers/request-hearing/date-priority-hearing/date-priority-hearing.component.spec.ts
+++ src/hearings/containers/request-hearing/date-priority-hearing/date-priority-hearing.component.spec.ts
@@ -18,7 +18,7 @@ class MockHearingPartiesComponent {
   @Input() public error: ErrorMessage;
 }
 
-describe('DatePriorityHearingComponent', () => {
+fdescribe('DatePriorityHearingComponent', () => {
   let component: DatePriorityHearingComponent;
   let fixture: ComponentFixture<DatePriorityHearingComponent>;
   let router: Router;

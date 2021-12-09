52385c8d9b9fd69903bef9a48fab6c888a4a798a
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2021-12-09 16:16:25 +0000

    redux prep

--- src/hearings/containers/request-hearing/hearing-requirements/hearing-requirements.component.ts
+++ src/hearings/containers/request-hearing/hearing-requirements/hearing-requirements.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import * as fromHearingStore from '../../../../hearings/store';
 @Component({
   selector: 'xui-hearing-requirements',
   templateUrl: './hearing-requirements.component.html',
   styleUrls: ['./hearing-requirements.component.scss']
 })
 export class HearingRequirementsComponent implements OnInit {
+  constructor(private readonly route: ActivatedRoute,
+    private readonly hearingStore: Store<fromHearingStore.State>,
+    fb: FormBuilder) {
+      this.hearingRequirmentForm = fb.group({
+      'stage-option': [null],
+      });
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    public ngOnInit() {
+    this.hearingStoreSub = this.hearingStore.pipe(select(fromHearingStore.getHearingValuesModel)).subscribe(
+    hearingValueModel => {
+    this.hearingType = hearingValueModel && hearingValueModel.hearingType ? hearingValueModel.hearingType :  this.hearingType;
+    }
+    );
+    }
 
 }
+
+
+// ServiceHearingValuesModel

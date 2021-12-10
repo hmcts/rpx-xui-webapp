433e021e7e47091c0538e42654184adbba3b6557
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2021-12-10 09:33:54 +0000

    grouping

--- src/hearings/containers/request-hearing/hearing-requirements/hearing-requirements.component.ts
+++ src/hearings/containers/request-hearing/hearing-requirements/hearing-requirements.component.ts
@@ -2,9 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { select, Store } from '@ngrx/store';
+import { CaseFlagGroup } from 'api/hearings/models/case-flag-group.model';
 import { ServiceHearingValuesModel } from 'api/hearings/models/serviceHearingValues.model';
+import groupBy from 'lodash/groupBy';
 import { Subscription } from 'rxjs';
+import { PartyFlagsModel } from '../../../../../api/hearings/models/partyFlags.model';
 import * as fromHearingStore from '../../../../hearings/store';
+// import { CaseFlagGroup } from '../../../../../api/hearings/models/case-flag-group.model';
+
+const groupBy = <T, K extends keyof any>(list: T[], getKey: (item: T) => K) =>
+  list.reduce((previous, currentItem) => {
+    const group = getKey(currentItem);
+    if (!previous[group]) { previous[group] = []; }
+    previous[group].push(currentItem);
+    return previous;
+  }, {} as Record<K, T[]>);
 @Component({
   selector: 'xui-hearing-requirements',
   templateUrl: './hearing-requirements.component.html',
@@ -14,6 +26,8 @@ export class HearingRequirementsComponent implements OnInit {
   public hearingRequirmentForm: FormGroup;
   public hearingStoreSub: Subscription;
   public hearingValueModel: ServiceHearingValuesModel;
+  public caseFlags: CaseFlagGroup[];
+  // public caseFlags: Record<string, PartyFlagsModel[]>;
 
   constructor(private readonly route: ActivatedRoute,
               private readonly hearingStore: Store<fromHearingStore.State>,
@@ -23,10 +37,30 @@ export class HearingRequirementsComponent implements OnInit {
     });
   }
 
+  public convertMapToArray(caseFlags: Record<string, PartyFlagsModel[]>): CaseFlagGroup[] {
+    const caseFlagGroups = [];
+    // tslint:disable-next-line: forin
+    for (const caseFlag in caseFlags) {
+        caseFlagGroups.push(
+          {
+            name: caseFlag,
+            partyFlags: caseFlags[caseFlag]
+          } as CaseFlagGroup);
+    }
+    return caseFlagGroups;
+  }
+
   public ngOnInit() {
     this.hearingStoreSub = this.hearingStore.pipe(select(fromHearingStore.getHearingValuesModel)).subscribe(
     hearingValueModel => {
       this.hearingValueModel = hearingValueModel;
+      if (this.hearingValueModel && this.hearingValueModel.caseFlags) {
+        // this.caseFlags = this.groupBy(this.hearingValueModel.caseFlags.flags);
+        const caseFlags = groupBy(this.hearingValueModel.caseFlags.flags, i => i.partyName);
+        this.caseFlags = this.convertMapToArray(caseFlags);
+      }
+
+      console.log(this.caseFlags);
     });
   }
 }

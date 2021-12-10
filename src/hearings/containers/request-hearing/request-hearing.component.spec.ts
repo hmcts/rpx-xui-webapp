e353c664e3c5bb7ac90a6e762a573ed1a69809e7
Raymond Liang <ray.liang@atos.net>
2021-12-10 17:46:36 +0000

    EUI-4858-page-flow: fix tests

--- src/hearings/containers/request-hearing/request-hearing.component.spec.ts
+++ src/hearings/containers/request-hearing/request-hearing.component.spec.ts
@@ -1,12 +1,12 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { Store } from '@ngrx/store';
-import { provideMockStore } from '@ngrx/store/testing';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {Store} from '@ngrx/store';
+import {provideMockStore} from '@ngrx/store/testing';
 import {of} from 'rxjs';
 import {ACTION} from '../../models/hearings.enum';
 import {HearingsService} from '../../services/hearings.service';
-import { RequestHearingComponent } from './request-hearing.component';
+import {RequestHearingComponent} from './request-hearing.component';
 
 describe('RequestHearingComponent', () => {
   let component: RequestHearingComponent;
@@ -17,14 +17,13 @@ describe('RequestHearingComponent', () => {
   hearingsService.navigateAction$ = of(ACTION.CONTINUE);
 
   beforeEach(() => {
-    // hearingsService = createSpyObj<any>('hearingsService', ['getAllHearings', 'loadHearingValues', 'onFormSubmission']);
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [RequestHearingComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
         provideMockStore(),
-        { provide: HearingsService, useValue: hearingsService }
+        {provide: HearingsService, useValue: hearingsService}
       ]
     })
       .compileComponents();
@@ -40,8 +39,15 @@ describe('RequestHearingComponent', () => {
   });
 
   it('should check continue method', () => {
+    spyOn(hearingsService, 'navigateAction');
+    component.onBack();
+    expect(hearingsService.navigateAction).toHaveBeenCalledWith(ACTION.BACK);
+  });
+
+  it('should check continue method', () => {
+    spyOn(hearingsService, 'navigateAction');
     component.onContinue();
-    expect(hearingsService.navigateAction).toHaveBeenCalled();
+    expect(hearingsService.navigateAction).toHaveBeenCalledWith(ACTION.CONTINUE);
   });
 
   afterEach(() => {

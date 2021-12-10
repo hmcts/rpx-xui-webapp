cae7a7a862082ca921a49dadc4b000681f9e3962
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2021-12-10 15:28:30 +0000

    workAllocationUrl

--- src/hearings/containers/request-hearing/hearing-requirements/hearing-requirements.component.spec.ts
+++ src/hearings/containers/request-hearing/hearing-requirements/hearing-requirements.component.spec.ts
@@ -1,14 +1,153 @@
+import { Component, Input } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ErrorMessage } from '@hmcts/ccd-case-ui-toolkit/dist/shared/domain';
+import { provideMockStore } from '@ngrx/store/testing';
+import { ServiceHearingValuesModel } from '../../../../../api/hearings/models/serviceHearingValues.model';
 import { HearingRequirementsComponent } from './hearing-requirements.component';
+@Component({
+  selector: 'exui-hearing-parties-title',
+  template: '',
+})
+class MockHearingPartiesComponent {
+  @Input() public error: ErrorMessage;
+}
 
-describe('HearingRequirementsComponent', () => {
+fdescribe('HearingRequirementsComponent', () => {
   let component: HearingRequirementsComponent;
   let fixture: ComponentFixture<HearingRequirementsComponent>;
 
+  const hearingValueModel: ServiceHearingValuesModel = {
+    autoListFlag: false,
+    hearingType: 'Final',
+    caseType: 'Personal Independence Payment',
+    caseSubTypes: [
+      'Conditions of Entitlement',
+      'Good cause',
+      'Rate of Assessment / Payability Issues - complex'
+    ],
+    hearingWindow: {
+      range: {
+        start: '2021-11-23T09:00:00.000+0000',
+        end: '2021-11-30T09:00:00.000+0000'
+      },
+      firstDateTimeMustBe: ''
+    },
+    duration: 45,
+    hearingPriorityType: 'Standard',
+    numberOfPhysicalAttendees: 2,
+    hearingInWelshFlag: false,
+    hearingLocations: [
+      '196538'
+    ],
+    facilitiesRequired: [],
+    listingComments: '',
+    hearingRequester: '',
+    privateHearingRequiredFlag: false,
+    leadJudgeContractType: '',
+    judiciary: {
+      roleType: [
+        ''
+      ],
+      authorisationTypes: [
+        ''
+      ],
+      authorisationSubType: [
+        ''
+      ],
+      panelComposition: [
+        {
+          memberType: '',
+          count: 1
+        }
+      ],
+      judiciaryPreferences: [
+        {
+          memberID: '',
+          memberType: '',
+          requirementType: 'EXCLUDE'
+        }
+      ],
+      judiciarySpecialisms: [
+        ''
+      ]
+    },
+    hearingIsLinkedFlag: false,
+    parties: [
+      {
+        partyName: 'Jane and Smith',
+        partyChannel: '',
+        unavailability: [
+          {
+            start: '2021-12-10T09:00:00.000+0000',
+            end: '2021-12-31T09:00:00.000+0000'
+          }
+        ]
+      },
+      {
+        partyName: 'DWP',
+        partyChannel: '',
+        unavailability: [
+          {
+            start: '2021-12-20T09:00:00.000+0000',
+            end: '2021-12-31T09:00:00.000+0000'
+          }
+        ]
+      }
+    ],
+    caseFlags: {
+      flags: [
+        {
+          partyName: 'Jane and Smith',
+          flagId: 'Language Interpreter',
+          flagDescription: 'Spanish interpreter required',
+          flagStatus: 'ACTIVE'
+        },
+        {
+          partyName: 'Jane and Smith',
+          flagId: 'Sign language interpreter',
+          flagDescription: 'Sign language interpreter required',
+          flagStatus: 'ACTIVE'
+        },
+        {
+          partyName: 'Jane and Smith',
+          flagId: 'Reasonable adjustment',
+          flagDescription: 'Hearing loop required',
+          flagStatus: 'ACTIVE'
+        },
+        {
+          partyName: 'DWP',
+          flagId: 'case flag 1',
+          flagDescription: 'case flag 1 description',
+          flagStatus: 'ACTIVE'
+        }
+      ],
+      flagAmendURL: '/'
+    },
+  } as ServiceHearingValuesModel;
+
+  const initialState = {
+    hearings: {
+      hearingList: {
+        caseHearingMainModel: [
+          {
+            hmctsServiceID: 'TEST'
+          }
+        ]
+      },
+        hearingValues:  {
+          serviceHearingValuesModel: hearingValueModel,
+          lastError: null,
+      },
+    }
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HearingRequirementsComponent ]
+      imports: [],
+      declarations: [ HearingRequirementsComponent, MockHearingPartiesComponent ],
+      providers: [
+        provideMockStore({initialState}),
+      ],
     })
     .compileComponents();
   }));
@@ -16,10 +155,43 @@ describe('HearingRequirementsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HearingRequirementsComponent);
     component = fixture.componentInstance;
+    spyOn(component, 'convertMapToArray').and.callThrough();
+    spyOn(component, 'assignHearingValue').and.callThrough();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set option collection', async (done) => {
+    expect(component).toBeDefined();
+    done();
+    expect(component.hearingValueModel).toEqual(hearingValueModel);
+  });
+
+  it('should call convertMapToArray during onint call', () => {
+    component.ngOnInit();
+    expect(component).toBeDefined();
+    expect(component.convertMapToArray).toHaveBeenCalled();
+  });
+
+  it('should call caseFlags count to be 4', async (done) => {
+    component.ngOnInit();
+    done();
+    expect(component.assignHearingValue).toHaveBeenCalled();
+  });
+
+  it('should assign values to caseFlags once convertMapToArray is called', () => {
+    component.assignHearingValue(hearingValueModel);
+    expect(component.caseFlags.length).toEqual(2);
+  });
+
+  it('should call unsubscribe', () => {
+    component.ngOnInit();
+    spyOn(component.hearingStoreSub, 'unsubscribe');
+    fixture.detectChanges();
+    component.ngOnDestroy();
+    expect(component.hearingStoreSub.unsubscribe).toHaveBeenCalled();
   });
 });
--- src/hearings/containers/request-hearing/hearing-requirements/hearing-requirements.component.ts
+++ src/hearings/containers/request-hearing/hearing-requirements/hearing-requirements.component.ts
@@ -1,51 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { select, Store } from '@ngrx/store';
 import { CaseFlagGroup } from 'api/hearings/models/case-flag-group.model';
 import { ServiceHearingValuesModel } from 'api/hearings/models/serviceHearingValues.model';
-import groupBy from 'lodash/groupBy';
 import { Subscription } from 'rxjs';
+import * as _ from 'underscore';
 import { PartyFlagsModel } from '../../../../../api/hearings/models/partyFlags.model';
 import * as fromHearingStore from '../../../../hearings/store';
-// import { CaseFlagGroup } from '../../../../../api/hearings/models/case-flag-group.model';
 
-const groupBy = <T, K extends keyof any>(list: T[], getKey: (item: T) => K) =>
-  list.reduce((previous, currentItem) => {
-    const group = getKey(currentItem);
-    if (!previous[group]) { previous[group] = []; }
-    previous[group].push(currentItem);
-    return previous;
-  }, {} as Record<K, T[]>);
 @Component({
-  selector: 'xui-hearing-requirements',
+  selector: 'exui-hearing-requirements',
   templateUrl: './hearing-requirements.component.html',
   styleUrls: ['./hearing-requirements.component.scss']
 })
-export class HearingRequirementsComponent implements OnInit {
+export class HearingRequirementsComponent implements OnInit, OnDestroy {
   public hearingRequirmentForm: FormGroup;
   public hearingStoreSub: Subscription;
   public hearingValueModel: ServiceHearingValuesModel;
   public caseFlags: CaseFlagGroup[];
-  // public caseFlags: Record<string, PartyFlagsModel[]>;
 
-  constructor(private readonly route: ActivatedRoute,
-              private readonly hearingStore: Store<fromHearingStore.State>,
-              fb: FormBuilder) {
-    this.hearingRequirmentForm = fb.group({
-    'stage-option': [null],
-    });
+  constructor(private readonly hearingStore: Store<fromHearingStore.State>) {
   }
 
   public convertMapToArray(caseFlags: Record<string, PartyFlagsModel[]>): CaseFlagGroup[] {
     const caseFlagGroups = [];
-    // tslint:disable-next-line: forin
     for (const caseFlag in caseFlags) {
+      if (caseFlags.hasOwnProperty(caseFlag)) {
         caseFlagGroups.push(
           {
             name: caseFlag,
             partyFlags: caseFlags[caseFlag]
           } as CaseFlagGroup);
+      }
     }
     return caseFlagGroups;
   }
@@ -53,43 +39,19 @@ export class HearingRequirementsComponent implements OnInit {
   public ngOnInit() {
     this.hearingStoreSub = this.hearingStore.pipe(select(fromHearingStore.getHearingValuesModel)).subscribe(
     hearingValueModel => {
-      this.hearingValueModel = hearingValueModel;
-      if (this.hearingValueModel && this.hearingValueModel.caseFlags) {
-        // this.caseFlags = this.groupBy(this.hearingValueModel.caseFlags.flags);
-        const caseFlags = groupBy(this.hearingValueModel.caseFlags.flags, i => i.partyName);
-        this.caseFlags = this.convertMapToArray(caseFlags);
-      }
-
-      console.log(this.caseFlags);
+      this.assignHearingValue(hearingValueModel);
     });
   }
-}
-// ServiceHearingValuesModel
 
-// caseFlags: {
-//   flags: [
-//     {
-//       partyName: 'Jane and Smith',
-//       flagId: 'Language Interpreter',
-//       flagDescription: 'Spanish interpreter required',
-//       flagStatus: 'ACTIVE',
-//     },
-//     {
-//       partyName: 'Jane and Smith',
-//       flagId: 'Sign language interpreter',
-//       flagDescription: 'Sign language interpreter required',
-//       flagStatus: 'ACTIVE',
-//     },
-//     {
-//       partyName: 'Jane and Smith',
-//       flagId: 'Reasonable adjustment',
-//       flagDescription: 'Hearing loop required',
-//       flagStatus: 'ACTIVE',
-//     },
-//     {
-//       partyName: 'DWP',
-//       flagId: 'case flag 1',
-//       flagDescription: 'case flag 1 description',
-//       flagStatus: 'ACTIVE',
-//     },
-//   ],
+  public assignHearingValue(hearingValueModel: any) {
+    this.hearingValueModel = hearingValueModel;
+    if (this.hearingValueModel && this.hearingValueModel.caseFlags) {
+      const caseFlags = _.groupBy(this.hearingValueModel.caseFlags.flags, 'partyName');
+      this.caseFlags = this.convertMapToArray(caseFlags);
+    }
+  }
+
+  public ngOnDestroy() {
+    this.hearingStoreSub.unsubscribe();
+  }
+}

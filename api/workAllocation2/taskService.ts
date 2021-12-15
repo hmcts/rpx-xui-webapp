6974aeac2ba7f8cdf5ff1ce94114a4699b36f3a5
Kuda Nyamainashe <35928491+kuda1992@users.noreply.github.com>
2021-12-15 14:24:12 +0000

    node api endpoint

--- api/workAllocation2/taskService.ts
+++ api/workAllocation2/taskService.ts
@@ -11,23 +11,23 @@ const httpMock: AxiosInstance = HttpMock.getInstance();
 const logger: JUILogger = log4jui.getLogger('task-service');
 
 export async function handleTaskGet(path: string, req: EnhancedRequest): Promise<any> {
-  logger.info('getting tasks for', path);
-  const headers = setHeaders(req);
-  const response: AxiosResponse = await http.get(path, { headers });
-  return response.data;
+    logger.info('getting tasks for', path);
+    const headers = setHeaders(req);
+    const response: AxiosResponse = await http.get(path, { headers });
+    return response.data;
 }
 
 export async function handleTaskSearch(path: string, payload: any, req: EnhancedRequest): Promise<any> {
   logger.info('search task for', payload);
   const headers = setHeaders(req);
-  return await http.post(path, payload, { headers });
+  return await http.post(path, payload, {headers});
 }
 
 export async function handleTaskPost(path: string, payload: any, req: EnhancedRequest): Promise<any> {
-  logger.info('posting tasks for', path);
+    logger.info('posting tasks for', path);
   // Return the whole response, not just the data, so we can
-  // see what the status of the response is.
-  return await handlePost(path, payload, req);
+    // see what the status of the response is.
+    return await handlePost(path, payload, req);
 }
 
 export async function handleGetTasksByCaseId(path: string, req: EnhancedRequest): Promise<any> {

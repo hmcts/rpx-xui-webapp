3e6a78ae9ad74099d96c4763e3d71025761c9a12
Kartheek-S <92437222+Kartheek-S@users.noreply.github.com>
2021-12-17 16:39:45 +0000

    case flag reference data (#1558)
    * case flag reference data
    
    * model update
    
    * updated route

--- api/hearings/routes.ts
+++ api/hearings/routes.ts
@@ -1,11 +1,12 @@
 import * as express from 'express';
 import authInterceptor from '../lib/middleware/auth';
-import {getHearings, getRefData, loadServiceHearingValues, submitHearingRequest} from './index';
+import {getCaseFlagRefData, getHearings, getRefData, loadServiceHearingValues, submitHearingRequest} from './index';
 
 export const router = express.Router({mergeParams: true});
 
 router.use(authInterceptor);
 router.get('/getHearings', getHearings);
+router.get('/getCaseFlagRefData', getCaseFlagRefData);
 router.get('/getRefData', getRefData);
 router.post('/loadServiceHearingValues', loadServiceHearingValues);
 router.post('/submitHearingRequest', submitHearingRequest);

f1e94d4e67b91cd20a7a865c339951ad26ae9f24
rayliangatos <57181888+rayliangatos@users.noreply.github.com>
2021-12-17 17:23:29 +0000

    Feature/e UI 4689 hearing welsh pf 2 (#1579)
    * EUI-4689-hearing-welsh-pf
    
    * feature/EUI-4689-hearing-welsh-pf

--- src/hearings/containers/request-hearing/hearing-welsh/hearing-welsh.component.ts
+++ src/hearings/containers/request-hearing/hearing-welsh/hearing-welsh.component.ts
@@ -1,25 +1,22 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {Store} from '@ngrx/store';
-import { ErrorMessage } from '../../../../app/models';
+import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {select, Store} from '@ngrx/store';
+import {Subscription} from 'rxjs';
+import {ErrorMessage} from '../../../../app/models';
 import {ACTION} from '../../../models/hearings.enum';
 import {HearingsService} from '../../../services/hearings.service';
 import * as fromHearingStore from '../../../store';
 import {RequestHearingPageFlow} from '../request-hearing.page.flow';
 
-export const NAME_ERROR: ErrorMessage = {
-  title: 'There is a problem',
-  description: 'You must select Yes or No',
-  fieldId: 'welsh-hint',
-};
-
 @Component({
   selector: 'exui-hearing-welsh',
   templateUrl: './hearing-welsh.component.html',
 })
-export class HearingWelshComponent extends RequestHearingPageFlow implements OnInit, OnDestroy {
+export class HearingWelshComponent extends RequestHearingPageFlow implements OnInit, AfterViewInit, OnDestroy {
   public error: ErrorMessage = null;
   public welshForm: FormGroup;
+  public hearingStoreSub: Subscription;
+  public hearingInWelshFlag: boolean = false;
 
   constructor(protected readonly hearingStore: Store<fromHearingStore.State>,
               protected readonly hearingsService: HearingsService,
@@ -29,6 +26,17 @@ export class HearingWelshComponent extends RequestHearingPageFlow implements OnI
 
   public ngOnInit(): void {
     this.initForm();
+    this.hearingStoreSub = this.hearingStore.pipe(select(fromHearingStore.getHearingRequest)).subscribe(
+      hearingRequestModel => {
+        this.hearingInWelshFlag = hearingRequestModel && hearingRequestModel.hearingRequestMainModel
+        && hearingRequestModel.hearingRequestMainModel.hearingDetails ?
+          hearingRequestModel.hearingRequestMainModel.hearingDetails.hearingInWelshFlag : false;
+      }
+    );
+  }
+
+  public ngAfterViewInit(): void {
+    this.welshForm.controls['hearingInWelshFlag'].setValue(this.hearingInWelshFlag);
   }
 
   public initForm(): void {
@@ -38,10 +46,26 @@ export class HearingWelshComponent extends RequestHearingPageFlow implements OnI
   }
 
   public executeAction(action: ACTION): void {
+    if (action === ACTION.CONTINUE) {
+      this.prepareHearingRequestData();
+    }
     super.navigateAction(action);
+  }
+
+  public prepareHearingRequestData(): void {
+    this.hearingRequestMainModel = {
+      ...this.hearingRequestMainModel,
+      hearingDetails: {
+        ...this.hearingRequestMainModel.hearingDetails,
+        hearingInWelshFlag: this.welshForm.value['hearingInWelshFlag']
+      }
+    };
   }
 
   public ngOnDestroy(): void {
     super.unsubscribe();
+    if (this.hearingStoreSub) {
+      this.hearingStoreSub.unsubscribe();
+    }
   }
 }

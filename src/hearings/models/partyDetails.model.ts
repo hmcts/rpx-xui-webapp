019cdae497d28ad5046151f19cd906f3ffac85f1
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2022-01-05 12:46:34 +0000

    reverse attendance
    reverse attendance push by accident

--- src/hearings/models/partyDetails.model.ts
+++ src/hearings/models/partyDetails.model.ts
@@ -1,15 +1,12 @@
-
-import { PartyType } from './hearings.enum';
-import { IndividualDetailsModel } from './individualDetails.model';
-import { OrganisationDetailsModel } from './organisationDetails.model';
-import { UnavailabilityDOWModel } from './unavailabilityDOW.model';
-import { UnavailabilityRangeModel } from './unavailabilityRange.model';
+import {PartyType} from './hearings.enum';
+import {IndividualDetailsModel} from './individualDetails.model';
+import {OrganisationDetailsModel} from './organisationDetails.model';
+import {UnavailabilityDOWModel} from './unavailabilityDOW.model';
+import {UnavailabilityRangeModel} from './unavailabilityRange.model';
 
 export interface PartyDetailsModel {
   partyID: string;
   partyType: PartyType;
-  partyName: string;
-  partyChannel: string;
   partyRole?: string;
   individualDetails?: IndividualDetailsModel;
   organisationDetails?: OrganisationDetailsModel;

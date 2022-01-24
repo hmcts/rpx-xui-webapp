6cc4e98db4a7eb3c069efabeaada40a9b3311d48
Kartheek-S <92437222+Kartheek-S@users.noreply.github.com>
2022-01-24 19:31:46 +0000

    EUI-4794 hearing instructions (#1664)
    * hearing instructions
    
    * yarn fix
    
    * yarn fix
    
    * remove fdescribe
    
    Co-authored-by: Raymond Liang <ray.liang@atos.net>

--- src/hearings/containers/request-hearing/hearing-additional-instructions/hearing-additional-instructions.component.ts
+++ src/hearings/containers/request-hearing/hearing-additional-instructions/hearing-additional-instructions.component.ts
@@ -1,33 +1,58 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Store} from '@ngrx/store';
-import {ACTION} from '../../../models/hearings.enum';
-import {HearingsService} from '../../../services/hearings.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { ACTION, HearingInstructionsEnum } from '../../../models/hearings.enum';
+import { HearingsService } from '../../../services/hearings.service';
 import * as fromHearingStore from '../../../store';
-import {RequestHearingPageFlow} from '../request-hearing.page.flow';
+import { RequestHearingPageFlow } from '../request-hearing.page.flow';
 
 @Component({
   selector: 'exui-hearing-additional-instructions',
   templateUrl: './hearing-additional-instructions.component.html',
 })
 export class HearingAdditionalInstructionsComponent extends RequestHearingPageFlow implements OnInit, OnDestroy {
+  public instructionsForm: FormGroup;
+  public instructionLength: number = HearingInstructionsEnum.InstructionLength;
 
   constructor(protected readonly hearingStore: Store<fromHearingStore.State>,
-              protected readonly hearingsService: HearingsService) {
+              protected readonly hearingsService: HearingsService,
+              private readonly formBuilder: FormBuilder) {
     super(hearingStore, hearingsService);
   }
 
   public ngOnInit(): void {
+    this.initForm();
+  }
+
+  public initForm(): void {
+    this.instructionsForm = this.formBuilder.group({
+      instructions: [this.hearingRequestMainModel.hearingDetails.listingComments]
+    });
   }
 
   public executeAction(action: ACTION): void {
-    if (this.isFormValid()) {
+    if (action === ACTION.CONTINUE) {
+      if (this.isFormValid()) {
+        this.prepareHearingRequestData();
+        super.navigateAction(action);
+      }
+    } else if (action === ACTION.BACK) {
       super.navigateAction(action);
     }
   }
 
+  public prepareHearingRequestData(): void {
+    this.hearingRequestMainModel = {
+      ...this.hearingRequestMainModel,
+      hearingDetails: {
+        ...this.hearingRequestMainModel.hearingDetails,
+        listingComments: this.instructionsForm.value.instructions.slice(0, this.instructionLength)
+      }
+    };
+  }
+
   public isFormValid(): boolean {
-    // TODO verify if form group is valid
-    return true;
+    return this.instructionsForm.valid;
   }
 
   public ngOnDestroy(): void {

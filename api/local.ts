bdf7477b3a6662d04d9c509b588a7d040ce4954a
Pardeep Singh Basi <paddybasi@gmail.com>
2022-01-26 22:03:49 +0000

    fixed TypeError and better handling of terminating node layer

--- api/local.ts
+++ api/local.ts
@@ -1,17 +1,26 @@
 /**
  * Local.ts used to run the application locally.
  */
-import { app } from './application'
-import { applicationConfiguration } from './configuration/appConfig'
-import { appInsights } from './lib/appInsights'
-import errorHandler from './lib/error.handler'
+import { app } from './application';
+import { applicationConfiguration } from './configuration/appConfig';
+import { appInsights } from './lib/appInsights';
+import errorHandler from './lib/error.handler';
+import {processErrorInit} from './lib/processError.handler';
+import {terminate} from './lib/terminate';
 
 /**
  * Show the developer the application configuration when they are developing locally.
  */
-console.log(applicationConfiguration())
+console.log(applicationConfiguration());
 
-app.use(appInsights)
-app.use(errorHandler)
+app.use(appInsights);
+app.use(errorHandler);
 
-app.listen(3001, () => console.log('Dev server listening on port 3001!'))
+const server = app.listen(3001, () => console.log('Dev server listening on port 3001!'));
+
+const exitHandler = terminate(server, {
+  coredump: false,
+  timeout: 500,
+});
+
+processErrorInit(exitHandler);

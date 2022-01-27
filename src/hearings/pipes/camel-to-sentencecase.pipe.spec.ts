6e5014516e9a60cc28bed8d1d205dd7becdeb5c3
Philip Whitaker <phillip.whitaker1@hmcts.net>
2022-01-27 16:10:12 +0000

    changed to sentence case after rereading the ticket

--- src/hearings/pipes/camel-to-sentencecase.pipe.spec.ts
+++ src/hearings/pipes/camel-to-sentencecase.pipe.spec.ts
@@ -1,20 +1,20 @@
 import { CamelToSentenceCasePipe } from './camel-to-sentencecase.pipe';
 
 describe('CamelToSentenceCasePipe', () => {
-  let camelToTitleCasePipe: CamelToSentenceCasePipe;
+  let camelToSentenceCasePipe: CamelToSentenceCasePipe;
 
   beforeEach(() => {
-    camelToTitleCasePipe = new CamelToSentenceCasePipe();
+    camelToSentenceCasePipe = new CamelToSentenceCasePipe();
   });
 
   it('should convert lowercase word to sentence case', () => {
-    const result = camelToTitleCasePipe.transform('word');
+    const result = camelToSentenceCasePipe.transform('word');
 
     expect(result).toBe('Word');
   });
 
   it('should convert multiple lowercase words to sentence case', () => {
-    const result = camelToTitleCasePipe.transform('word word word');
+    const result = camelToSentenceCasePipe.transform('word word word');
 
     expect(result).toBe('Word word word');
   });

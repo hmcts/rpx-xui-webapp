f28e7e2d9260b9c19f42421139cbee62cec28901
Raymond Liang <ray.liang@atos.net>
2022-02-03 17:37:49 +0000

    EUI-5008-case-flags-summary

--- src/hearings/converters/case-flag.converter.spec.ts
+++ src/hearings/converters/case-flag.converter.spec.ts
@@ -3,13 +3,13 @@ import {ActivatedRoute} from '@angular/router';
 import {Store} from '@ngrx/store';
 import {provideMockStore} from '@ngrx/store/testing';
 import {cold} from 'jasmine-marbles';
+import {Subscription} from 'rxjs';
 import {caseFlagsRefData, initialState} from '../hearing.test.data';
-import {AbstractConverter} from './abstract.converter';
 import {CaseFlagConverter} from './case-flag.converter';
 
-describe('CaseFlagConverter', () => {
+fdescribe('CaseFlagConverter', () => {
 
-  let converter: AbstractConverter;
+  let caseFlagConverter: CaseFlagConverter;
   let store: Store<any>;
   let router: any;
 
@@ -31,14 +31,20 @@ describe('CaseFlagConverter', () => {
     });
     store = TestBed.get(Store);
     router = TestBed.get(ActivatedRoute);
-    converter = new CaseFlagConverter(store, router);
+    caseFlagConverter = new CaseFlagConverter(store, router);
+    caseFlagConverter.storeSub = new Subscription();
   });
 
   it('should transform case flag', () => {
-    const result$ = converter.transformAnswer();
+    const result$ = caseFlagConverter.transformAnswer();
     const caseFlags = '<strong class="bold">Jane Smith</strong>\n<ul><li>Sign Language Interpreter</li><li>Hearing Loop</li><li>Larger font size</li><li>Reading documents for customer</li><li>Sign Language Interpreter</li><li>Language Interpreter</li></ul><br><strong class="bold">DWP</strong>\n<ul><li>Physical access and facilities</li></ul><br>';
     const expected = cold('(b|)', {b: caseFlags});
     expect(result$).toBeObservable(expected);
   });
 
+  it('should destroy subscription', () => {
+    const unsubSpy = spyOn(caseFlagConverter.storeSub, 'unsubscribe');
+    caseFlagConverter.ngOnDestroy();
+    expect(unsubSpy).toHaveBeenCalled();
+  });
 });

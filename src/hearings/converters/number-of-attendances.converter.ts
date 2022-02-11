5c108a828bbd2d5439a635d7c2af5d65811ef68e
Kuda Nyamainashe <35928491+kuda1992@users.noreply.github.com>
2022-02-11 10:05:37 +0000

    update unit tests

--- src/hearings/converters/number-of-attendances.converter.ts
+++ src/hearings/converters/number-of-attendances.converter.ts
@@ -1,20 +1,11 @@
-import { ActivatedRoute } from '@angular/router';
-import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import * as fromHearingStore from '../store';
-import { AbstractConverter } from './abstract.converter';
+import { AnswerConverter } from './answer.converter';
 
-export class NumberOfAttendancesConverter extends AbstractConverter {
-
-  constructor(
-    protected readonly hearingStore: Store<fromHearingStore.State>,
-    protected readonly route: ActivatedRoute) {
-    super(hearingStore);
-  }
-
-  public transformAnswer(): Observable<string> {
-    return this.hearingState.pipe(
+export class NumberOfAttendancesConverter implements AnswerConverter {
+  public transformAnswer(hearingState$: Observable<fromHearingStore.State>): Observable<string> {
+    return hearingState$.pipe(
       map(state => {
         return state.hearingValues.serviceHearingValuesModel.numberOfPhysicalAttendees.toString();
       })

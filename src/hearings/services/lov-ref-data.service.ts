b4fecba2aa9f338179fb8034ed7b9aad3b21dca4
Raymond Liang <ray.liang@atos.net>
2022-02-18 11:23:24 +0000

    EUI-5315-refactor-prd-apis

--- src/hearings/services/lov-ref-data.service.ts
+++ src/hearings/services/lov-ref-data.service.ts
@@ -1,11 +1,12 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { LovRefDataModel } from '../models/LovRefData.model';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {LovRefDataModel} from '../models/LovRefData.model';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class LovRefDataService {
-  public constructor(private readonly http: HttpClient) { }
+  public constructor(private readonly http: HttpClient) {
+  }
 
   public getListOfValues(category: string, service: string): Observable<LovRefDataModel[]> {
     return this.http.get<LovRefDataModel[]>(`api/prd/lov/getLovRefData?category=${category}&service=${service}`);

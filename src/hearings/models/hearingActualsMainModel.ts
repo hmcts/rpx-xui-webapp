4416eb6dc23405011734c435b4a2cd0d5eff294e
Kuda Nyamainashe <35928491+kuda1992@users.noreply.github.com>
2022-02-18 16:29:40 +0000

    add a get hearing actuals endpoint (#1753)
    * add a get hearing actuals endpoint
    
    * fix unit tests
    
    * kick the build again
    
    * address pr comments
    
    * remove generate hearing mock

---
+++ src/hearings/models/hearingActualsMainModel.ts
@@ -0,0 +1,83 @@
+import { HearingResult, HMCStatus } from './hearings.enum';
+
+export interface HearingActualsMainModel {
+  hearingActuals: HearingActualsModel;
+  hearingPlanned: HearingPlannedModel;
+  hmcStatus: HMCStatus;
+}
+
+export interface HearingPlannedModel {
+  plannedHearingType: number;
+  plannedHearingDays: PlannedHearingModel[];
+}
+
+export interface PlannedHearingModel {
+  plannedStartTime: string;
+  plannedEndTime: string;
+  parties: PartyModel[];
+}
+
+export interface PartyModel {
+  partyId: number;
+  partyRole: number;
+  individualDetails: IndividualDetailModel;
+  organisationDetails: OrganisationDetailsModel;
+  partyChannelSubType: string;
+}
+
+export interface IndividualDetailModel {
+  title: string;
+  firstName: string;
+  lastName: string;
+}
+
+export interface OrganisationDetailsModel {
+  name: string;
+  cftOrganisationID: string;
+}
+
+export interface HearingActualsModel {
+  hearingOutcome: HearingOutcomeModel;
+  actualHearingDays: ActualHearingDayModel[];
+}
+
+export interface HearingOutcomeModel {
+  hearingFinalFlag: boolean;
+  hearingResult: HearingResult;
+  hearingResultDate: string;
+  hearingResultReasonType: string;
+  hearingType: string;
+}
+
+export interface ActualHearingDayModel {
+  hearingDate: string;
+  hearingStartTime: string;
+  hearingEndTime: string;
+  pauseDateTimes: PauseDateTimeModel[];
+  actualDayParties: ActualDayPartyModel[];
+}
+
+export interface PauseDateTimeModel {
+  pauseStartTime: string;
+  pauseEndTime: string;
+}
+
+export interface ActualIndividualDetailModel {
+  firstName: string;
+  lastName: string;
+}
+
+export interface ActualOrganisationDetailsModel {
+  name: string;
+}
+
+export interface ActualDayPartyModel {
+  actualPartyId: number;
+  didNotAttendFlag: boolean;
+  actualIndividualDetails: ActualIndividualDetailModel;
+  actualOrganisationDetails: ActualOrganisationDetailsModel;
+  partyChannelSubType: string;
+  partyId: number;
+  partyRole: number;
+  representedParty: number;
+}

76f8cb1a3404376eaf6cd5d77b72447a90208184
John Benjamin <90200268+johnbenjamin-hmcts@users.noreply.github.com>
2022-02-21 12:13:03 +0000

    EUI-4978 Request hearing check your answers happy path (#1759)
    * EUI-4978 Request hearing check your answers happy path
    
    * EUI-4978 Request hearing check your answers happy path
    
    * EUI-4978 Request hearing check your answers happy path
    
    * EUI-4978 Request hearing check your answers happy path
    
    * EUI-4978 Request hearing check your answers happy path
    
    * EUI-4978 Request hearing check your answers happy path
    
    * EUI-4978 Request hearing check your answers happy path
    
    * EUI-4978 Changes after code review
    
    * EUI-4978 Changes after code review
    
    * EUI-4978 Changes after code review

---
+++ src/hearings/components/hearing-confirmation/hearing-confirmation.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'exui-hearing-confirmation',
+  templateUrl: './hearing-confirmation.component.html'
+})
+export class HearingConfirmationComponent {
+
+  @Input() public caseId: string;
+  @Input() public heading: string;
+  @Input() public headingDescription: string;
+  @Input() public subheading: string;
+  @Input() public subheadingDescription: string;
+  @Input() public additionalDescription: string;
+}

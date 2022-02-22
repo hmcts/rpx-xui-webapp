708e102a3e0642f6258a4ee64bbd0aaafe1499b9
Pardeep Singh Basi <paddybasi@gmail.com>
2022-02-22 09:40:57 +0000

    templating done

--- src/hearings/templates/hearing-actual-add-edit-summary.template.ts
+++ src/hearings/templates/hearing-actual-add-edit-summary.template.ts
@@ -4,70 +4,99 @@ import {Section} from '../models/section';
 export const HEARING_ACTUAL_ADD_EDIT_SUMMARY_TEMPLATE: Section[] = [
   {
     sectionHTMLTitle: '<span style="margin-bottom: 10px; display: block;" class="govuk-caption-l">Jane Smith vs DWP</span><h1 class="govuk-heading-l">Hearing details</h1>',
-    sectionOrder: 1,
     answers: [
       {
+        id: 'caseName',
         answerTitle: 'Hearing date',
         answerSource: AnswerSource.CASE_NAME,
-        answerOrder: 1
       }
     ],
   },
   {
     sectionHTMLTitle: '<h2 class="govuk-heading-m">Hearing stage and result</h2>',
-    sectionOrder: 2,
+    updateLink: 'hearing-actual/stage-and-result',
     answers: [
       {
+        id: 'stage',
         answerTitle: 'Hearing stage',
         answerSource: AnswerSource.CASE_FLAGS,
-        answerOrder: 1,
       },
       {
+        id: 'result',
         answerTitle: 'Hearing result',
         answerSource: AnswerSource.CASE_FLAGS,
-        answerOrder: 2,
       }
     ],
   },
   {
     sectionHTMLTitle: '<h2 class="govuk-heading-m">Hearing timings</h2>',
-    sectionOrder: 3,
+    updateLink: 'hearing-actual/actuals-date-and-time',
     answers: [
       {
+        id: 'startTime',
         answerTitle: 'Start time',
         answerSource: AnswerSource.ADDITIONAL_SECURITY_REQUIRED,
-        answerOrder: 1,
       },
       {
+        id: 'finishTime',
         answerTitle: 'Finish time',
         answerSource: AnswerSource.ADDITIONAL_FACILITIES_REQUIRED,
-        answerOrder: 2,
       },
       {
+        id: 'pauseTime',
         answerTitle: 'Pause time',
         answerSource: AnswerSource.ADDITIONAL_FACILITIES_REQUIRED,
-        answerOrder: 3,
       },
       {
+        id: 'resumeTime',
         answerTitle: 'Resume time',
         answerSource: AnswerSource.ADDITIONAL_FACILITIES_REQUIRED,
-        answerOrder: 4,
       }
     ],
   },
   {
     sectionHTMLTitle: '<h2 class="govuk-heading-m">Participants</h2>',
-    sectionOrder: 4,
-    answers: [
+    updateLink: 'hearing-actual/actuals-parties',
+    parties: [
       {
-        answerTitle: 'Jane Smith',
-        answerSource: AnswerSource.STAGE,
-        answerOrder: 1,
+        HMTLTitle: '<h2 class="govuk-heading-s">Jane Smith</h2>',
+        partyChannel: 'Video - Teams',
       },
       {
-        answerTitle: 'Attendance type',
-        answerSource: AnswerSource.STAGE,
-        answerOrder: 1,
+        HMTLTitle: '<h2 class="govuk-heading-s">DWP</h2>',
+        partyChannel: 'Video - Teams',
+      },
+      {
+        HMTLTitle: '<h2 class="govuk-heading-s">Attendee 1</h2>',
+        individualDetails: {
+          title: 'Ms',
+          firstName: 'Mary',
+          lastName: 'Jones',
+        },
+        organisationDetails: {
+          cftOrganisationID: '54321',
+          name: 'Company D',
+          organisationType: '',
+        },
+        partyID: '2',
+        partyRole: 'Interpreter',
+        partyChannel: 'Letter',
+      },
+      {
+        HMTLTitle: '<h2 class="govuk-heading-s">Attendee 2</h2>',
+        individualDetails: {
+          title: 'Miss',
+          firstName: 'Bob',
+          lastName: 'Jones',
+        },
+        organisationDetails: {
+          cftOrganisationID: '54321',
+          name: 'Company C',
+          organisationType: '',
+        },
+        partyID: '3',
+        partyRole: 'Solicitor',
+        partyChannel: 'Fax',
       },
     ],
   },

4377fd1af5cc81aaadb8bd2a835a0a7da7a0f762
John Benjamin <john.benjamin@hmcts.net>
2022-02-23 10:34:24 +0000

    EUI-5094 Hearing stage and result page

--- src/hearings/containers/view-hearing/hearing-stage-result/hearing-stage-result.component.ts
+++ src/hearings/containers/view-hearing/hearing-stage-result/hearing-stage-result.component.ts
@@ -1,36 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { select, Store } from '@ngrx/store';
-import { ServiceHearingValuesModel } from '../../../models/serviceHearingValues.model';
-import { Subscription } from 'rxjs';
-import { ControlTypeEnum } from '../../../models/hearings.enum';
+import { Observable, Subscription } from 'rxjs';
 import { LovRefDataModel } from '../../../models/lovRefData.model';
+import { ServiceHearingValuesModel } from '../../../models/serviceHearingValues.model';
+import { HearingsService } from '../../../services/hearings.service';
+import { LovRefDataService } from '../../../services/lov-ref-data.service';
 import * as fromHearingStore from '../../../store';
 
 @Component({
   selector: 'exui-hearing-stage-result',
-  templateUrl: './hearing-stage-result.component.html'
+  templateUrl: './hearing-stage-result.component.html',
+  styleUrls: ['./hearing-stage-result.component.scss']
 })
-export class HearingStageResultComponent implements OnInit {
+export class HearingStageResultComponent implements OnInit, OnDestroy {
   public hearingStageResultForm: FormGroup;
   public validationErrors: { id: string, message: string }[] = [];
-  public hearingResultSelectedOption: string;
-  public multiLevelSelections: LovRefDataModel[] = [];
-  public hasValidationRequested: boolean = false;
-  public configLevels: { level: number, controlType: ControlTypeEnum }[];
   public hearingResultType: string;
   public caseTitle: string;
   public serviceValueSub: Subscription;
-
+  public hearingTypes$: Observable<LovRefDataModel[]>;
+  private hearingState$: Observable<fromHearingStore.State>;
+  
   constructor(private readonly hearingStore: Store<fromHearingStore.State>,
-              private readonly formBuilder: FormBuilder) {
+    private readonly hearingsService: HearingsService,
+    private readonly lovRefDataService: LovRefDataService,
+    private readonly formBuilder: FormBuilder,
+    private readonly route: ActivatedRoute) {
+    this.hearingState$ = this.hearingStore.pipe(select(fromHearingStore.getHearingsFeatureState));
     this.hearingStageResultForm = this.formBuilder.group({});
   }
   
   public ngOnInit(): void {
+    this.hearingTypes$ = this.lovRefDataService.getListOfValues('HearingType', 'SSCS');
     this.serviceValueSub = this.hearingStore.pipe(select(fromHearingStore.getHearingValuesModel)).subscribe((hearingValueModel: ServiceHearingValuesModel) =>
       this.caseTitle = hearingValueModel ? hearingValueModel.caseName : ''
     );
+    this.hearingState$.subscribe(x => {
+      console.log('HEARING STATE', x);
+    });
+  }
+
+  public ngOnDestroy(): void {
+    if (this.serviceValueSub) {
+      this.serviceValueSub.unsubscribe();
+    }
   }
 
   public onHearingResult(hearingResultType: string): void {

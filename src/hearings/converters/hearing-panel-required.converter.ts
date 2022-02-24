c089859c7b10bcf2d60ad428794a5f0cb0d58700
Sivaprakasam Sivaprakasam <siva.sivaprakasam@centrica.com>
2022-02-24 16:45:02 +0000

    part of code review comments addressed

--- src/hearings/converters/hearing-panel-required.converter.ts
+++ src/hearings/converters/hearing-panel-required.converter.ts
@@ -1,5 +1,5 @@
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { MemberType } from '../models/hearings.enum';
 import { State } from '../store';
@@ -11,17 +11,15 @@ export class HearingPanelRequiredConverter implements AnswerConverter {
   public transformAnswer(hearingState$: Observable<State>): Observable<string> {
     return hearingState$.pipe(
       map((state) => {
-        if (
-          !state.hearingRequest.hearingRequestMainModel.hearingDetails ||
-          !state.hearingRequest.hearingRequestMainModel.hearingDetails
-            .panelRequirements
-        )
-          return;
-        const panelPreferences = state.hearingRequest.hearingRequestMainModel.hearingDetails.panelRequirements.panelPreferences || []
-        const hasPanelMembers = panelPreferences.filter((ref) => ref.memberType ===  MemberType.PANEL_MEMBER)
-        return hasPanelMembers.length > 0
-          ? 'Yes'
-          : 'No';
+        if (state.hearingRequest.hearingRequestMainModel.hearingDetails && 
+          state.hearingRequest.hearingRequestMainModel.hearingDetails.panelRequirements) 
+          {
+            const panelPreferences = state.hearingRequest.hearingRequestMainModel.hearingDetails.panelRequirements.panelPreferences || []
+            const hasPanelMembers = panelPreferences.filter((ref) => ref.memberType ===  MemberType.PANEL_MEMBER)
+            return hasPanelMembers.length > 0
+              ? 'Yes'
+              : 'No';
+        }
       })
     );
   }

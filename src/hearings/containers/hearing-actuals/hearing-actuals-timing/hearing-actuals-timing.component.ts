c287e601bebe8b4ad97e8d6e37475876fb382ebb
Kuda Nyamainashe <35928491+kuda1992@users.noreply.github.com>
2022-02-24 14:08:41 +0000

    add validators

--- src/hearings/containers/hearing-actuals/hearing-actuals-timing/hearing-actuals-timing.component.ts
+++ src/hearings/containers/hearing-actuals/hearing-actuals-timing/hearing-actuals-timing.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { filter } from 'rxjs/operators';
+import { HearingActualsMainModel } from '../../../models/hearingActualsMainModel';
+import { HearingActualsStateData } from '../../../models/hearingActualsStateData.model';
+import * as fromHearingStore from '../../../store';
+import { ValidatorsUtils } from '../../../utils/validators.utils';
 
 @Component({
   selector: 'exui-hearing-actuals-timing',
@@ -10,27 +16,41 @@ export class HearingActualsTimingComponent implements OnInit {
 
   public form: FormGroup;
 
-  public constructor(private readonly fb: FormBuilder) {
+  private hearingActuals: HearingActualsMainModel;
+
+  public constructor(private readonly fb: FormBuilder,
+                     private readonly hearingStore: Store<fromHearingStore.State>,
+                     private readonly validatorsUtils: ValidatorsUtils,
+                     ) {
   }
 
   public ngOnInit() {
-    this.form = this.createForm();
-  }
-
-  private createForm(): FormGroup {
-    return this.fb.group({
-      startTime: ['13:00', Validators.required],
-      finishTime: ['14:00', Validators.required],
-      recordTimes: [null, Validators.required],
-      pauseTime: [null],
-      resumeTime: [null],
+    this.hearingStore.select(fromHearingStore.getHearingActuals)
+      .pipe(
+        filter((state: HearingActualsStateData) => !!state.hearingActualsMainModel)
+      )
+      .subscribe((state: HearingActualsStateData) => {
+      this.hearingActuals = state.hearingActualsMainModel ;
+      this.form = this.createForm(this.hearingActuals);
     });
   }
 
   public submit(value: any, valid: boolean) {
-    console.log(value);
+    console.log(value, ' submit ', valid);
     if (valid) {
       console.log('valid');
     }
+  }
+
+  private createForm(hearingActuals: HearingActualsMainModel): FormGroup {
+    return this.fb.group({
+      startTime: ['13:00', [Validators.required, this.validatorsUtils.validTime()]],
+      finishTime: ['14:00', [Validators.required, this.validatorsUtils.validTime()]],
+      recordTimes: [null, [Validators.required]],
+      pauseTime: [null, [this.validatorsUtils.validTime()]],
+      resumeTime: [null, [this.validatorsUtils.validTime()]],
+    }, {
+      validator: this.validatorsUtils.validateTimeRange('startTime', 'finishTime')
+    });
   }
 }

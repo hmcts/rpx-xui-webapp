935a2cf56d4ea9265f76357d9bbb871e6ceced76
Kuda Nyamainashe <35928491+kuda1992@users.noreply.github.com>
2022-02-25 12:22:18 +0000

    fix unit tests

--- src/hearings/containers/hearing-actuals/hearing-actuals-timing/hearing-actuals-timing.component.spec.ts
+++ src/hearings/containers/hearing-actuals/hearing-actuals-timing/hearing-actuals-timing.component.spec.ts
@@ -1,16 +1,31 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { of } from 'rxjs/internal/observable/of';
+import { initialState } from '../../../hearing.test.data';
+import { HearingsService } from '../../../services/hearings.service';
+import { ValidatorsUtils } from '../../../utils/validators.utils';
 
 import { HearingActualsTimingComponent } from './hearing-actuals-timing.component';
 
-fdescribe('HearingTimingComponent', () => {
+describe('HearingTimingComponent', () => {
+  const hearingsService = jasmine.createSpyObj('HearingsService', ['updateHearingActuals']);
   let component: HearingActualsTimingComponent;
   let fixture: ComponentFixture<HearingActualsTimingComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HearingActualsTimingComponent ]
+      imports: [ReactiveFormsModule, RouterTestingModule, HttpClientTestingModule],
+      providers: [
+        provideMockStore({ initialState }),
+        { provide: HearingsService, useValue: hearingsService },
+        ValidatorsUtils
+      ],
+      declarations: [HearingActualsTimingComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +36,21 @@ fdescribe('HearingTimingComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create form ', () => {
+    expect(component.form.value.hearingStartTime).toBe('09:00');
+    expect(component.form.value.hearingEndTime).toBe('10:00');
+  });
+
+  it('should submit form ', () => {
+    hearingsService.updateHearingActuals.and.returnValue(of({}));
+    component.form.patchValue({
+      hearingStartTime: '09:00',
+      hearingEndTime: '10:00',
+      recordTimes: 'no'
+    });
+    component.submit(component.form.value, component.form.valid);
+    expect(hearingsService.updateHearingActuals).toHaveBeenCalled();
   });
 });

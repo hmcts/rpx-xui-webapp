64f10d63909183f3bfd8762c2331220bcd714048
Raymond Liang <ray.liang@atos.net>
2022-02-25 10:30:07 +0000

    EUI-4933-amend-journey-generic: remove duplicates

--- src/hearings/store/reducers/hearing-request-to-compare.reducer.ts
+++ src/hearings/store/reducers/hearing-request-to-compare.reducer.ts
@@ -1,62 +1,15 @@
 import {HearingRequestStateData} from '../../models/hearingRequestStateData.model';
-import * as fromCompareActions from '../actions/hearing-request-to-compare.action';
+import * as fromHearingRequestCompareActions from '../actions/hearing-request-to-compare.action';
+import * as fromHearingRequestReducer from './hearing-request.reducer';
 
 export const initialHearingRequestToCompareState: HearingRequestStateData = {
-  hearingRequestMainModel: {
-    requestDetails: {
-      requestTimeStamp: null,
-    },
-    hearingDetails: {
-      duration: null,
-      hearingType: null,
-      hearingLocations: [],
-      hearingIsLinkedFlag: false,
-      hearingWindow: null,
-      privateHearingRequiredFlag: false,
-      panelRequirements: null,
-      autolistFlag: false,
-      nonStandardHearingDurationReasons: [],
-      hearingPriorityType: null,
-      numberOfPhysicalAttendees: null,
-      hearingInWelshFlag: false,
-      facilitiesRequired: [],
-      listingComments: null,
-      hearingRequester: null,
-      leadJudgeContractType: null,
-    },
-    caseDetails: {
-      hmctsServiceCode: null,
-      caseRef: null,
-      requestTimeStamp: null,
-      hearingID: null,
-      externalCaseReference: null,
-      caseDeepLink: null,
-      hmctsInternalCaseName: null,
-      publicCaseName: null,
-      caseAdditionalSecurityFlag: null,
-      caseInterpreterRequiredFlag: false,
-      caseCategories: [],
-      caseManagementLocationCode: null,
-      caserestrictedFlag: false,
-      caseSLAStartDate: null,
-    },
-    partyDetails: [],
-    hearingResponse: {
-      listAssistTransactionID: null,
-      receivedDateTime: null,
-      responseVersion: null,
-      laCaseStatus: null,
-      hearingCancellationReason: null,
-      hearingDaySchedule: null,
-    }
-  },
-  lastError: null,
+  ...fromHearingRequestReducer.initialHearingRequestState
 };
 
 export function hearingRequestToCompareReducer(currentState = initialHearingRequestToCompareState,
-                                               action: fromCompareActions.HearingRequestToCompareAction): HearingRequestStateData {
+                                               action: fromHearingRequestCompareActions.HearingRequestToCompareAction): HearingRequestStateData {
   switch (action.type) {
-    case fromCompareActions.INITIALIZE_HEARING_REQUEST_TO_COMPARE: {
+    case fromHearingRequestCompareActions.INITIALIZE_HEARING_REQUEST_TO_COMPARE: {
       return {
         ...currentState,
         hearingRequestMainModel: action.payload

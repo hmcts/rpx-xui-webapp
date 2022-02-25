a6b2bf447febe062e6310e84cb84408c78da0b5a
Kuda Nyamainashe <35928491+kuda1992@users.noreply.github.com>
2022-02-25 12:24:23 +0000

    add router testing module

--- src/hearings/containers/hearing-actuals/hearing-actual-add-edit-summary/hearing-actual-add-edit-summary.component.spec.ts
+++ src/hearings/containers/hearing-actuals/hearing-actual-add-edit-summary/hearing-actual-add-edit-summary.component.spec.ts
@@ -1,5 +1,6 @@
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import {provideMockStore} from '@ngrx/store/testing';
 import {of} from 'rxjs';
 import {initialState} from '../../../hearing.test.data';
@@ -17,6 +18,7 @@ describe('HearingViewEditSummaryComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [HearingActualAddEditSummaryComponent],
+      imports: [RouterTestingModule],
       providers: [
         provideMockStore({initialState}),
         {provide: HearingsService, useValue: hearingsService},

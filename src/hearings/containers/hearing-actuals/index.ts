1549d323452ccf47f8eddb328d27df089cf08e68
Pardeep Singh Basi <paddybasi@gmail.com>
2022-02-25 10:14:16 +0000

    routing and basic templating done

--- src/hearings/containers/hearing-actuals/index.ts
+++ src/hearings/containers/hearing-actuals/index.ts
@@ -1,7 +1,9 @@
 import { HearingActualAddEditSummaryComponent } from './hearing-actual-add-edit-summary/hearing-actual-add-edit-summary.component';
+import { HearingActualsViewEditPartiesComponent } from './hearing-actuals-view-edit-parties/hearing-actuals-view-edit-parties.component';
 import { HearingActualsComponent } from './hearing-actuals.component';
 
 export const containers: any[] = [
   HearingActualsComponent,
-  HearingActualAddEditSummaryComponent
+  HearingActualAddEditSummaryComponent,
+  HearingActualsViewEditPartiesComponent
 ];

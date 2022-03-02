ec4fec6ed03063c98ef7c84929f58a54a03cb172
udaydenduluri33 <50869558+udaydenduluri33@users.noreply.github.com>
2022-03-02 13:57:52 +0000

    Feature/e UI 4749 5299 (#1795)
    * allocate a role link component
    
    * refactor reusable code to componentisation
    
    * Initial commit for getting roles via services
    
    * role-access changes
    
    * lint fix
    
    * fix unit tests
    
    * Edit tests and fix display name issue
    
    * RolesAndAccessComponent adminRoles
    
    * insert linkText
    
    * role allocate
    
    * Commit test and choose-role error component changes
    
    * choose role component with error component
    
    * Role allocation admin journey functionality
    
    * Ensure functionality correct and add tests
    
    Mock data included to set test role data
    
    * Update yarn-audit-known-issues
    
    * Adding actions and addressing PR comments
    
    Co-authored-by: Uday Denduluri <uday.denduluri@gmail.com>
    Co-authored-by: connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>

---
+++ src/cases/components/role-access-section/role-access-section.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { CaseView } from '@hmcts/ccd-case-ui-toolkit';
+
+import { CaseRole } from '../../../role-access/models';
+
+@Component({
+    selector: 'exui-role-access-section',
+    templateUrl: './role-access-section.component.html'
+  })
+export class RoleAccessSectionComponent {
+    @Input() public title: string;
+    @Input() roles: CaseRole [];
+    @Input() public caseDetails: CaseView;
+    @Input() public showAllocateRoleLink = false;
+    @Input() public roleRouterLink: string;
+    @Input() public roleQueryParams: string;
+    @Input() public roleCategory: string;
+    @Input() public showAllocate: boolean;
+    @Input() public linkText = 'Allocate a role';
+}

21a86a9c3b2a5d95fcf693a8790c387993387555
Kartheek-S <92437222+Kartheek-S@users.noreply.github.com>
2022-03-06 19:24:15 +0000

    sonar fix

--- src/hearings/pipes/is-amended.pipe.spec.ts
+++ src/hearings/pipes/is-amended.pipe.spec.ts
@@ -5,7 +5,7 @@ import {AnswerSource} from '../models/hearings.enum';
 import {State} from '../store/reducers';
 import {IsAmendedPipe} from './is-amended.pipe';
 
-describe('IsAmendedPipe', () => {
+fdescribe('IsAmendedPipe', () => {
 
   let isAmendedPipe: IsAmendedPipe;
 
@@ -31,7 +31,31 @@ describe('IsAmendedPipe', () => {
 
   it('should transform is amended for additional facilities required', () => {
     const STATE: State = initialState.hearings;
-    const result$ = isAmendedPipe.transform(AnswerSource.ADDITIONAL_FACILITIES_REQUIRED, of(STATE));
+    const result$ = isAmendedPipe.transform(AnswerSource.ATTENDANT_PERSON_AMOUNT, of(STATE));
+    const isAmended = false;
+    const expected = cold('(b|)', {b: isAmended});
+    expect(result$).toBeObservable(expected);
+  });
+
+  it('should transform is amended for additional facilities required', () => {
+    const STATE: State = initialState.hearings;
+    const result$ = isAmendedPipe.transform(AnswerSource.PANEL_INCLUSION, of(STATE));
+    const isAmended = false;
+    const expected = cold('(b|)', {b: isAmended});
+    expect(result$).toBeObservable(expected);
+  });
+
+  it('should transform is amended for additional facilities required', () => {
+    const STATE: State = initialState.hearings;
+    const result$ = isAmendedPipe.transform(AnswerSource.PANEL_EXCLUSION, of(STATE));
+    const isAmended = false;
+    const expected = cold('(b|)', {b: isAmended});
+    expect(result$).toBeObservable(expected);
+  });
+
+  it('should transform is amended for additional facilities required', () => {
+    const STATE: State = initialState.hearings;
+    const result$ = isAmendedPipe.transform(AnswerSource.PANEL_ROLES, of(STATE));
     const isAmended = false;
     const expected = cold('(b|)', {b: isAmended});
     expect(result$).toBeObservable(expected);

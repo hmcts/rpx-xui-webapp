2bdcf47ee09c09047c9006106a545bd2a6ea3509
Uday Denduluri <uday.denduluri@gmail.com>
2022-03-07 16:44:36 +0000

    sort alphabetically

--- api/roleAccess/models/allocate-role-state-data.interface.ts
+++ api/roleAccess/models/allocate-role-state-data.interface.ts
@@ -2,16 +2,16 @@ import { Person } from '@hmcts/rpx-xui-common-lib/lib/models/person.model';
 import { AllocateTo, DurationOfRole, Period, RoleCategory, SpecificRole} from './allocate-role.enum';
 
 export interface AllocateRoleData {
+  action: string;
+  allocateTo: AllocateTo;
+  assignmentId?: string;
   caseType: string;
   caseId: string;
-  action: string;
-  assignmentId?: string;
-  typeOfRole: SpecificRole;
-  allocateTo: AllocateTo;
+  durationOfRole: DurationOfRole;
+  jurisdiction?: string;
+  period: Period;
   personToBeRemoved?: Person;
   person: Person;
-  durationOfRole: DurationOfRole;
   roleCategory?: RoleCategory;
-  period: Period;
-  jurisdiction?: string;
+  typeOfRole: SpecificRole;
 }

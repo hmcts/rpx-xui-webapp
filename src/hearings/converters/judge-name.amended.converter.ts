9584c3d71d1410715fcd01033c7b84fb760fc603
Sivaprakasam Sivaprakasam <>
2022-03-07 17:44:36 +0000

    judge amended flag filter changes

--- src/hearings/converters/judge-name.amended.converter.ts
+++ src/hearings/converters/judge-name.amended.converter.ts
@@ -1,6 +1,7 @@
 import * as _ from 'lodash';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
+import { MemberType, RequirementType } from '../models/hearings.enum';
 import {State} from '../store/reducers';
 import {IsAmendedConverter} from './is-amended.converter';
 
@@ -9,8 +10,8 @@ export class JudgeNameAmendedConverter implements IsAmendedConverter {
     return hearingState$.pipe(map(state => {
       const objAPanelRequirements = state.hearingRequestToCompare.hearingRequestMainModel.hearingDetails.panelRequirements;
       const objBPanelRequirements = state.hearingRequest.hearingRequestMainModel.hearingDetails.panelRequirements;
-      const objA = objAPanelRequirements && objAPanelRequirements.panelPreferences;
-      const objB = objBPanelRequirements && objBPanelRequirements.panelPreferences;
+      const objA = objAPanelRequirements && objAPanelRequirements.panelPreferences && objAPanelRequirements.panelPreferences.filter(panel => panel.memberType === MemberType.JUDGE && panel.requirementType === RequirementType.MUSTINC);
+      const objB = objBPanelRequirements && objBPanelRequirements.panelPreferences && objBPanelRequirements.panelPreferences.filter(panel => panel.memberType === MemberType.JUDGE && panel.requirementType === RequirementType.MUSTINC);
       return !_.isEqual(objA, objB);
     }));
   }

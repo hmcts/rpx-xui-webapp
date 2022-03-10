bdda6449a99b16ffe3b059fbade979425de0c181
Pardeep Singh Basi <paddybasi@gmail.com>
2022-03-10 13:56:06 +0000

    Feature/e UI 5099 hearing actuals display participants (#1808)
    * routing and basic templating done
    
    * fix the ordering
    
    * remove tsconfig
    
    * added ref data and fixed up template
    
    * fixed participant list
    
    * added child_nodes and fixed attendance type being hardcoded
    
    * fixed values in mock data
    
    * display issue with roles fixed
    
    * tests fix
    
    * tests fix
    
    * fix the ordering
    
    * remove tsconfig
    
    * added ref data and fixed up template
    
    * added child_nodes and fixed attendance type being hardcoded
    
    * lint fixes
    
    * removed unnecessary files
    
    * cve issue ignore
    
    * cve issue ignore
    
    * fixed code coverage issue
    
    * fixed PR comments
    
    * fixed PR comments
    
    * fixed PR comments
    
    * fixed CVE issues
    
    Co-authored-by: Kuda Nyamainashe <35928491+kuda1992@users.noreply.github.com>

--- api/prd/lov/data/lov.mock.data.ts
+++ api/prd/lov/data/lov.mock.data.ts
@@ -97,7 +97,7 @@ export const DEFAULT_PARTYCHANNEL_REF: LovRefDataModel[] = [
     value_en: 'In person',
     value_cy: '',
     hintText_EN: 'in person',
-    hintTextCY: '',
+    hintTextCY: 'Wyneb yn wyneb',
     order: 1,
     parentKey: null,
   },
@@ -106,18 +106,94 @@ export const DEFAULT_PARTYCHANNEL_REF: LovRefDataModel[] = [
     value_en: 'By phone',
     value_cy: '',
     hintText_EN: 'By Phone',
-    hintTextCY: '',
+    hintTextCY: 'Ffôn',
     order: 2,
     parentKey: null,
+    child_nodes: [
+      {
+        key: 'telephone-btMeetMe',
+        value_en: 'Telephone - BTMeetme',
+        value_cy: '',
+        hintText_EN: 'By Phone bTMeetme',
+        hintTextCY: '',
+        order: 1,
+        parentKey: null,
+      },
+      {
+        key: 'telephone-CVP',
+        value_en: 'Telephone - CVP',
+        value_cy: '',
+        hintText_EN: 'By Phone CVP',
+        hintTextCY: '',
+        order: 2,
+        parentKey: null,
+      },
+      {
+        key: 'telephone-other',
+        value_en: 'Telephone - Other',
+        value_cy: '',
+        hintText_EN: 'By Phone Other',
+        hintTextCY: '',
+        order: 3,
+        parentKey: null,
+      },
+      {
+        key: 'telephone-skype',
+        value_en: 'Telephone - Skype',
+        value_cy: '',
+        hintText_EN: 'By Phone Skype',
+        hintTextCY: '',
+        order: 4,
+        parentKey: null,
+      },
+    ],
   },
   {
     key: 'byVideo',
     value_en: 'By video',
-    value_cy: '',
+    value_cy: 'Fideo',
     hintText_EN: 'By video',
     hintTextCY: '',
     order: 4,
     parentKey: null,
+    child_nodes: [
+      {
+        key: 'video-conference',
+        value_en: 'Video Conference',
+        value_cy: '',
+        hintText_EN: 'By video conference',
+        hintTextCY: '',
+        order: 4,
+        parentKey: null,
+      },
+      {
+        key: 'video-other',
+        value_en: 'Video - Other',
+        value_cy: '',
+        hintText_EN: 'By video other',
+        hintTextCY: '',
+        order: 4,
+        parentKey: null,
+      },
+      {
+        key: 'video-skype',
+        value_en: 'Video - Skype',
+        value_cy: '',
+        hintText_EN: 'By video skype',
+        hintTextCY: '',
+        order: 4,
+        parentKey: null,
+      },
+      {
+        key: 'video-teams',
+        value_en: 'Video - Teams',
+        value_cy: '',
+        hintText_EN: 'By video teams',
+        hintTextCY: '',
+        order: 4,
+        parentKey: null,
+      },
+    ],
   },
   {
     key: 'notAttending',
@@ -584,6 +660,59 @@ export const OTHER_PANEL_ROLES: LovRefDataModel[] = [{
   child_nodes: [],
 }];
 
+export const HEARING_ROLES_REF: LovRefDataModel[] = [
+  {
+    key: 'appellant',
+    value_en: 'Appellant',
+    value_cy: '',
+    hintText_EN: 'Appellant',
+    hintTextCY: '',
+    order: 1,
+    parentKey: null,
+  },
+  {
+    key: 'claimant',
+    value_en: 'Claimant',
+    value_cy: '',
+    hintText_EN: 'Claimant',
+    hintTextCY: '',
+    order: 2,
+    parentKey: null,
+  },
+  {
+    key: 'interpreter',
+    value_en: 'Interpreter',
+    value_cy: '',
+    hintText_EN: 'Interpreter',
+    hintTextCY: '',
+    order: 3,
+    parentKey: null,
+  },
+  {
+    key: 'solicitor',
+    value_en: 'Solicitor',
+    value_cy: '',
+    hintText_EN: 'Solicitor',
+    hintTextCY: '',
+    order: 4,
+    parentKey: null,
+  },
+  {
+    key: 'barrister',
+    value_en: 'Barrister',
+    value_cy: '',
+    hintText_EN: 'Barrister',
+    hintTextCY: '',
+    order: 5,
+    parentKey: null,
+  },
+];
+
+export const HEARING_ROLES: LovRefDataByServiceModel = {
+  serviceID: 'SSCS',
+  values: HEARING_ROLES_REF,
+};
+
 export const SSCS_STAGES: LovRefDataByServiceModel = {
   serviceID: 'SSCS',
   values: DEFAULT_STAGES_REF,
@@ -598,6 +727,7 @@ export const SSCS_PARTY_CHANNEL: LovRefDataByServiceModel = {
   serviceID: 'SSCS',
   values: DEFAULT_PARTYCHANNEL_REF,
 };
+
 export const SSCS_JUDGE_TYPES: LovRefDataByServiceModel = {
   serviceID: 'SSCS',
   values: DEFAULT_JUDGE_TYPES_REF,
@@ -655,5 +785,9 @@ export const ALL_REF_DATA: LovRefDataByCategoryModel[] = [
   {
     categoryKey: 'OtherPanelRoles',
     services: [SSCS_OTHER_PANEL_ROLES],
+  },
+  {
+    categoryKey: 'HearingRoles',
+    services: [HEARING_ROLES],
   },
 ];

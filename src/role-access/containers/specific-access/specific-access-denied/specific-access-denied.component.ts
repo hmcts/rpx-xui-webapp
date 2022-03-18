32c61550eb2155cea6c93ed9f7d08cb354135b28
Paul Howes <paul.howes@HMCTS.NET>
2022-03-18 08:39:42 +0000

    Specific access WIP

--- src/role-access/containers/specific-access/specific-access-denied/specific-access-denied.component.ts
+++ src/role-access/containers/specific-access/specific-access-denied/specific-access-denied.component.ts
@@ -1,9 +1,8 @@
 import { Component, Input, OnDestroy } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
-import { Answer, ExcludeOption, ExclusionNavigationEvent, ExclusionState, ExclusionStateData } from '../../../models';
-import { AnswerHeaderText, AnswerLabelText } from '../../../models/enums';
-import { ExclusionNavigation } from '../../../models/exclusion-navigation.interface';
+import { SpecificAccessNavigationEvent, SpecificAccessStateData } from '../../../models';
+import { SpecificAccessNavigation } from '../../../models/specific-access-navigation.interface';
 import * as fromFeature from '../../../store';
 
 @Component({
@@ -12,19 +11,17 @@ import * as fromFeature from '../../../store';
 })
 export class SpecificAccessDeniedComponent implements OnDestroy {
 
-  @Input() public navEvent: ExclusionNavigation;
+  @Input() public navEvent: SpecificAccessNavigation;
 
-  public answers: Answer[] = [];
-  public caption = AnswerHeaderText.AddExclusion;
-  public heading = AnswerHeaderText.CheckAnswers;
-  public hint = AnswerHeaderText.CheckInformation;
+  public caption = '';
+  public heading = '';
+  public hint = '';
   public storeSubscription: Subscription;
 
-  private exclusionStateData: ExclusionStateData;
+  private specificAccessStateData: SpecificAccessStateData;
 
   constructor(private readonly store: Store<fromFeature.State>) {
-    this.storeSubscription = this.store.pipe(select(fromFeature.getRoleAccessState)).subscribe(
-      exclusion => this.setAnswersFromExclusionStore(exclusion));
+    this.storeSubscription = this.store.pipe(select(fromFeature.getRoleAccessState)).subscribe();
   }
 
   public ngOnDestroy(): void {
@@ -33,10 +30,9 @@ export class SpecificAccessDeniedComponent implements OnDestroy {
     }
   }
 
-  public navigationHandler(navEvent: ExclusionNavigationEvent) {
+  public navigationHandler(navEvent: SpecificAccessNavigationEvent) {
     switch (navEvent) {
-      case ExclusionNavigationEvent.CONFIRM_EXCLUSION:
-        this.store.dispatch(new fromFeature.ConfirmExclusionAction(this.exclusionStateData));
+      case SpecificAccessNavigationEvent.CONTINUE:
         break;
       default:
         throw new Error('Invalid option');
@@ -45,21 +41,6 @@ export class SpecificAccessDeniedComponent implements OnDestroy {
 
   public onNavigate(action) {
     this.store.dispatch(new fromFeature.ChangeNavigation(action));
-  }
-
-  private setAnswersFromExclusionStore(exclusion: ExclusionStateData): void {
-    this.exclusionStateData = exclusion;
-    this.answers = [];
-    this.answers.push({ label: AnswerLabelText.WhoFor, value: exclusion.exclusionOption, action: ExclusionState.CHOOSE_EXCLUSION });
-    if (exclusion.exclusionOption === ExcludeOption.EXCLUDE_ANOTHER_PERSON) {
-      this.answers.push({ label: AnswerLabelText.PersonRole, value: exclusion.personRole, action: ExclusionState.CHOOSE_PERSON_ROLE });
-      let personDetails = exclusion.person.name;
-      if (exclusion.person.email) {
-        personDetails += `\n${exclusion.person.email}`;
-      }
-      this.answers.push({ label: AnswerLabelText.Person, value: personDetails, action: ExclusionState.FIND_PERSON });
-    }
-    this.answers.push({ label: AnswerLabelText.DescribeExclusion, value: exclusion.exclusionDescription, action: ExclusionState.DESCRIBE_EXCLUSION });
   }
 }
 

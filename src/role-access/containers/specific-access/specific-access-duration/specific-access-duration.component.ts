22d3e911290fd6f8c82c0480b4a48ee8159fcd45
Scott Turner <scott.turner@hmcts.net>
2022-03-21 11:04:51 +0000

    EUI-4230 - copied allocation-role > choose-duration component for specific-access > duration

--- src/role-access/containers/specific-access/specific-access-duration/specific-access-duration.component.ts
+++ src/role-access/containers/specific-access/specific-access-duration/specific-access-duration.component.ts
@@ -1,60 +1,251 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { PersonRole } from '@hmcts/rpx-xui-common-lib/lib/models/person.model';
-import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs';
-import { Observable } from 'rxjs/Observable';
-import { ERROR_MESSAGE, PERSON_ROLE } from '../../../constants';
-import { SpecificAccessNavigationEvent, SpecificAccessState } from '../../../models';
-import { SpecificAccessNavigation } from '../../../models/specific-access-navigation.interface';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { select, Store } from '@ngrx/store';
+import * as moment from 'moment';
+import { $enum as EnumUtil } from 'ts-enum-util';
+import {
+  Actions,
+  AllocateRoleStateData,
+  Period,
+  SpecificAccessState,
+  SpecificAccessNavigation,
+  SpecificAccessNavigationEvent
+} from '../../../models';
+import { DurationType } from '../../../models/enums';
 import * as fromFeature from '../../../store';
+import { getTitleText } from '../../../utils';
 
 @Component({
   selector: 'exui-specific-access-duration',
-  templateUrl: './specific-access-duration.component.html'
+  templateUrl: './specific-access-duration.component.html',
+  styleUrls: ['./specific-access-duration.component.scss']
 })
-export class SpecificAccessDurationComponent implements OnInit, OnDestroy {
-  public ERROR_MESSAGE = ERROR_MESSAGE;
+
+export class SpecificAccessDurationComponent implements OnInit {
+  public static sevenDaysDesc = 'Starts from today and ends at midnight 7 days from now.';
+  public static indefiniteDesc = 'Starts from today and lasts while the case is open.';
+  public static anotherPeriodDesc = 'You’ll need to provide both a start and end date for the role.';
+
   @Input() public navEvent: SpecificAccessNavigation;
-
-  public title = '';
-  public caption = '';
-
-  public submitted: boolean = false;
-
+  public radioSelected: FormControl;
+  public anotherPeriod: boolean;
+  public caption = 'Approve specific access request';
+  public title = 'How long do you want to give access to this case for?';
+  public readonly durations: DurationDescription [];
+  public selectedDuration: DurationType;
+  public dayStartDate: FormControl;
+  public monthStartDate: FormControl;
+  public yearStartDate: FormControl;
+  public dayEndDate: FormControl;
+  public monthEndDate: FormControl;
+  public yearEndDate: FormControl;
   public formGroup: FormGroup;
-  public radioOptionControl: FormControl;
-  public radioControlName: string = PERSON_ROLE;
+  public isEndDateError: boolean;
+  public isStartDateError: boolean;
+  public startDateErrorMessage: string;
+  public endDateErrorMessage: string;
+  public dateFormat = 'YYYY-MM-DD';
 
-  public specificAccessStateDataSub: Subscription;
-
-  public personRole: PersonRole;
-
-  constructor(private readonly store: Store<fromFeature.State>) {
-  }
+  constructor(
+    private readonly store: Store<fromFeature.State>,
+    private readonly builder: FormBuilder
+  ) {
+    this.durations = [
+      { id: '1', duration: DurationType.SEVEN_DAYS, description: SpecificAccessDurationComponent.sevenDaysDesc, checked: false },
+      { id: '2', duration: DurationType.INDEFINITE, description:  SpecificAccessDurationComponent.indefiniteDesc, checked: false },
+      { id: '3', duration: DurationType.ANOTHER_PERIOD, description: SpecificAccessDurationComponent.anotherPeriodDesc, checked: false }
+    ];
+    this.dayStartDate = new FormControl('', [Validators.required, Validators.min(1), Validators.max(31)]);
+    this.monthStartDate = new FormControl('', [Validators.required, Validators.min(1), Validators.max(12)]);
+    this.yearStartDate = new FormControl('', Validators.required);
+    this.dayEndDate = new FormControl(['', Validators.required, Validators.min(1), Validators.max(31)]);
+    this.monthEndDate = new FormControl('', [Validators.required, Validators.min(1), Validators.max(12)]);
+    this.yearEndDate = new FormControl('', Validators.required);
+    this.radioSelected = new FormControl('', Validators.required);
+   }
 
   public ngOnInit(): void {
-
+    this.formGroup = this.builder.group({
+      dayStartDate: this.dayStartDate,
+      monthStartDate: this.monthStartDate,
+      yearStartDate: this.yearStartDate,
+      dayEndDate: this.dayEndDate,
+      monthEndDate: this.monthEndDate,
+      yearEndDate: this.yearEndDate,
+      radioSelected: this.radioSelected
+    });
+    // TODO: SARD - refactor or remove
+    // this.store.pipe(select(fromFeature.getAllocateRoleState)).subscribe(roleAllocate => {
+    //   this.selectDurationRole(roleAllocate);
+    // });
   }
 
-  public navigationHandler(navEvent: SpecificAccessNavigationEvent) {
-    this.submitted = true;
-    this.dispatchEvent(navEvent);
+  // TODO: SARD - refactor or remove
+  // public selectDurationRole(roleAllocate: AllocateRoleStateData) {
+  //   const action = EnumUtil(Actions).getKeyOrDefault(roleAllocate.action);
+  //   const typeOfRole = roleAllocate.typeOfRole;
+  //   this.title = getTitleText(typeOfRole, action, roleAllocate.roleCategory);
+  //   this.selectedDuration = roleAllocate.durationOfRole;
+  //   this.durations.find(duration => duration.duration === this.selectedDuration).checked = true;
+  //   this.anotherPeriod = this.selectedDuration === DurationType.ANOTHER_PERIOD;
+  //   if (this.anotherPeriod && roleAllocate.period) {
+  //     this.radioSelected.setValue(this.selectedDuration);
+  //     if (roleAllocate.period.startDate) {
+  //       this.dayStartDate.setValue(roleAllocate.period.startDate.getDate());
+  //       this.monthStartDate.setValue(roleAllocate.period.startDate.getMonth() + 1);
+  //       this.yearStartDate.setValue(roleAllocate.period.startDate.getFullYear());
+  //     }
+  //     if (roleAllocate.period.endDate) {
+  //       this.dayEndDate.setValue(roleAllocate.period.endDate.getDate());
+  //       this.monthEndDate.setValue(roleAllocate.period.endDate.getMonth() + 1);
+  //       this.yearEndDate.setValue(roleAllocate.period.endDate.getFullYear());
+  //     }
+  //   }
+  // }
+
+  public navigationHandler(navEvent: SpecificAccessNavigationEvent): void {
+    this.resetPreviousErrors();
+    const period = this.getPeriod(this.selectedDuration);
+    if (period) {
+      switch (navEvent) {
+        case SpecificAccessNavigationEvent.CONTINUE:
+          // TODO: SARD - handle store dispatch
+          console.log('dispatch event');
+          // this.store.dispatch(new fromFeature.ChooseDurationAndGo({
+          //     durationOfRole: this.selectedDuration,
+          //     period,
+          //     allocateRoleState: SpecificAccessState.CHECK_ANSWERS}
+          //   ));
+          break;
+        default:
+          throw new Error('Invalid option');
+      }
+    }
+  }
+  public resetPreviousErrors(): void {
+    this.isStartDateError = false;
+    this.isEndDateError = false;
+    this.startDateErrorMessage = '';
+    this.endDateErrorMessage = '';
   }
 
-  public dispatchEvent(navEvent: SpecificAccessNavigationEvent) {
-    switch (navEvent) {
-      case SpecificAccessNavigationEvent.CONTINUE:
-        this.store.dispatch(new fromFeature.ChangeSpecificAccessNavigation(SpecificAccessState.SPECIFIC_ACCESS_APPROVED));
-        break;
-      default:
-        throw new Error('Invalid option');
+  public getPeriod(duration: DurationType): Period {
+    switch (duration) {
+      case DurationType.SEVEN_DAYS: {
+        const nextDate = this.getTodayDate().setDate(new Date().getDate() + 7);
+        return {
+          startDate: this.getTodayDate(),
+          endDate: new Date(nextDate)
+        };
+      }
+      case DurationType.INDEFINITE: {
+        return {
+          startDate: this.getTodayDate(),
+          endDate: null
+        };
+      }
+      case DurationType.ANOTHER_PERIOD: {
+        if (this.isDateValid() && this.datesMissing() && this.formGroup.valid && this.startDateNotInPast() && this.startDateLessThanEndDate()) {
+          return {
+            startDate: this.getStartDate(),
+            endDate: this.getEndDate(),
+          };
+        }
+      }
     }
   }
 
-  public ngOnDestroy(): void {
-    if (this.specificAccessStateDataSub) {
-      this.specificAccessStateDataSub.unsubscribe();
+  // TODO: SARD - move to base class
+  public startDateLessThanEndDate(): boolean {
+    const startDate = this.getStartDate();
+    const endDate = this.getEndDate();
+    if (startDate > endDate) {
+      this.isEndDateError = true;
+      this.endDateErrorMessage = 'The role end date must be after the role start date';
+      return false;
     }
+    return true;
   }
+
+  // TODO: SARD - move to base class
+  public startDateNotInPast(): boolean {
+    const startDate = this.getStartDate();
+    const currentDate = this.getTodayDate();
+    if (startDate < currentDate) {
+      this.isStartDateError = true;
+      this.startDateErrorMessage = 'The role start date must not be in the past';
+      return false;
+    }
+    return true;
+  }
+
+  // TODO: SARD - move to base class
+  public datesMissing(): boolean {
+    let dateMissing = true;
+    if (!parseInt(this.dayStartDate.value, 10) || !parseInt(this.monthStartDate.value, 10) || !parseInt(this.yearStartDate.value, 10)) {
+      this.startDateErrorMessage = 'Please enter some value';
+      this.isStartDateError = true;
+      dateMissing = false;
+    }
+
+    if (!parseInt(this.dayEndDate.value, 10) || !parseInt(this.monthEndDate.value, 10) || !parseInt(this.yearEndDate.value, 10)) {
+      this.endDateErrorMessage = 'Please enter some value';
+      this.isEndDateError = true;
+      dateMissing = false;
+    }
+    return dateMissing;
+  }
+
+  // TODO: SARD - move to base class
+  public isDateValid() {
+    const startDate = `${this.yearStartDate.value}-${this.formatString(this.monthStartDate.value)}-${this.formatString(this.dayStartDate.value)}`;
+    const isStartDateValid = moment(startDate, this.dateFormat, true).isValid();
+    if (!isStartDateValid) {
+      this.startDateErrorMessage = 'Invalid Start date';
+      this.isStartDateError = true;
+    }
+
+    const endDate = `${this.yearEndDate.value}-${this.formatString(this.monthEndDate.value)}-${this.formatString(this.dayEndDate.value)}`;
+    const isEndDateValid = moment(endDate, this.dateFormat, true).isValid();
+    if (!isEndDateValid) {
+      this.endDateErrorMessage = 'Invalid End date';
+      this.isEndDateError = true;
+    }
+    return isStartDateValid && isEndDateValid;
+  }
+
+  // TODO: SARD - move to base class
+  private formatString(month: number) {
+    return month >= 10 ? month.toString() : `0${month}`;
+  }
+
+  // TODO: SARD - move to base class
+  public getTodayDate(): Date {
+    const currentDate = new Date();
+    currentDate.setHours(0, 0, 0, 0);
+    return currentDate;
+  }
+
+  // TODO: SARD - move to base class
+  public getStartDate(): Date {
+    return new Date(this.yearStartDate.value, this.monthStartDate.value - 1, this.dayStartDate.value);
+  }
+
+  // TODO: SARD - move to base class
+  public getEndDate(): Date {
+    return new Date(this.yearEndDate.value, this.monthEndDate.value - 1, this.dayEndDate.value);
+  }
+
+  // TODO: SARD - rename this to be more sensible name
+  public onDurationChange(item: DurationType): void {
+    this.anotherPeriod = item === DurationType.ANOTHER_PERIOD;
+    this.selectedDuration = item;
+  }
+}
+
+export interface DurationDescription {
+  id: string;
+  duration: DurationType;
+  description: string;
+  checked: boolean;
 }

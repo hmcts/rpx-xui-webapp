2ad68b79dda6b633ff1d87af7124a7138606b17a
Scott Turner <scott.turner@hmcts.net>
2022-03-23 13:42:59 +0000

    EUI-4230 - unit tests for SpecificAccessDuration component

--- src/role-access/containers/specific-access/specific-access-duration/specific-access-duration.component.spec.ts
+++ src/role-access/containers/specific-access/specific-access-duration/specific-access-duration.component.spec.ts
@@ -1,24 +1,26 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { Store } from '@ngrx/store';
-import { ChooseRadioOptionComponent } from '../../../components';
+import { StoreModule } from '@ngrx/store';
 import { SpecificAccessDurationComponent } from './specific-access-duration.component';
+import { DurationHelperService } from '../../../services/duration-helper.service';
+import { DurationType } from '../../../models/enums';
+import createSpyObj = jasmine.createSpyObj;
 
 describe('SpecificAccessDurationComponent', () => {
   let component: SpecificAccessDurationComponent;
   let fixture: ComponentFixture<SpecificAccessDurationComponent>;
-  const mockStore = jasmine.createSpyObj('store', ['dispatch', 'pipe']);
+  let durationHelperService: DurationHelperService;
 
   beforeEach(async(() => {
+    durationHelperService = createSpyObj('durationHelperService', ['getInputClass']);
     TestBed.configureTestingModule({
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      declarations: [ChooseRadioOptionComponent, SpecificAccessDurationComponent],
-      imports: [ ],
+      imports: [FormsModule, ReactiveFormsModule, StoreModule.forRoot({})],
+      declarations: [SpecificAccessDurationComponent],
       providers: [
-        { provide: Store, useValue: mockStore },
+        FormBuilder,
+        { provide: DurationHelperService, useValue: durationHelperService }
       ]
-    })
-      .compileComponents();
+    });
   }));
 
   beforeEach(() => {
@@ -27,12 +29,60 @@ describe('SpecificAccessDurationComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('resetPreviousErrors', () => {
+
+    it('should reset the component properties as expected', () => {
+      component.startDateErrorMessage = 'Start date error message';
+      component.endDateErrorMessage = 'End date error message';
+      component.isStartDateError = true;
+      component.isEndDateError = true;
+
+      component.resetPreviousErrors();
+      expect(component.isStartDateError).toBe(false);
+      expect(component.isEndDateError).toBe(false);
+      expect(component.startDateErrorMessage).toBe('');
+      expect(component.endDateErrorMessage).toBe('');
+    });
+
+  });
+
+  describe('onDurationChange', () => {
+
+    it('should set component properties as expected for duration: 7 Days', () => {
+      const duration = DurationType.SEVEN_DAYS;
+      component.onDurationChange(duration);
+      expect(component.selectedDuration).toEqual(duration);
+      expect(component.anotherPeriod).toEqual(false);
+    });
+
+    it('should set component properties as expected for duration: Indefinite', () => {
+      const duration = DurationType.INDEFINITE;
+      component.onDurationChange(duration);
+      expect(component.selectedDuration).toEqual(duration);
+      expect(component.anotherPeriod).toEqual(false);
+    });
+
+    it('should set component properties as expected for duration: Another Period', () => {
+      const duration = DurationType.ANOTHER_PERIOD;
+      component.onDurationChange(duration);
+      expect(component.selectedDuration).toEqual(duration);
+      expect(component.anotherPeriod).toEqual(true);
+    });
+
+  });
+
+  describe('getInputClass', () => {
+
+    it('should call durationHelperService getInputClass method', () => {
+      component.getInputClass(false);
+      expect(durationHelperService.getInputClass).toHaveBeenCalledWith(false, false);
+    });
+
   });
 
   afterEach(() => {
     component = null;
     fixture.destroy();
   });
+
 });

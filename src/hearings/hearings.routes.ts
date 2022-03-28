428d93d83643d1b518013bf2af091b45b3a31009
SivaprakasamSivaprakasam <98454215+SivaprakasamSivaprakasam@users.noreply.github.com>
2022-03-28 15:37:14 +0100

    Feature/EUI-5110 - view details of Hearings from Hearings tab- Request failed (#1909)
    * init checkin of failed request handling
    
    * checkin of failed request handling
    
    * audit fix
    
    * lint fix
    
    * lint fix
    
    * updating adjourned/completed routes data for ischildrequired.
    
    * failed request handling
    
    * test fx
    
    * review comments fixes
    
    * review comments fixes
    
    Co-authored-by: Sivaprakasam Sivaprakasam <>

--- src/hearings/hearings.routes.ts
+++ src/hearings/hearings.routes.ts
@@ -27,6 +27,7 @@ import { RequestHearingComponent } from './containers/request-hearing/request-he
 import { HearingCancellationSummaryComponent } from './containers/view-hearing/hearing-cancellation-summary/hearing-cancellation-summary.component';
 import { HearingCancelledSummaryComponent } from './containers/view-hearing/hearing-cancelled-summary/hearing-cancelled-summary.component';
 import { HearingCompletedSummaryComponent } from './containers/view-hearing/hearing-completed-summary/hearing-completed-summary.component';
+import { HearingRequestFailedSummaryComponent } from './containers/view-hearing/hearing-request-failed-summary/hearing-request-failed-summary.component';
 import { ViewHearingComponent } from './containers/view-hearing/view-hearing.component';
 import { HearingCategory, MemberType } from './models/hearings.enum';
 import { ActualSummaryResponseResolver } from './resolvers/actual-summary-response-resolver.resolve';
@@ -372,7 +373,7 @@ export const ROUTES: Routes = [
         canActivate: [HealthCheckGuard],
         data: {
           title: 'HMCTS Hearings | Hearing Actuals | Completed',
-          isChildRequired: true
+          isChildRequired: [HearingCategory.PartyChannel]
         }
       },
       {
@@ -388,7 +389,21 @@ export const ROUTES: Routes = [
         canActivate: [HealthCheckGuard],
         data: {
           title: 'HMCTS Hearings | Hearing Actuals | Adjourned',
-          isChildRequired: true
+          isChildRequired: [HearingCategory.PartyChannel]
+        }
+      },
+      {
+        path: 'hearing-request-failed-summary/:id',
+        resolve: {
+          hearingStageOptions: HearingStageResolver,
+          judicialResponseUsers: JudicialUserSearchResponseResolver,
+          courtLocation: CourtLocationsDataResolver,
+          partyChannels: HearingActualPartyChannelResolverService,
+        },
+        component: HearingRequestFailedSummaryComponent,
+        canActivate: [HealthCheckGuard],
+        data: {
+          title: 'HMCTS Hearings | View Hearing | Request failed '
         }
       },
     ]

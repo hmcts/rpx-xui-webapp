1d794ab49ed7ec56156890741e866ec377ea3f0d
sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
2022-03-28 15:07:15 +0100

    Approve specific access request FT basic steps and page object added

---
+++ test/e2e/features/pageObjects/caseAccessManagement/rejectSpecificAccessRequestDetailsPage.js
@@ -0,0 +1,17 @@
+
+
+class RequestRejectionDetailsPage{
+
+
+    constructot(){
+        this.container = $('');
+        this.header = $('');
+        this.headerCaption = $('');
+
+        this.hintText = $('');
+        this.rejectionSummaryText = $('');
+        
+    }
+}
+
+module.exporrts = new RequestRejectionDetailsPage(); 

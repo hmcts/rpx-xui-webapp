6c506779c0b860f2c07520368f9c6d7b10075f58
John Benjamin <john.benjamin@hmcts.net>
2022-03-31 10:39:55 +0100

    EUI-5583 View details waiting to be listed for JOH

--- api/hearings/data/hearingRequests.mock.data.ts
+++ api/hearings/data/hearingRequests.mock.data.ts
@@ -2093,4 +2093,207 @@ export const HEARING_REQUEST_RESULTS: HearingRequestMainModel[] = [
       },
     },
   },
+  {
+    requestDetails: {
+      requestTimeStamp: '2022-02-23T09:00:00.000+0000',
+      versionNumber: 1,
+    },
+    hearingDetails: {
+      duration: 60,
+      hearingType: 'final',
+      hearingLocations: [
+        {
+          locationType: 'hearing',
+          locationId: '196538',
+        },
+      ],
+      hearingIsLinkedFlag: false,
+      hearingWindow: {
+        hearingWindowDateRange: {
+          hearingWindowStartDateRange: '2021-11-23T09:00:00.000+0000',
+          hearingWindowEndDateRange: '2021-11-30T09:00:00.000+0000',
+        },
+        hearingWindowFirstDate: '2021-12-01T09:00:00.000+0000',
+      },
+      privateHearingRequiredFlag: false,
+      panelRequirements: null,
+      autolistFlag: false,
+      hearingPriorityType: 'standard',
+      numberOfPhysicalAttendees: 2,
+      hearingInWelshFlag: false,
+      facilitiesRequired: [
+        'immigrationDetentionCentre',
+        'inCameraCourt',
+        'sameSexCourtroom',
+      ],
+      listingComments: 'Interpreter required',
+      hearingRequester: '',
+      leadJudgeContractType: '',
+    },
+    caseDetails: {
+      hmctsServiceCode: 'BBA3',
+      caseRef: '1584618195804035',
+      requestTimeStamp: null,
+      hearingID: 'h100014',
+      externalCaseReference: null,
+      caseDeepLink: null,
+      hmctsInternalCaseName: 'Jane Smith vs DWP',
+      publicCaseName: 'Jane Smith vs DWP',
+      caseAdditionalSecurityFlag: false,
+      caseInterpreterRequiredFlag: false,
+      caseCategories: [
+        {
+          categoryType: CategoryType.CaseType,
+          categoryValue: 'Personal Independence Payment',
+        },
+        {
+          categoryType: CategoryType.CaseSubType,
+          categoryValue: 'Conditions of Entitlement',
+        },
+        {
+          categoryType: CategoryType.CaseSubType,
+          categoryValue: 'Good cause',
+        },
+        {
+          categoryType: CategoryType.CaseSubType,
+          categoryValue: 'Rate of Assessment / Payability Issues - complex',
+        }],
+      caseManagementLocationCode: null,
+      caserestrictedFlag: false,
+      caseSLAStartDate: '2021-11-23T09:00:00.000+0000',
+    },
+    partyDetails: [
+      {
+        partyID: 'P1',
+        partyName: 'Jane and Smith',
+        partyType: PartyType.IND,
+        partyChannel: 'inPerson',
+        partyRole: 'appellant',
+        individualDetails: {
+          title: 'Miss',
+          firstName: 'Jane',
+          lastName: 'Smith',
+          reasonableAdjustments: [
+            'RA0042',
+            'RA0053',
+            'RA0013',
+            'RA0016',
+            'RA0042',
+          ],
+          interpreterLanguage: 'PF0015',
+        },
+        organisationDetails: {},
+        unavailabilityDOW: null,
+        unavailabilityRanges: [
+          {
+            unavailableFromDate: '2021-12-10T09:00:00.000+0000',
+            unavailableToDate: '2021-12-31T09:00:00.000+0000',
+          },
+        ],
+      },
+      {
+        partyID: 'P2',
+        partyName: 'DWP',
+        partyType: PartyType.ORG,
+        partyChannel: 'byVideo',
+        partyRole: 'claimant',
+        individualDetails: {
+          title: '',
+          firstName: 'DWP',
+          lastName: 'Representative',
+          reasonableAdjustments: [
+            'RA0005',
+          ],
+          interpreterLanguage: null,
+        },
+        organisationDetails: {
+          name: 'DWP',
+          organisationType: 'GOV',
+          cftOrganisationID: 'O100000',
+        },
+        unavailabilityDOW: null,
+        unavailabilityRanges: [
+          {
+            unavailableFromDate: '2021-12-20T09:00:00.000+0000',
+            unavailableToDate: '2021-12-31T09:00:00.000+0000',
+          },
+        ],
+      },
+    ],
+    hearingResponse: {
+      listAssistTransactionID: '123456789',
+      receivedDateTime: '2021-11-30T09:00:00.000+0000',
+      responseVersion: 0,
+      laCaseStatus: LaCaseStatus.LISTED,
+      listingStatus: ListingStatus.FIXED,
+      hearingCancellationReason: '123456543',
+      hearingDaySchedule: {
+        hearingStartDateTime: '2021-03-12T09:00:00.000+0000',
+        hearingEndDateTime: '2021-03-12T16:00:00.000+0000',
+        listAssistSessionID: '0d22d836-b25a-11eb-a18c-f2d58a9b7b15',
+        hearingVenueId: '815833',
+        hearingRoomId: 'room 4',
+        hearingJudgeId: 'p1000002',
+        panelMemberIds: ['p1000001'],
+        attendees: [
+          {
+            partyID: 'P1',
+            partyName: 'Jane and Smith',
+            partyType: PartyType.IND,
+            partyRole: 'appellant',
+            partyChannel: 'inPerson',
+            individualDetails: {
+              title: 'Miss',
+              firstName: 'Jane',
+              lastName: 'Smith',
+              reasonableAdjustments: [
+                'RA0042',
+                'RA0053',
+                'RA0013',
+                'RA0016',
+                'RA0042',
+              ],
+              interpreterLanguage: 'PF0015',
+            },
+            organisationDetails: {},
+            unavailabilityDOW: null,
+            unavailabilityRanges: [
+              {
+                unavailableFromDate: '2021-12-10T09:00:00.000+0000',
+                unavailableToDate: '2021-12-31T09:00:00.000+0000',
+              },
+            ],
+          },
+          {
+            partyID: 'P2',
+            partyName: 'DWP',
+            partyType: PartyType.ORG,
+            partyRole: 'claimant',
+            partyChannel: 'byVideo',
+            individualDetails: {
+              title: '',
+              firstName: 'DWP',
+              lastName: 'Representative',
+              reasonableAdjustments: [
+                'RA0005',
+              ],
+              interpreterLanguage: null,
+            },
+            organisationDetails: {
+              name: 'DWP',
+              organisationType: 'GOV',
+              cftOrganisationID: 'O100000',
+            },
+            unavailabilityDOW: null,
+            unavailabilityRanges: [
+              {
+                unavailableFromDate: '2021-12-20T09:00:00.000+0000',
+                unavailableToDate: '2021-12-31T09:00:00.000+0000',
+              },
+            ],
+          },
+        ],
+      },
+    },
+  },
 ];

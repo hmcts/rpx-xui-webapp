ff9616ebbfcf294bd9c0b6bfdb32f03673c7b392
Kartheek-S <92437222+Kartheek-S@users.noreply.github.com>
2022-04-11 23:12:09 +0100

    happy path

--- src/hearings/containers/linked-hearings/index.ts
+++ src/hearings/containers/linked-hearings/index.ts
@@ -1,3 +1,4 @@
+import { LinkedHearingsCheckYourAnswersComponent } from './check-your-answers/check-your-answers.component';
 import { HowLinkedHearingsBeHeardComponent } from './linked-hearings-how-to-heard/linked-hearings-how-to-heard.component';
 import { LinkedHearingsWithCaseComponent } from './linked-hearings-with-case/linked-hearings-with-case.component';
 import { LinkedHearingsComponent } from './linked-hearings.component';
@@ -5,5 +6,6 @@ import { LinkedHearingsComponent } from './linked-hearings.component';
 export const containers: any[] = [
   LinkedHearingsComponent,
   LinkedHearingsWithCaseComponent,
-  HowLinkedHearingsBeHeardComponent
+  HowLinkedHearingsBeHeardComponent,
+  LinkedHearingsCheckYourAnswersComponent
 ];
--- src/hearings/store/effects/hearing-links.effects.ts
+++ src/hearings/store/effects/hearing-links.effects.ts
@@ -1,10 +1,10 @@
-import {Injectable} from '@angular/core';
-import {Actions, Effect, ofType} from '@ngrx/effects';
-import {of} from 'rxjs';
-import {catchError, map, switchMap} from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
 import * as hearingLinksActions from '../../../hearings/store/actions/hearing-links.action';
-import {HttpError} from '../../../models/httpError.model';
-import {HearingsService} from '../../services/hearings.service';
+import { HttpError } from '../../../models/httpError.model';
+import { HearingsService } from '../../services/hearings.service';
 
 @Injectable()
 export class HearingLinksEffects {
@@ -23,6 +23,18 @@ export class HearingLinksEffects {
       return this.hearingsService.loadServiceLinkedCases(payload.caseReference, payload.hearingId).pipe(
         map(response => new hearingLinksActions.LoadServiceLinkedCasesSuccess(response)),
         catchError((error: HttpError) => of(new hearingLinksActions.LoadServiceLinkedCasesFailure(error)))
+      );
+    })
+  );
+
+  @Effect()
+  public loadLinkedHearingGroup$ = this.actions$.pipe(
+    ofType(hearingLinksActions.LOAD_LINKED_HEARING_GROUP),
+    map((action: hearingLinksActions.LoadLinkedHearingGroup) => action.payload),
+    switchMap(payload => {
+      return this.hearingsService.getLinkedHearingGroup(payload.caseReference, payload.hearingId).pipe(
+        map(response => new hearingLinksActions.LoadLinkedHearingGroupSuccess(response)),
+        catchError((error: HttpError) => of(new hearingLinksActions.LoadLinkedHearingGroupFailure(error)))
       );
     })
   );

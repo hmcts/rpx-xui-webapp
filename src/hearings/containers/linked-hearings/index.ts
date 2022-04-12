c78cd8cc7f9a6687759c0120b4b23e968fba52cb
John Benjamin <john.benjamin@hmcts.net>
2022-04-12 10:18:52 +0100

    Update check your answers page

--- src/hearings/containers/linked-hearings/index.ts
+++ src/hearings/containers/linked-hearings/index.ts
@@ -1,4 +1,4 @@
-import { LinkedHearingsCheckYourAnswersComponent } from './check-your-answers/check-your-answers.component';
+import { LinkedHearingsCheckYourAnswersComponent } from './linked-hearings-check-your-answers/linked-hearings-check-your-answers.component';
 import { HowLinkedHearingsBeHeardComponent } from './linked-hearings-how-to-heard/linked-hearings-how-to-heard.component';
 import { LinkedHearingsWithCaseComponent } from './linked-hearings-with-case/linked-hearings-with-case.component';
 import { LinkedHearingsComponent } from './linked-hearings.component';

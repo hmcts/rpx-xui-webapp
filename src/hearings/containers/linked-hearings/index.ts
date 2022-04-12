2f59c138010a5de56e4cd49184f7abbe6c6e1245
John Benjamin <john.benjamin@hmcts.net>
2022-04-12 14:22:41 +0100

    EUI-5364 Confirmation message display

--- src/hearings/containers/linked-hearings/index.ts
+++ src/hearings/containers/linked-hearings/index.ts
@@ -1,4 +1,5 @@
 import { LinkedHearingsCheckYourAnswersComponent } from './linked-hearings-check-your-answers/linked-hearings-check-your-answers.component';
+import { LinkedHearingsFinalConfirmationComponent } from './linked-hearings-final-confirmation/linked-hearings-final-confirmation.component';
 import { HowLinkedHearingsBeHeardComponent } from './linked-hearings-how-to-heard/linked-hearings-how-to-heard.component';
 import { LinkedHearingsWithCaseComponent } from './linked-hearings-with-case/linked-hearings-with-case.component';
 import { LinkedHearingsComponent } from './linked-hearings.component';
@@ -7,5 +8,6 @@ export const containers: any[] = [
   LinkedHearingsComponent,
   LinkedHearingsWithCaseComponent,
   HowLinkedHearingsBeHeardComponent,
-  LinkedHearingsCheckYourAnswersComponent
+  LinkedHearingsCheckYourAnswersComponent,
+  LinkedHearingsFinalConfirmationComponent
 ];

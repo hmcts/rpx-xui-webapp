50307365b32c7fb1fef651c13aa2ca390c14a549
Sivaprakasam Sivaprakasam <>
2022-04-13 17:55:19 +0100

    PR comments fixes on how linking should be heard page

--- src/hearings/containers/index.ts
+++ src/hearings/containers/index.ts
@@ -3,7 +3,6 @@ import * as fromHearingActuals from './hearing-actuals';
 import * as fromLinkedHearings from './linked-hearings';
 import * as fromRequestHearing from './request-hearing';
 import * as fromViewHearing from './view-hearing';
-import * as fromLinkedHearing from './linked-hearings/linked-hearings-how-to-heard'
 
 export const containers: any[] = [
   ...fromCancelHearing.containers,
--- src/hearings/containers/linked-hearings/linked-hearings-how-to-heard/index.ts
+++
@@ -1,5 +0,0 @@
-import {HowLinkedHearingsBeHeardComponent} from './linked-hearings-how-to-heard.component';
-
-export const containers: any[] = [
-  HowLinkedHearingsBeHeardComponent
-];

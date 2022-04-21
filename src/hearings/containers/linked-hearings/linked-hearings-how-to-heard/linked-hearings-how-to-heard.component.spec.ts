c89d5f20fa46b23bf08c6d578994a49a02cac059
Sivaprakasam Sivaprakasam <>
2022-04-21 07:34:48 +0100

    manage links tests for how to heard page

--- src/hearings/containers/linked-hearings/linked-hearings-how-to-heard/linked-hearings-how-to-heard.component.spec.ts
+++ src/hearings/containers/linked-hearings/linked-hearings-how-to-heard/linked-hearings-how-to-heard.component.spec.ts
@@ -1,98 +1,92 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
-import { provideMockStore } from '@ngrx/store/testing';
-import { of } from 'rxjs';
-import { initialState } from '../../../hearing.test.data';
-import { HMCStatus } from '../../../models/hearings.enum';
-import { ServiceLinkedCasesModel } from '../../../models/linkHearings.model';
-import { HearingsPipesModule } from '../../../pipes/hearings.pipes.module';
-import { HearingsService } from '../../../services/hearings.service';
-import { HowLinkedHearingsBeHeardComponent } from './linked-hearings-how-to-heard.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { FormBuilder, ReactiveFormsModule } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
+import { provideMockStore } from "@ngrx/store/testing";
+import { of } from "rxjs";
+import { initialState } from "../../../hearing.test.data";
+import { HMCStatus, Mode } from "../../../models/hearings.enum";
+import { ServiceLinkedCasesModel } from "../../../models/linkHearings.model";
+import { HearingsPipesModule } from "../../../pipes/hearings.pipes.module";
+import { HearingsService } from "../../../services/hearings.service";
+import { HowLinkedHearingsBeHeardComponent } from "./linked-hearings-how-to-heard.component";
 
-describe('HowLinkedHearingsBeHeardComponent', () => {
-  let component: HowLinkedHearingsBeHeardComponent;
-  let fixture: ComponentFixture<HowLinkedHearingsBeHeardComponent>;
-  const mockedHttpClient = jasmine.createSpyObj('HttpClient', ['get', 'post']);
-  const hearingsService = new HearingsService(mockedHttpClient);
-  const mockStore = jasmine.createSpyObj('Store', ['pipe', 'dispatch']);
-  const mockRouter = jasmine.createSpyObj('Router', ['navigate']);
-
-  const source: ServiceLinkedCasesModel[] = [
-    {
-      caseReference: '4652724902696213',
-      caseName: 'Smith vs Peterson',
-      reasonsForLink: [
-        'Linked for a hearing'
-      ],
-      hearings: [{
-        hearingId: 'h100001',
+const mockLinkedHearingGroup = {
+  linkedHearingGroup: {
+    groupDetails: {
+      groupName: "Group A",
+      groupReason: "Reason 1",
+      groupLinkType: "Ordered",
+      groupComments: "Comment 1",
+    },
+    hearingsInGroup: [
+      { hearingId: "h100001", hearingOrder: 2, caseRef: "4652724902696213" },
+      { hearingId: "h100003", hearingOrder: 1, caseRef: "8254902572336147" },
+    ],
+  },
+  lastError: null,
+};
+const mockResponse: ServiceLinkedCasesModel[] = [
+  {
+    caseReference: "4652724902696213",
+    caseName: "Smith vs Peterson",
+    reasonsForLink: ["Linked for a hearing"],
+    hearings: [
+      {
+        hearingId: "h100001",
         hearingStage: HMCStatus.UPDATE_REQUESTED,
         isSelected: true,
         hearingStatus: HMCStatus.AWAITING_LISTING,
-        hearingIsLinkedFlag: false
-      }]
-    },
-    {
-      caseReference: '5283819672542864',
-      caseName: 'Smith vs Peterson',
-      reasonsForLink: [
-        'Linked for a hearing',
-        'Progressed as part of lead case'
-      ]
-    },
-    {
-      caseReference: '8254902572336147',
-      caseName: 'Smith vs Peterson',
-      reasonsForLink: [
-        'Familial',
-        'Guardian',
-        'Linked for a hearing'
-      ],
-      hearings: [{
-        hearingId: 'h100010',
+        hearingIsLinkedFlag: false,
+      },
+    ],
+  },
+  {
+    caseReference: "5283819672542864",
+    caseName: "Smith vs Peterson",
+    reasonsForLink: ["Linked for a hearing", "Progressed as part of lead case"],
+  },
+  {
+    caseReference: "8254902572336147",
+    caseName: "Smith vs Peterson",
+    reasonsForLink: ["Familial", "Guardian", "Linked for a hearing"],
+    hearings: [
+      {
+        hearingId: "h100010",
         hearingStage: HMCStatus.UPDATE_REQUESTED,
         isSelected: true,
         hearingStatus: HMCStatus.AWAITING_LISTING,
-        hearingIsLinkedFlag: false
-      }, {
-        hearingId: 'h100012',
+        hearingIsLinkedFlag: false,
+      },
+      {
+        hearingId: "h100012",
         hearingStage: HMCStatus.UPDATE_REQUESTED,
         isSelected: false,
         hearingStatus: HMCStatus.AWAITING_LISTING,
-        hearingIsLinkedFlag: false
-      }]
-    }
-  ];
+        hearingIsLinkedFlag: false,
+      },
+    ],
+  },
+];
+
+const hearingLinksMock = {
+  ...mockLinkedHearingGroup,
+  ...mockResponse
+
+}
+
+let component: HowLinkedHearingsBeHeardComponent;
+let fixture: ComponentFixture<HowLinkedHearingsBeHeardComponent>;
+const mockedHttpClient = jasmine.createSpyObj("HttpClient", ["get", "post"]);
+const hearingsService = new HearingsService(mockedHttpClient);
+const mockStore = jasmine.createSpyObj("Store", ["pipe", "dispatch"]);
+const mockRouter = jasmine.createSpyObj("Router", ["navigate"]);
+
+describe("Linking - HowLinkedHearingsBeHeardComponent", () => {
+
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [HowLinkedHearingsBeHeardComponent],
-      imports: [ReactiveFormsModule, RouterTestingModule,
-        HearingsPipesModule],
-      providers: [
-        provideMockStore({initialState}),
-        { provide: HearingsService, useValue: hearingsService },
-        { provide: Router, useValue: mockRouter },
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            snapshot: {
-              data: {
-                linkedCase: { serviceLinkedCases: source }
-              },
-              params: {
-                caseId: '8254902572336147'
-              }
-            },
-            fragment: of('point-to-me'),
-          }
-        },
-        FormBuilder
-      ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    }).compileComponents();
+    ConfigureTestBedModule(hearingsService, mockRouter, Mode.LINK_HEARINGS);
   }));
 
   beforeEach(() => {
@@ -101,43 +95,121 @@ describe('HowLinkedHearingsBeHeardComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have validation errors mapped when nothing selected', () => {
-    mockStore.pipe.and.returnValue(of(source));
+  it("should have validation errors mapped when nothing selected", () => {
+    mockStore.pipe.and.returnValue(of(mockResponse));
     component.onSubmit();
     expect(component.validationErrors.length).toBe(1);
   });
 
-  it('should able to submit when form particularOrder radio is selected', () => {
+  it("should able to submit when form particularOrder radio is selected", () => {
     const nativeElement = fixture.debugElement.nativeElement;
-    const firstRadioButtonElement = nativeElement.querySelector('#particularOrder');
+    const firstRadioButtonElement =
+      nativeElement.querySelector("#particularOrder");
     firstRadioButtonElement.click();
     fixture.detectChanges();
-    component.form.patchValue({hearingGroup: 'particularOrder'});
+    component.form.patchValue({ hearingGroup: "particularOrder" });
     component.onOrderChange(0);
     component.onOrderChange(1);
     expect(component.validationErrors.length).toBe(0);
   });
 
-  it('should able to submit when form together radio is selected', () => {
+  it("should able to submit when form together radio is selected", () => {
     const nativeElement = fixture.debugElement.nativeElement;
-    const firstRadioButtonElement = nativeElement.querySelector('#heardTogether');
+    const firstRadioButtonElement =
+      nativeElement.querySelector("#heardTogether");
     firstRadioButtonElement.click();
     fixture.detectChanges();
     expect(component.validationErrors.length).toBe(0);
   });
 
-  it('should navigate to previous page', () => {
-    component.caseId = '8254902572336147';
-    component.hearingId = 'h100010';
+  it("should navigate to previous page", () => {
+    component.caseId = "8254902572336147";
+    component.hearingId = "h100010";
     component.onBack();
-    expect(mockRouter.navigate).toHaveBeenCalledWith(['/', 'hearings', 'link', '8254902572336147', 'h100010']);
+    expect(mockRouter.navigate).toHaveBeenCalledWith([
+      "/",
+      "hearings",
+      "link",
+      "8254902572336147",
+      "h100010",
+    ]);
   });
 
   afterEach(() => {
     fixture.destroy();
   });
 });
+
+describe("Manage Linking - HowLinkedHearingsBeHeardComponent", () => {
+  beforeEach(async(() => {
+    ConfigureTestBedModule(hearingsService, mockRouter, Mode.MANAGE_HEARINGS);
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HowLinkedHearingsBeHeardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should have a same slot group preselected", () => {
+    const nativeElement = fixture.debugElement.nativeElement;
+    mockLinkedHearingGroup.linkedHearingGroup.groupDetails.groupLinkType = "Same Slot";
+    const firstRadioButtonElement = nativeElement.querySelector("input[name=hearingGroup]:checked");
+    expect(firstRadioButtonElement).not.toBeNull();
+    mockStore.pipe.and.returnValue(of(hearingLinksMock));
+    component.onSubmit();
+    expect(component.validationErrors.length).toBe(0);
+  });
+
+  it("should have a order group preselected", () => {
+    const nativeElement = fixture.debugElement.nativeElement;
+    mockLinkedHearingGroup.linkedHearingGroup.groupDetails.groupLinkType = "Ordered";
+    const firstRadioButtonElement = nativeElement.querySelector("input[name=hearingGroup]:checked");
+    expect(firstRadioButtonElement).not.toBeNull();
+    mockStore.pipe.and.returnValue(of(hearingLinksMock));
+    component.onSubmit();
+    expect(component.validationErrors.length).toBe(0);
+  });
+
+  afterEach(() => {
+    fixture.destroy();
+  });
+});
+
+function ConfigureTestBedModule(hearingsService: HearingsService, mockRouter: any, mode: Mode) {
+  TestBed.configureTestingModule({
+    declarations: [HowLinkedHearingsBeHeardComponent],
+    imports: [ReactiveFormsModule, RouterTestingModule, HearingsPipesModule],
+    providers: [
+      provideMockStore({ initialState }),
+      { provide: HearingsService, useValue: hearingsService },
+      { provide: Router, useValue: mockRouter },
+      {
+        provide: ActivatedRoute,
+        useValue: {
+          snapshot: {
+            data: {
+              mode: mode
+            },
+            params: {
+              caseId: "8254902572336147",
+              hearingId: 'h100001'
+            },
+          },
+          fragment: of("point-to-me"),
+        },
+      },
+      FormBuilder,
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  }).compileComponents();
+}
+

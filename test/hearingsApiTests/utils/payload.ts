5548f81518f05c21ecbbd69bea81a438fc36dcb6
VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>
2022-05-10 15:31:50 +0100

    hearings_api_tests

---
+++ test/hearingsApiTests/utils/payload.ts
@@ -0,0 +1,9 @@
+const payload = {
+  firstName: 'Vamshi',
+  lastName: 'Muniganti',
+  email: 'vam.mun1752@mailnesia.com',
+  roles: ['pui-organisation-manager'],
+  resendInvite: true,
+};
+
+module.exports

d442164b17a07ffe25256da0e60e1af5928c449d
tarik.dogan <tarik.dogan@hmcts.net>
2022-05-16 10:09:23 +0100

    initial commit

--- src/role-access/models/specific-access-state-data.interface.ts
+++ src/role-access/models/specific-access-state-data.interface.ts
@@ -1,11 +1,33 @@
+import { RoleCategory } from '@hmcts/ccd-case-ui-toolkit/dist/shared';
+import { Person } from '@hmcts/rpx-xui-common-lib';
+import { SpecificRole } from '.';
+import { Period } from './allocate-role.enum';
 import { AccessReason } from './enums';
 import { HttpError } from './http-error.interface';
 import { SpecificAccessFormData, SpecificAccessMoreInformationForm } from './specific-access-form-data.interface';
 import { SpecificAccessState } from './specific-access-state.enum';
 
+// export interface SpecificAccessStateData {
+//   state: SpecificAccessState;
+//   accessReason: AccessReason;
+//   lastError?: HttpError;
+//   specificAccessFormData?: SpecificAccessFormData;
+//   SpecificAccessMoreInformationFormData?: SpecificAccessMoreInformationForm;
+
+
+// }
+
 export interface SpecificAccessStateData {
   state: SpecificAccessState;
+  caseId: string;
+  jurisdiction: string;
+  typeOfRole?: SpecificRole;
+  roleCategory: RoleCategory;
+  requestedRole: string;
   accessReason: AccessReason;
+  period?: Period;
+  person: Person;
+  requestId: string;
   lastError?: HttpError;
   specificAccessFormData?: SpecificAccessFormData;
   SpecificAccessMoreInformationFormData?: SpecificAccessMoreInformationForm;
--- src/role-access/store/effects/specific-access.effects.ts
+++ src/role-access/store/effects/specific-access.effects.ts
@@ -1,14 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Action } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
+import { catchError, map, mergeMap, tap } from 'rxjs/operators';
 import * as routeAction from '../../../app/store/index';
-import { RoleAccessHttpError } from '../../models';
+import { RoleAccessHttpError, SpecificAccessState } from '../../models';
 import { REDIRECTS } from '../../models/enums/redirect-urls';
+import { AllocateRoleService } from '../../services';
+import * as fromFeature from '../../store/actions';
+import {  RequestMoreInfoSpecificAccessRequest, SpecificAccessActionTypes } from '../actions';
 
 @Injectable()
 export class SpecificAccessEffects {
 
-  constructor(
+  @Effect() public requestMoreInfoSpecificAccessRequest$ = this.actions$
+    .pipe(
+      ofType<RequestMoreInfoSpecificAccessRequest>(SpecificAccessActionTypes.REQUEST_MORE_INFO_SPECIFIC_ACCESS_REQUEST),
+      mergeMap(
+        (data) => of({status:201, message:'done'})//this.allocateRoleService.specificAccessApproval(data.payload)
+          .pipe(
+          map((data) => {
+            console.log(data);
+            debugger;
+              return new fromFeature.ChangeSpecificAccessNavigation(SpecificAccessState.SPECIFIC_ACCESS_DENIED);
+            }),
+            catchError(error => {
+                return SpecificAccessEffects.handleError(error, SpecificAccessActionTypes.REQUEST_MORE_INFO_SPECIFIC_ACCESS_REQUEST);
+              }
+            )
+          )
+      )
+    );
+
+  constructor(private readonly actions$: Actions
   ) {
   }
 

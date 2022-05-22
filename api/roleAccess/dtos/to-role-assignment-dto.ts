fb4309832a5b9ce49b5e828301f2ca48a89fd410
tarik.dogan <tarik.dogan@hmcts.net>
2022-05-22 21:14:34 +0100

    EUI-4238 roleassignment finetuned

--- api/roleAccess/dtos/to-role-assignment-dto.ts
+++ api/roleAccess/dtos/to-role-assignment-dto.ts
@@ -79,6 +79,63 @@ export function toSARoleAssignmentBody(currentUserId: string, allocateRoleData: 
   };
 }
 
+export function toDenySARoleAssignmentBody(currentUserId: string, allocateRoleData: AllocateRoleData): any {
+  const todayDate = new Date();
+  return {
+    roleRequest: {
+      assignerId: currentUserId,
+      replaceExisting: true,
+      process: 'specific-access',
+      reference: `${allocateRoleData.caseId}/${allocateRoleData.requestedRole}/${allocateRoleData.person.id}`,
+    },
+    requestedRoles: [{
+      roleType: 'CASE',
+      readOnly: true,
+      grantType: 'BASIC', //SPECIFIC
+      classification: 'PRIVATE',
+      attributes: {
+        caseId: allocateRoleData.caseId,
+        requestedRole: allocateRoleData.requestedRole,
+      },
+      roleName: allocateRoleData.typeOfRole.id,
+      roleCategory: allocateRoleData.roleCategory,
+      actorIdType: 'IDAM',
+      actorId: allocateRoleData.person.id,
+      endTime: '2022-06-21T12:34:09.101Z',//new Date(new Date().setDate(new Date().getDate() + 30)),
+      beginTime: null,
+      //beginTime: allocateRoleData.period.startDate,
+      // endTime: allocateRoleData.period.endDate
+      // ? allocateRoleData.period.endDate : new Date(todayDate.setMonth(todayDate.getMonth() + 1)),
+      // TODO: Include notes once we have that information
+      notes: [{comment: "{\"specificReason\":\"Testing testing testing\"}",
+      time: "2022-05-10T16:34:18.763Z",
+      userId: allocateRoleData.person.id}],
+    }
+    // ,
+    // {
+    //   roleType: 'CASE',
+    //   readOnly: true,
+    //   grantType: 'SPECIFIC',
+    //   classification: 'RESTRICTED',
+    //   attributes: {
+    //     caseId: allocateRoleData.caseId,
+    //     requestedRole: allocateRoleData.requestedRole,
+    //   },
+    //   roleName: allocateRoleData.requestedRole,
+    //   roleCategory: allocateRoleData.roleCategory,
+    //   actorIdType: 'IDAM',
+    //   actorId: allocateRoleData.person.id,
+    //   // beginTime: allocateRoleData.period.startDate,
+    //   // endTime: allocateRoleData.period.endDate,
+    //   // TODO: Include notes once we have that information
+    //   notes: [{comment: "{\"specificReason\":\"Testing testing testing\"}",
+    //   time: "2022-05-10T16:34:18.763Z",
+    //   userId: allocateRoleData.person.id}, ],
+    // }
+  ],
+  };
+}
+
 export function toSARequestRoleAssignmentBody(allocateRoleData: AllocateRoleData): any {
   const todayDate = new Date();
   return {

a6dea515d24fbdd1a912351a5a727eda967e95ec
jDman <davis.jaym@gmail.com>
2022-05-27 17:08:27 +0100

    Add providers for NGXLogger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,77 @@
-import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
-import { APP_INITIALIZER, CUSTOM_ELEMENTS_SCHEMA, ErrorHandler, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ExtraOptions, RouterModule } from '@angular/router';
-import { NgIdleKeepaliveModule } from '@ng-idle/keepalive';
+import { HttpClientModule, HttpClientXsrfModule } from "@angular/common/http";
+import {
+  APP_INITIALIZER,
+  CUSTOM_ELEMENTS_SCHEMA,
+  ErrorHandler,
+  NgModule,
+} from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ExtraOptions, RouterModule } from "@angular/router";
+import { PaymentLibModule } from "@hmcts/ccpay-web-component";
+import {
+  ExuiCommonLibModule,
+  FeatureToggleService,
+  LaunchDarklyService,
+  TimeoutNotificationsService,
+} from "@hmcts/rpx-xui-common-lib";
+import { NgIdleKeepaliveModule } from "@ng-idle/keepalive";
 // ngrx modules - START
-import { EffectsModule } from '@ngrx/effects';
-import { RouterStateSerializer, StoreRouterConnectingModule } from '@ngrx/router-store';
-import { MetaReducer, Store, StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { storeFreeze } from 'ngrx-store-freeze';
-import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
-import { environment } from '../environments/environment';
-import { initApplication } from './app-initilizer';
+import { EffectsModule } from "@ngrx/effects";
+import {
+  RouterStateSerializer,
+  StoreRouterConnectingModule,
+} from "@ngrx/router-store";
+import { MetaReducer, Store, StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { storeFreeze } from "ngrx-store-freeze";
+import {
+  LoggerModule,
+  NGXLogger,
+  NGXLoggerHttpService,
+  NgxLoggerLevel,
+  NGXMapperService,
+} from "ngx-logger";
+import { environment } from "../environments/environment";
+import {
+  EnvironmentConfig,
+  ENVIRONMENT_CONFIG,
+} from "../models/environmentConfig.model";
+import { initApplication } from "./app-initilizer";
 // app routes
-import { ROUTES, routingConfiguration } from './app.routes';
-import { AppComponent } from './containers/app/app.component';
+import { ROUTES, routingConfiguration } from "./app.routes";
+import { AppComponent } from "./containers/app/app.component";
 // common provider
-import { ProvidersModule } from './providers/providers.module';
-import { AcceptTermsService } from './services/acceptTerms/acceptTerms.service';
-import { ExuiCommonLibModule, FeatureToggleService, LaunchDarklyService, TimeoutNotificationsService } from '@hmcts/rpx-xui-common-lib';
-import { PaymentLibModule } from '@hmcts/ccpay-web-component';
-import { ENVIRONMENT_CONFIG, EnvironmentConfig } from '../models/environmentConfig.model';
-import { CaseShareService } from './services/case/share-case.service';
-import { DefaultErrorHandler } from './services/errorHandler/defaultErrorHandler';
-import { AbstractAppInsights, AppInsightsWrapper } from './services/logger/appInsightsWrapper';
-import { CryptoWrapper } from './services/logger/cryptoWrapper';
-import { LoggerService } from './services/logger/logger.service';
-import { MonitoringService } from './services/logger/monitoring.service';
-import { SharedModule } from './shared/shared.module';
-import { effects } from './store/effects';
+import { ProvidersModule } from "./providers/providers.module";
+import { AcceptTermsService } from "./services/acceptTerms/acceptTerms.service";
+import { CaseShareService } from "./services/case/share-case.service";
+import { DefaultErrorHandler } from "./services/errorHandler/defaultErrorHandler";
+import {
+  AbstractAppInsights,
+  AppInsightsWrapper,
+} from "./services/logger/appInsightsWrapper";
+import { CryptoWrapper } from "./services/logger/cryptoWrapper";
+import { LoggerService } from "./services/logger/logger.service";
+import { MonitoringService } from "./services/logger/monitoring.service";
+import { SharedModule } from "./shared/shared.module";
+import { effects } from "./store/effects";
 // ngrx modules - END
 // APP store
-import { CustomSerializer, reducers } from './store/reducers';
+import { CustomSerializer, reducers } from "./store/reducers";
 // enforces immutability
 export const metaReducers: MetaReducer<any>[] = !environment.production
   ? [storeFreeze]
   : [];
 
-export function launchDarklyClientIdFactory(envConfig: EnvironmentConfig): string {
-  return envConfig.launchDarklyClientId || '';
+export function launchDarklyClientIdFactory(
+  envConfig: EnvironmentConfig
+): string {
+  return envConfig.launchDarklyClientId || "";
 }
 
 const routerOptions: ExtraOptions = {
-  scrollPositionRestoration: 'enabled',
-  anchorScrolling: 'enabled'
+  scrollPositionRestoration: "enabled",
+  anchorScrolling: "enabled",
 };
 
 @NgModule({
@@ -52,14 +79,14 @@ const routerOptions: ExtraOptions = {
   imports: [
     LoggerModule.forRoot({
       level: NgxLoggerLevel.TRACE,
-      disableConsoleLogging: false
+      disableConsoleLogging: false,
     }),
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
     HttpClientXsrfModule.withOptions({
-      cookieName: 'XSRF-TOKEN',
-      headerName: 'X-XSRF-TOKEN'
+      cookieName: "XSRF-TOKEN",
+      headerName: "X-XSRF-TOKEN",
     }),
     ProvidersModule.forRoot(),
     RouterModule.forRoot(ROUTES, routingConfiguration),
@@ -67,41 +94,44 @@ const routerOptions: ExtraOptions = {
     EffectsModule.forRoot(effects),
     StoreRouterConnectingModule,
     StoreDevtoolsModule.instrument({
-      logOnly: environment.production
+      logOnly: environment.production,
     }),
     SharedModule,
     ExuiCommonLibModule,
     NgIdleKeepaliveModule.forRoot(),
-    PaymentLibModule
+    PaymentLibModule,
   ],
   providers: [
+    NGXLogger,
+    NGXLoggerHttpService,
+    NGXMapperService,
     {
       provide: RouterStateSerializer,
-      useClass: CustomSerializer
+      useClass: CustomSerializer,
     },
     {
       provide: APP_INITIALIZER,
       useFactory: initApplication,
       deps: [Store, ENVIRONMENT_CONFIG],
-      multi: true
+      multi: true,
     },
     CryptoWrapper,
     MonitoringService,
     LoggerService,
     {
       provide: AbstractAppInsights,
-      useClass: AppInsightsWrapper
+      useClass: AppInsightsWrapper,
     },
     {
       provide: ErrorHandler,
-      useClass: DefaultErrorHandler
+      useClass: DefaultErrorHandler,
     },
     AcceptTermsService,
     CaseShareService,
     { provide: FeatureToggleService, useClass: LaunchDarklyService },
-    TimeoutNotificationsService
+    TimeoutNotificationsService,
   ],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}

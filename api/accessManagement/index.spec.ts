675676e2a27743aa3c8e846f68f3de1fe45a4146
Surekha-HMCTS <surekha.athikesavan@hmcts.net>
2022-06-14 13:30:23 +0100

    fix

--- api/accessManagement/index.spec.ts
+++ api/accessManagement/index.spec.ts
@@ -3,38 +3,30 @@ import { expect } from 'chai'
 import 'mocha'
 import * as sinonChai from 'sinon-chai'
 chai.use(sinonChai)
-import { mockReq, mockRes } from 'sinon-express-mock'
+import { mockRes } from 'sinon-express-mock'
 import * as accessManagement from './index'
+import 'mocha';
+import * as sinon from 'sinon';
 
 describe('Judicial Booking', () => {
     let res;
     let req;
+    let spy: any;
+    let sandbox;
 
     beforeEach(() => {
-        res = mockRes()
-        req = mockReq({
-           params: {}
-        })
+      res = mockRes()
+      sandbox = sinon.createSandbox();
     })
 
     afterEach(() => {
+      sandbox.restore();
     })
 
     describe('access management', () => {
-        it('should return some test bookings', async () => {
-            req.body.userId = "21334a2b-79ce-44eb-9168-2d49a744be9c";
-            const response = await accessManagement.getBookings(req, res, null)
-            expect(response).to.not.equal(undefined)
-        })
-
-        it('should return a sample booking response', async () => {
-            const response = await accessManagement.createBooking(req, res, null)
-            expect(response).to.not.equal(undefined)
-        })
-
-        it('should return a role assignment success message response', async () => {
-            const response = await accessManagement.refreshRoleAssignments(req, res, null)
-            expect(response).to.not.equal(undefined)
-        })
+      it('should return a role assignment success message response', async () => {
+          const response = await accessManagement.refreshRoleAssignments(req, res, null)
+          expect(response).to.not.equal(undefined)
+      })
     })
 })

c2af374c2a5c1f1bb5d6f21b9e25f1bdfdba6f17
Kartheek-S <92437222+Kartheek-S@users.noreply.github.com>
2022-06-27 12:03:26 +0100

    Task e UI 6007 (#2102)
    * task/EUI-6007
    
    * yarn audit
    
    * lint fix
    
    * reorder with key
    
    * tests fix

--- src/hearings/containers/request-hearing/hearing-judge/hearing-judge.component.spec.ts
+++ src/hearings/containers/request-hearing/hearing-judge/hearing-judge.component.spec.ts
@@ -9,7 +9,7 @@ import { of } from 'rxjs';
 import { HearingJudgeNamesListComponent } from '../../../components';
 import { initialState } from '../../../hearing.test.data';
 import { ACTION, HearingJudgeSelectionEnum, MemberType, RadioOptions, RequirementType } from '../../../models/hearings.enum';
-import {JudicialUserModel} from '../../../models/judicialUser.model';
+import { JudicialUserModel } from '../../../models/judicialUser.model';
 import { LovRefDataModel } from '../../../models/lovRefData.model';
 import { HearingsService } from '../../../services/hearings.service';
 import { HearingJudgeComponent } from './hearing-judge.component';
@@ -23,7 +23,7 @@ describe('HearingJudgeComponent', () => {
   hearingsService.navigateAction$ = of(ACTION.CONTINUE);
   const judgeTypes: LovRefDataModel[] = [
     {
-      key: 'tj',
+      key: 'Tribunal',
       value_en: 'Tribunal Judge',
       value_cy: '',
       hint_text_en: 'Tribunal',

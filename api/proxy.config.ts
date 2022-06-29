284da79d60d7f93190d4e23202a8b90a89ee08a0
Sivaprakasam Sivaprakasam <>
2022-06-29 12:23:10 +0100

    EUI-5886- linked cases bottom table api integration

--- api/proxy.config.ts
+++ api/proxy.config.ts
@@ -16,7 +16,8 @@ import {
   SERVICES_REFUNDS_API_URL,
   SERVICES_LOCATION_REF_API_URL,
   SERVICES_ROLE_ASSIGNMENT_API_PATH,
-  SERVICES_COMMONRD_API_URL
+  SERVICES_COMMONRD_API_URL,
+  SERVICES_CCD_DATA_STORE_API_PATH
 } from './configuration/references';
 import { applyProxy } from './lib/middleware/proxy';
 import * as searchCases from './searchCases';
@@ -144,5 +145,10 @@ export const initProxy = (app: Express) => {
     rewrite: false,
     source: '/refdata/commondata/lov/categories/CaseLinkingReasonCode',
     target: getConfigValue(SERVICES_COMMONRD_API_URL),
+  });
+  applyProxy(app, {
+    rewrite: false,
+    source: '/getLinkedCases',
+    target: getConfigValue(SERVICES_CCD_DATA_STORE_API_PATH),
   });
 }

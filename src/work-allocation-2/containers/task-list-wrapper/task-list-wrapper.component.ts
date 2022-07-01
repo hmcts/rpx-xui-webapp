bf9a27a65b44ab447d8f3a89e1d27a43aa7e2815
Uday Denduluri <uday.denduluri@gmail.com>
2022-07-01 13:09:20 +0100

    get Task Names

--- src/work-allocation-2/containers/task-list-wrapper/task-list-wrapper.component.ts
+++ src/work-allocation-2/containers/task-list-wrapper/task-list-wrapper.component.ts
@@ -147,7 +147,6 @@ export class TaskListWrapperComponent implements OnDestroy, OnInit {
   }
 
   public ngOnInit(): void {
-    this.taskService.getTaskNames().subscribe(x => console.log(x));
     // get supported jurisdictions on initialisation in order to get caseworkers by these services
     this.waSupportedJurisdictions$ = this.waSupportedJurisdictionsService.getWASupportedJurisdictions();
 

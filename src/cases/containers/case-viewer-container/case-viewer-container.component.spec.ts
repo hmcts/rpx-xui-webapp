a6632d9a92529e3f972aee625ffe13098de6695a
connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>
2022-07-04 16:04:11 +0100

    Update case-viewer-container.component.spec.ts

--- src/cases/containers/case-viewer-container/case-viewer-container.component.spec.ts
+++ src/cases/containers/case-viewer-container/case-viewer-container.component.spec.ts
@@ -130,6 +130,8 @@ describe('CaseViewerContainerComponent', () => {
     ]
   };
 
+  const mockSupportedJurisdictionsService = jasmine.createSpyObj('WASupportedJurisdictionsService', ['getWASupportedJurisdictions']);
+
   class MockFeatureToggleService implements FeatureToggleService {
     public getValue<R>(_key: string, _defaultValue: R): Observable<R> {
       // @ts-ignore
@@ -183,7 +185,6 @@ describe('CaseViewerContainerComponent', () => {
             'payments',
             'payments-refund',
             'payments-refund-approver',
-            'pui-case-manager',
             'pui-finance-manager',
             'pui-organisation-manager',
             'pui-user-manager'
@@ -226,7 +227,7 @@ describe('CaseViewerContainerComponent', () => {
           }
         },
         {provide: FeatureToggleService, useClass: MockFeatureToggleService},
-        {provide: WASupportedJurisdictionsService, useClass: }
+        {provide: WASupportedJurisdictionsService, useValue: mockSupportedJurisdictionsService}
       ],
       declarations: [CaseViewerContainerComponent, CaseViewerComponent]
     })
@@ -235,6 +236,7 @@ describe('CaseViewerContainerComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CaseViewerContainerComponent);
+    mockSupportedJurisdictionsService.getWASupportedJurisdictions.and.returnValue(of(['IA', 'SSCS']));
     component = fixture.componentInstance;
     debug = fixture.debugElement;
     fixture.detectChanges();

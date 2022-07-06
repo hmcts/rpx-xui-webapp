d326656301d8cb7f4819e7da2ec518dbc2e9e386
maliumer <Muhammad.Umer@hmcts.net>
2022-07-06 16:17:13 +0100

    EUI-6235
    Choose a date Range text should be shown in CYA page and view details page

--- src/hearings/converters/hearing-specific-date.answer.converter.ts
+++ src/hearings/converters/hearing-specific-date.answer.converter.ts
@@ -4,49 +4,35 @@ import {map} from 'rxjs/operators';
 import {HearingDateEnum, RadioOptions} from '../models/hearings.enum';
 import {State} from '../store';
 import {AnswerConverter} from './answer.converter';
-​
+
 export class HearingSpecificDateAnswerConverter implements AnswerConverter {
-​
+
   public transformAnswer(hearingState$: Observable<State>): Observable<string> {
     return hearingState$.pipe(
       map(state => this.createAnswer(state))
     );
   }
-​
+
   private createAnswer(state: State): string {
     let specificDateSelection: string = '';
     let earliestHearingDate: string = '';
     let latestHearingDate: string = '';
-    const hrmMainModel = state.hearingRequest.hearingRequestMainModel;
-    const hearingWindow = hrmMainModel.hearingDetails.hearingWindow;
-    switch (hrmMainModel.hearingDetails.hearingType.toLowerCase()) {
-      case 'yes':
-        if (hearingWindow.dateRangeStart && !hearingWindow.dateRangeEnd) {
-          specificDateSelection = RadioOptions.YES;
-          earliestHearingDate = moment(hearingWindow.dateRangeStart).format(HearingDateEnum.DisplayMonth);
-          specificDateSelection += earliestHearingDate !== HearingDateEnum.InvalidDate ? `<dt class="heading-h3 bottom-0">Earliest hearing date</dt>${earliestHearingDate}` : '';
-        } else if (!hearingWindow.dateRangeStart && hearingWindow.dateRangeEnd) {
-          specificDateSelection = RadioOptions.YES;
-          latestHearingDate = moment(hearingWindow.dateRangeEnd).format(HearingDateEnum.DisplayMonth);
-          specificDateSelection += latestHearingDate !== HearingDateEnum.InvalidDate ? `<dt class="heading-h3 bottom-0">Latest hearing date</dt>${latestHearingDate}` : '';
-        } else if (hearingWindow.firstDateTimeMustBe) {
-          specificDateSelection = RadioOptions.YES;
-          const firstDate = moment(hearingWindow.firstDateTimeMustBe).format(HearingDateEnum.DisplayMonth);
-          specificDateSelection += firstDate !== HearingDateEnum.InvalidDate ? `<dt class="heading-h3 bottom-0">The first date of the hearing must be</dt>${firstDate}` : '';
-        }
-        break;
-      case 'choose a date range':
-        specificDateSelection = RadioOptions.CHOOSE_DATE_RANGE;
-        earliestHearingDate = moment(hearingWindow.dateRangeStart).format(HearingDateEnum.DisplayMonth);
-        latestHearingDate = moment(hearingWindow.dateRangeEnd).format(HearingDateEnum.DisplayMonth);
-        specificDateSelection += earliestHearingDate !== HearingDateEnum.InvalidDate ? `<dt class="heading-h3 bottom-0">Earliest hearing date</dt>${earliestHearingDate}` : '';
-        specificDateSelection += latestHearingDate !== HearingDateEnum.InvalidDate ? `<dt class="heading-h3 bottom-0">Latest hearing date</dt>${latestHearingDate}` : '';
-        break;
-      default:
-        specificDateSelection = RadioOptions.NO;
-        break;
+    const hearingWindow = state.hearingRequest.hearingRequestMainModel.hearingDetails.hearingWindow;
+
+    if (hearingWindow && (hearingWindow.dateRangeStart || hearingWindow.dateRangeEnd)) {
+      specificDateSelection = RadioOptions.CHOOSE_DATE_RANGE;
+      earliestHearingDate = moment(hearingWindow.dateRangeStart).format(HearingDateEnum.DisplayMonth);
+      latestHearingDate = moment(hearingWindow.dateRangeEnd).format(HearingDateEnum.DisplayMonth);
+      specificDateSelection += earliestHearingDate !== HearingDateEnum.InvalidDate ? `<dt class="heading-h3 bottom-0">Earliest hearing date</dt>${earliestHearingDate}` : '';
+      specificDateSelection += latestHearingDate !== HearingDateEnum.InvalidDate ? `<dt class="heading-h3 bottom-0">Latest hearing date</dt>${latestHearingDate}` : '';
+    } else if (hearingWindow && hearingWindow.firstDateTimeMustBe) {
+      specificDateSelection = RadioOptions.YES;
+      earliestHearingDate = moment(hearingWindow.dateRangeStart).format(HearingDateEnum.DisplayMonth);
+      specificDateSelection += earliestHearingDate !== HearingDateEnum.InvalidDate ? `<dt class="heading-h3 bottom-0">The first date of the hearing must be</dt>${earliestHearingDate}` : '';
+    } else if (hearingWindow) {
+      specificDateSelection = RadioOptions.NO;
     }
+
     return specificDateSelection;
   }
-​
 }

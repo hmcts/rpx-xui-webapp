29d3370ec34740054feaefcc704b70d8549364d3
John Benjamin <john.benjamin@hmcts.net>
2022-07-11 12:47:40 +0100

    EUI-6094

--- src/hearings/converters/hearing-response-length.answer.converter.spec.ts
+++ src/hearings/converters/hearing-response-length.answer.converter.spec.ts
@@ -67,6 +67,16 @@ describe('HearingResponseLengthAnswerConverter', () => {
     expect(result$).toBeObservable(expected);
   });
 
+  it('should transform hearing stage both days and hours', () => {
+    const STATE: State = _.cloneDeep(initialState.hearings);
+    STATE.hearingRequest.hearingRequestMainModel.hearingResponse.hearingDaySchedule[0].hearingStartDateTime = '2022-07-11T09:00:00.000Z';
+    STATE.hearingRequest.hearingRequestMainModel.hearingResponse.hearingDaySchedule[0].hearingEndDateTime = '2022-07-14T11:00:00.000Z';
+    const result$ = converter.transformAnswer(of(STATE), 0);
+    const hearingDuration = '12 Days 2 Hours';
+    const expected = cold('(b|)', {b: hearingDuration});
+    expect(result$).toBeObservable(expected);
+  });
+
   it('should transform hearing stage both hours and minutes', () => {
     const STATE: State = _.cloneDeep(initialState.hearings);
     STATE.hearingRequest.hearingRequestMainModel.hearingResponse.hearingDaySchedule[0].hearingStartDateTime = '2021-03-12T09:00:00.000Z';

2a9759cad95eab8af5e95933427064e7747ff9a8
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2022-07-12 16:04:09 +0100

    Add in serviceRefData calls

--- src/work-allocation-2/work-allocation2.module.ts
+++ src/work-allocation-2/work-allocation2.module.ts
@@ -13,7 +13,7 @@ import { PriorityFieldComponentModule } from './components/priority-field/priori
 import { WorkAllocationComponentsModule } from './components/work-allocation.components.module';
 import * as fromContainers from './containers';
 import { WorkAllocationFeatureToggleGuard } from './guards';
-import { CaseworkerDataService, WASupportedJurisdictionsService, WorkAllocationFeatureService, WorkAllocationTaskService } from './services';
+import { CaseworkerDataService, ServiceRefDataService, WASupportedJurisdictionsService, WorkAllocationFeatureService, WorkAllocationTaskService } from './services';
 import { workAllocationRouting } from './work-allocation-feature.routes';
 
 // from containers
@@ -40,7 +40,8 @@ import { workAllocationRouting } from './work-allocation-feature.routes';
     AlertService,
     CaseworkerDataService,
     WorkAllocationFeatureService,
-    WASupportedJurisdictionsService
+    WASupportedJurisdictionsService,
+    ServiceRefDataService
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })

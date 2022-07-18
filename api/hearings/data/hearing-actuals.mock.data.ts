fe03bb02519c3013842158eb204b7b0d6417aead
maliumer <Muhammad.Umer@hmcts.net>
2022-07-18 14:34:55 +0100

    Update hearing-actuals.mock.data.ts
    not required was missing in hearing actual mock data.ts

--- api/hearings/data/hearing-actuals.mock.data.ts
+++ api/hearings/data/hearing-actuals.mock.data.ts
@@ -211,6 +211,7 @@ export const HEARING_ACTUAL_COMPLETED: HearingActualsMainModel = {
         hearingDate: '2021-03-12',
         hearingStartTime: '2021-03-12T09:00:00.000Z',
         hearingEndTime: '2021-03-13T10:00:00.000Z',
+        notRequired: false,
         pauseDateTimes: [{
           pauseStartTime: '2021-03-12T10:10:00.000Z',
           pauseEndTime: '2021-03-12T11:15:00.000Z',

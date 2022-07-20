95485bb88023e5ecc2a043b9b44bcd3816c53e84
Matei Baciu <matei.baciu@hmcts.net>
2022-07-20 15:40:25 +0100

    moved test to hearing-actual-summary-single-day.component from previous component

--- src/hearings/components/hearing-actual-summary-single-day/hearing-actual-summary-single-day.component.spec.ts
+++ src/hearings/components/hearing-actual-summary-single-day/hearing-actual-summary-single-day.component.spec.ts
@@ -1,25 +1,214 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HearingActualSummarySingleDayComponent } from './hearing-actual-summary-single-day.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
+import { LovRefDataModel } from '../../models/lovRefData.model';
+import { hearingActualsMainModel } from '../../hearing.test.data';
+
 
 describe('HearingActualSummarySingleDayComponent', () => {
   let component: HearingActualSummarySingleDayComponent;
   let fixture: ComponentFixture<HearingActualSummarySingleDayComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HearingActualSummarySingleDayComponent ]
-    })
-    .compileComponents();
-  }));
+  let router: Router;
+  const partyChannels: LovRefDataModel[] = [
+    {
+      key: 'inPerson',
+      value_en: 'In person',
+      value_cy: '',
+      hint_text_en: 'in person',
+      hint_text_cy: 'Wyneb yn wyneb',
+      lov_order: 1,
+      parent_key: null,
+      category_key: 'HearingChannel',
+      parent_category: '',
+      active_flag: 'Y',
+      child_nodes: null,
+    },
+    {
+      key: 'byPhone',
+      value_en: 'By phone',
+      value_cy: '',
+      hint_text_en: 'By Phone',
+      hint_text_cy: 'Ffôn',
+      lov_order: 2,
+      parent_key: null,
+      category_key: 'HearingChannel',
+      parent_category: '',
+      active_flag: 'Y',
+      child_nodes: [
+        {
+          key: 'telephone-btMeetMe',
+          value_en: 'Telephone - BTMeetme',
+          value_cy: '',
+          hint_text_en: 'By Phone bTMeetme',
+          hint_text_cy: '',
+          lov_order: 1,
+          parent_key: 'byPhone',
+          category_key: 'HearingChannel',
+          parent_category: 'byPhone',
+          active_flag: 'Y',
+          child_nodes: null,
+        },
+        {
+          key: 'telephone-CVP',
+          value_en: 'Telephone - CVP',
+          value_cy: '',
+          hint_text_en: 'By Phone CVP',
+          hint_text_cy: '',
+          lov_order: 2,
+          parent_key: 'byPhone',
+          category_key: 'HearingChannel',
+          parent_category: 'byPhone',
+          active_flag: 'Y',
+          child_nodes: null,
+        },
+        {
+          key: 'telephone-other',
+          value_en: 'Telephone - Other',
+          value_cy: '',
+          hint_text_en: 'By Phone Other',
+          hint_text_cy: '',
+          lov_order: 3,
+          parent_key: 'byPhone',
+          category_key: 'HearingChannel',
+          parent_category: 'byPhone',
+          active_flag: 'Y',
+          child_nodes: null,
+        },
+        {
+          key: 'telephone-skype',
+          value_en: 'Telephone - Skype',
+          value_cy: '',
+          hint_text_en: 'By Phone Skype',
+          hint_text_cy: '',
+          lov_order: 4,
+          parent_key: 'byPhone',
+          category_key: 'HearingChannel',
+          parent_category: 'byPhone',
+          active_flag: 'Y',
+          child_nodes: null,
+        },
+      ],
+    },
+    {
+      key: 'byVideo',
+      value_en: 'By video',
+      value_cy: 'Fideo',
+      hint_text_en: 'By video',
+      hint_text_cy: '',
+      lov_order: 4,
+      parent_key: null,
+      category_key: 'HearingChannel',
+      parent_category: '',
+      active_flag: 'Y',
+      child_nodes: [
+        {
+          key: 'video-conference',
+          value_en: 'Video Conference',
+          value_cy: '',
+          hint_text_en: 'By video conference',
+          hint_text_cy: '',
+          lov_order: 4,
+          parent_key: 'byVideo',
+          category_key: 'HearingChannel',
+          parent_category: 'byVideo',
+          active_flag: 'Y',
+          child_nodes: null,
+        },
+        {
+          key: 'video-other',
+          value_en: 'Video - Other',
+          value_cy: '',
+          hint_text_en: 'By video other',
+          hint_text_cy: '',
+          lov_order: 4,
+          parent_key: 'byVideo',
+          category_key: 'HearingChannel',
+          parent_category: 'byVideo',
+          active_flag: 'Y',
+          child_nodes: null,
+        },
+        {
+          key: 'video-skype',
+          value_en: 'Video - Skype',
+          value_cy: '',
+          hint_text_en: 'By video skype',
+          hint_text_cy: '',
+          lov_order: 4,
+          parent_key: 'byVideo',
+          category_key: 'HearingChannel',
+          parent_category: 'byVideo',
+          active_flag: 'Y',
+          child_nodes: null,
+        },
+        {
+          key: 'video-teams',
+          value_en: 'Video - Teams',
+          value_cy: '',
+          hint_text_en: 'By video teams',
+          hint_text_cy: '',
+          lov_order: 4,
+          parent_key: 'byVideo',
+          category_key: 'HearingChannel',
+          parent_category: 'byVideo',
+          active_flag: 'Y',
+          child_nodes: null,
+        },
+      ],
+    },
+    {
+      key: 'notAttending',
+      value_en: 'Not attending',
+      value_cy: '',
+      hint_text_en: 'not attending',
+      hint_text_cy: '',
+      lov_order: 5,
+      parent_key: null,
+      category_key: 'HearingChannel',
+      parent_category: '',
+      active_flag: 'Y',
+      child_nodes: null,
+    },
+  ];
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [HearingActualSummarySingleDayComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                partyChannels
+              }
+            },
+            fragment: of('point-to-me'),
+          }
+        },
+      ],
+    })
+      .compileComponents();
     fixture = TestBed.createComponent(HearingActualSummarySingleDayComponent);
     component = fixture.componentInstance;
+    component.actualHearingDay = hearingActualsMainModel.hearingActuals.actualHearingDays[0];
+    component.plannedHearingDay = hearingActualsMainModel.hearingPlanned.plannedHearingDays[0];
+    router = TestBed.get(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should check getChannelInfo', () => {
+    expect(component.getChannelInfo('inPerson')).toEqual({ channel: 'In person', subChannel: '' });
+    expect(component.getChannelInfo('video-teams')).toEqual({ channel: 'By video', subChannel: 'Video - Teams' });
+  });
+
+  afterEach(() => {
+    fixture.destroy();
   });
 });

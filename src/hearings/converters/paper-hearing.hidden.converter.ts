3fec8ea8ffb32e87763fe1c44c8fd9e4c96aeae8
Kartheek-S <92437222+Kartheek-S@users.noreply.github.com>
2022-07-20 21:34:43 +0100

    story/EUI-6018

---
+++ src/hearings/converters/paper-hearing.hidden.converter.ts
@@ -0,0 +1,15 @@
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HearingChannelEnum } from '../models/hearings.enum';
+import { State } from '../store';
+import { HiddenConverter } from './hidden.converter';
+
+export class PaperHearingHiddenConverter implements HiddenConverter {
+
+  public transformHidden(hearingState$: Observable<State>): Observable<boolean> {
+    return hearingState$.pipe(map(state => {
+      return state.hearingRequest.hearingRequestMainModel.hearingDetails.hearingChannels.includes(HearingChannelEnum.ONPPR);
+    }
+    ));
+  }
+}

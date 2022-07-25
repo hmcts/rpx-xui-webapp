b2717accee5064430bd17b13fca9772a389eb171
tarik.dogan <tarik.dogan@hmcts.net>
2022-07-25 16:58:27 +0100

    work access page implimented

--- src/app/components/routing/application-routing.component.spec.ts
+++ src/app/components/routing/application-routing.component.spec.ts
@@ -1,17 +1,25 @@
 import { of } from 'rxjs';
 import { ApplicationRoutingComponent } from './application-routing.component';
+import {FeatureToggleService} from '@hmcts/rpx-xui-common-lib';
+import { TestBed } from '@angular/core/testing';
 
-describe('ApplicationRoutingComponent', () => {
+fdescribe('ApplicationRoutingComponent', () => {
   let component: ApplicationRoutingComponent;
   let router;
   let waFeatureService;
   let mockStore;
-
+  const featureToggleMock = jasmine.createSpyObj('featureToggleService', ['isEnabled', 'getValueOnce']);
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: FeatureToggleService, useValue: featureToggleMock }
+      ]
+    }).compileComponents();
     router = jasmine.createSpyObj('router', ['navigate']);
     waFeatureService = jasmine.createSpyObj('service', ['getActiveWAFeature']);
     mockStore = jasmine.createSpyObj('store', ['pipe']);
-    component = new ApplicationRoutingComponent(router, waFeatureService, mockStore);
+    featureToggleMock.isEnabled.and.returnValue(of(true));
+    component = new ApplicationRoutingComponent(router, waFeatureService, mockStore, featureToggleMock);
   });
 
   it('should create', () => {
@@ -23,19 +31,37 @@ describe('ApplicationRoutingComponent', () => {
     expect(router.navigate).toHaveBeenCalledWith([ApplicationRoutingComponent.defaultPage]);
   });
 
-  it('should navigateUrlBasedOnFeatureToggle WorkAllocationRelease2', () => {
+  it('should navigateUrlBasedOnFeatureToggle booking Url', async () => {
+    featureToggleMock.getValueOnce.and.returnValue(of(true));
+    mockStore.pipe.and.returnValue( of( { userInfo: { roles: ['caseworker-ia-iacjudge'], roleCategory: 'JUDICIAL' },
+    roleAssignmentInfo: [
+      {
+          bookable: true,
+          substantive: 'N',
+          jurisdiction: 'IA',
+          isCaseAllocator: true
+      }
+    ]}));
+    component.navigateUrlBasedOnFeatureToggle('WorkAllocationRelease2');
+    expect(router.navigate).toHaveBeenCalledWith([ApplicationRoutingComponent.bookingUrl]);
+  });
+
+  it('should navigateUrlBasedOnFeatureToggle WorkAllocationRelease2', async () => {
+    featureToggleMock.getValueOnce.and.returnValue(of(true));
     mockStore.pipe.and.returnValue(of({userInfo: {roles: ['caseworker-ia-iacjudge']}}));
     component.navigateUrlBasedOnFeatureToggle('WorkAllocationRelease2');
     expect(router.navigate).toHaveBeenCalledWith([ApplicationRoutingComponent.defaultWAPage]);
   });
 
-  it('should navigateBasedOnUserRole caseworker-ia-iacjudge', () => {
+  it('should navigateBasedOnUserRole caseworker-ia-iacjudge', async () => {
+    featureToggleMock.getValueOnce.and.returnValue(of(true));
     mockStore.pipe.and.returnValue(of({userInfo: {roles: ['caseworker-ia-iacjudge']}}));
     component.navigateBasedOnUserRole();
     expect(router.navigate).toHaveBeenCalledWith([ApplicationRoutingComponent.defaultWAPage]);
   });
 
-  it('should navigateBasedOnUserRole caseworker-ia-caseofficer', () => {
+  it('should navigateBasedOnUserRole caseworker-ia-caseofficer', async () => {
+    featureToggleMock.getValueOnce.and.returnValue(of(true));
     mockStore.pipe.and.returnValue(of({userInfo: {roles: ['caseworker-ia-caseofficer']}}));
     component.navigateBasedOnUserRole();
     expect(router.navigate).toHaveBeenCalledWith([ApplicationRoutingComponent.defaultWAPage]);
--- src/app/components/routing/application-routing.component.ts
+++ src/app/components/routing/application-routing.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
+import { combineLatest, Observable } from 'rxjs';
+import {AppConstants} from '../../../app/app.constants';
 import { WorkAllocationFeatureService } from '../../../work-allocation-2/services';
 import * as fromActions from '../../store';
+import {FeatureToggleService, RoleCategory} from '@hmcts/rpx-xui-common-lib';
+import { map } from 'rxjs/operators';
 
 @Component({ templateUrl: './application-routing.component.html'})
 export class ApplicationRoutingComponent implements OnInit {
   constructor(
     private readonly router: Router,
     private readonly workAllocationFeatureService: WorkAllocationFeatureService,
-    private readonly store: Store<fromActions.State>
+    private readonly store: Store<fromActions.State>,
+    private readonly featureToggleService: FeatureToggleService
   ) {}
   public static defaultWAPage = '/work/my-work/list';
   public static defaultPage = '/cases';
+  public static bookingUrl: string = '../booking';
   public ngOnInit(): void {
     this.workAllocationFeatureService.getActiveWAFeature().subscribe((currentWAFeatureName) =>
         this.navigateUrlBasedOnFeatureToggle(currentWAFeatureName)
@@ -27,13 +33,22 @@ export class ApplicationRoutingComponent implements OnInit {
 
   public navigateBasedOnUserRole() {
     const userDetails$ = this.store.pipe(select(fromActions.getUserDetails));
-    userDetails$.subscribe(userDetails => {
+    const bookingFeatureToggle$: Observable<boolean> = this.featureToggleService.getValueOnce(AppConstants.FEATURE_NAMES.booking, false);
+    const userAccess$ = combineLatest([userDetails$, bookingFeatureToggle$]);
+
+    userAccess$.pipe(map(([userDetails, bookingFeatureToggle]) => {
+      const { roleAssignmentInfo, userInfo } = userDetails;
+      const isBookableAndJudicialRole = userInfo.roleCategory === RoleCategory.JUDICIAL && roleAssignmentInfo.some( roleAssignment => 'bookable' in roleAssignment && roleAssignment.bookable === true );
+      if (bookingFeatureToggle && isBookableAndJudicialRole) {
+        return this.router.navigate([ApplicationRoutingComponent.bookingUrl]);
+      }
       userDetails && userDetails.userInfo && userDetails.userInfo.roles &&
       (userDetails.userInfo.roles.includes('caseworker-ia-iacjudge')
       || userDetails.userInfo.roles.includes('caseworker-ia-caseofficer')
       || userDetails.userInfo.roles.includes('caseworker-ia-admofficer'))
       ? this.router.navigate([ApplicationRoutingComponent.defaultWAPage]) : this.router.navigate([ApplicationRoutingComponent.defaultPage]);
-    });
+    })).subscribe();
+
   }
 
 }

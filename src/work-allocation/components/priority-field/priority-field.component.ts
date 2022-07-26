9e90635b2e5939a592b78c80931a0a9d1def48d0
DavidJayakumar <David.Jayakumar@hmcts.net>
2022-07-26 15:52:28 +0100

    Fix the scenario that failed in test

--- src/work-allocation/components/priority-field/priority-field.component.ts
+++ src/work-allocation/components/priority-field/priority-field.component.ts
@@ -36,8 +36,8 @@ export class PriorityFieldComponent {
     if (!(d1 && d2)) {
       return 0;
     }
-    const utc1 = Date.UTC(d1.getFullYear(), d1.getMonth(), d1.getDay());
-    const utc2 = Date.UTC(d2.getFullYear(), d1.getMonth(), d1.getDay());
-    return Math.round((utc2 - utc1) / 1000 * 60 * 60 * 24);
+    const utc1 = Date.UTC(d1.getFullYear(), d1.getMonth(), d1.getDate());
+    const utc2 = Date.UTC(d2.getFullYear(), d2.getMonth(), d2.getDate());
+    return Math.floor((utc1 - utc2) / (1000 * 60 * 60 * 24));
   }
 }

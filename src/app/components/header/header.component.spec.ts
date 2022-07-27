0727e103da20a8b5e061eff32939d9c43abf59b8
John Benjamin <john.benjamin@hmcts.net>
2022-07-27 19:52:52 +0100

    Merge WA in to GS

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -9,51 +9,51 @@ import { HmctsGlobalHeaderComponent } from '../hmcts-global-header/hmcts-global-
 import { HeaderComponent } from './header.component';
 
 describe('Header Component', () => {
-    let mockStore: any;
-    let mockService: any;
-    let component: HeaderComponent;
-    let fixture: ComponentFixture<HeaderComponent>;
+  let mockStore: any;
+  let mockService: any;
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
 
-    beforeEach(async(() => {
-      TestBed.configureTestingModule({
-        declarations: [HeaderComponent, HmctsGlobalHeaderComponent, PhaseBannerComponent],
-        imports: [HttpClientModule, RouterTestingModule],
-        schemas: [CUSTOM_ELEMENTS_SCHEMA],
-        providers: [{ provide: Store, useValue: mockStore }]
-      }).compileComponents();
-    }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeaderComponent, HmctsGlobalHeaderComponent, PhaseBannerComponent],
+      imports: [HttpClientModule, RouterTestingModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [{ provide: Store, useValue: mockStore }]
+    }).compileComponents();
+  }));
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(HeaderComponent);
-        mockStore = jasmine.createSpyObj('store', ['pipe']);
-        mockService = jasmine.createSpyObj('service', ['get']);
-        component = new HeaderComponent(mockStore);
-    });
+  beforeEach(() => {
+      fixture = TestBed.createComponent(HeaderComponent);
+      mockStore = jasmine.createSpyObj('store', ['pipe']);
+      mockService = jasmine.createSpyObj('service', ['get']);
+      component = new HeaderComponent(mockStore);
+  });
 
-    it('should create', () => {
+  it('should create', () => {
 
-        expect(component).toBeTruthy();
-    });
+      expect(component).toBeTruthy();
+  });
 
-    it('should render the skip to content link', () => {
-      const element = fixture.debugElement.query(By.css('.govuk-skip-link')).nativeElement;
-      expect(element.innerHTML).toEqual('Skip to main content');
-    });
+  it('should render the skip to content link', () => {
+    const element = fixture.debugElement.query(By.css('.govuk-skip-link')).nativeElement;
+    expect(element.innerHTML).toEqual('Skip to main content');
+  });
 
-    it('should call emitNavigate with event and this.navigate', () => {
+  it('should call emitNavigate with event and this.navigate', () => {
+
+    const event = {};
+    spyOn(component, 'emitNavigate');
+
+    component.onNavigate(event);
+    expect(component.emitNavigate).toHaveBeenCalled();
+  });
+
+  it('should emitNavigate', () => {
 
       const event = {};
-      spyOn(component, 'emitNavigate');
-
-      component.onNavigate(event);
-      expect(component.emitNavigate).toHaveBeenCalled();
-    });
-
-    it('should emitNavigate', () => {
-
-        const event = {};
-        const emitter = jasmine.createSpyObj('emitter', ['emit']);
-        component.emitNavigate(event, emitter);
-        expect(emitter.emit).toHaveBeenCalled();
-    });
+      const emitter = jasmine.createSpyObj('emitter', ['emit']);
+      component.emitNavigate(event, emitter);
+      expect(emitter.emit).toHaveBeenCalled();
+  });
 });

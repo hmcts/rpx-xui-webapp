50738c724d891823b7f4bfde1eddfe916c22f8f4
Uday Denduluri <uday.denduluri@gmail.com>
2022-07-29 11:45:44 +0100

    location controller court_venues fix

--- api/workAllocation2/locationService.ts
+++ api/workAllocation2/locationService.ts
@@ -2,10 +2,9 @@ import { AxiosResponse } from 'axios';
 import { http } from '../lib/http';
 import { EnhancedRequest } from '../lib/models';
 import { setHeaders } from '../lib/proxy';
-import { CourtVenue } from './interfaces/location';
 
-export async function handleLocationGet(fullPath: string, req: EnhancedRequest): Promise<AxiosResponse<CourtVenue[]>> {
+export async function handleLocationGet(fullPath: string, req: EnhancedRequest): Promise<AxiosResponse<any>> {
   const headers = setHeaders(req);
-  const response = await http.get<CourtVenue[]>(fullPath, {headers});
+  const response = await http.get<any>(fullPath, {headers});
   return response;
 }

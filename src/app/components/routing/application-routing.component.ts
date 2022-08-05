e0f504ba5803d9591553e824e01ec9cea36bfbae
tarik.dogan <tarik.dogan@hmcts.net>
2022-08-05 14:50:16 +0100

    EUI-6536 lint fix

--- src/app/components/routing/application-routing.component.ts
+++ src/app/components/routing/application-routing.component.ts
@@ -28,7 +28,7 @@ export class ApplicationRoutingComponent implements OnInit {
   public navigateBasedOnUserRole() {
     const userDetails$ = this.store.pipe(select(fromActions.getUserDetails));
     userDetails$.subscribe(userDetails => {
-      if(this.router.url !== '/' ) {
+      if (this.router.url !== '/' ) {
         return;
       }
       userDetails && userDetails.userInfo && userDetails.userInfo.roles &&

d82dba0e30be82957d54fbca104bee49412c47a7
tarik.dogan <tarik.dogan@hmcts.net>
2022-08-15 15:04:23 +0100

    EUI-6473 code finetune and updating branch

--- src/app/resolvers/case-tasks-resolver.service.spec.ts
+++ src/app/resolvers/case-tasks-resolver.service.spec.ts
@@ -5,7 +5,7 @@ import { TestBed } from '@angular/core/testing';
 import { ActivatedRouteSnapshot } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
-import { TaskList } from '../../work-allocation-2/models/dtos';
+import { TaskList } from '../../work-allocation/models/dtos';
 
 import { CaseTasksResolverService } from './case-tasks-resolver.service';
 

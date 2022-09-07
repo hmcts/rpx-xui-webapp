c9fb3a18c11c5844ad80edd45dbf4a04120ecfb3
MateiHMCTS <matei.baciu@hmcts.net>
2022-09-07 09:56:40 +0100

    added filter-config component

--- src/staff-administrator/components/staff-search/staff-search.component.ts
+++ src/staff-administrator/components/staff-search/staff-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FilterConfig } from '@hmcts/rpx-xui-common-lib/lib/models';
 
 @Component({
   selector: 'exui-staff-search',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./staff-search.component.scss']
 })
 export class StaffSearchComponent implements OnInit {
-
+  public filterConfig: FilterConfig = {
+    id: 'staff-filters',
+    fields: [{
+      name: 'user-type',
+      title: 'Search for a user by name',
+      options: [],
+      minSelected: 0,
+      maxSelected: 0,
+      type: 'find-person'
+    }],
+    persistence: 'session',
+    applyButtonText: 'Search',
+    cancelButtonText: '',
+    enableDisabledButton: false,
+    showCancelFilterButton: false
+  };
   constructor() { }
 
   public ngOnInit() {
   }
-
 }

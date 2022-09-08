0fd75a35d5dcbbd2e592d8299892f2beb838598a
DavidJayakumar <David.Jayakumar@hmcts.net>
2022-09-08 14:58:12 +0100

    Fix unit test issues

--- src/staff-administrator/components/staff-search/staff-search.component.spec.ts
+++ src/staff-administrator/components/staff-search/staff-search.component.spec.ts
@@ -22,7 +22,6 @@ describe('StaffSearchComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(StaffSearchComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {

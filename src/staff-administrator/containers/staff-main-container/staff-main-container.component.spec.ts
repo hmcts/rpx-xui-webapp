449fef6f20e9c61e552a8e8a4e11e752a94e00a9
Matei Baciu <matei.baciu@hmcts.net>
2022-09-13 13:25:09 +0100

    fix and add tests

--- src/staff-administrator/containers/staff-main-container/staff-main-container.component.spec.ts
+++ src/staff-administrator/containers/staff-main-container/staff-main-container.component.spec.ts
@@ -4,11 +4,14 @@ import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ExuiCommonLibModule } from '@hmcts/rpx-xui-common-lib';
-import { SharedModule } from '../../../app/shared/shared.module';
+import { ErrorMessageComponent } from '../../../app/components';
 import { StaffAdvFilterComponent } from '../../components/staff-adv-filter/staff-adv-filter.component';
 import { StaffSearchComponent } from '../../components/staff-search/staff-search.component';
 import { StaffUserListComponent } from '../../components/staff-user-list/staff-user-list.component';
+import { StaffDataAccessService } from '../../services/staff-data-access.service';
+import { StaffDataFilterService } from '../../services/staff-data-filter.service';
 import { staffFilterOptionsTestData } from '../../test-data/staff-filter-options.test.data';
 import { StaffMainContainerComponent } from './staff-main-container.component';
 
@@ -32,16 +35,20 @@ describe('StaffMainContainerComponent', () => {
         FooterStubComponent,
         StaffSearchComponent,
         StaffUserListComponent,
-        StaffAdvFilterComponent
+        StaffAdvFilterComponent,
+        StaffUserListComponent,
+        ErrorMessageComponent
       ],
       imports: [
+        RouterTestingModule,
         HttpClientTestingModule,
         ExuiCommonLibModule,
         CdkTableModule,
         ReactiveFormsModule,
-        SharedModule
       ],
       providers: [
+        StaffDataFilterService,
+        StaffDataAccessService,
         {
           provide: ActivatedRoute,
           useValue: {

66393c4035b11f3c127e6dad564cc99758994663
Matei Baciu <matei.baciu@hmcts.net>
2022-09-13 11:38:52 +0100

    update rpx-xui-common-lib

--- src/work-allocation/components/case-manager-filter/case-manager-filter.component.ts
+++ src/work-allocation/components/case-manager-filter/case-manager-filter.component.ts
@@ -83,7 +83,7 @@ export class CaseManagerFilterComponent implements OnInit, OnDestroy {
       enableCondition: 'selectLocation=search',
       minSelectedError: 'You must select a location',
       maxSelectedError: null,
-      enableAddLocationButton: false,
+      enableAddButton: false,
       type: 'find-location',
       radioSelectionChange: 'selectLocation=search'
     };

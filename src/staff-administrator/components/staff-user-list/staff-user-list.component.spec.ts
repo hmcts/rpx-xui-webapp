5502e8708f8258c2356465769685e09f3ac0ed73
Matei Baciu <matei.baciu@hmcts.net>
2022-09-14 14:06:39 +0100

    fix rebase mistake

--- src/staff-administrator/components/staff-user-list/staff-user-list.component.spec.ts
+++ src/staff-administrator/components/staff-user-list/staff-user-list.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CdkTableModule } from '@angular/cdk/table';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { StaffDataAccessService } from '../../services/staff-data-access.service';
+import { StaffDataAccessService } from '../../services/staff-data-access/staff-data-access.service';
 import { StaffDataFilterService } from '../../services/staff-data-filter.service';
 import { StaffUserListComponent } from './staff-user-list.component';
 

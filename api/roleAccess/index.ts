59fbbd4621e1651ea3dac12f7d83df6532c99f9c
connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>
2022-10-13 16:09:27 +0100

    Bug/e UI 6829 case access reason (#2368)
    * Error handling and slight cleaning of comments
    
    * Fix lint and test issues
    
    * Add specific reason toolkit changes
    
    * Update index.ts
    
    * Change to working tag

--- api/roleAccess/index.ts
+++ api/roleAccess/index.ts
@@ -162,8 +162,8 @@ export function mapResponseToCaseRoles(
     roleName: roleAssignment.roleName,
     start: roleAssignment.beginTime ? roleAssignment.beginTime.toString() : null,
     created: roleAssignment.created ? roleAssignment.created : null,
-    notes: roleAssignment.attributes && roleAssignment.attributes.notes ?
-     roleAssignment.attributes.notes : 'No reason for case access given',
+    notes: roleAssignment.attributes && roleAssignment.attributes.specificAccessReason ?
+     getSpecificReason(roleAssignment.attributes.specificAccessReason) : 'No reason for case access given',
     requestedRole: roleAssignment.attributes && roleAssignment.attributes.requestedRole ?
      roleAssignment.attributes.requestedRole : null,
   }));
@@ -346,4 +346,13 @@ export function getAccessRolesRequestPayload(caseId: string,
       },
     ],
   };
+}
+
+// instances of specific reason appearing as JSON from toolkit versions - this enables both possibilities
+export function getSpecificReason(note: string): string {
+  if (note.charAt(0) !== '{') {
+    return note;
+  }
+  const noteObject = JSON.parse(note);
+  return noteObject ? noteObject.specificReason : null;
 }

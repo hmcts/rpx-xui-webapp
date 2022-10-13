fb31ed06946908e64c4d4b400a09a07f16c12989
Daniel Tade <daniel.tade@hmcts.net>
2022-10-13 05:11:16 +0100

    add backend

---
+++ Hofse-code/angular-smarter-api-backend/server.js
@@ -0,0 +1,220 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+const PORT = 3000;
+
+const JurisdictionsList = [
+  {
+    id: "DIVORCE",
+    name: "Family Divorce",
+    description: "Family Divorce: dissolution of marriage",
+  },
+  {
+    id: "PROBATE",
+    name: "Manage probate application",
+    description:
+      "Services (grant of representation, caveats, will lodgment, standing search, settlers, pre lodgement)",
+  },
+  {
+    id: "CIVIL",
+    name: "Civil",
+    description: "Civil Jurisdiction",
+  },
+];
+
+const StatesList = [
+  {
+    id: "CaseCreated",
+    name: "Case created",
+    description: "Case created",
+    order: 1,
+    title_display: null,
+    caseTypeId: "CaseViewCallbackMessages2",
+  },
+  {
+    id: "AfterEvent",
+    name: "After event",
+    description: "After event",
+    order: 2,
+    title_display: null,
+    caseTypeId: "CaseViewCallbackMessages2",
+  },
+  {
+    id: "CaseUpdated",
+    name: "Case updated",
+    description: "Case updated",
+    order: 3,
+    title_display: null,
+    caseTypeId: "CaseViewCallbackMessages2",
+  },
+  {
+    id: "Created",
+    name: "Bulk case list created",
+    description: "Bulk case list created",
+    order: 1,
+    caseTypeId: "NO_FAULT_DIVORCE_BulkAction",
+  },
+  {
+    id: "Empty",
+    name: "Bulk case empty",
+    description: "Bulk case empty",
+    order: 5,
+    title_display: null,
+    caseTypeId: "NO_FAULT_DIVORCE_BulkAction",
+  },
+  {
+    id: "Draft",
+    name: "Draft",
+    description: "Draft",
+    order: 34,
+    caseTypeId: "NFD",
+  },
+  {
+    id: "AosDrafted",
+    name: "AoS drafted",
+    description: "AoS drafted",
+    order: 3,
+    caseTypeId: "NFD",
+  },
+  {
+    id: "AfterEvent",
+    name: "After event",
+    description: "After event",
+    order: 2,
+    title_display: null,
+    caseTypeId: "xuiTestJurisdiction",
+  },
+  {
+    id: "CaseUpdated",
+    name: "Case Updated",
+    description: "Case Updated",
+    order: 3,
+    title_display: null,
+    caseTypeId: "xuiTestJurisdiction",
+  },
+  {
+    id: "StandingSearchCreated",
+    name: "Standing search created",
+    description: "Standing search created",
+    caseTypeId: "StandingSearch",
+  },
+  {
+    id: "WillWithdrawn",
+    name: "Will withdrawn",
+    description: "Will withdrawn",
+    order: 3,
+    caseTypeId: "WillLodgement",
+  },
+  {
+    id: "WillImported",
+    name: "Will imported",
+    description: "Will imported",
+    order: 4,
+    caseTypeId: "WillLodgement",
+  },
+  {
+    id: "dummyCaseCreated",
+    name: "Create dummy case",
+    description: "Create dummy case",
+    order: 1,
+    caseTypeId: "LegacySearch",
+  },
+  {
+    id: "CASE_DISMISSED",
+    name: "Claim Dismissed",
+    description: "Claim has been dismissed",
+    order: 6,
+    jurisdictionId: "CIVIL",
+  },
+  {
+    id: "PROCEEDS_IN_HERITAGE_SYSTEM",
+    name: "Case Proceeds Offline",
+    jurisdictionId: "GENERALAPPLICATION",
+  },
+];
+
+const CaseTypesList = [
+  {
+    id: "CaseViewCallbackMessages2",
+    description:
+      "CaseType for testing callback errors or warnings triggered on CaseView screen",
+
+    name: "CaseView Callback Messages 2",
+    jurisdictionId: "DIVORCE",
+  },
+  {
+    id: "NO_FAULT_DIVORCE_BulkAction",
+    description: "Handling of the dissolution of marriage",
+
+    name: "New Law Bulk Case",
+    jurisdictionId: "DIVORCE",
+  },
+  {
+    id: "NFD",
+    description: "Handling of the dissolution of marriage",
+
+    name: "New Law Case",
+    jurisdictionId: "DIVORCE",
+  },
+  {
+    id: "xuiTestJurisdiction",
+    description:
+      "CaseType for testing callback errors or warnings triggered on CaseView screen",
+
+    name: "XUI Case PoC",
+    jurisdictionId: "DIVORCE",
+  },
+  {
+    id: "StandingSearch",
+    description: "Probate - Tanding search",
+
+    name: "Standing search",
+    jurisdictionId: "PROBATE",
+  },
+  {
+    id: "WillLodgement",
+    description: "Probate - Will lodgement",
+
+    name: "Will lodgement",
+    jurisdictionId: "PROBATE",
+  },
+  {
+    id: "LegacySearch",
+    description: "ProbateMan cases",
+
+    name: "Legacy Case Search",
+    jurisdictionId: "PROBATE",
+  },
+  {
+    id: "CIVIL",
+    description: "Civil",
+
+    name: "Civil",
+    jurisdictionId: "CIVIL",
+  },
+  {
+    id: "GENERALAPPLICATION",
+    description: "Civil General Application",
+
+    name: "Civil General Application",
+    jurisdictionId: "CIVIL",
+  },
+];
+
+app.get("/jurisdictions", (req, res) => {
+  res.status(200);
+  res.send(JurisdictionsList);
+});
+
+app.get("/casetypes", (req, res) => {
+  res.status(200);
+  res.send(CaseTypesList);
+});
+
+app.get("/states", (req, res) => {
+  res.status(200);
+  res.send(StatesList);
+});
+
+app.listen(PORT, console.log(`Server running on ${PORT}`));
--- Hofse-code/angular-smarter-api/src/app/app.component.ts
+++ Hofse-code/angular-smarter-api/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
@@ -13,208 +14,17 @@ export class AppComponent {
   caseType: string = '';
   stateO: string = '';
 
-  JurisdictionsList = [
-    {
-      id: 'DIVORCE',
-      name: 'Family Divorce',
-      description: 'Family Divorce: dissolution of marriage',
-    },
-    {
-      id: 'PROBATE',
-      name: 'Manage probate application',
-      description:
-        'Services (grant of representation, caveats, will lodgment, standing search, settlers, pre lodgement)',
-    },
-    {
-      id: 'CIVIL',
-      name: 'Civil',
-      description: 'Civil Jurisdiction',
-    },
-  ];
-
-  StatesList = [
-    {
-      id: 'CaseCreated',
-      name: 'Case created',
-      description: 'Case created',
-      order: 1,
-      title_display: null,
-      caseTypeId: 'CaseViewCallbackMessages2',
-    },
-    {
-      id: 'AfterEvent',
-      name: 'After event',
-      description: 'After event',
-      order: 2,
-      title_display: null,
-      caseTypeId: 'CaseViewCallbackMessages2',
-    },
-    {
-      id: 'CaseUpdated',
-      name: 'Case updated',
-      description: 'Case updated',
-      order: 3,
-      title_display: null,
-      caseTypeId: 'CaseViewCallbackMessages2',
-    },
-    {
-      id: 'Created',
-      name: 'Bulk case list created',
-      description: 'Bulk case list created',
-      order: 1,
-      caseTypeId: 'NO_FAULT_DIVORCE_BulkAction',
-    },
-    {
-      id: 'Empty',
-      name: 'Bulk case empty',
-      description: 'Bulk case empty',
-      order: 5,
-      title_display: null,
-      caseTypeId: 'NO_FAULT_DIVORCE_BulkAction',
-    },
-    {
-      id: 'Draft',
-      name: 'Draft',
-      description: 'Draft',
-      order: 34,
-      caseTypeId: 'NFD',
-    },
-    {
-      id: 'AosDrafted',
-      name: 'AoS drafted',
-      description: 'AoS drafted',
-      order: 3,
-      caseTypeId: 'NFD',
-    },
-    {
-      id: 'AfterEvent',
-      name: 'After event',
-      description: 'After event',
-      order: 2,
-      title_display: null,
-      caseTypeId: 'xuiTestJurisdiction',
-    },
-    {
-      id: 'CaseUpdated',
-      name: 'Case Updated',
-      description: 'Case Updated',
-      order: 3,
-      title_display: null,
-      caseTypeId: 'xuiTestJurisdiction',
-    },
-    {
-      id: 'StandingSearchCreated',
-      name: 'Standing search created',
-      description: 'Standing search created',
-      caseTypeId: 'StandingSearch',
-    },
-    {
-      id: 'WillWithdrawn',
-      name: 'Will withdrawn',
-      description: 'Will withdrawn',
-      order: 3,
-      caseTypeId: 'WillLodgement',
-    },
-    {
-      id: 'WillImported',
-      name: 'Will imported',
-      description: 'Will imported',
-      order: 4,
-      caseTypeId: 'WillLodgement',
-    },
-    {
-      id: 'dummyCaseCreated',
-      name: 'Create dummy case',
-      description: 'Create dummy case',
-      order: 1,
-      caseTypeId: 'LegacySearch',
-    },
-    {
-      id: 'CASE_DISMISSED',
-      name: 'Claim Dismissed',
-      description: 'Claim has been dismissed',
-      order: 6,
-      jurisdictionId: 'CIVIL',
-    },
-    {
-      id: 'PROCEEDS_IN_HERITAGE_SYSTEM',
-      name: 'Case Proceeds Offline',
-      jurisdictionId: 'GENERALAPPLICATION',
-    },
-  ];
-
-  CaseTypesList = [
-    {
-      id: 'CaseViewCallbackMessages2',
-      description:
-        'CaseType for testing callback errors or warnings triggered on CaseView screen',
-
-      name: 'CaseView Callback Messages 2',
-      jurisdictionId: 'DIVORCE',
-    },
-    {
-      id: 'NO_FAULT_DIVORCE_BulkAction',
-      description: 'Handling of the dissolution of marriage',
-
-      name: 'New Law Bulk Case',
-      jurisdictionId: 'DIVORCE',
-    },
-    {
-      id: 'NFD',
-      description: 'Handling of the dissolution of marriage',
-
-      name: 'New Law Case',
-      jurisdictionId: 'DIVORCE',
-    },
-    {
-      id: 'xuiTestJurisdiction',
-      description:
-        'CaseType for testing callback errors or warnings triggered on CaseView screen',
-
-      name: 'XUI Case PoC',
-      jurisdictionId: 'DIVORCE',
-    },
-    {
-      id: 'StandingSearch',
-      description: 'Probate - Tanding search',
-
-      name: 'Standing search',
-      jurisdictionId: 'PROBATE',
-    },
-    {
-      id: 'WillLodgement',
-      description: 'Probate - Will lodgement',
-
-      name: 'Will lodgement',
-      jurisdictionId: 'PROBATE',
-    },
-    {
-      id: 'LegacySearch',
-      description: 'ProbateMan cases',
-
-      name: 'Legacy Case Search',
-      jurisdictionId: 'PROBATE',
-    },
-    {
-      id: 'CIVIL',
-      description: 'Civil',
-
-      name: 'Civil',
-      jurisdictionId: 'CIVIL',
-    },
-    {
-      id: 'GENERALAPPLICATION',
-      description: 'Civil General Application',
-
-      name: 'Civil General Application',
-      jurisdictionId: 'CIVIL',
-    },
-  ];
-
   title = 'XUI API Performance demo';
 
   ngOnInit() {
-    this.jurisdictions = this.JurisdictionsList;
+    axios
+      .get('http://127.0.0.1:3000/jurisdictions')
+      .then((response: any) => {
+        this.jurisdictions = response.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   onJurisdictionChange(sel: string) {
@@ -223,7 +33,16 @@ export class AppComponent {
       this.states = [];
       return;
     }
-    this.caseTypes = this.CaseTypesList;
+    axios
+      .get('http://127.0.0.1:3000/casetypes')
+      .then((response: any) => {
+        this.caseTypes = response.data;
+      })
+      .catch((error) => {
+        this.caseTypes = [];
+        this.states = [];
+        console.error(error);
+      });
   }
 
   onCasetypeChange(sel: string) {
@@ -231,7 +50,15 @@ export class AppComponent {
       this.states = [];
       return;
     }
-    this.states = this.StatesList;
+    axios
+      .get('http://127.0.0.1:3000/states')
+      .then((response: any) => {
+        this.states = response.data;
+      })
+      .catch((error) => {
+        this.states = [];
+        console.error(error);
+      });
   }
 
   onApplyClick() {

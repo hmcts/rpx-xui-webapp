5b39af3e3746ddad6638ba8dd345724371bf1aea
John Benjamin <john.benjamin@hmcts.net>
2022-10-17 14:30:29 +0100

    EUI-6935 Remove the mock objects in the code for api failures

--- src/cases/containers/case-hearings/case-hearings.component.spec.ts
+++ src/cases/containers/case-hearings/case-hearings.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -6,6 +7,8 @@ import { Store } from '@ngrx/store';
 import { provideMockStore } from '@ngrx/store/testing';
 import * as moment from 'moment';
 import { Observable, of } from 'rxjs';
+import { LovRefDataModel } from 'src/hearings/models/lovRefData.model';
+import { LovRefDataService } from 'src/hearings/services/lov-ref-data.service';
 import { UserRole } from '../../../app/models';
 import { RoleCategoryMappingService } from '../../../app/services/role-category-mapping/role-category-mapping.service';
 import { HearingConditions } from '../../../hearings/models/hearingConditions';
@@ -29,6 +32,7 @@ describe('CaseHearingsComponent', () => {
   let mockStore: Store<fromHearingStore.State>;
   let spyStore: any;
   let mockRoleCategoryMappingService: RoleCategoryMappingService;
+  let mockLovRefDataService: any;
   let mockRouter: any;
 
   const HEARING_DAY_SCHEDULE_1: HearingDayScheduleModel = {
@@ -346,15 +350,62 @@ describe('CaseHearingsComponent', () => {
     }
   };
 
+  const HEARING_TYPES_REF_DATA: LovRefDataModel[] = [
+    {
+      active_flag: 'Y',
+      category_key: 'HearingType',
+      child_nodes: null,
+      hint_text_cy: '',
+      hint_text_en: '',
+      key: 'BBA3-CHA',
+      lov_order: null,
+      parent_category: '',
+      parent_key: '',
+      value_cy: '',
+      value_en: 'Chambers Outcome',
+    },
+    {
+      active_flag: 'Y',
+      category_key: 'HearingType',
+      child_nodes: null,
+      hint_text_cy: '',
+      hint_text_en: '',
+      key: 'BBA3-CHA',
+      lov_order: null,
+      parent_category: '',
+      parent_key: '',
+      value_cy: '',
+      value_en: 'Substantive'
+    },
+    {
+      active_flag: 'Y',
+      category_key: 'HearingType',
+      child_nodes: null,
+      hint_text_cy: '',
+      hint_text_en: '',
+      key: 'BBA3-CHA',
+      lov_order: null,
+      parent_category: '',
+      parent_key: '',
+      value_cy: '',
+      value_en: 'Direction Hearings'
+    }
+  ];
+
   mockRouter = {
     navigate: jasmine.createSpy('navigate')
   };
 
   beforeEach(() => {
     mockRoleCategoryMappingService = jasmine.createSpyObj('RoleCategoryMappingService', ['getUserRoleCategory']);
+    mockLovRefDataService = jasmine.createSpyObj('LovRefDataService', ['getListOfValues']);
+    mockLovRefDataService.getListOfValues.and.returnValue(of(HEARING_TYPES_REF_DATA));
     TestBed.configureTestingModule({
       declarations: [CaseHearingsComponent],
-      imports: [RouterTestingModule],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
         provideMockStore({ initialState }),
@@ -375,6 +426,10 @@ describe('CaseHearingsComponent', () => {
         {
           provide: RoleCategoryMappingService,
           useValue: mockRoleCategoryMappingService,
+        },
+        {
+          provide: LovRefDataService,
+          useValue: mockLovRefDataService
         }
       ]
     }).compileComponents();
@@ -393,9 +448,19 @@ describe('CaseHearingsComponent', () => {
 
   it('should unsubscribe', () => {
     component.lastErrorSubscription = new Observable().subscribe();
+    component.roleCatSubscription = new Observable().subscribe();
+    component.hearingValuesSubscription = new Observable().subscribe();
+    component.refDataSubscription = new Observable().subscribe();
     spyOn(component.lastErrorSubscription, 'unsubscribe').and.callThrough();
+    spyOn(component.roleCatSubscription, 'unsubscribe').and.callThrough();
+    spyOn(component.hearingValuesSubscription, 'unsubscribe').and.callThrough();
+    spyOn(component.refDataSubscription, 'unsubscribe').and.callThrough();
+
     component.ngOnDestroy();
     expect(component.lastErrorSubscription.unsubscribe).toHaveBeenCalled();
+    expect(component.roleCatSubscription.unsubscribe).toHaveBeenCalled();
+    expect(component.hearingValuesSubscription.unsubscribe).toHaveBeenCalled();
+    expect(component.refDataSubscription.unsubscribe).toHaveBeenCalled();
   });
 
   it('should set hearings actions', () => {

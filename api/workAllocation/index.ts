6ce3fedf325b41b06272e5121d7eecdc9e83d6cb
DavidJayakumar <David.Jayakumar@hmcts.net>
2022-10-25 12:11:55 +0100

    Filter active role assignmets

--- api/workAllocation/index.ts
+++ api/workAllocation/index.ts
@@ -474,15 +474,9 @@ export function getCaseListPromises(data: CaseDataType, req: EnhancedRequest): A
 export async function getMyAccess(req: EnhancedRequest, res: Response, next: NextFunction): Promise<Response> {
   const roleAssignments = req.session.roleAssignmentResponse as RoleAssignment[];
   const mappedCases = await getMyAccessMappedCaseList(roleAssignments, req);
-  const validCases = mappedCases.filter(item => {
-    const todayDate = (new Date()).setHours(0,0,0,0);
-    const endDate = item.endDate && new Date(item.endDate).setHours(0,0,0,0);
-    let isValidRecord: boolean = endDate ? todayDate <= endDate : true;
-    return isValidRecord;
-  });
 
   const result = {
-    cases: validCases,
+    cases: mappedCases,
     total_records: 0,
     unique_cases: 0,
   };

1bf58a8367302c1b8fab8c9b13e66d140062ac99
Corey Grimes <corey.grimes@hmcts.net>
2022-11-01 13:36:30 +0000

    Added skip link method for skip navigation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -27,11 +27,22 @@ export class HeaderComponent implements OnInit {
   @Input() public currentUrl: string;
   @Output() public navigate = new EventEmitter<string>();
 
+  skipLinkNavigate: string = "";
+  contentHash: string = "#content";
+
   constructor(
-    public store: Store<fromRoot.State>,
+    public store: Store<fromRoot.State>
   ) {}
 
   public ngOnInit() {
+  }
+
+  public checkSkipLink(urlSkipLink: string) {
+    if(urlSkipLink.endsWith(this.contentHash)){
+      this.skipLinkNavigate = urlSkipLink;
+    } else {
+      this.skipLinkNavigate = `${urlSkipLink}${this.contentHash}`;
+    }
   }
 
   public onNavigate(event) {

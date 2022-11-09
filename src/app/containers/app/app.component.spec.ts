fb80e449d92dd988a819062eacc93f97921c013f
RiteshHMCTS <ritesh.dsouza@HMCTS.net>
2022-11-09 15:14:16 +0000

    capturing pageRefresh

--- src/app/containers/app/app.component.spec.ts
+++ src/app/containers/app/app.component.spec.ts
@@ -14,6 +14,7 @@ describe('AppComponent', () => {
   let router: any;
   let title: any;
   let testRoute: RoutesRecognized;
+  let sessionStorageService;
 
   beforeEach(() => {
       store = jasmine.createSpyObj('store', ['pipe', 'dispatch']);
@@ -23,6 +24,7 @@ describe('AppComponent', () => {
       cookieService = jasmine.createSpyObj('CookieService', ['deleteCookieByPartialMatch']);
       loggerService = jasmine.createSpyObj('LoggerService', ['enableCookies']);
       environmentService = jasmine.createSpyObj('environmentService', ['config$']);
+      sessionStorageService = jasmine.createSpyObj('SessionStorageService', ['setItem']);
       testRoute = new RoutesRecognized(1, 'test', 'test', {
           url: 'test',
           root: {
@@ -61,7 +63,7 @@ describe('AppComponent', () => {
       });
       router = { events: of(testRoute) };
       title = jasmine.createSpyObj('Title', ['setTitle']);
-      appComponent = new AppComponent(store, googleTagManagerService, timeoutNotificationService, router, title, featureToggleService, loggerService, cookieService, environmentService);
+      appComponent = new AppComponent(store, googleTagManagerService, timeoutNotificationService, router, title, featureToggleService, loggerService, cookieService, environmentService, sessionStorageService);
   });
 
   it('Truthy', () => {

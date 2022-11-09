815fa085ba7a15762cb2b689e5043c25973f5f9e
tarik.dogan <tarik.dogan@hmcts.net>
2022-11-09 16:28:01 +0000

    reverting changes on module export

--- api/webpack-node.config.js
+++ api/webpack-node.config.js
@@ -12,9 +12,7 @@ module.exports = {
         minimize: true,
         minimizer: [
             new TerserPlugin({
-                terserOptions: {
-                    sourceMap: true
-                }
+              sourceMap: true,
             }),
         ],
     },

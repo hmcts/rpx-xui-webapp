4148a074e83163c2cc36cf1034e076c90eb2ccf8
Dmitry Panov <dpanov76@yahoo.com>
2022-11-11 18:08:01 +0000

    Fixed: case-create-submit, hearing-requirenments

--- src/cases/containers/case-create-submit/case-create-submit.component.ts
+++ src/cases/containers/case-create-submit/case-create-submit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { CaseEventTrigger, CaseEventData, CasesService, DraftService, Draft } from '@hmcts/ccd-case-ui-toolkit';
+import { CaseEventData, CaseEventTrigger, CasesService, Draft, DraftService } from '@hmcts/ccd-case-ui-toolkit';
 import * as fromCaseCreate from '../../store';
 import {Store} from '@ngrx/store';
 import { ActionBindingModel } from '../../../cases/models/create-case-actions.model';

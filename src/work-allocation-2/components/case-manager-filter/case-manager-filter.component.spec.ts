9f73265f2ace32341bf64fb3b6853d737fde23c3
GaryScottHMCTS <gary.scott@hmcts.net>
2022-11-15 15:55:15 +0000

    Linting fix CaseManagerFilterComponent

--- src/work-allocation-2/components/case-manager-filter/case-manager-filter.component.spec.ts
+++ src/work-allocation-2/components/case-manager-filter/case-manager-filter.component.spec.ts
@@ -12,12 +12,13 @@ import * as fromStore from '../../../app/store';
 import { LocationDataService, WorkAllocationCaseService } from '../../services';
 import { ALL_LOCATIONS } from '../constants/locations';
 
+/* tslint:disable:component-selector */
 @Component({
   selector: 'xuilib-generic-filter',
   template: '<span></span>',
 })
 class MockGenericFilterComponent {
-  @Input() config;
+  @Input() public config;
 }
 
 

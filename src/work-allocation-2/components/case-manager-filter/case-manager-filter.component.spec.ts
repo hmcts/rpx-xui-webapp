ecea537a428a85791c988730a4de35989d332b1f
GaryScottHMCTS <gary.scott@hmcts.net>
2022-11-16 09:36:22 +0000

    TaskManagerFilterComponent test corrected

--- src/work-allocation-2/components/case-manager-filter/case-manager-filter.component.spec.ts
+++ src/work-allocation-2/components/case-manager-filter/case-manager-filter.component.spec.ts
@@ -56,7 +56,7 @@ describe('CaseManagerFilterComponent', () => {
         },
       ]
     }).compileComponents();
-    store = TestBed.get(Store);
+    store = TestBed.inject(Store);
     storePipeMock = spyOn(store, 'pipe');
 
     fixture = TestBed.createComponent(CaseManagerFilterComponent);

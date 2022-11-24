5273cd9cab8b3647ce67d1cfffcf7161311355aa
tarik.dogan <tarik.dogan@hmcts.net>
2022-11-24 13:17:54 +0000

    more linting fix

--- src/cases/components/roles-and-access/roles-and-access.component.spec.ts
+++ src/cases/components/roles-and-access/roles-and-access.component.spec.ts
@@ -8,7 +8,7 @@ import { CaseRole, RoleCategory, RoleExclusion } from '../../../role-access/mode
 import { Caseworker } from '../../../work-allocation-2/models/dtos';
 import { ShowAllocateLinkDirective } from '../../directives/show-allocate-link.directive';
 import { RolesAndAccessComponent } from './roles-and-access.component';
-
+import 'mocha';
 
 describe('RolesAndAccessComponent', () => {
   let component: RolesAndAccessComponent;
@@ -205,3 +205,7 @@ describe('RolesAndAccessComponent', () => {
     expect(component.namedExclusions[3].name).toBe('D Test');
   });
 });
+function waitForAsync(arg0: () => void): Mocha.Func {
+  throw new Error('Function not implemented.');
+}
+

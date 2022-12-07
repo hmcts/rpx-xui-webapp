a88638555bce223dc0740655de926df4eb26916a
Matei Baciu <matei.baciu@hmcts.net>
2022-12-07 14:46:31 +0000

    remove CHALLENGED from My Access

--- api/workAllocation/util.ts
+++ api/workAllocation/util.ts
@@ -14,7 +14,7 @@ import { CASE_ALLOCATOR_ROLE } from '../user/constants';
 import { RoleAssignment } from '../user/interfaces/roleAssignment';
 
 import { exists, reflect } from '../lib/util';
-import { TaskPermission, ViewType, VIEW_PERMISSIONS_ACTIONS_MATRIX } from './constants/actions';
+import { TaskPermission, VIEW_PERMISSIONS_ACTIONS_MATRIX, ViewType } from './constants/actions';
 import { getCaseListPromises } from "./index";
 import { Case, CaseList } from './interfaces/case';
 import { CaseworkerPayload, ServiceCaseworkerData } from './interfaces/caseworkerPayload';
@@ -362,8 +362,6 @@ export async function getMyAccessMappedCaseList(roleAssignmentList: RoleAssignme
     roleAssignment.roleName === 'specific-access-requested'
     ||
     roleAssignment.roleName === 'specific-access-denied'
-    ||
-    roleAssignment.grantType === 'CHALLENGED'
   );
 
   const cases = await getCaseIdListFromRoles(specificRoleAssignments, req);

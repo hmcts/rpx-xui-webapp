e10beb725a8efdd70c26dd13889bc3e2260161fe
John Benjamin <john.benjamin@hmcts.net>
2022-12-09 14:49:46 +0000

    EUI-6646-Restricted-case-access-user-attempts-to-access-restricted-case

---
+++ src/cases/containers/restricted-case-access-container/restricted-case-access-container.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RestrictedCaseAccessContainerComponent } from './restricted-case-access-container.component';
+
+describe('RestrictedCaseAccessContainerComponent', () => {
+  let component: RestrictedCaseAccessContainerComponent;
+  let fixture: ComponentFixture<RestrictedCaseAccessContainerComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [],
+      declarations: [RestrictedCaseAccessContainerComponent]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RestrictedCaseAccessContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

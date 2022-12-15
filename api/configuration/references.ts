d297b9d5e21e302482e9f072faa421abd930c9be
Phillip Whitaker <phillip.whitaker1@hmcts.net>
2022-12-15 16:16:55 +0000

    updated cloud redis URL scerent

--- api/configuration/references.ts
+++ api/configuration/references.ts
@@ -9,7 +9,7 @@
  * This file should be representative of the .json files in the root /config folder
  */
 export const S2S_SECRET = 'secrets.rpx.mc-s2s-client-secret';
-export const REDIS_CLOUD_URL = 'secrets.rpx.webapp-redis-connection-string';
+export const REDIS_CLOUD_URL = 'secrets.rpx.webapp-redis-connection-string-new';
 export const APP_INSIGHTS_KEY = 'secrets.rpx.appinsights-instrumentationkey-mc';
 export const LAUNCH_DARKLY_CLIENT_ID = 'secrets.rpx.launch-darkly-client-id';
 

e6c5fb1fea344e1c2a37360d4a03ac9015cfdaeb
John Benjamin <john.benjamin@hmcts.net>
2022-12-19 13:14:52 +0000

    Angular 11 merge

--- api/proxy.config.ts
+++ api/proxy.config.ts
@@ -14,7 +14,6 @@ import {
   SERVICES_LOCATION_REF_API_URL,
   SERVICES_MARKUP_API_URL,
   SERVICES_PAYMENTS_URL,
-  SERVICES_CCD_DATA_STORE_API_PATH,
   SERVICES_PRD_COMMONDATA_API,
   SERVICES_REFUNDS_API_URL
 } from './configuration/references';
@@ -142,7 +141,7 @@ export const initProxy = (app: Express) => {
     target: getConfigValue(SERVICES_LOCATION_REF_API_URL),
   });
 
-	applyProxy(app, {
+  applyProxy(app, {
     rewrite: false,
     source: '/refdata/commondata/lov/categories/CaseLinkingReasonCode',
     target: getConfigValue(SERVICES_PRD_COMMONDATA_API),
@@ -160,7 +159,7 @@ export const initProxy = (app: Express) => {
     target: getConfigValue(SERVICES_CCD_DATA_STORE_API_PATH),
   });
 
-	applyProxy(app, {
+  applyProxy(app, {
     rewrite: false,
     source: '/getLinkedCases',
     target: getConfigValue(SERVICES_CCD_DATA_STORE_API_PATH),

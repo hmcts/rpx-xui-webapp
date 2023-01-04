cd85dce21cab4b7bf2a1ff396effcdaa7c46aa37
John Benjamin <john.benjamin@hmcts.net>
2023-01-04 14:29:32 +0000

    EUI-6646 Restricted case access user attempts to access restricted case

--- src/cases/components/restricted-case-access/restricted-case-access.component.ts
+++ src/cases/components/restricted-case-access/restricted-case-access.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RestrictedCase } from '../../models/restricted-case.model';
 
 @Component({
   selector: 'exui-restricted-case-access',
   templateUrl: './restricted-case-access.component.html'
 })
-export class RestrictedCaseAccessComponent implements OnInit {
-  public ngOnInit(): void {
+export class RestrictedCaseAccessComponent {
 
-  }
+  @Input() restrictedCases: RestrictedCase[];
 }

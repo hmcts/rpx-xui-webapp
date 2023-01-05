03b1eceed0cd6ddbd33d8f2e8c194bcfb6353e04
GaryScottHMCTS <gary.scott@hmcts.net>
2023-01-05 09:28:49 +0000

    EUI-6573: Added pipe to error message component

--- src/app/components/error-message/error-message.component.spec.ts
+++ src/app/components/error-message/error-message.component.spec.ts
@@ -1,6 +1,7 @@
 import { Component, Input, ViewChild } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MockRpxTranslatePipe } from '../../shared/test/mock-rpx-translate.pipe';
 import { ErrorMessage } from '../../models';
 import { ErrorMessageComponent } from './error-message.component';
 
@@ -20,7 +21,7 @@ describe('ErrorMessageComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ WrapperComponent, ErrorMessageComponent ],
+      declarations: [ WrapperComponent, ErrorMessageComponent, MockRpxTranslatePipe ],
       imports: [ RouterTestingModule ]
     }).compileComponents();
     fixture = TestBed.createComponent(WrapperComponent);

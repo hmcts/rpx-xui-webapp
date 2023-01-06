d4a68a92fa0cab97640e6daa6b9083190f6d5419
Phillip Whitaker <phillip.whitaker1@hmcts.net>
2023-01-06 10:05:17 +0000

    Added notificationsApiRoot to ccpay component props

--- src/refunds/refunds/refunds.component.ts
+++ src/refunds/refunds/refunds.component.ts
@@ -9,6 +9,7 @@ import { UserService } from '../../app/services/user/user.service';
 export class RefundsComponent implements OnInit {
 
   public refundsApiRoot = 'api/refund';
+  public notificationsApiRoot = 'api/notication';
   public ccdCaseNumber = 0;
   public userEmail = '';
   public userRoles = [];

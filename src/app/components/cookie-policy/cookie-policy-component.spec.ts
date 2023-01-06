0fa785e3a3ff53f41ebcc5d1685f05b96faf2621
GaryScottHMCTS <gary.scott@hmcts.net>
2023-01-06 10:55:05 +0000

    EUI-6535 Added the ability for content on Policy and Cookie pages to show different languages

--- src/app/components/cookie-policy/cookie-policy-component.spec.ts
+++ src/app/components/cookie-policy/cookie-policy-component.spec.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { async, ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { RpxTranslationService } from 'rpx-xui-translation';
 import { CookiePolicyComponent } from './cookie-policy.component';
 
 describe('CookiePolicyComponentTest', () => {
+  const translationServiceMock = {
+    language: 'cy'
+  }
 
   @Component({selector: 'exui-app-header', template: ''})
   class HeaderStubComponent {
@@ -19,16 +22,18 @@ describe('CookiePolicyComponentTest', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HeaderStubComponent, FooterStubComponent, CookiePolicyComponent ],
-      imports: [
-        RouterTestingModule
+      providers: [
+        { provide: RpxTranslationService, useValue: translationServiceMock }
       ]
     })
-      .compileComponents();
+    .compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(CookiePolicyComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
@@ -50,6 +55,17 @@ describe('CookiePolicyComponentTest', () => {
       expect(ccc.cat).toBe(component.SECURITY);
       cc = cc + 1;
     }
-    expect (cc).toEqual(component.countCookies(component.SECURITY));
+    expect(cc).toEqual(component.countCookies(component.SECURITY));
+  });
+
+  describe('showWelshTranslation', () => {
+    it('should be true', async () => {
+      expect(component.showWelshTranslation).toEqual(true);
+    });
+
+    it('should be false', async () => {
+      translationServiceMock.language = 'en';
+      expect(component.showWelshTranslation).toEqual(false);
+    });
   });
 });
--- src/app/components/cookie-policy/cookie-policy.component.ts
+++ src/app/components/cookie-policy/cookie-policy.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { RpxTranslationService } from 'rpx-xui-translation';
+
+interface CookieDetails{
+  name: string;
+  cat: string;
+  purpose: string;
+  purposeWelsh: string;
+  expires: string;
+}
 
 @Component({
   selector: 'exui-cookie-policy',
-  templateUrl: './cookie-policy.component.html'
+  templateUrl: './cookie-policy.component.html',
 })
 export class CookiePolicyComponent {
-
-  public readonly googlePurpose = 'This helps us count how many people visit the service by tracking if you\'ve visited before';
-// Ideally this would be an enum but angular can't seem to cope with enums in templates
+  public readonly googlePurpose =
+    `This helps us count how many people visit the service by tracking if you've visited before`;
+  // Ideally this would be an enum but angular can't seem to cope with enums in templates
   public readonly USAGE = 'Usage';
   public readonly INTRO = 'Intro';
   public readonly SESSION = 'Session';
@@ -15,26 +24,114 @@ export class CookiePolicyComponent {
   public readonly SECURITY = 'Security';
   public readonly GOOGLE = 'Google';
 
-  public cookieDetails =
-    [
-      {name: 'xui-webapp', cat: this.SECURITY, purpose: 'Used to secure communications with HMCTS data services', expires: '8 hours'},
-      {name: 'rxVisitor', cat: this.USAGE, purpose: 'Generated user ID for usage tracking (Dynatrace)', expires: '2 years'},
-      {name: 'ai_user', cat: this.USAGE, purpose: 'Generated user ID for usage tracking (Application Insights)', expires: '6 months'},
-      {name: '_oauth2_proxy', cat: this.SECURITY, purpose: 'Used to protect your login session', expires: '4 hours'},
-      {name: '_gid', cat: this.GOOGLE, purpose: this.googlePurpose, expires: '1 day'},
-      {name: '_ga', cat: this.GOOGLE, purpose: 'This stores information about your session', expires: '2 years'},
-      {name: '_ga_XXXXXXXXXX', cat: this.GOOGLE, purpose: 'This stores information about your session', expires: '2 years'},
-      {name: '_gat_XXXXXXXXXX', cat: this.GOOGLE, purpose: 'This is used to control the rate at which requests to the analytics software are made', expires: '1 day'},
-      {name: '__userid__', cat: this.IDENTIFY, purpose: 'Your user ID', expires: 'When you close your browser'},
-      {name: '__auth__', cat: this.SECURITY, purpose: 'Information about your current system authorisations', expires: 'When you close your browser'},
-      {name: 'XSRF-TOKEN', cat: this.SECURITY, purpose: 'Used to protect your session against cross site scripting attacks', expires: 'When you close your browser'}
-    ];
+  public cookieDetails: CookieDetails[] = [
+    {
+      name: 'xui-webapp',
+      cat: this.SECURITY,
+      purpose: 'Used to secure communications with HMCTS data services',
+      purposeWelsh: 'Used to secure communications with HMCTS data services',
+      expires: '8 hours',
+    },
+    {
+      name: 'rxVisitor',
+      cat: this.USAGE,
+      purpose: 'Generated user ID for usage tracking (Dynatrace)',
+      purposeWelsh: 'Generated user ID for usage tracking (Dynatrace)',
+      expires: '2 years',
+    },
+    {
+      name: 'ai_user',
+      cat: this.USAGE,
+      purpose: 'Generated user ID for usage tracking (Application Insights)',
+      purposeWelsh:
+        'Generated user ID for usage tracking (Application Insights)',
+      expires: '6 months',
+    },
+    {
+      name: '_oauth2_proxy',
+      cat: this.SECURITY,
+      purpose: 'Used to protect your login session',
+      purposeWelsh: 'Used to protect your login session',
+      expires: '4 hours',
+    },
+    {
+      name: '_gid',
+      cat: this.GOOGLE,
+      purpose: this.googlePurpose,
+      purposeWelsh: this.googlePurpose,
+      expires: '1 day',
+    },
+    {
+      name: '_ga',
+      cat: this.GOOGLE,
+      purpose: 'This stores information about your session',
+      purposeWelsh: 'This stores information about your session',
+      expires: '2 years',
+    },
+    {
+      name: '_ga_XXXXXXXXXX',
+      cat: this.GOOGLE,
+      purpose: 'This stores information about your session',
+      purposeWelsh: 'This stores information about your session',
+      expires: '2 years',
+    },
+    {
+      name: '_gat_XXXXXXXXXX',
+      cat: this.GOOGLE,
+      purpose:
+        'This is used to control the rate at which requests to the analytics software are made',
+      purposeWelsh:
+        'This is used to control the rate at which requests to the analytics software are made',
+      expires: '1 day',
+    },
+    {
+      name: '__userid__',
+      cat: this.IDENTIFY,
+      purpose: 'Your user ID',
+      purposeWelsh: 'Your user ID',
+      expires: 'When you close your browser',
+    },
+    {
+      name: '__auth__',
+      cat: this.SECURITY,
+      purpose: 'Information about your current system authorisations',
+      purposeWelsh: 'Information about your current system authorisations',
+      expires: 'When you close your browser',
+    },
+    {
+      name: 'XSRF-TOKEN',
+      cat: this.SECURITY,
+      purpose:
+        'Used to protect your session against cross site scripting attacks',
+      purposeWelsh:
+        'Used to protect your session against cross site scripting attacks',
+      expires: 'When you close your browser',
+    },
+  ];
+
+  constructor(private readonly langService: RpxTranslationService) {}
+
+  public get showWelshTranslation(): boolean {
+    return this.langService.language === 'cy';
+  }
 
   public countCookies(category: string): number {
     return this.cookiesByCat(category).length;
   }
 
-  public cookiesByCat(category: string): {name: string, cat: string, purpose: string, expires: string}[] {
-    return this.cookieDetails && this.cookieDetails.filter(c => c.cat === category);
+  public cookiesByCat(
+    category: string
+  ): Omit<CookieDetails, 'purposeWelsh'>[] {
+    return (
+      this.cookieDetails &&
+      this.cookieDetails
+        .filter((c) => c.cat === category)
+        .map((c) => ({
+          name: c.name,
+          cat: c.cat,
+          expires: c.expires,
+          purpose: this.showWelshTranslation ? c.purposeWelsh : c.purpose,
+        }))
+    );
   }
 }

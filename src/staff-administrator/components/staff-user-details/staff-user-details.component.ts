b3ebf144a851a433f10caf432357da7c59ed8db5
Matei Baciu <matei.baciu@hmcts.net>
2023-01-16 11:08:56 +0000

    progress on EUI-6406

--- src/staff-administrator/components/staff-user-details/staff-user-details.component.ts
+++ src/staff-administrator/components/staff-user-details/staff-user-details.component.ts
@@ -21,4 +21,67 @@ export class StaffUserDetailsComponent {
       this.router.navigateByUrl('/staff');
     }
   }
+
+  public setDataAndNavigateToUpdateUser() {
+    const FILTER_ID = 'staff-add-edit-user';
+
+    const primaryLocation = this.userDetails.base_locations.find(item => item.is_primary);
+    const formValues = {
+      id: FILTER_ID,
+      fields: [
+        {
+          name: 'first_name',
+          value: [this.userDetails.first_name]
+        },
+        {
+          name: 'last_name',
+          value: [this.userDetails.last_name]
+        },
+        {
+          name: 'email_id',
+          value: [this.userDetails.email_id]
+        },
+        {
+          name: 'user-services',
+          value: this.userDetails.services.map(item => item.service_code)
+        },
+        {
+          name: 'region_id',
+          value: [this.userDetails.region_id]
+        },
+        {
+          name: 'primaryLocation',
+          value: [{ epimms_id: primaryLocation.location_id, site_name: primaryLocation.location }]
+        },
+        {
+          name: 'additionalLocations',
+          value: this.userDetails.base_locations.filter(item => !item.is_primary).map(location => {
+            return { epimms_id: location.location_id, site_name: location.location };
+          })
+        },
+        {
+          name: 'user_type',
+          value: [this.userDetails.user_type]
+        },
+        {
+          name: 'roles',
+          value: [
+            this.userDetails.case_allocator ? 'case_allocator' : false,
+            this.userDetails.task_supervisor ? 'task-supervisor' : false,
+            this.userDetails.staff_admin ? 'staff_admin' : false
+          ].filter(item => item)
+        },
+        {
+          name: 'jobTitle',
+          value: this.userDetails.roles.map(item => Number(item.role_id))
+        },
+        {
+          name: 'user-skills',
+          value: this.userDetails.skills.map(item => item.skill_id)
+        }
+      ]
+    };
+    sessionStorage.setItem(FILTER_ID, JSON.stringify(formValues));
+    this.router.navigateByUrl('/staff/add-edit-user', { state: {userDetails: this.userDetails}});
+  }
 }

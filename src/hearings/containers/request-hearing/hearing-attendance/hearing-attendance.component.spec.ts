c81e9761dd740008c6089945f0b837ee38c8c3e2
Kartheek-S <92437222+Kartheek-S@users.noreply.github.com>
2023-01-19 12:08:46 +0000

    hearing channel changes

--- src/hearings/containers/request-hearing/hearing-attendance/hearing-attendance.component.spec.ts
+++ src/hearings/containers/request-hearing/hearing-attendance/hearing-attendance.component.spec.ts
@@ -1,16 +1,17 @@
-import {Component, CUSTOM_ELEMENTS_SCHEMA, Input} from '@angular/core';
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {AbstractControl, FormArray, FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
-import {ActivatedRoute} from '@angular/router';
-import {ErrorMessage} from '@hmcts/ccd-case-ui-toolkit';
-import {provideMockStore} from '@ngrx/store/testing';
-import {of} from 'rxjs';
-import {initialState} from '../../../hearing.test.data';
-import {ACTION, RadioOptions} from '../../../models/hearings.enum';
-import {HearingsService} from '../../../services/hearings.service';
-import {LovRefDataService} from '../../../services/lov-ref-data.service';
-import {ValidatorsUtils} from '../../../utils/validators.utils';
-import {HearingAttendanceComponent} from './hearing-attendance.component';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, Input } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ErrorMessage } from '@hmcts/ccd-case-ui-toolkit';
+import { provideMockStore } from '@ngrx/store/testing';
+import { of } from 'rxjs';
+import { LovRefDataModel } from '../../../../hearings/models/lovRefData.model';
+import { initialState } from '../../../hearing.test.data';
+import { ACTION, RadioOptions } from '../../../models/hearings.enum';
+import { HearingsService } from '../../../services/hearings.service';
+import { LovRefDataService } from '../../../services/lov-ref-data.service';
+import { ValidatorsUtils } from '../../../utils/validators.utils';
+import { HearingAttendanceComponent } from './hearing-attendance.component';
 
 @Component({
   selector: 'exui-hearing-parties-title',
@@ -27,18 +28,54 @@ describe('HearingAttendanceComponent', () => {
   const hearingsService = new HearingsService(mockedHttpClient);
   const lovRefDataService = jasmine.createSpyObj('lovRefDataService', ['getListOfValues']);
   hearingsService.navigateAction$ = of(ACTION.CONTINUE);
+  const refData: LovRefDataModel[] = [
+    {
+      key: 'INTER',
+      value_en: 'In Person',
+      value_cy: '',
+      hint_text_en: 'In Person',
+      hint_text_cy: '',
+      lov_order: 1,
+      parent_key: null,
+      category_key: 'inPerson',
+      parent_category: '',
+      active_flag: 'Y',
+      child_nodes: null,
+      from: 'exui-default',
+    },
+    {
+      key: 'TEL',
+      value_en: 'Telephone',
+      value_cy: '',
+      hint_text_en: 'Telephone',
+      hint_text_cy: '',
+      lov_order: 2,
+      parent_key: null,
+      category_key: 'Telephone',
+      parent_category: '',
+      active_flag: 'Y',
+      child_nodes: null,
+      from: 'exui-default',
+      selected: true,
+    }
+  ];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [ReactiveFormsModule],
       declarations: [HearingAttendanceComponent, MockHearingPartiesComponent],
       providers: [
-        provideMockStore({initialState}),
-        {provide: HearingsService, useValue: hearingsService},
-        {provide: LovRefDataService, useValue: lovRefDataService},
+        provideMockStore({ initialState }),
+        { provide: HearingsService, useValue: hearingsService },
+        { provide: LovRefDataService, useValue: lovRefDataService },
         {
           provide: ActivatedRoute,
           useValue: {
+            snapshot: {
+              data: {
+                hearingChannels: refData
+              }
+            },
             fragment: of('point-to-me'),
           },
         },
@@ -50,6 +87,7 @@ describe('HearingAttendanceComponent', () => {
 
     fixture = TestBed.createComponent(HearingAttendanceComponent);
     component = fixture.componentInstance;
+    component.hearingRequestMainModel.hearingDetails.hearingLevelParticipantAttendance = ['TEL'];
     spyOn(component, 'initialiseFromHearingValues').and.callThrough();
     spyOn(component, 'prepareHearingRequestData').and.callThrough();
     spyOn(component, 'isFormValid').and.callThrough();
@@ -64,6 +102,8 @@ describe('HearingAttendanceComponent', () => {
     (component.attendanceFormGroup.controls.parties as FormArray).controls.forEach(element => {
       element.value.individualDetails.preferredHearingChannel = 'inperson';
     });
+    (component.attendanceFormGroup.controls.hearingLevelChannels as FormArray).controls
+      .forEach((element: AbstractControl) => element.value.selected = true);
     component.executeAction(ACTION.CONTINUE);
     expect(component.prepareHearingRequestData).toHaveBeenCalled();
   });
@@ -77,6 +117,8 @@ describe('HearingAttendanceComponent', () => {
     (component.attendanceFormGroup.controls.parties as FormArray).controls.forEach((element: AbstractControl) => {
       element.value.individualDetails.preferredHearingChannel = 'inperson';
     });
+    (component.attendanceFormGroup.controls.hearingLevelChannels as FormArray).controls
+      .forEach((element: AbstractControl) => element.value.selected = true);
     const formValid = component.isFormValid();
     expect((component.attendanceFormGroup.controls.parties as FormArray).length).toBeGreaterThan(0);
     expect(formValid).toEqual(true);
@@ -104,4 +146,3 @@ describe('HearingAttendanceComponent', () => {
     fixture.destroy();
   });
 });
-

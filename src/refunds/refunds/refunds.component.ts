89ed43c0faada50ff382184e67c3a6b2ea21f5b5
RiteshHMCTS <ritesh.dsouza@HMCTS.net>
2023-02-02 17:24:52 +0000

    missing property added

--- src/refunds/refunds/refunds.component.ts
+++ src/refunds/refunds/refunds.component.ts
@@ -14,6 +14,7 @@ export class RefundsComponent implements OnInit {
   public userEmail = '';
   public userRoles = [];
   public userDataLoaded = false;
+  public apiRoot = '/payments';
 
   constructor(private readonly userService: UserService) { }
 

0b570600c8b0f2cd133f1446323890da5426e6a8
Matei Baciu <matei.baciu@hmcts.net>
2023-02-03 13:44:14 +0000

    dummy change

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -14,6 +14,7 @@ import { ApplicationRoutingComponent } from './routing/application-routing.compo
 import { ServiceDownComponent } from './service-down/service-down.component';
 import { SignedOutComponent } from './signed-out/signed-out.component';
 
+
 export const components: any[] = [
   HmctsGlobalFooterComponent,
   HeaderComponent,

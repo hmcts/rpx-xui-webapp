2b3c11cb1167e2aec5c95f18d8829068885b9b39
Surekha <surekha.nandiyal@gmail.com>
2023-02-07 13:37:15 +0000

    fix r4 issues

--- src/staff-administrator/containers/staff-users/staff-users.component.ts
+++ src/staff-administrator/containers/staff-users/staff-users.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { BannerDataModel } from '@hmcts/rpx-xui-common-lib/lib/gov-ui/models';
+import { HmctsBannerInfo } from '@hmcts/rpx-xui-common-lib/lib/gov-ui/components/hmcts-banner/hmcts-banner-info.interface';
 import { InfoMessageCommService } from '../../../app/shared/services/info-message-comms.service';
 import { StaffDataFilterService } from '../../components/staff-users/services/staff-data-filter/staff-data-filter.service';
-
 @Component({
   selector: 'exui-staff-users',
   templateUrl: './staff-users.component.html',
@@ -10,7 +9,7 @@ import { StaffDataFilterService } from '../../components/staff-users/services/st
 })
 export class StaffUsersComponent  {
   public advancedSearchEnabled = false;
-  public bannerData: BannerDataModel;
+  public bannerData: HmctsBannerInfo;
 
   constructor(
     public staffDataFilterService: StaffDataFilterService,

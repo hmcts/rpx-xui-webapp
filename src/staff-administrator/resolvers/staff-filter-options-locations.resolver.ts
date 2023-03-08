c8d8078688c9a9e156bddd2a0fcc50ed6bcf1220
Surekha <surekha.nandiyal@gmail.com>
2023-03-08 12:21:16 +0000

    used WASupportedJurisdictionsService

--- src/staff-administrator/resolvers/staff-filter-options-locations.resolver.ts
+++ src/staff-administrator/resolvers/staff-filter-options-locations.resolver.ts
@@ -3,18 +3,20 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { LocationByEPIMMSModel } from '@hmcts/rpx-xui-common-lib';
 import { Observable } from 'rxjs';
-import { StaffFilterOption } from '../models/staff-filter-option.model';
+import { map, switchMap } from 'rxjs/operators';
+import { WASupportedJurisdictionsService } from '../../work-allocation/services';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StaffFilterOptionsLocationsResolver implements Resolve<LocationByEPIMMSModel[]> {
 
-  constructor(private readonly http: HttpClient) {
+  constructor(protected waSupportedJurisdictionsService: WASupportedJurisdictionsService, private readonly http: HttpClient) {
   }
 
   public resolve(route?: ActivatedRouteSnapshot): Observable<LocationByEPIMMSModel[]> {
-
-    return this.http.post<LocationByEPIMMSModel[]>(`api/locations/getLocations`, {serviceIds: ['SSCS','IA'], locationType: 'case-management', searchTerm: 'cen'});
+    return this.waSupportedJurisdictionsService.getWASupportedJurisdictions().pipe(switchMap(supportedJurisdiction => {
+      return this.http.post<LocationByEPIMMSModel[]>(`api/locations/getLocations`, {serviceIds: supportedJurisdiction, locationType: 'case-management', searchTerm: 'cen'});
+    }));
   }
 }

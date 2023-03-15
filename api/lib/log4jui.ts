ca99f3b6b808f99c4eb2102c1dcdb0291a51c07c
LucaDelBuonoHMCTS <luca.delbuono@hmcts.net>
2023-03-15 09:49:05 +0000

    EUI-4129: Some linting

--- api/lib/log4jui.ts
+++ api/lib/log4jui.ts
@@ -1,60 +1,59 @@
-import * as log4js from 'log4js'
-import {getConfigValue} from '../configuration'
+import * as log4js from 'log4js';
+import {getConfigValue} from '../configuration';
 import {
-  LOG4_J_CONFIG,
-  LOGGING,
-} from '../configuration/references'
-import { client } from './appInsights'
-import { JUILogger } from './models'
+LOG4_J_CONFIG,
+LOGGING,
+} from '../configuration/references';
+import { client } from './appInsights';
+import { JUILogger } from './models';
 
 // the longest category length we have currently
-const maxCatLength = 14
+const maxCatLength = 14;
 
 // This is done to mimic log4js calls
-log4js.configure(getConfigValue(LOG4_J_CONFIG))
+log4js.configure(getConfigValue(LOG4_J_CONFIG));
 
 // TODO: this should be moved into util but the import seems to fail
 export function leftPad(str: string, length = 20): string {
-    return `${' '.repeat(Math.max(length - str.length, 0))}${str}`
+  return `${' '.repeat(Math.max(length - str.length, 0))}${str}`;
 }
 
 export function getLogger(category: string): JUILogger {
-    const logger: log4js.Logger = log4js.getLogger(category)
-    // @ts-ignore
-    logger.level = getConfigValue(LOGGING) || 'off'
+  const logger: log4js.Logger = log4js.getLogger(category);
+  // @ts-ignore
+  logger.level = getConfigValue(LOGGING) || 'off';
 
-    const catFormatted = leftPad(category, maxCatLength)
-    logger.addContext('catFormatted', `${catFormatted} `)
+  const catFormatted = leftPad(category, maxCatLength);
+  logger.addContext('catFormatted', `${catFormatted} `);
 
-    return {
-        _logger: logger,
-        debug,
-        error,
-        info,
-        trackRequest,
-        warn,
-    }
+  return {
+    _logger: logger,
+    debug,
+    error,
+    info,
+    trackRequest,
+    warn,
+  };
 }
 
 function info(...messages: any[]) {
-    this._logger.info(...messages)
+  this._logger.info(...messages);
 }
 
 function warn(...messages: any[]) {
-    this._logger.warn(...messages)
+  this._logger.warn(...messages);
 }
 
 function debug(...messages: any[]) {
-    this._logger.debug(...messages)
+  this._logger.debug(...messages);
 }
 
 function trackRequest(obj: any) {
-    if (client) {
-        client.trackRequest(obj)
-    }
+  if (client) {
+    client.trackRequest(obj);
+  }
 }
 
 function error(...messages: any[]) {
-    this._logger.error(...messages)
-
+  this._logger.error(...messages);
 }

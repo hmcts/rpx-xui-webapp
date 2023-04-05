ea8cb4a9a2e4130c3617766eeee76d765e7a463b
David Rajkumar Jayakumar <107850923+DavidJayakumar@users.noreply.github.com>
2023-04-05 10:18:35 +0100

    Fix issue with task completion (#2904)
    * Fix issue with task completion
    
    * Fix audit issues

--- src/app/services/ccd-config/ccd-case.config.ts
+++ src/app/services/ccd-config/ccd-case.config.ts
@@ -39,33 +39,13 @@ export class AppConfig extends AbstractAppConfig {
       }
     });
 
-    // Default value is passed to resolve EUI-7670.
-    // ToDo: Remove default value and get the latest not null value
-    const defaultConfig = {
-      configurations: [
-        {
-          caseTypes: [
-            'Asylum'
-          ],
-          releaseVersion: '3.5',
-          serviceName: 'IA'
-        },
-        {
-          caseTypes:[
-            'CIVIL',
-            'GENERALAPPLICATION'
-          ],
-          releaseVersion: '3.5',
-          serviceName: 'CIVIL'
+    this.environmentService.config$.subscribe(config => {
+      this.featureToggleService.getValue('wa-service-config', config.waSupportedServices).subscribe({
+        next: (val) => this.config = {
+          ...this.config,
+          wa_service_config: val
         }
-      ]
-    };
-
-    this.featureToggleService.getValue('wa-service-config', defaultConfig).subscribe({
-      next: (val) => this.config = {
-        ...this.config,
-        wa_service_config: val
-      }
+      });
     });
 
     this.featureToggleService.getValue('access-management-basic-view-mock', {}).subscribe({

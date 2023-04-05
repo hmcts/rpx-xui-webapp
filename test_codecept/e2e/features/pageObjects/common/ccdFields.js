9c06f09860fdb68914dee4d2e13d276f49c148a9
sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
2023-04-05 11:28:30 +0100

    protractor to codeceptjs migration

---
+++ test_codecept/e2e/features/pageObjects/common/ccdFields.js
@@ -0,0 +1,50 @@
+
+var CaseManager = require("./CaseManager");
+var BrowserWaits = require('../../../support/customWaits');
+var path = require('path');
+
+class CcdFields {
+
+  constructor() {
+    this.caseManager = new CaseManager();
+
+    this.addressField = element(by.xpath('//input[@name=\'postcode\']'));
+    this.findAddressBtn = element(by.xpath('//button[contains(text(),\'Find address\')]'));
+    this.selectAddressField = element(by.xpath('//select[@name=\'address\']'));
+    this.firstOption = element(by.xpath('//option[@value=\'1: Object\']'));
+
+
+    this.addNewBtn = element(by.xpath('//button[contains(text(),\'Add new\')]'));
+    this.docUploadField = element(by.xpath('//input[@class=\'form-control bottom-30\']'));
+    this.describeDocField = $("#uploadTheNoticeOfDecisionDocs_0_description");
+
+    this.uploadDone = element(by.xpath('//ccd-write-document-field//span[contains(text(),"Uploading")]'));
+
+  }
+
+  async docUpload(){
+    var fileToUpload = path.resolve(__dirname, "../../../documents/dummy.pdf");
+    await this.docUploadField.sendKeys(fileToUpload);
+    await browser.sleep(1);
+    await BrowserWaits.waitForCondition(async () => {
+      let isUploadDone = await this.uploadDone.isPresent();
+      console.log("file upload status : " + isUploadDone);
+      await browser.sleep(5);
+      return !isUploadDone;
+    });
+  }
+
+  async docDescriptionField(){
+    await this.describeDocField.sendKeys('description');
+  }
+
+  async postcodeLookup(){
+    await BrowserWaits.waitForSeconds(3);
+    await this.addressField.sendKeys('n1');
+    await this.findAddressBtn.click();
+    await BrowserWaits.waitForSeconds(3);
+    await this.firstOption.click();
+  }
+}
+
+module.exports = CcdFields;
---
+++ test_codecept/e2e/features/pageObjects/workAllocation/common/taskAssignmentPersonNotAuthorisedPage.js
@@ -0,0 +1,13 @@
+
+
+class TaskAssignmentPersonNotAuthorised{
+
+    constructor(){
+        this.container = $('exui-task-assignment-person-not-authorised');
+        this.message = $('exui-task-assignment-person-not-authorised h1');
+        this.backButton = $('exui-task-assignment-person-not-authorised button');
+    }
+
+}
+
+module.exports = new TaskAssignmentPersonNotAuthorised(); 

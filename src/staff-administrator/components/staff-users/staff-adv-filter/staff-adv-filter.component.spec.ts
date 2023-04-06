2e57ea88fb5bb7120fe0d7877ab8039cd53bd5f6
Matei <108479475+MateiHMCTS@users.noreply.github.com>
2023-04-06 16:18:46 +0100

    Feature/e UI 7977 (#2914)
    * EUI-7977
    
    * EUI-7977 tests

--- src/staff-administrator/components/staff-users/staff-adv-filter/staff-adv-filter.component.spec.ts
+++ src/staff-administrator/components/staff-users/staff-adv-filter/staff-adv-filter.component.spec.ts
@@ -1,21 +1,27 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ExuiCommonLibModule, FilterService } from '@hmcts/rpx-xui-common-lib';
-import { of } from 'rxjs';
+import { FilterService, FilterSetting } from '@hmcts/rpx-xui-common-lib';
+import { filterConfig } from '@stryker-mutator/mocha-runner/src/utils';
+import { BehaviorSubject } from 'rxjs';
 import { StaffDataAccessService } from '../../../services/staff-data-access/staff-data-access.service';
 import { staffFilterOptionsTestData } from '../../../test-data/staff-filter-options.test.data';
 import { StaffDataFilterService } from '../services/staff-data-filter/staff-data-filter.service';
 import { StaffAdvFilterComponent } from './staff-adv-filter.component';
+
+// tslint:disable-next-line:component-selector -- this is a stub component -- it is imported from common lib which has a different prefix
+@Component({selector: 'xuilib-generic-filter', template: ''})
+class GenericFilterStubComponent {
+  public submitted = false;
+}
 
 describe('StaffAdvFilterComponent', () => {
   let component: StaffAdvFilterComponent;
   let fixture: ComponentFixture<StaffAdvFilterComponent>;
 
   let mockStaffDataFilterService: jasmine.SpyObj<StaffDataFilterService>;
-
   const mockFilterServiceResponse = {
     id: 'staff-advanced-filters',
     fields: [
@@ -46,29 +52,30 @@ describe('StaffAdvFilterComponent', () => {
     ],
     reset: false
   };
-
-  const mockFilterService = {
-    getStream: () => of(mockFilterServiceResponse),
-    get: jasmine.createSpy(),
-    persist: jasmine.createSpy(),
-    givenErrors: {
-      pipe: () => null,
-      subscribe: () => of([{ error: 'errorMessage' }]),
-      next: () => null,
-      unsubscribe: () => null
-    },
-    clearSessionAndLocalPersistance: jasmine.createSpy()
-  };
+  let mockFilterStreamSubject: BehaviorSubject<any>;
+  let mockFilterErrorsSubject: BehaviorSubject<any>;
 
   beforeEach(() => {
+    mockFilterStreamSubject = new BehaviorSubject(mockFilterServiceResponse);
+    mockFilterErrorsSubject = new BehaviorSubject(null);
+
+    const mockFilterService = {
+      getStream: () => mockFilterStreamSubject,
+      get: jasmine.createSpy(),
+      persist: jasmine.createSpy(),
+      givenErrors: mockFilterErrorsSubject,
+      clearSessionAndLocalPersistance: jasmine.createSpy()
+    };
+
     mockStaffDataFilterService = jasmine.createSpyObj<StaffDataFilterService>('mockStaffDataFilterService',
       ['search', 'changePage', 'setErrors']);
+    mockStaffDataFilterService.search.and.callThrough();
+
     TestBed.configureTestingModule({
-      declarations: [ StaffAdvFilterComponent ],
+      declarations: [ StaffAdvFilterComponent, GenericFilterStubComponent ],
       imports: [
         RouterTestingModule,
         HttpClientTestingModule,
-        ExuiCommonLibModule,
       ],
       providers: [
         { provide: StaffDataFilterService, useValue: mockStaffDataFilterService },
@@ -88,42 +95,8 @@ describe('StaffAdvFilterComponent', () => {
           }
         },
       ],
-      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
-
-    // mockStaffDataFilterService.search.and.returnValue(of([{
-    //   email_id: '2',
-    //   first_name: 'Victoria',
-    //   last_name: 'Patton',
-    //   suspended: false,
-    //   user_type: 'Officer2',
-    //   userCategory: '',
-    //   task_supervisor: false,
-    //   case_allocator: true,
-    //   staff_admin: false,
-    //   roles: [{
-    //     role_id: 1,
-    //     role: 'Role 1',
-    //     is_primary: true,
-    //   }],
-    //   skills: [{
-    //     skill_id: 123,
-    //     description: 'Skill 123',
-    //     skill_code: 'SK123'
-    //   }],
-    //   services: [{
-    //     service: 'Service',
-    //     service_code: 'Service_Code'
-    //   }],
-    //   base_locations: [{
-    //     location_id: 123,
-    //     location: 'Location',
-    //     is_primary: true
-    //   }],
-    //   region: 'Region',
-    //   region_id: 999
-    // }]));
 
   });
 
@@ -133,36 +106,141 @@ describe('StaffAdvFilterComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create and set filterConfig', () => {
     expect(component).toBeTruthy();
+    expect(filterConfig).toBeTruthy();
   });
 
-  it('should have All by default in job title', () => {
+  it('should have All by default in job title in filterConfig', () => {
     const field = component.filterConfig.fields.find(f => f.name === 'user-job-title');
     expect(field.defaultOption.key).toBe('All');
-    const element = fixture.debugElement.nativeElement;
-    const userJobTitle = element.querySelector('#select_user-job-title');
-    expect(userJobTitle.value).toBe('All');
   });
 
   it('should have All by default in user type', () => {
     const field = component.filterConfig.fields.find(f => f.name === 'user-type');
     expect(field.defaultOption.key).toBe('All');
-    const element = fixture.debugElement.nativeElement;
-    const userJobTitle = element.querySelector('#select_user-type');
-    expect(userJobTitle.value).toBe('All');
   });
 
-  it('should call search on when filterService emits', () => {
+  describe('filterSub' , () => {
+    it('should unsubscribe from filterService on destroy', () => {
+      // @ts-expect-error -- private property
+      spyOn(component.filterSub, 'unsubscribe');
+      component.ngOnDestroy();
+      // @ts-expect-error -- private property
+      expect(component.filterSub.unsubscribe).toHaveBeenCalled();
+    });
 
+    it('should call search on when filterService emits and it contains values', () => {
+      const filterSetting = {
+        id: 'staff-advanced-filters',
+        fields: [
+          {
+            value: [
+              { key: 'AAA7', label: 'Service A' },
+              { key: 'BFA1', label: 'Service B' },
+              { key: 'CDA3', label: 'Service C' },
+            ],
+            name: 'user-services'
+          },
+          {
+            value: [],
+            name: 'user-location'
+          },
+          {
+            value: ['All'],
+            name: 'user-type'
+          },
+          {
+            value: ['All'],
+            name: 'user-job-title'
+          },
+          {
+            value: ['All'],
+            name: 'user-skills'
+          },
+          {
+            value: [],
+            name: 'user-role'
+          }
+        ],
+        reset: false
+      };
+      mockFilterStreamSubject.next(filterSetting);
+
+      // @ts-expect-error - private property
+      expect(component.staffDataFilterService.search).toHaveBeenCalledWith(
+        { advancedSearchFilters: { serviceCode: [ 'AAA7', 'BFA1', 'CDA3' ] }, pageNumber: 1, pageSize: 15 }
+      );
+    });
+
+    describe('when filterService has no setted filters', () => {
+      let filterSetting: FilterSetting;
+
+      beforeEach(() => {
+        filterSetting = {
+          id: 'staff-advanced-filters',
+          fields: [
+            {
+              value: [],
+              name: 'user-services'
+            },
+            {
+              value: [],
+              name: 'user-location'
+            },
+            {
+              value: ['All'],
+              name: 'user-type'
+            },
+            {
+              value: ['All'],
+              name: 'user-job-title'
+            },
+            {
+              value: ['All'],
+              name: 'user-skills'
+            },
+            {
+              value: [],
+              name: 'user-role'
+            }
+          ],
+          reset: false
+        };
+      });
+
+      it('should set an error when filterService emits no values the genericFilter is submitted', () => {
+        component.genericFilterComponent.submitted = true;
+        fixture.detectChanges();
+        mockFilterStreamSubject.next(filterSetting);
+        // @ts-expect-error - private property
+        expect(component.staffDataFilterService.setErrors).toHaveBeenCalledWith(
+          // @ts-expect-error - private property
+          [{ name: component.FILTER_NAME, error: component.ERROR_MESSAGE_MIN_ONE_CRITERIA }]
+        );
+      });
+
+      it('should not set an error when filterService emits no values and the genericFilter is not submitted', () => {
+        // @ts-expect-error - private property
+        expect(component.staffDataFilterService.setErrors).not.toHaveBeenCalled();
+      });
+    });
   });
 
-  // it('should update the page number of searchFilters', () => {
-  //   const originalValue = ad.value;
-  //   changePage.call({ searchFilters }, pageNumber);
-  //
-  //   expect(searchFilters.value.pageNumber).toBe(pageNumber);
-  //   expect(searchFilters.value).not.toBe(originalValue);
-  //   expect(searchFilters.value).toEqual({ ...originalValue, pageNumber });
-  // });
+  describe('filterErrorsSub', () => {
+    it('should call setErrors on staffDataFilterService when filterService givenErrors emits', () => {
+      const errors = [{ name: 'test', error: 'test' }];
+      mockFilterErrorsSubject.next(errors);
+      // @ts-expect-error - private property
+      expect(component.staffDataFilterService.setErrors).toHaveBeenCalledWith(errors);
+    });
+
+    it('should unsubscribe from filterService givenErrors on destroy', () => {
+      // @ts-expect-error -- private property
+      spyOn(component.filterErrorsSub, 'unsubscribe');
+      component.ngOnDestroy();
+      // @ts-expect-error -- private property
+      expect(component.filterErrorsSub.unsubscribe).toHaveBeenCalled();
+    });
+  });
 });

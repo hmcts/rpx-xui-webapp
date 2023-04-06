a3985bf2b70507d364da3bff4db1d77f802c3bd0
sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
2023-04-06 10:22:59 +0100

    codecept test update

--- test_codecept/backendMock/services/roleAssignments/index.js
+++ test_codecept/backendMock/services/roleAssignments/index.js
@@ -9,11 +9,25 @@ class RoleAssignments{
 
     }   
 
+
     getActorRoles(actorID){
+        const roles = []
         const role = this.getRoleAssignmentTemplate();
         role.actorId = actorID;
-        return {
-            roleAssignmentResponse : [role]}
+
+        const caseAllocator = this.getRoleAssignmentTemplate();
+        caseAllocator.actorId = actorID;
+        caseAllocator.roleName = 'case-allocator'
+
+        const taskSupervisor = this.getRoleAssignmentTemplate();
+        taskSupervisor.actorId = actorID;
+        taskSupervisor.roleName = 'task-supervisor'
+
+        roles.push(role)
+        roles.push(caseAllocator)
+        roles.push(taskSupervisor)
+
+        return { roleAssignmentResponse: roles }
     }
 
     getServiceUsersRolesAssignments(reqBody){
@@ -52,6 +66,14 @@ class RoleAssignments{
         return roles;
     }
 
+
+    addRoleAssignmentResponse(req){
+        const responseTemplate = this.getAddRoleAssignmentResponseTemplate();
+        const response = {roleAssignmentResponse:  req.body };
+        response.roleAssignmentResponse.roleRequest['authenticatedUserid'] = response.roleAssignmentResponse.roleRequest.assignerId;
+        return responseTemplate;
+    }
+
     getBookings(reqBody){
         return {
             "bookings": [
@@ -87,6 +109,49 @@ class RoleAssignments{
                 "primaryLocation": "219164",
                 "jurisdiction": "IA",
                 "workTypes": "hearing_work, routine_work, decision_making_work, applications"
+            }
+        }
+    }
+
+
+    getAddRoleAssignmentResponseTemplate(){
+        return {
+            roleAssignmentResponse: {
+                roleRequest: {
+                    id: "bf28c9a7-67d7-401b-93c1-fede693b5d1c",
+                    authenticatedUserId: "***REMOVED***",
+                    correlationId: "bc10952c-b000-4c96-bbde-04126a2def46",
+                    assignerId: "***REMOVED***",
+                    requestType: "CREATE",
+                    replaceExisting: false,
+                    status: "APPROVED",
+                    created: "2023-04-04T15:50:14.28129674Z",
+                    log: "Request has been approved",
+                    byPassOrgDroolRule: true,
+                },
+                requestedRoles: [
+                    {
+                        id: "96a2d8fc-3f69-44cd-afe7-abff7d49b94a",
+                        actorIdType: "IDAM",
+                        actorId: "fbf510c3-7f8a-48cf-98eb-1291dd44392e",
+                        roleType: "CASE",
+                        roleName: "hearing-judge",
+                        classification: "PUBLIC",
+                        grantType: "SPECIFIC",
+                        roleCategory: "JUDICIAL",
+                        readOnly: false,
+                        beginTime: "2023-04-03T23:00:00Z",
+                        status: "LIVE",
+                        created: "2023-04-04T15:50:14.281320878Z",
+                        log: "Create requested with replace: false\nCase Allocator approved : case_allocator_approve_create_case_role\nStage 1 approved : ia_hearing_judge_create_case_roles\nStage 1 approved : ia_hearing_judge_create_case_roles\nApproved : validate_role_assignment_against_patterns",
+                        attributes: {
+                            caseId: "1547466725650882",
+                            jurisdiction: "IA",
+                            caseType: "Asylum",
+                            substantive: "Y",
+                        },
+                    },
+                ],
             }
         }
     }

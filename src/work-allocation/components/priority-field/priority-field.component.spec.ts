01e381d278c53634150082d8b3531989a6ec9b81
Uday Denduluri <uday.denduluri1@hmcts.net>
2023-04-11 13:59:24 +0100

    Add unit tests

--- src/work-allocation/components/priority-field/priority-field.component.spec.ts
+++ src/work-allocation/components/priority-field/priority-field.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { TaskPriority } from '../../enums';
+import { PriorityLimits, TaskPriority } from '../../enums';
 import { PriorityFieldComponent } from './priority-field.component';
 
 
@@ -32,7 +32,44 @@ describe('PriorityFieldComponent', () => {
     expect(component.priority).toBeDefined();
   });
 
-  it('should correctly set the priority', () => {
+  it('should correctly set the priority to HIGH', () => {
+    expect(component.priority).toBe(TaskPriority.HIGH);
+    const yesterdayDate = new Date();
+    yesterdayDate.setDate(new Date().getDate() - 1);
+    component.majorPriority = PriorityLimits.High;
+    component.date = yesterdayDate;
+    fixture.detectChanges();
+    expect(component.priority).toBe(TaskPriority.HIGH);
+  });
+  it('should correctly set the priority to MEDIUM', () => {
+    const tomorrowDate = new Date();
+    tomorrowDate.setDate(new Date().getDate() + 1);
+    component.majorPriority = PriorityLimits.High;
+    component.date = tomorrowDate;
+    fixture.detectChanges();
+    expect(component.priority).toBe(TaskPriority.MEDIUM);
+  });
+  it('should correctly set the priority to LOW', () => {
+    const dayafterTomorrowDate = new Date();
+    dayafterTomorrowDate.setDate(new Date().getDate() + 2);
+    component.majorPriority = PriorityLimits.High;
+    component.date = dayafterTomorrowDate;
+    fixture.detectChanges();
+    expect(component.priority).toBe(TaskPriority.LOW);
+  });
+  it('should correctly set the priority to URGENT', () => {
+    component.majorPriority = PriorityLimits.Urgent;
+    fixture.detectChanges();
+    expect(component.priority).toBe(TaskPriority.URGENT);
+  });
+  it('should correctly set the priority to LOW when priority greater than 5000', () => {
+    component.majorPriority = 5001;
+    fixture.detectChanges();
+    expect(component.priority).toBe(TaskPriority.LOW);
+  });
+  it('should correctly set the priority to HIGH when priority between 2000 and 5000', () => {
+    component.majorPriority = 2500;
+    fixture.detectChanges();
     expect(component.priority).toBe(TaskPriority.HIGH);
   });
 });

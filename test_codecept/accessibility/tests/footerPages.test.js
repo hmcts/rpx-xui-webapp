11a4f6e7706c3a33a18cb3a01a7997a47795672f
sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
2023-04-12 22:45:23 +0100

    a11y test run fix

--- test_codecept/accessibility/tests/footerPages.test.js
+++ test_codecept/accessibility/tests/footerPages.test.js
@@ -11,36 +11,40 @@ const divorceCaseActions = require('../caseCreationActions/divorce');
 const MockApp = require('../../nodeMock/app');
 
 
-xdescribe('Pa11y Accessibility tests', function () {
-    before(async function (done) {
+describe('Pa11y Accessibility tests', function () {
+    // before(async function (done) {
       
-        done();
-    });
-    after(async function (done) {
+    //     done();
+    // });
+    // after(async function (done) {
       
-        done();
-    });
+    //     done();
+    // });
     
     it('Accessibility Page', async function () {
         const actions = [];
+        await initBrowser()
         await pa11ytest(this, actions, conf.baseUrl + 'accessibility');
 
     });
 
     it('Cookies Page', async function () {
         const actions = [];
+        await initBrowser()
         await pa11ytest(this, actions, conf.baseUrl + 'cookies');
 
     });
 
     it('privacy-policy Page', async function () {
         const actions = [];
+        await initBrowser()
         await pa11ytest(this, actions, conf.baseUrl + 'privacy-policy');
 
     });
 
     it('get-help Page', async function () {
         const actions = [];
+        await initBrowser()
         await pa11ytest(this, actions, conf.baseUrl + 'get-help');
 
     });

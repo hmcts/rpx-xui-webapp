4b50bae1ddcdee304554d2530ee5a268593c360b
sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
2023-04-12 20:51:39 +0100

    a11y tests enabled with codecept runner

--- test_codecept/codeceptCommon/codecept.conf.ts
+++ test_codecept/codeceptCommon/codecept.conf.ts
@@ -30,7 +30,7 @@ if (testType === 'e2e'){
 } else if (testType === 'ngIntegration'){
   features = `../ngIntegration/tests/features/**/*.feature`
 
-}else{
+} else{
   throw new Error(`Unrecognized test type ${testType}`);
 }
 

98abbff6800974cddae00c5cd3c48af321bd1c02
Matei Baciu <matei.baciu@hmcts.net>
2023-04-17 10:28:28 +0100

    EUI-8120

--- src/work-allocation/components/priority-field/priority-field.component.spec.ts
+++ src/work-allocation/components/priority-field/priority-field.component.spec.ts
@@ -1,19 +1,17 @@
-import { async, ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { PriorityLimits, TaskPriority } from '../../enums';
-import { PriorityFieldComponent } from './priority-field.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { PriorityFieldComponent } from './priority-field.component';
 
 describe('PriorityFieldComponent', () => {
   let component: PriorityFieldComponent;
   let fixture: ComponentFixture<PriorityFieldComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [ PriorityFieldComponent ]
     })
     .compileComponents();
-  }));
-
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PriorityFieldComponent);
@@ -23,53 +21,5 @@ describe('PriorityFieldComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should only show if there is a dueDate set', () => {
-    expect(component.priority).toBe(TaskPriority.HIGH);
-    component.date = new Date();
-    fixture.detectChanges();
-    expect(component.priority).toBeDefined();
-  });
-
-  it('should correctly set the priority to HIGH', () => {
-    expect(component.priority).toBe(TaskPriority.HIGH);
-    const yesterdayDate = new Date();
-    yesterdayDate.setDate(new Date().getDate() - 1);
-    component.majorPriority = PriorityLimits.High;
-    component.date = yesterdayDate;
-    fixture.detectChanges();
-    expect(component.priority).toBe(TaskPriority.HIGH);
-  });
-  it('should correctly set the priority to MEDIUM', () => {
-    const tomorrowDate = new Date();
-    tomorrowDate.setDate(new Date().getDate() + 1);
-    component.majorPriority = PriorityLimits.High;
-    component.date = tomorrowDate;
-    fixture.detectChanges();
-    expect(component.priority).toBe(TaskPriority.MEDIUM);
-  });
-  it('should correctly set the priority to LOW', () => {
-    const dayafterTomorrowDate = new Date();
-    dayafterTomorrowDate.setDate(new Date().getDate() + 2);
-    component.majorPriority = PriorityLimits.High;
-    component.date = dayafterTomorrowDate;
-    fixture.detectChanges();
-    expect(component.priority).toBe(TaskPriority.LOW);
-  });
-  it('should correctly set the priority to URGENT', () => {
-    component.majorPriority = PriorityLimits.Urgent;
-    fixture.detectChanges();
-    expect(component.priority).toBe(TaskPriority.URGENT);
-  });
-  it('should correctly set the priority to LOW when priority greater than 5000', () => {
-    component.majorPriority = 5001;
-    fixture.detectChanges();
-    expect(component.priority).toBe(TaskPriority.LOW);
-  });
-  it('should correctly set the priority to HIGH when priority between 2000 and 5000', () => {
-    component.majorPriority = 2500;
-    fixture.detectChanges();
-    expect(component.priority).toBe(TaskPriority.HIGH);
   });
 });

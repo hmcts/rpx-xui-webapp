85bb0b50223f7b54f24c118b6b4694eba6cef202
Matei <108479475+MateiHMCTS@users.noreply.github.com>
2023-04-18 11:44:01 +0100

    Feature/e UI 6841 (#2937)
    * EUI-6841
    
    * fix 6841
    
    * update common lib
    
    * 6841
    
    * EUI-6841

--- src/staff-administrator/containers/staff-users/staff-users.component.spec.ts
+++ src/staff-administrator/containers/staff-users/staff-users.component.spec.ts
@@ -1,17 +1,32 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import SpyObj = jasmine.SpyObj;
+import { of } from 'rxjs';
+import { ErrorMessage } from '../../../app/models';
+import { InfoMessageCommService } from '../../../app/shared/services/info-message-comms.service';
 import {
   StaffDataFilterService
 } from '../../components/staff-users/services/staff-data-filter/staff-data-filter.service';
-import { StaffDataAccessService } from '../../services/staff-data-access/staff-data-access.service';
 import { StaffUsersComponent } from './staff-users.component';
 
 describe('StaffUsersComponent', () => {
   let component: StaffUsersComponent;
   let fixture: ComponentFixture<StaffUsersComponent>;
+  let infoMessageCommMock: SpyObj<InfoMessageCommService>;
+  let staffDataFilterServiceMock: Partial<StaffDataFilterService>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
+    infoMessageCommMock = jasmine.createSpyObj('InfoMessageCommService', ['removeAllMessages']);
+    staffDataFilterServiceMock = {
+      errors$: of({
+        title: 'There is a problem',
+        description: '',
+        multiple: true,
+        errors: []
+      } as ErrorMessage)
+    };
+
     TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule
@@ -20,8 +35,8 @@ describe('StaffUsersComponent', () => {
         StaffUsersComponent
       ],
       providers: [
-        StaffDataFilterService,
-        StaffDataAccessService
+        { provide : StaffDataFilterService, useValue: staffDataFilterServiceMock },
+        { provide: InfoMessageCommService, useValue: infoMessageCommMock }
       ],
       schemas: [NO_ERRORS_SCHEMA],
     })
@@ -36,5 +51,20 @@ describe('StaffUsersComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('advancedSearchClicked', () => {
+    it('should toggle advancedSearchEnabled', () => {
+      expect(component.advancedSearchEnabled).toBe(false);
+      component.advancedSearchClicked();
+      expect(component.advancedSearchEnabled).toBe(true);
+    });
+    it('should call removeAllMessages', () => {
+      // @ts-expect-error - infoMessageCommService is private
+      expect(component.infoMessageCommService.removeAllMessages).not.toHaveBeenCalled();
+      component.advancedSearchClicked();
+      // @ts-expect-error - infoMessageCommService is private
+      expect(component.infoMessageCommService.removeAllMessages).toHaveBeenCalled();
+    });
   });
 });

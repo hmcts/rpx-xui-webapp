cc4d013aedf91daf67d60a7a7985d8bb9ba95f33
LucaDelBuonoHMCTS <luca.delbuono@hmcts.net>
2023-04-27 15:33:09 +0100

    EXUI-190: Add test

--- src/cases/containers/case-details-home/case-details-home.component.spec.ts
+++ src/cases/containers/case-details-home/case-details-home.component.spec.ts
@@ -2,6 +2,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ActivatedRoute, Navigation, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Title } from '@angular/platform-browser';
 import {
   AlertService,
   ErrorNotifierService,
@@ -9,7 +10,8 @@ import {
 } from '@hmcts/ccd-case-ui-toolkit';
 import { combineReducers, Store, StoreModule } from '@ngrx/store';
 import { of } from 'rxjs';
-import { CaseDetailsHomeComponent } from '..';
+import { CaseDetailsHomeComponent } from './case-details-home.component';
+import { CaseReferencePipe } from '../../../hearings/pipes/case-reference.pipe';
 import { reducers } from '../../../app/store';
 import { InfoMessage } from '../../../work-allocation/enums';
 import * as fromFeature from '../../store';
@@ -84,5 +86,76 @@ describe('CaseDetailsHomeComponent', () => {
       expect(mockAlertService.setPreserveAlerts).toHaveBeenCalled();
       expect(mockAlertService.success).toHaveBeenCalled();
     });
+  });
+});
+
+describe('CaseDetailsHomeComponent set the page title on init', () => {
+  let component: CaseDetailsHomeComponent;
+  let fixture: ComponentFixture<CaseDetailsHomeComponent>;
+  let activatedRouteMock: Partial<ActivatedRoute>;
+  let sessionStorageServiceMock: jasmine.SpyObj<SessionStorageService>;
+  let titleServiceMock: jasmine.SpyObj<Title>;
+  let caseReferencePipeMock: jasmine.SpyObj<CaseReferencePipe>;
+
+  beforeEach(() => {
+    activatedRouteMock = {
+      data: of({
+        case: {
+          case_id: '123',
+          case_type: {
+            id: '456',
+            jurisdiction: {
+              id: '789',
+            },
+          },
+          tabs: [
+            {
+              id: 'caseDetails',
+              fields: [
+                {
+                  id: 'appellantGivenNames',
+                  formatted_value: 'John',
+                },
+                {
+                  id: 'appellantFamilyName',
+                  formatted_value: 'Doe',
+                },
+              ],
+            },
+            {
+              id: 'overview',
+              fields: [
+                {
+                  id: 'appealReferenceNumber',
+                  formatted_value: 'AA/12345/2019',
+                },
+              ],
+            },
+          ],
+        },
+      }),
+    };
+
+    sessionStorageServiceMock = jasmine.createSpyObj<SessionStorageService>('SessionStorageService', ['setItem']);
+    titleServiceMock = jasmine.createSpyObj<Title>('Title', ['setTitle']);
+    caseReferencePipeMock = jasmine.createSpyObj<CaseReferencePipe>('CaseReferencePipe', ['transform']);
+
+    TestBed.configureTestingModule({
+      declarations: [CaseDetailsHomeComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRouteMock },
+        { provide: SessionStorageService, useValue: sessionStorageServiceMock },
+        { provide: Title, useValue: titleServiceMock },
+        { provide: CaseReferencePipe, useValue: caseReferencePipeMock },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CaseDetailsHomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should set the title of the page', () => {
+    expect(titleServiceMock.setTitle).toHaveBeenCalledWith('John Doe (AA/12345/2019) - HM Courts & Tribunals Service - GOV.UK');
   });
 });

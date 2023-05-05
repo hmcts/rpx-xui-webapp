9cc407651e059e2c655501c97cd3a5de69b6ba3e
LucaDelBuonoHMCTS <luca.delbuono@hmcts.net>
2023-05-05 17:46:13 +0100

    EXUI-190: Move setting window title to Toolkit

--- src/cases/containers/case-details-home/case-details-home.component.ts
+++ src/cases/containers/case-details-home/case-details-home.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 import { AlertService, SessionStorageService } from '@hmcts/ccd-case-ui-toolkit';
-import { CaseReferencePipe } from '../../../hearings/pipes/case-reference.pipe';
 
 @Component({
   selector: 'exui-case-details-home',
-  templateUrl: './case-details-home.component.html',
-  providers: [CaseReferencePipe]
+  templateUrl: './case-details-home.component.html'
 })
 export class CaseDetailsHomeComponent implements OnInit {
   private readonly extras: NavigationExtras;
@@ -16,9 +13,7 @@ export class CaseDetailsHomeComponent implements OnInit {
     private readonly alertService: AlertService,
     private readonly activatedRoute: ActivatedRoute,
     private readonly router: Router,
-    private readonly sessionStorageService: SessionStorageService,
-    private readonly titleService: Title,
-    private readonly caseReferencePipe: CaseReferencePipe
+    private readonly sessionStorageService: SessionStorageService
   ) {
     const navigation = this.router.getCurrentNavigation();
     if (navigation) {
@@ -34,28 +29,6 @@ export class CaseDetailsHomeComponent implements OnInit {
     }
     this.activatedRoute.data.subscribe((data) => {
       if (data?.case?.case_type?.jurisdiction) {
-        let fullName: string = '';
-        let caseDetails: string = '';
-
-        const caseDetailsTab = data.case.tabs?.find((tab) => tab.id === 'caseDetails');
-        const firstName: string = caseDetailsTab?.fields.find((field) => field.id === 'appellantGivenNames')?.formatted_value;
-        const lastName: string = caseDetailsTab?.fields.find((field) => field.id === 'appellantFamilyName')?.formatted_value;
-        fullName += firstName ? firstName : '';
-        fullName += lastName ? ` ${lastName}` : '';
-
-        const overviewTab = data.case.tabs?.find((tab) => tab.id === 'overview');
-        const appealReferenceNumber: string = overviewTab?.fields.find((field) => field.id === 'appealReferenceNumber')?.formatted_value;
-
-        caseDetails += fullName ? fullName : '';
-
-        caseDetails += fullName && appealReferenceNumber ? ` (${appealReferenceNumber})` : '';
-        caseDetails += fullName && !appealReferenceNumber ? ` (${this.caseReferencePipe.transform(data.case.case_id)})` : '';
-
-        caseDetails += !fullName && appealReferenceNumber ? appealReferenceNumber : '';
-        caseDetails += !fullName && !appealReferenceNumber ? this.caseReferencePipe.transform(data.case.case_id) : '';
-
-        this.titleService.setTitle(`${caseDetails} - HM Courts & Tribunals Service - GOV.UK`);
-
         const caseInfo = {
           cid: data.case.case_id,
           caseType: data.case.case_type.id,

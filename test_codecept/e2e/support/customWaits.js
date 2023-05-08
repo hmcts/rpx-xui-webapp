9c2f1f32c4620663dcfd6d5f4cc51275e8aaddb5
sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
2023-05-08 21:21:32 +0100

    codecept element waitForFor clikable updated inline with codeceptjs behaviour

--- test_codecept/e2e/support/customWaits.js
+++ test_codecept/e2e/support/customWaits.js
@@ -54,12 +54,19 @@ class BrowserWaits{
         const startTime = Date.now();
         const waitTimeInMilliSec = waitInSec ? waitInSec * 1000 : this.waitTime;
         CucumberReporter.AddMessage("starting wait for element clickable max in sec " + waitTimeInMilliSec + " : " + JSON.stringify(element.selector));
-        try {
-            // await I.waitForElement(EC.elementToBeClickable(element), waitTimeInMilliSec, "Error waitForElementClickable : " + JSON.stringify(element.selector));
-        } catch (err) {
-            CucumberReporter.AddMessage(`Wait for element clikable failed ${JSON.stringify(element.selector) }, not throwing exception to let test fail in next step if required state not met`);
+        let isEnabled = false;
+        for(let i = 0; i< 20;i++){
+            await this.waitForSeconds(1);
+            isEnabled = await element.isEnabled();
+            if(isEnabled){
+                break;
+            }
         }
+
         CucumberReporter.AddMessage("wait done in sec " + (Date.now() - startTime) / 1000);
+        if (!isEnabled){
+            throw Error(`element is not enabled : ${JSON.stringify(element.selector) }`)
+        }
     }
 
     async waitForCondition(condition, message) {

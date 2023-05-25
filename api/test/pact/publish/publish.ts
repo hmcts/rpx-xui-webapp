bad7ca6fc611542d027d43da8ddd510d1df4086a
Ruban <ruban72@hotmail.com>
2023-05-25 11:11:49 +0100

    EUI-8429: Fix pactbroker url missing protocol issue

--- api/test/pact/publish/publish.ts
+++ api/test/pact/publish/publish.ts
@@ -11,16 +11,18 @@ import {
 } from '../../../configuration/references';
 
 const publish = async (): Promise<void> => {
+  function getPactBrokerURL() {
+    return getConfigValue(PACT_BROKER_URL).includes('localhost') ? getConfigValue(PACT_BROKER_URL)
+      : `https://${getConfigValue(PACT_BROKER_URL)}`;
+  }
+
   try {
-    const pactBroker = getConfigValue(PACT_BROKER_URL) ?
-      'https://' + getConfigValue(PACT_BROKER_URL) : 'http://localhost:80';
+    const pactBroker = getConfigValue(PACT_BROKER_URL) ? getPactBrokerURL() : 'http://localhost:80';
     const pactTag = getConfigValue(PACT_BRANCH_NAME) ?
       getConfigValue(PACT_BRANCH_NAME) : 'Dev';
 
     const consumerVersion = getConfigValue(PACT_CONSUMER_VERSION) !== '' ?
       getConfigValue(PACT_CONSUMER_VERSION) : git.short();
-
-    process.env.SSL_CERT_FILE = path.resolve(__dirname, '../cer/ca-bundle.crt');
 
     const opts = {
       consumerVersion,

075368ffb7f40918a67ba668fc61057770d2c832
Matei Baciu <matei.baciu@hmcts.net>
2023-05-31 12:57:32 +0100

    8412

--- src/cases/containers/query-management-container/query-management-container.component.ts
+++ src/cases/containers/query-management-container/query-management-container.component.ts
@@ -1,7 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { partyMessagesMockData, QueryListItem } from '@hmcts/ccd-case-ui-toolkit';
+import { ActivatedRoute } from '@angular/router';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'exui-query-management-container',
   templateUrl: './query-management-container.component.html'
 })
-export class QueryManagementContainerComponent { }
+export class QueryManagementContainerComponent implements OnInit {
+  public queryItem: QueryListItem | undefined;
+  public formGroup: FormGroup = new FormGroup({});
+
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  public ngOnInit() {
+    const queryItemId = this.activatedRoute.snapshot.data.qid;
+    if (queryItemId) {
+      this.queryItem = new QueryListItem();
+      Object.assign(this.queryItem, partyMessagesMockData[0].partyMessages[0]);
+    }
+  }
+}

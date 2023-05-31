12f9c4c530435ffc07e930f6a5a7c503ec7910b1
Kartheek-S <92437222+Kartheek-S@users.noreply.github.com>
2023-05-31 13:27:44 +0100

    Update yarnaudit.sh

--- yarnaudit.sh
+++ yarnaudit.sh
@@ -1,1 +1,1 @@
-yarn npm audit --environment production --json | jq -cr '.advisories| to_entries[] | {"type": "auditAdvisory", "data": { "advisory": .value }}' > yarn-audit-known-issues
+yarn audit --groups dependencies --json | grep auditAdvisory > yarn-audit-known-issues

8c108679e85cd01ea72da65650965a1fcb85915c
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2023-06-01 16:42:29 +0100

    Add functionality to check service against locations

--- src/staff-administrator/components/staff-add-edit-user/staff-add-edit-user-form/staff-select-location/staff-select-location.component.ts
+++ src/staff-administrator/components/staff-add-edit-user/staff-add-edit-user-form/staff-select-location/staff-select-location.component.ts
@@ -20,6 +20,7 @@ export class StaffSelectLocationComponent implements OnInit {
   public filteredList$: Observable<LocationByEPIMMSModel[] | boolean>;
   public searchTermFormControl: FormControl = new FormControl('');
   public autocompleteSelectedLocation: LocationByEPIMMSModel | false;
+  private fullLocations: LocationByEPIMMSModel[];
 
   public get selectedLocations(): StaffUserLocation[] {
     return this.locationsControl?.value;
@@ -51,12 +52,42 @@ export class StaffSelectLocationComponent implements OnInit {
           map((locations) => locations.filter((location, index, array) =>
             index === array.findIndex((item) => item.venue_name === location.venue_name)
           )),
+          // Give each location a respective list of service codes
+          map((locations) => this.setLocationServiceCodes(locations)),
         ),
         // Returns false if the search term is empty to not show the autocomplete field i.e. ngIf should be false
         of(false)
       )
       )
     );
+  }
+
+  private setLocationServiceCodes(locations: LocationByEPIMMSModel[]): LocationByEPIMMSModel[] {
+    locations.map((location) => {
+      const currentId = location.epimms_id;
+      const serviceCodes = location.serviceCodes;
+      location.serviceCodes = this.getAllServiceCodes(serviceCodes, currentId);
+    });
+    // EUI-8051 - as well as setting the correct service codes for new locations, we need to edit them for existing locations
+    // note: we could edit location types to produce less code - i.e. making them the same
+    const fixedSelectedLocations = this.locationsControl.value;
+    fixedSelectedLocations.forEach((location) => {
+      const currentId = location.location_id;
+      const serviceCodes = location.service_codes;
+      location.service_codes = this.getAllServiceCodes(serviceCodes, currentId);
+    });
+    this.locationsControl.setValue(fixedSelectedLocations);
+    return locations;
+  }
+
+  public getAllServiceCodes(serviceCodes: string[], currentId: string): string[] {
+    const duplicateServiceLocations = this.fullLocations.filter((duplicateLocation) => duplicateLocation.epimms_id === currentId && duplicateLocation.serviceCodes[0] !== serviceCodes[0]);
+      duplicateServiceLocations.forEach((duplicateLocation) => {
+        if (!serviceCodes.includes(duplicateLocation.serviceCodes[0])) {
+          serviceCodes.push(duplicateLocation.serviceCodes[0]);
+        }
+      });
+    return serviceCodes;
   }
 
   public onSelectionChange(location: LocationByEPIMMSModel) {
@@ -68,9 +99,9 @@ export class StaffSelectLocationComponent implements OnInit {
       const locationToBeAdded = {
         location_id: this.autocompleteSelectedLocation.epimms_id,
         location: this.autocompleteSelectedLocation.venue_name,
-        is_primary: this.isPrimaryMode
+        is_primary: this.isPrimaryMode,
+        service_codes: this.autocompleteSelectedLocation.serviceCodes
       };
-
       // If Primary, we need to remove the other primary location first
       let currentSelectedLocations = this.selectedLocations;
       if (this.isPrimaryMode) {
@@ -97,6 +128,7 @@ export class StaffSelectLocationComponent implements OnInit {
     locations: LocationByEPIMMSModel[],
     selectedLocations: StaffUserLocation[],
   ): LocationByEPIMMSModel[] {
+    this.fullLocations = locations;
     return locations.filter(
       (location) => !selectedLocations.map((selectedLocation) => selectedLocation.location_id).includes(location.epimms_id) && location.venue_name
     );

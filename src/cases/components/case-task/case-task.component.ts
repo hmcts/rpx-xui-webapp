93812248c9ddb505da0e2397d6da03b92a20c8f0
John Benjamin <john.benjamin@hmcts.net>
2023-06-05 17:24:13 +0100

    Update case-task.component.ts

--- src/cases/components/case-task/case-task.component.ts
+++ src/cases/components/case-task/case-task.component.ts
@@ -124,7 +124,10 @@ export class CaseTaskComponent implements OnInit {
       showAssigneeColumn: true
     };
     const actionUrl = `/work/${task.id}/${option.id}`;
-    this.router.navigate([actionUrl], { queryParams: { service: task.jurisdiction }, state });
+    this.router.navigate([actionUrl], { queryParams: { service: task.jurisdiction }, state }).then(
+      (result) => console.log(result),
+      (error) => console.error(error)
+    );
   }
 
   /**

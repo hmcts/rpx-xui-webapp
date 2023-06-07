357b476725ed3fe4cd82b2b7b3565b576cf33720
DavidJayakumar <David.Jayakumar@hmcts.net>
2023-06-07 10:39:14 +0100

    pass form group

--- src/cases/containers/query-management-container/query-management-container.component.ts
+++ src/cases/containers/query-management-container/query-management-container.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { partyMessagesMockData, QueryListItem } from '@hmcts/ccd-case-ui-toolkit';
-import { ActivatedRoute } from '@angular/router';
 import { FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { QueryListItem, partyMessagesMockData } from '@hmcts/ccd-case-ui-toolkit';
 
 @Component({
   selector: 'exui-query-management-container',
@@ -9,15 +9,25 @@ import { FormGroup } from '@angular/forms';
 })
 export class QueryManagementContainerComponent implements OnInit {
   public queryItem: QueryListItem | undefined;
+  public showSummary: boolean = false;
   public formGroup: FormGroup = new FormGroup({});
 
   constructor(private activatedRoute: ActivatedRoute) {}
 
   public ngOnInit(): void {
-    const queryItemId = this.activatedRoute.snapshot.data.qid;
+    const queryItemId = this.activatedRoute.snapshot.params.qid;
     if (queryItemId) {
       this.queryItem = new QueryListItem();
       Object.assign(this.queryItem, partyMessagesMockData[0].partyMessages[0]);
     }
+  }
+
+  public showResponseForm(): void {
+    this.showSummary = false;
+  }
+
+  public confirmDetails(formGroup: FormGroup): void {
+    this.formGroup = formGroup;
+    this.showSummary = true;
   }
 }

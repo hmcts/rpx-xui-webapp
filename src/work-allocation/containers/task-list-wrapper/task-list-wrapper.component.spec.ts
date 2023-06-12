a2c306cb92172d641e50b83b58a3fda7c8c30fd4
Matei <108479475+MateiHMCTS@users.noreply.github.com>
2023-06-12 17:21:07 +0100

    Feature/e UI 8379 (#3059)
    * EUI-8367/EUI-8398: Query Management routing
    
    Define new routes for Query Management feature, using the existing `CaseResolver` resolver to load case data. Also define new Query Management container component.
    
    * Fix linting errors
    
    * fix
    
    * update ccd-toolkit
    
    * 8412
    
    * lint warning
    
    * fix test
    
    * pr comment
    
    * EUI-8379
    
    * EUI-8379
    
    * fix test
    
    * bump ccd-case-ui-toolkit version
    
    * bump ccd toolkit
    
    ---------
    
    Co-authored-by: Daniel Lam <daniel.2.lam@atos.net>

--- src/work-allocation/containers/task-list-wrapper/task-list-wrapper.component.spec.ts
+++ src/work-allocation/containers/task-list-wrapper/task-list-wrapper.component.spec.ts
@@ -65,7 +65,7 @@ describe('TaskListWrapperComponent', () => {
   let mockRpxTranslationService: jasmine.SpyObj<RpxTranslationService>;
 
   beforeEach((() => {
-    mockRpxTranslationService = jasmine.createSpyObj('mockRpxTranslationService', ['translate', 'getTranslation']);
+    mockRpxTranslationService = jasmine.createSpyObj('mockRpxTranslationService', ['translate', 'getTranslation$']);
     storeMock = jasmine.createSpyObj('Store', ['dispatch']);
     TestBed.configureTestingModule({
       imports: [

cf1242070027334353be267f4d0c109c448da5a5
Matei Baciu <matei.baciu@hmcts.net>
2023-06-15 15:26:32 +0100

    EUI-8425-rebased

--- src/cases/containers/query-management-container/query-management-container.component.ts
+++ src/cases/containers/query-management-container/query-management-container.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { Document, FormDocument, QueryItemType, QueryListItem, partyMessagesMockData } from '@hmcts/ccd-case-ui-toolkit';
+import {
+  Document,
+  FormDocument,
+  partyMessagesMockData,
+  QueryItemType,
+  QueryListItem,
+  RaiseQueryErrorMessage,
+  RespondToQueryErrorMessages
+} from '@hmcts/ccd-case-ui-toolkit';
+import { ErrorMessage } from '../../../app/models';
 
 @Component({
   selector: 'exui-query-management-container',
@@ -9,20 +18,22 @@ import { Document, FormDocument, QueryItemType, QueryListItem, partyMessagesMock
   styleUrls: ['./query-management-container.component.scss']
 })
 export class QueryManagementContainerComponent implements OnInit {
+  public queryCreateContext: QueryItemType = QueryItemType.NEW;
   public queryItem: QueryListItem | undefined;
   public showSummary: boolean = false;
   public formGroup: FormGroup = new FormGroup({});
   public submitted = false;
-  public queryCreateContext: QueryItemType;
+  public errorMessages: ErrorMessage[] = [];
 
   constructor(private activatedRoute: ActivatedRoute) {}
 
   public ngOnInit(): void {
     this.formGroup = new FormGroup({
       fullName: new FormControl(null, Validators.required),
-      subject: new FormControl(null, Validators.required),
+      subject: new FormControl(null),
       body: new FormControl(null, Validators.required),
-      isHearingRelated: new FormControl(null, Validators.required),
+      isHearingRelated: new FormControl(null),
+      hearingDate: new FormControl(null),
       attachments: new FormControl([] as Document[])
     });
 
@@ -31,6 +42,9 @@ export class QueryManagementContainerComponent implements OnInit {
       this.queryItem = new QueryListItem();
       Object.assign(this.queryItem, partyMessagesMockData[0].partyMessages[0]);
       this.queryCreateContext = queryItemId === '1' ? QueryItemType.RESPOND : QueryItemType.FOLLOWUP;
+    } else {
+      (this.formGroup.get('subject') as FormControl).setValidators([Validators.required]);
+      (this.formGroup.get('isHearingRelated') as FormControl).setValidators([Validators.required]);
     }
   }
 
@@ -39,8 +53,9 @@ export class QueryManagementContainerComponent implements OnInit {
   }
 
   public submitForm(): void {
-    this.showSummary = true;
     this.submitted = true;
+    this.validateForm();
+    this.showSummary = this.errorMessages?.length === 0;
   }
 
   public onDocumentCollectionUpdate(uploadedDocuments: FormDocument[]): void {
@@ -59,5 +74,61 @@ export class QueryManagementContainerComponent implements OnInit {
     );
 
     this.formGroup.get('attachments').setValue(attachments);
+  }
+
+  public validateForm(): void {
+    const contextIsNew = this.queryCreateContext === QueryItemType.NEW;
+    this.errorMessages = [];
+    if (!this.formGroup.get('fullName').valid) {
+      this.errorMessages.push({
+        title: '',
+        description: contextIsNew ? RaiseQueryErrorMessage.FULL_NAME : RespondToQueryErrorMessages.FULL_NAME,
+        fieldId: 'fullName'
+      });
+    }
+
+    if (!this.formGroup.get('subject').valid) {
+      this.errorMessages.push({
+        title: '',
+        description: RaiseQueryErrorMessage.QUERY_SUBJECT,
+        fieldId: 'subject'
+      });
+    }
+
+    if (!this.formGroup.get('body').valid) {
+      this.errorMessages.push({
+        title: '',
+        description: contextIsNew ? RaiseQueryErrorMessage.QUERY_BODY : RespondToQueryErrorMessages.QUERY_BODY,
+        fieldId: 'body'
+      });
+    }
+
+    if (!this.formGroup.get('isHearingRelated').valid) {
+      this.errorMessages.push({
+        title: '',
+        description: RaiseQueryErrorMessage.QUERY_HEARING_RELATED,
+        fieldId: 'isHearingRelated-yes'
+      });
+    } else {
+      if (this.formGroup.get('isHearingRelated').value === true &&
+          this.formGroup.get('hearingDate').value === null) {
+        this.errorMessages.push({
+          title: '',
+          description: RaiseQueryErrorMessage.QUERY_HEARING_DATE,
+          fieldId: 'hearingDate-day'
+        });
+      }
+    }
+    window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
+  }
+
+  public navigateToErrorElement(elementId: string): void {
+    if (elementId) {
+      const htmlElement = document.getElementById(elementId);
+      if (htmlElement) {
+        htmlElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        htmlElement.focus();
+      }
+    }
   }
 }

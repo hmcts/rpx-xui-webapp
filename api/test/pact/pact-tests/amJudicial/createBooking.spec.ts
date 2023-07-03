3c434b0ddf11ffb60ad90844e93f7c4cc34d0e7c
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2023-07-03 17:04:46 +0100

    Add the pact tests
    Get bookings and create booking

--- api/test/pact/pact-tests/amJudicial/createBooking.spec.ts
+++ api/test/pact/pact-tests/amJudicial/createBooking.spec.ts
@@ -10,25 +10,26 @@ const { Matchers } = require('@pact-foundation/pact');
 const { somethingLike } = Matchers;
 const pactSetUp = new PactTestSetup({ provider: 'am_judicialBooking', port: 8000 });
 
-const MockApp = require('../../../../../test/nodeMock/app');
-
-const REQUEST_BODY = {
-  queryRequest: { userIds: ['018a0310-f122-4377-9504-f635301f39ed-test2'] }
-};
-
-const RESPONSE_BODY = { bookings: [{
+const RESPONSE_BODY = {
+  userId: somethingLike('018a0310-f122-4377-9504-f635301f39ed-test2'),
+  locationId: somethingLike('123'),
+  regionId: somethingLike('1'),
   beginTime: somethingLike('01-01-2000'),
   endTime: somethingLike('01-01-3000'),
   created: somethingLike('01-01-1999'),
-  id: somethingLike('123456789'),
-  locationId: somethingLike('123'),
-  regionId: somethingLike('1'),
-  userId: somethingLike('018a0310-f122-4377-9504-f635301f39ed-test2'),
-  locationName: somethingLike('Test Location') }]
+  log: somethingLike('Booking created')
 };
 
-describe('Access management api, get bookings', () => {
-  describe('post /am/bookings/query', () => {
+const REQUEST_BODY = { bookingRequest: {
+  beginTime: somethingLike('01-01-2000'),
+  endTime: somethingLike('01-01-3000'),
+  locationId: somethingLike('123'),
+  regionId: somethingLike('1'),
+  userId: somethingLike('018a0310-f122-4377-9504-f635301f39ed-test2')
+} };
+
+describe('Access management api, create booking', () => {
+  describe('post /am/bookings', () => {
     const sandbox: sinon.SinonSandbox = sinon.createSandbox();
     let next;
 
@@ -39,11 +40,11 @@ describe('Access management api, get bookings', () => {
     before(async () => {
       await pactSetUp.provider.setup();
       const interaction = {
-        state: 'return bookings for specific user',
+        state: 'create booking',
         uponReceiving: 'relevant user id',
         withRequest: {
           method: 'POST',
-          path: '/am/bookings/query',
+          path: '/am/bookings',
           headers: {
             'Authorization': 'Bearer someAuthorizationToken',
             'ServiceAuthorization': 'Bearer someServiceAuthorizationToken',
@@ -66,35 +67,15 @@ describe('Access management api, get bookings', () => {
     afterEach(() => {
       sandbox.restore();
       sinon.reset();
-      MockApp.stopServer();
     });
 
     it('returns the correct response', async () => {
-      MockApp.setServerPort(8080);
-      MockApp.init();
-
-      MockApp.onGet('/refdata/location/court-venues/services', (req, res) => {
-        res.send({
-          court_venues: [
-            { epimms_id: '1234', location: [{ site_name: 'Test location' }] }
-          ]
-        });
-      });
-      await MockApp.startServer();
-
       const configValues = getJudicialBookingAPIOverrides(pactSetUp.provider.mockService.baseUrl);
-      configValues['services.location_api'] = 'http://localhost:8080';
-
-      // @ts-ignore
-      configValues.serviceRefDataMapping = [
-        { 'service': 'IA', 'serviceCodes': ['BFA1'] }, { 'service': 'CIVIL', 'serviceCodes': ['AAA6', 'AAA7'] }
-      ];
-
       sandbox.stub(config, 'get').callsFake((prop) => {
         return configValues[prop];
       });
 
-      const { getBookings } = requireReloaded('../../../../accessManagement/index');
+      const { createBooking } = requireReloaded('../../../../accessManagement/index');
 
       const req = mockReq({
         headers: {
@@ -103,8 +84,11 @@ describe('Access management api, get bookings', () => {
           'content-type': 'application/json'
         },
         body: {
-          userId: '018a0310-f122-4377-9504-f635301f39ed-test2',
-          bookableServices: ['IA']
+          beginTime: '01-01-2000',
+          endTime: '01-01-3000',
+          locationId: '123',
+          regionId: '1',
+          userId: '018a0310-f122-4377-9504-f635301f39ed-test2'
         }
 
       });
@@ -115,7 +99,7 @@ describe('Access management api, get bookings', () => {
       };
 
       try {
-        await getBookings(req, response, next);
+        await createBooking(req, response, next);
 
         assertResponses(returnedResponse);
         pactSetUp.provider.verify();
@@ -131,12 +115,10 @@ describe('Access management api, get bookings', () => {
 });
 
 function assertResponses(dto: any) {
-  expect(dto[0].beginTime).to.be.equal('01-01-2000');
-  expect(dto[0].endTime).to.be.equal('01-01-3000');
-  expect(dto[0].created).to.be.equal('01-01-1999');
-  expect(dto[0].id).to.be.equal('123456789');
-  expect(dto[0].locationId).to.be.equal('123');
-  expect(dto[0].regionId).to.be.equal('1');
-  expect(dto[0].userId).to.be.equal('018a0310-f122-4377-9504-f635301f39ed-test2');
-  expect(dto[0].locationName).to.be.equal(null);
+  expect(dto.beginTime).to.be.equal('01-01-2000');
+  expect(dto.endTime).to.be.equal('01-01-3000');
+  expect(dto.created).to.be.equal('01-01-1999');
+  expect(dto.locationId).to.be.equal('123');
+  expect(dto.regionId).to.be.equal('1');
+  expect(dto.userId).to.be.equal('018a0310-f122-4377-9504-f635301f39ed-test2');
 }

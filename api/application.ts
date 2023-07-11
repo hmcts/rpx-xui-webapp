0dab78f02cdfbbed6a37fa377c03a2cb2206267c
connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>
2023-07-11 14:02:14 +0100

    Fix pagination issue (#3145)
    * Node-lib 2.27.0
    
    * audit changes
    
    * Idam check
    
    * lint fix
    
    * audit
    
    * Fix pagination issue
    
    * rollback the nodelibrary
    
    * udit
    
    * Update yarn-audit-known-issues
    
    * Update all-work-task.component.ts
    
    * yarn audit
    
    ---------
    
    Co-authored-by: Uday Denduluri <uday.denduluri1@hmcts.net>

--- api/application.ts
+++ api/application.ts
@@ -20,6 +20,7 @@ import openRoutes from './openRoutes';
 import { initProxy } from './proxy.config';
 import routes from './routes';
 import workAllocationRouter from './workAllocation/routes';
+import { idamCheck } from './idamCheck';
 
 export const app = express();
 
@@ -117,3 +118,5 @@ app.use(csrf({ cookie: { key: 'XSRF-TOKEN', httpOnly: false, secure: true }, ign
 
 const logger: JUILogger = log4jui.getLogger('Application');
 logger.info(`Started up using ${getConfigValue(PROTOCOL)}`);
+
+new Promise(idamCheck).then(() => 'IDAM is up and running');

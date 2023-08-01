021b58be0a11aa9ef22974620fc8610557082fd7
DavidJayakumar <David.Jayakumar@hmcts.net>
2023-08-01 15:10:41 +0100

    Add translation

--- src/cases/components/restricted-case-access/restricted-case-access.component.spec.ts
+++ src/cases/components/restricted-case-access/restricted-case-access.component.spec.ts
@@ -1,5 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RestrictedCaseAccessComponent } from './restricted-case-access.component';
+
+@Pipe({ name: 'rpxTranslate' })
+class RpxTranslateMockPipe implements PipeTransform {
+  public transform(value: string): string {
+    return value;
+  }
+}
 
 describe('RestrictedCaseAccessComponent', () => {
   let component: RestrictedCaseAccessComponent;
@@ -8,7 +16,7 @@ describe('RestrictedCaseAccessComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [],
-      declarations: [RestrictedCaseAccessComponent]
+      declarations: [RestrictedCaseAccessComponent, RpxTranslateMockPipe]
     })
       .compileComponents();
     fixture = TestBed.createComponent(RestrictedCaseAccessComponent);

c967db049150f3fe658923526b8f9a64710f655d
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2023-08-17 09:14:53 +0100

    EXUI-266: Initial commit of HMC required changes

--- api/lib/http/index.ts
+++ api/lib/http/index.ts
@@ -2,7 +2,11 @@ import axios, { AxiosInstance } from 'axios';
 import { errorInterceptor, requestInterceptor, successInterceptor } from '../interceptors';
 
 export const http: AxiosInstance = axios.create({});
+const deploymentId: string = process.env.DEPLOYMENT_ID;
 
+if (deploymentId) {
+  axios.defaults.headers.common['hmcts-deployment-id'] = deploymentId;
+}
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 http.interceptors.request.use(requestInterceptor);
 http.interceptors.response.use(successInterceptor, errorInterceptor);

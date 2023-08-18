79124ff4ff6cafdc8bee24fe368f9ec656ada011
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2023-08-18 14:34:08 +0100

    EXUI-266: Tweak script

--- populatepreviewdeploymentid.sh
+++ populatepreviewdeploymentid.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-CHART_DIRECTORY=$1
-BRANCH=$2
-BRANCH_PR_NUMBER=$3
+BRANCH_PR_NUMBER=$1
 
-cd ${CHART_DIRECTORY}
-sed -i -e "s/^PREVIEW_DEPLOYMENT_ID:.*/PREVIEW_DEPLOYMENT_ID: exui-preview-deployment-BRANCH_PR_NUMBER/" "${CHART_DIRECTORY}/charts/xui-webapp/values.preview.template.yaml"
+sed -i -e "s/^PREVIEW_DEPLOYMENT_ID:.*/PREVIEW_DEPLOYMENT_ID: exui-preview-deployment-$BRANCH_PR_NUMBER/" "/charts/xui-webapp/values.preview.template.yaml"
 
 exit 1

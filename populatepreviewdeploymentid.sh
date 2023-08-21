570c6c008172d2cca1e90f87f86dd017befa6627
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2023-08-21 11:05:13 +0100

    EXUI-266: Update to github action after local testing

--- populatepreviewdeploymentid.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BRANCH_PR_NUMBER=$1
-
-sed -i -e "s/^PREVIEW_DEPLOYMENT_ID:.*/PREVIEW_DEPLOYMENT_ID: exui-preview-deployment-$BRANCH_PR_NUMBER/" "/charts/xui-webapp/values.preview.template.yaml"
-
-exit 1

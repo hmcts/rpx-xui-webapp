4977ed2e113f7a092220469256ad4a0f0bf40b0f
Ritesh Dsouza <ritesh.dsouza@HMCTS.net>
2023-08-23 14:09:18 +0100

    added required roles

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -183,9 +183,9 @@ export class AppConstants {
 
 export const LD_FLAG_REMOVE_USER_FROM_CASE_MC: string = 'remove-user-from-case-mc';
 export const LD_FLAG_MC_APPLICATION_THEMES: string = 'mc-application-themes';
-export const LEGAL_OPS_ROLE_LIST: string[] = ['caseworker-ia-caseofficer', 'caseworker-ia', 'caseworker-ia-admofficer', 'task-supervisor', 'caseworker-civil', 'caseworker-privatelaw'];
-export const JUDICIAL_ROLE_LIST: string[] = ['caseworker-ia-iacjudge', 'caseworker-privatelaw-judge', 'judge'];
-export const ADMIN_ROLE_LIST: string[] = ['admin'];
-export const CTSC_ROLE_LIST: string[] = ['ctsc'];
+export const LEGAL_OPS_ROLE_LIST: string[] = ['caseworker-ia-caseofficer', 'caseworker-ia', 'caseworker-ia-admofficer', 'task-supervisor', 'caseworker-civil', 'caseworker-privatelaw', 'hmcts-legal-operations'];
+export const JUDICIAL_ROLE_LIST: string[] = ['caseworker-ia-iacjudge', 'caseworker-privatelaw-judge', 'judge', 'hmcts-judiciary', 'judiciary', 'panelmember'];
+export const ADMIN_ROLE_LIST: string[] = ['hmcts-admin'];
+export const CTSC_ROLE_LIST: string[] = ['hmcts-ctsc'];
 export const SERVICE_OPTIONS_LIST = [{ key: 'IA', label: 'Immigration and Asylum' }, { key: 'SSCS', label: 'Social security and child support' }];
 export const PUI_CASE_MANAGER = 'pui-case-manager';

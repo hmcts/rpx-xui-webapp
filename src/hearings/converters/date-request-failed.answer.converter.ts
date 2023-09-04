44a438eccdc7d4b9d9c2eeab3fe7323ffb346f21
olusegun odunukan <olusegun.odunukan@hmcts.net>
2023-09-04 13:06:06 +0100

    fix: EXUI-388 - Timestamp in Hearing Requests in error page

--- src/hearings/converters/date-request-failed.answer.converter.ts
+++ src/hearings/converters/date-request-failed.answer.converter.ts
@@ -9,8 +9,14 @@ export class DateRequestFailedAnswerConverter implements AnswerConverter {
   public transformAnswer(hearingState$: Observable<State>): Observable<string> {
     return hearingState$.pipe(
       map((state) => {
-        const hearingResponse = state.hearingRequest.hearingRequestMainModel.hearingResponse;
-        return moment(hearingResponse.errorTimestamp).format(HearingDateEnum.RequestFailedDateAndTime);
+        const hearingResponse = state.hearingRequest.hearingRequestMainModel;
+
+        if (hearingResponse.hearingResponse.errorTimestamp) {
+          return moment(hearingResponse.hearingResponse.errorTimestamp).format(HearingDateEnum.RequestFailedDateAndTime);
+        } else if (hearingResponse.requestDetails) {
+          return moment(hearingResponse.requestDetails.timestamp).format(HearingDateEnum.RequestFailedDateAndTime);
+        }
+        return '';
       })
     );
   }

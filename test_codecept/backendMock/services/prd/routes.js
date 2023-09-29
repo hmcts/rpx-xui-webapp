8ae826100a5d73d4ea0725f6f327a44b203665e3
sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
2023-09-29 13:52:56 +0100

    create hearings scenarios added

--- test_codecept/backendMock/services/prd/routes.js
+++ test_codecept/backendMock/services/prd/routes.js
@@ -9,15 +9,27 @@ const userApiData = require('../userApiData')
 const hearingTypeCategories = require('./hearingType_Categories')
 
 
-router.get('/lov/categories/HearingType', (req, res) => {
+router.get('/lov/categories/:category', (req, res) => {
+    res.send({ list_of_values: service.categoryTypes[req.params.category].value})
+    // if (req.params.category === 'HearingType'){
+    //     res.send(hearingTypeCategories)
+    // } else if (req.params.category === 'CaseLinkingReasonCode'){
+    //     res.send({ list_of_values  : []})
+    // } else if (req.params.category === 'HearingChannel') {
+    //     res.send(hearingTypeCategories)
+    // } else if (req.params.category === 'PanelMemberType') {
+    //     res.send({ list_of_values: [] })
 
-    res.send(hearingTypeCategories)
-});
+    // } else if (req.params.category === 'HearingPriority') {
+    //     res.send({ list_of_values: [] })
 
+    // } else if (req.params.category === 'HearingSubChannel') {
+    //     res.send({ list_of_values: [] })
 
-router.get('/lov/categories/:categoryid', (req, res) => {
- 
-    res.send({ list_of_values: service.categoryTypes[req.params.categoryid].value })
+    // }
+    // else {
+    //     throw new Error(`unknown category: ${req.params.category}`)
+    // }
 });
 
 router.get('/caseflags/:service-id=ABA5', (req,res) => {

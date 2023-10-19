fa2871e1df68a293db9b43144576d6d1e3d484d1
Richard Ash <richard.ash@hmcts.net>
2023-10-19 11:44:14 +0100

    Add judicial user response model

---
+++ src/hearings/models/judicialUserReponse.model.ts
@@ -0,0 +1,14 @@
+export interface JudicialUserResponseModel {
+  title: string;
+  knownAs: string;
+  surname: string;
+  fullName: string;
+  emailId: string;
+  idamId: string;
+  initials: string;
+  postNominals: string;
+  personalCode: string;
+  isJudge: string;
+  isMagistrate: string;
+  isPanelMember: string;
+}

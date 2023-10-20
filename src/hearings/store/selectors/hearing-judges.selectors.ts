e7dd9f6f2e2804964e99c38934b45b0208e934d8
Richard Ash <richard.ash@hmcts.net>
2023-10-20 10:43:19 +0100

    Refactor getHearingJudgeIds selector, test for null caseHearings and null hearingDaySchedule

--- src/hearings/store/selectors/hearing-judges.selectors.ts
+++ src/hearings/store/selectors/hearing-judges.selectors.ts
@@ -3,12 +3,13 @@ import * as fromFeature from '../reducers';
 export const getHearingJudgeIds = createSelector(
   fromFeature.getHearingsFeatureState,
   (state: fromFeature.State) => {
-    const hearingJudges: string[] = [];
-    for (const caseHearings of state.hearingList.hearingListMainModel.caseHearings) {
-      for (const hearingDaySchedule of caseHearings.hearingDaySchedule) {
-        hearingJudges.push(hearingDaySchedule.hearingJudgeId);
+    const caseHearings = state.hearingList && state.hearingList.hearingListMainModel && state.hearingList.hearingListMainModel.caseHearings || [];
+    return caseHearings.reduce((acc, caseHearing) => {
+      if (caseHearing.hearingDaySchedule) {
+        const judges = caseHearing.hearingDaySchedule.map((schedule) => schedule.hearingJudgeId);
+        return acc.concat(judges);
       }
-    }
-    return hearingJudges;
+      return acc;
+    }, []);
   }
 );

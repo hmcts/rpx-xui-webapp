ad91b75fd4419a1fc7a1e70bb8c39763a26d8b52
Richard Ash <richard.ash@hmcts.net>
2023-10-23 09:11:56 +0100

    Fix linting errors

--- src/hearings/store/selectors/hearing-judges.selectors.spec.ts
+++ src/hearings/store/selectors/hearing-judges.selectors.spec.ts
@@ -1,33 +1,27 @@
 import { getHearingJudgeIds } from './hearing-judges.selectors';
 
 describe('Hearing Judges selectors', () => {
-
   fdescribe('getHearingJudgeIds', () => {
-
     it('should return an empty array if hearingList is undefined', () => {
       const state = { hearingList: undefined };
       const result = getHearingJudgeIds.projector(state);
       expect(result).toEqual([]);
     });
-
     it('should return an empty array if hearingListMainModel is undefined', () => {
       const state = { hearingList: { hearingListMainModel: undefined } };
       const result = getHearingJudgeIds.projector(state);
       expect(result).toEqual([]);
     });
-
     it('should return an empty array if caseHearings is undefined', () => {
       const state = { hearingList: { hearingListMainModel: { caseHearings: undefined } } };
       const result = getHearingJudgeIds.projector(state);
       expect(result).toEqual([]);
     });
-
     it('should return an empty array if caseHearings is empty', () => {
       const state = { hearingList: { hearingListMainModel: { caseHearings: [] } } };
       const result = getHearingJudgeIds.projector(state);
       expect(result).toEqual([]);
     });
-
     it('should return the correct judge ids for each hearing', () => {
       const state = {
         hearingList: {

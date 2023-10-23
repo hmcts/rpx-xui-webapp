51c5713ee9557064c3d74d0b092fdb0dd04e4043
Richard Ash <richard.ash@hmcts.net>
2023-10-23 09:02:36 +0100

    Add further test scenarios to hearing judges selectors unit tests

--- src/hearings/store/selectors/hearing-judges.selectors.spec.ts
+++ src/hearings/store/selectors/hearing-judges.selectors.spec.ts
@@ -1,30 +1,46 @@
-import { TestBed } from '@angular/core/testing';
-import { select, Store, StoreModule } from '@ngrx/store';
-import { reducers } from '../index';
-import { State } from '../reducers';
 import { getHearingJudgeIds } from './hearing-judges.selectors';
 
 describe('Hearing Judges selectors', () => {
-  let store: Store<State>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        StoreModule.forRoot({}),
-        StoreModule.forFeature('hearings', reducers)
-      ]
-    });
-    store = TestBed.inject(Store);
-    spyOn(store, 'dispatch').and.callThrough();
-  });
+  fdescribe('getHearingJudgeIds', () => {
 
-  describe('getHearingJudgeIds', () => {
-    it('should return hearing judge ids', () => {
-      let result = null;
-      store.pipe(select(getHearingJudgeIds)).subscribe((value) => {
-        result = value;
-      });
+    it('should return an empty array if hearingList is undefined', () => {
+      const state = { hearingList: undefined };
+      const result = getHearingJudgeIds.projector(state);
       expect(result).toEqual([]);
+    });
+
+    it('should return an empty array if hearingListMainModel is undefined', () => {
+      const state = { hearingList: { hearingListMainModel: undefined } };
+      const result = getHearingJudgeIds.projector(state);
+      expect(result).toEqual([]);
+    });
+
+    it('should return an empty array if caseHearings is undefined', () => {
+      const state = { hearingList: { hearingListMainModel: { caseHearings: undefined } } };
+      const result = getHearingJudgeIds.projector(state);
+      expect(result).toEqual([]);
+    });
+
+    it('should return an empty array if caseHearings is empty', () => {
+      const state = { hearingList: { hearingListMainModel: { caseHearings: [] } } };
+      const result = getHearingJudgeIds.projector(state);
+      expect(result).toEqual([]);
+    });
+
+    it('should return the correct judge ids for each hearing', () => {
+      const state = {
+        hearingList: {
+          hearingListMainModel: {
+            caseHearings: [
+              { hearingDaySchedule: [{ hearingJudgeId: 1001 }, { hearingJudgeId: 1002 }] },
+              { hearingDaySchedule: [{ hearingJudgeId: 1003 }] }
+            ]
+          }
+        }
+      };
+      const result = getHearingJudgeIds.projector(state);
+      expect(result).toEqual([1001, 1002, 1003]);
     });
   });
 });

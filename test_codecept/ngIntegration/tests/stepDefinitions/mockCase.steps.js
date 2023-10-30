eefd7d436f93940e3fcc9f7f97896df89bdfeb4c
sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
2023-10-30 16:28:27 +0000

    mock case data fix

--- test_codecept/ngIntegration/tests/stepDefinitions/mockCase.steps.js
+++ test_codecept/ngIntegration/tests/stepDefinitions/mockCase.steps.js
@@ -72,7 +72,7 @@ const {postTaskAction, getTask} = require("../../../../api/workAllocation");
     });
 
     Given('I set MOCK case details with reference {string}', async function(caseDetailsReference){
-      const caseDetails = ccdMockData.caseDetailsResponse;
+        const caseDetails = ccdMockData.getDetailsMockData();
          global.scenarioData[caseDetailsReference] = caseDetails;
 
         await serviceMock.updateCaseData(global.scenarioData[caseDetailsReference], 200)

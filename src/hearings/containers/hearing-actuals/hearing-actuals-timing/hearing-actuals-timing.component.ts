b90885615e3e807b5aaf5bb5af78f080c6fbf19f
RiteshHMCTS <74713687+RiteshHMCTS@users.noreply.github.com>
2023-11-08 16:09:44 +0000

    exui-977-pass hearing date if field is not availab (#3341)
    * exui-977-pass hearing date if field is not availab
    
    * cve
    
    * cve
    
    * toolkit version updated
    
    * cve
    
    * table css amendments (#3332)
    
    Co-authored-by: Ritesh Dsouza <ritesh.dsouza@HMCTS.net>
    Co-authored-by: RiteshHMCTS <74713687+RiteshHMCTS@users.noreply.github.com>
    
    * toolkit version updated
    
    * deleted unwanted file
    
    * cve
    
    * toolkit version updated
    
    * toolkit version updated
    
    ---------
    
    Co-authored-by: OgunyemiO <142920130+OgunyemiO@users.noreply.github.com>

--- src/hearings/containers/hearing-actuals/hearing-actuals-timing/hearing-actuals-timing.component.ts
+++ src/hearings/containers/hearing-actuals/hearing-actuals-timing/hearing-actuals-timing.component.ts
@@ -186,8 +186,8 @@ export class HearingActualsTimingComponent implements OnInit, OnDestroy {
   }
 
   private getHearingTime(value:string, actualIndex: number, plannedIndex: number, time: 'startTime' | 'endTime'): string {
-    const hearingDate = this.hearingActuals.hearingActuals.actualHearingDays[actualIndex].hearingDate;
-
+    const hearingDate = this.hearingActuals.hearingActuals?.actualHearingDays.length > 0 ?
+      this.hearingActuals.hearingActuals.actualHearingDays[actualIndex].hearingDate : this.hearingDate;
     return value ? HearingActualsTimingComponent.replaceTime(hearingDate, moment(value, 'HH:mm')) : null;
   }
 

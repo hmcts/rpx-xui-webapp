35858e649febe98ad68ffb7ed116f38cc14a0c9b
Richard Ash <richard.ash@hmcts.net>
2023-11-24 13:01:27 +0000

    Add units test for hearingIsInFuture

--- src/hearings/containers/hearing-actuals/hearing-actuals-add-edit-summary/hearing-actuals-add-edit-summary.component.spec.ts
+++ src/hearings/containers/hearing-actuals/hearing-actuals-add-edit-summary/hearing-actuals-add-edit-summary.component.spec.ts
@@ -352,6 +352,27 @@ describe('HearingActualsAddEditSummaryComponent', () => {
       const actual = component.getPauseDateTime(actualHearingDays, 'start');
       expect(actual).toEqual(null);
     });
+
+  });
+
+  describe('hearingIsInFuture', () => {
+    it('should return true if hearing date is in the future', () => {
+      const hearingDate = '2050-01-01';
+      const result = component.hearingIsInFuture(hearingDate);
+      expect(result).toEqual(true);
+    });
+
+    it('should return false if hearing date is in the past', () => {
+      const hearingDate = '2021-01-01';
+      const result = component.hearingIsInFuture(hearingDate);
+      expect(result).toEqual(false);
+    });
+
+    it('should return false if the hearing date is the current date', () => {
+      const currentDate = new Date().toLocaleDateString();
+      const result = component.hearingIsInFuture(currentDate);
+      expect(result).toBe(false);
+    });
   });
 
   afterEach(() => {

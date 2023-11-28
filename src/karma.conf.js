1aad6275d63fb7b14518bcdb74f4a69101a955fa
olusegun odunukan <olusegun.odunukan@hmcts.net>
2023-11-28 10:10:25 +0000

    update karma config and sonar

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -2,48 +2,55 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-spec-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      jasmine:{
-        random: false,
-      },
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
-    },
-    coverageReporter: {
-      dir: require('path').join(__dirname, '../reports/tests/coverage/ng'),
-      subdir: '.',
-      reporters: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
     defaultTimeoutInterval: 60000,
     browserNoActivityTimeout: 60000,
     browserDisconnectTimeout: 60000,
-    reporters: ['spec', 'progress', 'kjhtml'],
+    basePath: '',
+    frameworks: ['parallel', 'jasmine', '@angular-devkit/build-angular'],
+    parallelOptions: {
+      executors: 1,
+      shardStrategy: 'round-robin'
+    },
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-parallel'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-spec-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-sonarqube-unit-reporter')
+    ],
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {
+        random: false,
+        verboseDeprecations: true,
+      }
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, '../reports/tests/coverage/ng'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true,
+      combineBrowserReports: true
+    },
+    reporters: ['spec', 'progress', 'kjhtml', 'coverage-istanbul'],
+    specReporter: {
+      maxLogLines: 5, // limit number of lines logged per test
+      suppressErrorSummary: false,// do not print error summary
+      suppressFailed: false, // do not print information about failed tests
+      suppressPassed: false, // do not print information about passed tests
+      suppressSkipped: true, // do not print information about skipped tests
+      showSpecTiming: false, // print the time elapsed for each spec
+      failFast: false // test would finish with error when a first fail occurs.
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
     singleRun: true,
     restartOnFileChange: true
   });

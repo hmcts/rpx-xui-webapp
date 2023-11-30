a165a91a1f208280838be8705abf2b58b79ff1b2
John Benjamin <john.benjamin@hmcts.net>
2023-11-30 19:11:16 +0000

    EUI-8920 CR-84 Hearings edit summary

--- src/hearings/containers/edit-hearing/hearing-requirements-section/hearing-requirements-section.component.ts
+++ src/hearings/containers/edit-hearing/hearing-requirements-section/hearing-requirements-section.component.ts
@@ -1,10 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CaseFlagReferenceModel } from '../../../models/caseFlagReference.model';
+import { HearingRequestMainModel } from '../../../models/hearingRequestMain.model';
+import { ServiceHearingValuesModel } from '../../../models/serviceHearingValues.model';
+import { CaseFlagsUtils } from '../../../utils/case-flags.utils';
 
 @Component({
   selector: 'exui-hearing-requirements-section',
   templateUrl: './hearing-requirements-section.component.html'
 })
 export class HearingRequirementsSectionComponent implements OnInit {
+  @Input() public caseFlagsRefData: CaseFlagReferenceModel[];
+  @Input() public hearingRequestMainModel: HearingRequestMainModel;
+  @Input() public serviceHearingValuesModel: ServiceHearingValuesModel;
+
+  public partyWithFlags: Map<string, CaseFlagReferenceModel[]>;
+
   public ngOnInit(): void {
+    this.partyWithFlags = CaseFlagsUtils.convertPartiesToPartyWithFlags(this.caseFlagsRefData, this.hearingRequestMainModel.partyDetails, this.serviceHearingValuesModel.parties);
   }
 }
--- src/hearings/containers/edit-hearing/panel-details-section/panel-details-section.component.ts
+++ src/hearings/containers/edit-hearing/panel-details-section/panel-details-section.component.ts
@@ -1,18 +1,28 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { MemberType, RadioOptions } from '../../../models/hearings.enum';
+import { MemberType, RadioOptions, RequirementType } from '../../../models/hearings.enum';
 import { PanelRequirementsModel } from '../../../models/panelRequirements.model';
+import { JudicialUserModel } from 'src/hearings/models/judicialUser.model';
+import { LovRefDataModel } from 'src/hearings/models/lovRefData.model';
 
 @Component({
   selector: 'exui-panel-details-section',
   templateUrl: './panel-details-section.component.html'
 })
 export class PanelDetailsSectionComponent implements OnInit {
+  @Input() public panelRolesRefData: LovRefDataModel[];
   @Input() public panelRequirements: PanelRequirementsModel;
+  @Input() public panelMembers: JudicialUserModel[];
 
   public hearingPanel: string;
+  public includedPanelMembers: string;
+  public excludedPanelMembers: string;
+  public panelRoles: string;
 
   public ngOnInit(): void {
     this.hearingPanel = this.getHearingPanel();
+    this.includedPanelMembers = this.getIncludedPanelMembers();
+    this.excludedPanelMembers = this.getExcludedPanelMembers();
+    this.panelRoles = this.getPanelRoles();
   }
 
   private getHearingPanel(): string {
@@ -22,5 +32,53 @@ export class PanelDetailsSectionComponent implements OnInit {
       return RadioOptions.YES;
     }
     return RadioOptions.NO;
+  }
+
+  private getIncludedPanelMembers(): string {
+    const includedPanelMembers = this.panelRequirements?.panelPreferences?.filter(
+      (preference) => preference.memberType === MemberType.PANEL_MEMBER && preference.requirementType === RequirementType.MUSTINC
+    ).map((preference) => preference.memberID);
+
+    const includedPanelMemberNames: string[] = [];
+    this.panelMembers.forEach((panelMemberInfo) => {
+      if (includedPanelMembers.includes(panelMemberInfo.personalCode)) {
+        includedPanelMemberNames.push(panelMemberInfo.fullName);
+      }
+    });
+    return includedPanelMemberNames.join();
+  }
+
+  private getExcludedPanelMembers(): string {
+    const excludedJudges = this.panelRequirements?.panelPreferences?.filter(
+      (preference) => preference.memberType === MemberType.PANEL_MEMBER && preference.requirementType === RequirementType.EXCLUDE
+    ).map((preference) => preference.memberID);
+
+    const excludedJudgeNames: string[] = [];
+    this.panelMembers.forEach((panelMemberInfo) => {
+      if (excludedJudges.includes(panelMemberInfo.personalCode)) {
+        excludedJudgeNames.push(panelMemberInfo.fullName);
+      }
+    });
+    return excludedJudgeNames.join();
+  }
+
+  private getPanelRoles(): string {
+    const selectedSpecialisms: string[] = [];
+    this.panelRequirements?.panelSpecialisms?.forEach((panelSpecialism) => {
+      let selectedSpecialism = '';
+      this.panelRolesRefData.forEach((panelRole) => {
+        if (panelRole.key === panelSpecialism) {
+          selectedSpecialism = panelRole.value_en;
+        } else if (panelRole.child_nodes?.length > 0) {
+          panelRole.child_nodes.forEach((specialism) => {
+            if (panelSpecialism === specialism.key && !selectedSpecialism.length) {
+              selectedSpecialism = `${panelRole.value_en} - ${specialism.value_en}`;
+            }
+          });
+        }
+      });
+      selectedSpecialisms.push(selectedSpecialism);
+    });
+    return selectedSpecialisms.join('<br>');
   }
 }

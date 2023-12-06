f33c7447950ef4b0da16de83f7249352b9ccd01d
DavidJayakumar <David.Jayakumar@hmcts.net>
2023-12-06 17:39:57 +0000

    Update reference
    Add new template for view hearing details

--- src/hearings/containers/view-hearing/hearing-view-summary/hearing-view-summary.component.spec.ts
+++ src/hearings/containers/view-hearing/hearing-view-summary/hearing-view-summary.component.spec.ts
@@ -1,24 +1,79 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FeatureToggleService } from '@hmcts/rpx-xui-common-lib';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
+import { UserDetails } from '../../../../app/models';
+import * as fromAppStore from '../../../../app/store';
+import { HEARING_REQUEST_VIEW_SUMMARY_TEMPLATE } from '../../../../hearings/templates/hearing-request-view-summary.template';
+import { HEARING_VIEW_ONLY_SUMMARY_TEMPLATE } from '../../../../hearings/templates/hearing-view-only-summary.template';
 import { HearingViewSummaryComponent } from './hearing-view-summary.component';
 
 describe('HearingViewSummaryComponent', () => {
   let component: HearingViewSummaryComponent;
   let fixture: ComponentFixture<HearingViewSummaryComponent>;
+  let storeMock: jasmine.SpyObj<Store<fromAppStore.State>>;
+  let featureToggleServiceMock: any;
+
+  const USER: UserDetails = {
+    canShareCases: true,
+    sessionTimeout: {
+      idleModalDisplayTime: 10,
+      totalIdleTime: 50
+    },
+    userInfo: {
+      id: '***REMOVED***',
+      forename: 'Luke',
+      surname: 'Wilson',
+      email: 'lukesuperuserxui@mailnesia.com',
+      active: true,
+      roles: [
+        'caseworker',
+        'caseworker-sscs'
+      ]
+    }
+  };
 
   beforeEach(() => {
+    storeMock = jasmine.createSpyObj<Store<fromAppStore.State>>('store', ['pipe']);
+    featureToggleServiceMock = jasmine.createSpyObj('featureToggleService', ['isEnabled']);
     TestBed.configureTestingModule({
       declarations: [HearingViewSummaryComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {
+          provide: Store,
+          useValue: storeMock
+        },
+        {
+          provide: FeatureToggleService,
+          useValue: featureToggleServiceMock
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HearingViewSummaryComponent);
     component = fixture.componentInstance;
+    storeMock.pipe.and.returnValue(of(USER));
+    featureToggleServiceMock.isEnabled.and.returnValue(of(false));
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set new template if the feature toggle is off and user is not hearing manager', () => {
+    component.ngOnInit();
+    expect(component.template).toBe(HEARING_VIEW_ONLY_SUMMARY_TEMPLATE);
+  });
+
+  it('should set new template if the feature toggle is on and user is hearing manager', () => {
+    USER.userInfo.roles.push('hearing-manager');
+    featureToggleServiceMock.isEnabled.and.returnValue(of(true));
+    fixture.detectChanges();
+    component.ngOnInit();
+    expect(component.template).toBe(HEARING_REQUEST_VIEW_SUMMARY_TEMPLATE);
   });
 
   afterEach(() => {

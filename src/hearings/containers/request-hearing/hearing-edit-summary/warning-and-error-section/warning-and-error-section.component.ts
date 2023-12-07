866fadd4c2cce23d7a2ed8bd571d2738f06d9eaa
John Benjamin <john.benjamin@hmcts.net>
2023-12-07 08:01:49 +0000

    EUI-8920 CR-84 Hearings edit summary

--- src/hearings/containers/request-hearing/hearing-edit-summary/warning-and-error-section/warning-and-error-section.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'exui-warning-and-error-section',
-  templateUrl: './warning-and-error-section.component.html'
-})
-export class WarningAndErrorSectionComponent {
-}

8536c2d13970d862b5bba5f2a715475d5153bbe8
DavidJayakumar <David.Jayakumar@hmcts.net>
2023-12-11 16:15:37 +0000

    Fix lint

--- src/hearings/containers/request-hearing/hearing-edit-summary/warning-and-error-section/warning-and-error-section.component.ts
+++ src/hearings/containers/request-hearing/hearing-edit-summary/warning-and-error-section/warning-and-error-section.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'exui-warning-and-error-section',
-  templateUrl: './warning-and-error-section.component.html',
+  templateUrl: './warning-and-error-section.component.html'
 })
 export class WarningAndErrorSectionComponent {
   @Input() public displayValildationMessage: boolean;

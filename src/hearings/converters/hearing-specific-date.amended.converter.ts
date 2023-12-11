373c6c04b9140fe5148f410a8c665e73cf22fc45
olusegun odunukan <olusegun.odunukan@hmcts.net>
2023-12-11 12:14:22 +0000

    fix: EXUI-1135 resolve issue with amend appearing

--- src/hearings/converters/hearing-specific-date.amended.converter.ts
+++ src/hearings/converters/hearing-specific-date.amended.converter.ts
@@ -13,7 +13,14 @@ export class HearingSpecificDateAmendedConverter implements IsAmendedConverter {
       const objBHearingWindow = state.hearingRequest.hearingRequestMainModel.hearingDetails.hearingWindow;
       if (objAHearingWindow.dateRangeStart && objBHearingWindow.dateRangeStart) {
         const isStartDatesUpdated = moment(objAHearingWindow.dateRangeStart).format(HearingDateEnum.DefaultFormat) !== moment(objBHearingWindow.dateRangeStart).format(HearingDateEnum.DefaultFormat);
-        const isEndDatesUpdated = moment(objAHearingWindow.dateRangeEnd).format(HearingDateEnum.DefaultFormat) !== moment(objBHearingWindow.dateRangeEnd).format(HearingDateEnum.DefaultFormat);
+
+        let isEndDatesUpdated = false;
+        if (objAHearingWindow.hasOwnProperty('dateRangeEnd') || objBHearingWindow.dateRangeEnd) {
+          const dateObjA = objAHearingWindow.hasOwnProperty('dateRangeEnd') ? moment(objAHearingWindow.dateRangeEnd).format(HearingDateEnum.DefaultFormat) : null;
+          const dateObjB = objBHearingWindow.dateRangeEnd ? moment(objBHearingWindow.dateRangeEnd).format(HearingDateEnum.DefaultFormat) : null;
+
+          isEndDatesUpdated = dateObjA !== dateObjB;
+        }
         return isStartDatesUpdated || isEndDatesUpdated;
       } else if (objAHearingWindow.firstDateTimeMustBe && objBHearingWindow.firstDateTimeMustBe) {
         return moment(objAHearingWindow.firstDateTimeMustBe).format(HearingDateEnum.DefaultFormat) !== moment(objBHearingWindow.firstDateTimeMustBe).format(HearingDateEnum.DefaultFormat);

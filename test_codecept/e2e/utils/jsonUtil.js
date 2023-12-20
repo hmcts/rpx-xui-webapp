33a61bb4b9f1c848e183ba07f98e9c7c918cda46
sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
2023-12-20 11:20:12 +0000

    EUI-8981 test updates

---
+++ test_codecept/e2e/utils/jsonUtil.js
@@ -0,0 +1,24 @@
+
+const jsonpath = require('jsonpath')
+const reportLogger = require('../../codeceptCommon/reportLogger')
+
+class JSONUtil{
+
+  updateJsonWithJsonPath(updatePaths, updateJson){
+    for (const row of updatePaths) {
+      const actualVal = jsonpath.query(updateJson, row.jsonpath);
+
+      let val = row.value;
+      if (val.startsWith('[') && val.endsWith(']')) {
+        val = val.replace('[', '').replace(']', '').split(',')
+      }
+      const updatedValue = jsonpath.value(updateJson, row.jsonpath, val);
+
+      reportLogger.AddMessage(`Updated ${row.jsonpath} =>${actualVal} to  ${updatedValue}`);
+    }
+
+  }
+
+}
+
+module.exports = new JSONUtil()

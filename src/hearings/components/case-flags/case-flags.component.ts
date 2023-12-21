b9363adbc258d24b9937e4e5284a660a09d6dee5
John Benjamin <john.benjamin@hmcts.net>
2023-12-21 15:35:15 +0000

    EUI-9038 Hearing CR-84 Hearing requirements page labels

--- src/hearings/components/case-flags/case-flags.component.ts
+++ src/hearings/components/case-flags/case-flags.component.ts
@@ -1,12 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CaseFlagGroup } from '../../models/caseFlagGroup.model';
+import { AmendmentLabelStatus } from '../../models/hearingsUpdateMode.enum';
+import { HearingsService } from '../../services/hearings.service';
 
 @Component({
   selector: 'exui-case-flags',
   templateUrl: './case-flags.component.html',
   styleUrls: ['./case-flags.component.scss']
 })
-export class CaseFlagsComponent {
+export class CaseFlagsComponent implements OnInit {
   @Input() public caseFlagsGroup: CaseFlagGroup[];
   @Input() public info: string;
+
+  public reasonableAdjustmentChangesConfirmed: boolean;
+  public amendmentLabelEnum = AmendmentLabelStatus;
+
+  constructor(private readonly hearingsService: HearingsService) {}
+
+  public ngOnInit(): void {
+    console.log('CASE FLAGS GROUP', this.caseFlagsGroup);
+    console.log('PropertiesUpdatedOnPageVisit', this.hearingsService.propertiesUpdatedOnPageVisit);
+    this.reasonableAdjustmentChangesConfirmed = this.hearingsService.propertiesUpdatedOnPageVisit?.afterPageVisit?.reasonableAdjustmentChangesConfirmed;
+  }
 }

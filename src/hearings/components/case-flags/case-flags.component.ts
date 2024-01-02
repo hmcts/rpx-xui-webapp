b653681923701ea8768b131fe4eef2dde75a20d8
John Benjamin <john.benjamin@hmcts.net>
2024-01-02 14:50:04 +0000

    EUI-9038 Hearing CR-84 Hearing requirements page labels

--- src/hearings/components/case-flags/case-flags.component.ts
+++ src/hearings/components/case-flags/case-flags.component.ts
@@ -1,29 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CaseFlagGroup } from '../../models/caseFlagGroup.model';
-import { HearingConditions } from '../../models/hearingConditions';
-import { HearingRequestMainModel } from '../../models/hearingRequestMain.model';
-import { AmendmentLabelStatus } from '../../models/hearingsUpdateMode.enum';
-import { ServiceHearingValuesModel } from '../../models/serviceHearingValues.model';
-import { HearingsService } from '../../services/hearings.service';
 
 @Component({
   selector: 'exui-case-flags',
   templateUrl: './case-flags.component.html',
   styleUrls: ['./case-flags.component.scss']
 })
-export class CaseFlagsComponent implements OnInit {
+export class CaseFlagsComponent {
   @Input() public caseFlagsGroup: CaseFlagGroup[];
   @Input() public info: string;
-  @Input() public hearingCondition: HearingConditions;
-  @Input() public hearingRequestMainModel: HearingRequestMainModel;
-  @Input() public serviceHearingValuesModel: ServiceHearingValuesModel;
-
-  public amendmentLabelEnum = AmendmentLabelStatus;
-
-  constructor(private readonly hearingsService: HearingsService) {}
-
-  public ngOnInit(): void {
-    console.log('CASE FLAGS GROUP', this.caseFlagsGroup);
-    console.log('PropertiesUpdatedOnPageVisit', this.hearingsService.propertiesUpdatedOnPageVisit);
-  }
 }

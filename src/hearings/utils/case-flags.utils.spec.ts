e7ec05de486b96cd93ad43f6c95d88f625424541
John Benjamin <john.benjamin@hmcts.net>
2024-01-04 10:40:45 +0000

    EUI-9038 Hearing CR-84 Hearing requirements page labels

--- src/hearings/utils/case-flags.utils.spec.ts
+++ src/hearings/utils/case-flags.utils.spec.ts
@@ -1,6 +1,8 @@
 import { caseFlagsRefData, serviceHearingValuesModel } from '../hearing.test.data';
-import { CaseFlagType, PartyType, UnavailabilityType } from '../models/hearings.enum';
+import { CaseFlagType, PartyType } from '../models/hearings.enum';
+import { AmendmentLabelStatus } from '../models/hearingsUpdateMode.enum';
 import { PartyDetailsModel } from '../models/partyDetails.model';
+import { PartyFlagsModel } from '../models/partyFlags.model';
 import { CaseFlagsUtils } from './case-flags.utils';
 
 describe('CaseFlagsUtils', () => {
@@ -19,30 +21,26 @@ describe('CaseFlagsUtils', () => {
       {
         partyID: 'P1',
         partyType: PartyType.IND,
-        partyRole: 'appellant',
         partyName: 'Jane Smith',
+        partyRole: 'APPL',
         individualDetails: {
-          title: 'Mrs',
           firstName: 'Jane',
           lastName: 'Smith',
-          preferredHearingChannel: 'inPerson',
           reasonableAdjustments: [
-            'RA001'
+            'RA0002'
           ]
         }
       },
       {
         partyID: 'P2',
         partyType: PartyType.IND,
-        partyRole: 'claimant',
         partyName: 'Jack Ryan',
+        partyRole: 'APPL',
         individualDetails: {
-          title: 'Mr',
           firstName: 'Jack',
           lastName: 'Ryan',
-          preferredHearingChannel: 'inPerson',
           reasonableAdjustments: [
-            'RA002'
+            'RA0002'
           ]
         }
       }
@@ -52,42 +50,106 @@ describe('CaseFlagsUtils', () => {
       {
         partyID: 'P1',
         partyType: PartyType.IND,
-        partyRole: 'appellant',
-        partyName: 'Jane Smith',
+        partyName: 'Jane Butler',
+        partyRole: 'APPL',
         individualDetails: {
-          title: 'Mrs',
           firstName: 'Jane',
           lastName: 'Smith',
-          preferredHearingChannel: 'inPerson',
           reasonableAdjustments: [
-            'RA001'
+            'RA0002'
           ]
         }
       },
       {
         partyID: 'P2',
         partyType: PartyType.IND,
-        partyRole: 'claimant',
         partyName: 'Jack Ryan',
+        partyRole: 'APPL',
         individualDetails: {
-          title: 'Mr',
           firstName: 'Jack',
           lastName: 'Ryan',
-          preferredHearingChannel: 'inPerson',
           reasonableAdjustments: [
-            'RA002'
+            'RA0002'
+          ]
+        }
+      },
+      {
+        partyID: 'P3',
+        partyType: PartyType.IND,
+        partyName: 'Rob Kennedy',
+        partyRole: 'APPL',
+        individualDetails: {
+          firstName: 'Rob',
+          lastName: 'Kennedy',
+          reasonableAdjustments: [
+            'RA0002'
           ]
         }
       }
     ];
 
-    const caseFlags = [
-
+    const caseFlags: PartyFlagsModel[] = [
+      {
+        partyId: 'P1',
+        partyName: 'Jane Smith',
+        flagParentId: 'RA0001',
+        flagId: 'RA0002',
+        flagDescription: 'Sign language interpreter required',
+        flagStatus: 'ACTIVE'
+      },
+      {
+        partyId: 'P1',
+        partyName: 'Jane Smith',
+        flagParentId: 'PF0001',
+        flagId: 'PF0015',
+        flagDescription: 'Language Interpreter',
+        flagStatus: 'ACTIVE'
+      },
+      {
+        partyId: 'P1',
+        partyName: 'Jane Smith',
+        flagParentId: 'CF0001',
+        flagId: 'CF0006',
+        flagDescription: 'Potential fraud',
+        flagStatus: 'ACTIVE'
+      },
+      {
+        partyId: 'P2',
+        partyName: 'Jack Ryan',
+        flagParentId: 'RA0001',
+        flagId: 'RA0002',
+        flagDescription: 'Sign language interpreter required',
+        flagStatus: 'ACTIVE'
+      },
+      {
+        partyId: 'P2',
+        partyName: 'Jack Ryan',
+        flagParentId: 'RA0008',
+        flagId: 'RA0042',
+        flagDescription: 'Sign language interpreter required',
+        flagStatus: 'ACTIVE'
+      },
+      {
+        partyId: 'P3',
+        partyName: 'Rob Kennedy',
+        flagParentId: 'RA0001',
+        flagId: 'RA0002',
+        flagDescription: 'Sign language interpreter required',
+        flagStatus: 'ACTIVE'
+      }
     ];
+
     it('should return reasonable adjustment flags', () => {
-
-
-      const reasonableAdjustmentFlagsGroup = CaseFlagsUtils.getReasonableAdjustmentFlags(caseFlagsRefData, serviceHearingValuesModel.caseFlags.flags, partiesInHMC, partiesInSHV);
+      const flagsGroup = CaseFlagsUtils.getReasonableAdjustmentFlags(caseFlagsRefData, caseFlags, partiesInHMC, partiesInSHV);
+      expect(flagsGroup[0].name).toEqual('Jane Butler');
+      expect(flagsGroup[0].partyAmendmentLabelStatus).toEqual(AmendmentLabelStatus.AMENDED);
+      expect(flagsGroup[0].partyFlags[0].flagAmendmentLabelStatus).toBeUndefined();
+      expect(flagsGroup[1].name).toEqual('Jack Ryan');
+      expect(flagsGroup[1].partyAmendmentLabelStatus).toEqual(AmendmentLabelStatus.NONE);
+      expect(flagsGroup[1].partyFlags[1].flagAmendmentLabelStatus).toEqual(AmendmentLabelStatus.ACTION_NEEDED);
+      expect(flagsGroup[2].name).toEqual('Rob Kennedy');
+      expect(flagsGroup[2].partyAmendmentLabelStatus).toEqual(AmendmentLabelStatus.ACTION_NEEDED);
+      expect(flagsGroup[2].partyFlags[0].flagAmendmentLabelStatus).toBeUndefined();
     });
   });
 });

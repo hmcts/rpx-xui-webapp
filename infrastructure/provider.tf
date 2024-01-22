8fb10eee5f676148291a624dafebcb377086388c
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2024-01-22 10:53:35 +0000

    Update provider.tf

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.19.1"
+      version = "~> 3.85.0"
     }
   }
 
-  required_version = "~> 1.3.5"
+  required_version = "~> 1.6.6"
 }
 
 provider "azurerm" {

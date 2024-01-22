7896bfa7dd12c9a0a84e97811f93b2e5969efa3b
RiteshHMCTS <74713687+RiteshHMCTS@users.noreply.github.com>
2024-01-22 09:54:47 +0000

    Revert "DTSPO-15909 - migrated Application Insights"

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -4,13 +4,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.85.0"
+      version = "~> 3.19.1"
     }
   }
-
-  required_version = "~> 1.6.6"
+  
+  required_version = "~> 1.3.5"
 }
 
 provider "azurerm" {
-  features {}
+    features {}
 }

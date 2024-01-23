6746c4409b6737a967dc5f514178d6ca29d2740d
DavidJayakumar <David.Jayakumar@hmcts.net>
2024-01-23 09:59:26 +0000

    fix lint

--- src/hearings/containers/request-hearing/hearing-edit-summary/judge-details-section/judge-details-section.component.ts
+++ src/hearings/containers/request-hearing/hearing-edit-summary/judge-details-section/judge-details-section.component.ts
@@ -46,7 +46,6 @@ export class JudgeDetailsSectionComponent implements OnInit {
       const objA = state.hearingRequestToCompare.hearingRequestMainModel.hearingDetails.panelRequirements;
       const objB = state.hearingRequest.hearingRequestMainModel.hearingDetails.panelRequirements;
       this.showAmmendedForNeedJudge = !_.isEqual(objA?.roleType.length > 0, objB?.roleType.length > 0);
-      // tslint:disable-next-line
       this.showAmmendedForJudgeType = !_.isEqual([...objA.roleType].sort((a, b) => a.localeCompare(b)), [...objB.roleType].sort((a, b) => a.localeCompare(b)));
       this.showAmmendedForJudgeName = !_.isEqual(objA?.panelPreferences.filter((panel) => panel.memberType === MemberType.JUDGE && panel.requirementType === RequirementType.MUSTINC),
         objB?.panelPreferences.filter((panel) => panel.memberType === MemberType.JUDGE && panel.requirementType === RequirementType.MUSTINC));

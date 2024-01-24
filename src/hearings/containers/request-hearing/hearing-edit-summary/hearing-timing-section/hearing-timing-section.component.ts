325db7678844da27fff2af0a8078132002199fba
John Benjamin <john.benjamin@hmcts.net>
2024-01-24 18:10:31 +0000

    EUI-9153 Hearing CR-84 Length date priority page

--- src/hearings/containers/request-hearing/hearing-edit-summary/hearing-timing-section/hearing-timing-section.component.ts
+++ src/hearings/containers/request-hearing/hearing-edit-summary/hearing-timing-section/hearing-timing-section.component.ts
@@ -20,6 +20,9 @@ export class HearingTimingSectionComponent implements OnInit {
 
   public hearingLength: string;
   public specificDate: string;
+  public earliestHearingDate: string;
+  public latestHearingDate: string;
+  public firstHearingDate: string;
   public hearingPriority: string;
   public hearingWindowChangesRequired: boolean;
   public hearingWindowChangesConfirmed: boolean;
@@ -103,6 +106,30 @@ export class HearingTimingSectionComponent implements OnInit {
 
     return specificDateSelection;
   }
+
+  private getSpecificDates(): string {
+    let specificDateSelection: string = RadioOptions.NO;
+    let earliestHearingDate: string = '';
+    let latestHearingDate: string = '';
+    const hearingWindow = this.hearingDetails.hearingWindow;
+
+    if (hearingWindow?.dateRangeStart || hearingWindow?.dateRangeEnd) {
+      specificDateSelection = RadioOptions.CHOOSE_DATE_RANGE;
+      earliestHearingDate = moment(hearingWindow.dateRangeStart).format(HearingDateEnum.DisplayMonth);
+      latestHearingDate = hearingWindow.dateRangeEnd && moment(hearingWindow.dateRangeEnd).format(HearingDateEnum.DisplayMonth);
+      specificDateSelection += earliestHearingDate !== HearingDateEnum.InvalidDate ? `<br>Earliest start date: ${earliestHearingDate}` : '';
+      specificDateSelection += latestHearingDate && latestHearingDate !== HearingDateEnum.InvalidDate ? `<br>Latest end date: ${latestHearingDate}` : '';
+    } else if (hearingWindow?.firstDateTimeMustBe) {
+      specificDateSelection = RadioOptions.YES;
+      const firstDate = moment(hearingWindow.firstDateTimeMustBe).format(HearingDateEnum.DisplayMonth);
+      specificDateSelection += firstDate !== HearingDateEnum.InvalidDate ? `<dt class="heading-h3 bottom-0">The first date of the hearing must be</dt><exui-amendment-label [displayLabel]="${AmendmentLabelStatus.ACTION_NEEDED}"></exui-amendment-label>${firstDate}` : '';
+    } else if (hearingWindow === null) {
+      specificDateSelection = RadioOptions.NO;
+    }
+
+    return specificDateSelection;
+  }
+
 
   private getHearingPriority(): string {
     const hearingPriorityFromRefData = this.hearingPrioritiesRefData.find((priority) => priority.key === this.hearingDetails.hearingPriorityType);

8eb5e4641df0757253f7f381f0f65cbf9de50979
olusegun odunukan <olusegun.odunukan@hmcts.net>
2024-02-22 09:26:28 +0000

    proxy config update

--- api/proxy.config.ts
+++ api/proxy.config.ts
@@ -21,7 +21,6 @@ import {
 } from './configuration/references';
 import { applyProxy } from './lib/middleware/proxy';
 import * as searchCases from './searchCases';
-import * as documents from './documents';
 
 export const initProxy = (app: Express) => {
   applyProxy(app, {

c7ea172f12e8e5466919d3e860d9e0ec90d90ad3
Kasi Subramaniam <Kasi.Subramaniam@hmcts.net>
2024-02-29 11:47:05 +0000

    fix for integration tests

--- src/hearings/containers/request-hearing/hearing-judge/hearing-judge.component.ts
+++ src/hearings/containers/request-hearing/hearing-judge/hearing-judge.component.ts
@@ -168,11 +168,11 @@ export class HearingJudgeComponent extends RequestHearingPageFlow implements OnI
 
     let preSelectedPanelRoles: string[] = [];
     if (includedJudges === 0 && includedJudgesBeforeChange === 0) {
-      preSelectedPanelRoles = HearingsUtils.getRestOfRoleType(panelRequirements.roleType);
+      preSelectedPanelRoles = HearingsUtils.getRestOfRoleType(panelRequirements?.roleType);
     } else if (includedJudges > 0 && includedJudgesBeforeChange === 0) {
       preSelectedPanelRoles = HearingsUtils.getRestOfRoleType(panelRequirements?.roleType);
     } else {
-      preSelectedPanelRoles = panelRequirements.roleType;
+      preSelectedPanelRoles = panelRequirements?.roleType;
     }
     const selectedPanelMembers = panelRequirements?.panelPreferences.filter((preferences) => preferences.memberType === MemberType.PANEL_MEMBER) || [];
     this.hearingRequestMainModel = {

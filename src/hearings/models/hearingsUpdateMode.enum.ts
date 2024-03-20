ce5b53ac0905244eec2647649b2f58251213e840
John Benjamin <john.benjamin@hmcts.net>
2024-03-20 14:17:20 +0000

    CSFD-53

--- src/hearings/models/hearingsUpdateMode.enum.ts
+++ src/hearings/models/hearingsUpdateMode.enum.ts
@@ -60,6 +60,8 @@ export interface AfterPageVisitProperties {
   partyDetailsChangesConfirmed?: boolean;
   hearingWindowChangesRequired: boolean;
   hearingWindowChangesConfirmed?: boolean;
+  hearingFacilitiesChangesRequired: boolean;
+  hearingFacilitiesChangesConfirmed?: boolean;
 }
 
 export interface PropertiesUpdatedAutomatically {

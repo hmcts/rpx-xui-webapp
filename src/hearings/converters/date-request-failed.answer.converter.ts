d58effc1da9764def2b02520f2159a29b61646f5
olusegun odunukan <olusegun.odunukan@hmcts.net>
2024-05-16 14:30:48 +0100

    lint fixes

--- src/hearings/converters/date-request-failed.answer.converter.ts
+++ src/hearings/converters/date-request-failed.answer.converter.ts
@@ -9,16 +9,16 @@ export class DateRequestFailedAnswerConverter implements AnswerConverter {
   public transformAnswer(hearingState$: Observable<State>): Observable<string> {
     return hearingState$.pipe(
       map((state) => {
-          const hearingResponse = state.hearingRequest.hearingRequestMainModel;
-          if (hearingResponse.hearingResponse?.errorTimestamp) {
-            const errorTimestampBST = moment.utc(hearingResponse.hearingResponse.errorTimestamp).tz('Europe/London'); // Convert to BST
-            return errorTimestampBST.format(HearingDateEnum.RequestFailedDateAndTime);
-          } else if (hearingResponse.requestDetails) {
-            const requestTimestampBST = moment.utc(hearingResponse.requestDetails.timestamp).tz('Europe/London'); // Convert to BST
-            return requestTimestampBST.format(HearingDateEnum.RequestFailedDateAndTime);
-          }
+        const hearingResponse = state.hearingRequest.hearingRequestMainModel;
+        if (hearingResponse.hearingResponse?.errorTimestamp) {
+          const errorTimestampBST = moment.utc(hearingResponse.hearingResponse.errorTimestamp).tz('Europe/London'); // Convert to BST
+          return errorTimestampBST.format(HearingDateEnum.RequestFailedDateAndTime);
+        } else if (hearingResponse.requestDetails) {
+          const requestTimestampBST = moment.utc(hearingResponse.requestDetails.timestamp).tz('Europe/London'); // Convert to BST
+          return requestTimestampBST.format(HearingDateEnum.RequestFailedDateAndTime);
+        }
 
-          return '';
+        return '';
       })
     );
   }

8782aa2b47bd66541e76cf3115e35f41d61e06ce
david-gimelle <david.gimelle2@hmcts.net>
2024-06-05 12:12:35 +0100

    remove skips

--- playwright_tests/E2E/login-and-log-out-with-valid user.test.ts
+++ playwright_tests/E2E/login-and-log-out-with-valid user.test.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { userLogin } from "./steps/login-steps"
 
-test.skip('login and log out from EUI with valid user', async ({ page }) => {
+test('login and log out from EUI with valid user', async ({ page }) => {
   await userLogin(page, 'PROD_LIKE');
   await expect(page.getByRole('heading', { name: 'Case list' })).toBeVisible();
   await page.getByText('Sign out').click();

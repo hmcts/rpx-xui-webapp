72af79813deb95250cd89908ac74df4c775d2b8d
Anthony Dummer <anthony.dummer@HMCTS.net>
2024-06-14 14:30:54 +0100

    EXUI-2018 - initial check in.

--- src/hearings/containers/request-hearing/hearing-edit-summary/hearing-edit-summary.component.ts
+++ src/hearings/containers/request-hearing/hearing-edit-summary/hearing-edit-summary.component.ts
@@ -458,7 +458,15 @@ export class HearingEditSummaryComponent extends RequestHearingPageFlow implemen
     const interpreterLanguagesSHV = this.extractInterpreterLanguages(SHVIndividualParties);
     const interpreterLanguagesHMC = this.extractInterpreterLanguages(HRMIndividualParties);
 
-    return !_.isEqual(interpreterLanguagesSHV, interpreterLanguagesHMC);
+    // return !_.isEqual(interpreterLanguagesSHV, interpreterLanguagesHMC);
+    if (!_.isEqual(interpreterLanguagesSHV, interpreterLanguagesHMC)){
+      return true;
+    }
+
+    if (this.hearingsService.propertiesUpdatedOnPageVisit.afterPageVisit.reasonableAdjustmentChangesRequired) {
+      return !this.hearingsService.propertiesUpdatedOnPageVisit.afterPageVisit.reasonableAdjustmentChangesConfirmed
+    }
+    return false;
   }
 
   private extractReasonableAdjustments(partyDetails: PartyDetailsModel[]) {

7d4a267f3a005234c4753ea12200d40375c4ec66
Ritesh Dsouza <ritesh.dsouza@HMCTS.net>
2024-06-20 16:08:39 +0100

    lint error

--- src/cases/containers/roles-and-access-container/roles-and-access-container.component.ts
+++ src/cases/containers/roles-and-access-container/roles-and-access-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CaseField, CaseView } from '@hmcts/ccd-case-ui-toolkit';
 import { FeatureToggleService } from '@hmcts/rpx-xui-common-lib';

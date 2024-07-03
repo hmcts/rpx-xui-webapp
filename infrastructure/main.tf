74c34ea3799b43835cd3440624ca12c787bd5d0d
Andy Wilkins <andy.wilkins@hmcts.net>
2024-07-03 14:24:10 +0100

    Fix typo

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -97,7 +97,7 @@ resource "azurerm_key_vault_secret" "app_insights_key" {
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
 
-resource "azurerm_key_vault_secret" "app_insights_connection_string {
+resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "appinsights-connection-string-mc"
   value        = module.application_insights.connection_string
   key_vault_id = data.azurerm_key_vault.key_vault.id

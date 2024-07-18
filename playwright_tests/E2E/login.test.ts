d05a2244594de9f6435e53b5ad4cbe916561a806
david-gimelle <david.gimelle2@hmcts.net>
2024-07-18 14:14:22 +0100

    reactivate all pw tests

--- playwright_tests/E2E/login.test.ts
+++ playwright_tests/E2E/login.test.ts
@@ -2,20 +2,20 @@ import { test, expect } from '@playwright/test';
 import { signIn } from "./steps/login-steps"
 import config from "../config"
 
-test.skip('login and log out from EUI with valid user', async ({ page }) => {
+test('login and log out from EUI with valid user', async ({ page }) => {
   await signIn(page, 'PROD_LIKE');
   await expect(page.getByRole('heading', { name: 'Case list' })).toBeVisible();
   await page.getByText('Sign out').click();
   await expect(page.getByRole('heading', { name: 'Sign in or create an account' })).toBeVisible();
 });
 
-test.skip('login Verify the direct link navigate to login page', async ({ page }) => {
+test('login Verify the direct link navigate to login page', async ({ page }) => {
     await page.goto(config.CaseBaseURL);
     await expect(page.getByRole('heading', { name: 'Sign in or create an account' })).toBeVisible();
     await expect(page.url()).toContain('idam-web-public');
   });
 
-  test.skip('login un-authenticated user login', async ({ page }) => {
+  test('login un-authenticated user login', async ({ page }) => {
     await page.goto(config.CaseBaseURL);
     await page.getByLabel('Email address').fill('test_nonexisting_or_invalid@gmail.com');
     await page.getByLabel('Password').click();

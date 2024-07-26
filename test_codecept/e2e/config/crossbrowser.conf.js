b30902456d40c8202cc72bd20788d34ae6487014
Josh <josh.glasgow@hmcts.net>
2024-07-26 12:50:11 +0100

    Update command, update config

--- test_codecept/e2e/config/crossbrowser.conf.js
+++ test_codecept/e2e/config/crossbrowser.conf.js
@@ -25,11 +25,57 @@ const config = {
   params: {
     serverUrls: process.env.TEST_URL || 'http://localhost:3000/',
     targetEnv: argv.env || 'local',
-    //username: process.env.TEST_EMAIL,
-    //password: process.env.TEST_PASSWORD,
+    username: 'lukesuperuserxui_new@mailnesia.com',
+    password: 'Monday01',
+    caseworkerUser:'mahesh_fr_courtadmn@mailinator.com',
+    caseworkerPassword: 'London01',
+    probate_username: process.env.PROBATE_EMAIL || 'probaterequesteraat@mailnesia.com',
+    probate_password: process.env.PROBATE_PASSWORD || 'LevelAt12',
+    fr_judge_username: process.env.FR_EMAIL,
+    fr_judge_password: process.env.FR_PASSWORD,
+    sscs_username: process.env.SSCS_EMAIL,
+    sscs_password: process.env.SSCS_PASSWORD,
+
+    caseworker_iac_off_username: process.env.CASEWORKER_IAC_OFF_USERNAME ? process.env.CASEWORKER_IAC_OFF_USERNAME :'xui_auto_co_r1@justice.gov.uk' ,
+    caseworker_iac_off_password: process.env.CASEWORKER_IAC_OFF_PASSWORD ? process.env.CASEWORKER_IAC_OFF_PASSWORD :'Welcome01',
+    hrsTesterUser: 'xui.hrs.tester@hmcts.net',
+    hrsTesterPassword: 'Monday01',
+    ia_users_credentials: {
+      case_officer: {
+        username: process.env.TEST_CASEOFFICER_USERNAME || 'ia.caseofficer.ccd@gmail.com',
+        password: process.env.TEST_CASEOFFICER_PASSWORD|| 'AldgateT0wer'
+      }, 
+      legal_rep: {
+        username: process.env.TEST_LAW_FIRM_A_USERNAME || 'ia.legalrep.b.ccd@gmail.com',
+        password: process.env.TEST_LAW_FIRM_A_PASSWORD || 'AldgateT0wer'
+      },
+      admin_officer: {
+        username: process.env.TEST_ADMINOFFICER_USERNAME || 'ia.adm1nofficer.ccd@gmail.com',
+        password: process.env.TEST_ADMINOFFICER_PASSWORD || 'AldgateT0wer'
+      },
+      homeoffice_respondent: {
+        username: process.env.TEST_HOMEOFFICE_GENERIC_USERNAME || 'ia.respondentoffice.ccd@gmail.com',
+        password: process.env.TEST_HOMEOFFICE_GENERIC_PASSWORD || 'AldgateT0wer'
+      },
+      homeoffice_apc: {
+        username: process.env.TEST_HOMEOFFICE_APC_USERNAME || 'ia.respondentapc.ccd@gmail.com',
+        password: process.env.TEST_HOMEOFFICE_APC_PASSWORD || 'AldgateT0wer'
+      },
+      homeoffice_larts: {
+        username: process.env.TEST_HOMEOFFICE_LART_USERNAME || 'ia.respondentlarts.ccd@gmail.com',
+        password: process.env.TEST_HOMEOFFICE_LART_PASSWORD || 'AldgateT0wer'
+      },
+      homeoffice_pou: {
+        username: process.env.TEST_HOMEOFFICE_POU_USERNAME || 'ia.respondentpou.ccd@gmail.com',
+        password: process.env.TEST_HOMEOFFICE_POU_PASSWORD || 'AldgateT0wer'
+      },
+      judge: {
+        username: process.env.TEST_JUDICIARY_USERNAME || 'ia.iacjudge.ccd@gmail.com',
+        password: process.env.TEST_JUDICIARY_PASSWORD|| 'AldgateT0wer'
+      }
+    }
   },
-
-
+  directConnect: true,
  // sauceProxy: 'http://proxyout.reform.hmcts.net:8080',  // Proxy for the REST API
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
@@ -49,7 +95,6 @@ const config = {
       sharedTestFiles: false,
       maxInstances: 1
     },
-
     {
       browserName: 'firefox',
       version: 'latest',
@@ -60,29 +105,6 @@ const config = {
       sharedTestFiles: false,
       maxInstances: 1
     },
-
-    // {
-    //   browserName: 'internet explorer',
-    //   platform: 'Windows 10',
-    //   version: 'latest',
-    //   name: 'IE-TEST',
-    //   tunnelIdentifier: 'reformtunnel',
-    //   extendedDebugging: true,
-    //   sharedTestFiles: false,
-    //   maxInstances: 1
-    // },
-
-    // {
-    //   browserName: 'safari',
-    //   platform: 'macOS 10.15',
-    //   version: '13.1',
-    //   name: 'Safari-TEST',
-    //   tunnelIdentifier: 'reformtunnel',
-    //   extendedDebugging: true,
-    //   sharedTestFiles: false,
-    //   maxInstances: 1
-    // },
-
     {
       browserName: 'MicrosoftEdge',
       platform: 'macOS 10.15',
@@ -93,7 +115,6 @@ const config = {
       sharedTestFiles: false,
       maxInstances: 1
     },
-
     {
       browserName: 'chrome',
       version: 'latest',
@@ -104,7 +125,6 @@ const config = {
       sharedTestFiles: false,
       maxInstances: 1
     },
-
     {
       browserName: 'firefox',
       version: 'latest',
@@ -114,7 +134,7 @@ const config = {
       extendedDebugging: true,
       sharedTestFiles: false,
       maxInstances: 1
-    },
+    }
   ],
 
   exclude: [],
@@ -122,10 +142,12 @@ const config = {
   cucumberOpts: {
     strict: true,
     format: ['node_modules/cucumber-pretty', 'json:cb_reports/saucelab_results.json'],
-    require: ['../support/world.js', '../support/*.js', '../features/step_definitions/**/*.steps.js', '../features/step_definitions/**/*.steps.js'],
+    require: [
+      '../support/world.js',
+      '../support/*.js',
+      '../features/step_definitions/*.steps.js'],
     tags: ['@crossbrowser']
   },
-
 
   plugins: [
     {
@@ -136,13 +158,11 @@ const config = {
         reportName: 'XUI MC CrossBrowser Tests',
         jsonDir: 'reports/tests/crossbrowser',
         reportPath: 'reports/tests/crossbrowser'
-
       }
     }
   ],
 
   onPrepare() {
-    const caps = browser.getCapabilities();
     browser.manage()
       .window()
       .maximize();

c7302091331931d31c3fe188a1fa38eb5141d8d5
Josh <josh.glasgow@hmcts.net>
2024-07-26 14:25:10 +0100

    Update config

--- test_codecept/e2e/config/crossbrowser.conf.js
+++ test_codecept/e2e/config/crossbrowser.conf.js
@@ -1,33 +1,40 @@
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
-const minimist = require('minimist');
-const argv = minimist(process.argv.slice(2));
-var screenShotUtils = require("protractor-screenshot-utils").ProtractorScreenShotUtils;
-
+const { setHeadlessWhen } = require('@codeceptjs/configure'); // Add CodeceptJS configuration
+setHeadlessWhen(process.env.HEADLESS); // Enable headless mode if specified
+const testType = process.env.TEST_TYPE;
 
 const config = {
-  framework: 'custom',
-  frameworkPath: require.resolve('protractor-cucumber-framework'),
-
- sauceSeleniumAddress: 'ondemand.eu-central-1.saucelabs.com:443/wd/hub',
-
- // sauceSeleniumAddress: 'https://vmuniganti:ed5cdbf5-4d8f-47e4-ab72-1757ee05e15f@eu-central-1.saucelabs.com:443/wd/hub',
-
-  host: 'ondemand.eu-central-1.saucelabs.com',
-  sauceRegion: 'eu',
-  port: 80,
-  sauceConnect: true,
-  specs: ['../features/**/*.feature'],
-
-  baseUrl: (process.env.TEST_URL || 'http://localhost:3000/').replace('https', 'http'),
-
+  tests: '../features/app/**/*.feature',
+  output: `${__dirname}/../../functional-output/tests/codecept-${testType}`, // Specify output directory
+  helpers: {
+    Playwright: {
+      url: process.env.TEST_URL || 'http://localhost:3000/',
+      browser: 'chromium', // Set default browser to Playwright's Chromium
+      // Add Sauce Labs options
+      connection: {
+        server: 'https://ondemand.eu-central-1.saucelabs.com/wd/hub',
+        user: process.env.SAUCE_USERNAME,
+        key: process.env.SAUCE_ACCESS_KEY
+      }
+      // Additional Playwright options can be added here
+    }
+  },
+  include: {
+    I: '../../**/*.steps.js' // Include steps file
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'crossbrowser-tests',
+  plugins: {
+    retryFailedStep: {
+      enabled: true // Enable retry for failed steps
+    }
+  },
   params: {
     serverUrls: process.env.TEST_URL || 'http://localhost:3000/',
-    targetEnv: argv.env || 'local',
+    targetEnv: process.argv[2] || 'local', // Use argv for target environment
     username: 'lukesuperuserxui_new@mailnesia.com',
     password: 'Monday01',
-    caseworkerUser:'mahesh_fr_courtadmn@mailinator.com',
+    caseworkerUser: 'mahesh_fr_courtadmn@mailinator.com',
     caseworkerPassword: 'London01',
     probate_username: process.env.PROBATE_EMAIL || 'probaterequesteraat@mailnesia.com',
     probate_password: process.env.PROBATE_PASSWORD || 'LevelAt12',
@@ -35,16 +42,15 @@ const config = {
     fr_judge_password: process.env.FR_PASSWORD,
     sscs_username: process.env.SSCS_EMAIL,
     sscs_password: process.env.SSCS_PASSWORD,
-
-    caseworker_iac_off_username: process.env.CASEWORKER_IAC_OFF_USERNAME ? process.env.CASEWORKER_IAC_OFF_USERNAME :'xui_auto_co_r1@justice.gov.uk' ,
-    caseworker_iac_off_password: process.env.CASEWORKER_IAC_OFF_PASSWORD ? process.env.CASEWORKER_IAC_OFF_PASSWORD :'Welcome01',
+    caseworker_iac_off_username: process.env.CASEWORKER_IAC_OFF_USERNAME ? process.env.CASEWORKER_IAC_OFF_USERNAME : 'xui_auto_co_r1@justice.gov.uk',
+    caseworker_iac_off_password: process.env.CASEWORKER_IAC_OFF_PASSWORD ? process.env.CASEWORKER_IAC_OFF_PASSWORD : 'Welcome01',
     hrsTesterUser: 'xui.hrs.tester@hmcts.net',
     hrsTesterPassword: 'Monday01',
     ia_users_credentials: {
       case_officer: {
         username: process.env.TEST_CASEOFFICER_USERNAME || 'ia.caseofficer.ccd@gmail.com',
-        password: process.env.TEST_CASEOFFICER_PASSWORD|| 'AldgateT0wer'
-      }, 
+        password: process.env.TEST_CASEOFFICER_PASSWORD || 'AldgateT0wer'
+      },
       legal_rep: {
         username: process.env.TEST_LAW_FIRM_A_USERNAME || 'ia.legalrep.b.ccd@gmail.com',
         password: process.env.TEST_LAW_FIRM_A_PASSWORD || 'AldgateT0wer'
@@ -71,24 +77,15 @@ const config = {
       },
       judge: {
         username: process.env.TEST_JUDICIARY_USERNAME || 'ia.iacjudge.ccd@gmail.com',
-        password: process.env.TEST_JUDICIARY_PASSWORD|| 'AldgateT0wer'
+        password: process.env.TEST_JUDICIARY_PASSWORD || 'AldgateT0wer'
       }
     }
   },
-  directConnect: true,
- // sauceProxy: 'http://proxyout.reform.hmcts.net:8080',  // Proxy for the REST API
-  sauceUser: process.env.SAUCE_USERNAME,
-  sauceKey: process.env.SAUCE_ACCESS_KEY,
-  SAUCE_REST_ENDPOINT: 'https://eu-central-1.saucelabs.com/rest/v1/',
-  allScriptsTimeout: 111000,
-
-
-  useAllAngular2AppRoots: true,
   multiCapabilities: [
     {
-      browserName: 'chrome',
-      version: 'latest',
+      browserName: 'chromium',
       platform: 'Windows 10',
+      version: 'latest',
       name: 'chrome-win-tests',
       tunnelIdentifier: 'reformtunnel',
       extendedDebugging: true,
@@ -97,8 +94,8 @@ const config = {
     },
     {
       browserName: 'firefox',
-      version: 'latest',
       platform: 'Windows 10',
+      version: 'latest',
       name: 'firefox-win-tests',
       tunnelIdentifier: 'reformtunnel',
       extendedDebugging: true,
@@ -109,16 +106,16 @@ const config = {
       browserName: 'MicrosoftEdge',
       platform: 'macOS 10.15',
       version: '90.0',
-      name: 'chromium-tests',
+      name: 'edge-mac-tests',
       tunnelIdentifier: 'reformtunnel',
       extendedDebugging: true,
       sharedTestFiles: false,
       maxInstances: 1
     },
     {
-      browserName: 'chrome',
-      version: 'latest',
+      browserName: 'chromium',
       platform: 'macOS 10.15',
+      version: 'latest',
       name: 'chrome-mac-tests',
       tunnelIdentifier: 'reformtunnel',
       extendedDebugging: true,
@@ -127,8 +124,8 @@ const config = {
     },
     {
       browserName: 'firefox',
-      version: 'latest',
       platform: 'macOS 10.15',
+      version: 'latest',
       name: 'ff-mac-tests',
       tunnelIdentifier: 'reformtunnel',
       extendedDebugging: true,
@@ -136,35 +133,17 @@ const config = {
       maxInstances: 1
     }
   ],
-
-  exclude: [],
-
   cucumberOpts: {
     strict: true,
-    format: ['node_modules/cucumber-pretty', 'json:cb_reports/saucelab_results.json'],
     require: [
       '../support/timeout.js',
       '../support/hooks.js',
       '../support/world.js',
       '../support/*.js',
-      '../features/step_definitions/*.steps.js'
+      '../features/step_definitions/**/*.steps.js'
     ],
     tags: ['@crossbrowser']
   },
-
-  plugins: [
-    {
-      package: 'protractor-multiple-cucumber-html-reporter-plugin',
-      options: {
-        automaticallyGenerateReport: true,
-        removeExistingJsonReportFile: true,
-        reportName: 'XUI MC CrossBrowser Tests',
-        jsonDir: 'reports/tests/crossbrowser',
-        reportPath: 'reports/tests/crossbrowser'
-      }
-    }
-  ],
-
   onPrepare() {
     browser.manage()
       .window()
@@ -179,6 +158,5 @@ const config = {
     });
   }
 };
-
 
 exports.config = config;

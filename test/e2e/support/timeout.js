565b40f64e1cf9ca6759bd5cb65ad5e4a6903d38
Josh <josh.glasgow@hmcts.net>
2024-07-26 09:05:37 +0100

    revert timeout change

--- test/e2e/support/timeout.js
+++ test/e2e/support/timeout.js
@@ -1,5 +1,5 @@
 // timeout.js
-const { setDefaultTimeout } = require('@cucmber/cucumber');
+var { setDefaultTimeout } = require('cucumber');
 
 setDefaultTimeout(600 * 1000);
 // this timeout value is global setting impact all step definition function,

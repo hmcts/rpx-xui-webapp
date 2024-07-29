bc87b7be8abdcceb703da8da6b8eb62739597122
Josh <josh.glasgow@hmcts.net>
2024-07-29 16:47:02 +0100

    change config

--- test_codecept/e2e/config/crossbrowser.conf.js
+++ test_codecept/e2e/config/crossbrowser.conf.js
@@ -1,111 +1,72 @@
-const path = require('path');
-const minimist = require('minimist');
-const argv = minimist(process.argv.slice(2));
-const retry = require('protractor-retry').retry;
-const tsNode = require('ts-node');
+const { join } = require('path');
 
-console.log(process.env.SAUCE_USERNAME)
-console.log(process.env.SAUCE_ACCESS_KEY)
-console.log(process.env.TUNNEL_IDENTIFIER)
-console.log(path.join(__dirname, '/tsconfig.e2e.json'))
-
-// Debugging statement to check the module name
-const frameworkPath = 'protractor-cucumber-framework';
-console.log(`Resolving module: ${frameworkPath}`);
-const resolvedFrameworkPath = require.resolve(frameworkPath);
-console.log(`Resolved path: ${resolvedFrameworkPath}`);
-
-const config = {
-  framework: 'custom',
-  frameworkPath: resolvedFrameworkPath,
+exports.config = {
+  framework: 'cucumber',
   cucumberOpts: {
-    require: [
-      '../features/stepDefinitions/**/*.steps',
-      '../support/timeout.js',
-    ],
-    keepAlive: false,
-    tags: '@crossbrowser',
-    profile: false,
-    'no-source': true,
-    strict: true,
-    format: ['node_modules/cucumber-pretty', 'json:./cb_reports/saucelab_results.json'],
-    retry: 1,
+    require: ['../step_definitions/**/*.js'],
+    format: ['json:reports/cucumber_report.json'],
+    timeout: 240000,
   },
-
-  sauceSeleniumAddress: 'ondemand.eu-central-1.saucelabs.com:443/wd/hub',
-  host: 'ondemand.eu-central-1.saucelabs.com',
-  sauceRegion: 'eu',
-  port: 80,
-  sauceConnect: true,
-  sauceUser: process.env.SAUCE_USERNAME,
-  sauceKey: process.env.SAUCE_ACCESS_KEY,
-  SAUCE_REST_ENDPOINT: 'https://eu-central-1.saucelabs.com/rest/v1/',
-
-  specs: ['../features/**/*.feature'],
+  specs: [
+    '../features/**/*.feature'
+  ],
   baseUrl: 'https://manage-case.aat.platform.hmcts.net',
-  allScriptsTimeout: 240000,
-  useAllAngular2AppRoots: true,
+  maxInstances: 1,
   multiCapabilities: [
     {
       browserName: 'firefox',
-      version: 'latest',
-      platform: 'Windows 10',
-      name: 'MC-firefox-windows-test',
-      tunnelIdentifier: 'reformtunnel',
-      extendedDebugging: true,
-      sharedTestFiles: false,
-      capturePerformance: true,
-      maxInstances: 1
+      platformName: 'Windows 10',
+      browserVersion: 'latest',
+        name: 'MC-firefox-windows-test',
+        tunnelIdentifier: 'reformtunnel',
+        extendedDebugging: true,
+        capturePerformance: true,
     },
     {
       browserName: 'chrome',
-      version: 'latest',
-      platform: 'macOS 10.13',
-      name: 'MC-chrome-mac-test',
-      tunnelIdentifier: 'reformtunnel',
-      extendedDebugging: true,
-      sharedTestFiles: false,
-      capturePerformance: true,
-      maxInstances: 1,
+      platformName: 'macOS 10.13',
+      browserVersion: 'latest',
+        name: 'MC-chrome-mac-test',
+        tunnelIdentifier: 'reformtunnel',
+        extendedDebugging: true,
+        capturePerformance: true,
     },
     {
       browserName: 'MicrosoftEdge',
-      version: 'latest',
-      platform: 'Windows 10',
-      name: 'MC-microsoft-edge-windows-test',
-      tunnelIdentifier: 'reformtunnel',
-      extendedDebugging: true,
-      sharedTestFiles: false,
-      capturePerformance: true,
-      maxInstances: 1,
-    },
+      platformName: 'Windows 10',
+      browserVersion: 'latest',
+        name: 'MC-microsoft-edge-windows-test',
+        tunnelIdentifier: 'reformtunnel',
+    }
   ],
-  maxSessions: 1,
-
-  plugins: [
-    {
-      package: 'protractor-multiple-cucumber-html-reporter-plugin',
-      options: {
-        saveCollectedJSON: true,
-        automaticallyGenerateReport: true,
-        removeExistingJsonReportFile: true,
-        reportName: 'MC Cross Browser Test',
-        jsonDir: 'reports/tests/crossbrowser',
-        reportPath: 'reports/tests/crossbrowser',
-        pageFooter: '<div><p> </p></div>',
-      },
-    },
+  services: [
+    ['sauce', {
+      sauceConnect: true,
+      sauceConnectOpts: {
+        tunnelIdentifier: 'reformtunnel'
+      }
+    }]
   ],
-
-  onCleanUp(results, files) {
-    retry.onCleanUp(results, files);
-  },
+  user: process.env.SAUCE_USERNAME,
+  key: process.env.SAUCE_ACCESS_KEY,
+  region: 'eu',
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 240000,
+  connectionRetryCount: 3,
+  path: '/wd/hub',
+  sauceSeleniumAddress: 'ondemand.eu-central-1.saucelabs.com:443/wd/hub',
+  SAUCE_REST_ENDPOINT: 'https://eu-central-1.saucelabs.com/rest/v1/',
+  reporters: ['spec', ['json', {
+    outputDir: './cb_reports',
+    outputFileFormat: function(opts) {
+      return `saucelab_results-${opts.cid}.json`
+    }
+  }]],
 
   onPrepare() {
     const caps = browser.getCapabilities();
     browser.manage().window().maximize();
     browser.waitForAngularEnabled(true);
-    retry.onPrepare();
     tsNode.register({
       project: path.join(__dirname, '/tsconfig.e2e.json'),
     });
@@ -114,9 +75,6 @@ const config = {
         console.log(log.message);
       });
     });
-  },
-  afterLaunch() {
-    return retry.afterLaunch(1);
   },
   onComplete() {
     return browser.getProcessedConfig().then(function (c) {
@@ -127,5 +85,3 @@ const config = {
     });
   },
 };
-
-exports.config = config;

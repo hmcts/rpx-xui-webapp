2b1f4a93995dde2c3ee4d2de747f0daf9ea2d874
Josh <josh.glasgow@hmcts.net>
2024-08-22 14:00:58 +0100

    Move mc-menu-theme to environment.ts, update code in app-header to load the correct config based on roles

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,7 +12,12 @@ export const environment = {
     roles: 'roles'
   },
   googleAnalyticsKey: 'UA-124734893-1',
-  googleTagManagerKey: 'GTM-TQ7PKDF'
+  googleTagManagerKey: 'GTM-TQ7PKDF',
+  themes: {
+    'judge|judiciary|panelmember': { appTitle: { name: 'Judicial Case Manager', url: '/' }, backgroundColor: '#8d0f0e', logo: 'judicial' },
+    'pui-case-manager': { appTitle: { name: 'Manage Cases', url: '/' }, backgroundColor: '#202020', logo: 'myhmcts' },
+    'default': { appTitle: { name: 'Manage Cases', url: '/' }, backgroundColor: '#202020', logo: 'none' }
+  }
 };
 /**
  * Do not add configuration to this file if possible

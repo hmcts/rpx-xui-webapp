7226933f5f8f01ee5d66f546de9d2ea4f00723a3
Andy Wilkins <49269487+andywilkinshmcts@users.noreply.github.com>
2024-08-29 10:59:58 +0100

    Revert "Ex UI 152 noc sonar security latest renamed (#3847)"
    This reverts commit c016975046ac9347cebf52ad93f073e33733e0de.

--- src/noc/containers/noc-field/utils/noc-validators.spec.ts
+++ src/noc/containers/noc-field/utils/noc-validators.spec.ts
@@ -17,7 +17,7 @@ describe('NocValidators', () => {
   it('numberValidator valid case', () => {
     control.setValue('100');
     const numberValidator = NocValidators.numberValidator();
-    expect(numberValidator(control)).toEqual({ number: true });
+    expect(numberValidator(control)).toBeUndefined();
   });
 
   it('postcodeValidator invalid case', () => {
@@ -29,10 +29,10 @@ describe('NocValidators', () => {
   it('postcodeValidator valid case', () => {
     control.setValue('l155ax');
     const postcodeValidator = NocValidators.postcodeValidator();
-    expect(postcodeValidator(control)).toBeNull();
+    expect(postcodeValidator(control)).toBeUndefined();
   });
 
-  xit('phoneUKValidator invalid case', () => {
+  it('phoneUKValidator invalid case', () => {
     control.setValue('123546547897');
     const phoneUKValidator = NocValidators.phoneUKValidator();
     expect(phoneUKValidator(control)).toEqual({ phoneUK: true });
@@ -41,49 +41,49 @@ describe('NocValidators', () => {
   it('phoneUKValidator valid case', () => {
     control.setValue('07777777777');
     const phoneUKValidator = NocValidators.phoneUKValidator();
-    expect(phoneUKValidator(control)).toEqual({ phoneUK: true });
+    expect(phoneUKValidator(control)).toBeUndefined();
   });
 
   it('dateValidator valid case', () => {
     control.setValue('2019-12-24');
     const validator = NocValidators.dateValidator();
-    expect(validator(control)).toBeNull();
+    expect(validator(control)).toBeUndefined();
   });
 
   it('dateValidator invalid case', () => {
     control.setValue('2019-13-24');
     const validator = NocValidators.dateValidator();
-    expect(validator(control)).toEqual({ date: true, month: true, year: true, valid: false });
+    expect(validator(control)).toEqual({ date: true, month: true, valid: false });
   });
 
   it('dateTimeValidator valid 12 hour case', () => {
     control.setValue('2019-12-24T09:15:00.000');
     const validator = NocValidators.dateTimeValidator();
-    expect(validator(control)).toBeNull();
+    expect(validator(control)).toBeUndefined();
   });
 
   it('dateTimeValidator valid 24 hour case', () => {
     control.setValue('2019-12-24T22:00:00.000');
     const validator = NocValidators.dateTimeValidator();
-    expect(validator(control)).toBeNull();
+    expect(validator(control)).toBeUndefined();
   });
 
   it('dateTimeValidator invalid case', () => {
     control.setValue('2019-13-24T30:15:00.000');
     const validator = NocValidators.dateTimeValidator();
-    expect(validator(control)).toEqual({ datetime: true, month: true, year: true, valid: false });
+    expect(validator(control)).toEqual({ datetime: true, month: true, valid: false });
   });
 
   it('timeValidator valid 12 hour case', () => {
     control.setValue('09:15:00');
     const validator = NocValidators.timeValidator();
-    expect(validator(control)).toBeNull();
+    expect(validator(control)).toBeUndefined();
   });
 
   it('timeValidator valid 24 hour case', () => {
     control.setValue('22:00:00');
     const validator = NocValidators.timeValidator();
-    expect(validator(control)).toBeNull();
+    expect(validator(control)).toBeUndefined();
   });
 
   it('timeValidator invalid case', () => {

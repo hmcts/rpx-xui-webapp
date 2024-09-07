580bec26a232fc8d4b3b0ebc7636816b5a3f2f4c
Daniel Furnivall <danielfurnivall@gmail.com>
2024-09-07 14:49:39 +0100

    whole batch of other improvements brings it down to 21 seconds

--- api/webpack-node.config.js
+++ api/webpack-node.config.js
@@ -1,44 +1,48 @@
-/* eslint-disable */
 const path = require('path');
 const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 const TerserPlugin = require('terser-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-const {
-  NODE_ENV = 'production'
-} = process.env;
+const { NODE_ENV = 'production' } = process.env;
 
 module.exports = {
   optimization: {
     minimize: true,
     minimizer: [
       new TerserPlugin({
+        parallel: true,
         terserOptions: {
           sourceMap: true,
           compress: {
-            drop_console: true
-          }
-        }
-      })
-    ]
+            drop_console: true,
+          },
+        },
+      }),
+    ],
   },
   entry: ['./server.ts'],
   mode: NODE_ENV,
   target: 'node',
-  devtool: 'source-map',
+  devtool: NODE_ENV === 'production' ? 'source-map' : 'eval-cheap-module-source-map',
   node: {
     __dirname: false,
-    __filename: false
+    __filename: false,
+  },
+  cache: {
+    type: 'filesystem',
   },
   plugins: [
-    new webpack.DefinePlugin({ "global.GENTLY": false })
+    new webpack.DefinePlugin({ "global.GENTLY": false }),
+    new ForkTsCheckerWebpackPlugin(),
   ],
   output: {
     path: path.resolve(__dirname, '../dist/rpx-exui/api'),
-    filename: 'server.bundle.js'
+    filename: 'server.bundle.js',
   },
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
   },
   externals: [nodeExternals()],
   module: {
@@ -46,10 +50,16 @@ module.exports = {
       {
         test: /\.ts$/,
         use: [
-          'ts-loader'
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              experimentalWatchApi: true,
+            },
+          },
         ],
-        exclude: /node_modules/
-      }
-    ]
-  }
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };

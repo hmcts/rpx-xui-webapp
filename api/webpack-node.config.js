dcffebabfb14a4eee8ccbf4df1c5ffa104e4df12
Daniel Furnivall <danielfurnivall@gmail.com>
2024-09-07 15:22:39 +0100

    second batch of changes - reduces bundle size slightly

--- api/webpack-node.config.js
+++ api/webpack-node.config.js
@@ -4,25 +4,38 @@ const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 const TerserPlugin = require('terser-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-
 const { NODE_ENV = 'production' } = process.env;
 
 module.exports = {
   optimization: {
-    minimize: true,
+    minimize: NODE_ENV === 'production',
     minimizer: [
       new TerserPlugin({
         parallel: true,
         terserOptions: {
-          sourceMap: true,
           compress: {
-            drop_console: true,
+            drop_console: NODE_ENV === 'production',
+            dead_code: true,
+            drop_debugger: true,
+            conditionals: true,
+            evaluate: true,
+            booleans: true,
+            loops: true,
+            unused: true,
+            hoist_funs: true,
+            keep_fargs: false,
+            hoist_vars: true,
+            if_return: true,
+            join_vars: true,
+            side_effects: true,
+            warnings: false,
           },
         },
       }),
     ],
+    // Remove splitChunks configuration
   },
-  entry: ['./server.ts'],
+  entry: './server.ts', // Changed to a single entry point
   mode: NODE_ENV,
   target: 'node',
   devtool: NODE_ENV === 'production' ? 'source-map' : 'eval-cheap-module-source-map',
@@ -32,18 +45,31 @@ module.exports = {
   },
   cache: {
     type: 'filesystem',
+    compression: 'gzip',
   },
   plugins: [
     new webpack.DefinePlugin({ "global.GENTLY": false }),
-    new ForkTsCheckerWebpackPlugin(),
+    new ForkTsCheckerWebpackPlugin({
+      typescript: {
+        memoryLimit: 4096, // Increase memory limit to 4GB
+      },
+    }),
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^\.\/locale$/,
+      contextRegExp: /moment$/,
+    }),
   ],
   output: {
     path: path.resolve(__dirname, '../dist/rpx-exui/api'),
     filename: 'server.bundle.js',
+    clean: true,
   },
   resolve: {
     extensions: ['.ts', '.js'],
     modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
   externals: [nodeExternals()],
   module: {

1e32e8867a349df1ac81690ee05167fe97e80bdb
Hemanth Potipati <hrpotipati@gmail.com>
2024-09-09 22:25:18 +0100

    Update and rename login.test.ts to login.spec.ts

--- playwright_tests/E2E/login.spec.ts
+++ playwright_tests/E2E/login.spec.ts
@@ -1,22 +1,26 @@
 import { test, expect } from '@playwright/test';
-import { signIn } from "./steps/login-steps"
+import { signIn } from "../steps/login-steps"
+//import axeTest from "../helpers/accessibilityTestHelper";
 import config from "../config"
 
-test('login and log out from EUI with valid user', async ({ page }) => {
+test('login and log out from EUI with valid user @login', async ({ page }) => {
   await signIn(page, 'PROD_LIKE');
   await expect(page.getByRole('heading', { name: 'Case list' })).toBeVisible();
   await page.getByText('Sign out').click();
   await expect(page.getByRole('heading', { name: 'Sign in or create an account' })).toBeVisible();
+  //await axeTest(page);
 });
 
 test('login Verify the direct link navigate to login page', async ({ page }) => {
     await page.goto(config.CaseBaseURL);
+    //await axeTest(page);
     await expect(page.getByRole('heading', { name: 'Sign in or create an account' })).toBeVisible();
     await expect(page.url()).toContain('idam-web-public');
   });
 
   test('login un-authenticated user login', async ({ page }) => {
     await page.goto(config.CaseBaseURL);
+    //await axeTest(page);
     await page.getByLabel('Email address').fill('test_nonexisting_or_invalid@gmail.com');
     await page.getByLabel('Password').click();
     await page.getByLabel('Password').fill('123');
@@ -24,4 +28,3 @@ test('login Verify the direct link navigate to login page', async ({ page }) => 
     await expect(page.getByRole('heading', { name: 'Incorrect email or password' })).toBeVisible();
     await expect(page.url()).toContain('idam-web-public');
   });
-

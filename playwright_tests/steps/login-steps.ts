57135346053a9355242fe95ec69a212d9df1df92
Hemanth Potipati <hrpotipati@gmail.com>
2024-09-09 22:34:04 +0100

    Create login-steps.ts

---
+++ playwright_tests/steps/login-steps.ts
@@ -0,0 +1,27 @@
+import config from "../config";
+const testConfig = require("../../test_codecept/e2e/config/appTestConfig");
+
+export async function signIn(page: any, userIdentifier: string) {
+  const matchingUsers = testConfig.users[testConfig.testEnv].filter(
+    (user: any) => user.userIdentifier === userIdentifier
+  );
+  const email = matchingUsers[0].email;
+  await page.goto(config.CaseBaseURL);
+  await page.getByLabel("Email address").click();
+  await page.getByLabel("Email address").fill(email);
+  await page.getByLabel("Password").click();
+  await page.getByLabel("Password").fill(matchingUsers[0].key);
+  await page.getByRole("button", { name: "Sign in" }).click();
+  console.log("Signed in as " + email);
+
+}
+
+export async function signOut(page) {
+  try {
+    await page.getByText("Sign out").click();
+    console.log("Signed out");
+  } catch (error) {
+    console.log(`Sign out failed: ${error}`);
+  }
+
+}

16b14ce66900b69a64e544698819d0314db14faf
Josh <josh.glasgow@hmcts.net>
2024-09-09 09:15:05 +0100

    Update to send header config in configUI. remove new api files, add config files for each env

---
+++ api/configuration/menuConfigs/configs.ts
@@ -0,0 +1,9 @@
+import { previewConfig } from './preview-config';
+import { aatConfig } from './aat-config';
+import { prodConfig } from './prod-config';
+
+export const menuConfigs = {
+  'preview': previewConfig,
+  'aat': aatConfig,
+  'prod': prodConfig
+};
--- api/header/routes.ts
+++
@@ -1,6 +0,0 @@
-import * as express from 'express';
-import { getHeaderConfig } from './index';
-
-export const router = express.Router({ mergeParams: true });
-
-router.get('/config', getHeaderConfig);

aa7cd249695361d8a2ef20f26e77d2b29591855a
Hemanth Potipati <hrpotipati@gmail.com>
2024-09-09 22:01:05 +0100

    Add TCCreateCase_1.spec.ts

---
+++ playwright_tests/E2E/TCCreateCase_1.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+import { signIn } from "../steps/login-steps"
+// import axeTest from "../helpers/accessibilityTestHelper";
+// import config from "../config"
+import { validateCaseEventNextStepTriggerActions, validateUpdateFormPageNextStepTriggerActions } from '../steps/search-steps';
+import { validateInvalidDateErrorMessage } from '../steps/createCase-steps';
+
+test.beforeEach(async ({ page }) => {
+    await signIn(page, 'PROD_LIKE');
+    await expect(page.getByRole('heading', { name: 'Case list' })).toBeVisible();
+});
+
+test('Validate Case event next step trigger actions @test', async ({ page }) => {
+  await validateCaseEventNextStepTriggerActions(page);
+});
+
+test('Validate update form page click on next step trigger actions @test', async ({ page }) => {
+  await validateUpdateFormPageNextStepTriggerActions(page);
+});
+
+test('Validate invalid date error message @test', async ({ page }) => {
+  await validateInvalidDateErrorMessage(page);
+});

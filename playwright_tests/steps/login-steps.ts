d269a531b8e9b7f42cbbd8687d504985d8a4dcf5
Hemanth Potipati <hrpotipati@gmail.com>
2024-10-01 15:02:24 +0100

    Update login-steps.ts

--- playwright_tests/steps/login-steps.ts
+++ playwright_tests/steps/login-steps.ts
@@ -1,19 +1,19 @@
 import config from "../config";
 const testConfig = require("../../test_codecept/e2e/config/appTestConfig");
 
-export async function signIn(page: any, userIdentifier: string) {
+export async function signIn(page: any, userIdentifier: string, goToCaseBaseURL: boolean = true) {
   const matchingUsers = testConfig.users[testConfig.testEnv].filter(
     (user: any) => user.userIdentifier === userIdentifier
   );
   const email = matchingUsers[0].email;
-  await page.goto(config.CaseBaseURL);
+  if (goToCaseBaseURL) await page.goto(config.CaseBaseURL);
+  else await page.goto(config.AppBaseURL);
   await page.getByLabel("Email address").click();
   await page.getByLabel("Email address").fill(email);
   await page.getByLabel("Password").click();
   await page.getByLabel("Password").fill(matchingUsers[0].key);
   await page.getByRole("button", { name: "Sign in" }).click();
   console.log("Signed in as " + email);
-
 }
 
 export async function signOut(page) {
@@ -23,5 +23,4 @@ export async function signOut(page) {
   } catch (error) {
     console.log(`Sign out failed: ${error}`);
   }
-
 }

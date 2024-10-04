1d8fa3dbf0a0681993ab3b7d61eb4b6b131e5725
hemantt <hrpotipati@gmail.com>
2024-10-04 10:35:35 +0100

    revert last commit

--- playwright_tests/steps/login-steps.ts
+++ playwright_tests/steps/login-steps.ts
@@ -1,13 +1,11 @@
 import config from "../config";
 const testConfig = require("../../test_codecept/e2e/config/appTestConfig");
-
-export async function signIn(page: any, userIdentifier: string, goToCaseBaseURL: boolean = true) {
+export async function signIn(page: any, userIdentifier: string) {
   const matchingUsers = testConfig.users[testConfig.testEnv].filter(
     (user: any) => user.userIdentifier === userIdentifier
   );
   const email = matchingUsers[0].email;
-  if (goToCaseBaseURL) await page.goto(config.CaseBaseURL);
-  else await page.goto(config.AppBaseURL);
+  await page.goto(config.CaseBaseURL);
   await page.getByLabel("Email address").click();
   await page.getByLabel("Email address").fill(email);
   await page.getByLabel("Password").click();
@@ -15,7 +13,6 @@ export async function signIn(page: any, userIdentifier: string, goToCaseBaseURL:
   await page.getByRole("button", { name: "Sign in" }).click();
   console.log("Signed in as " + email);
 }
-
 export async function signOut(page) {
   try {
     await page.getByText("Sign out").click();

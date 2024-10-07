0decf8a7ab831ce655c4bc911fac253991f95a05
Josh <josh.glasgow@hmcts.net>
2024-10-07 14:55:25 +0100

    Add appinsight log when pod is using session cookie

--- api/auth/index.ts
+++ api/auth/index.ts
@@ -25,22 +25,20 @@ import {
   SYSTEM_USER_NAME,
   SYSTEM_USER_PASSWORD
 } from '../configuration/references';
-import { client } from '../lib/appInsights';
+import { client, trackTrace } from '../lib/appInsights';
 import * as log4jui from '../lib/log4jui';
 import { EnhancedRequest } from '../lib/models';
-const os = require('os');
 
 const logger = log4jui.getLogger('auth');
 
-const podName = process.env.HOSTNAME || os.hostname();
 const totalReplicas = 24;
 const specialReplicasCount = 2;
 
-function shouldSetSpecialFlag(totalReplicas, specialReplicasCount) {
+function shouldSetSessionCookieFlag(totalReplicas, specialReplicasCount) {
   const randomNumber = Math.floor(Math.random() * totalReplicas);
   return randomNumber < specialReplicasCount;
 }
-const isSpecialPod = shouldSetSpecialFlag(totalReplicas, specialReplicasCount);
+const isSpecialPod = shouldSetSessionCookieFlag(totalReplicas, specialReplicasCount);
 
 export const successCallback = (req: EnhancedRequest, res: Response, next: NextFunction) => {
   const { user } = req.session.passport;
@@ -129,9 +127,10 @@ export const getXuiNodeMiddleware = () => {
     secret: getConfigValue(SESSION_SECRET)
   } as SessionMetadata;
 
-  console.log(`pod name : ${podName}`);
   if (!isSpecialPod){
     baseStoreOptions.cookie.maxAge = 28800000;
+  } else {
+    trackTrace('Pod is serving session cookie');
   }
 
   const redisStoreOptions = {

deeb3ebe57ce43e1fae0b855bd9fd5af2801df5c
Josh <josh.glasgow@hmcts.net>
2024-11-07 11:47:09 +0000

    read defaultjson

--- test_codecept/backendMock/configCopy.js
+++ test_codecept/backendMock/configCopy.js
@@ -1,11 +1,25 @@
 const fs = require('fs')
 const path = require('path')
 
-const files = fs.readdirSync(path.resolve(__dirname, '../../config'))
+const files = fs.readdirSync(path.resolve(__dirname, '../../config'));
 
 console.log(files);
 
-const mockConfig = files.find(file => file === 'local-mock.json')
+const mockConfig = files.find(file => file === 'local-mock.json');
+let defaultConfig;
+let defaultSecrets;
+if (process.env.LOCAL){
+    defaultConfig = files.find((file) => file === 'local-development.json');
+} else {
+    if (files.find((file) => file === 'default.json')){
+        const defaultConfigPath = path.join(__dirname, '../../config', 'default.json');
+        defaultConfig = JSON.parse(fs.readFileSync(defaultConfigPath, 'utf8'));
+        console.log(defaultConfig);
+    }
+
+}
+
+console.log(defaultConfig);
 
 if (mockConfig === undefined){
     fs.copyFileSync(path.resolve(__dirname, 'local-mock.json'), path.resolve(__dirname, '../../config/local-mock.json') )

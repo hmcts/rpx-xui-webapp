be6e2a408b99177f91050f217a902dc02c5076c6
Anthony Dummer <anthony.dummer@HMCTS.net>
2024-11-21 11:09:10 +0000

    EXUI-2544 - removing app insights trip

--- src/cases/containers/case-viewer-container/case-viewer-container.component.ts
+++ src/cases/containers/case-viewer-container/case-viewer-container.component.ts
@@ -134,14 +134,12 @@ export class CaseViewerContainerComponent implements OnInit {
     const caseJurisdiction = this.caseDetails && this.caseDetails.case_type && this.caseDetails.case_type.jurisdiction ? this.caseDetails.case_type.jurisdiction.id : null;
     const caseType = this.caseDetails && this.caseDetails.case_type ? this.caseDetails.case_type.id : null;
     let requiredFeature = false;
-    this.loggerService.log('in enablePrependedTabs');
-    if (!isEmpty(features)){
-      this.loggerService.log('in check of empty features  ');
+
+    if (isEmpty(features)){
       this.launchDarklyError('WAFeatureConfig');
       features = this.waDefaultServiceConfig;
     }
     if (isEmpty(supportedServices)){
-      this.loggerService.log('in check of empty supportedServices ');
       this.launchDarklyError('WASupportedJurisdictions');
       supportedServices = this.waDefaultSupportedJurisdictions;
     }

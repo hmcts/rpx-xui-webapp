73bffe883dd6f4de6f453a69a939105567a4bfda
RiteshHMCTS <74713687+RiteshHMCTS@users.noreply.github.com>
2024-12-10 17:20:23 +0000

    exui-2472-playwright-crossbrowser-tests (#4148)
    * run playwright crossbrowser tests
    
    * changed command

---
+++ playwright-nightly.config.ts
@@ -0,0 +1,62 @@
+import { defineConfig, devices } from "@playwright/test";
+
+const headlessMode = process.env.HEAD !== 'true';
+export const axeTestEnabled = process.env.ENABLE_AXE_TESTS === 'true';
+
+module.exports = defineConfig({
+  testDir: "./playwright_tests/E2E",
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: 3, // Set the number of retries for all projects
+
+  timeout: 3 * 60 * 1000,
+  expect: {
+    timeout: 1 * 60 * 1000
+  },
+  reportSlowTests: null,
+
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.FUNCTIONAL_TESTS_WORKERS ? parseInt(process.env.FUNCTIONAL_TESTS_WORKERS, 10) : 1,
+
+  reporter: [[process.env.CI ? 'html' : 'list'],
+    ['html', { open: 'never', outputFolder: 'functional-output/tests/playwright-e2e' }]],
+
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"],
+        channel: "chrome",
+        headless: headlessMode,
+        trace: "on-first-retry",
+      },
+    },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"],
+        screenshot: 'only-on-failure',
+        headless: headlessMode,
+        trace: 'off'
+      },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"],
+        screenshot: 'only-on-failure',
+        headless: headlessMode,
+        trace: 'off'
+      },
+    },
+    {
+      name: "MicrosoftEdge",
+      use: { ...devices["Desktop Edge"],
+        channel: "msedge",
+        screenshot: 'only-on-failure',
+        headless: headlessMode,
+        trace: 'off'
+      },
+    }
+  ]
+});

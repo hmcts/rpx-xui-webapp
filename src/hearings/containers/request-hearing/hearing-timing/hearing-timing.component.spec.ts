6d2053b36903cbe51664d4faa626e68c10aa66e9
Anthony Dummer <anthony.dummer@HMCTS.net>
2024-12-13 13:13:39 +0000

    applying to fix to failing unit test.

--- src/hearings/containers/request-hearing/hearing-timing/hearing-timing.component.spec.ts
+++ src/hearings/containers/request-hearing/hearing-timing/hearing-timing.component.spec.ts
@@ -360,12 +360,12 @@ describe('HearingTimingComponent', () => {
     component.earliestDateOfHearingError = null;
     component.earliestHearingFormGroup.get('earliestHearingDate_day').setValue('13');
     component.earliestHearingFormGroup.get('earliestHearingDate_month').setValue('12');
-    component.earliestHearingFormGroup.get('earliestHearingDate_year').setValue('2024');
+    component.earliestHearingFormGroup.get('earliestHearingDate_year').setValue('2025');
     component.latestHearingFormGroup.get('latestHearingDate_day').setValue('11');
     component.latestHearingFormGroup.get('latestHearingDate_month').setValue('12');
-    component.latestHearingFormGroup.get('latestHearingDate_year').setValue('2024');
+    component.latestHearingFormGroup.get('latestHearingDate_year').setValue('2025');
     component.showChosenDateRangeError();
-    expect(component.latestDateOfHearingError.isInvalid).toBeTruthy();
+    expect(component.earliestDateOfHearingError.isInvalid).toBeTruthy();
   });
 
   it('should check showChosenDateRangeError valid earliest date check', () => {

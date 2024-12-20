a251825a5a435e6a9b6a8b296b28b802ce88edcb
Andy Wilkins <49269487+andywilkinshmcts@users.noreply.github.com>
2024-12-20 13:59:27 +0000

    Fix test

--- api/staffSupportedJurisdictions/index.spec.ts
+++ api/staffSupportedJurisdictions/index.spec.ts
@@ -26,12 +26,12 @@ describe('Staff Supported Jurisdictions', () => {
   it('should get supported jurisdictions', async() => {
     await staffSupportedJurisdictions.getStaffSupportedJurisdictions(req, res, null);
     // note: CMC not included for caseworker errors
-    const response = ['ST_CIC', 'CIVIL', 'EMPLOYMENT', 'PRIVATELAW', 'PUBLICLAW', 'IA', 'SSCS'];
+    const response = ['ST_CIC', 'CIVIL', 'EMPLOYMENT', 'PRIVATELAW', 'PUBLICLAW', 'IA', 'SSCS', 'DIVORCE', 'FR'];
     expect(res.send).to.have.been.calledWith(sinon.match(response));
   });
 
   it('should get only the list of supported jurisdictions', async() => {
     const jurisdictionList = staffSupportedJurisdictions.getStaffSupportedJurisdictionsList();
-    expect(jurisdictionList).to.deep.equal(['ST_CIC', 'CIVIL', 'EMPLOYMENT', 'PRIVATELAW', 'PUBLICLAW', 'IA', 'SSCS']);
+    expect(jurisdictionList).to.deep.equal(['ST_CIC', 'CIVIL', 'EMPLOYMENT', 'PRIVATELAW', 'PUBLICLAW', 'IA', 'SSCS', 'DIVORCE', 'FR']);
   });
 });

e2107f72612c3a64f5644f767e5934cfa492b3da
RiteshHMCTS <74713687+RiteshHMCTS@users.noreply.github.com>
2025-01-22 16:13:01 +0000

    exui-1717-show warning, error or success message simultaneously (#4128)
    * to show warning, error or success message simultaneously
    
    * lint error
    
    * toolkit version updated
    
    * unit test added
    
    * yarn lock change
    
    * toolkit version updated
    
    * toolkit version updated
    
    * toolkit version updated
    
    * toolkit version updated
    
    ---------
    
    Co-authored-by: Andy Wilkins <49269487+andywilkinshmcts@users.noreply.github.com>

--- src/cases/components/case-alert/alert.component.spec.ts
+++ src/cases/components/case-alert/alert.component.spec.ts
@@ -4,6 +4,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AlertComponent as CCDAlertComponent, AlertIconClassPipe, AlertService } from '@hmcts/ccd-case-ui-toolkit';
 import { of } from 'rxjs';
 import { AlertComponent } from './alert.component';
+import { By } from '@angular/platform-browser';
 
 @Pipe({ name: 'rpxTranslate' })
 class RpxTranslateMockPipe implements PipeTransform {
@@ -17,7 +18,7 @@ describe('AlertComponent', () => {
   let fixture: ComponentFixture<AlertComponent>;
 
   const alertServiceMock = {
-    alerts: of({ message: 'message', level: 'success' })
+    alerts: of({ message: 'success message', level: 'success' })
   };
 
   beforeEach(waitForAsync(() => {
@@ -54,8 +55,10 @@ describe('AlertComponent', () => {
     component.ngOnInit();
     await fixture.whenStable();
     fixture.detectChanges();
-    expect(component.message).toEqual('message');
+    const alertElement = fixture.debugElement.query(By.css('.alert-message')).nativeElement.textContent;
+    expect(component.successMessage).toEqual('success message');
     expect(component.level).toEqual('success');
+    expect(alertElement).toBe(' success message ');
   });
 
   it('should unsubscribe onDestroy', () => {
@@ -80,7 +83,53 @@ describe('AlertComponent', () => {
   });
 
   it('should hyphenate every 4th digit of alert', () => {
-    component.message = '1234567890123456';
+    component.successMessage = '1234567890123456';
     expect(component.hyphenate('1234567890123456')).toBe('1234-5678-9012-3456');
+  });
+});
+
+describe('AlertComponent warning message', () => {
+  let component: AlertComponent;
+  let fixture: ComponentFixture<AlertComponent>;
+
+  const alertServiceMock = {
+    alerts: of({ message: 'warning message', level: 'warning' })
+  };
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes([
+        ])
+      ],
+      declarations: [AlertComponent, CCDAlertComponent, AlertIconClassPipe, RpxTranslateMockPipe],
+      providers: [{
+        provide: AlertService,
+        useValue: alertServiceMock
+      }]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AlertComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    spyOn(component, 'ngOnDestroy').and.callFake(() => {});
+    fixture.destroy();
+  });
+
+  it('should have updated the value to warning message and level in ngOnInit', async () => {
+    component.ngOnInit();
+    await fixture.whenStable();
+    fixture.detectChanges();
+    const alertElement = fixture.debugElement.query(By.css('.alert-message')).nativeElement.textContent;
+    expect(component.warningMessage).toEqual('warning message');
+    expect(component.level).toEqual('warning');
+    expect(alertElement).toBe(' warning message ');
   });
 });

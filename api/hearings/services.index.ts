a0b2620813b8ce79ef6c832e6693b2d36ebf59ee
Anthony Dummer <anthony.dummer@HMCTS.net>
2025-02-10 12:44:47 +0000

    re-adding comment

--- api/hearings/services.index.ts
+++ api/hearings/services.index.ts
@@ -27,6 +27,7 @@ export async function loadServiceHearingValues(req: EnhancedRequest, res: Respon
   try {
     const { status, data }: { status: number, data: ServiceHearingValuesModel } = await sendPost(markupPath, reqBody, req);
     let dataByDefault = mapDataByDefault(data);
+    // If service don't supply the screenFlow pre-set the default screen flow from ExUI
     if (!data.screenFlow) {
       dataByDefault = {
         ...data,

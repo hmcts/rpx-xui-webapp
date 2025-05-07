dc0afae883db223dfa7970cfce63ae08ac0dc971
Anthony Dummer <anthony.dummer@HMCTS.net>
2025-05-07 09:25:16 +0100

    peer review changes plus test enhancement for provided screens

--- src/hearings/components/radio-list-builder/radio-list-builder.component.ts
+++ src/hearings/components/radio-list-builder/radio-list-builder.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FormArray, FormGroup, FormControl } from '@angular/forms';
 
 interface RadioListItem {

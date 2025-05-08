3eeda8ed799847d8248e3532066ce4cde93170c4
Munish Sharma <munish.sharma@hmcts.net>
2025-05-08 11:50:26 +0100

    using createApp instead of app in redisHealth

--- api/health/redis.health.ts
+++ api/health/redis.health.ts
@@ -1,10 +1,11 @@
-import { app } from '../application';
+import { createApp } from '../application';
 import * as log4jui from '../lib/log4jui';
 import { JUILogger } from '../lib/models';
 
 const logger: JUILogger = log4jui.getLogger('RedisHealth');
 
-export const redisHealth = (): Promise<boolean> => {
+export const redisHealth = async (): Promise<boolean> => {
+  const app = await createApp();
   return new Promise<boolean>((resolve) => {
     try {
       app.locals.redisClient.ping((err, pong) => {

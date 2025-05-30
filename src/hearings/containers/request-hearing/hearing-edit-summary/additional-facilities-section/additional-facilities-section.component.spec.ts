1ac94f362b4228b198861d764c6309f2fd18aa37
Anthony Dummer <anthony.dummer@HMCTS.net>
2025-05-30 18:09:37 +0100

    Adding test for coverage

--- src/hearings/containers/request-hearing/hearing-edit-summary/additional-facilities-section/additional-facilities-section.component.spec.ts
+++ src/hearings/containers/request-hearing/hearing-edit-summary/additional-facilities-section/additional-facilities-section.component.spec.ts
@@ -337,4 +337,166 @@ describe('AdditionalFacilitiesSectionComponent', () => {
       expect(component.pageTitleDisplayLabel).toEqual(AmendmentLabelStatus.AMENDED);
     });
   });
+  describe('setAmendmentLabels', () => {
+    beforeEach(() => {
+      // Reset properties that might be affected by previous tests
+      component.pageTitleDisplayLabel = undefined;
+      component.facilitiesRequiredToCompare = [];
+      component.caseAdditionalSecurityFlagChanged = false;
+      component.facilitiesChanged = false;
+
+      // Reset service state
+      hearingsService.propertiesUpdatedOnPageVisit = {
+        hearingId: 'h000001',
+        caseFlags: initialState.hearings.hearingValues.serviceHearingValuesModel.caseFlags,
+        parties: initialState.hearings.hearingValues.serviceHearingValuesModel.parties,
+        hearingWindow: initialState.hearings.hearingValues.serviceHearingValuesModel.hearingWindow,
+        afterPageVisit: {
+          reasonableAdjustmentChangesRequired: false,
+          nonReasonableAdjustmentChangesRequired: false,
+          nonReasonableAdjustmentChangesConfirmed: false,
+          partyDetailsChangesRequired: false,
+          hearingWindowChangesRequired: false,
+          hearingFacilitiesChangesRequired: false,
+          hearingFacilitiesChangesConfirmed: false,
+          hearingUnavailabilityDatesChanged: false
+        }
+      };
+    });
+
+    it('should set EMPTY label when no changes are detected', () => {
+      // Set identical models to ensure no changes
+      component.hearingRequestMainModel = {...initialState.hearings.hearingRequest.hearingRequestMainModel};
+      component.hearingRequestToCompareMainModel = {...initialState.hearings.hearingRequest.hearingRequestMainModel};
+
+      // Call ngOnInit which will trigger setAmendmentLabels()
+      component.ngOnInit();
+
+      expect(component.caseAdditionalSecurityFlagChanged).toBeFalse();
+      expect(component.facilitiesChanged).toBeFalse();
+      expect(component.pageTitleDisplayLabel).toEqual(AmendmentLabelStatus.EMPTY);
+    });
+
+    it('should set AMENDED label when security flag has changed', () => {
+      // Change security flag in main model
+      component.hearingRequestMainModel = {
+        ...initialState.hearings.hearingRequest.hearingRequestMainModel,
+        caseDetails: {
+          ...initialState.hearings.hearingRequest.hearingRequestMainModel.caseDetails,
+          caseAdditionalSecurityFlag: true
+        }
+      };
+
+      component.hearingRequestToCompareMainModel = {
+        ...initialState.hearings.hearingRequest.hearingRequestMainModel,
+        caseDetails: {
+          ...initialState.hearings.hearingRequest.hearingRequestMainModel.caseDetails,
+          caseAdditionalSecurityFlag: false
+        }
+      };
+
+      component.ngOnInit();
+
+      expect(component.caseAdditionalSecurityFlagChanged).toBeTrue();
+      expect(component.pageTitleDisplayLabel).toEqual(AmendmentLabelStatus.AMENDED);
+    });
+
+    it('should set AMENDED label when facilities have changed', () => {
+      // Change facilities in main model
+      component.hearingRequestMainModel = {
+        ...initialState.hearings.hearingRequest.hearingRequestMainModel,
+        hearingDetails: {
+          ...initialState.hearings.hearingRequest.hearingRequestMainModel.hearingDetails,
+          facilitiesRequired: ['newFacility', 'inCameraCourt']
+        }
+      };
+
+      component.ngOnInit();
+
+      expect(component.facilitiesChanged).toBeTrue();
+      expect(component.pageTitleDisplayLabel).toEqual(AmendmentLabelStatus.AMENDED);
+    });
+
+    it('should set ACTION_NEEDED label when nonReasonableAdjustmentChangesRequired but not confirmed', () => {
+      hearingsService.propertiesUpdatedOnPageVisit.afterPageVisit.nonReasonableAdjustmentChangesRequired = true;
+      hearingsService.propertiesUpdatedOnPageVisit.afterPageVisit.nonReasonableAdjustmentChangesConfirmed = false;
+
+      component.ngOnInit();
+
+      expect(component.pageTitleDisplayLabel).toEqual(AmendmentLabelStatus.ACTION_NEEDED);
+    });
+
+    it('should set ACTION_NEEDED label when hearingFacilitiesChangesRequired but not confirmed', () => {
+      hearingsService.propertiesUpdatedOnPageVisit.afterPageVisit.hearingFacilitiesChangesRequired = true;
+      hearingsService.propertiesUpdatedOnPageVisit.afterPageVisit.hearingFacilitiesChangesConfirmed = false;
+
+      component.ngOnInit();
+
+      expect(component.pageTitleDisplayLabel).toEqual(AmendmentLabelStatus.ACTION_NEEDED);
+    });
+
+    it('should prioritize ACTION_NEEDED over AMENDED', () => {
+      // Make changes that would normally result in AMENDED
+      component.hearingRequestMainModel = {
+        ...initialState.hearings.hearingRequest.hearingRequestMainModel,
+        caseDetails: {
+          ...initialState.hearings.hearingRequest.hearingRequestMainModel.caseDetails,
+          caseAdditionalSecurityFlag: true
+        }
+      };
+
+      // But also set conditions for ACTION_NEEDED
+      hearingsService.propertiesUpdatedOnPageVisit.afterPageVisit.nonReasonableAdjustmentChangesRequired = true;
+      hearingsService.propertiesUpdatedOnPageVisit.afterPageVisit.nonReasonableAdjustmentChangesConfirmed = false;
+
+      component.ngOnInit();
+
+      expect(component.caseAdditionalSecurityFlagChanged).toBeTrue();
+      expect(component.pageTitleDisplayLabel).toEqual(AmendmentLabelStatus.ACTION_NEEDED);
+    });
+
+    it('should set facilitiesRequiredToCompare from the compare model', () => {
+      // Set some specific facilities in the compare model
+      component.hearingRequestToCompareMainModel = {
+        ...initialState.hearings.hearingRequest.hearingRequestMainModel,
+        hearingDetails: {
+          ...initialState.hearings.hearingRequest.hearingRequestMainModel.hearingDetails,
+          facilitiesRequired: ['inCameraCourt', 'secureDock']
+        }
+      };
+
+      component.ngOnInit();
+
+      // Check if the facilitiesRequiredToCompare was populated correctly
+      expect(component.facilitiesRequiredToCompare).toContain('in camera court');
+      expect(component.facilitiesRequiredToCompare).toContain('secure dock');
+    });
+
+    it('should identify when specific facilities have been added or removed', () => {
+      // Main model has one facility that compare model doesn't have
+      component.hearingRequestMainModel = {
+        ...initialState.hearings.hearingRequest.hearingRequestMainModel,
+        hearingDetails: {
+          ...initialState.hearings.hearingRequest.hearingRequestMainModel.hearingDetails,
+          facilitiesRequired: ['inCameraCourt', 'witnessScreen']
+        }
+      };
+
+      component.hearingRequestToCompareMainModel = {
+        ...initialState.hearings.hearingRequest.hearingRequestMainModel,
+        hearingDetails: {
+          ...initialState.hearings.hearingRequest.hearingRequestMainModel.hearingDetails,
+          facilitiesRequired: ['inCameraCourt']
+        }
+      };
+
+      component.ngOnInit();
+
+      // The facility "witness screen" should be marked as amended (not in compare list)
+      expect(component.showAmendedForFacilitiesRequired('witness screen')).toBeTrue();
+      // The facility "in camera court" should not be marked as amended (in both lists)
+      expect(component.showAmendedForFacilitiesRequired('in camera court')).toBeFalse();
+    });
+  });
+
 });

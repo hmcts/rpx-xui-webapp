2224c864f7f921dac8230582b0859794c3a29c78
RiteshHMCTS <ritesh.dsouza@hmcts.net>
2025-06-04 11:34:16 +0100

    url change

--- src/role-access/containers/add-exclusion/add-exclusion-home/add-exclusion-home.component.ts
+++ src/role-access/containers/add-exclusion/add-exclusion-home/add-exclusion-home.component.ts
@@ -46,6 +46,7 @@ export class AddExclusionHomeComponent implements OnInit, OnDestroy {
   public exclusionOption: ExcludeOption;
   public caseId: string;
   public jurisdiction: string;
+  public caseType: string
 
   public navEvent: ExclusionNavigation;
 
@@ -60,10 +61,12 @@ export class AddExclusionHomeComponent implements OnInit, OnDestroy {
   constructor(private readonly store: Store<fromFeature.State>,
               private readonly route: ActivatedRoute,
               private readonly router: Router) {
-    this.caseId = this.route.snapshot.queryParams && this.route.snapshot.queryParams.caseId ?
+    this.caseId = this.route.snapshot.queryParams?.caseId ?
       this.route.snapshot.queryParams.caseId : '';
-    this.jurisdiction = this.route.snapshot.queryParams && this.route.snapshot.queryParams.jurisdiction ?
+    this.jurisdiction = this.route.snapshot.queryParams?.jurisdiction ?
       this.route.snapshot.queryParams.jurisdiction : '';
+    this.caseType = this.route.snapshot.queryParams?.caseType ?
+      this.route.snapshot.queryParams.caseType : '';
     this.store.dispatch(new fromFeature.ExclusionSetCaseId(this.caseId, this.jurisdiction));
   }
 
@@ -150,7 +153,7 @@ export class AddExclusionHomeComponent implements OnInit, OnDestroy {
         break;
       }
       case ExclusionNavigationEvent.CANCEL:
-        this.router.navigateByUrl(`cases/case-details/${this.caseId}/roles-and-access`);
+        this.router.navigateByUrl(`cases/case-details/${this.jurisdiction}/${this.caseType}/${this.caseId}/roles-and-access`);
         break;
       default:
         this.showSpinner = false;

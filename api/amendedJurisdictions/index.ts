73a944c70734a81bb6292b31dcd46b8df49d5f2c
Antony Leons <antony@leons.dev>
2025-06-25 13:09:49 +0100

    lite models
    remove new model
    
    update tests
    
    make tests more realistic

--- api/amendedJurisdictions/index.ts
+++ api/amendedJurisdictions/index.ts
@@ -6,14 +6,35 @@ const jurisdictions = /aggregated\/.+jurisdictions\?/;
 /**
  * Manually filtering returned jurisdictions
  * to make available jurisdiction in filters array only
+ * and reducing the data size by only keeping the required fields
  */
-export const getJurisdictions = (proxyRes, req, res, data: any[]) => {
+export const getJurisdictions = (proxyRes, req, res, data: any[]): any[] | any => {
   if (!Array.isArray(data)
         || !jurisdictions.test(req.url)) {
     return data;
   }
+
   const filters = getConfigValue(JURISDICTIONS);
-  req.session.jurisdictions = [...data].filter((o) => filters.includes(o.id));
+  const filteredJurisdictions = [...data]
+    .filter((o) => filters.includes(o.id))
+    .map((jurisdiction) => {
+      return {
+        id: jurisdiction.id,
+        name: jurisdiction.name,
+        caseTypes: (jurisdiction.caseTypes || []).map((caseType) => {
+          return {
+            id: caseType.id,
+            name: caseType.name,
+            states: (caseType.states || []).map((state) => ({
+              id: state.id,
+              name: state.name
+            }))
+          };
+        })
+      };
+    });
+
+  req.session.jurisdictions = filteredJurisdictions;
   return req.session.jurisdictions;
 };
 

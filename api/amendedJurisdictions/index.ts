3ef6a6693949a09c6b69e8f20dab06df440d6791
Antony Leons <antony@leons.dev>
2025-06-25 13:17:32 +0100

    remove new model

--- api/amendedJurisdictions/index.ts
+++ api/amendedJurisdictions/index.ts
@@ -1,6 +1,5 @@
 import { getConfigValue } from '../configuration';
 import { JURISDICTIONS } from '../configuration/references';
-import { JurisdictionLite } from './jurisdictionLite.model';
 
 const jurisdictions = /aggregated\/.+jurisdictions\?/;
 
@@ -9,18 +8,18 @@ const jurisdictions = /aggregated\/.+jurisdictions\?/;
  * to make available jurisdiction in filters array only
  * and reducing the data size by only keeping the required fields
  */
-export const getJurisdictions = (proxyRes, req, res, data: any[]): JurisdictionLite[] | any => {
+export const getJurisdictions = (proxyRes, req, res, data: any[]): any[] | any => {
   if (!Array.isArray(data)
         || !jurisdictions.test(req.url)) {
     return data;
   }
-  
+
   // Debug information to measure the size reduction
   const originalDataSize = JSON.stringify(data).length;
   console.log(`[Jurisdictions] Original data size: ${originalDataSize} bytes`);
-  
+
   const filters = getConfigValue(JURISDICTIONS);
-  const filteredJurisdictions: JurisdictionLite[] = [...data]
+  const filteredJurisdictions = [...data]
     .filter((o) => filters.includes(o.id))
     .map((jurisdiction) => {
       return {
@@ -38,12 +37,12 @@ export const getJurisdictions = (proxyRes, req, res, data: any[]): JurisdictionL
         })
       };
     });
-  
+
   // Log the size reduction achieved
   const optimizedDataSize = JSON.stringify(filteredJurisdictions).length;
   console.log(`[Jurisdictions] Optimized data size: ${optimizedDataSize} bytes`);
   console.log(`[Jurisdictions] Size reduction: ${(((originalDataSize - optimizedDataSize) / originalDataSize) * 100).toFixed(2)}%`);
-  
+
   req.session.jurisdictions = filteredJurisdictions;
   return req.session.jurisdictions;
 };

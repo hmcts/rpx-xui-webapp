31d2f4eefb016007c1a7f1e59560e0e2972d8776
chrisjones-hmcts <chris.jones@cgi.com>
2025-06-27 15:28:25 +0100

    EXUI-3247: tidied up code

--- api/accessManagement/routes.spec.ts
+++ api/accessManagement/routes.spec.ts
@@ -17,7 +17,6 @@ describe('Access Management Routes', () => {
   beforeEach(() => {
     sandbox = sinon.createSandbox();
     
-    // Create a mock router with all the methods we need
     mockRouter = {
       use: sandbox.stub(),
       get: sandbox.stub(),
@@ -27,7 +26,6 @@ describe('Access Management Routes', () => {
       patch: sandbox.stub()
     };
 
-    // Stub express.Router to return our mock router
     expressStub = sandbox.stub(express, 'Router').returns(mockRouter);
   });
 
@@ -37,7 +35,6 @@ describe('Access Management Routes', () => {
 
   describe('Route Registration', () => {
     it('should create router with mergeParams option', () => {
-      // Re-require the routes module to trigger registration
       delete require.cache[require.resolve('./routes')];
       require('./routes');
 
@@ -45,7 +42,6 @@ describe('Access Management Routes', () => {
     });
 
     it('should register authInterceptor middleware', () => {
-      // Re-require the routes module to trigger registration
       delete require.cache[require.resolve('./routes')];
       require('./routes');
 
@@ -54,7 +50,6 @@ describe('Access Management Routes', () => {
     });
 
     it('should register POST /createBooking route', () => {
-      // Re-require the routes module to trigger registration
       delete require.cache[require.resolve('./routes')];
       require('./routes');
 
@@ -62,7 +57,6 @@ describe('Access Management Routes', () => {
     });
 
     it('should register POST /getBookings route', () => {
-      // Re-require the routes module to trigger registration
       delete require.cache[require.resolve('./routes')];
       require('./routes');
 
@@ -70,7 +64,6 @@ describe('Access Management Routes', () => {
     });
 
     it('should register POST /role-mapping/judicial/refresh route', () => {
-      // Re-require the routes module to trigger registration
       delete require.cache[require.resolve('./routes')];
       require('./routes');
 
@@ -78,7 +71,6 @@ describe('Access Management Routes', () => {
     });
 
     it('should register POST /specific-access-approval route', () => {
-      // Re-require the routes module to trigger registration
       delete require.cache[require.resolve('./routes')];
       require('./routes');
 
@@ -86,14 +78,11 @@ describe('Access Management Routes', () => {
     });
 
     it('should register all routes in correct order', () => {
-      // Re-require the routes module to trigger registration
       delete require.cache[require.resolve('./routes')];
       require('./routes');
 
-      // Verify middleware is registered first
       expect(mockRouter.use.firstCall).to.have.been.calledWith(authInterceptor);
 
-      // Verify all routes are registered
       const postCalls = mockRouter.post.getCalls();
       expect(postCalls).to.have.lengthOf(4);
       
@@ -111,7 +100,6 @@ describe('Access Management Routes', () => {
     });
 
     it('should export the router as default', () => {
-      // Re-require the routes module
       delete require.cache[require.resolve('./routes')];
       const routes = require('./routes');
 
@@ -121,7 +109,6 @@ describe('Access Management Routes', () => {
 
   describe('Handler Function References', () => {
     it('should use the correct handler functions from index module', () => {
-      // Verify that the imported functions exist
       expect(index.createBooking).to.be.a('function');
       expect(index.getBookings).to.be.a('function');
       expect(index.refreshRoleAssignments).to.be.a('function');
@@ -129,17 +116,14 @@ describe('Access Management Routes', () => {
     });
 
     it('should register handlers as direct references not wrapped functions', () => {
-      // Re-require the routes module to trigger registration
       delete require.cache[require.resolve('./routes')];
       require('./routes');
 
-      // Get the registered handlers
       const createBookingHandler = mockRouter.post.getCall(0).args[1];
       const getBookingsHandler = mockRouter.post.getCall(1).args[1];
       const refreshRoleHandler = mockRouter.post.getCall(2).args[1];
       const approveAccessHandler = mockRouter.post.getCall(3).args[1];
 
-      // Verify they are the exact same function references
       expect(createBookingHandler).to.equal(index.createBooking);
       expect(getBookingsHandler).to.equal(index.getBookings);
       expect(refreshRoleHandler).to.equal(index.refreshRoleAssignments);
@@ -149,47 +133,37 @@ describe('Access Management Routes', () => {
 
   describe('Router Configuration', () => {
     it('should only register POST routes', () => {
-      // Re-require the routes module to trigger registration
       delete require.cache[require.resolve('./routes')];
       require('./routes');
 
-      // Verify no other HTTP methods are used
       expect(mockRouter.get).to.not.have.been.called;
       expect(mockRouter.put).to.not.have.been.called;
       expect(mockRouter.delete).to.not.have.been.called;
       expect(mockRouter.patch).to.not.have.been.called;
       
-      // Verify POST is used
       expect(mockRouter.post).to.have.been.called;
       expect(mockRouter.post.callCount).to.equal(4);
     });
 
     it('should apply authInterceptor to all routes', () => {
-      // Re-require the routes module to trigger registration
       delete require.cache[require.resolve('./routes')];
       require('./routes');
 
-      // authInterceptor should be registered before any routes
       expect(mockRouter.use).to.have.been.calledBefore(mockRouter.post);
       expect(mockRouter.use.firstCall.args[0]).to.equal(authInterceptor);
     });
 
     it('should handle require cache properly for multiple imports', () => {
-      // First import
       delete require.cache[require.resolve('./routes')];
       const routes1 = require('./routes');
 
-      // Reset stubs to track new calls
       mockRouter.use.resetHistory();
       mockRouter.post.resetHistory();
 
-      // Second import (should use cache, no new registrations)
       const routes2 = require('./routes');
 
-      // Should return same instance
       expect(routes1).to.equal(routes2);
       
-      // Should not re-register routes
       expect(mockRouter.use).to.not.have.been.called;
       expect(mockRouter.post).to.not.have.been.called;
     });

0ffa93c0f55187e944d1e58ff04aeef85c5aca3f
Antony Leons <antony@leons.dev>
2025-07-28 11:34:53 +0100

    fix(monitoring): correct frequency and time_window values for azurerm_monitor_scheduled_query_rules_alert
    The values were updated from string durations to numeric values to match the expected format by the Azure provider

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -164,8 +164,8 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "welsh_usage_report" {
   QUERY
 
   severity    = 3
-  frequency   = "P1M"
-  time_window = "P1M"
+  frequency   = 5
+  time_window = 43200
 
   trigger {
     operator  = "GreaterThanOrEqual"

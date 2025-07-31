a329723dfb3af4934a9ab8f0719e64ef2469f3f0
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2025-07-31 15:39:28 +0100

    Update csp-config.ts

--- api/interfaces/csp-config.ts
+++ api/interfaces/csp-config.ts
@@ -5,14 +5,14 @@ export const MC_CSP: Parameters<typeof csp>[0] = {
   extraScript: [
     'https://www.googletagmanager.com',
     'https://www.google-analytics.com',
-    'az416426.vo.msecnd.net'
+    'az416426.vo.msecnd.net',
+    '\'unsafe-eval\''
   ],
 
   extraStyle: [
     'https://fonts.googleapis.com',
     'https://fonts.gstatic.com',
-    'https://www.googletagmanager.com',
-    "'unsafe-eval'"
+    'https://www.googletagmanager.com'
   ],
 
   extraFont: [

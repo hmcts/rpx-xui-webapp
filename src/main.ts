7b61530af30479493f5d654ea167de40cb3023dd
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2025-07-31 16:29:50 +0100

    Add bootstrap nonce

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,25 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, CSP_NONCE } from '@angular/core';
+import { platformBrowserDynamic }    from '@angular/platform-browser-dynamic';
+import { AppModule }                 from './app/app.module';
+import { environment }               from './environments/environment';
+import { ENVIRONMENT_CONFIG }        from './models/environmentConfig.model';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-import { ENVIRONMENT_CONFIG } from './models/environmentConfig.model';
-
-const meta = document.querySelector<HTMLMetaElement>('meta[name="csp-nonce"]');
-if (meta?.content) {
-  new MutationObserver((records) => {
-    for (const rec of records) {
-      rec.addedNodes.forEach((node) => {
-        if (node.nodeName === 'STYLE') {
-          (node as HTMLStyleElement).setAttribute('nonce', meta.content);
-        }
-      });
-    }
-  }).observe(document.head, { childList: true });
-}
+/* ①  Read the <meta name="csp-nonce"> placed by the server template */
+const nonce =
+  document.querySelector<HTMLMetaElement>('meta[name="csp-nonce"]')?.content ||
+  '';
 
 if (environment.production) {
   enableProdMode();
 }
 
-fetch('/external/configuration-ui/').then(async (response) => {
-  let config = await response.json();
-  config = config || {};
-  platformBrowserDynamic([{ provide: ENVIRONMENT_CONFIG, useValue: config }])
-    .bootstrapModule(AppModule)
-    .catch((err) => console.log(err));
-});
+/* ②  Fetch runtime config, then bootstrap with the nonce providers */
+fetch('/external/configuration-ui/')
+  .then(async res => (await res.json()) || {})
+  .then(config =>
+    platformBrowserDynamic([
+      { provide: ENVIRONMENT_CONFIG, useValue: config },
+      { provide: CSP_NONCE,          useValue: nonce },
+    ]).bootstrapModule(AppModule)
+  )
+  .catch(err => console.error(err));

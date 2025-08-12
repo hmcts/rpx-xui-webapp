7511ec39569287c32711a27d41c9032254c86759
Antony Leons <antony@leons.dev>
2025-08-12 14:12:37 +0100

    Revert "refactor(amendedJurisdictions): simplify jurisdiction filtering logic"
    This reverts commit e26a2a09339ac073d8e4cb79f6ac8acf89c82306.

--- api/amendedJurisdictions/index.ts
+++ api/amendedJurisdictions/index.ts
@@ -6,14 +6,48 @@ const jurisdictions = /aggregated\/.+jurisdictions\?/;
 /**
  * Manually filtering returned jurisdictions
  * to make available jurisdiction in filters array only
+ * and reducing the data size by only keeping the required fields
  */
-export const getJurisdictions = (proxyRes, req, res, data: any[]) => {
+export const getJurisdictions = (proxyRes, req, res, data: any[]): any[] | any => {
   if (!Array.isArray(data)
         || !jurisdictions.test(req.url)) {
     return data;
   }
+
   const filters = getConfigValue(JURISDICTIONS);
-  req.session.jurisdictions = [...data].filter((o) => filters.includes(o.id));
+  const filteredJurisdictions = [...data]
+    .filter((o) => filters.includes(o.id))
+    .map((jurisdiction) => {
+      const result: any = {
+        id: jurisdiction.id
+      };
+
+      if (jurisdiction.name !== undefined) {
+        result.name = jurisdiction.name;
+      }
+
+      if (jurisdiction.caseTypes !== undefined) {
+        result.caseTypes = jurisdiction.caseTypes.map((caseType) => {
+          const mappedCaseType: any = {
+            id: caseType.id,
+            name: caseType.name
+          };
+
+          if (caseType.states) {
+            mappedCaseType.states = caseType.states.map((state) => ({
+              id: state.id,
+              name: state.name
+            }));
+          }
+
+          return mappedCaseType;
+        });
+      }
+
+      return result;
+    });
+
+  req.session.jurisdictions = filteredJurisdictions;
   return req.session.jurisdictions;
 };
 

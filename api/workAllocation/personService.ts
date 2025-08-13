9bcdebdca0340abd55be6b0a4593e75f4eaad008
RiteshHMCTS <ritesh.dsouza@hmcts.net>
2025-08-13 14:59:39 +0100

    test

--- api/workAllocation/personService.ts
+++ api/workAllocation/personService.ts
@@ -16,6 +16,7 @@ const JUDICIAL_REF_URL = getConfigValue(SERVICES_CASE_JUDICIAL_REF_PATH);
 
 // judicial person search
 export async function postFindPersonSearch(req: EnhancedRequest, res: Response) {
+  trackTrace(`In postFindPersonSearch`);
   if (!req.body || !req.body.searchOptions || !req.body.searchOptions.searchTerm) {
     res.status(400);
     res.send('searchOptions body missing. searchTerm is missing');
@@ -27,6 +28,7 @@ export async function postFindPersonSearch(req: EnhancedRequest, res: Response) 
   const serviceCodes: string[] = [];
   const serviceRefDataMapping = getServiceRefDataMappingList();
   // add the service references in order to search by service
+  trackTrace(`In postFindPersonSearch domain: ${domain}`);
   serviceRefDataMapping.forEach((serviceRef) => {
     if (services.includes(serviceRef.service)) {
       serviceRef.serviceCodes.forEach((serviceRefserviceCodes) => {

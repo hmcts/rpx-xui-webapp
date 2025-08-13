687cf6da4f9a51865c3800cf14db5ab0e6a70d43
Josh-HMCTS <128602796+Josh-HMCTS@users.noreply.github.com>
2025-08-13 11:30:44 +0100

    Revert "EXUI-2079 - Passport upgrade (#4532)" (#4584)
    This reverts commit 237c5a5d8226fb4ef88848ebaeeb0085d11597d5.

--- api/local.ts
+++ api/local.ts
@@ -1,7 +1,7 @@
 /**
  * Local.ts used to run the application locally.
  */
-import { app } from './application';
+import { createApp } from './application';
 import { applicationConfiguration } from './configuration/appConfig';
 import { appInsights } from './lib/appInsights';
 import errorHandler from './lib/error.handler';
@@ -9,9 +9,12 @@ import errorHandler from './lib/error.handler';
 /**
  * Show the developer the application configuration when they are developing locally.
  */
-console.log(applicationConfiguration());
+createApp()
+  .then((app) => {
+    console.log(applicationConfiguration());
 
-app.use(appInsights);
-app.use(errorHandler);
+    app.use(appInsights);
+    app.use(errorHandler);
 
-app.listen(3001, () => console.log('Dev server listening on port 3001!'));
+    app.listen(3001, () => console.log('Dev server listening on port 3001!'));
+  });

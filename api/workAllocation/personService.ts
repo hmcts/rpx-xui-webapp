181d1eba632e9382af01dcb324f89daac48b510e
Josh-HMCTS <128602796+Josh-HMCTS@users.noreply.github.com>
2025-08-21 13:27:58 +0100

    use pr-release mv and tk (#4575)
    * use pr-release mv and tk
    
    * CVE
    
    * update tk and mv pre-release versions
    
    * Use new tk and mv pre-rel vers
    
    * use latest mv, tk pre-releases
    
    * toolkit version updated
    
    * removed unwanted tracktrace change
    
    * updated library versions
    
    ---------
    
    Co-authored-by: RiteshHMCTS <74713687+RiteshHMCTS@users.noreply.github.com>
    Co-authored-by: RiteshHMCTS <ritesh.dsouza@hmcts.net>
    Co-authored-by: Chris Jones <christopher.jones@hmcts.net>

--- api/workAllocation/personService.ts
+++ api/workAllocation/personService.ts
@@ -8,7 +8,6 @@ import { getServiceRefDataMappingList } from '../ref-data/ref-data-utils';
 import { PERSON } from './constants/mock.data';
 import { PersonRole } from './interfaces/person';
 import { applySearchFilter } from './util';
-import { trackTrace } from '../lib/appInsights';
 
 const HEADER_ACCEPT_V1 = 'application/json';
 const HEADER_ACCEPT_V2 = 'application/vnd.jrd.api+json;Version=2.0';
@@ -16,7 +15,6 @@ const JUDICIAL_REF_URL = getConfigValue(SERVICES_CASE_JUDICIAL_REF_PATH);
 
 // judicial person search
 export async function postFindPersonSearch(req: EnhancedRequest, res: Response) {
-  trackTrace('In postFindPersonSearch');
   if (!req.body || !req.body.searchOptions || !req.body.searchOptions.searchTerm) {
     res.status(400);
     res.send('searchOptions body missing. searchTerm is missing');
@@ -28,7 +26,6 @@ export async function postFindPersonSearch(req: EnhancedRequest, res: Response) 
   const serviceCodes: string[] = [];
   const serviceRefDataMapping = getServiceRefDataMappingList();
   // add the service references in order to search by service
-  trackTrace(`In postFindPersonSearch domain: ${domain}`);
   serviceRefDataMapping.forEach((serviceRef) => {
     if (services.includes(serviceRef.service)) {
       serviceRef.serviceCodes.forEach((serviceRefserviceCodes) => {
@@ -46,7 +43,6 @@ export async function postFindPersonSearch(req: EnhancedRequest, res: Response) 
           : HEADER_ACCEPT_V1;
         const headers = setHeaders(req);
         const body = { searchString, serviceCode };
-        trackTrace(`postFindPersonSearch headers: ${JSON.stringify(headers)}`);
         const response = await http.post(`${JUDICIAL_REF_URL}/refdata/judicial/users/search`, body, { headers });
         searchResult = response.data ? [...response.data, ...searchResult] : searchResult;
       }

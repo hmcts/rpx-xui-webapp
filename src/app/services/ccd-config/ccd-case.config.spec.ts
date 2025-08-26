1474846799df1437f8b12168abf69ed34b83f688
Olu <142989683+olusegz07@users.noreply.github.com>
2025-08-26 12:01:30 +0100

    toolkit update, (#4526)
    * toolkit update,
    
    * code tidy
    
    * Update ccd-case.config.spec.ts
    
    * Update index.spec.ts
    
    * Update query-management-container.component.ts
    
    * Update index.spec.ts
    
    * toolkit update
    
    * Update specific-access-duration.component.spec.ts
    
    * Update ccd-case.config.spec.ts
    
    * toolkit update
    
    * Update yarn-audit-known-issues
    
    * fix test
    
    * toolkit update
    
    * updated ccd-case-ui-toolkit version number
    
    * updated yarn-audit-known-issues
    
    ---------
    
    Co-authored-by: Chris Jones <christopher.jones@hmcts.net>

--- src/app/services/ccd-config/ccd-case.config.spec.ts
+++ src/app/services/ccd-config/ccd-case.config.spec.ts
@@ -58,7 +58,8 @@ class MockConfigService {
       wa_service_config: { test: 'config' },
       events_to_hide: [
         'queryManagementRespondQuery'
-      ]
+      ],
+      enable_service_specific_multi_followups: ['SERVICE_1']
     };
   }
 
@@ -229,6 +230,10 @@ describe('AppConfiguration', () => {
     tick(5000);
     expect(service.initialisationComplete).toBeTruthy();
   })));
+
+  it('should return enable_service_specific_multi_followups list', inject([AppConfig], (service: AppConfig) => {
+    expect(service.getEnableServiceSpecificMultiFollowups()).toEqual(['foo']);
+  }));
 
   describe('getCaseDataUrl', () => {
     it('should return case data url', inject([AppConfig], (service: AppConfig) => {

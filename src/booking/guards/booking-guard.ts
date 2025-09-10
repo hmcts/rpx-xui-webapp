6921bb895c0c0d8ba64179fab5bbd1b110b82678
Olu <142989683+olusegz07@users.noreply.github.com>
2025-09-10 13:23:09 +0100

    Update booking-guard.ts

--- src/booking/guards/booking-guard.ts
+++ src/booking/guards/booking-guard.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, UrlTree } from '@angular/router';
 import { RoleCategory } from '@hmcts/rpx-xui-common-lib';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { filter, map, take, tap } from 'rxjs/operators';
 import { UserDetails } from '../../app/models';
 import * as fromActions from '../../app/store';
 
@@ -19,17 +19,14 @@ export class BookingGuard {
     return userInfo.roleCategory === RoleCategory.JUDICIAL && roleAssignmentInfo.some((roleAssignment) => 'bookable' in roleAssignment && (roleAssignment.bookable === true || roleAssignment.bookable === 'true'));
   }
 
-  public canActivate(): Observable<boolean> {
-    const userDetails$: Observable<UserDetails> = this.store.pipe(select(fromActions.getUserDetails));
-    const userAccess$ = userDetails$;
-    return userAccess$.pipe(map((userDetails) => {
-      // note: in order to enable booking url for guarded users just set return true for testing purposes
-      // return true;
-      return this.hasAccess(userDetails);
-    })).pipe(tap((hasAccesss) => {
-      if (!hasAccesss) {
-        this.router.navigate([BookingGuard.defaultUrl]);
-      }
-    }));
-  }
+public canActivate(): Observable<boolean | UrlTree> {
+  return this.store.pipe(
+    select(fromActions.getUserDetails),
+    // wait until userDetails (and userInfo) are present
+    filter((u): u is UserDetails => !!u && !!u.userInfo),
+    take(1),
+    map((u) => this.hasAccess(u) ? true : this.router.createUrlTree([BookingGuard.defaultUrl]))
+  );
+}
+
 }

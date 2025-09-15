fcc665f42a53eb5b989945d7a85324a4fb919faa
Olu <142989683+olusegz07@users.noreply.github.com>
2025-09-15 15:00:29 +0100

    Update noc.routes.ts

--- src/noc/noc.routes.ts
+++ src/noc/noc.routes.ts
@@ -1,6 +1,5 @@
 import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FeatureToggleGuard } from '@hmcts/rpx-xui-common-lib';
 import { NocHomeComponent } from './containers/noc-home/noc-home.component';
 
 export const ROUTES: Routes = [

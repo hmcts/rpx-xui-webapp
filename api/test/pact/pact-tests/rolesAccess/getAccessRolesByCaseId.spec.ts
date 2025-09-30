021587bc90b4fa3a3ad3e26f74c78ccd66c82195
Kasi Subramaniam <singlekasi@gmail.com>
2025-09-30 21:59:33 +0100

    Fixed getAccessRolesByCaseId pact to publish to local pact broker on docker.

--- api/test/pact/pact-tests/rolesAccess/getAccessRolesByCaseId.spec.ts
+++ api/test/pact/pact-tests/rolesAccess/getAccessRolesByCaseId.spec.ts
@@ -7,15 +7,14 @@ const { Matchers } = require('@pact-foundation/pact');
 const { somethingLike } = Matchers;
 
 const pactSetUp = new PactV3TestSetup({ provider: 'am_roleAssignment_queryAssignment', port: 8000 });
+const RAS_V2_HEADER = 'application/vnd.uk.gov.hmcts.role-assignment-service.post-assignment-query-request+json;charset=UTF-8;version=2.0';
 
-const caseId = '12345';
-
-xdescribe('getAccessRolesByCaseId - access management service, query role assignments', () => {
+describe('getAccessRolesByCaseId - access management service, query role assignments', () => {
   const REQUEST_BODY = {
-    queryRequests: [
+    'queryRequests': [
       {
-        attributes: {
-          caseId: [somethingLike('12345')]
+        'attributes': {
+          'caseId': ['1546883526751282']
         }
       }
     ]
@@ -44,7 +43,7 @@ xdescribe('getAccessRolesByCaseId - access management service, query role assign
     ]
   };
 
-  describe('post /am/role-assignments/query', () => {
+  describe('POST  /am/role-assignments/query', () => {
     before(async () => {
       const interaction = {
         states: [{ description: 'A list of role assignments for the search query' }],
@@ -55,14 +54,14 @@ xdescribe('getAccessRolesByCaseId - access management service, query role assign
           headers: {
             'Authorization': 'Bearer someAuthorizationToken',
             'ServiceAuthorization': 'Bearer someServiceAuthorizationToken',
-            'Content-Type': 'application/vnd.uk.gov.hmcts.role-assignment-service.post-assignment-query-request+json;charset=UTF-8;version=2.0'
+            'content-type': RAS_V2_HEADER
           },
           body: REQUEST_BODY
         },
         willRespondWith: {
           status: 200,
           headers: {
-            'Content-Type': 'application/vnd.uk.gov.hmcts.role-assignment-service.post-assignment-query-request+json;charset=UTF-8;version=2.0'
+            'content-type': RAS_V2_HEADER
           },
           body: RESPONSE_BODY
         }
@@ -77,35 +76,35 @@ xdescribe('getAccessRolesByCaseId - access management service, query role assign
         const payload = {
           queryRequests: [
             {
-              attributes: {
-                caseId: '12345'
+              'attributes': {
+                'caseId': ['1546883526751282']
               }
             }
           ]
         };
         const response = await accessRolesByCaseId(path, payload);
-        //console.log(' response .headers for getAccessRolesByCaseID', response.headers);
-        const accessRolesResponse = <AccessRolesDto[]> response.data;
+        const accessRolesResponse = <AccessRolesDto> response.data;
+        console.log('....accessLogResponseReceived is \n \n ', accessRolesResponse);
         assertResponses(accessRolesResponse);
       });
     });
   });
 });
 
-function assertResponses(dto: any) {
+function assertResponses(dto: AccessRolesDto) {
   console.log(dto);
-  expect(dto.length).to.be.equal(1);
-  expect(dto[0].actions[0].id).to.be.equal('reallocate');
-  expect(dto[0].actions[0].title).to.be.equal('Reallocate');
-  expect(dto[0].actorId).to.be.equal('271ebdd4-f757-492d-b57f-101b8e47c90e');
-  expect(dto[0].end).to.be.equal(null);
-  expect(dto[0].id).to.be.equal('b83acc2f-6720-4cf9-a1f8-52367c35963d');
-  expect(dto[0].roleId).to.be.equal(null);
-  expect(dto[0].location).to.be.equal(null);
-  expect(dto[0].roleCategory).to.be.equal('LEGAL_OPERATIONS');
-  expect(dto[0].roleName).to.be.equal('case-manager');
-  expect(dto[0].start).to.be.equal('2022-09-15T23:00:00Z');
-  expect(dto[0].created).to.be.equal('2022-09-16T13:06:44.295367Z');
-  expect(dto[0].notes).to.be.equal('No reason for case access given');
-  expect(dto[0].requestedRole).to.be.equal(null);
+  expect(dto.roleAssignmentResponse[0].id).to.be.equal('b83acc2f-6720-4cf9-a1f8-52367c35963d');
+  expect(dto.roleAssignmentResponse[0].actorIdType).to.be.equal('IDAM');
+  expect(dto.roleAssignmentResponse[0].actorId).to.be.equal('271ebdd4-f757-492d-b57f-101b8e47c90e');
+  expect(dto.roleAssignmentResponse[0].roleType).to.be.equal('CASE');
+  expect(dto.roleAssignmentResponse[0].roleName).to.be.equal('case-manager');
+  expect(dto.roleAssignmentResponse[0].classification).to.be.equal('PUBLIC');
+  expect(dto.roleAssignmentResponse[0].grantType).to.be.equal('SPECIFIC');
+  expect(dto.roleAssignmentResponse[0].beginTime).to.be.equal('2022-09-15T23:00:00Z');
+  expect(dto.roleAssignmentResponse[0].readOnly).to.be.equal(false);
+  expect(dto.roleAssignmentResponse[0].created).to.be.equal('2022-09-16T13:06:44.295367Z');
+  expect(dto.roleAssignmentResponse[0].attributes.substantive).to.be.equal('Y');
+  expect(dto.roleAssignmentResponse[0].attributes.caseId).to.be.equal('1546883526751282');
+  expect(dto.roleAssignmentResponse[0].attributes.jurisdiction).to.be.equal('IA');
+  expect(dto.roleAssignmentResponse[0].attributes.caseType).to.be.equal('Asylum');
 }

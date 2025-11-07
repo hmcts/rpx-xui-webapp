12f4842ebf90f7d184fdf81010df5df2d8e5c280
danH-test <dan.hitchcock@hmcts.net>
2025-11-07 11:18:10 +0000

    Update existing login scripts structure

--- playwright_tests/E2E/steps/login-steps.ts
+++ playwright_tests/E2E/steps/login-steps.ts
@@ -5,15 +5,15 @@ export async function signIn(page: any, userIdentifier: string, goToCaseBaseURL:
   const matchingUsers = testConfig.users[testConfig.testEnv].filter(
     (user: any) => user.userIdentifier === userIdentifier
   );
-  const email = matchingUsers[0].email;
+
   if (goToCaseBaseURL) await page.goto(config.CaseBaseURL);
   else await page.goto(config.AppBaseURL);  
   await page.getByLabel("Email address").click();
-  await page.getByLabel("Email address").fill(email);
+  await page.getByLabel("Email address").fill(matchingUsers[0].email);
   await page.getByLabel("Password").click();
   await page.getByLabel("Password").fill(matchingUsers[0].key);
   await page.getByRole("button", { name: "Sign in" }).click();
-  console.log("Signed in as " + email);
+  console.log("Signed in as " + matchingUsers[0].email);
   
 }
 

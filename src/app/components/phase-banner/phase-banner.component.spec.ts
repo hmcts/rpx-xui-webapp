73a42ab798ad6a48b8ed4c893075a97718798bf8
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2025-11-14 13:26:21 +0000

    Slight edition

--- src/app/components/phase-banner/phase-banner.component.spec.ts
+++ src/app/components/phase-banner/phase-banner.component.spec.ts
@@ -2,9 +2,9 @@ import { NO_ERRORS_SCHEMA, Pipe, PipeTransform } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { RpxLanguage, RpxTranslationService } from 'rpx-xui-translation';
+import { BehaviorSubject } from 'rxjs';
 import { SessionStorageService } from './../../../app/services';
 import { PhaseBannerComponent } from './phase-banner.component';
-import { BehaviorSubject } from 'rxjs';
 
 const mockSessionStorageService = {
   getItem: jasmine.createSpy('getItem').and.returnValue(JSON.parse('false')),

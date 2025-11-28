824335e728899d9b0962ab6c89d41f957453eafe
danH-test <dan.hitchcock@hmcts.net>
2025-11-28 16:40:36 +0000

    updating imports

--- playwright_tests_new/E2E/page-objects/pages/exui/caseList.po.ts
+++ playwright_tests_new/E2E/page-objects/pages/exui/caseList.po.ts
@@ -1,6 +1,6 @@
 import { Page } from "@playwright/test";
-import config from "../../../utils/config.utils.js";
-import { Base } from "../../base.js";
+import config from "../../../utils/config.utils";
+import { Base } from "../../base";
 
 export class CaseListPage extends Base {
   readonly container = this.page.locator("exui-case-home");
@@ -8,10 +8,14 @@ export class CaseListPage extends Base {
   readonly caseTypeSelect = this.page.locator("#wb-case-type")
   readonly textField0Input = this.page.locator("#TextField0")
   readonly caseListResultsAmount = this.page.locator("#search-result .pagination-top");
+  readonly caseSearchResultsMessage = this.page.locator('#search-result')
+  readonly pagination = this.page.locator('.ngx-pagination');
 
   constructor(page: Page) {
     super(page);
-  }
+  } 
+  
+
   public async searchByJurisdiction(jurisdiction: string): Promise<void> {
     await this.jurisdictionSelect.selectOption(jurisdiction);
     await this.exuiCaseListComponent.filters.applyFilterBtn.click();
@@ -33,5 +37,10 @@ export class CaseListPage extends Base {
   async goto() {
     await this.page.goto(config.urls.manageCaseBaseUrl);
     await this.exuiHeader.checkIsVisible();
+  }
+
+  async getPaginationFinalItem(): Promise<string | undefined> {
+    const items = (await this.pagination.locator('li').allTextContents()).map(i => i.trim());
+    return items.length > 0 ? items[items.length - 1] : undefined;
   }
 }

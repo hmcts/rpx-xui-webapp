d5a5258cc14dbcb23135436730b3cf973bd48a1f
Shamrez <shamrez.darwish@hmcts.net>
2026-01-02 15:29:23 +0000

    Review comments Addressed

--- playwright_tests_new/E2E/test/updateCase/updateCase.spec.ts
+++ playwright_tests_new/E2E/test/updateCase/updateCase.spec.ts
@@ -14,19 +14,16 @@ test.describe("Verify creating and updating a case works as expected", () => {
     caseListPage,
     caseDetailsPage,
     page,
-    
 
   }) => {
     let caseNumber: string;
     const textField0 = faker.lorem.word();
-    // Create Case
     await test.step("Create a case and validate the case number", async () => {
       await createCasePage.createDivorceCase("DIVORCE", "XUI Case PoC", textField0);
       await expect(createCasePage.exuiCaseDetailsComponent.caseHeader).toBeVisible();
       caseNumber = await createCasePage.exuiCaseDetailsComponent.caseHeader.innerText();
       validatorUtils.validateDivorceCaseNumber(caseNumber);
     });
-    //Find and open the case
     await test.step("Find the created case in the case list", async () => {
       await caseListPage.goto();
       await caseListPage.searchByJurisdiction("Family Divorce");
@@ -39,15 +36,16 @@ test.describe("Verify creating and updating a case works as expected", () => {
       const cleanedCaseNumber = caseNumber.replace(/^#/, "");
       await caseListPage.openCaseByReference(cleanedCaseNumber);
     });
-    //Start Update Case event
+
     await test.step("Start Update Case event", async () => {
       await page.waitForSelector('#next-step')
-      await page.getByLabel("Next step").selectOption("3: Object");
-      await page.waitForTimeout(2000)
-      await page.getByRole("button", { name: "Go" }).click();
+      page.getByLabel("Next step").selectOption({ label: 'Update case' })
+      await expect(page.getByLabel('Next step').locator('option:checked')).toHaveText('Update case');
+      await Promise.all([
+      page.getByRole('button', { name: 'Go' }).click(),
+      ]);
 
     });
-    // Update Case
     await test.step("Update case fields", async () => {
       await page
         .locator("#Person2_FirstName")
@@ -57,13 +55,12 @@ test.describe("Verify creating and updating a case works as expected", () => {
         .fill("test street");
       await page.getByRole("button", { name: "Continue" }).click();
       await page.getByRole("button", { name: "Submit" }).click();
+      
+    });
+    await test.step("Verify update success banner", async () => {
+    await expect(page.getByText(/Case #[\d-]+ has/i)).toBeVisible();
 
     });
-    // Verify update success banner
-    await test.step("Verify update success banner", async () => {
-      await expect(page.getByText(/Case #[\d-]+ has/i)).toBeVisible();
-    });
-    // Check History Tab
     await test.step("Verify update event appears in history", async () => {
       await caseDetailsPage.openHistoryTab();
       const eventEntries = page.locator('ccd-event-log-details span.text-16', { hasText: 'Update case' });

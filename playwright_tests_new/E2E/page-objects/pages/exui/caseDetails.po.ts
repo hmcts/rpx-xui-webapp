73ef5d15ef515f8498488d71a7d7cf803a53db5d
Shamrez <shamrez.darwish@hmcts.net>
2026-01-09 11:18:13 +0000

    Rebasing with master

--- playwright_tests_new/E2E/page-objects/pages/exui/caseDetails.po.ts
+++ playwright_tests_new/E2E/page-objects/pages/exui/caseDetails.po.ts
@@ -1,6 +1,7 @@
 import { Page } from "@playwright/test";
 import { Base } from "../../base";
 
+
 export class CaseDetailsPage extends Base {
 
 
@@ -11,36 +12,40 @@ export class CaseDetailsPage extends Base {
   readonly caseActionGoButton = this.page.locator('.event-trigger button');
   readonly submitCaseFlagButton = this.page.locator('.button[type="submit"]');
   readonly caseAlertMessage = this.page.locator('.hmcts-banner--success .alert-message');
-  readonly person2Group = this.page.getByRole('group', { name: /Person 2 - not retained/i });
+  //readonly person2Group = this.page.getByRole('group', { name: /Person 2 - not retained/i });
   readonly person2FirstName = this.page.getByRole('group', { name: 'Person 2 - retained (Optional)' }).getByLabel('First Name (Optional)');
-  readonly person2LastName= this.page.getByRole('group', { name: 'Person 2 - retained (Optional)' }).getByLabel('Last Name (Optional)');
+  readonly person2LastName = this.page.getByRole('group', { name: 'Person 2 - retained (Optional)' }).getByLabel('Last Name (Optional)');
+  readonly continueButton = this.page.getByRole("button", { name: "Continue" });
+  readonly submitButton = this.page.getByRole("button", { name: "Submit" });
+  readonly eventTable = this.page.locator("EventLogTable");
+  readonly historyButton = this.page.getByRole("tab", { name: "History" });
+
   constructor(page: Page) {
     super(page);
   }
   async selectCaseAction(action: string) {
+    await this.caseActionGoButton.waitFor()
     await this.caseActionsDropdown.selectOption(action);
     await this.caseActionGoButton.click();
   }
-    async startUpdateCase(action: string) {
-    await this.caseActionsDropdown.waitFor();
-    await this.caseActionsDropdown.selectOption(action);
-    await this.caseActionGoButton.click();
+  async startUpdateCase(person2FirstName: string, person2LastName: string) {
+    await this.person2FirstName.fill(person2FirstName)
+    await this.person2LastName.fill(person2LastName)
+    await this.continueButton.click()
+    await this.submitButton.click()
   }
   async selectCaseDetailsTab(tabName: string) {
     await this.caseDetailsTabs.filter({ hasText: tabName }).click()
   }
-  async fillPerson2FirstName(first: string) {
-    await this.person2FirstName.fill(first);
-  }
-  async fillPerson2LastName(last: string) {
-    await this.person2LastName.fill(last);
-  }
-  async openHistoryTab() {                   
-    await this.page.getByRole("tab", { name: "History" }).click();
+
+  async validateUpdateEventHistory() {
+    await this.historyButton.click();
     await this.exuiSpinnerComponent.wait();
-}
+    await expect(this.eventTable.first().textContent()).toContain('Update case')
+    await this.eventTable.first().isVisible();
+  }
 
 }
-  
-  
+
+
 
--- playwright_tests_new/E2E/test/updateCase/updateCase.spec.ts
+++ playwright_tests_new/E2E/test/updateCase/updateCase.spec.ts
@@ -1,5 +1,6 @@
 import { faker } from "@faker-js/faker";
 import { expect, test } from "../../fixtures";
+import { executablePath } from "puppeteer";
 
 
 test.describe("Verify creating and updating a case works as expected", () => {
@@ -9,11 +10,8 @@ test.describe("Verify creating and updating a case works as expected", () => {
     await idamPage.login({ username: email, password });
   });
   test("Create, update and verify case history", async ({
-    validatorUtils,
     createCasePage,
-    caseListPage,
     caseDetailsPage,
-    page,
 
   }) => {
     let caseNumber: string;
@@ -22,49 +20,38 @@ test.describe("Verify creating and updating a case works as expected", () => {
       await createCasePage.createDivorceCase("DIVORCE", "XUI Case PoC", textField0);
       await expect(createCasePage.exuiCaseDetailsComponent.caseHeader).toBeVisible();
       caseNumber = await createCasePage.exuiCaseDetailsComponent.caseHeader.innerText();
-      validatorUtils.validateDivorceCaseNumber(caseNumber);
+    
     });
-    await test.step("Find the created case in the case list", async () => {
+    /*await test.step("Find the created case in the case list", async () => {
       await caseListPage.goto();
       await caseListPage.searchByJurisdiction("Family Divorce");
       await caseListPage.searchByCaseType("XUI Case PoC");
       await caseListPage.searchByTextField0(textField0);
       await caseListPage.exuiCaseListComponent.searchByCaseState("Case created");
       await caseListPage.applyFilters();
-    });
-    await test.step("Open the created case", async () => {
-      const cleanedCaseNumber = caseNumber.replace(/^#/, "");
-      await caseListPage.openCaseByReference(cleanedCaseNumber);
-    });
+      */
+
+    // });
+    // await test.step("Open the created case", async () => {
+    //   const cleanedCaseNumber = caseNumber.replace(/^#/, "");
+    //   await caseListPage.openCaseByReference(cleanedCaseNumber);
+    // });
 
     await test.step("Start Update Case event", async () => {
-      await page.waitForSelector('#next-step')
-      page.getByLabel("Next step").selectOption({ label: 'Update case' })
-      await expect(page.getByLabel('Next step').locator('option:checked')).toHaveText('Update case');
-      await Promise.all([
-      page.getByRole('button', { name: 'Go' }).click(),
-      ]);
+      await caseDetailsPage.selectCaseAction('Update case')
 
     });
     await test.step("Update case fields", async () => {
-      await page
-        .locator("#Person2_FirstName")
-        .fill("test");
-      await page
-        .locator("#Person2_LastName")
-        .fill("test street");
-      await page.getByRole("button", { name: "Continue" }).click();
-      await page.getByRole("button", { name: "Submit" }).click();
-      
+      await caseDetailsPage.startUpdateCase('test', 'test street');
     });
+
     await test.step("Verify update success banner", async () => {
-    await expect(page.getByText(/Case #[\d-]+ has/i)).toBeVisible();
+    expect(caseDetailsPage.caseAlertMessage)
+    await this.caseAlertMessage.isVisible()
 
     });
     await test.step("Verify update event appears in history", async () => {
-      await caseDetailsPage.openHistoryTab();
-      const eventEntries = page.locator('ccd-event-log-details span.text-16', { hasText: 'Update case' });
-      await expect(eventEntries.first()).toBeVisible();
+      await caseDetailsPage.validateUpdateEventHistory();
 
     });
   });

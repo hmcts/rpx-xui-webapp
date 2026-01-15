1fc41499696b03aedfec3b249a4bc6bce2c856bd
Antony Leons <antony@leons.dev>
2026-01-15 16:34:22 +0000

    Enhance data handling in Welsh report runbook by forcing enumeration of results and adding type checks for better debugging

--- infrastructure/automation.tf
+++ infrastructure/automation.tf
@@ -291,7 +291,7 @@ resource "azurerm_automation_schedule" "welsh_monthly_schedule" {
   frequency               = "Month"
   interval                = 1
   # Run 5 minutes from now for testing
-  start_time              = formatdate("YYYY-MM-16'T'16:32:00Z", timestamp())
+  start_time              = formatdate("YYYY-MM-16'T'16:40:00Z", timestamp())
   timezone                = "Etc/UTC"
 }
 

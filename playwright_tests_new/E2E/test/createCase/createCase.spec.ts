39f5d92814a31829aaf04ca28ce28ee60fc0ff54
danH-test <dan.hitchcock@hmcts.net>
2026-01-15 18:29:43 +0000

    Update create case expects and test function

--- playwright_tests_new/E2E/test/createCase/createCase.spec.ts
+++ playwright_tests_new/E2E/test/createCase/createCase.spec.ts
@@ -2,6 +2,8 @@ import { faker } from "@faker-js/faker";
 import { expect, test } from "../../fixtures";
 import { loadSessionCookies } from '../../../common/sessionCapture';
 let sessionCookies: any[] = [];
+const jurisdiction = 'DIVORCE';
+const caseType = 'XUI Case PoC';
 
 test.describe("Verify creating cases works as expected", () => {
     test.beforeEach(async ({ page }) => {
@@ -13,15 +15,16 @@ test.describe("Verify creating cases works as expected", () => {
         await page.goto('/');
     });
 
-    test("Verify creating a case in the divorce jurisdiction works as expected", async ({ validatorUtils, createCasePage, caseDetailsPage, caseListPage, tableUtils }) => {
+    test("Verify creating a case in the divorce jurisdiction works as expected", async ({ page,validatorUtils, createCasePage, caseDetailsPage, caseListPage, tableUtils }) => {
         let caseNumber: string;
         let textField0 = faker.lorem.word();
 
-        await test.step("Create a case and validate the case number", async () => {
-            await createCasePage.createDivorceCase("DIVORCE", "XUI Case PoC", textField0);
+        await test.step("Create a case and validate the case details", async () => {
+            await createCasePage.createDivorceCase(jurisdiction, caseType, textField0);
             expect.soft(createCasePage.exuiCaseDetailsComponent.caseHeader).toBeInViewport();
             caseNumber = await caseDetailsPage.getCaseNumberFromAlert();
             expect(caseNumber).toMatch(validatorUtils.DIVORCE_CASE_NUMBER_REGEX);
+            expect(page.url()).toContain(`/${jurisdiction}/${caseType}/`);
         });
 
         await test.step("Find the created case in the case list", async () => {

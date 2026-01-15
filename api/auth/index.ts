b07ce0afd53ad381d96e2b749ae6acdaf7dcee6c
Andrew Grizhenkov <andrew.grizhenkov@hmcts.net>
2026-01-15 15:00:48 +0000

    EXUI-3788 update to latest playwright-common library

--- api/auth/index.ts
+++ api/auth/index.ts
@@ -41,11 +41,12 @@ export const successCallback = (req: EnhancedRequest, res: Response, next: NextF
   const { accessToken } = user.tokenset;
   const cookieToken = getConfigValue(COOKIES_TOKEN);
   const cookieUserId = getConfigValue(COOKIES_USER_ID);
+  const secureCookieEnabled = showFeature(FEATURE_SECURE_COOKIE_ENABLED);
 
   logger.info('Setting session and cookies');
 
-  res.cookie(cookieUserId, userinfo.uid, { sameSite: 'strict' });
-  res.cookie(cookieToken, accessToken, { sameSite: 'strict' });
+  res.cookie(cookieUserId, userinfo.uid, { sameSite: 'strict', secure: secureCookieEnabled });
+  res.cookie(cookieToken, accessToken, { sameSite: 'strict', secure: secureCookieEnabled });
 
   if (!req.isRefresh) {
     return res.redirect('/');

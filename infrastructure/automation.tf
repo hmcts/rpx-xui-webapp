c8ea8046507469abf30a03610b46714a4165b7d5
Antony Leons <antony@leons.dev>
2026-01-16 11:57:29 +0000

    Update automation.tf

--- infrastructure/automation.tf
+++ infrastructure/automation.tf
@@ -245,7 +245,7 @@ resource "azurerm_automation_schedule" "welsh_monthly_schedule" {
   frequency               = "Month"
   interval                = 1
   # Run 5 minutes from now for testing
-  start_time              = formatdate("YYYY-MM-16'T'11:52:00Z", timestamp())
+  start_time              = formatdate("YYYY-MM-16'T'12:07:00Z", timestamp())
   timezone                = "Etc/UTC"
 }
 

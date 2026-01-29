fde47317a91e72716e7d7748b34d542eae2e570b
danH-test <dan.hitchcock@hmcts.net>
2026-01-29 11:43:59 +0000

    Update tests steps

--- playwright_tests_new/E2E/test/myWork/myTasks.spec.ts
+++ playwright_tests_new/E2E/test/myWork/myTasks.spec.ts
@@ -5,38 +5,37 @@ let sessionCookies: any[] = [];
 
 test.describe("Verify my tasks works as expected", () => {
     test.beforeEach(async ({ page }) => {
-        const { cookies } = loadSessionCookies('STAFF_ADMIN');
+        const { cookies } = loadSessionCookies('CASEWORKER_GLOBALSEARCH');
         sessionCookies = cookies;
         if (sessionCookies.length) {
             await page.context().addCookies(sessionCookies);
         }
         await page.goto('/');
+        await page.waitForResponse(res =>
+            res.url().includes('/workallocation/task') && res.ok()
+        );
     });
-
 
     test("Verify tasks", async ({ taskListPage, tableUtils, page }) => {
 
         await test.step("Navigate to the task list page", async () => {
-
-           // await taskListPage.applyAllFilterOptions();
-
-            
             await expect(taskListPage.taskListTable).toBeVisible();
             await taskListPage.exuiSpinnerComponent.wait();
         });
 
         await test.step("Check my available tasks has data in the table", async () => {
-            const table = await tableUtils.mapExuiTable( taskListPage.taskListTable);
-            expect(table.length).toBeGreaterThan(0);            
+            const table = await tableUtils.mapExuiTable(taskListPage.taskListTable);
+            expect(table.length).toBeGreaterThan(0);
         });
 
-         await test.step("Verify task details view", async () => {
+        await test.step("Verify task details view", async () => {
             await taskListPage.manageCaseButtons.nth(0).click();
-            const table = await tableUtils.mapExuiTable( taskListPage.taskListTable);
+            const table = await tableUtils.mapExuiTable(taskListPage.taskListTable);
             console.log(table);
-            page.getByLabel('Go to task').click();
+
+            await page.getByLabel('Go to task').click();
             await taskListPage.exuiSpinnerComponent.wait();
-         });
+        });
 
     });
 });

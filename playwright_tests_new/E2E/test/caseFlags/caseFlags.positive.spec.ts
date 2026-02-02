9421344ee2829b37634f1102ebdc53d63ce98708
Josh <josh.glasgow@hmcts.net>
2026-02-02 19:08:19 +0000

    Fix test merge

--- playwright_tests_new/E2E/test/caseFlags/caseFlags.positive.spec.ts
+++ playwright_tests_new/E2E/test/caseFlags/caseFlags.positive.spec.ts
@@ -26,62 +26,10 @@ test.describe('Case level case flags', () => {
       expect.soft(table[0]).toMatchObject({});
     });
 
-    test('Create a new case level flag and verify the flag is displayed on the case', async ({ caseDetailsPage, tableUtils }) => {
-      await test.step('Check there are no flags already present', async () => {
-        await caseDetailsPage.selectCaseDetailsTab('Flags');
-        const table = await tableUtils.mapExuiTable(await caseDetailsPage.getTableByName('Case level flags'));
-        expect.soft(table[0]).toMatchObject({});
-      });
-
-      await test.step('Create a new case level flag', async () => {
-        await caseDetailsPage.exuiSpinnerComponent.wait();
-        await caseDetailsPage.selectCaseAction('Create a case flag');
-        await caseDetailsPage.selectCaseFlagTarget('Welsh');
-      });
-
-      await test.step('Check the case flag creation messages are seen', async () => {
-        expect
-          .soft(await caseDetailsPage.caseAlertSuccessMessage.innerText())
-          .toContain(`Case ${caseNumber} has been updated with event: Create a case flag`);
-        expect.soft(await caseDetailsPage.caseNotificationBannerTitle.innerText()).toContain('Important');
-        expect
-          .soft(await caseDetailsPage.caseNotificationBannerBody.innerText())
-          .toContain('There is 1 active flag on this case.');
-      });
-
-      await test.step('Verify the case level flag is shown in the flags tab', async () => {
-        await caseDetailsPage.selectCaseDetailsTab('Flags');
-        const expectedFlag = {
-          'Case flags': 'Welsh forms and communications',
-          Comments: `Welsh`,
-          'Creation date': await caseDetailsPage.todaysDateFormatted(),
-          'Last modified': '',
-          'Flag status': 'ACTIVE',
-        };
-        const table = await tableUtils.mapExuiTable(await caseDetailsPage.getTableByName('Case level flags'));
-        expect(table[0]).toMatchObject(expectedFlag);
-      });
-
-      await test.step('Verify that event details are shown on the History tab', async () => {
-        await caseDetailsPage.selectCaseDetailsTab('History');
-        const { updateRow, updateDate, updateAuthor, expectedDate } =
-          await caseDetailsPage.getUpdateCaseHistoryInfo('Create a case flag');
-
-        expect.soft(updateRow, 'Update case row should be present').toBeTruthy();
-        expect.soft(updateDate.startsWith(expectedDate), `${updateDate} should start with ${expectedDate}`).toBe(true);
-        expect.soft(updateAuthor, 'Update case author should be present').not.toBe('');
-
-        const expectedDetails = {
-          Date: updateDate,
-          Author: updateAuthor,
-          'End state': 'Submitted',
-          Event: 'Create a case flag',
-          Summary: '-',
-          Comment: '-',
-        };
-        const table = await caseDetailsPage.trRowsToObjectInPage(caseDetailsPage.historyDetailsTable);
-        expect(table).toMatchObject(expectedDetails);
-      });
+    await test.step('Create a new case level flag', async () => {
+      await caseDetailsPage.exuiSpinnerComponent.wait();
+      await caseDetailsPage.selectCaseAction('Create a case flag');
+      await caseDetailsPage.selectCaseFlagTarget('Welsh');
     });
 
     await test.step('Check the case flag creation messages are seen', async () => {
@@ -103,6 +51,27 @@ test.describe('Case level case flags', () => {
       };
       const table = await tableUtils.mapExuiTable(await caseDetailsPage.getTableByName('Case level flags'));
       expect(table[0]).toMatchObject(expectedFlag);
+    });
+
+    await test.step('Verify that event details are shown on the History tab', async () => {
+      await caseDetailsPage.selectCaseDetailsTab('History');
+      const { updateRow, updateDate, updateAuthor, expectedDate } =
+        await caseDetailsPage.getUpdateCaseHistoryInfo('Create a case flag');
+
+      expect.soft(updateRow, 'Update case row should be present').toBeTruthy();
+      expect.soft(updateDate.startsWith(expectedDate), `${updateDate} should start with ${expectedDate}`).toBe(true);
+      expect.soft(updateAuthor, 'Update case author should be present').not.toBe('');
+
+      const expectedDetails = {
+        Date: updateDate,
+        Author: updateAuthor,
+        'End state': 'Submitted',
+        Event: 'Create a case flag',
+        Summary: '-',
+        Comment: '-',
+      };
+      const table = await caseDetailsPage.trRowsToObjectInPage(caseDetailsPage.historyDetailsTable);
+      expect(table).toMatchObject(expectedDetails);
     });
   });
 });

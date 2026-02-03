eb3a13b591cd531da73c713cfa1be1157c26d0e4
Andrew Grizhenkov <andrew.grizhenkov@hmcts.net>
2026-02-03 17:02:50 +0000

    EXUI-4062 test: ensure node tests pick up repo config fixes

--- api/test/setup-config-env.ts
+++ api/test/setup-config-env.ts
@@ -1,9 +1,6 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import path = require('node:path');
 
-const currentFilePath = typeof __filename === 'string' ? __filename : fileURLToPath(import.meta.url);
-
-const currentDir = path.dirname(currentFilePath);
+const currentDir = __dirname;
 const apiRoot = path.resolve(currentDir, '..');
 const repoRoot = path.resolve(apiRoot, '..');
 const repoConfig = path.join(repoRoot, 'config');

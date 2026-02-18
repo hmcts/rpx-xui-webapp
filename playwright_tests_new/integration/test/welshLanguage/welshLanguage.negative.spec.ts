18930b766fb5aa2d0eef73441a6d12d3cef8ced0
danH-test <dan.hitchcock@hmcts.net>
2026-02-18 15:48:17 +0000

    Add a negative test case for a translation endpoint error

--- playwright_tests_new/integration/test/welshLanguage/welshLanguage.negative.spec.ts
+++ playwright_tests_new/integration/test/welshLanguage/welshLanguage.negative.spec.ts
@@ -13,12 +13,11 @@ test.describe('Verify users can switch the language', () => {
     }
     await page.goto('/');
     await page.route('**api/translation/cy*', async (route) => {
-      const body = JSON.stringify(welshTranslationsSmall);
-      await route.fulfill({ status: 200, contentType: 'application/json', body });
+      await route.fulfill({ status: 500, contentType: 'application/json', body: '{}' });
     });
   });
 
-  test('Verify translations are shown when the user selects to view the site in Welsh', async ({
+  test('Verify translations are not shown when the translation endpoint returns an error', async ({
     createCasePage,
     caseListPage,
     page,
@@ -28,35 +27,28 @@ test.describe('Verify users can switch the language', () => {
       await caseListPage.exuiSpinnerComponent.wait();
       await page.waitForLoadState('domcontentloaded');
       await caseListPage.exuiHeader.headerAppLink.waitFor({ state: 'attached' });
+      await page.waitForResponse((r) => r.url().includes('/cy') && r.status() === 500, { timeout: 5_000 });
     });
 
-    await test.step('Check the translation are shown, and the language toggle switches to English', async () => {
+    await test.step('Check the translations are not shown, but the translated banner still is', async () => {
       expect.soft(await caseListPage.exuiHeader.languageToggle.innerText()).toContain('English');
       await expect.soft(caseListPage.exuiHeader.notificationBanner).toBeVisible();
       await expect.soft(caseListPage.exuiHeader.notificationBannerTitle).toContainText('Pwysig');
       await expect
         .soft(caseListPage.exuiHeader.headerAppLink)
-        .toContainText(welshTranslationsSmall.translations['Manage Cases'].translation);
+        .not.toContainText(welshTranslationsSmall.translations['Manage Cases'].translation);
       await expect
         .soft(caseListPage.exuiHeader.signOutLink)
-        .toContainText(welshTranslationsSmall.translations['Sign out'].translation);
-      await expect.soft(caseListPage.caseListHeading).toContainText(welshTranslationsSmall.translations['Case list'].translation);
+        .not.toContainText(welshTranslationsSmall.translations['Sign out'].translation);
+      await expect
+        .soft(caseListPage.caseListHeading)
+        .not.toContainText(welshTranslationsSmall.translations['Case list'].translation);
       await expect
         .soft(createCasePage.createCaseButton)
-        .toContainText(welshTranslationsSmall.translations['Create case'].translation);
+        .not.toContainText(welshTranslationsSmall.translations['Create case'].translation);
       await expect
         .soft(caseListPage.exuiFooter.copyrightLink)
-        .toContainText(welshTranslationsSmall.translations['© Crown copyright'].translation);
-    });
-
-    await test.step('Check the language can be switched back to English and the correct translations are shown', async () => {
-      await caseListPage.exuiHeader.switchLanguage('English');
-      await caseListPage.exuiSpinnerComponent.wait();
-      expect.soft(await caseListPage.exuiHeader.header.isVisible()).toBe(true);
-      expect.soft(await caseListPage.exuiHeader.headerAppLink.innerText()).toContain('Manage Cases');
-      expect.soft(await caseListPage.exuiHeader.languageToggle.innerText()).toContain('Cymraeg');
-      expect.soft(await caseListPage.exuiHeader.signOutLink.innerText()).toContain('Sign out');
-      expect.soft(caseListPage.exuiHeader.notificationBanner).not.toBeVisible();
+        .not.toContainText(welshTranslationsSmall.translations['© Crown copyright'].translation);
     });
   });
 });

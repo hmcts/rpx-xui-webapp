e096ec57029effe500ebe09f1347059e4d748efb
danH-test <dan.hitchcock@hmcts.net>
2026-02-20 14:07:29 +0000

    Add small task table test

--- playwright_tests_new/integration/test/manageTasks/availableTaskList.positive.spec.ts
+++ playwright_tests_new/integration/test/manageTasks/availableTaskList.positive.spec.ts
@@ -6,17 +6,20 @@ import { formatUiDate } from '../../utils/tableUtils';
 
 const userIdentifier = 'STAFF_ADMIN';
 let sessionCookies: any[] = [];
-let taskListMockResponse;
 
 test.beforeEach(async ({ page }) => {
   const { cookies } = await applySessionCookies(page, userIdentifier);
   sessionCookies = cookies;
   const userId = extractUserIdFromCookies(sessionCookies);
-  taskListMockResponse = buildTaskListMock(3000, '', availableActionsList);
 });
 
 test.describe(`Available Task List as ${userIdentifier}`, () => {
-  test(`User ${userIdentifier} can view assigned tasks on the task list page`, async ({ taskListPage, page, tableUtils }) => {
+  test(`User ${userIdentifier} can view a large number of available tasks on the task list page`, async ({
+    taskListPage,
+    page,
+    tableUtils,
+  }) => {
+    const taskListMockResponse = buildTaskListMock(3000, '', availableActionsList);
     await test.step('Setup route mock for task list', async () => {
       await page.route('**/workallocation/task*', async (route) => {
         const body = JSON.stringify(taskListMockResponse);
@@ -49,6 +52,7 @@ test.describe(`Available Task List as ${userIdentifier}`, () => {
         expect.soft(table[i]['Hearing date']).toBe(formatUiDate(expectedHearingDate));
       }
     });
+
     await test.step('Verify the pagination controls behave and display as expected for large result sets', async () => {
       await expect.soft(taskListPage.paginationPreviousButton).not.toBeVisible();
       await expect.soft(taskListPage.paginationNextButton).toBeVisible();
@@ -58,6 +62,45 @@ test.describe(`Available Task List as ${userIdentifier}`, () => {
       expect.soft(await taskListPage.paginationCurrentPage.textContent()).toContain('2');
       await expect.soft(taskListPage.paginationPreviousButton).toBeVisible();
       expect.soft(await taskListPage.getResultsText()).toBe(`Showing 26 to 50 of ${taskListMockResponse.total_records} results`);
+    });
+  });
+
+  test(`User ${userIdentifier} can view a small number of assigned tasks on the task list page`, async ({
+    taskListPage,
+    page,
+    tableUtils,
+  }) => {
+    const taskListMockResponse = buildTaskListMock(3, '', availableActionsList);
+    await test.step('Setup route mock for task list', async () => {
+      await page.route('**/workallocation/task*', async (route) => {
+        const body = JSON.stringify(taskListMockResponse);
+        await route.fulfill({ status: 200, contentType: 'application/json', body });
+      });
+    });
+
+    await test.step('Navigate to the available tasks list page', async () => {
+      await taskListPage.goto();
+      await taskListPage.taskTableTabs.filter({ hasText: 'Available tasks' }).first().click();
+      await expect(taskListPage.taskListTable).toBeVisible();
+      await taskListPage.exuiSpinnerComponent.wait();
+    });
+
+    await test.step('Verify the UI shows the expected layout and data, given the mock response', async () => {
+      expect
+        .soft(await taskListPage.getResultsText())
+        .toBe(`Showing 1 to ${Math.min(taskListMockResponse.tasks.length, 25)} of ${taskListMockResponse.total_records} results`);
+      const table = await tableUtils.parseWorkAllocationTable(taskListPage.taskListTable);
+      for (let i = 0; i < table.length; i++) {
+        const expectedCaseName = taskListMockResponse.tasks[i].case_name;
+        expect.soft(table[i]['Case name']).toBe(expectedCaseName);
+        expect.soft(table[i]['Case category']).toBe(taskListMockResponse.tasks[i].case_category);
+        expect.soft(table[i]['Location']).toBe(taskListMockResponse.tasks[i].location_name);
+        expect.soft(table[i]['Task']).toBe(taskListMockResponse.tasks[i].task_title);
+        expect.soft(table[i]['Due date']).toBe(formatUiDate(taskListMockResponse.tasks[i].due_date));
+        const expectedHearingDate = taskListMockResponse.tasks[i].next_hearing_date || '';
+        expect.soft(table[i]['Hearing date']).toBe(formatUiDate(expectedHearingDate));
+      }
+      await expect(taskListPage.paginationControls).not.toBeVisible();
     });
   });
 
@@ -85,4 +128,6 @@ test.describe(`Available Task List as ${userIdentifier}`, () => {
       await expect(taskListPage.paginationControls).not.toBeVisible();
     });
   });
+
+  // CHECK THE FIILTERD VIEW SENDS THE RELEVANT API CALL
 });

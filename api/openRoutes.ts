c8ea5f53750b57853a7aac5dbcd9978ec205f5e5
Chris Jones <christopher.jones@hmcts.net>
2026-02-20 14:10:54 +0000

    tidied up code

--- api/openRoutes.ts
+++ api/openRoutes.ts
@@ -4,7 +4,7 @@ import getUiConfigRouter from './configuration/uiConfigRouter';
 const router = express.Router({ mergeParams: true });
 
 /**
- * // TODO: Consolidate duplicate config routes. Keep both for now as multiple teams use them both for performance testing.
+ * TODO: Consolidate duplicate config routes. Keep both for now as multiple teams use them both for performance testing.
  */
 router.use('/configuration-ui', getUiConfigRouter);
 router.use('/config/ui', getUiConfigRouter);

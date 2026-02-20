8933185cf6837ed89e64e6a91f0b5e9c8b7808f2
danH-test <dan.hitchcock@hmcts.net>
2026-02-20 12:49:59 +0000

    EXUI-3636 - Add case details e2e test (#4947)
    * Update the case creation code to generate input data to drive and verify inputs
    
    * Add string function for divorce case numbers
    
    * Update test functions and add Case tab checks for new case test
    
    * Already covered by update case e2e test
    
    * Already covered by new e2e search tests removing old tests
    
    * Removing old case flag test as we have a new e2e replacement
    
    * Removing test that validates a dropdown already used in new e2e tests
    
    * linting update
    
    * EXUI-3636 updated Work allocation API tests
    
    * EXUI-3636 updated Work allocation API tests
    
    * Remove search steps from the create case test as this is covered elsewhere
    
    * Update the createDivorceCasePoC
    
    * PR comments update
    
    * cve fix
    
    * Error formatting update
    
    * Expect hardening
    
    ---------
    
    Co-authored-by: Andrew Grizhenkov <andrew.grizhenkov@hmcts.net>
    Co-authored-by: Josh-HMCTS <128602796+Josh-HMCTS@users.noreply.github.com>

--- playwright_tests_new/E2E/utils/validator.utils.ts
+++ playwright_tests_new/E2E/utils/validator.utils.ts
@@ -1,4 +1,18 @@
-// Use this as a store for validation regexes, don't add expects to this file, only validation methods or constants.
+/**
+ * Format a case number by inserting dashes every 4 digits.
+ *
+ * Examples:
+ *  - "1771235201345649" -> "1771-2352-0134-5649"
+ *  - "1771 2352 0134 5649" -> "1771-2352-0134-5649"
+ *  - "1771-2352-0134-5649" -> "1771-2352-0134-5649"
+ */
+export function formatCaseNumberWithDashes(input: string): string {
+  if (!input) return input;
+  const digits = String(input).replace(/\D/g, '');
+  if (!digits) return '';
+  const groups = digits.match(/.{1,4}/g) || [];
+  return groups.join('-');
+}
 
 export class ValidatorUtils {
   public DIVORCE_CASE_NUMBER_REGEX = /#?\d{4}-?\d{4}-?\d{4}-?\d{4}/;

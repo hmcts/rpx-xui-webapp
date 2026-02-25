1eeb8fa91dc251a385fb8d4af7ca6d0ac71069ac
danH-test <dan.hitchcock@hmcts.net>
2026-02-25 15:42:48 +0000

    Update positive test

--- playwright_tests_new/integration/test/createCase/createCase.positive.spec.ts
+++ playwright_tests_new/integration/test/createCase/createCase.positive.spec.ts
@@ -1,8 +1,10 @@
-import { test } from '../../../E2E/fixtures';
+import { test, expect} from '../../../E2E/fixtures';
 import { applySessionCookies } from '../../../common/sessionCapture';
 import { TEST_USERS } from '../../testData';
 
 const userIdentifier = TEST_USERS.SOLICITOR;
+const jurisdiction = 'DIVORCE';
+const caseType = 'XUI Case PoC';
 
 test.beforeEach(async ({ page }) => {
   await applySessionCookies(page, userIdentifier);
@@ -13,6 +15,9 @@ test.describe(`Create case as ${userIdentifier}`, () => {
     await test.step('Navigate to the create case page and fill in case details', async () => {
       await caseListPage.navigateTo();
       await createCasePage.createCaseButton.click();
+      await createCasePage.createCase(jurisdiction, caseType,'');
+      await expect(createCasePage.person1Title).toBeVisible();
     });
+
   });
 });

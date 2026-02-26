915d1de452e792c67bbd7e41bbddae77dbe65f3e
Ben James <ben.james@hmcts.net>
2026-02-26 15:55:00 +0000

    Removed unsafe eval from CSP

--- api/interfaces/csp-config.ts
+++ api/interfaces/csp-config.ts
@@ -6,7 +6,6 @@ export const MC_CSP: Parameters<typeof csp>[0] = {
     'https://www.googletagmanager.com',
     'https://www.google-analytics.com',
     'az416426.vo.msecnd.net',
-    "'unsafe-eval'",
   ],
 
   extraStyle: ['https://fonts.googleapis.com', 'https://fonts.gstatic.com', 'https://www.googletagmanager.com'],

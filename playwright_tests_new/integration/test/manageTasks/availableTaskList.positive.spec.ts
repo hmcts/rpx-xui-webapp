a018c78478abd0920cdab0b16aba49d9dc459f6c
danH-test <dan.hitchcock@hmcts.net>
2026-03-02 17:46:17 +0000

    Add a test to check for task assignement and update Page objects

--- playwright_tests_new/integration/test/manageTasks/availableTaskList.positive.spec.ts
+++ playwright_tests_new/integration/test/manageTasks/availableTaskList.positive.spec.ts
@@ -9,6 +9,7 @@ let sessionCookies: any[] = [];
 test.beforeEach(async ({ page }) => {
   const { cookies } = await applySessionCookies(page, userIdentifier);
   sessionCookies = cookies;
+  // workallocation/region-location
 });
 
 test.describe(`Available Task List as ${userIdentifier}`, () => {
@@ -149,6 +150,48 @@ test.describe(`Available Task List as ${userIdentifier}`, () => {
       await expect(taskListPage.sortByTaskTableHeader).toBeVisible();
       await expect(taskListPage.sortByDueDateTableHeader).toBeVisible();
       await expect(taskListPage.sortByHearingDateTableHeader).toBeVisible();
+    });
+  });
+
+  test(`User ${userIdentifier} can assign a task to themselves and see the expected notifications`, async ({
+    taskListPage,
+    page,
+  }) => {
+    const taskListMockResponse = buildTaskListMock(3, '', availableActionsList);
+    await test.step('Setup route mock for task list', async () => {
+      await page.route('**/workallocation/task*', async (route) => {
+        const body = JSON.stringify(taskListMockResponse);
+        await route.fulfill({ status: 200, contentType: 'application/json', body });
+      });
+      await page.route('**/claim', async (route) => {
+        const body = JSON.stringify({});
+        await route.fulfill({ status: 200, contentType: 'application/json', body });
+      });
+    });
+
+    await test.step('Navigate to the available tasks list page', async () => {
+      await taskListPage.goto();
+      await taskListPage.taskTableTabs.filter({ hasText: 'Available tasks' }).first().click();
+      await expect(taskListPage.taskListTable).toBeVisible();
+      await taskListPage.exuiSpinnerComponent.wait();
+    });
+
+    await test.step('Verify the UI shows the expected layout and data, given the mock response', async () => {
+      expect
+        .soft(await taskListPage.getResultsText())
+        .toBe(`Showing 1 to ${Math.min(taskListMockResponse.tasks.length, 25)} of ${taskListMockResponse.total_records} results`);
+    });
+
+    await test.step('Verify you can assign test', async () => {
+      await taskListPage.manageCaseButtons.first().click();
+      await expect(taskListPage.taskActionClaim).toBeVisible();
+      await taskListPage.taskActionClaim.click();
+      await expect(taskListPage.taskActionClaim).not.toBeVisible();
+      await expect(taskListPage.exuiBodyComponent.infoMessage).toBeVisible();
+      await expect(taskListPage.exuiBodyComponent.successMessage).toContainText(
+        `You've assigned yourself a task. It's available in My tasks.`
+      );
+      await expect(taskListPage.exuiBodyComponent.infoMessage).toContainText(`The list has been refreshed.`);
     });
   });
 });

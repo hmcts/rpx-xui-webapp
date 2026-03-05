0f562d9c0b851aa908b19c917b0ab123923895d8
danH-test <dan.hitchcock@hmcts.net>
2026-03-05 15:40:18 +0000

    Remove chrome from the nightly browser tests

--- playwright-nightly.config.ts
+++ playwright-nightly.config.ts
@@ -123,19 +123,6 @@ module.exports = defineConfig({
 
   projects: [
     {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-        headless: headlessMode,
-        trace: 'on-first-retry',
-        screenshot: {
-          mode: 'only-on-failure',
-          fullPage: true,
-        },
-        video: 'off',
-      },
-    },
-    {
       name: 'firefox',
       use: {
         ...devices['Desktop Firefox'],
@@ -160,14 +147,5 @@ module.exports = defineConfig({
         video: 'off',
       },
     },
-    // {
-    //   name: 'MicrosoftEdge',
-    //   use: { ...devices['Desktop Edge'],
-    //     channel: 'msedge',
-    //     screenshot: 'only-on-failure',
-    //     headless: headlessMode,
-    //     trace: 'off'
-    //   }
-    // }
   ],
 });

a8fd7cae2a25c99cf88c86d7525d6195d7c02ec4
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2026-04-01 13:58:40 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -71,7 +71,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_key_vault_secret" "app_insights_key" {
   name         = "appinsights-instrumentationkey-mc"
-  value        = module.application_insights.instrumentation_key
+  value        = azurerm_application_insights.appinsight.instrumentation_key
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
 

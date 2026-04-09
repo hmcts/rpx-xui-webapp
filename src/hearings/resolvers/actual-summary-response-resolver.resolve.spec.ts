963e6f4b171e66f3578225334e9c830d2c9a5742
Josh <josh.glasgow@hmcts.net>
2026-04-09 17:14:24 +0100

    add additional coverage flagged by sonar

--- src/hearings/resolvers/actual-summary-response-resolver.resolve.spec.ts
+++ src/hearings/resolvers/actual-summary-response-resolver.resolve.spec.ts
@@ -2,8 +2,11 @@ import { APP_BASE_HREF } from '@angular/common';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRouteSnapshot } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { provideMockStore } from '@ngrx/store/testing';
 import { initialState } from '../hearing.test.data';
+import * as actions from '../store/actions/hearing-actuals.action';
 import { ActualSummaryResponseResolver } from './actual-summary-response-resolver.resolve';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
@@ -24,5 +27,19 @@ describe('ActualSummaryResponseResolver', () => {
   it('should be created', () => {
     const service: ActualSummaryResponseResolver = TestBed.inject(ActualSummaryResponseResolver);
     expect(service).toBeTruthy();
+  });
+
+  it('should dispatch hearing actuals request with route params and query params', () => {
+    const service: ActualSummaryResponseResolver = TestBed.inject(ActualSummaryResponseResolver);
+    const store = TestBed.inject(Store);
+    const dispatchSpy = spyOn(store, 'dispatch');
+    const route = {
+      params: { id: 'hearing-123' },
+      queryParams: { caseRef: 'case-456' },
+    } as unknown as ActivatedRouteSnapshot;
+
+    service.resolve(route);
+
+    expect(dispatchSpy).toHaveBeenCalledWith(new actions.GetHearingActuals({ id: 'hearing-123', caseRef: 'case-456' }));
   });
 });

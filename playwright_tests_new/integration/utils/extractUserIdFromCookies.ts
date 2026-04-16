2896d9e369ad902ed92cafc74c85e894b57096d1
Chris Jones <christopher.jones@hmcts.net>
2026-04-16 13:40:55 +0100

    updated test util to log userOf from session cookie

--- playwright_tests_new/integration/utils/extractUserIdFromCookies.ts
+++ playwright_tests_new/integration/utils/extractUserIdFromCookies.ts
@@ -1,10 +1,23 @@
+import { logger } from '../../E2E/utils/logger.utils';
+
 /**
  * Extracts the __userid__ value from a cookies array.
  * @param {Array} cookies - Array of cookie objects (from session).
  * @returns {string|null} The __userid__ value, or null if not found.
  */
 export function extractUserIdFromCookies(cookies: any[]): string | null {
-  if (!Array.isArray(cookies)) return null;
+  if (!Array.isArray(cookies)) {
+    logger.info('extractUserIdFromCookies received a non-array cookies value', { hasUserId: false });
+    return null;
+  }
   const userIdCookie = cookies.find((c) => c.name === '__userid__');
-  return userIdCookie ? userIdCookie.value : null;
+  const userId = userIdCookie ? userIdCookie.value : null;
+
+  logger.info('extractUserIdFromCookies evaluated session cookies', {
+    hasUserId: Boolean(userId),
+    userIdLength: userId?.length ?? 0,
+    cookieCount: cookies.length,
+  });
+
+  return userId;
 }

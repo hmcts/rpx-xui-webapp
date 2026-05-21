37c0eb998d364e2239833365a784dd51a9dd2716
RiteshHMCTS <ritesh.dsouza@hmcts.net>
2026-05-21 20:15:37 +0100

    automate cve supprssion command on github push

--- bin/prepush.sh
+++ bin/prepush.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
-yarn cve:check & pid1=$!
+
+yarn npm audit --recursive --environment production --json > yarn-audit-known-issues
+cve_suppress_status=$?
+
+if [ $cve_suppress_status -ne 0 ] && [ ! -s yarn-audit-known-issues ]; then
+  printf "=============================================================\n" >&2
+  printf "Unable to refresh yarn-audit-known-issues\n" >&2
+  printf "=============================================================\n" >&2
+  exit $cve_suppress_status
+fi
+
+if ! git diff --quiet -- yarn-audit-known-issues; then
+  printf "=============================================================\n" >&2
+  printf "yarn-audit-known-issues was refreshed with the latest CVE audit output.\n" >&2
+  printf "Commit yarn-audit-known-issues and push again.\n" >&2
+  printf "=============================================================\n" >&2
+  exit 1
+fi
+
 yarn lint & pid2=$!
 yarn test:coverage:ng:prepush & pid3=$!
 
-wait $pid1; status1=$?
 wait $pid2; status2=$?
 wait $pid3; status3=$?
 
 failed=""
-if [ $status1 -ne 0 ]; then 
-  failed="$failed cve:check"
-fi
 if [ $status2 -ne 0 ]; then 
   failed="$failed lint"
 fi
@@ -21,11 +35,6 @@ fi
 if [ -n "$failed" ]; then
   printf "=============================================================\n" >&2
   printf "The following commands failed:$failed\n" >&2
-  if [ $status1 -ne 0 ]; then
-    printf "\n" >&2
-    printf "There are unsupressed vulnerabilities, you can run ( yarn cve:fix ) to auto fix this\n" >&2
-    printf "\n" >&2
-  fi
   printf "=============================================================\n" >&2
   exit 1
 fi

46cf1375c79ba8ed2afe787760f79223ae9ecc7a
RiteshHMCTS <ritesh.dsouza@hmcts.net>
2026-06-03 11:54:57 +0100

    removed workflow

--- bin/prepush.sh
+++ bin/prepush.sh
@@ -18,23 +18,4 @@ if ! git diff --quiet -- yarn-audit-known-issues; then
   exit 1
 fi
 
-yarn lint & pid2=$!
-yarn test:coverage:ng:prepush & pid3=$!
-
-wait $pid2; status2=$?
-wait $pid3; status3=$?
-
-failed=""
-if [ $status2 -ne 0 ]; then 
-  failed="$failed lint"
-fi
-if [ $status3 -ne 0 ]; then 
-  failed="$failed test:coverage:ng:prepush"
-fi
-
-if [ -n "$failed" ]; then
-  printf "=============================================================\n" >&2
-  printf "The following commands failed:$failed\n" >&2
-  printf "=============================================================\n" >&2
-  exit 1
-fi
+yarn lint

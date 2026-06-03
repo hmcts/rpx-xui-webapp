64559e563d2fec9b532e49d05f8b2c27424e4b7a
RiteshHMCTS <ritesh.dsouza@hmcts.net>
2026-06-03 13:54:33 +0100

    check cve suppression on change commit

--- bin/prepush.sh
+++ bin/prepush.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+yarn cve:check & pid1=$!
+yarn lint & pid2=$!
+yarn test:coverage:ng:prepush & pid3=$!
+
+wait $pid1; status1=$?
+wait $pid2; status2=$?
+wait $pid3; status3=$?
+
+failed=""
+if [ $status1 -ne 0 ]; then
+  failed="$failed cve:check"
+fi
+if [ $status2 -ne 0 ]; then
+  failed="$failed lint"
+fi
+if [ $status3 -ne 0 ]; then
+  failed="$failed test:coverage:ng:prepush"
+fi
+
 yarn npm audit --recursive --environment production --json > yarn-audit-known-issues
 cve_suppress_status=$?
 
@@ -18,4 +37,14 @@ if ! git diff --quiet -- yarn-audit-known-issues; then
   exit 1
 fi
 
-yarn lint
+if [ -n "$failed" ]; then
+  printf "=============================================================\n" >&2
+  printf "The following commands failed:$failed\n" >&2
+  if [ $status1 -ne 0 ]; then
+    printf "\n" >&2
+    printf "There are unsuppressed vulnerabilities, you can run ( yarn cve:fix ) to auto fix this\n" >&2
+    printf "\n" >&2
+  fi
+  printf "=============================================================\n" >&2
+  exit 1
+fi

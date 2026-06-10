06e9be89023fe2c354b88ae5852593f956222f83
Andrew Grizhenkov <andrew.grizhenkov@hmcts.net>
2026-06-10 09:59:48 +0100

    test: isolate share case selector spec

--- src/cases/store/selectors/share-case.selectors.spec.ts
+++ src/cases/store/selectors/share-case.selectors.spec.ts
@@ -1,42 +1,22 @@
-import { TestBed, waitForAsync } from '@angular/core/testing';
-import { select, Store, StoreModule } from '@ngrx/store';
-import { CaseListComponent } from '../../containers';
-import { getShareCaseListState, reducers, State } from '../index';
+import { SharedCase } from '@hmcts/rpx-xui-common-lib';
+import { getShareCaseListState } from '../index';
+import { ShareCasesState } from '../reducers';
 
 describe('Share case selectors', () => {
-  let store: Store<State>;
-
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      imports: [StoreModule.forRoot({}), StoreModule.forFeature('cases', reducers)],
-    });
-    store = TestBed.inject(Store);
-    spyOn(store, 'dispatch').and.callThrough();
-  }));
-
   describe('get share case state', () => {
     it('should return search state', () => {
-      const caseListComponent = new CaseListComponent(store, null, null, null, null, null, null);
-      caseListComponent.selectedCases = [
-        {
-          case_id: '1',
-          case_fields: {
-            solsSolicitorAppReference: 'James123',
-          },
-        },
-        {
-          case_id: '2',
-          case_fields: {
-            solsSolicitorAppReference: 'Steve321',
-          },
-        },
+      const selectedCases: SharedCase[] = [
+        { caseId: '1', caseTitle: 'James123' },
+        { caseId: '2', caseTitle: 'Steve321' },
       ];
-      caseListComponent.shareCaseSubmit();
-      let result = [];
-      store.pipe(select(getShareCaseListState)).subscribe((value) => {
-        result = value;
-      });
-      expect(result.length).toEqual(2);
+      const shareCaseState: ShareCasesState = {
+        shareCases: selectedCases,
+        loading: false,
+        error: undefined,
+        users: [],
+      };
+
+      expect(getShareCaseListState.projector(shareCaseState)).toEqual(selectedCases);
     });
   });
 });

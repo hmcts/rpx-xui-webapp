d4dc383d5b880c74312f497d52242ef2b89f81a9
anthony.dummer <anthony.dummer@hmcts.net>
2026-06-11 16:25:01 +0100

    Updating base config

--- api/configuration/menuConfigs/aat-diffs.ts
+++ api/configuration/menuConfigs/aat-diffs.ts
@@ -1,32 +1,10 @@
 export const aatDifferences = {
-  '(judge)|(judiciary)|(panelmember)': [
-    {
-      roles: ['caseworker-sscs-judge', 'caseworker-sscs-panelmember', 'hmcts-judiciary'],
-      text: 'My work',
-    },
-    {
-      roles: ['caseworker-sscs-judge', 'caseworker-sscs-panelmember', 'hmcts-judiciary'],
-      text: 'Search',
-    },
-  ],
+  '(judge)|(judiciary)|(panelmember)': [],
   '(pui-case-manager)': [
     {
       roles: ['caseworker-civil', 'caseworker-civil-solictor', 'caseworker-befta_master-solicitor'],
       text: 'Notice of change',
     },
   ],
-  '.+': [
-    {
-      roles: ['caseworker-sscs-clerk', 'caseworker-sscs-registrar', 'hmcts-admin', 'hmcts-ctsc', 'hmcts-legal-operations'],
-      text: 'My work',
-    },
-    {
-      roles: ['caseworker-sscs-clerk', 'caseworker-sscs-registrar', 'caseworker-probate', 'hmcts-admin', 'hmcts-ctsc', 'hmcts-legal-operations'],
-      text: 'Search',
-    },
-    {
-      roles: ['hmcts-admin', 'hmcts-ctsc', 'hmcts-legal-operations'],
-      text: 'Find case',
-    },
-  ],
+  '.+': [],
 };

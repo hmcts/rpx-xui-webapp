a6da53439e4809ecaf40c511fd30b9192647a332
JamesCatto <James.Catto@HMCTS.NET>
2026-07-17 14:31:07 +0100

    pofcc-329 - use the decentralisedServiceMap in the environment to get the service baseUrl and use it to construct the absolute URL

--- src/app/models/theming.model.ts
+++ src/app/models/theming.model.ts
@@ -18,5 +18,5 @@ export interface NavigationItem {
   notFlags?: FlagDefinition[];
   ngClass?: string;
   align?: 'right' | 'left';
-  isDecentralisedService?: false;
+  decentralisedServiceId?: string;
 }

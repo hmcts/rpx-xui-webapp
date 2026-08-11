7781abb387c5ab7bbafc1aa20a6fffddd7dc68a9
JamesCatto <James.Catto@HMCTS.NET>
2026-08-11 12:24:01 +0100

    pofcc-375 - renamed getDecentralisedWebUrl() to getUrlForCaseType() and reused this in api/noc

--- common/decentralisation/decentralised-redirect.util.ts
+++ common/decentralisation/decentralised-redirect.util.ts
@@ -2,30 +2,30 @@ import { DecentralisedCaseTypeMap } from './decentralised-casetype';
 
 const TEMPLATE_PLACEHOLDER = '%s';
 
-export const getDecentralisedWebUrl = (caseTypeConfig: DecentralisedCaseTypeMap, caseType?: string): string | null => {
-  if (!caseTypeConfig || !caseType) {
+export const getUrlForCaseType = (caseTypeMap: DecentralisedCaseTypeMap, caseType?: string): string | null => {
+  if (!caseTypeMap || !caseType) {
     return null;
   }
 
-  const configuredCaseType = getConfiguredCaseType(caseTypeConfig, caseType);
+  const configuredCaseType = getConfiguredCaseType(caseTypeMap, caseType);
   if (!configuredCaseType) {
     return null;
   }
 
-  const webUrl = caseTypeConfig[configuredCaseType].webUrl;
+  const webUrl = caseTypeMap[configuredCaseType].webUrl;
   return webUrl ? resolveUrl(webUrl, configuredCaseType, caseType) : null;
 };
 
-export const getConfiguredCaseType = (caseTypeConfig: DecentralisedCaseTypeMap, caseType: string): string | null => {
+const getConfiguredCaseType = (caseTypeMap: DecentralisedCaseTypeMap, caseType: string): string | null => {
   const lowerCaseType = caseType.toLowerCase();
   return (
-    Object.keys(caseTypeConfig)
+    Object.keys(caseTypeMap)
       .filter((configuredCaseType) => lowerCaseType.startsWith(configuredCaseType.toLowerCase()))
       .sort((first, second) => second.length - first.length)[0] || null
   );
 };
 
-export const resolveUrl = (url: string, configuredCaseType: string, caseType: string): string => {
+const resolveUrl = (url: string, configuredCaseType: string, caseType: string): string => {
   let resolvedUrl = url.replace(TEMPLATE_PLACEHOLDER, caseType.substring(configuredCaseType.length));
   while (resolvedUrl.endsWith('/')) {
     resolvedUrl = resolvedUrl.slice(0, -1);

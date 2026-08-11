c6a6b557d05414d4185464129be1a6b3229e0769
JamesCatto <James.Catto@HMCTS.NET>
2026-08-11 16:44:52 +0100

    pofcc-375 - added tests for common/decentralisation/decentralised-redirect.util.ts; allow tests in common to be executed individually and as part of yarn test

--- common/decentralisation/decentralised-redirect.util.spec.ts
+++ common/decentralisation/decentralised-redirect.util.spec.ts
@@ -1,1 +1,29 @@
-describe('getDecentralisedWebUrl', () => {});
+import * as assert from 'node:assert/strict';
+import { getUrlForCaseType } from './decentralised-redirect.util';
+
+describe('getUrlForCaseType', () => {
+  const caseTypeMap = {
+    A11: { webUrl: 'http://localhost:8080/a11' },
+    A1: { webUrl: 'http://localhost:8080/a' },
+    B2: { webUrl: 'http://localhost:8080/b' },
+    C3: { webUrl: 'http://localhost:8080/prefix/%s/suffix/' },
+  };
+
+  it('returns the URL for the given case type', () => {
+    assert.equal(getUrlForCaseType(caseTypeMap, 'A1'), 'http://localhost:8080/a');
+    assert.equal(getUrlForCaseType(caseTypeMap, 'A11'), 'http://localhost:8080/a11');
+    assert.equal(getUrlForCaseType(caseTypeMap, 'B2'), 'http://localhost:8080/b');
+
+    // N.B. the placeholder is replaced with a substring of the given case type that excludes the part of the case type that is the caseTypeMap key
+    assert.equal(getUrlForCaseType(caseTypeMap, 'C345'), 'http://localhost:8080/prefix/45/suffix');
+  });
+
+  it('returns null when there is no matching case type', () => {
+    assert.equal(getUrlForCaseType(caseTypeMap, 'A'), null);
+  });
+
+  it('returns null when there is no case type map or no case tpye', () => {
+    assert.equal(getUrlForCaseType({}, 'A'), null);
+    assert.equal(getUrlForCaseType({}, undefined), null);
+  });
+});

1261fb94b03a828411828b5d25acd3dcf885b9fb
Josh <josh.glasgow@hmcts.net>
2026-08-17 13:58:17 +0100

    use mv ver

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
 import { enableProdMode, CSP_NONCE } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { GlobalWorkerOptions } from 'pdfjs-dist';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { ENVIRONMENT_CONFIG } from './models/environmentConfig.model';
-
-// @hmcts/media-viewer 4.2.30 points at a .js file that is not included in the
-// package. Use the shipped ES module worker before the application bootstraps.
-GlobalWorkerOptions.workerSrc = '/assets/build/pdf.worker.min.mjs';
 
 /* ①  Read the <meta name="csp-nonce"> placed by the server template */
 const nonce = document.querySelector<HTMLMetaElement>('meta[name="csp-nonce"]')?.content || '';

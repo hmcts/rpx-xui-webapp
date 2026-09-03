6d4243e875d56daa05c3ea0d435a18f36d5784e5
Shamrez <shamrez.darwish@hmcts.net>
2026-09-03 17:20:37 +0100

    lint fix

--- playwright_tests_new/integration/test/hearings/hearingAdditionalFacilities.caseFlags.positive.spec.ts
+++ playwright_tests_new/integration/test/hearings/hearingAdditionalFacilities.caseFlags.positive.spec.ts
@@ -51,7 +51,10 @@ function buildCaseFlagsFixture(): ServiceFlag[] {
 }
 
 function buildServiceHearingValuesWithFixtureFlags(): ServiceHearingValuesModel {
-  const serviceHearingValues = buildServiceHearingValuesMock(undefined, LISTED_HEARING_SCENARIO) as unknown as ServiceHearingValuesModel;
+  const serviceHearingValues = buildServiceHearingValuesMock(
+    undefined,
+    LISTED_HEARING_SCENARIO
+  ) as unknown as ServiceHearingValuesModel;
   serviceHearingValues.caseFlags = {
     ...deepClone(serviceHearingValuesModel.caseFlags),
     flags: buildCaseFlagsFixture(),

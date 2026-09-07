37c4674e3e926f5100a3c9de0dcf8a7560df7777
jamescatto-cgi <James.Catto@HMCTS.NET>
2026-09-07 16:05:14 +0100

    POFCC-375 - remove duplication in decentralisation code between api/ and src/ (#5361)
    * pofcc-375 - Created common root module; moved decentralisation source into the common module; use common module in api/ and src/
    
    * pofcc-375 - Moved angular specific code into the Service
    
    * pofcc-375 - renamed getDecentralisedWebUrl() to getUrlForCaseType() and reused this in api/noc
    
    * pofcc-375 - tidied decentralised-redirect.service
    
    * pofcc-375 - added tests for common/decentralisation/decentralised-redirect.util.ts; allow tests in common to be executed individually and as part of yarn test
    
    * pofcc-375 - configure the project to treat JS files as commonjs to avoid node having to convert files
    
    * pofcc-375 - reinstated property nocBaseUrl incorrectly removed
    
    * pofcc-375 - only expose a subset of the configuration properties to the frontend angular code; this prevents using properties in the frontend that may cause failures.

--- src/models/environmentConfig.model.ts
+++ src/models/environmentConfig.model.ts
@@ -1,7 +1,7 @@
 import { InjectionToken } from '@angular/core';
 import { WAFeatureConfig } from '../work-allocation/models/common/service-config.model';
-import { DecentralisedService } from '../decentralisation/decentralised-service';
-import { DecentralisedCaseTypeMap } from '../decentralisation/decentralised-casetype';
+import { DecentralisedService } from '../../common/decentralisation/decentralised-service';
+import { CaseTypeMap, FrontendDecentralisedCaseType } from '../../common/decentralisation/decentralised-casetype';
 
 export const ENVIRONMENT_CONFIG = new InjectionToken<EnvironmentConfig>('environment.config');
 
@@ -21,6 +21,6 @@ export interface EnvironmentConfig {
     hearingJurisdictions: object;
     hearingAmendment: object;
   };
-  decentralisedCaseTypeConfig?: DecentralisedCaseTypeMap;
+  decentralisedCaseTypeConfig?: CaseTypeMap<FrontendDecentralisedCaseType>;
   decentralisedServiceMap?: Record<string, DecentralisedService>;
 }

2a46e2825793c7d79af8db28359bd7f3b9f016e0
Chris Jones <christopher.jones@hmcts.net>
2026-09-14 17:10:49 +0100

    improved unit test coverage

--- api/proxy.config.spec.ts
+++ api/proxy.config.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import 'mocha';
 import * as sinon from 'sinon';
+import * as amendedJurisdictions from './amendedJurisdictions';
 import * as configuration from './configuration';
 import * as middlewareProxy from './lib/middleware/proxy';
 import { initProxy } from './proxy.config';
@@ -21,7 +22,7 @@ describe('proxy configuration', () => {
     sandbox.restore();
   });
 
-  it('routes Data Store-backed CCD paths directly to the Data Store API', () => {
+  it('routes case searches to the Data Store API', () => {
     initProxy({} as any);
 
     const configs = applyProxyStub.getCalls().map((call) => call.args[1]);
@@ -30,19 +31,66 @@ describe('proxy configuration', () => {
       source: '/data/internal/searchCases',
       target: SERVICES_CCD_DATA_STORE_API_PATH,
     });
+  });
+
+  it('rewrites case-search URLs for the Data Store API', () => {
+    initProxy({} as any);
+
+    const configs = applyProxyStub.getCalls().map((call) => call.args[1]);
     const searchCasesConfig = configs.find((config) => config.source === '/data/internal/searchCases');
+
     expect(searchCasesConfig.rewriteUrl('/?ctid=PCS&use_case=WORKBASKET&page=1')).to.equal(
       '/internal/searchCases?ctid=PCS&use_case=WORKBASKET&page=1'
     );
+  });
+
+  it('routes Data Store paths to the Data Store API with URL rewriting', () => {
+    initProxy({} as any);
+
+    const configs = applyProxyStub.getCalls().map((call) => call.args[1]);
+
     expect(configs.find((config) => config.source === '/data')).to.include({
       target: SERVICES_CCD_DATA_STORE_API_PATH,
       rewrite: true,
     });
+  });
+
+  it('excludes case searches from the generic Data Store proxy', () => {
+    initProxy({} as any);
+
+    const configs = applyProxyStub.getCalls().map((call) => call.args[1]);
+
+    expect(configs.find((config) => config.source === '/data').filter).to.eql(['!/data/internal/searchCases']);
+  });
+
+  it('routes aggregated data to the Data Store API without URL rewriting', () => {
+    initProxy({} as any);
+
+    const configs = applyProxyStub.getCalls().map((call) => call.args[1]);
+
     expect(configs.find((config) => config.source === '/aggregated')).to.include({
       source: '/aggregated',
       target: SERVICES_CCD_DATA_STORE_API_PATH,
       rewrite: false,
     });
+  });
+
+  it('rewrites the caseworker UID before checking cached aggregated jurisdictions', () => {
+    const rewriteCaseworkerUidStub = sandbox.stub(amendedJurisdictions, 'rewriteCaseworkerUid');
+    const checkCachedJurisdictionsStub = sandbox.stub(amendedJurisdictions, 'checkCachedJurisdictions');
+    const proxyRequest = {};
+    const request = {};
+
+    initProxy({} as any);
+
+    const configs = applyProxyStub.getCalls().map((call) => call.args[1]);
+    const aggregatedConfig = configs.find((config) => config.source === '/aggregated');
+
+    aggregatedConfig.onReq(proxyRequest, request);
+
+    expect(rewriteCaseworkerUidStub.calledOnceWithExactly(proxyRequest, request)).to.be.true;
+    expect(checkCachedJurisdictionsStub.calledOnceWithExactly(proxyRequest, request)).to.be.true;
+    expect(rewriteCaseworkerUidStub.calledBefore(checkCachedJurisdictionsStub)).to.be.true;
   });
 
   it('keeps non-Data Store CCD proxy paths on the component gateway', () => {

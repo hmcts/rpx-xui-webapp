ac043621d99f163b151d93762e1942f075119e3f
Olu <142989683+olusegz07@users.noreply.github.com>
2026-09-23 11:22:18 +0100

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -28,7 +28,7 @@ variable "application_type" {
 }
 
 variable "sampling_percentage" {
-  default     = 100
+  default     = 1
   description = "Specifies the sampling percentage for Application Insights"
 }
 
